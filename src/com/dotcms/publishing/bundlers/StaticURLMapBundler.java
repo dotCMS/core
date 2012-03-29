c3303ead0018d0420e001e2d137b46bc68d16891
Will Ezell <will@dotcms.com>
2012-03-29 10:43:20 -0400

    #10
    Former-commit-id: 185321c5eef701d0848d83517275d23bccab7c86

---
+++ src/com/dotcms/publishing/bundlers/StaticURLMapBundler.java
@@ -0,0 +1,22 @@
+package com.dotcms.publishing.bundlers;
+
+import java.io.File;
+
+import com.dotcms.publishing.IBundler;
+import com.dotcms.publishing.PublisherConfig;
+
+public class StaticURLMapBundler implements IBundler {
+
+	@Override
+	public void setConfig(PublisherConfig pc) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public long generate(File bundleRoot) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
