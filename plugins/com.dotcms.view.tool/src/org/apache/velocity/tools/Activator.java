e2692dbde34bda317daf53750f50e7fd8d5ba4e3
michele.mastrogiovanni@gmail.com <michele.mastrogiovanni@gmail.com>
2012-03-30 10:42:10 +0200

    Added felix configuration and auto-deploy directory. In the dotCMS/felix/bundle directory are also preloaded osgi libraries. Added Activator for Osgi Contex registration. Add ViewTool service availability on startup hook for OSGI plugins. #14

---
+++ plugins/com.dotcms.view.tool/src/org/apache/velocity/tools/Activator.java
@@ -0,0 +1,11 @@
+package org.apache.velocity.tools;
+
+import com.dotmarketing.osgi.AbstractViewToolActivator;
+
+public class Activator extends AbstractViewToolActivator {
+	
+	public Activator() {
+		super(new MicheleToolInfo());
+	}
+
+}
