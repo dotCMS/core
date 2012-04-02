e403d8214713ea04264a45a9061781f35604550e
michele.mastrogiovanni@gmail.com <michele.mastrogiovanni@gmail.com>
2012-04-02 11:54:22 +0200

    Added OSGI support for WorkFlowActionlets
    Former-commit-id: 0b2f03971bc170bac4eb20fc32e91b7c216df790

---
+++ plugins/com.dotcms.actionlet/src/org/apache/velocity/tools/MyActionlet.java
@@ -0,0 +1,38 @@
+package org.apache.velocity.tools;
+
+import java.util.List;
+import java.util.Map;
+
+import com.dotmarketing.portlets.workflows.actionlet.WorkFlowActionlet;
+import com.dotmarketing.portlets.workflows.model.WorkflowActionClassParameter;
+import com.dotmarketing.portlets.workflows.model.WorkflowActionFailureException;
+import com.dotmarketing.portlets.workflows.model.WorkflowActionletParameter;
+import com.dotmarketing.portlets.workflows.model.WorkflowProcessor;
+
+public class MyActionlet extends WorkFlowActionlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public List<WorkflowActionletParameter> getParameters() {
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return "My Osgi Actionlet";
+	}
+
+	@Override
+	public String getHowTo() {
+		return null;
+	}
+
+	@Override
+	public void executeAction(WorkflowProcessor processor, Map<String, WorkflowActionClassParameter> params)
+			throws WorkflowActionFailureException {
+		System.out.println("Launch actionlet");
+	}
+
+	
+}
