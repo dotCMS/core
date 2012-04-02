30fc258b097653c9cbd4b253d7ee74037c71ae54
michele.mastrogiovanni@gmail.com <michele.mastrogiovanni@gmail.com>
2012-04-02 11:54:22 +0200

    Added OSGI support for WorkFlowActionlets

---
+++ plugins/com.dotcms.actionlet/src/org/apache/velocity/tools/Activator.java
@@ -0,0 +1,11 @@
+package org.apache.velocity.tools;
+
+import com.dotmarketing.osgi.AbstractWorkFlowActionletActivator;
+
+public class Activator extends AbstractWorkFlowActionletActivator {
+	
+	public Activator() {
+		super(MyActionlet.class);
+	}
+
+}
