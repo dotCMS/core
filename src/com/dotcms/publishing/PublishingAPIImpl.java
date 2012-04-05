ada98e6c2ffcc9e53ceb50d80545eeea6c442e84
Will Ezell <will@dotcms.com>
2012-04-05 15:27:23 -0400

    #10
    Former-commit-id: 86fe6e5630cc0179ea704d6252d3bf2dc22f600c

--- src/com/dotcms/publishing/PublishingAPIImpl.java
+++ src/com/dotcms/publishing/PublishingAPIImpl.java
@@ -1,5 +1,6 @@
 package com.dotcms.publishing;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,29 +14,47 @@ public class PublishingAPIImpl implements PublisherAPI {
 		
 		
 		
+		try {
 		
-		
-		List<Publisher> pubs = new ArrayList<Publisher>();
+			List<Publisher> pubs = new ArrayList<Publisher>();
+	
 
-		util.initBundle(config);
-		
-		for (Class<Publisher> c : config.getPublishers()) {
-
-			Publisher p;
-			try {
-				p = c.newInstance();
-			} catch (Exception e) {
-				Logger.error(PublishingAPIImpl.class,e.getMessage());
-				throw new DotPublishingException(e.getMessage());
-			} 
-
-			config = p.init(config);
-			pubs.add(p);
-		}
-
-		for (Publisher p : pubs) {
-			p.process();
-		}
+			
+			List<Class> bundlers = new ArrayList<Class>();
+			for (Class<Publisher> c : config.getPublishers()) {
+				Publisher p = c.newInstance();
+				
+				for(Class clazz : p.getBundlers()){
+					if(!bundlers.contains(clazz)){
+						bundlers.add(c);
+					}
+				}
+			}
+			config.put("bundlers", bundlers);
+			File bundleRoot = util.initBundle(config);
+			
+			
+			
+			for (Class c : bundlers) {
+				IBundler b = (IBundler) c.newInstance();
+				b.generate(bundleRoot);
+			}
+			
+			for (Class<Publisher> c : config.getPublishers()) {
+	
+				Publisher p = c.newInstance();
+	
+				config = p.init(config);
+				pubs.add(p);
+			}
+	
+			for (Publisher p : pubs) {
+				p.process();
+			}
+		} catch (Exception e) {
+			Logger.error(PublishingAPIImpl.class,e.getMessage());
+			throw new DotPublishingException(e.getMessage());
+		} 
 
 	}
 
