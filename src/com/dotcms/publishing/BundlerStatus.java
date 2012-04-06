e0f4662ae33d25fd5178ece9fe0a851481cd39d1
Will Ezell <will@dotcms.com>
2012-04-05 20:30:00 -0400

    #10

--- src/com/dotcms/publishing/BundlerStatus.java
+++ src/com/dotcms/publishing/BundlerStatus.java
@@ -1,32 +1,54 @@
 package com.dotcms.publishing;
 
 public class BundlerStatus {
-
+	private long total = 0;
 	private long count = 0;
 	private long failures = 0;
+
 	/**
 	 * @return the count
 	 */
 	public long getCount() {
 		return count;
 	}
+
 	/**
-	 * @param count the count to set
+	 * get the total
+	 * 
+	 * @return
+	 */
+	public long getTotal() {
+		return total;
+	}
+	/**
+	 * sets the total 
+	 * @param tot
+	 */
+	public void setTotal(long tot) {
+		this.total = tot;
+	}
+
+	/**
+	 * @param count
+	 *            the count to set
 	 */
 	public void addCount() {
-		count= count +1;
+		count = count + 1;
 	}
+
 	/**
 	 * @return the failuers
 	 */
 	public long getFailuers() {
 		return failures;
 	}
+
 	/**
-	 * @param failuers the failuers to set
+	 * @param failuers
+	 *            the failuers to set
 	 */
 	public void addFailuer() {
-		failures = failures+ 1;
+		failures = failures + 1;
 	}
-	
+
 }
