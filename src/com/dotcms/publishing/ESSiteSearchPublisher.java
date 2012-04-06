006680bccd515545f048bdfcf16f2d4bf61da262
Will Ezell <will@dotcms.com>
2012-04-05 21:06:52 -0400

    #10

--- src/com/dotcms/publishing/ESSiteSearchPublisher.java
+++ src/com/dotcms/publishing/ESSiteSearchPublisher.java
@@ -4,12 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.dotcms.publishing.bundlers.FileObjectBundler;
-import com.dotmarketing.beans.Host;
-import com.dotmarketing.business.APILocator;
-import com.dotmarketing.portlets.folders.model.Folder;
-import com.dotmarketing.portlets.structure.factories.StructureFactory;
-import com.dotmarketing.portlets.structure.model.Structure;
-import com.dotmarketing.util.Logger;
 
 public class ESSiteSearchPublisher extends Publisher {
 
@@ -47,56 +41,7 @@ public class ESSiteSearchPublisher extends Publisher {
 	@Override
 	public PublisherConfig process() throws DotPublishingException {
 
-		List<Host> hosts = config.getHosts();
-		List<Folder> folders = config.getFolders();
-		List<Structure> strucs = config.getStructures();
-		
-		
-		if(hosts == null || hosts.size() ==0){
-			try {
-				hosts = APILocator.getHostAPI().findAll(config.getUser(), true);
-			} catch (Exception e) {
-				Logger.error(ESSiteSearchPublisher.class,e.getMessage(),e);
-				throw new DotPublishingException(e.getMessage());
-			} 
-		}
-		
-		if(strucs == null || strucs.size() ==0){
-			strucs = StructureFactory.getStructures();
-		}
-		
-		
-		for(Host h : hosts){
-			
-			
-			
-			
-		}
-		
-		
-		for(Folder f : folders){
-			
-			
-			
-			
-		}
-		
-		
-		for(Structure s : strucs){
-			
-			
-			
-			
-		}
-
-		
-		
-		
 		return config;
-		
-		
-		
-		
 		
 		
 		
