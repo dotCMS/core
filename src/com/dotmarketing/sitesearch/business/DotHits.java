8891fda9c040db67445c9290bb8da68de6003ae4
Jason Tesser <jason.tesser@dotcms.com>
2012-04-12 11:33:16 -0400

    #89
    Former-commit-id: 74572fd276006d7d5ad901163a2024b3695829f3

--- src/com/dotmarketing/sitesearch/business/DotHits.java
+++ src/com/dotmarketing/sitesearch/business/DotHits.java
@@ -1,46 +1,43 @@
 package com.dotmarketing.sitesearch.business;
 
 
-import org.apache.nutch.searcher.Hit;
-import org.apache.nutch.searcher.HitDetails;
-import org.apache.nutch.searcher.Summary;
 
 public class DotHits {
 	
-	private Hit[] hits;
+//	private Hit[] hits;
 	
-	private HitDetails[] details;
+//	private HitDetails[] details;
 	
-	private Summary[] summary;
+//	private Summary[] summary;
 	
 	private int length;
 	
     private long totalHits;
 
     
-	public Hit[] getHits() {
-		return hits;
-	}
+//	public Hit[] getHits() {
+//		return hits;
+//	}
 
-	public void setHits(Hit[] hits) {
-		this.hits = hits;
-	}
-
-	public HitDetails[] getDetails() {
-		return details;
-	}
-
-	public void setDetails(HitDetails[] details) {
-		this.details = details;
-	}
-
-	public Summary[] getSummary() {
-		return summary;
-	}
-
-	public void setSummary(Summary[] summary) {
-		this.summary = summary;
-	}
+//	public void setHits(Hit[] hits) {
+//		this.hits = hits;
+//	}
+//
+//	public HitDetails[] getDetails() {
+//		return details;
+//	}
+//
+//	public void setDetails(HitDetails[] details) {
+//		this.details = details;
+//	}
+//
+//	public Summary[] getSummary() {
+//		return summary;
+//	}
+//
+//	public void setSummary(Summary[] summary) {
+//		this.summary = summary;
+//	}
 
 	public int getLength() {
 		return length;
