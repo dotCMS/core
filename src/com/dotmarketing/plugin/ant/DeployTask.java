81bd38f285bf6f6b5666be3ab59df30e5149b741
Jonathan Gamba <jonathan.gamba@dotcms.com>
2012-04-16 10:30:54 -0600

    Working on issue #110
    Former-commit-id: fc1c478e5fca7c1b7d2880df29b2d9af2d4d5613

--- src/com/dotmarketing/plugin/ant/DeployTask.java
+++ src/com/dotmarketing/plugin/ant/DeployTask.java
@@ -1,5 +1,6 @@
 package com.dotmarketing.plugin.ant;
 
+import com.dotmarketing.plugin.util.PluginFileMerger;
 import com.dotmarketing.plugin.util.PluginRoot;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Logger;
@@ -7,51 +8,44 @@ import org.apache.log4j.PatternLayout;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 
-import com.dotmarketing.plugin.util.PluginFileMerger;
-
 
 /**
- * 
  * @author Andres Olarte
  * @since 1.6.5c
  */
 public class DeployTask extends Task {
 
-	private String root;
-	/**
-	 * The
-	 */
-	private String plugins;
-	
-	@Override
-	public void execute() throws BuildException {
-		//Init log4j to see the messages in ant's output
-		Logger logRoot = Logger.getRootLogger();
-		if (!logRoot.getAllAppenders().hasMoreElements()) {
-			logRoot.addAppender(new ConsoleAppender(   new PatternLayout("%m%n")));
-		}
-        new PluginRoot(root, plugins).deploy();
-		new PluginFileMerger().mergePlugins(root, plugins);
-	}
+    private String root;
+    private String plugins;
 
-	/**
-	 * Set the root of the web app (Servlet context)
-	 * 
-	 * @param root
-	 *            The root of the web app
-	 */
-	public synchronized void setRoot(String root) {
-		this.root = root;
-	}
+    @Override
+    public void execute () throws BuildException {
 
-	/**
-	 * Set the directory where the plugins live
-	 * 
-	 * @param plugins
-	 *            The directory where the plugins live
-	 */
-	public synchronized void setPlugins(String plugins) {
-		this.plugins = plugins;
-	}
+        //Init log4j to see the messages in ant's output
+        Logger logRoot = Logger.getRootLogger();
+        if ( !logRoot.getAllAppenders().hasMoreElements() ) {
+            logRoot.addAppender( new ConsoleAppender( new PatternLayout( "%m%n" ) ) );
+        }
+        new PluginRoot( root, plugins ).deploy();//Plugin that will allow any file to be overridden or added.
+        new PluginFileMerger().mergePlugins( root, plugins );
+    }
+
+    /**
+     * Set the root of the web app (Servlet context)
+     *
+     * @param root The root of the web app
+     */
+    public synchronized void setRoot ( String root ) {
+        this.root = root;
+    }
+
+    /**
+     * Set the directory where the plugins live
+     *
+     * @param plugins The directory where the plugins live
+     */
+    public synchronized void setPlugins ( String plugins ) {
+        this.plugins = plugins;
+    }
 
 }
