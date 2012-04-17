9255a487198aa9380210231bbd904706f737bd1b
Jorge Urdaneta <jorge.urdaneta@dotcms.com>
2012-04-17 12:04:54 -0430

    #149 moved mssql specific code to the right place

--- src/com/dotmarketing/portlets/workflows/business/MSSQLWorkflowSQL.java
+++ src/com/dotmarketing/portlets/workflows/business/MSSQLWorkflowSQL.java
@@ -2,8 +2,8 @@ package com.dotmarketing.portlets.workflows.business;
 
  class MSSQLWorkflowSQL extends WorkflowSQL{
 
-	
-	
-	
-
+     // tweaks for MSSQL because of "key" field in workflow_action_class_pars
+     protected static String INSERT_ACTION_CLASS_PARAM= "insert into workflow_action_class_pars (id,workflow_action_class_id,[key],value) values (?,?, ?, ?)";
+     protected static String UPDATE_ACTION_CLASS_PARAM= "update workflow_action_class_pars set workflow_action_class_id= ?, [key]=?, value=? where id =?";
+     
 }
