78e9aed4262719dc7ac2d98564a9b61de716b330
Jorge Urdaneta <jorge.urdaneta@dotcms.com>
2012-04-17 12:07:50 -0430

    #149 moved mssql tweaks to constructor to avoid sql code to be repeated

--- src/com/dotmarketing/portlets/workflows/business/MSSQLWorkflowSQL.java
+++ src/com/dotmarketing/portlets/workflows/business/MSSQLWorkflowSQL.java
@@ -2,8 +2,9 @@ package com.dotmarketing.portlets.workflows.business;
 
  class MSSQLWorkflowSQL extends WorkflowSQL{
 
-     // tweaks for MSSQL because of "key" field in workflow_action_class_pars
-     protected static String INSERT_ACTION_CLASS_PARAM= "insert into workflow_action_class_pars (id,workflow_action_class_id,[key],value) values (?,?, ?, ?)";
-     protected static String UPDATE_ACTION_CLASS_PARAM= "update workflow_action_class_pars set workflow_action_class_id= ?, [key]=?, value=? where id =?";
-     
+     public MSSQLWorkflowSQL() {
+         // tweaks for MSSQL because of "key" field in workflow_action_class_pars
+         INSERT_ACTION_CLASS_PARAM=super.INSERT_ACTION_CLASS_PARAM.replace("key", "[key]");
+         UPDATE_ACTION_CLASS_PARAM=super.UPDATE_ACTION_CLASS_PARAM.replace("key", "[key]");
+     }
 }
