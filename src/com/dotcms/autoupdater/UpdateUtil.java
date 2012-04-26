4be61047747c88623273ea3df25e5414efdbc843
Jonathan Gamba <jonathan.gamba@dotcms.com>
2012-04-26 17:04:22 -0600

    Working on issue #102 autoupdater for 2.0

--- src/com/dotcms/autoupdater/UpdateUtil.java
+++ src/com/dotcms/autoupdater/UpdateUtil.java
@@ -286,4 +286,54 @@ public class UpdateUtil {
         return dir.delete();
     }
 
+    /**
+     * Copy the contents of a given folder into another
+     *
+     * @param src
+     * @param dest
+     * @throws IOException
+     */
+    public static void copyFolder ( File src, File dest ) throws IOException {
+
+        if ( src.isDirectory() ) {
+
+            //if directory not exists, create it
+            if ( !dest.exists() ) {
+                dest.mkdir();
+                UpdateAgent.logger.debug( "Directory copied from " + src + "  to " + dest );
+            }
+
+            //list all the directory contents
+            String files[] = src.list();
+
+            for ( String file : files ) {
+
+                //construct the src and dest file structure
+                File srcFile = new File( src, file );
+                File destFile = new File( dest, file );
+
+                //recursive copy
+                copyFolder( srcFile, destFile );
+            }
+
+        } else {
+
+            //if file, then copy it (Use bytes stream to support all file types)
+            InputStream in = new FileInputStream( src );
+            OutputStream out = new FileOutputStream( dest );
+
+            byte[] buffer = new byte[1024];
+
+            int length;
+            //copy the file content in bytes
+            while ( ( length = in.read( buffer ) ) > 0 ) {
+                out.write( buffer, 0, length );
+            }
+
+            in.close();
+            out.close();
+            UpdateAgent.logger.debug( "File copied from " + src + " to " + dest );
+        }
+    }
+
 }
