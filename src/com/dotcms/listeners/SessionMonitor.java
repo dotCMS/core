4191f9ec8fb22e7ee7c48018b80bda37066eba4c
Jorge Urdaneta <jorge.urdaneta@dotcms.com>
2012-05-18 16:58:20 -0430

    #373 complete feature to see threads & users

--- src/com/dotcms/listeners/SessionMonitor.java
+++ src/com/dotcms/listeners/SessionMonitor.java
@@ -3,65 +3,112 @@ package com.dotcms.listeners;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import javax.servlet.ServletContext;
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionAttributeListener;
 import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 
-import com.dotmarketing.util.Logger;
 import com.dotmarketing.util.WebKeys;
 
-/** 
- *  Listener that keeps track of logged in users
- *  by monitoring for USER_ID session attribute addition.
- *  
- *  By: IPFW Web Team
+/**
+ * Listener that keeps track of logged in users by monitoring for USER_ID
+ * session attribute addition.
+ * 
+ * By: IPFW Web Team
  */
-public class SessionMonitor implements HttpSessionAttributeListener {
-  
-  // this will hold all logged in users
-  private Map<String, String> sysUsers = new ConcurrentHashMap<String, String>();
-  // getter for the bean - sysUsers
-  public Map<String, String> getUsers(){
-	return( sysUsers );
-  }
-	
-  /** Checks if the attribute added was USER_ID
-   *  if so, adds the login user to the bean.
-   *  
-   *  The bean is then saved to the servlet context.
-   */
-  public void attributeAdded(HttpSessionBindingEvent event) {
-    String currentAttributeName = event.getName().toString();
-	if(currentAttributeName.equals("USER_ID")){
-		String currentItemName = event.getValue().toString();
-		String id = event.getSession().getId();
-		sysUsers.put(id, currentItemName);
-		ServletContext context = event.getSession().getServletContext();			
-		context.setAttribute(WebKeys.LOGGED_USERS, sysUsers);	
-	}
-	   
-  }
-
-  /** 
-   *  Checks if the attribute removed is "USER_ID".
-   *  If so, remove the logout user from the bean.
-   * 
-   */
-  public void attributeRemoved(HttpSessionBindingEvent event) {
-	String currentAttributeName = event.getName().toString();
+public class SessionMonitor implements ServletRequestListener,
+        HttpSessionAttributeListener, HttpSessionListener {
     
-	if(currentAttributeName.equals("USER_ID")){
-		
-		String id = event.getSession().getId();
-		
-		sysUsers.remove(id);
-		event.getSession().getServletContext().setAttribute(WebKeys.LOGGED_USERS, sysUsers);
-	}
-  }
-
-  /** 
-   *  Do nothing here.
-   */
-  public void attributeReplaced(HttpSessionBindingEvent event) {}
-
+    // this will hold all logged in users
+    private Map<String, String> sysUsers = new ConcurrentHashMap<String, String>();
+    private Map<String, HttpSession> userSessions = new ConcurrentHashMap<String, HttpSession>();
+    private Map<String, String> sysUsersAddress = new ConcurrentHashMap<String, String>();
+    
+    public Map<String, String> getSysUsers() {
+        return sysUsers;
+    }
+    
+    public Map<String, HttpSession> getUserSessions() {
+        return userSessions;
+    }
+    
+    public Map<String, String> getSysUsersAddress() {
+        return sysUsersAddress;
+    }
+    
+    public void attributeAdded(HttpSessionBindingEvent event) {
+    }
+    
+    /**
+     * Checks if the attribute removed is "USER_ID". If so, remove the logout
+     * user
+     * 
+     */
+    public void attributeRemoved(HttpSessionBindingEvent event) {
+        String currentAttributeName = event.getName().toString();
+        
+        if (currentAttributeName.equals("USER_ID")) {
+            
+            String id = event.getSession().getId();
+            
+            sysUsers.remove(id);
+            userSessions.remove(id);
+            sysUsersAddress.remove(id);
+        }
+    }
+    
+    /**
+     * Do nothing here.
+     */
+    public void attributeReplaced(HttpSessionBindingEvent event) {
+    }
+    
+    public void sessionCreated(HttpSessionEvent event) {
+    }
+    
+    public void sessionDestroyed(HttpSessionEvent event) {
+        String userId = (String) event.getSession().getAttribute("USER_ID");
+        if (userId != null) {
+            String id = event.getSession().getId();
+            sysUsers.remove(id);
+            userSessions.remove(id);
+            sysUsersAddress.remove(id);
+        }
+    }
+    
+    @Override
+    public void requestDestroyed(ServletRequestEvent arg0) {
+    }
+    
+    @Override
+    public void requestInitialized(ServletRequestEvent event) {
+        HttpSession session = ((HttpServletRequest) event.getServletRequest())
+                .getSession(false);
+        if (session != null && session.getAttribute("USER_ID") != null) {
+            String userId = (String) session.getAttribute("USER_ID");
+            String id = session.getId();
+            synchronized (sysUsers) {
+                if (!sysUsers.containsKey(id)) {
+                    sysUsers.put(id, userId);
+                }
+            }
+            synchronized (userSessions) {
+                if (!userSessions.containsKey(id)) {
+                    userSessions.put(id, session);
+                }
+            }
+            synchronized (sysUsersAddress) {
+                if (!sysUsersAddress.containsKey(id)) {
+                    sysUsersAddress.put(id, event.getServletRequest()
+                            .getRemoteAddr());
+                }
+            }
+        }
+        event.getServletContext().setAttribute(WebKeys.USER_SESSIONS, this);
+    }
+    
 }
