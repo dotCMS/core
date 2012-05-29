44a26f73203a591c5afa64d9e8f5611249f88bce
Jorge Urdaneta <jorge.urdaneta@dotcms.com>
2012-05-29 16:15:49 -0430

    #378 completed implementation for oracle. Fix for postgres
    Former-commit-id: 54ed27e54f1900c106ae67dcc0d36eabf630e9a0

--- src/com/dotmarketing/portlets/workflows/business/PostgresWorkflowSQL.java
+++ src/com/dotmarketing/portlets/workflows/business/PostgresWorkflowSQL.java
@@ -5,7 +5,7 @@ class PostgresWorkflowSQL extends WorkflowSQL{
     public PostgresWorkflowSQL() {
         SELECT_EXPIRED_TASKS =
             "select workflow_task.id from workflow_task join workflow_step on (workflow_task.status=workflow_step.id) "+
-            "where workflow_step.resolved=false and workflow_task.escalation_enable=true and now()>workflow_task.mod_date+ "+
+            "where workflow_step.resolved=false and workflow_step.escalation_enable=true and now()>workflow_task.mod_date+ "+
             "cast(cast(workflow_step.escalation_time as varchar(20))||' seconds' as interval) ";
 
     }
