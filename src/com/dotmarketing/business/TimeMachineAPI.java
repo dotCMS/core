c351f0ddb280c3a1951112ea53992dd74b848cd5
michele.mastrogiovanni@gmail.com <michele.mastrogiovanni@gmail.com>
2012-05-31 12:44:39 +0200

    Added TimeMachine filter and simple jsp to administrate it

---
+++ src/com/dotmarketing/business/TimeMachineAPI.java
@@ -0,0 +1,16 @@
+package com.dotmarketing.business;
+
+import java.util.Date;
+import java.util.List;
+
+import com.dotcms.publishing.PublishStatus;
+import com.dotmarketing.beans.Host;
+import com.dotmarketing.exception.DotDataException;
+
+public interface TimeMachineAPI {
+
+	public List<Date> getAvailableTimeMachineForSite(Host host) throws DotDataException;
+	
+	public PublishStatus startTimeMachineForHostAndDate(Host host, Date date) throws DotDataException;
+	
+}
