8d1b2872c4d8aebef325d6ae80b975eccb0842a1
Jonathan Gamba <jonathan.gamba@dotcms.com>
2012-06-06 12:16:52 -0600

    Working on issue #472
    Former-commit-id: 70350bf66f8fd401a5a426e42e22858f484b0b89

---
+++ src/com/dotmarketing/startup/runonce/Task00920AddContentletVersionSystemHost.java
@@ -0,0 +1,71 @@
+package com.dotmarketing.startup.runonce;
+
+import com.dotmarketing.beans.Host;
+import com.dotmarketing.common.db.DotConnect;
+import com.dotmarketing.db.DbConnectionFactory;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotRuntimeException;
+import com.dotmarketing.startup.StartupTask;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * User: Jonathan Gamba
+ * Date: 6/6/12
+ * Time: 10:07 AM
+ */
+public class Task00920AddContentletVersionSystemHost implements StartupTask {
+
+    @Override
+    public boolean forceRun () {
+
+        return true;
+    }
+
+    @Override
+    public void executeUpgrade () throws DotDataException, DotRuntimeException {
+
+        //Update identifier table for the SYSTEM_HOST
+        DotConnect dc = new DotConnect();
+        dc.setSQL( "update identifier set asset_name = ? where id = ?" );
+        dc.addParam( "system host" );
+        dc.addParam( Host.SYSTEM_HOST );
+
+        dc.loadResult();
+
+        //Getting the SYSTEM_HOST contentlet
+        dc = new DotConnect();
+        dc.setSQL( "select inode, language_id from contentlet where title = 'System Host'" );
+
+        ArrayList<Map<String, String>> results = dc.loadResults();
+        if ( results != null && results.size() > 0 ) {
+
+            String inode = results.get( 0 ).get( "inode" );
+            String languageId = results.get( 0 ).get( "language_id" );
+
+            //Insert a contentlet version for the SYSTEM_HOST contentlet
+            dc = new DotConnect();
+            dc.setSQL( "insert into contentlet_version_info values (?,?,?,?,?,?,?,?)" );
+            dc.addParam( Host.SYSTEM_HOST );
+            dc.addParam( Long.valueOf( languageId ) );
+            dc.addParam( inode );
+            dc.addParam( inode );
+            if ( DbConnectionFactory.isPostgres() ) {
+                dc.addParam( false );
+            } else {
+                dc.addParam( DbConnectionFactory.getDBFalse() );
+            }
+            dc.addObject( null );
+            dc.addParam( new Date() );
+            dc.addParam( new Date() );
+
+            dc.loadResult();
+        } else {
+            throw new DotRuntimeException( "Error querying SYSTEM_HOST contentlet." );
+        }
+
+    }
+
+}
