cb2e82bdcacf8ff6126bb8fcfe728a4f4e284f1b
Samuel Mortha <sam.mortha@dotcms.com>
2012-06-07 23:40:36 +0530

    #417
    Former-commit-id: 501010c86bba75c6e9c110713d141631e1f198d1

---
+++ dotCMS/html/js/dojo/release/dojo/nls/dot-dojo_zh-tw.js
@@ -0,0 +1,482 @@
+require({cache:{
+'dijit/form/nls/zh/validate':function(){
+define(
+"dijit/form/nls/zh/validate", //begin v1.x content
+({
+	invalidMessage: "输入的值无效。",
+	missingMessage: "此值是必需值。",
+	rangeMessage: "此值超出范围。"
+})
+//end v1.x content
+);
+
+},
+'dijit/form/nls/zh-tw/validate':function(){
+define(
+"dijit/form/nls/zh-tw/validate", //begin v1.x content
+({
+	invalidMessage: "輸入的值無效。",
+	missingMessage: "必須提供此值。",
+	rangeMessage: "此值超出範圍。"
+})
+//end v1.x content
+);
+
+},
+'dojo/cldr/nls/zh/gregorian':function(){
+define(
+"dojo/cldr/nls/zh/gregorian", //begin v1.x content
+{
+	"months-format-narrow": [
+		"1月",
+		"2月",
+		"3月",
+		"4月",
+		"5月",
+		"6月",
+		"7月",
+		"8月",
+		"9月",
+		"10月",
+		"11月",
+		"12月"
+	],
+	"field-weekday": "周天",
+	"dateFormatItem-yQQQ": "y年QQQ",
+	"dateFormatItem-yMEd": "y年M月d日，E",
+	"dateFormatItem-MMMEd": "MMMd日E",
+	"eraNarrow": [
+		"公元前",
+		"公元"
+	],
+	"dayPeriods-format-wide-earlyMorning": "清晨",
+	"dayPeriods-format-wide-morning": "上午",
+	"dateFormat-long": "y年M月d日",
+	"months-format-wide": [
+		"1月",
+		"2月",
+		"3月",
+		"4月",
+		"5月",
+		"6月",
+		"7月",
+		"8月",
+		"9月",
+		"10月",
+		"11月",
+		"12月"
+	],
+	"dateTimeFormat-medium": "{1} {0}",
+	"dayPeriods-format-wide-pm": "下午",
+	"dateFormat-full": "y年M月d日EEEE",
+	"dateFormatItem-Md": "M-d",
+	"field-era": "时期",
+	"dateFormatItem-yM": "yyyy-M",
+	"months-standAlone-wide": [
+		"一月",
+		"二月",
+		"三月",
+		"四月",
+		"五月",
+		"六月",
+		"七月",
+		"八月",
+		"九月",
+		"十月",
+		"十一月",
+		"十二月"
+	],
+	"timeFormat-short": "ah:mm",
+	"quarters-format-wide": [
+		"第1季度",
+		"第2季度",
+		"第3季度",
+		"第4季度"
+	],
+	"timeFormat-long": "zah时mm分ss秒",
+	"field-year": "年",
+	"dateFormatItem-yMMM": "y年MMM",
+	"dateFormatItem-yQ": "y年QQQ",
+	"dateFormatItem-yyyyMMMM": "y年MMMM",
+	"field-hour": "小时",
+	"dateFormatItem-MMdd": "MM-dd",
+	"months-format-abbr": [
+		"1月",
+		"2月",
+		"3月",
+		"4月",
+		"5月",
+		"6月",
+		"7月",
+		"8月",
+		"9月",
+		"10月",
+		"11月",
+		"12月"
+	],
+	"dateFormatItem-yyQ": "yy年第Q季度",
+	"timeFormat-full": "zzzzah时mm分ss秒",
+	"field-day-relative+0": "今天",
+	"field-day-relative+1": "明天",
+	"field-day-relative+2": "后天",
+	"dateFormatItem-H": "H时",
+	"months-standAlone-abbr": [
+		"一月",
+		"二月",
+		"三月",
+		"四月",
+		"五月",
+		"六月",
+		"七月",
+		"八月",
+		"九月",
+		"十月",
+		"十一月",
+		"十二月"
+	],
+	"quarters-format-abbr": [
+		"1季",
+		"2季",
+		"3季",
+		"4季"
+	],
+	"quarters-standAlone-wide": [
+		"第1季度",
+		"第2季度",
+		"第3季度",
+		"第4季度"
+	],
+	"dateFormatItem-M": "M月",
+	"days-standAlone-wide": [
+		"星期日",
+		"星期一",
+		"星期二",
+		"星期三",
+		"星期四",
+		"星期五",
+		"星期六"
+	],
+	"dateFormatItem-yyMMM": "yy年MMM",
+	"timeFormat-medium": "ah:mm:ss",
+	"dateFormatItem-Hm": "H:mm",
+	"quarters-standAlone-abbr": [
+		"1季",
+		"2季",
+		"3季",
+		"4季"
+	],
+	"eraAbbr": [
+		"公元前",
+		"公元"
+	],
+	"field-minute": "分钟",
+	"field-dayperiod": "上午/下午",
+	"days-standAlone-abbr": [
+		"周日",
+		"周一",
+		"周二",
+		"周三",
+		"周四",
+		"周五",
+		"周六"
+	],
+	"dayPeriods-format-wide-night": "晚上",
+	"dateFormatItem-d": "d日",
+	"dateFormatItem-ms": "mm:ss",
+	"field-day-relative+-1": "昨天",
+	"dateFormatItem-h": "ah时",
+	"dateTimeFormat-long": "{1}{0}",
+	"field-day-relative+-2": "前天",
+	"dateFormatItem-MMMd": "MMMd日",
+	"dayPeriods-format-wide-midDay": "中午",
+	"dateFormatItem-MEd": "M-dE",
+	"dateTimeFormat-full": "{1}{0}",
+	"field-day": "日",
+	"days-format-wide": [
+		"星期日",
+		"星期一",
+		"星期二",
+		"星期三",
+		"星期四",
+		"星期五",
+		"星期六"
+	],
+	"field-zone": "区域",
+	"dateFormatItem-y": "y年",
+	"months-standAlone-narrow": [
+		"1月",
+		"2月",
+		"3月",
+		"4月",
+		"5月",
+		"6月",
+		"7月",
+		"8月",
+		"9月",
+		"10月",
+		"11月",
+		"12月"
+	],
+	"dateFormatItem-yyMM": "yy-MM",
+	"dateFormatItem-hm": "ah:mm",
+	"days-format-abbr": [
+		"周日",
+		"周一",
+		"周二",
+		"周三",
+		"周四",
+		"周五",
+		"周六"
+	],
+	"dateFormatItem-yMMMd": "y年MMMd日",
+	"eraNames": [
+		"公元前",
+		"公元"
+	],
+	"days-format-narrow": [
+		"日",
+		"一",
+		"二",
+		"三",
+		"四",
+		"五",
+		"六"
+	],
+	"field-month": "月",
+	"days-standAlone-narrow": [
+		"日",
+		"一",
+		"二",
+		"三",
+		"四",
+		"五",
+		"六"
+	],
+	"dateFormatItem-MMM": "LLL",
+	"dayPeriods-format-wide-am": "上午",
+	"dateFormatItem-MMMMdd": "MMMMdd日",
+	"dayPeriods-format-wide-weeHours": "凌晨",
+	"dateFormat-short": "yy-M-d",
+	"dayPeriods-format-wide-afternoon": "下午",
+	"field-second": "秒钟",
+	"dateFormatItem-yMMMEd": "y年MMMd日EEE",
+	"dateFormatItem-Ed": "d日E",
+	"field-week": "周",
+	"dateFormat-medium": "yyyy-M-d",
+	"dateFormatItem-yyyyM": "y年M月",
+	"dateTimeFormat-short": "{1} {0}",
+	"dateFormatItem-Hms": "H:mm:ss",
+	"dateFormatItem-hms": "ah:mm:ss",
+	"dateFormatItem-yyyy": "y年"
+}
+//end v1.x content
+);
+},
+'dojo/cldr/nls/zh-tw/gregorian':function(){
+define(
+"dojo/cldr/nls/zh-tw/gregorian", //begin v1.x content
+{
+	"quarters-standAlone-wide": [
+		"第1季",
+		"第2季",
+		"第3季",
+		"第4季"
+	],
+	"quarters-format-abbr": [
+		"第1季",
+		"第2季",
+		"第3季",
+		"第4季"
+	],
+	"dateFormat-medium": "yyyy/M/d",
+	"field-second": "秒",
+	"quarters-standAlone-abbr": [
+		"第1季",
+		"第2季",
+		"第3季",
+		"第4季"
+	],
+	"dateFormatItem-MMdd": "MM/dd",
+	"dateFormatItem-MEd": "M/d（E）",
+	"dateFormatItem-yMEd": "yyyy/M/d（EEE）",
+	"field-week": "週",
+	"dateFormatItem-H": "H時",
+	"eraNarrow": [
+		"西元前",
+		"西元"
+	],
+	"field-day-relative+-3": "大前天",
+	"timeFormat-full": "zzzzah時mm分ss秒",
+	"dateFormatItem-Md": "M/d",
+	"months-standAlone-narrow": [
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		"10",
+		"11",
+		"12"
+	],
+	"eraNames": [
+		"西元前",
+		"西元"
+	],
+	"field-minute": "分鐘",
+	"field-hour": "小時",
+	"field-day-relative+2": "後天",
+	"field-day-relative+3": "大後天",
+	"dateFormat-short": "yy/M/d",
+	"dateFormatItem-yMMMEd": "y年M月d日EEE",
+	"field-era": "年代",
+	"dateFormatItem-yM": "yyyy/M",
+	"timeFormat-long": "zah時mm分ss秒",
+	"eraAbbr": [
+		"西元前",
+		"西元"
+	],
+	"dateFormatItem-h": "ah時",
+	"dateFormatItem-yMMM": "y年M月",
+	"quarters-format-wide": [
+		"第1季",
+		"第2季",
+		"第3季",
+		"第4季"
+	],
+	"field-weekday": "週天",
+	"field-zone": "區域",
+	"dateFormatItem-Ed": "d日(E)"
+}
+//end v1.x content
+);
+},
+'dijit/nls/zh/loading':function(){
+define(
+"dijit/nls/zh/loading", //begin v1.x content
+({
+	loadingState: "正在加载...",
+	errorState: "对不起，发生了错误"
+})
+//end v1.x content
+);
+
+},
+'dijit/nls/zh-tw/loading':function(){
+define(
+"dijit/nls/zh-tw/loading", //begin v1.x content
+({
+	loadingState: "載入中...",
+	errorState: "抱歉，發生錯誤"
+})
+//end v1.x content
+);
+
+},
+'dojo/cldr/nls/zh/number':function(){
+define(
+"dojo/cldr/nls/zh/number", //begin v1.x content
+{
+	"decimalFormat": "#,##0.###",
+	"group": ",",
+	"scientificFormat": "#E0",
+	"percentFormat": "#,##0%",
+	"currencyFormat": "¤#,##0.00",
+	"decimal": "."
+}
+//end v1.x content
+);
+},
+'dojo/cldr/nls/zh-tw/number':function(){
+define('dojo/cldr/nls/zh-tw/number',{});
+},
+'dojox/grid/enhanced/nls/zh/EnhancedGrid':function(){
+define(
+"dojox/grid/enhanced/nls/zh/EnhancedGrid", //begin v1.x content
+({
+	singleSort: "单层排序",
+	nestedSort: "嵌套排序",
+	ascending: "升序",
+	descending: "降序",
+	sortingState: "${0} - ${1}",
+	unsorted: "请勿对此列进行排序",
+	indirectSelectionRadio: "第 ${0} 行，单选，单选框",
+	indirectSelectionCheckBox: "第 ${0} 行，多选，复选框",
+	selectAll: "全部选中"
+})
+//end v1.x content
+);
+
+
+},
+'dojox/grid/enhanced/nls/zh-tw/EnhancedGrid':function(){
+define(
+"dojox/grid/enhanced/nls/zh-tw/EnhancedGrid", //begin v1.x content
+({
+	singleSort: "單一排序",
+	nestedSort: "巢狀排序",
+	ascending: "遞增",
+	descending: "降冪",
+	sortingState: "${0} - ${1}",
+	unsorted: "請勿對此欄執行排序",
+	indirectSelectionRadio: "第 ${0} 行，單一選項，圓鈕框",
+	indirectSelectionCheckBox: "第 ${0} 行，多重選項，勾選框",
+	selectAll: "全選"
+})
+//end v1.x content
+);
+
+
+},
+'dijit/form/nls/zh/ComboBox':function(){
+define(
+"dijit/form/nls/zh/ComboBox", //begin v1.x content
+({
+		previousMessage: "先前选项",
+		nextMessage: "更多选项"
+})
+//end v1.x content
+);
+
+},
+'dijit/form/nls/zh-tw/ComboBox':function(){
+define(
+"dijit/form/nls/zh-tw/ComboBox", //begin v1.x content
+({
+		previousMessage: "前一個選擇項",
+		nextMessage: "其他選擇項"
+})
+//end v1.x content
+);
+
+},
+'dijit/nls/zh/common':function(){
+define(
+"dijit/nls/zh/common", //begin v1.x content
+({
+	buttonOk: "确定",
+	buttonCancel: "取消",
+	buttonSave: "保存",
+	itemClose: "关闭"
+})
+//end v1.x content
+);
+
+},
+'dijit/nls/zh-tw/common':function(){
+define(
+"dijit/nls/zh-tw/common", //begin v1.x content
+({
+	buttonOk: "確定",
+	buttonCancel: "取消",
+	buttonSave: "儲存",
+	itemClose: "關閉"
+})
+//end v1.x content
+);
+
+}}});
+define("dojo/nls/dot-dojo_zh-tw", [], 1);
