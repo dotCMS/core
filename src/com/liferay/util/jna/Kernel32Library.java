95caff4f2cf21c2bee9fbf73233e6c6812e41531
Jason Tesser <jason.tesser@dotcms.com>
2012-06-26 08:50:50 -0400

    closes #714
    Former-commit-id: 51c1ceac6debd0422b1dcdebc14468b73171a920

--- src/com/liferay/util/jna/Kernel32Library.java
+++ src/com/liferay/util/jna/Kernel32Library.java
@@ -5,10 +5,11 @@ import com.sun.jna.Platform;
 import com.sun.jna.Pointer;
 import com.sun.jna.Structure;
 import com.sun.jna.win32.StdCallLibrary;
+import com.sun.jna.win32.W32APIOptions;
 
 public interface Kernel32Library extends StdCallLibrary {
     Kernel32Library INSTANCE = (Kernel32Library) (Platform.isWindows()
-            ? Native.loadLibrary("kernel32", Kernel32Library.class)
+            ? Native.loadLibrary("kernel32", Kernel32Library.class,W32APIOptions.UNICODE_OPTIONS)
             : null);
 
     public static class LPSECURITY_ATTRIBUTES extends Structure {
