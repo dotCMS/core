603c5aff9925013c0d0fdb8dd25cae4508c80535
Will Ezell <will@dotcms.com>
2012-06-27 11:48:49 -0400

    #12
    Former-commit-id: b0b46f85e4736b99ffeebbd62b579cb4eb50fb9f

---
+++ bin/autoupdater/autoUpdater.bat
@@ -0,0 +1,85 @@
+@ECHO OFF
+
+REM Some constants
+SET JARFILE=autoUpdater.jar
+SET NEWFILE=autoUpdater.new
+SET OLDFILE=autoUpdater.old
+
+REM save the parameters for later
+set CMD_LINE_ARGS=
+:setArgs
+if ""%1""=="""" goto doneSetArgs
+set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
+shift
+goto setArgs
+:doneSetArgs
+
+
+
+echo %CMD_LINE_ARGS%|findstr /i "url" >nul:
+if %errorlevel%==1 goto :url_not_found
+:url_not_found
+set CMD_LINE_ARGS= ""%CMD_LINE_ARGS% -url http://www.dotcms.com:8080/servlets/upgrade2x""
+
+
+echo %CMD_LINE_ARGS%|findstr /i "home" >nul:
+if %errorlevel%==1 goto :home_not_found
+:home_not_found
+SET CurrDir=%CD%
+CD..
+SET InstPath=%CD%
+CD..
+SET RootPath=%CD%
+CD %CurrDir%
+set CMD_LINE_ARGS= ""%CMD_LINE_ARGS% -home %RootPath%""
+
+IF NOT EXIST "%JAVA_HOME%\bin\java.exe" GOTO nojava
+
+
+REM Get directory where we need to run from
+IF EXIST %JARFILE% SET PRGDIR=%CD%\bin
+IF EXIST %PRGDIR%\%JARFILE% GOTO gotprg
+IF EXIST %CD%\%JARFILE% SET PRGDIR=%CD%
+IF EXIST %PRGDIR%\%JARFILE% GOTO gotprg
+
+ECHO Couldn't find %JARFILE%. Aborting.
+GOTO end
+
+
+:gotprg
+ECHO Running from: %PRGDIR%
+
+REM Make sure we're clear to start
+IF EXIST %PRGDIR%\%NEWFILE% GOTO newjar
+
+
+GOTO run
+
+
+
+:run
+REM RUN
+"%JAVA_HOME%\bin\java" -jar "%PRGDIR%\%JARFILE%" %CMD_LINE_ARGS%
+IF EXIST "%PRGDIR%\%NEWFILE%" GOTO updatejar
+GOTO end
+
+:updatejar
+ECHO %NEWFILE% exists, updating agent.
+DEL "%PRGDIR%\%OLDFILE%"
+COPY "%PRGDIR%\%JARFILE%" "%PRGDIR%\%OLDFILE%"
+DEL "%PRGDIR%\%JARFILE%"
+COPY "%PRGDIR%\%NEWFILE%" "%PRGDIR%\%JARFILE%"
+DEL "%PRGDIR%\%NEWFILE%"
+ECHO "autoUpdater upgraded, restarting process."
+"%JAVA_HOME%\bin\java" -jar "%PRGDIR%\%JARFILE%" %CMD_LINE_ARGS%
+GOTO end
+
+:newjar
+ECHO %NEWFILE% exists, it was most likely left by a failed attempt.  Please remove before starting again. Aborting.
+GOTO end
+
+:nojava
+ECHO "JAVA_HOME needs to be set.  Aborting."
+GOTO end
+
+:end
---
+++ bin/autoupdater/autoUpdater.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+#Some constants
+JARFILE=autoUpdater.jar
+NEWFILE=autoUpdater.new
+OLDFILE=autoUpdater.old
+if [ ! -e  $JAVA_HOME/bin/java ]
+then
+	echo "JAVA_HOME needs to be set.  Aborting."
+	exit 1
+fi
+
+# resolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+# Get directory where we need to run from
+PRGDIR=`dirname "$PRG"`
+
+if [ ! -e $PRGDIR/$JARFILE ] 
+then  
+  echo "Couldn't find $JARFILE. Aborting."
+  exit 1
+fi
+  
+# Save the parameters for later
+CMD_LINE_ARGS=""
+while [ "$1" != "" ]
+do
+        CMD_LINE_ARGS="$CMD_LINE_ARGS $1"
+        shift
+done
+
+
+SOURCE="-home"
+if echo "$CMD_LINE_ARGS" | grep -q -- "$SOURCE"; then
+  echo "home parameter not found setting default";
+else
+  DIRECTORY=$(cd `dirname $0` && pwd)
+  cd $DIRECTORY/..
+  DIRECTORY2=$(cd `dirname $0` && pwd)
+  cd $DIRECTORY
+  CMD_LINE_ARGS="$CMD_LINE_ARGS -home $DIRECTORY2"
+fi
+
+SOURCE="-url"
+if echo "$CMD_LINE_ARGS" | grep -q -- "$SOURCE"; then
+  echo "url parameter not found setting default";
+else
+  CMD_LINE_ARGS="$CMD_LINE_ARGS -url http://www.dotcms.com:8080/servlets/upgrade2x"
+fi
+
+
+echo "Running from $PRGDIR"
+
+#Make sure we're clear to start
+if [ -e $PRGDIR/$NEWFILE ]
+then
+  echo "$NEWFILE exists, it was most likely left by a failed attempt.  Please remove before starting again. Aborting."
+  exit 1
+fi
+
+$JAVA_HOME/bin/java -jar $PRGDIR/autoUpdater.jar $CMD_LINE_ARGS
+
+
+if [ -e $PRGDIR/$NEWFILE ]
+then
+  # Make sure file is not zero length
+  if [ -s $PRGDIR/$NEWFILE ]
+  then
+    echo "$NEWFILE exists, updating agent."
+    mv $PRGDIR/$JARFILE $PRGDIR/$OLDFILE
+    mv $PRGDIR/$NEWFILE $PRGDIR/$JARFILE
+    echo "autoUpdater upgraded, restarting process."
+    $JAVA_HOME/bin/java -jar $PRGDIR/autoUpdater.jar $CMD_LINE_ARGS
+  fi
+fi
