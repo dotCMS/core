d1e5a317a21a6b79564027ed2d6282ce4c7dc95c
Jason Tesser <jason.tesser@dotcms.com>
2012-07-02 08:59:57 -0400

    #585

--- docs/examples/osgi/com.dotcms.controller/src/com/dotmarketing/osgi/controller/ExampleController.java
+++ docs/examples/osgi/com.dotcms.controller/src/com/dotmarketing/osgi/controller/ExampleController.java
@@ -1,19 +1,25 @@
 package com.dotmarketing.osgi.controller;
 
 import com.dotmarketing.util.Logger;
+
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Created by Jonathan Gamba
  * Date: 6/18/12
  */
-@Controller
+
+@EnableWebMvc
+@Configuration
 @RequestMapping ("/examplecontroller")
+@Controller
 public class ExampleController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
