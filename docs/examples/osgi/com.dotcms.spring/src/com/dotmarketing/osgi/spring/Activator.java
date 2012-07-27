34b0687e2ad53011acf51510844a84ed703a1a06
Jonathan Gamba <jonathan.gamba@dotcms.com>
2012-07-27 17:52:28 -0600

    #585
    Former-commit-id: d641f51983982357e8aff5dc3c793bab8b3b881b

---
+++ docs/examples/osgi/com.dotcms.spring/src/com/dotmarketing/osgi/spring/Activator.java
@@ -0,0 +1,45 @@
+package com.dotmarketing.osgi.spring;
+
+import com.dotmarketing.filters.CMSFilter;
+import com.dotmarketing.osgi.GenericBundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * Created by Jonathan Gamba
+ * Date: 6/18/12
+ */
+public class Activator extends GenericBundleActivator {
+
+    @SuppressWarnings ("unchecked")
+    public void start ( BundleContext context ) throws Exception {
+
+        ServiceReference sRef = context.getServiceReference( HttpService.class.getName() );
+        if ( sRef != null ) {
+
+            //Publish bundle services
+            publishBundleServices();
+
+            HttpService service = (HttpService) context.getService( sRef );
+            try {
+                DispatcherServlet ds = new DispatcherServlet();
+                ds.setContextConfigLocation( "spring/example-servlet.xml" );
+                service.registerServlet( "/spring", ds, null, null );
+            } catch ( Exception e ) {
+                e.printStackTrace();
+            }
+            CMSFilter.addExclude( "/dynamic/spring" );
+        }
+    }
+
+    public void stop ( BundleContext context ) throws Exception {
+
+        CMSFilter.removeExclude( "/dynamic/spring" );
+
+        //Unpublish bundle services
+        unpublishBundleServices();
+    }
+
+}
