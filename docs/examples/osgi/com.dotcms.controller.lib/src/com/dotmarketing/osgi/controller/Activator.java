75d0085e74ca14685b63dd8bd41e952d4d52a46d
Jonathan Gamba <jonathan.gamba@dotcms.com>
2012-07-31 15:03:21 -0600

    #585

---
+++ docs/examples/osgi/com.dotcms.controller.lib/src/com/dotmarketing/osgi/controller/Activator.java
@@ -0,0 +1,25 @@
+package com.dotmarketing.osgi.controller;
+
+import com.dotmarketing.osgi.GenericBundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Created by Jonathan Gamba
+ * Date: 6/18/12
+ */
+public class Activator extends GenericBundleActivator {
+
+    @SuppressWarnings ("unchecked")
+    public void start ( BundleContext context ) throws Exception {
+
+        //Publish bundle services
+        publishBundleServices();
+    }
+
+    public void stop ( BundleContext context ) throws Exception {
+
+        //Unpublish bundle services
+        unpublishBundleServices();
+    }
+
+}
