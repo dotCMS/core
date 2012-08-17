8c7429c7c3ebcd24123245aee81938beb157fec8
Ravi T <ravit@dotcms.com>
2012-08-18 00:46:13 +0530

    fixes #940
    Former-commit-id: 1ef98231acc982275000c93fa4a37b8199db74b2

---
+++ src/com/dotmarketing/util/SchedulerJobLocator.java
@@ -0,0 +1,28 @@
+package com.dotmarketing.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SchedulerJobLocator {
+	
+	public static List<String> getJobClassess(){
+		
+		List<String> jobsList = new ArrayList<String>();
+		jobsList.add("com.dotmarketing.quartz.job.BinaryCleanupJob");
+		jobsList.add("com.dotmarketing.quartz.job.CalendarReminderThread");
+		jobsList.add("com.dotmarketing.quartz.job.CascadePermissionsJob");
+		jobsList.add("com.dotmarketing.quartz.job.CleanBlockCacheScheduledTask");
+		jobsList.add("com.dotmarketing.quartz.job.ContentFromEmailJob");
+		jobsList.add("com.dotmarketing.quartz.job.ContentImportThread");
+		jobsList.add("com.dotmarketing.quartz.DotJob");
+		jobsList.add("com.dotmarketing.quartz.job.PopBouncedMailThread");
+		jobsList.add("com.dotmarketing.quartz.job.ResetPermissionsJob");
+		jobsList.add("com.dotmarketing.quartz.job.TrashCleanupJob");
+		jobsList.add("com.dotmarketing.quartz.job.UsersToDeleteThread");
+		jobsList.add("com.dotmarketing.quartz.job.WebDavCleanupJob");
+		jobsList.add("com.dotmarketing.portlets.webforms.jobs.WebFormsMailExcelJob");
+		
+		return jobsList;
+	}
+
+}
