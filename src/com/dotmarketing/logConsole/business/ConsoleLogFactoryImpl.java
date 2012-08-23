a9dffa986f48af290689a1a0033070a853cc89c2
Jonathan Gamba <jonathan.gamba@dotcms.com>
2012-08-23 17:31:42 -0600

    #960

--- src/com/dotmarketing/logConsole/business/ConsoleLogFactoryImpl.java
+++ src/com/dotmarketing/logConsole/business/ConsoleLogFactoryImpl.java
@@ -1,93 +1,90 @@
 package com.dotmarketing.logConsole.business;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import org.apache.commons.beanutils.BeanUtils;
-import org.codehaus.jackson.map.ObjectMapper;
 import com.dotmarketing.common.db.DotConnect;
 import com.dotmarketing.db.HibernateUtil;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.logConsole.model.LogMapperRow;
-import com.dotmarketing.portlets.workflows.model.WorkflowAction;
-import com.dotmarketing.portlets.workflows.model.WorkflowHistory;
 import com.dotmarketing.util.Logger;
+import org.apache.commons.beanutils.BeanUtils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class ConsoleLogFactoryImpl implements ConsoleLogFactory {
 
-	public List convertListToObjects(List<Map<String, Object>> rs, Class clazz) throws DotDataException {
-		final ObjectMapper m = new ObjectMapper();
+    public List convertListToObjects ( List<Map<String, Object>> rs, Class clazz ) throws DotDataException {
 
-		final List ret = new ArrayList();
-		try {
-			for (final Map<String, Object> map : rs) {
-				ret.add(this.convertMaptoObject(map, clazz));
-			}
-		} catch (final Exception e) {
-			throw new DotDataException("cannot convert object to " + clazz + " " + e.getMessage());
+        final List ret = new ArrayList();
+        try {
+            for ( final Map<String, Object> map : rs ) {
+                ret.add( this.convertMaptoObject( map, clazz ) );
+            }
+        } catch ( final Exception e ) {
+            throw new DotDataException( "cannot convert object to " + clazz + " " + e.getMessage() );
 
-		}
-		return ret;
+        }
+        return ret;
 
-	}
+    }
 
-	public Object convertMaptoObject(Map<String, Object> map, Class clazz) throws InstantiationException, IllegalAccessException, InvocationTargetException {
+    public Object convertMaptoObject ( Map<String, Object> map, Class clazz ) throws InstantiationException, IllegalAccessException, InvocationTargetException {
 
-		final Object obj = clazz.newInstance();
+        final Object obj = clazz.newInstance();
 
-		if(obj instanceof LogMapperRow) {
-			return this.convertLogMapper(map);
-		}
+        if ( obj instanceof LogMapperRow ) {
+            return this.convertLogMapper( map );
+        }
 
-		return this.convert(obj, map);
+        return this.convert( obj, map );
+    }
 
-	}
+    private Object convertLogMapper ( Map<String, Object> row ) throws IllegalAccessException, InvocationTargetException {
 
-	private Object convertLogMapper(Map<String, Object> row) throws IllegalAccessException, InvocationTargetException {
+        final LogMapperRow scheme = new LogMapperRow();
+        row.put( "actionId", row.get( "workflow_action_id" ) );
 
-		final LogMapperRow scheme = new LogMapperRow();
-		row.put("actionId", row.get("workflow_action_id"));
+        BeanUtils.copyProperties( scheme, row );
+        return scheme;
+    }
 
-		BeanUtils.copyProperties(scheme, row);
-		return scheme;
-	}
-	
-	private Object convert(Object obj, Map<String, Object> map) throws IllegalAccessException, InvocationTargetException {
-		BeanUtils.copyProperties(obj, map);
-		return obj;
-	}
+    private Object convert ( Object obj, Map<String, Object> map ) throws IllegalAccessException, InvocationTargetException {
+        BeanUtils.copyProperties( obj, map );
+        return obj;
+    }
 
-	public List<LogMapperRow> findLogMapper() throws DotDataException {
-		final DotConnect db = new DotConnect();
-		try {
+    public List<LogMapperRow> findLogMapper () throws DotDataException {
 
-			db.setSQL(ConsoleLoggerSQL.SELECT_LOGGING_CRITERIA);
-			db.loadResult();
+        final DotConnect db = new DotConnect();
+        try {
 
-		} catch (final Exception e) {
-			Logger.debug(this.getClass(), e.getMessage(), e);
-		}
+            db.setSQL( ConsoleLoggerSQL.SELECT_LOGGING_CRITERIA );
+            db.loadResult();
 
-		return (List<LogMapperRow>) this.convertListToObjects(db.loadObjectResults(), LogMapperRow.class);
-	}
+        } catch ( final Exception e ) {
+            Logger.debug( this.getClass(), e.getMessage(), e );
+        }
 
-	public void updateLogMapper(LogMapperRow r) throws DotDataException {
-		
-		final DotConnect db = new DotConnect();
-		try {
+        return (List<LogMapperRow>) this.convertListToObjects( db.loadObjectResults(), LogMapperRow.class );
+    }
 
-			db.setSQL(ConsoleLoggerSQL.UPDATE_LOGGING_CRITERIA);
-			db.addParam(r.getEnabled());
-			db.addParam(r.getLog_name());
-			db.loadResult();
+    public void updateLogMapper ( LogMapperRow r ) throws DotDataException {
 
-		} catch (final Exception e) {
-			Logger.debug(this.getClass(), e.getMessage(), e);
-		} finally{
-			HibernateUtil.commitTransaction();
-		}
-		
-	}
+        final DotConnect db = new DotConnect();
+        try {
+
+            db.setSQL( ConsoleLoggerSQL.UPDATE_LOGGING_CRITERIA );
+            db.addParam( r.getEnabled() );
+            db.addParam( r.getLog_name() );
+            db.loadResult();
+
+        } catch ( final Exception e ) {
+            Logger.debug( this.getClass(), e.getMessage(), e );
+        } finally {
+            HibernateUtil.commitTransaction();
+        }
+
+    }
 
 }
