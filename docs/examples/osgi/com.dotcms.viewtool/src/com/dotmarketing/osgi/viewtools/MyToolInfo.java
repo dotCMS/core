33f103fc77a348ec31dd675ae9387d2fb3f9a9eb
Jonathan Gamba <jonathan.gamba@dotcms.com>
2012-09-12 15:33:20 -0400

    Clean up code #1113

--- docs/examples/osgi/com.dotcms.viewtool/src/com/dotmarketing/osgi/viewtools/MyToolInfo.java
+++ docs/examples/osgi/com.dotcms.viewtool/src/com/dotmarketing/osgi/viewtools/MyToolInfo.java
@@ -5,22 +5,30 @@ import org.apache.velocity.tools.view.servlet.ServletToolInfo;
 
 public class MyToolInfo extends ServletToolInfo {
 
-	@Override
-	public String getKey() {
-		return "osgitool";
-	}
+    @Override
+    public String getKey () {
+        return "osgitool";
+    }
 
-	@Override
-	public String getClassname() {
-		return MyViewTool.class.getName();
-	}
+    @Override
+    public String getScope () {
+        return ViewContext.APPLICATION;
+    }
 
-	@Override
-	public Object getInstance(Object initData) {
-		MyViewTool viewTool = new MyViewTool();
-		viewTool.init(initData);
-		setScope(ViewContext.APPLICATION);
-		return viewTool;
-	}
-	
-};
+    @Override
+    public String getClassname () {
+        return MyViewTool.class.getName();
+    }
+
+    @Override
+    public Object getInstance ( Object initData ) {
+
+        MyViewTool viewTool = new MyViewTool();
+        viewTool.init( initData );
+
+        setScope( ViewContext.APPLICATION );
+
+        return viewTool;
+    }
+
+}
