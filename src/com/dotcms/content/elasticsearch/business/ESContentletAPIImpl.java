09e349d5ce9b612027c3dfdf0a5c036bd7ec53ed
Alberto <alberto@heaven-work.(none)>
2012-10-26 18:48:46 +0200

    First versione of receiver publisher done
    Former-commit-id: aff703318505177e6aa49ea3138e040c2c346ced

--- src/com/dotcms/content/elasticsearch/business/ESContentletAPIImpl.java
+++ src/com/dotcms/content/elasticsearch/business/ESContentletAPIImpl.java
@@ -3164,7 +3164,7 @@ public class ESContentletAPIImpl implements ContentletAPI {
     }
 
     public void validateContentlet(Contentlet contentlet, ContentletRelationships contentRelationships, List<Category> cats)throws DotContentletValidationException {
-        if(contentlet.getStringProperty("_dont_validate_me")!=null)
+        if(contentlet.getMap().get("_dont_validate_me") != null)
             return;
         
         DotContentletValidationException cve = new DotContentletValidationException("Contentlet's fields are not valid");
