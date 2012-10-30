5fc71f54a1e3a6a13cc300b3e8b2f1f86330435a
Alberto <alberto@heaven-work.(none)>
2012-10-30 19:00:36 +0100

    Added EndpointDetail.java

---
+++ src/com/dotcms/publisher/business/EndpointDetail.java
@@ -0,0 +1,18 @@
+package com.dotcms.publisher.business;
+
+public class EndpointDetail {
+	private int status;
+	private String info;
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+}
