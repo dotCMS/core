993dc5e341b3ca6cf50b0533bc5cb2e357d6bfd2
Jorge Urdaneta <jorge.urdaneta@dotcms.com>
2012-11-08 15:24:14 -0430

    #1499 LinkCheckerAPITest stub
    Former-commit-id: 00ae531c0258f32a747c7f9ae41c0643a853cca6

---
+++ test/com/dotmarketing/portlets/linkchecker/business/LinkCheckerAPITest.java
@@ -0,0 +1,43 @@
+package com.dotmarketing.portlets.linkchecker.business;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.dotcms.TestBase;
+import com.dotmarketing.portlets.contentlet.model.Contentlet;
+import com.dotmarketing.portlets.linkchecker.bean.InvalidLink;
+import com.dotmarketing.portlets.structure.model.Field;
+import com.liferay.portal.model.User;
+
+public class LinkCheckerAPITest extends TestBase {
+    @Test
+    public void findInvalidLinks(String htmltext, User user) {
+        
+    }
+    
+    @Test
+    public void saveInvalidLinks(Contentlet contentlet, Field field, List<InvalidLink> links) {
+        
+    }
+    
+    @Test
+    public void deleteInvalidLinks(Contentlet contentlet) {
+        
+    }
+    
+    @Test
+    public void findByInode(String inode)  {
+        
+    }
+    
+    @Test
+    public void findAll(int offset, int pageSize) {
+        
+    }
+    
+    @Test
+    public void findAllCount() {
+        
+    }
+}
