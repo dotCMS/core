65e1c9071e511e1f0f99593b8af4d02f10af57b9
Jorge Urdaneta <jorge.urdaneta@dotcms.com>
2012-11-13 17:13:43 -0430

    #1540 getting subfolders

--- src/com/dotmarketing/viewtools/navigation/NavTool.java
+++ src/com/dotmarketing/viewtools/navigation/NavTool.java
@@ -7,10 +7,12 @@ import org.apache.velocity.tools.view.context.ViewContext;
 import org.apache.velocity.tools.view.tools.ViewTool;
 
 import com.dotmarketing.beans.Host;
+import com.dotmarketing.beans.Inode;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.business.web.WebAPILocator;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotSecurityException;
+import com.dotmarketing.portlets.folders.model.Folder;
 import com.dotmarketing.util.RegEX;
 import com.dotmarketing.util.RegExMatch;
 import com.dotmarketing.util.UtilMethods;
@@ -43,13 +45,21 @@ public class NavTool implements ViewTool {
             List<RegExMatch> find = RegEX.find(path, "^//(\\w+)/(.+)");
             if(find.size()==1) {
                 String hostname=find.get(0).getGroups().get(0).getMatch();
-                path=find.get(0).getGroups().get(1).getMatch();
+                path="/"+find.get(0).getGroups().get(1).getMatch();
                 host=APILocator.getHostAPI().findByName(hostname, user, true);
             }
         }
         
-        
-        
+        if(path.startsWith("/")) {
+            List<Folder> folders = APILocator.getFolderAPI().findSubFolders(host, true);
+        }
+        else {
+            Folder folder=APILocator.getFolderAPI().findFolderByPath(path, host, user, true);
+            List<Inode> menuItems = APILocator.getFolderAPI().findMenuItems(folder, user, true);
+            
+            
+            List<Folder> folders = APILocator.getFolderAPI().findSubFolders(folder, true);
+        }
         return list;
     }
     
