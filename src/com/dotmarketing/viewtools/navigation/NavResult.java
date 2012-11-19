f1ce81fda02a5833fb505cfd6390ba973cfe530b
Jorge Urdaneta <jorge.urdaneta@dotcms.com>
2012-11-19 17:57:41 -0430

    #1540 refactored and started testing

--- src/com/dotmarketing/viewtools/navigation/NavResult.java
+++ src/com/dotmarketing/viewtools/navigation/NavResult.java
@@ -1,6 +1,7 @@
 package com.dotmarketing.viewtools.navigation;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,25 +11,31 @@ import org.apache.velocity.context.Context;
 import org.apache.velocity.tools.view.tools.ViewRenderTool;
 
 import com.dotmarketing.beans.Host;
+import com.dotmarketing.beans.Identifier;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotSecurityException;
+import com.dotmarketing.portlets.folders.business.FolderAPI;
+import com.dotmarketing.portlets.folders.model.Folder;
 import com.dotmarketing.util.UtilMethods;
 import com.dotmarketing.util.VelocityUtil;
 import com.dotmarketing.velocity.VelocityServlet;
+import com.liferay.portal.model.User;
 
-public class NavResult {
+public class NavResult implements Iterable<NavResult> {
     private String title;
     private String href;
     private int order;
     private boolean hrefVelocity;
     
-    private String internalPath;
-    private String internalHostId;
+    private String hostId;
+    private String folderId;
+    private List<String> childrenFolderIds;
+    private List<NavResult> children;
     
-    public NavResult(String hostId, String path) {
-        internalPath=path;
-        internalHostId=hostId;
+    public NavResult(String hostId, String folderId) {
+        this.hostId=hostId;
+        this.folderId=folderId;
         hrefVelocity=false;
         title=href="";
         order=0;
@@ -87,20 +94,56 @@ public class NavResult {
             return false;
     }
 
-    public List<NavResult> getChildren() throws DotDataException, DotSecurityException {
-        if(internalPath!=null && internalHostId!=null) {
-            Host host=APILocator.getHostAPI().find(internalHostId, APILocator.getUserAPI().getAnonymousUser(), true);
-            return NavTool.getNav(host, internalPath);
-        }
-        return new ArrayList<NavResult>();
+    public void setChildren(List<NavResult> children) {
+        this.children=children;
     }
     
-    public NavResult getParent() {
-        // TODO: finish this
-        return null;
+    public boolean isFolder() {
+        return hostId!=null && folderId!=null;
     }
-
-
+    
+    public List<NavResult> getChildren() throws Exception {
+        if(children==null && hostId!=null && folderId!=null) {
+            // lazy loadinge children
+            User user=APILocator.getUserAPI().getAnonymousUser();
+            Host host=APILocator.getHostAPI().find(hostId, user, true);
+            Folder folder=APILocator.getFolderAPI().find(folderId, user, true);
+            Identifier ident=APILocator.getIdentifierAPI().find(folder);
+            NavResult lazyMe=NavTool.getNav(host, ident.getPath());
+            children=lazyMe.getChildren();
+            childrenFolderIds=lazyMe.getChildrenFolderIds();
+        }
+        if(children!=null) {
+            ArrayList<NavResult> list=new ArrayList<NavResult>();
+            for(NavResult nn : children) {
+                if(nn.isFolder()) {
+                    // for folders we avoid returning the same instance
+                    // it could be changed elsewhere and we need it to
+                    // load its children lazily
+                    NavResult ff=new NavResult(nn.hostId,nn.folderId);
+                    ff.setTitle(nn.getTitle());
+                    ff.setHref(nn.getHref());
+                    ff.setOrder(nn.getOrder());
+                    list.add(ff);
+                }
+                else {
+                    list.add(nn);
+                }
+            }
+            return list;
+        }
+        else
+            return new ArrayList<NavResult>();
+    }
+    
+    public String getParentPath() throws DotDataException, DotSecurityException {
+        if(folderId==null || folderId.equals(FolderAPI.SYSTEM_FOLDER)) return null; // no parent!
+        User user=APILocator.getUserAPI().getAnonymousUser();
+        Folder folder=APILocator.getFolderAPI().find(folderId, user, true);
+        Identifier ident=APILocator.getIdentifierAPI().find(folder);
+        return ident.getParentPath();
+    }
+    
     public String toString() {
         if(!hrefVelocity) {
             String titleToShow;
@@ -115,4 +158,22 @@ public class NavResult {
             return getHref();
         }
     }    
+    
+    public List<String> getChildrenFolderIds() {
+        return childrenFolderIds!=null ? childrenFolderIds : new ArrayList<String>();
+    }
+
+    public void setChildrenFolderIds(List<String> childrenFolderIds) {
+        this.childrenFolderIds = childrenFolderIds;
+    }
+
+    @Override
+    public Iterator<NavResult> iterator() {
+        try {
+            return getChildren().iterator();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
