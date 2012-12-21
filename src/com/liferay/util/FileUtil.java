e5d576127a1e2ba86ae2300d87137ca7be0e5725
Will Ezell <will@dotcms.com>
2012-12-21 13:27:02 -0500

    fixes #1749 - tested on scc
    Former-commit-id: cec875976e6d2a36959c31edce4394874fb9ada8

--- src/com/liferay/util/FileUtil.java
+++ src/com/liferay/util/FileUtil.java
@@ -375,8 +375,11 @@ public class FileUtil {
 		}
 
 		destination.delete();
-
-		return source.renameTo(destination);
+		
+		copyFile(source, destination);
+		
+		
+		return source.delete();
 	}
 
 	public static String read(String fileName) throws IOException {
