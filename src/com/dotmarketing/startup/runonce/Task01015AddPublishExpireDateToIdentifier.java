53a97eb92fb68e00edc5b49c57a7d534954bb360
Jorge Urdaneta <jorge.urdaneta@dotcms.com>
2013-01-03 17:45:13 -0430

    #1763 adding new fields
    Former-commit-id: 1f19f7e5b1b49768823dc6c1caa9be539d90d3ee

---
+++ src/com/dotmarketing/startup/runonce/Task01015AddPublishExpireDateToIdentifier.java
@@ -0,0 +1,55 @@
+package com.dotmarketing.startup.runonce;
+
+import java.util.List;
+
+import com.dotmarketing.common.db.DotConnect;
+import com.dotmarketing.db.DbConnectionFactory;
+import com.dotmarketing.startup.AbstractJDBCStartupTask;
+
+public class Task01015AddPublishExpireDateToIdentifier extends
+        AbstractJDBCStartupTask {
+    
+    @Override
+    public boolean forceRun() {
+        try {
+            DotConnect dc=new DotConnect();
+            dc.setSQL("select publish_date,expire_date from identifier where publish_date>"
+                      +DbConnectionFactory.getDBDateTimeFunction());
+            dc.loadResult();
+        }
+        catch(Exception ex) {
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public String getPostgresScript() {
+        return "alter table identifier add syspublish_date timestamp;" +
+        		"alter table identifier add sysexpire_date timestamp;";
+    }
+    
+    @Override
+    public String getMySQLScript() {
+        return "alter table identifier add syspublish_date datetime;" +
+                "alter table identifier add sysexpire_date datetime;";
+    }
+    
+    @Override
+    public String getOracleScript() {
+        return "alter table identifier add syspublish_date timestamp;" +
+                "alter table identifier add sysexpire_date timestamp;";
+    }
+    
+    @Override
+    public String getMSSQLScript() {
+        return "alter table identifier add syspublish_date datetime;" +
+                "alter table identifier add sysexpire_date datetime;";
+    }
+    
+    @Override
+    protected List<String> getTablesToDropConstraints() {
+        return null;
+    }
+    
+}
