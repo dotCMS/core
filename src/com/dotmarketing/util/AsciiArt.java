0ff923fa96c2b895b14499b759ae60edd6b70c36
Will Ezell <will@dotcms.com>
2013-01-15 09:46:41 -0500

    fixes #1840
    Former-commit-id: 92e36733a6219ecba70b362437a43353d24546a4

---
+++ src/com/dotmarketing/util/AsciiArt.java
@@ -0,0 +1,34 @@
+package com.dotmarketing.util;
+
+
+public class AsciiArt {
+    
+	private static boolean artDone = false;
+	
+	public static void doArt(){
+		
+		if(artDone) return;
+			artDone=true;
+		
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "                                                                                   ");
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "                                                                                   ");
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "                                                                                   ");
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "           OOOO                            7777777   7777       7777     77777777  ");
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "           OOOO                 OO       777777777  77777      77777    77777777   ");
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "           OOOO                OOO      77777       777777     77777   7777        ");
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "     OOOOOOOOOO   OOOOOOOO   OOOOOOOOO 7777         7777777   777777   7777        ");
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "    OOOO  OOOOO  OOOO  OOOO    OOOO    7777         7777777  7777777    777777     ");
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "   OOOO    OOOO  OOO    OOOO   OOOO    7777         77777777 777 7777     777777   ");
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "   OOOO    OOOO OOOO    OOOO   OOOO    7777         777  777 777 7777        7777  ");
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "   OOOO    OOOO  OOO    OOOO   OOOO    77777        777  777777  7777         7777 ");
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "    OOOO   OOOO  OOOO   OOO    OOOO     77777       777   7777   7777        7777  ");
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "     OOOOOOOOOO   OOOOOOOO      OOOOO    777777777  777   7777   7777  777777777   ");
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "                                                                                   ");
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "                                                     Web Content Management System ");
+		Logger.info(com.dotmarketing.util.AsciiArt.class, "                                                                                   ");
+		
+	}
+	
+	
+	
+}
