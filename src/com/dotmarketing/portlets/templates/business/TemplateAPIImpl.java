70344bce4f2286e28fc9b500f6e0dea537f4bd3a
Jorge Urdaneta <jorge.urdaneta@dotcms.com>
2013-04-15 09:32:09 -0430

    #2629 fixed and unit tested templateAPI.findLiveTemplate

--- src/com/dotmarketing/portlets/templates/business/TemplateAPIImpl.java
+++ src/com/dotmarketing/portlets/templates/business/TemplateAPIImpl.java
@@ -461,7 +461,8 @@ public class TemplateAPIImpl extends BaseWebAssetAPI implements TemplateAPI {
 	public Template find(String inode, User user,  boolean respectFrontEndRoles) throws DotSecurityException,
 			DotDataException {
 		Template t =  templateFactory.find(inode);
-		if(t!=null&&!permissionAPI.doesUserHavePermission(t, PermissionAPI.PERMISSION_READ, user, respectFrontEndRoles)){
+		if(t!=null && InodeUtils.isSet(t.getInode()) && 
+		      !permissionAPI.doesUserHavePermission(t, PermissionAPI.PERMISSION_READ, user, respectFrontEndRoles)){
 			throw new DotSecurityException("User does not have access to template:" + inode);
 		}
 		return t;
