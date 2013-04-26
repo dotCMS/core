034eacbea683f116fd43c68b69c27c5a346640c8
Jorge Urdaneta <jorge.urdaneta@dotcms.com>
2013-04-26 10:54:08 -0430

    remove logg

--- src/com/dotmarketing/cms/urlmap/filters/URLMapFilter.java
+++ src/com/dotmarketing/cms/urlmap/filters/URLMapFilter.java
@@ -100,8 +100,6 @@ public class URLMapFilter implements Filter {
 		if (uri.endsWith("/"))
 			uri = uri.substring(0, uri.length() - 1);
 		
-		Logger.info(this, "Getting reques host: "+host.getHostname()+" uri: "+uri+"");
-	    
 		String pointer = null;
 		
 		if(host!=null){
@@ -140,7 +138,6 @@ public class URLMapFilter implements Filter {
 			chain.doFilter(req, res);
 			return;
 		}
-		Logger.info(this, "master pattern "+mastRegEx+" to match url "+url);
 		if (RegEX.contains(url, mastRegEx)) {
 			boolean ADMIN_MODE = (session.getAttribute(com.dotmarketing.util.WebKeys.ADMIN_MODE_SESSION) != null);
 			boolean EDIT_MODE = ((session.getAttribute(com.dotmarketing.util.WebKeys.EDIT_MODE_SESSION) != null) && ADMIN_MODE);
@@ -238,9 +235,7 @@ public class URLMapFilter implements Filter {
                     }
 					
 					try {
-					    Logger.info(this, "this is the query "+query.toString());
 						cons = conAPI.searchIndex(query.toString(), 1, 0, (hostIsRequired?"conhost, modDate": "modDate"), user, true);
-						Logger.info(this, "got "+cons.size()+" contents");
 						ContentletSearch c = cons.get(0);
 						session.setAttribute(com.dotmarketing.util.WebKeys.HTMLPAGE_LANGUAGE,String.valueOf(conAPI.find(c.getInode(), user, true).getLanguageId()));
 						request.setAttribute(WebKeys.WIKI_CONTENTLET, c.getIdentifier());
@@ -308,10 +303,9 @@ public class URLMapFilter implements Filter {
 							request.setAttribute(com.dotmarketing.util.WebKeys.EDIT_MODE_SESSION, null);
 							LogFactory.getLog(this.getClass()).debug("URLMAP FILTER Cleaning PREVIEW_MODE_SESSION PREVIEW!!!!");
 						}
-						Logger.info(this, "go into request dispatcher");
+						
 						request.getRequestDispatcher(ident.getURI()).forward(req, res);
 					}else{
-					    Logger.info(this, "pass the chain... no content match");
 						chain.doFilter(req, res);
 					}
 					return;
@@ -321,7 +315,6 @@ public class URLMapFilter implements Filter {
 			}
 
 		}
-		Logger.info(this, "pass the chain... no pattern match");
 		chain.doFilter(req, res);
 	}
 
