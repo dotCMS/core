bdc2405161088afa314d2d793df9ae8869c3dc78
Jonathan Gamba <jonathan.gamba@dotcms.com>
2013-05-20 16:28:18 -0600

    #2870

--- docs/examples/osgi/com.dotcms.hooks/src/com/dotmarketing/osgi/hooks/SamplePostContentHook.java
+++ docs/examples/osgi/com.dotcms.hooks/src/com/dotmarketing/osgi/hooks/SamplePostContentHook.java
@@ -1,15 +1,15 @@
 package com.dotmarketing.osgi.hooks;
 
+import com.dotmarketing.beans.Permission;
 import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.portlets.categories.model.Category;
 import com.dotmarketing.portlets.contentlet.business.ContentletAPIPostHookAbstractImp;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.portlets.structure.model.ContentletRelationships;
-import com.dotmarketing.portlets.structure.model.ContentletRelationships.ContentletRelationshipRecords;
-import com.dotmarketing.portlets.categories.model.Category;
-import com.liferay.portal.model.User;
-import java.util.List;
-import com.dotmarketing.beans.Permission;
 import com.dotmarketing.util.Logger;
+import com.liferay.portal.model.User;
+
+import java.util.List;
 
 
 public class SamplePostContentHook extends ContentletAPIPostHookAbstractImp {
@@ -29,13 +29,12 @@ public class SamplePostContentHook extends ContentletAPIPostHookAbstractImp {
     }
 
     @Override
-	public void checkin(Contentlet currentContentlet, ContentletRelationships relationshipsData, List<Category> cats, List<Permission> selectedPermissions, User user,	boolean respectFrontendRoles, Contentlet returnValue) {
+    public void checkin ( Contentlet currentContentlet, ContentletRelationships relationshipsData, List<Category> cats, List<Permission> selectedPermissions, User user, boolean respectFrontendRoles, Contentlet returnValue ) {
 
-        Logger.info(this,"+++++++++++++++++++++++++++++++++++++++++++++++" );
-        Logger.info(this,"INSIDE SamplePostContentHook.checkin()" );
-        Logger.info(this,"+++++++++++++++++++++++++++++++++++++++++++++++" );
-		super.checkin(currentContentlet, relationshipsData, cats,selectedPermissions, user, respectFrontendRoles, returnValue);
-
-	}
+        Logger.info( this, "+++++++++++++++++++++++++++++++++++++++++++++++" );
+        Logger.info( this, "INSIDE SamplePostContentHook.checkin()" );
+        Logger.info( this, "+++++++++++++++++++++++++++++++++++++++++++++++" );
+        super.checkin( currentContentlet, relationshipsData, cats, selectedPermissions, user, respectFrontendRoles, returnValue );
+    }
 
 }
