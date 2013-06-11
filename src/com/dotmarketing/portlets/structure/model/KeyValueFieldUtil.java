cbd027f28c08dfe917c5310b09219af19a3fa98f
Graziano Aliberti <graziano.aliberti@eng.it>
2013-06-11 14:53:27 +0200

    #3022 try to use the Gson API for revert the JSON string to Map object

--- src/com/dotmarketing/portlets/structure/model/KeyValueFieldUtil.java
+++ src/com/dotmarketing/portlets/structure/model/KeyValueFieldUtil.java
@@ -8,8 +8,8 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
 
 import com.dotmarketing.util.UtilMethods;
-import com.dotmarketing.util.json.JSONException;
-import com.dotmarketing.util.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 /**
  * Utility class to handle Key Value field 
@@ -24,12 +24,14 @@ public class KeyValueFieldUtil {
 	public static Map<String,Object> JSONValueToHashMap(final String json){
 		LinkedHashMap<String,Object> keyValueMap  = new LinkedHashMap<String,Object>();
 		if(UtilMethods.isSet(json)){
-			TypeReference<LinkedHashMap<String,Object>> typeRef = new TypeReference<LinkedHashMap<String,Object>>() {}; 
-			try {
-				keyValueMap = mapper.readValue(json, typeRef);
-			} catch (Exception e) {
-				//TODO
-			} 
+//			TypeReference<LinkedHashMap<String,Object>> typeRef = new TypeReference<LinkedHashMap<String,Object>>() {}; 
+//			try {
+//				keyValueMap = mapper.readValue(json, typeRef);
+//			} catch (Exception e) {
+//				//TODO
+//			}
+			Gson gson = new Gson();
+			return gson.fromJson(json, new TypeToken<LinkedHashMap<String, String>>() {}.getType());
 		}
 		return keyValueMap;
 	}
