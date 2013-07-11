8880ad4c38b825561b4dd64e49bda0a7038c2ca6
ravit <ravit@dotcms.com>
2013-07-11 14:38:37 +0530

    fixes #2850 Crash when entering a hint that is too long

--- src/com/dotmarketing/portlets/containers/action/EditContainerAction.java
+++ src/com/dotmarketing/portlets/containers/action/EditContainerAction.java
@@ -13,8 +13,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.beanutils.BeanUtils;
+import org.apache.struts.Globals;
+import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
 
 import com.dotmarketing.beans.Host;
 import com.dotmarketing.beans.Identifier;
@@ -76,7 +79,9 @@ public class EditContainerAction extends DotPortletAction implements
 		HttpServletRequest httpReq = reqImpl.getHttpServletRequest();
 
 		if ((referer != null) && (referer.length() != 0)) {
-			referer = URLDecoder.decode(referer, "UTF-8");
+			while(referer.startsWith("%")){
+				referer = URLDecoder.decode(referer, "UTF-8");
+			}
 		}
 
 		Logger.debug(this, "EditContainerAction cmd=" + cmd);
@@ -143,8 +148,8 @@ public class EditContainerAction extends DotPortletAction implements
 		 */
 		if ((cmd != null) && cmd.equals(Constants.ADD)) {
 			try {
-
-				if (Validator.validate(req, form, mapping)) {
+				String note = fm.getNotes();
+				if (Validator.validate(req, form, mapping) && note.length()<=255) {
 
 					Logger.debug(this, "Calling Save Method");
 					_saveWebAsset(req, res, config, form, user);
@@ -184,6 +189,12 @@ public class EditContainerAction extends DotPortletAction implements
 
 					}
 				}
+				if(note.length()>255){
+					ActionErrors ae = new ActionErrors();
+					ae.add(Globals.ERROR_KEY, new ActionMessage("Note field Contains more than 255 characters"));
+					req.setAttribute(Globals.ERROR_KEY, ae);
+				}
+				_editWebAsset(req, res, config, form, user);
 
 			} catch (Exception ae) {
 				_handleException(ae, req);
