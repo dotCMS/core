61c552ebf73530a842f12b53c2f219061013a780
Daniel Silva <daniel.silva@dotcms.com>
2013-07-24 18:19:15 -0430

    fixes #3417

--- src/com/dotcms/publisher/assets/business/PushedAssetsFactory.java
+++ src/com/dotcms/publisher/assets/business/PushedAssetsFactory.java
@@ -10,8 +10,10 @@ public abstract class PushedAssetsFactory {
 	protected static String INSERT_ASSETS = "INSERT INTO publishing_pushed_assets VALUES (?,?,?,?,?)";
 	protected static String SELECT_ASSETS_BY_BUNDLE_ENV= "SELECT * FROM publishing_pushed_assets WHERE bundle_id = ? and environment_id = ?";
 	protected static String SELECT_ASSETS_BY_ASSET_ID= "SELECT * FROM publishing_pushed_assets WHERE asset_id = ?";
+	protected static String SELECT_ASSETS_BY_ENV_ID= "SELECT * FROM publishing_pushed_assets WHERE environment_id = ?";
 	protected static String DELETE_ASSETS_BY_BUNDLE_ENV= "DELETE FROM publishing_pushed_assets WHERE bundle_id = ? and environment_id = ?";
 	protected static String DELETE_ASSETS_BY_ASSET_ID= "DELETE FROM publishing_pushed_assets WHERE asset_id = ?";
+	protected static String DELETE_ASSETS_BY_ENVIRONMENT_ID= "DELETE FROM publishing_pushed_assets WHERE environment_id = ?";
 	protected static String DELETE_ALL_ASSETS= "TRUNCATE publishing_pushed_assets";
 
 	public abstract void savePushedAsset(PushedAsset asset) throws DotDataException;
@@ -20,10 +22,14 @@ public abstract class PushedAssetsFactory {
 
 	public abstract void deletePushedAssets(String assetId)  throws DotDataException;
 
+	public abstract void deletePushedAssetsByEnvironment(String environmentId)  throws DotDataException;
+
 	public abstract List<PushedAsset> getPushedAssets(String bundleId, String environmentId)  throws DotDataException;
 
 	public abstract void deleteAllPushedAssets() throws DotDataException;
 
 	public abstract List<PushedAsset> getPushedAssets(String assetId) throws DotDataException;
+
+	public abstract List<PushedAsset> getPushedAssetsByEnvironment(String assetId) throws DotDataException;
 
 }
