d81f6ddcbea30bf9382352ac1ef3dd9c3dc0cfe9
Jonathan Gamba <jonathan.gamba@dotcms.com>
2013-08-01 10:08:53 -0600

    #2398
    Former-commit-id: c28474600f80f3f8e1116bc89a209df77bee42c4

--- src/com/dotcms/publishing/IPublisher.java
+++ src/com/dotcms/publishing/IPublisher.java
@@ -5,14 +5,22 @@ import java.util.List;
 
 public interface IPublisher {
 
+    /**
+     * Method to configure any required value before to process this Publisher
+     *
+     * @param config Object that have the main configuration values for the processing of this Publisher
+     * @return
+     * @throws DotPublishingException
+     */
+    public PublisherConfig init ( PublisherConfig config ) throws DotPublishingException;
 
-	public PublisherConfig process(PublishStatus status) throws DotPublishingException;
-	
-	public PublisherConfig init(PublisherConfig config) throws DotPublishingException;
-	
-	public List<Class> getBundlers();
-	
-	
-	
-	
+    public PublisherConfig process ( PublishStatus status ) throws DotPublishingException;
+
+    /**
+     * List fo Bundlers used by this Publisher
+     *
+     * @return
+     */
+    public List<Class> getBundlers ();
+
 }
