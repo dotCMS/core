00c5ef59051a35a7b94bc74b2742a87ef723a0df
Jorge Urdaneta <jorge.urdaneta@dotcms.com>
2013-08-07 16:59:15 -0430

    #3592 structurewrapper now stores field variables

--- src/com/dotcms/publisher/pusher/wrapper/StructureWrapper.java
+++ src/com/dotcms/publisher/pusher/wrapper/StructureWrapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.dotcms.publisher.pusher.PushPublisherConfig.Operation;
 import com.dotmarketing.portlets.structure.model.Field;
+import com.dotmarketing.portlets.structure.model.FieldVariable;
 import com.dotmarketing.portlets.structure.model.Structure;
 
 public class StructureWrapper {
@@ -12,13 +13,23 @@ public class StructureWrapper {
 	private String workflowSchemaId;
 	private String workflowSchemaName;
 	private Operation operation;
+	private List<FieldVariable> fieldVariables;
 	
-	public StructureWrapper() {}
+    public StructureWrapper() {}
 	
-	public StructureWrapper(Structure structure, List<Field> fields) {
+	public StructureWrapper(Structure structure, List<Field> fields, List<FieldVariable> variables) {
 		this.structure = structure;
 		this.fields = fields;
+		this.fieldVariables = variables;
 	}
+	
+	public List<FieldVariable> getFieldVariables() {
+        return fieldVariables;
+    }
+
+    public void setFieldVariables(List<FieldVariable> fieldVariables) {
+        this.fieldVariables = fieldVariables;
+    }
 
 	public String getWorkflowSchemaName() {
         return workflowSchemaName;
