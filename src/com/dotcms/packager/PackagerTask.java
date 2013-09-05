43d67b71c8934a2351a8eae13eb00405f70171be
Jonathan Gamba <jonathan.gamba@dotcms.com>
2013-09-05 16:12:57 -0600

    #3266

--- src/com/dotcms/packager/PackagerTask.java
+++ src/com/dotcms/packager/PackagerTask.java
@@ -1,6 +1,5 @@
-package com.dotcms.util;
+package com.dotcms.packager;
 
-import com.dotcms.packager.Inspector;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
@@ -38,8 +37,17 @@ public class PackagerTask extends Task {
 
         File libFolder = new File( this.libraryPath );
         log( "Reading from: " + libFolder.getAbsolutePath(), Project.MSG_INFO );
+
+        //Create a formatter to read and display the results
+        Formatter formatter = new PlainFormatter();
+        formatter.setOutput( System.out );
+        formatter.setDuplicatesOnly( true );
+
+        //Create the inspector in order to analyze the given path
         Inspector inspector = new Inspector();
+        inspector.addFormatter( formatter );
         inspector.inspect( libFolder );
+        inspector.report( "Duplicated classes" );//Generate a report
 
         log( "Found " + inspector.getClassCount() + " unique classes", Project.MSG_INFO );
         log( "Found " + inspector.getDuplicateCount() + " duplicated classes", Project.MSG_INFO );
