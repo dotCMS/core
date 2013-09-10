3da5a2f1e7c61b5ab0325c12e0d02e86db5279fe
Jonathan Gamba <jonathan.gamba@dotcms.com>
2013-09-10 17:36:47 -0600

    #3266
    Former-commit-id: 03d44b71445496e6ae480a86f706607e2996b81e

---
+++ src/com/dotcms/packager/InspectorTask.java
@@ -0,0 +1,60 @@
+package com.dotcms.packager;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Task;
+
+import java.io.File;
+
+/**
+ * @author Jonathan Gamba
+ *         Date: 9/10/13
+ */
+public class InspectorTask extends Task {
+
+    private String libraryPath;
+    public static Inspector globalInspector;
+
+    public void setLibraryPath ( String libraryPath ) {
+        this.libraryPath = libraryPath;
+    }
+
+    /*public static void main ( String[] args ) {
+
+        InspectorTask task = new InspectorTask();
+        task.setLibraryPath( args[0] );
+        task.execute();
+    }*/
+
+    @Override
+    public void execute () throws BuildException {
+
+        log( "Executing Packager task....", Project.MSG_INFO );
+
+        // Validate input
+        if ( this.libraryPath == null ) {
+            throw new IllegalArgumentException( "No path element to inspect!" );
+        }
+
+        File libFolder = new File( this.libraryPath );
+        log( "Reading from: " + libFolder.getAbsolutePath(), Project.MSG_INFO );
+
+        //Create a formatter to read and display the results
+        Formatter formatter = new PlainFormatter();
+        formatter.setOutput( System.out );
+        formatter.setDuplicatesOnly( true );
+
+        //Create the inspector in order to analyze the given path
+        Inspector inspector = new Inspector();
+        inspector.addFormatter( formatter );
+        inspector.inspect( libFolder );
+        inspector.report( "Duplicated classes" );//Generate a report
+
+        //Sharing the resulting info
+        globalInspector = inspector;
+
+        log( "Found " + inspector.getClassCount() + " unique classes", Project.MSG_INFO );
+        log( "Found " + inspector.getDuplicateCount() + " duplicated classes", Project.MSG_INFO );
+    }
+
+}
