5774ade31f595d1e8ad77acf76ffb6405f203ecf
Jorge Urdaneta <jorge.urdaneta@dotcms.com>
2013-09-17 13:47:33 -0430

    #3966 mssql not unique

--- sql/cms/dotcms_extra_mssql.sql
+++ sql/cms/dotcms_extra_mssql.sql
@@ -756,7 +756,7 @@ CREATE TABLE publishing_queue (
 
 create table publishing_bundle(
   	  id varchar(36) NOT NULL  primary key,
-	  name varchar(255) NOT NULL unique,
+	  name varchar(255) NOT NULL,
  	  publish_date DATETIME,
 	  expire_date DATETIME,
 	  owner varchar(100)
