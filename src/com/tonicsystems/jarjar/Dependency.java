b8646ac5551fecc2ede87b858923a57561241288
Jonathan Gamba <jonathan.gamba@dotcms.com>
2013-11-07 08:30:47 -0600

    #3266
    Former-commit-id: bd48bd9cbfe4ef5c6fd79cb9cd08db6fb29157c9

---
+++ src/com/tonicsystems/jarjar/Dependency.java
@@ -0,0 +1,22 @@
+package com.tonicsystems.jarjar;
+
+/**
+ * @author Jonathan Gamba
+ *         Date: 11/6/13
+ */
+public class Dependency {
+
+    private String path;
+
+    public Dependency () {
+    }
+
+    public String getPath () {
+        return this.path;
+    }
+
+    public void setPath ( String path ) {
+        this.path = path;
+    }
+
+}
