52502d1ed16e93fd50234b16781909aadb22e6f3
Jonathan Gamba <jonathan.gamba@dotcms.com>
2013-11-18 17:25:45 -0600

    #3266
    Former-commit-id: 48a259346aaea0969055d12389d46d576af84cdc

---
+++ src/com/tonicsystems/jarjar/PackagerWildcard.java
@@ -0,0 +1,65 @@
+package com.tonicsystems.jarjar;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Jonathan Gamba
+ *         Date: 10/24/13
+ */
+public class PackagerWildcard {
+
+    private Collection<Pattern> patterns;
+    private Pattern simplePattern;
+    private String replaceWith;
+
+    public PackagerWildcard ( Rule rule ) {
+        init( rule.getPattern(), rule.getResult() );
+    }
+
+    public PackagerWildcard ( String pattern, String result ) {
+        init( pattern, result );
+    }
+
+    private void init ( String pattern, String result ) {
+
+        if ( pattern.equals( "**" ) ) {
+            throw new IllegalArgumentException( "'**' is not a valid pattern" );
+        }
+        if ( pattern.contains( "***" ) ) {
+            throw new IllegalArgumentException( "The sequence '***' is invalid in a package pattern" );
+        }
+
+        patterns = new ArrayList<Pattern>();
+
+        String regex = pattern;
+        regex = regex.replaceAll( "\\*\\*", "" );
+        simplePattern = Pattern.compile( regex );
+        patterns.add( Pattern.compile( "(\"|>| )" + regex + "\\w+(<|\"|\\n|,)" ) );
+
+        //Clean up the result
+        if ( result.lastIndexOf( "@" ) != -1 ) {
+            result = result.substring( 0, result.lastIndexOf( "@" ) );
+        }
+        this.replaceWith = result;
+    }
+
+    public String replace ( String value ) {
+
+        for ( Pattern pattern : patterns ) {
+
+            Matcher matcher = pattern.matcher( value );
+            while ( matcher.find() ) {
+
+                String group = matcher.group();
+                String replacement = group.replaceAll( simplePattern.pattern(), replaceWith );
+                value = value.replaceAll( group, replacement );
+            }
+        }
+
+        return value;
+    }
+
+}
