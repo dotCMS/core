6ee4c6b53965bb8fd427bf74597ebcb92d126beb
Daniel Silva <daniel.silva@dotcms.com>
2013-12-03 10:10:07 -0430

    #3546

--- src/com/dotcms/cluster/bean/ESProperty.java
+++ src/com/dotcms/cluster/bean/ESProperty.java
@@ -11,7 +11,7 @@ public enum ESProperty {
 	ES_NETWORK_HOST(Config.getStringProperty("es.network.host", "localhost"), "es.network.host"),
 	ES_TRANSPORT_TCP_PORT(Config.getStringProperty("es.transport.tcp.port", null), "es.transport.tcp.port"),
 	ES_HTTP_PORT(Config.getStringProperty("es.http.port", null), "es.http.port"),
-	ES_DISCOVERY_ZEN_PING_MULTICAST_ENABLED(Config.getStringProperty("es.discovery.zen.ping.multicast.enabled", null), "es.discovery.zen.ping.multicast.enabled"),
+	ES_DISCOVERY_ZEN_PING_MULTICAST_ENABLED(Config.getStringProperty("es.discovery.zen.ping.multicast.enabled", "true"), "es.discovery.zen.ping.multicast.enabled"),
 	ES_DISCOVERY_ZEN_PING_TIMEOUT(Config.getStringProperty("es.discovery.zen.ping.timeout", null), "es.discovery.zen.ping.timeout"),
 	ES_DISCOVERY_ZEN_PING_UNICAST_HOSTS(Config.getStringProperty("es.discovery.zen.ping.unicast.hosts", null), "es.discovery.zen.ping.unicast.hosts");
 
