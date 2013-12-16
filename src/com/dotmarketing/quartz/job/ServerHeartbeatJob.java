9a6450d11eae82780f8fa220a68f751db1ec285e
Daniel Silva <daniel.silva@dotcms.com>
2013-12-15 20:48:27 -0430

    #3546
    Former-commit-id: bcf0782cb303b8e3dc676db4b5e5ff534c84f40a

--- src/com/dotmarketing/quartz/job/ServerHeartbeatJob.java
+++ src/com/dotmarketing/quartz/job/ServerHeartbeatJob.java
@@ -3,9 +3,9 @@
  */
 package com.dotmarketing.quartz.job;
 
+import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.quartz.StatefulJob;
 
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.exception.DotDataException;
@@ -16,7 +16,7 @@ import com.dotmarketing.util.Logger;
  * The idea here is to act as a heart beat of the server for clustering purposes
  *
  */
-public class ServerHeartbeatJob implements StatefulJob {
+public class ServerHeartbeatJob implements Job {
 
 	public void execute(JobExecutionContext ctx) throws JobExecutionException {
 		try {
