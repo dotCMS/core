198bca99e828a6158c70688405a28f8b24bdd34a
Daniel Silva <daniel.silva@dotcms.com>
2013-12-18 15:18:42 -0430

    fixes #4556

--- src/com/dotcms/publisher/business/PublisherAPIImpl.java
+++ src/com/dotcms/publisher/business/PublisherAPIImpl.java
@@ -1,6 +1,8 @@
 package com.dotcms.publisher.business;
 
+import com.dotcms.publisher.bundle.bean.Bundle;
 import com.dotcms.publisher.business.PublishAuditStatus.Status;
+import com.dotcms.publisher.environment.bean.Environment;
 import com.dotcms.publisher.mapper.PublishQueueMapper;
 import com.dotcms.publisher.util.PublisherUtil;
 import com.dotmarketing.beans.Identifier;
@@ -118,9 +120,9 @@ public class PublisherAPIImpl extends PublisherAPI{
         List<String> errorsList = new ArrayList<String>();
 
     	  if ( identifiers != null ) {
-    		  
+
     		  boolean localTransaction = false;
-    			
+
     		  try {
     			  localTransaction = HibernateUtil.startLocalTransactionIfNeeded();
     		  } catch(DotDataException dde) {
@@ -694,15 +696,15 @@ public class PublisherAPIImpl extends PublisherAPI{
 	 * @throws DotPublisherException
 	 */
 	public void updateElementStatusFromPublishQueueTable(long id, Date last_try,int num_of_tries, boolean in_error,String last_results ) throws DotPublisherException {
-		
+
 		boolean localTransaction = false;
-		
+
 		try {
 			localTransaction = HibernateUtil.startLocalTransactionIfNeeded();
 		} catch(DotDataException dde) {
 			throw new DotPublisherException("Error starting Transaction", dde);
 		}
-		
+
 		try{
 			DotConnect dc = new DotConnect();
 			if(DbConnectionFactory.getDBType().equals(DbConnectionFactory.POSTGRESQL)){
@@ -721,7 +723,7 @@ public class PublisherAPIImpl extends PublisherAPI{
 			dc.addParam(last_results);
 			dc.addParam(id);
 			dc.loadResult();
-			
+
 			if(localTransaction){
                 HibernateUtil.commitTransaction();
             }
@@ -753,13 +755,13 @@ public class PublisherAPIImpl extends PublisherAPI{
 	 */
 	public void deleteElementFromPublishQueueTable(String identifier) throws DotPublisherException{
 		boolean localTransaction = false;
-		
+
 		try {
 			localTransaction = HibernateUtil.startLocalTransactionIfNeeded();
 		} catch(DotDataException dde) {
 			throw new DotPublisherException("Error starting Transaction", dde);
 		}
-		
+
 		try{
 			DotConnect dc = new DotConnect();
 			if(DbConnectionFactory.getDBType().equals(DbConnectionFactory.POSTGRESQL)){
@@ -774,7 +776,7 @@ public class PublisherAPIImpl extends PublisherAPI{
 			}
 			dc.addParam(identifier);
 			dc.loadResult();
-			
+
 			if(localTransaction) {
 			    HibernateUtil.commitTransaction();
 			}
@@ -806,13 +808,13 @@ public class PublisherAPIImpl extends PublisherAPI{
 	 */
 	public void deleteElementsFromPublishQueueTable(String bundleId) throws DotPublisherException{
 		boolean localTransaction = false;
-		
+
 		try {
 			localTransaction = HibernateUtil.startLocalTransactionIfNeeded();
 		} catch(DotDataException dde) {
 			throw new DotPublisherException("Error starting Transaction", dde);
 		}
-		
+
 		try{
 			DotConnect dc = new DotConnect();
 			if(DbConnectionFactory.getDBType().equals(DbConnectionFactory.POSTGRESQL)){
@@ -827,7 +829,7 @@ public class PublisherAPIImpl extends PublisherAPI{
 			}
 			dc.addParam(bundleId);
 			dc.loadResult();
-			
+
 			if(localTransaction) {
 			    HibernateUtil.commitTransaction();
 			}
@@ -854,13 +856,13 @@ public class PublisherAPIImpl extends PublisherAPI{
 	 */
 	public void deleteAllElementsFromPublishQueueTable() throws DotPublisherException{
 		boolean localTransaction = false;
-		
+
 		try {
 			localTransaction = HibernateUtil.startLocalTransactionIfNeeded();
 		} catch(DotDataException dde) {
 			throw new DotPublisherException("Error starting Transaction", dde);
 		}
-		
+
 		try{
 			DotConnect dc = new DotConnect();
 			if(DbConnectionFactory.getDBType().equals(DbConnectionFactory.POSTGRESQL)){
@@ -874,7 +876,7 @@ public class PublisherAPIImpl extends PublisherAPI{
 				dc.setSQL(OCLDELETEALLELEMENTFROMQUEUESQL);
 			}
 			dc.loadResult();
-			
+
 			if(localTransaction) {
                 HibernateUtil.commitTransaction();
             }
@@ -959,7 +961,7 @@ public class PublisherAPIImpl extends PublisherAPI{
 
 		publishBundleAssets(bundleId, publishDate);
 
-        // insert a new version of each asset but with the unpublish operation and the expiration date
+        // insert a new version of each asset but with the unpublish operation, the expiration date AND a NEW BUNDLE ID
 
 		List<PublishQueueElement> assets = getQueueElementsByBundleId(bundleId);
 
@@ -969,7 +971,20 @@ public class PublisherAPIImpl extends PublisherAPI{
 			ids.add(asset.getAsset());
 		}
 
-		addAssetsToQueue(ids, bundleId, expireDate, user, DELETE_ELEMENT);
+		try {
+
+			Bundle publishBundle = APILocator.getBundleAPI().getBundleById(bundleId);
+			List<Environment> envsToSendTo = APILocator.getEnvironmentAPI().findEnvironmentsByBundleId(bundleId);
+
+			Bundle deleteBundle = new Bundle(publishBundle.getName(), publishDate, expireDate, user.getUserId(), publishBundle.isForcePush());
+	    	APILocator.getBundleAPI().saveBundle(deleteBundle, envsToSendTo);
+
+	        addContentsToUnpublish( ids, deleteBundle.getId(), expireDate, user );
+
+		} catch (DotDataException e) {
+			throw new DotPublisherException(e);
+		}
+
 	}
 
 }
