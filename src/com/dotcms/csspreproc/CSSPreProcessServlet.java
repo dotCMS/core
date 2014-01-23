f52de70c998b0445a4add20937c30e9af54eeac4
Jorge Urdaneta <jorge.urdaneta@dotcms.com>
2014-01-23 15:06:32 -0600

    #3773 adding the servlet and stuff

---
+++ src/com/dotcms/csspreproc/CSSPreProcessServlet.java
@@ -0,0 +1,18 @@
+package com.dotcms.csspreproc;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CSSPreProcessServlet extends HttpServlet {
+    private static final long serialVersionUID = -3315180323197314439L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        
+    }
+}
