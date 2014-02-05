cdcfcc8b2f4cb0376150d66fd761452ee5fd6c4e
Jason Tesser <jason.tesser@dotcms.com>
2014-02-05 07:27:43 -0500

    #4824

--- src-conf/mssql.sql
+++ src-conf/mssql.sql
@@ -3423,9 +3423,9 @@ CREATE INDEX idx_pub_qa_1 ON publishing_queue_audit (status);
 
 --Cluster Tables
 
-CREATE TABLE cluster(cluster_id varchar(36), PRIMARY KEY (cluster_id) );
+CREATE TABLE dot_cluster(cluster_id varchar(36), PRIMARY KEY (cluster_id) );
 CREATE TABLE cluster_server(server_id varchar(36) NOT NULL, cluster_id varchar(36) NOT NULL, name varchar(100), ip_address varchar(39) NOT NULL, host varchar(36), cache_port SMALLINT, es_transport_tcp_port SMALLINT, es_network_port SMALLINT, es_http_port SMALLINT, PRIMARY KEY (server_id) );
-ALTER TABLE cluster_server add constraint fk_cluster_id foreign key (cluster_id) REFERENCES cluster(cluster_id);
+ALTER TABLE cluster_server add constraint fk_cluster_id foreign key (cluster_id) REFERENCES dot_cluster(cluster_id);
 CREATE TABLE cluster_server_uptime(id varchar(36) NOT NULL, server_id varchar(36) NOT NULL, startup datetime null, heartbeat datetime null, PRIMARY KEY (id) );
 ALTER TABLE cluster_server_uptime add constraint fk_cluster_server_id foreign key (server_id) REFERENCES cluster_server(server_id);
 
