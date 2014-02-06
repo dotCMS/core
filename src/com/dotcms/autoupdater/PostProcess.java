9b0406e051b7a19f830743c9e1c3839e0607c49f
Jonathan Gamba <jonathan.gamba@dotcms.com>
2014-02-06 16:52:09 -0600

    #4566

--- src/com/dotcms/autoupdater/PostProcess.java
+++ src/com/dotcms/autoupdater/PostProcess.java
@@ -1,30 +1,29 @@
 package com.dotcms.autoupdater;
 
+import java.io.File;
 import java.io.IOException;
 
 public class PostProcess {
 
-    private String home;
+    private String distributionHome;
+    private String dotcmsHome;
 
-    public String getHome () {
-        return home;
-    }
-
-    public void setHome ( String home ) {
-        this.home = home;
+    public PostProcess ( String distributionHome, String dotcmsHome ) {
+        this.distributionHome = distributionHome;
+        this.dotcmsHome = dotcmsHome;
     }
 
     public boolean postProcess ( boolean clean ) {
 
-        AntInvoker invoker = new AntInvoker( home );
+        AntInvoker invoker = new AntInvoker( getDistributionHome() );
         try {
             if ( clean ) {
-                boolean ret = invoker.runTask( "clean-plugins", null );
+                boolean ret = invoker.runTask( "clean-plugins", getDistributionHome() + File.separator + UpdateAgent.FOLDER_HOME_BIN + File.separator + "ant" + File.separator + "build.xml" );
                 if ( !ret ) {
                     return false;
                 }
             }
-            return invoker.runTask( "deploy-plugins", null );
+            return invoker.runTask( "deploy-plugins", getDistributionHome() + File.separator + UpdateAgent.FOLDER_HOME_BIN + File.separator + "ant" + File.separator + "build.xml" );
 
         } catch ( IOException e ) {
             UpdateAgent.logger.fatal( "IOException: " + e.getMessage(), e );
@@ -33,9 +32,24 @@ public class PostProcess {
     }
 
     public boolean checkRequisites () throws UpdateException {
-
-        AntInvoker invoker = new AntInvoker( home );
+        AntInvoker invoker = new AntInvoker( getDistributionHome() );
         return invoker.checkRequisites();
+    }
+
+    public String getDistributionHome () {
+        return distributionHome;
+    }
+
+    public void setDistributionHome ( String distributionHome ) {
+        this.distributionHome = distributionHome;
+    }
+
+    public String getDotcmsHome () {
+        return dotcmsHome;
+    }
+
+    public void setDotcmsHome ( String dotcmsHome ) {
+        this.dotcmsHome = dotcmsHome;
     }
 
 }
