c520c8742e25662a62c0ef0ef68223832cd9c557
Daniel Silva <daniel.silva@dotcms.com>
2014-02-21 18:36:20 -0430

    fixes #4921

--- src/com/dotmarketing/servlets/AjaxFileUploadServlet.java
+++ src/com/dotmarketing/servlets/AjaxFileUploadServlet.java
@@ -3,6 +3,8 @@ package com.dotmarketing.servlets;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
@@ -22,15 +24,16 @@ import com.dotmarketing.business.APILocator;
 import com.dotmarketing.portlets.contentlet.util.ContentletUtil;
 import com.dotmarketing.util.Config;
 import com.dotmarketing.util.Constants;
+import com.liferay.util.FileUtil;
 import com.missiondata.fileupload.MonitoredDiskFileItemFactory;
 
 public class AjaxFileUploadServlet extends HttpServlet {
-	
+
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	boolean isEmptyFile ;
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
@@ -50,61 +53,74 @@ public class AjaxFileUploadServlet extends HttpServlet {
 
 	private void doFileRetrieve(HttpSession session, HttpServletRequest request,
 			HttpServletResponse response) throws IOException {
-		
-		String userId = session.getAttribute("USER_ID").toString();
-		String fieldName = request.getParameter("fieldName");
-		String fileName = request.getParameter("fileName");
 
-		File tempUserFolder = new File(APILocator.getFileAPI().getRealAssetPathTmpBinary() + File.separator + userId + File.separator + fieldName);
-		File file = new File(tempUserFolder.getAbsolutePath() + File.separator + fileName);
-		if(file.exists()) {
-			FileInputStream fis = new FileInputStream(file);
-			byte[] buffer = new byte[1000];
-			int count = 0;
-			String mimeType = this.getServletContext().getMimeType(file.getName());
-			response.setContentType(mimeType);
-			ServletOutputStream outStream = response.getOutputStream();
-			while((count = fis.read(buffer)) > 0) {
-				outStream.write(buffer, 0, count);
+		try {
+
+			String userId = session.getAttribute("USER_ID").toString();
+			String fieldName = request.getParameter("fieldName");
+			String fileName = request.getParameter("fileName");
+
+			File tempUserFolder = new File(APILocator.getFileAPI().getRealAssetPathTmpBinary() + File.separator + userId + File.separator + fieldName);
+
+			File file = new File(tempUserFolder.getAbsolutePath() + File.separator + fileName);
+
+			if(!isValidPath(file.getCanonicalPath())) {
+				throw new Exception("Invalid fileName or Path");
 			}
-			outStream.flush();
-			outStream.close();
+
+			if(file.exists()) {
+				FileInputStream fis = new FileInputStream(file);
+				byte[] buffer = new byte[1000];
+				int count = 0;
+				String mimeType = this.getServletContext().getMimeType(file.getName());
+				response.setContentType(mimeType);
+				ServletOutputStream outStream = response.getOutputStream();
+				while((count = fis.read(buffer)) > 0) {
+					outStream.write(buffer, 0, count);
+				}
+				outStream.flush();
+				outStream.close();
+			}
+
+		} catch (Exception e) {
+			sendCompleteResponse(response, e.getMessage());
+			e.printStackTrace();
 		}
 
 	}
-	
+
 	private void doFileUpload(HttpSession session, HttpServletRequest request,
 			HttpServletResponse response) throws IOException {
 		String fieldName =null;
 		AjaxFileUploadListener listener = null;
 		try {
-			
+
 			String fileName = "";
 
 			listener = new AjaxFileUploadListener(request.getContentLength());
 			FileItemFactory factory = new MonitoredDiskFileItemFactory(listener);
 			fieldName = request.getParameter("fieldName");
-			Enumeration params = request.getParameterNames(); 
+			Enumeration params = request.getParameterNames();
 			session.setAttribute("FILE_UPLOAD_STATS_" + fieldName, listener.getFileUploadStats());
 			ServletFileUpload upload = new ServletFileUpload(factory);
-			
+
 			List items = upload.parseRequest(request);
 			boolean hasError = false;
 			isEmptyFile = false;
-			
-			
+
+
 			String userId = session.getAttribute("USER_ID").toString();
-			
+
 			for (Iterator i = items.iterator(); i.hasNext();) {
 				FileItem fileItem = (FileItem) i.next();
 
 				if (!fileItem.isFormField()) {
-					
+
 
 					// *************************************************
 					// This is where you would process the uploaded file
 					// *************************************************
-					
+
 					if(fileItem.getSize() == 0)
 						isEmptyFile = true;
 
@@ -116,8 +132,13 @@ public class AjaxFileUploadServlet extends HttpServlet {
 					}
 					fileName = ContentletUtil.sanitizeFileName(fileName);
 
-					File tempUserFolder = new File(APILocator.getFileAPI().getRealAssetPathTmpBinary() + File.separator + userId + 
+					File tempUserFolder = new File(APILocator.getFileAPI().getRealAssetPathTmpBinary() + File.separator + userId +
 							File.separator + fieldName);
+
+					if(!isValidPath(tempUserFolder.getCanonicalPath())) {
+						throw new IOException("Invalid fileName or Path");
+					}
+
 					if (!tempUserFolder.exists())
 						tempUserFolder.mkdirs();
 					File dest=new File(tempUserFolder.getAbsolutePath() + File.separator + fileName);
@@ -127,7 +148,7 @@ public class AjaxFileUploadServlet extends HttpServlet {
 					fileItem.delete();
 				}
 			}
-			
+
 			if(isEmptyFile)
 				fileName = "";
 
@@ -145,11 +166,19 @@ public class AjaxFileUploadServlet extends HttpServlet {
 	}
 
 	private void sendCompleteResponse(HttpServletResponse response, String errorMessage) throws IOException {
-		if (errorMessage == null) {			
+		if (errorMessage == null) {
 			response.getOutputStream().print("");
 		} else {
-			response.getOutputStream().print("");
+			response.getOutputStream().print(errorMessage);
 		}
 	}
-	
+
+	private static boolean isValidPath(String path) throws IOException {
+		Path child = Paths.get(path).toAbsolutePath();
+		String tempBinaryPath = new File(APILocator.getFileAPI().getRealAssetPathTmpBinary()).getCanonicalPath();
+		Path parent = Paths.get(tempBinaryPath).toAbsolutePath();
+
+		return child.startsWith(parent);
+	}
+
 }
