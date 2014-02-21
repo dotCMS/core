f5038b39dddb5bc8c1e3ba58a60a2acec0a3c0e3
Jonathan Gamba <jonathan.gamba@dotcms.com>
2014-02-21 15:21:26 -0600

    #4917

--- src/com/liferay/util/PwdGenerator.java
+++ src/com/liferay/util/PwdGenerator.java
@@ -22,45 +22,39 @@
 
 package com.liferay.util;
 
+import org.apache.commons.lang.RandomStringUtils;
+
 /**
  * <a href="PwdGenerator.java.html"><b><i>View Source</i></b></a>
  *
- * @author  Brian Wing Shun Chan
+ * @author Brian Wing Shun Chan
  * @version $Revision: 1.6 $
- *
  */
 public class PwdGenerator {
 
-	public static String KEY1 = "0123456789";
+    public static String KEY1 = "0123456789";
+    public static String KEY2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static String KEY3 = "abcdefghijklmnopqrstuvwxyz";
+    public static String KEY4 = "`~!@#$%^&*()_-+={}[]\\|:;\"'<>,.?/";
 
-	public static String KEY2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static String getPinNumber () {
+        return _getPassword( KEY1, 4 );
+    }
 
-	public static String KEY3 = "abcdefghijklmnopqrstuvwxyz";
+    public static String getPassword () {
+        return getPassword( 12 );
+    }
 
-	public static String getPinNumber() {
-		return _getPassword(KEY1, 4);
-	}
+    public static String getPassword ( int length ) {
+        return _getPassword( KEY1 + KEY2 + KEY3 + KEY4, length );
+    }
 
-	public static String getPassword() {
-		return getPassword(8);
-	}
+    public static String getPassword ( String key, int length ) {
+        return _getPassword( key, length );
+    }
 
-	public static String getPassword(int length) {
-		return _getPassword(KEY1 + KEY2 + KEY3, length);
-	}
-
-	public static String getPassword(String key, int length) {
-		return _getPassword(key, length);
-	}
-
-	private static String _getPassword(String key, int length) {
-		StringBuffer sb = new StringBuffer();
-
-		for (int i = 0; i < length; i++) {
-			sb.append(key.charAt((int)(Math.random() * key.length())));
-		}
-
-		return sb.toString();
-	}
+    private static String _getPassword ( String key, int length ) {
+        return RandomStringUtils.random( length, key );
+    }
 
 }
