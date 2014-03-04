4ef55b54603da90a7e35ff66252e20bad04cb07b
Daniel Silva <daniel.silva@dotcms.com>
2014-03-04 18:02:48 -0430

    fixes #5041

--- src/com/dotmarketing/portlets/virtuallinks/business/VirtualLinkFactory.java
+++ src/com/dotmarketing/portlets/virtuallinks/business/VirtualLinkFactory.java
@@ -8,25 +8,32 @@ import com.dotmarketing.portlets.virtuallinks.model.VirtualLink;
 public interface VirtualLinkFactory {
 	/**
 	 * Get all the virtual links filtered by the title and/or url.
-	 * 
+	 *
 	 * @param title
 	 * @param url
 	 * @param orderby
 	 * @return List<VirtualLink>
 	 */
 	public List<VirtualLink> getVirtualLinks(String title, String url, VirtualLinkAPI.OrderBy orderby);
-	
+
 	/**
 	 * Retrieves all virtual links associated to a host
 	 * @param host
 	 * @return
 	 */
 	public List<VirtualLink> getHostVirtualLinks(Host host);
-	
-	
+
+	/**
+	 * Retrieves all virtual links associated to a host with the specified URI
+	 * @param host
+	 * @return
+	 */
+	public List<VirtualLink> getVirtualLinksByURI(String uri);
+
+
 	/**
 	 * Get all the virtual links filtered by the title and/or the list of hosts.
-	 * 
+	 *
 	 * @param title
 	 * @param hosts
 	 * @param orderby
