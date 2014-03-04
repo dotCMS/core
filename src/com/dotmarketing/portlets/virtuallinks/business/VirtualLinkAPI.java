1c98177d692bfbdbffe6dbf0a0b769b1704d29a4
Daniel Silva <daniel.silva@dotcms.com>
2014-03-04 18:02:48 -0430

    fixes #5041

--- src/com/dotmarketing/portlets/virtuallinks/business/VirtualLinkAPI.java
+++ src/com/dotmarketing/portlets/virtuallinks/business/VirtualLinkAPI.java
@@ -10,50 +10,50 @@ import com.dotmarketing.portlets.virtuallinks.model.VirtualLink;
 import com.liferay.portal.model.User;
 
 public interface VirtualLinkAPI {
-	public enum OrderBy { 
+	public enum OrderBy {
 		TITLE,
 		DATE_ADDED,
 		URL
 	}
-	
+
 	/**
 	 * Get all the virtual links filtered by the title and/or url.
-	 * 
+	 *
 	 * @param title
 	 * @param url
 	 * @param orderby
 	 * @return List<VirtualLink>
 	 */
 	public List<VirtualLink> getVirtualLinks(String title, String url, OrderBy orderby);
-	
+
 	/**
 	 * Copies a virtual link and moves it to the passed host
 	 * @param sourceVirtualLink
 	 * @return
-	 * @throws DotHibernateException 
+	 * @throws DotHibernateException
 	 */
 	public VirtualLink copyVirtualLink(VirtualLink sourceVirtualLink, Host destinationHost) throws DotHibernateException;
-	
+
 	/**
 	 * Retrieves all virtual links associated to a host
 	 * @param host
 	 * @return
 	 */
 	public List<VirtualLink> getHostVirtualLinks(Host host);
-	
-	
+
+
 	/**
 	 * Get all the virtual links filtered by the title and/or the list of hosts.
-	 * 
+	 *
 	 * @param title
 	 * @param hosts
 	 * @param orderby
 	 * @return
 	 */
 	public List<VirtualLink> getVirtualLinks(String title, List<Host> hosts, OrderBy orderby);
-	
+
 	/**
-	 * 
+	 *
 	 * @param list
 	 * @param user
 	 * @return
@@ -61,9 +61,9 @@ public interface VirtualLinkAPI {
 	 * @throws DotSecurityException
 	 */
 	public List<VirtualLink> checkListForCreateVirtualLinkspermission(java.util.List<VirtualLink> list,User user) throws DotDataException, DotSecurityException;
-	
+
 	/**
-	 * 
+	 *
 	 * @param link
 	 * @param user
 	 * @return
@@ -71,4 +71,11 @@ public interface VirtualLinkAPI {
 	 * @throws DotSecurityException
 	 */
 	public VirtualLink checkVirtualLinkForEditPermissions(VirtualLink link,User user) throws DotDataException, DotSecurityException;
+
+	/**
+	 * Retrieves all virtual links associated to a host with the specified URI
+	 * @param host
+	 * @return
+	 */
+	public List<VirtualLink> getVirtualLinksByURI(String uri);
 }
