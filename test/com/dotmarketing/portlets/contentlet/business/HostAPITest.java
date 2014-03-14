b5e4bb0cb8519827950e57ac4ac8b5342d6ba7b9
Oswaldo Gallango <oswaldo@dotcms.com>
2014-03-14 16:10:35 -0600

    fixes #5130 included documentation to test method

--- test/com/dotmarketing/portlets/contentlet/business/HostAPITest.java
+++ test/com/dotmarketing/portlets/contentlet/business/HostAPITest.java
@@ -68,9 +68,14 @@ public class HostAPITest {
     @Test
     public void makeDefault() throws Exception {
     	User user=APILocator.getUserAPI().getSystemUser();
-    	
+    	/*
+    	 * Get the current Default host
+    	 */
     	Host hdef = APILocator.getHostAPI().findDefaultHost(user, false);
     	
+    	/*
+    	 * Create a new Host and make it default
+    	 */
     	Host host=new Host();
         host.setHostname("test"+System.currentTimeMillis()+".demo.dotcms.com");
         host.setDefault(false);
@@ -79,18 +84,35 @@ public class HostAPITest {
         APILocator.getHostAPI().makeDefault(host, user, false);
         APILocator.getContentletAPI().isInodeIndexed(host.getInode());
         
+        /*
+         * Validate if the previous default host. Is live and not default
+         */
         hdef = APILocator.getHostAPI().find(hdef.getIdentifier(), user, false);
         Assert.assertTrue(hdef.isLive());
         Assert.assertFalse(hdef.isDefault());
         
+        /*
+         * get Back to default the previous host
+         */
         APILocator.getHostAPI().makeDefault(hdef, user, false);
         APILocator.getContentletAPI().isInodeIndexed(hdef.getInode());
         
+        /*
+         * Validate if the new host is not default anymore and if its live
+         */
         host = APILocator.getHostAPI().find(host.getIdentifier(), user, false);
         Assert.assertFalse(host.isDefault());
         Assert.assertTrue(host.isLive());
         
-        Host def = APILocator.getHostAPI().findDefaultHost(user, false);
-        Assert.assertEquals(hdef.getIdentifier(), def.getIdentifier());
+        /*
+         * Delete the new test host
+         */
+        APILocator.getHostAPI().delete(host, user, false);
+        
+        /*
+         * Validate if the current Original default host is the current default one
+         */
+        host = APILocator.getHostAPI().findDefaultHost(user, false);
+        Assert.assertEquals(hdef.getIdentifier(), host.getIdentifier());
     }
 }
