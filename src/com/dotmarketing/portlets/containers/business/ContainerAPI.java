041c2a103f9e4cfb2b8eafaa0f924678f7d3f4d0
Jason Tesser <jason.tesser@dotcms.com>
2014-05-14 08:51:10 -0400

    master-2.5 merge #5086

--- src/com/dotmarketing/portlets/containers/business/ContainerAPI.java
+++ src/com/dotmarketing/portlets/containers/business/ContainerAPI.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import com.dotmarketing.beans.ContainerStructure
+import com.dotmarketing.beans.ContainerStructure;
 import com.dotmarketing.beans.Host;
 import com.dotmarketing.business.DotStateException;
 import com.dotmarketing.exception.DotDataException;
