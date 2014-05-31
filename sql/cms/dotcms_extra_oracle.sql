4d5f55950c2f403fb551c7f7647d1aa62b36500a
Daniel Silva <daniel.silva@dotcms.com>
2014-05-30 20:23:06 -0430

    fixes #5679

--- sql/cms/dotcms_extra_oracle.sql
+++ sql/cms/dotcms_extra_oracle.sql
@@ -880,7 +880,12 @@ CREATE TABLE publishing_queue (
 
 DROP SEQUENCE PUBLISHING_QUEUE_SEQ;
 CREATE SEQUENCE PUBLISHING_QUEUE_SEQ START WITH 1 INCREMENT BY 1;
-CREATE OR REPLACE TRIGGER PUBLISHING_QUEUE_TRIGGER before insert on publishing_queue for each row begin select PUBLISHING_QUEUE_SEQ.nextval into :new.id from dual; end;
+
+CREATE OR REPLACE TRIGGER PUBLISHING_QUEUE_TRIGGER before
+insert on publishing_queue for each row
+begin select PUBLISHING_QUEUE_SEQ.nextval into :new.id from dual;
+end;
+/
 
 create table publishing_bundle(
 	  id varchar2(36) NOT NULL  primary key,
