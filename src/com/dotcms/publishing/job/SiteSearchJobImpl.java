ce86974a7a911092b8ad306d46acbfd8906eea10
Daniel Silva <daniel.silva@dotcms.com>
2014-08-12 15:16:20 -0430

    #5956

--- src/com/dotcms/publishing/job/SiteSearchJobImpl.java
+++ src/com/dotcms/publishing/job/SiteSearchJobImpl.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.dotcms.repackage.org.elasticsearch.ElasticSearchException;
+
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -28,6 +29,9 @@ import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.sitesearch.business.SiteSearchAPI;
 import com.dotmarketing.sitesearch.model.SiteSearchAudit;
+import com.dotmarketing.util.ActivityLogger;
+import com.dotmarketing.util.AdminLogger;
+import com.dotmarketing.util.DateUtil;
 import com.dotmarketing.util.Logger;
 import com.dotmarketing.util.StringUtils;
 import com.dotmarketing.util.UtilMethods;
@@ -45,16 +49,16 @@ public class SiteSearchJobImpl {
     public void run(JobExecutionContext jobContext) throws JobExecutionException, DotPublishingException, DotDataException, DotSecurityException, ElasticSearchException, IOException {
         if(LicenseUtil.getLevel()<200)
             return;
-        
+
         HibernateUtil.startTransaction();
 
         JobDataMap dataMap = jobContext.getJobDetail().getJobDataMap();
-        
+
         String jobId=(String)dataMap.get("JOB_ID");
         if(jobId==null) {
             jobId=dataMap.getString("QUARTZ_JOB_NAME");
         }
-        
+
         boolean indexAll = UtilMethods.isSet((String) dataMap.get("indexAll")) ? true : false;
         String[] indexHosts = null;
         Object obj = (dataMap.get("indexhost") != null) ?dataMap.get("indexhost") : new String[0];
@@ -65,14 +69,14 @@ public class SiteSearchJobImpl {
             indexHosts = (String[]) obj;
         }
 
-        
-        
+
+
         boolean incremental = dataMap.getString("incremental") != null;
-        
+
         User userToRun = APILocator.getUserAPI().getSystemUser();
 
         boolean include = ("all".equals(dataMap.getString("includeExclude")) || "include".equals(dataMap.getString("includeExclude")));
-        
+
         String path = dataMap.getString("paths");
         List<String> paths = new ArrayList<String>();
         if(path != null){
@@ -82,13 +86,13 @@ public class SiteSearchJobImpl {
                 if(UtilMethods.isSet(x)){
                     paths.add(x);
                 }
-            }   
+            }
         }
-        
+
         Date startDate,endDate;
         if(incremental) {
             endDate=jobContext.getFireTime();
-            
+
             startDate=null;
             try {
                 List<SiteSearchAudit> recentAudits = APILocator.getSiteSearchAuditAPI().findRecentAudits(jobId, 0, 1);
@@ -103,38 +107,38 @@ public class SiteSearchJobImpl {
             // set null explicitly just in case
             startDate=endDate=null;
         }
-        
+
         String[] languageToIndex=(String[])dataMap.get("langToIndex");
-        int counter = 0;  
+        int counter = 0;
         String indexName = null;
         boolean createNew=false;
         for(String lang : languageToIndex) {
         	counter = counter + 1;
             SiteSearchConfig config = new SiteSearchConfig();
             config.setJobId(jobId);
-            config.setLanguage(Long.parseLong(lang));       
-            
+            config.setLanguage(Long.parseLong(lang));
+
             config.setJobName(dataMap.getString("QUARTZ_JOB_NAME"));
-    
+
             List<Host> hosts=new ArrayList<Host>();
-    
+
             if(indexAll){
-                    hosts = APILocator.getHostAPI().findAll(userToRun, true);               
+                    hosts = APILocator.getHostAPI().findAll(userToRun, true);
             }else{
-                
+
                 for(String h : indexHosts){
                     hosts.add(APILocator.getHostAPI().find(h, userToRun, true));
                 }
-                
+
             }
-    
-            config.setHosts( hosts);        
-            
+
+            config.setHosts( hosts);
+
             // reuse or create new indexes as needed
             String indexAlias = dataMap.getString("indexAlias");
             ESIndexAPI iapi=new ESIndexAPI();
             Map<String,String> aliasMap=iapi.getAliasToIndexMap(APILocator.getSiteSearchAPI().listIndices());
-            
+
             if(counter==1){
 	            if(indexAlias.equals(APILocator.getIndiciesAPI().loadIndicies().site_search)){
 	            	Logger.info(this, "Index Alias is DEFAULT");
@@ -154,11 +158,11 @@ public class SiteSearchJobImpl {
             if(createNew && languageToIndex.length == counter){
             	config.setSwitchIndexWhenDone(true);
             }
-            
+
             config.setIndexName(indexName);
-    
-            
-            
+
+
+
             // if it is going to be an incremental job, write the bundle to the same folder
             // every time.  Otherwise, create a new folder using a date stamp.
             if(dataMap.get("incremental")!=null){
@@ -168,7 +172,7 @@ public class SiteSearchJobImpl {
                 String x = UtilMethods.dateToJDBC(new Date()).replace(':', '-').replace(' ', '_');
                 config.setId(x);
             }
-            
+
             config.setStartDate(startDate);
             config.setEndDate(endDate);
             config.setIncremental(incremental);
@@ -179,11 +183,11 @@ public class SiteSearchJobImpl {
             else {
                 config.setExcludePatterns(paths);
             }
-            
+
             APILocator.getPublisherAPI().publish(config,status);
-            
+
         }
-        
+
         int filesCount=0,pagesCount=0,urlmapCount=0;
         for(BundlerStatus bs : status.getBundlerStatuses()) {
             if(bs.getBundlerClass().equals(StaticHTMLPageBundler.class.getName()))
@@ -193,8 +197,8 @@ public class SiteSearchJobImpl {
             else if(bs.getBundlerClass().equals(URLMapBundler.class.getName()))
                 urlmapCount+=bs.getTotal();
         }
-        
-        
+
+
         try {
             SiteSearchAudit audit=new SiteSearchAudit();
             audit.setPagesCount(pagesCount);
@@ -220,6 +224,10 @@ public class SiteSearchJobImpl {
         finally {
             HibernateUtil.closeSession();
         }
+
+        String date = DateUtil.getCurrentDate();
+        ActivityLogger.logInfo(getClass(), "Job Finished", "User: " +APILocator.getUserAPI().getSystemUser().getUserId()+ "; Date: " + date + "; Job Identifier: " + SiteSearchAPI.ES_SITE_SEARCH_NAME  );
+        AdminLogger.log(getClass(), "Job Finished", "User: " +APILocator.getUserAPI().getSystemUser().getUserId()+ "; Date: " + date + "; Job Identifier: " + SiteSearchAPI.ES_SITE_SEARCH_NAME );
     }
 
 }
