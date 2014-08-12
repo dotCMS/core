0b956deff4e32d6652dcb78bc42af1dda880839b
Oscar Arrieta <oscar.arrieta@dotcms.com>
2014-08-11 21:17:19 -0600

    #6084: updating testcases
    Former-commit-id: a437b1974db44ab02756889d0b6bf0f602f2638d

---
+++ test/com/dotcms/LicenseTestUtil.java
@@ -0,0 +1,52 @@
+package com.dotcms;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.dotcms.repackage.org.apache.commons.httpclient.NameValuePair;
+import com.dotcms.repackage.org.apache.commons.httpclient.methods.PostMethod;
+import com.dotcms.repackage.org.apache.commons.httpclient.HttpClient;
+import com.dotcms.repackage.org.mockito.stubbing.Answer;
+import com.dotcms.repackage.org.mockito.Mockito;
+import com.dotcms.enterprise.LicenseUtil;
+
+/*
+ * Util class created to emulate the get license form request
+ * should only be used inside the test suite. 
+ */
+public class LicenseTestUtil {
+	
+	public static String getLicense() throws Exception{
+		String license = "";
+		
+		HttpServletRequest req=Mockito.mock(HttpServletRequest.class);
+		Mockito.when(req.getParameter("iwantTo")).thenReturn("request_code");
+		Mockito.when(req.getParameter("license_type")).thenReturn("trial");
+		Mockito.when(req.getParameter("license_level")).thenReturn("400");
+
+		final StringBuilder reqcode=new StringBuilder();
+
+		Mockito.doAnswer(new Answer() {
+		    public Object answer(com.dotcms.repackage.org.mockito.invocation.InvocationOnMock invocation) throws Throwable {
+		        reqcode.append(invocation.getArguments()[1].toString());
+		        return null;
+		    }
+		}).when(req).setAttribute(Mockito.eq("requestCode"),Mockito.any(String.class));
+
+		LicenseUtil.processForm(req);
+		
+		HttpClient client=new HttpClient();
+		PostMethod post=new PostMethod("http://support.dotcms.com/app/licenseRequest3");
+		post.setRequestBody(new NameValuePair[] { new NameValuePair("code", reqcode.toString()) });
+		client.executeMethod(post);
+		
+		if(post.getStatusCode()==200){
+			license = post.getResponseBodyAsString();
+			HttpServletRequest req2=Mockito.mock(HttpServletRequest.class);
+			Mockito.when(req2.getParameter("iwantTo")).thenReturn("paste_license");
+			Mockito.when(req2.getParameter("license_text")).thenReturn(license);
+			LicenseUtil.processForm(req2);
+		}
+		
+		return license;
+	}
+}
