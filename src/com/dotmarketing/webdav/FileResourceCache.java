6281065421cf7dc84dec818eeb1dabcd1f42bbe8
Daniel Silva <daniel.silva@dotcms.com>
2014-09-03 18:37:11 -0430

    #6228

--- src/com/dotmarketing/webdav/FileResourceCache.java
+++ src/com/dotmarketing/webdav/FileResourceCache.java
@@ -1,17 +1,23 @@
 package com.dotmarketing.webdav;
 
+import java.util.Date;
+import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutionException;
 
 import com.dotcms.repackage.com.google.common.cache.Cache;
 import com.dotcms.repackage.com.google.common.cache.CacheBuilder;
+import com.dotmarketing.business.Cachable;
 import com.dotmarketing.util.Config;
 import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.UtilMethods;
 
-public class ResourceCache {
+public class FileResourceCache  {
 
 	private Cache<String, Long> cache;
 
-	public ResourceCache() {
+	public FileResourceCache() {
 		CacheBuilder<Object, Object> cb  = CacheBuilder
 				.newBuilder()
 				.maximumSize(100)
@@ -50,6 +56,27 @@ public class ResourceCache {
 //        cache.
     }
 
+    public void clearExpiredEntries() {
+    	ConcurrentMap<String, Long> map = cache.asMap();
+    	Set<String> keys = map.keySet();
+    	Date currentDate = new Date();
+    	long minTimeAllowed = Config.getIntProperty("WEBDAV_MIN_TIME_AFTER_PUBLISH_TO_ALLOW_DELETING_OF_FILES", 5);
+
+
+    	for (String key : keys) {
+			try {
+				Long time = cache.get(key);
+
+				if(UtilMethods.isSet(time) && ((currentDate.getTime()-time)/1000>=minTimeAllowed)) {
+					cache.invalidate(key);
+				}
+			} catch (Exception e) {
+				Logger.error(getClass(), "Error getting FileResourceCache entry. Key: " + key, e);
+			}
+
+		}
+    }
+
     /* (non-Javadoc)
 	 * @see com.dotmarketing.business.PermissionCache#remove(java.lang.String)
 	 */
@@ -66,8 +93,6 @@ public class ResourceCache {
 		public Object call() throws Exception {
 			return null;
 		}
-
-
 	}
 
 }
