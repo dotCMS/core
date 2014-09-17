01b9d0c8d4bf182227eec4cea98c41f8daca619f
Jonathan Gamba <jonathan.gamba@dotcms.com>
2014-09-17 12:51:52 -0600

    #6353

--- dotCMS/html/js/dotcms/dojo/data/RoleReadStore.js
+++ dotCMS/html/js/dotcms/dojo/data/RoleReadStore.js
@@ -17,26 +17,48 @@ dojo.declare("dotcms.dojo.data.RoleReadStore", dojox.data.QueryReadStore, {
 	},
 
 
-    fetch:function(request) {
-    	var searchName = dijit.byId(this.nodeId).get("displayedValue");
+    fetch: function (request) {
+
+        var searchName = dijit.byId(this.nodeId).get("displayedValue");
         var query = request.query;
 
         if (query) {
+            if (query.name && (!searchName || searchName == "") && (query.name != "*")) {
+                searchName = query.name;
+            }
+
             if (query.name) {
                 query = query.name;
             } else if (query.toString() == "[object Object]") {
-                query = "";
+                query = "*";
             }
         }
 
+        /*
+         We can exclude the "(" ")" as the rol is not really use it in order to filter the store,
+         actually write in the select box to the point of write part of the role will return no results,
+         just the name is what it actually expects for filtering.
+         */
+        if (searchName) {
+            searchName = searchName.replace("(", "").replace(")", "");//We are not using the role for filter and we are generating XSS errors sending "(" or ")"
+        }
+
+        if (query) {
+            /*
+             We can exclude unneeded characters here as we don' really use this "query" inside our ajax class,
+             is used only by the store component and the role is not really use on the server filter search.
+             */
+            query = query.replace("(", "").replace(")", "");
+        }
+
         request.serverQuery = {
-        	"getRoles":request.cmd,
-        	"q":query,
-        	"searchName":searchName, 
-        	"roleId":request.identity,
-        	"start":request.start,
-        	"count":request.count,
-        	"includeFake":this.includeFake
+            "getRoles": request.cmd,
+            "q": query,
+            "searchName": searchName,
+            "roleId": request.identity,
+            "start": request.start,
+            "count": request.count,
+            "includeFake": this.includeFake
         };
         return this.inherited("fetch", arguments);
     },
