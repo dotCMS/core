ecd0c3807859aba4835ea704854313b975e53493
Oswaldo Gallango <oswaldo@dotcms.com>
2014-10-31 14:34:03 -0600

    fixes #6634 spellcheck and suggestions display

---
+++ src/org/tinymce/spellchecker/SpellcheckMethod.java
@@ -0,0 +1,12 @@
+package org.tinymce.spellchecker;
+
+/**
+* SpellcheckMethod.java
+* Created on 5/13/13 by Andrey Chorniy
+* Copyright 2013, itMD, LLC. All Rights Reserved.
+*/
+enum SpellcheckMethod {
+    checkWords,
+    getSuggestions,
+    spellcheck //this method is passed in TinyMCE 4.0
+}
