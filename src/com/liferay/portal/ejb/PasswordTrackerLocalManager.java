75f0e01aa6cbb96b67e14694cf24f7c789e87001
Jose Castro <josecastro@joses-mbp.dotcms>
2014-12-02 09:55:33 -0600

    #6615 : Revert changes to simplify code review.

--- src/com/liferay/portal/ejb/PasswordTrackerLocalManager.java
+++ src/com/liferay/portal/ejb/PasswordTrackerLocalManager.java
@@ -22,86 +22,23 @@
 
 package com.liferay.portal.ejb;
 
-import java.util.List;
-
-import com.liferay.portal.PortalException;
-import com.liferay.portal.SystemException;
-
 /**
- * Provides utility routines to deal with the password security policies
- * specified for the server, which includes:
- * <ul>
- * <li>Character and length validation.</li>
- * <li>Recycling validation.</li>
- * </ul>
+ * <a href="PasswordTrackerLocalManager.java.html"><b><i>View Source</i></b></a>
  *
- * @author Brian Wing Shun Chan
- * @version $Revision: 1.6 $
+ * @author  Brian Wing Shun Chan
+ * @version $Revision: 1.5 $
  *
  */
 public interface PasswordTrackerLocalManager {
-
-	/**
-	 * Deletes the complete password history for a specific user.
-	 * 
-	 * @param userId
-	 *            - The ID of the user whose records will be deleted.
-	 * @throws com.liferay.portal.SystemException
-	 */
 	public void deleteAll(java.lang.String userId)
-			throws com.liferay.portal.SystemException;
+		throws com.liferay.portal.SystemException;
 
-	/**
-	 * Validates the password of a given user according to the portal security
-	 * settings. If validation errors are present, they can be retrieved through
-	 * the {@link #getValidationErrors()} method.
-	 * 
-	 * @param userId
-	 *            - The ID of the user whose password is being validated.
-	 * @param password
-	 *            - The human-readable password.
-	 * @return If the password contains valid characters, the correct length
-	 *         and, if applicable, it is not recycled, returns <code>true</code>
-	 *         .
-	 * @throws PortalException
-	 * @throws SystemException
-	 */
 	public boolean isValidPassword(java.lang.String userId,
-			java.lang.String password)
-			throws com.liferay.portal.PortalException,
+		java.lang.String password)
+		throws com.liferay.portal.PortalException, 
 			com.liferay.portal.SystemException;
 
-	/**
-	 * Keeps track of a valid password that has been set for a given user. This
-	 * allows the password recycle process to correctly validate password (if
-	 * activated).
-	 * 
-	 * @param userId
-	 *            - The ID of the user whose password is being tracked.
-	 * @param encPwd
-	 *            - The encrypted user password.
-	 * @throws PortalException
-	 * @throws SystemException
-	 */
 	public void trackPassword(java.lang.String userId, java.lang.String encPwd)
-			throws com.liferay.portal.PortalException,
+		throws com.liferay.portal.PortalException, 
 			com.liferay.portal.SystemException;
-
-	/**
-	 * Indicates whether the password recycling policy is active or not.
-	 * 
-	 * @return If the password recycling policy is enabled, returns
-	 *         <code>true</code>.
-	 */
-	public boolean isPasswordRecyclingActive();
-
-	/**
-	 * Returns a list containing the security checks that failed during the
-	 * password validation process.
-	 * 
-	 * @return A simple {@link List} of errors. An empty list means the
-	 *         validation has been successful.
-	 */
-	public List<Object> getValidationErrors();
-
 }
