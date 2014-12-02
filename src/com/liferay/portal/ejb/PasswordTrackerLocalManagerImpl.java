a0841eb96d880703eb6267b05f9b7a54e9b4413b
Jose Castro <josecastro@joses-mbp.dotcms>
2014-12-02 09:55:33 -0600

    #6615 : Revert changes to simplify code review.

--- src/com/liferay/portal/ejb/PasswordTrackerLocalManagerImpl.java
+++ src/com/liferay/portal/ejb/PasswordTrackerLocalManagerImpl.java
@@ -22,16 +22,14 @@
 
 package com.liferay.portal.ejb;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
-import java.util.List;
 
 import com.dotcms.repackage.com.liferay.counter.ejb.CounterManagerUtil;
 import com.liferay.portal.PortalException;
 import com.liferay.portal.SystemException;
 import com.liferay.portal.model.PasswordTracker;
-import com.liferay.portal.pwd.RegExpToolkit;
+import com.liferay.portal.model.User;
 import com.liferay.portal.util.PropsUtil;
 import com.liferay.util.Encryptor;
 import com.liferay.util.GetterUtil;
@@ -41,81 +39,75 @@ import com.liferay.util.Time;
  * <a href="PasswordTrackerLocalManagerImpl.java.html"><b><i>View Source</i></b>
  * </a>
  *
- * @author Brian Wing Shun Chan
- * @version $Revision: 1.3 $
+ * @author  Brian Wing Shun Chan
+ * @version $Revision: 1.2 $
  *
  */
-public class PasswordTrackerLocalManagerImpl implements
-		PasswordTrackerLocalManager {
+public class PasswordTrackerLocalManagerImpl
+	implements PasswordTrackerLocalManager {
 
-	private List<Object> validationErrorsList = null;
+	// Business methods
 
-	@Override
 	public void deleteAll(String userId) throws SystemException {
 		PasswordTrackerUtil.removeByUserId(userId);
 	}
 
-	@Override
 	public boolean isValidPassword(String userId, String password)
-			throws PortalException, SystemException {
-		RegExpToolkit regExpToolkit = new RegExpToolkit();
-		this.validationErrorsList = new ArrayList<Object>();
-		boolean successful = true;
-		// Validate character rules
-		if (!regExpToolkit.validate(password)) {
-			this.validationErrorsList
-					.add("The password does not meet the portal security policies.");
-			successful = false;
-		}
-		// Validate recycling
-		if (isPasswordRecyclingActive()) {
+		throws PortalException, SystemException {
+
+		int passwordsRecycle = GetterUtil.getInteger(
+			PropsUtil.get(PropsUtil.PASSWORDS_RECYCLE));
+
+		if (passwordsRecycle > 0) {
 			String newEncPwd = Encryptor.digest(password);
+
+			User user = UserUtil.findByPrimaryKey(userId);
+
+			String oldEncPwd = user.getPassword();
+			if (!user.isPasswordEncrypted()) {
+				oldEncPwd = Encryptor.digest(user.getPassword());
+			}
+
+			if (oldEncPwd.equals(newEncPwd)) {
+				return false;
+			}
+
 			Date now = new Date();
-			int passwordsRecycle = GetterUtil.getInteger(PropsUtil
-					.get(PropsUtil.PASSWORDS_RECYCLE));
+
 			Iterator itr = PasswordTrackerUtil.findByUserId(userId).iterator();
+
 			while (itr.hasNext()) {
-				PasswordTracker passwordTracker = (PasswordTracker) itr.next();
-				Date recycleDate = new Date(passwordTracker.getCreateDate()
-						.getTime() + Time.DAY * passwordsRecycle);
+				PasswordTracker passwordTracker = (PasswordTracker)itr.next();
+
+				Date recycleDate = new Date(
+					passwordTracker.getCreateDate().getTime() +
+					Time.DAY * passwordsRecycle);
+
 				if (recycleDate.after(now)) {
 					if (passwordTracker.getPassword().equals(newEncPwd)) {
-						this.validationErrorsList
-								.add("The password has been used lately and cannot be recycled.");
-						successful = false;
+						return false;
 					}
 				}
 			}
 		}
-		return successful;
+
+		return true;
 	}
 
-	@Override
 	public void trackPassword(String userId, String encPwd)
-			throws PortalException, SystemException {
-		String passwordTrackerId = Long.toString(CounterManagerUtil
-				.increment(PasswordTracker.class.getName()));
-		PasswordTracker passwordTracker = PasswordTrackerUtil
-				.create(passwordTrackerId);
+		throws PortalException, SystemException {
+
+		String passwordTrackerId = Long.toString(CounterManagerUtil.increment(
+			PasswordTracker.class.getName()));
+
+		PasswordTracker passwordTracker =
+			PasswordTrackerUtil.create(passwordTrackerId);
+
 		passwordTracker.setUserId(userId);
 		passwordTracker.setCreateDate(new Date());
 		passwordTracker.setPassword(encPwd);
+
 		PasswordTrackerUtil.update(passwordTracker);
-	}
-
-	@Override
-	public boolean isPasswordRecyclingActive() {
-		int passwordsRecycle = GetterUtil.getInteger(PropsUtil
-				.get(PropsUtil.PASSWORDS_RECYCLE));
-		if (passwordsRecycle > 0) {
-			return true;
-		}
-		return false;
-	}
-
-	@Override
-	public List<Object> getValidationErrors() {
-		return this.validationErrorsList;
 	}
 
 }
