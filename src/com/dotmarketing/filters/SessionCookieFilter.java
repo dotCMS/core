d3a7f04c9d957dfc55d2b0670768fad61fa6a6b5
Daniel Silva <daniel.silva@dotcms.com>
2014-12-11 13:32:37 -0430

    #6823

--- src/com/dotmarketing/filters/SessionCookieFilter.java
+++ src/com/dotmarketing/filters/SessionCookieFilter.java
@@ -17,6 +17,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -27,23 +28,31 @@ public class SessionCookieFilter implements Filter {
     public void destroy() {
 
     }
+    
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
             ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
 
-		String sessionid = request.getSession().getId();
-
 		String cookiesSecureFlag = Config.getStringProperty("COOKIES_SECURE_FLAG", "https");
 
 		String cookiesHttpOnly = Config.getBooleanProperty("COOKIES_HTTP_ONLY", true)?"; HttpOnly;":"";
-
-		if(cookiesSecureFlag.equals("always")) {
-			response.setHeader("SET-COOKIE", "JSESSIONID=" + sessionid + "; secure" + cookiesHttpOnly);
-		} else if(cookiesSecureFlag.equals("https") && req.isSecure()) {
-			response.setHeader("SET-COOKIE", "JSESSIONID=" + sessionid + "; secure" + cookiesHttpOnly);
+		
+		Cookie[] cookies = request.getCookies();
+		
+		if(cookies!=null) {
+		
+			for(Cookie cookie : cookies){
+					if(cookiesSecureFlag.equals("always") || (cookiesSecureFlag.equals("https") && req.isSecure())) {
+						response.setHeader("SET-COOKIE", cookie.getName() + "=" + cookie.getValue() + "; Path=/; secure" + cookiesHttpOnly);
+					} else { 
+						response.setHeader("SET-COOKIE", cookie.getName() + "=" + cookie.getValue() + "; Path=/; " + cookiesHttpOnly);
+					}
+				}
+		
 		}
+		
 
         chain.doFilter(req, response);
     }
