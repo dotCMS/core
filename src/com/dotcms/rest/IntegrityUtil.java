d2605d5c7978670dd7fe78e07748a43389552417
Oscar Arrieta <oscar.arrieta@dotcms.com>
2014-12-17 11:43:11 -0600

    #6841: Updating Multi Tree

--- src/com/dotcms/rest/IntegrityUtil.java
+++ src/com/dotcms/rest/IntegrityUtil.java
@@ -1548,6 +1548,13 @@ public class IntegrityUtil {
 		    dc.addParam(assetName);
 		    dc.addParam(newHtmlPageIdentifier);
 		    dc.loadResult();
+
+            dc.setSQL("UPDATE multi_tree "
+                    + "SET parent1 = ? "
+                    + "WHERE parent1 = ?");
+            dc.addParam(newHtmlPageIdentifier);
+            dc.addParam(oldHtmlPageIdentifier);
+            dc.loadResult();
 		}
 
 		discardConflicts(serverId, IntegrityType.HTMLPAGES);
