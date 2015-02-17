e4d2ef7926bf6c34beb47a406e02bce53efa4ef8
mrizo <mauricio.rizo@dotcms.com>
2015-02-17 09:34:13 -0600

    Fixes #7202 Identifier Test

--- test/com/dotmarketing/business/IdentifierAPITest.java
+++ test/com/dotmarketing/business/IdentifierAPITest.java
@@ -27,18 +27,18 @@ public class IdentifierAPITest {
         // if we find a fake id it should end up in 404 cache
         api.find(fakeId);
         Assert.assertEquals(id404, cache.getIdentifier(fakeId).getAssetType());
-        api.find(syshost, "/"+fakeId+".content");
-        Assert.assertEquals(id404, cache.getIdentifier(syshost.getIdentifier(),"/"+fakeId+".content").getAssetType());
+        api.find(syshost, "/content."+fakeCont.getInode());
+        Assert.assertEquals(id404, cache.getIdentifier(syshost.getIdentifier(),"/content."+fakeCont.getInode()).getAssetType());
         
         // now if we create an asset with that ID it should be cleared
         api.createNew(fakeCont, syshost, fakeId);
         Assert.assertNull(cache.getIdentifier(fakeId));
-        Assert.assertNull(cache.getIdentifier(syshost.getIdentifier(), "/"+fakeId+".content"));
+        Assert.assertNull(cache.getIdentifier(syshost.getIdentifier(), "/content."+fakeCont.getInode()));
         
         // this should load the identifier in both cache entries (by url and by id)
         api.find(fakeId);
         Assert.assertEquals(fakeId, cache.getIdentifier(fakeId).getId());
-        Assert.assertEquals(fakeId, cache.getIdentifier(syshost.getIdentifier(), "/"+fakeId+".content").getId());
+        Assert.assertEquals(fakeId, cache.getIdentifier(syshost.getIdentifier(), "/content."+fakeCont.getInode()).getId());
         
         
     }
