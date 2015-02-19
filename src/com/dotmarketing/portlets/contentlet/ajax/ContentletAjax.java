e10196e4385520ea15f0678c2fd188e9b8dc3596
mrizo <mauricio.rizo@dotcms.com>
2015-02-19 08:41:36 -0600

    Fixes #7297

--- src/com/dotmarketing/portlets/contentlet/ajax/ContentletAjax.java
+++ src/com/dotmarketing/portlets/contentlet/ajax/ContentletAjax.java
@@ -998,7 +998,7 @@ public class ContentletAjax {
 			searchResult.put("locked", locked.toString());
 			searchResult.put("structureInode", con.getStructureInode());
 			searchResult.put("workflowMandatory", String.valueOf(APILocator.getWorkflowAPI().findSchemeForStruct(con.getStructure()).isMandatory()));
-			searchResult.put("contentStructureType", "x"+con.getStructure().getStructureType());
+			searchResult.put("contentStructureType", ""+con.getStructure().getStructureType());
 
 			// Workflow Actions
 
