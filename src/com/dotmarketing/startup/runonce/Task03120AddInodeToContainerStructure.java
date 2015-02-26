1f605da492b780e85238c05eca3243627f2d1d70
Oscar Arrieta <oscar.arrieta@dotcms.com>
2015-02-26 15:58:00 -0600

    #7251: Fix postgresql

--- src/com/dotmarketing/startup/runonce/Task03120AddInodeToContainerStructure.java
+++ src/com/dotmarketing/startup/runonce/Task03120AddInodeToContainerStructure.java
@@ -1,6 +1,7 @@
 package com.dotmarketing.startup.runonce;
 
 import com.dotmarketing.common.db.DotConnect;
+import com.dotmarketing.db.DbConnectionFactory;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotRuntimeException;
 import com.dotmarketing.startup.StartupTask;
@@ -21,17 +22,18 @@ import java.util.UUID;
  */
 public class Task03120AddInodeToContainerStructure implements StartupTask {
 
-    private static final String MYSQL_ADD_INODE_COLUMN = "ALTER TABLE container_structures ADD container_inode varchar(36)";
-    private static final String MYSQL_ADD_FK_INODE = "ALTER TABLE container_structures ADD CONSTRAINT FK_cs_inode FOREIGN KEY (container_inode) references inode(inode)";
-    private static final String MYSQL_ADD_NOT_NULL = "ALTER TABLE container_structures MODIFY container_inode varchar(36) not null";
-    private static final String MYSQL_INSERT_INTO_CONTAINER_STRUCTURE = "INSERT INTO container_structures(id, container_id, structure_id, code, container_inode) VALUES(?, ?, ?, ?, ?)";
-    private static final String MYSQL_DELETE_FROM_CONTAINER = "DELETE FROM containers WHERE inode = ?";
-    private static final String MYSQL_UPDATE_CONTAINER_STRUCTURE_BY_IDENTIFIER = "UPDATE container_structures SET container_inode = ? WHERE container_id = ?";
-    private static final String MYSQL_UPDATE_CONTAINER_STRUCTURE_BY_ID = "UPDATE container_structures SET container_inode = ? WHERE id = ?";
-    private static final String MYSQL_GET_CONTAINER_VERSION = "SELECT identifier FROM container_version_info WHERE working_inode = ? AND live_inode = ?";
-    private static final String MYSQL_GET_CONTAINER_STRUCTURE = "SELECT id, container_id, structure_id, code, container_inode FROM container_structures WHERE container_id = ?";
+    private static final String SQL_ADD_INODE_COLUMN = "ALTER TABLE container_structures ADD container_inode varchar(36)";
+    private static final String SQL_ADD_FK_INODE = "ALTER TABLE container_structures ADD CONSTRAINT FK_cs_inode FOREIGN KEY (container_inode) references inode(inode)";
+    private static final String SQL_ADD_NOT_NULL = "ALTER TABLE container_structures MODIFY container_inode varchar(36) not null";
+    private static final String POSTGRES_ADD_NOT_NULL = "ALTER TABLE container_structures ALTER COLUMN container_inode TYPE character varchar(36), ALTER COLUMN container_inode SET NOT NULL";
+    private static final String SQL_INSERT_INTO_CONTAINER_STRUCTURE = "INSERT INTO container_structures(id, container_id, structure_id, code, container_inode) VALUES(?, ?, ?, ?, ?)";
+    private static final String SQL_DELETE_FROM_CONTAINER = "DELETE FROM containers WHERE inode = ?";
+    private static final String SQL_UPDATE_CONTAINER_STRUCTURE_BY_IDENTIFIER = "UPDATE container_structures SET container_inode = ? WHERE container_id = ?";
+    private static final String SQL_UPDATE_CONTAINER_STRUCTURE_BY_ID = "UPDATE container_structures SET container_inode = ? WHERE id = ?";
+    private static final String SQL_GET_CONTAINER_VERSION = "SELECT identifier FROM container_version_info WHERE working_inode = ? AND live_inode = ?";
+    private static final String SQL_GET_CONTAINER_STRUCTURE = "SELECT id, container_id, structure_id, code, container_inode FROM container_structures WHERE container_id = ?";
 
-    private final String MYSQL_GET_NON_WORKING_LIVE_INODES = "SELECT containers.inode " +
+    private final String SQL_GET_NON_WORKING_LIVE_INODES = "SELECT containers.inode " +
             "FROM containers " +
             "WHERE containers.inode " +
             "NOT IN(SELECT containers.inode " +
@@ -40,7 +42,7 @@ public class Task03120AddInodeToContainerStructure implements StartupTask {
                 "AND (containers.inode = container_version_info.working_inode " +
                     "OR containers.inode = container_version_info.live_inode))";
 
-    private static final String MYSQL_GET_CONTAINER_IDENT_INODE = "SELECT containers.identifier, containers.inode " +
+    private static final String SQL_GET_CONTAINER_IDENT_INODE = "SELECT containers.identifier, containers.inode " +
             "FROM containers, container_version_info " +
             "WHERE containers.identifier = container_version_info.identifier " +
             "AND (containers.inode = container_version_info.working_inode " +
@@ -67,12 +69,12 @@ public class Task03120AddInodeToContainerStructure implements StartupTask {
 
         DotConnect dc = new DotConnect();
         //Add Column inode to container_structures.
-        dc.setSQL(MYSQL_ADD_INODE_COLUMN);
+        dc.setSQL(SQL_ADD_INODE_COLUMN);
         dc.loadResults();
 
         //Gets list of inodes that are NOT live nor working to DELETE from container_structures.
         dc = new DotConnect();
-        dc.setSQL(MYSQL_GET_NON_WORKING_LIVE_INODES);
+        dc.setSQL(SQL_GET_NON_WORKING_LIVE_INODES);
         ArrayList<Map<String, String>> inodesToDelete = dc.loadResults();
 
         if (inodesToDelete != null && !inodesToDelete.isEmpty()) {
@@ -80,7 +82,7 @@ public class Task03120AddInodeToContainerStructure implements StartupTask {
                 dc = new DotConnect();
                 String inode = inodeToDelete.get("inode");
 
-                dc.setSQL(MYSQL_DELETE_FROM_CONTAINER);
+                dc.setSQL(SQL_DELETE_FROM_CONTAINER);
                 dc.addParam(inode);
                 dc.loadResults();
             }
@@ -88,7 +90,7 @@ public class Task03120AddInodeToContainerStructure implements StartupTask {
 
         //Get list of identifier-inode to update container_structures.
         dc = new DotConnect();
-        dc.setSQL(MYSQL_GET_CONTAINER_IDENT_INODE);
+        dc.setSQL(SQL_GET_CONTAINER_IDENT_INODE);
         ArrayList<Map<String, String>> containerIdentInodes = dc.loadResults();
 
         if (containerIdentInodes != null && !containerIdentInodes.isEmpty()) {
@@ -97,7 +99,7 @@ public class Task03120AddInodeToContainerStructure implements StartupTask {
                 String inode = containerIdentInode.get("inode");
 
                 dc = new DotConnect();
-                dc.setSQL(MYSQL_GET_CONTAINER_VERSION);
+                dc.setSQL(SQL_GET_CONTAINER_VERSION);
                 dc.addParam(inode);
                 dc.addParam(inode);
                 ArrayList<Map<String, String>> containerVersion = dc.loadResults();
@@ -105,7 +107,7 @@ public class Task03120AddInodeToContainerStructure implements StartupTask {
                 //If inode is both Working and Live, then update container_structures to add inode.
                 if (containerVersion != null && !containerVersion.isEmpty()) {
                     dc = new DotConnect();
-                    dc.setSQL(MYSQL_UPDATE_CONTAINER_STRUCTURE_BY_IDENTIFIER);
+                    dc.setSQL(SQL_UPDATE_CONTAINER_STRUCTURE_BY_IDENTIFIER);
                     dc.addParam(inode);
                     dc.addParam(identifier);
                     dc.loadResult();
@@ -113,7 +115,7 @@ public class Task03120AddInodeToContainerStructure implements StartupTask {
                 } else {
                     //Else if inode is not same Working and Live, we need to duplicate row and add one inode for each.
                     dc = new DotConnect();
-                    dc.setSQL(MYSQL_GET_CONTAINER_STRUCTURE);
+                    dc.setSQL(SQL_GET_CONTAINER_STRUCTURE);
                     dc.addParam(identifier);
                     ArrayList<Map<String, String>> containerStructures = dc.loadResults();
 
@@ -131,7 +133,7 @@ public class Task03120AddInodeToContainerStructure implements StartupTask {
                                 id = UUID.randomUUID().toString();
 
                                 dc = new DotConnect();
-                                dc.setSQL(MYSQL_INSERT_INTO_CONTAINER_STRUCTURE);
+                                dc.setSQL(SQL_INSERT_INTO_CONTAINER_STRUCTURE);
                                 dc.addParam(id);
                                 dc.addParam(containerId);
                                 dc.addParam(structureId);
@@ -142,7 +144,7 @@ public class Task03120AddInodeToContainerStructure implements StartupTask {
                             } else {
                                 //If the row doesn't have the inode we just need to update the row to set the value.
                                 dc = new DotConnect();
-                                dc.setSQL(MYSQL_UPDATE_CONTAINER_STRUCTURE_BY_ID);
+                                dc.setSQL(SQL_UPDATE_CONTAINER_STRUCTURE_BY_ID);
                                 dc.addParam(inode);
                                 dc.addParam(id);
                                 dc.loadResult();
@@ -155,12 +157,15 @@ public class Task03120AddInodeToContainerStructure implements StartupTask {
 
         //Add FK between inode - container_structures.
         dc = new DotConnect();
-        dc.setSQL(MYSQL_ADD_FK_INODE);
+        dc.setSQL(SQL_ADD_FK_INODE);
         dc.loadResults();
 
         //Add contraint not null to inode column.
         dc = new DotConnect();
-        dc.setSQL(MYSQL_ADD_NOT_NULL);
+        dc.setSQL(SQL_ADD_NOT_NULL);
+        if (DbConnectionFactory.isPostgres()) {
+            dc.setSQL(POSTGRES_ADD_NOT_NULL);
+        }
         dc.loadResults();
     }
 }
