a5fefcba1678eebfd1421d2fb9c9145d2cf506f4
Jonathan Gamba <jonathan.gamba@dotcms.com>
2015-03-16 14:50:43 -0600

    OSGI small fixes

--- docs/examples/osgi/com.dotcms.spring/src/main/java/com/dotmarketing/osgi/spring/Activator.java
+++ docs/examples/osgi/com.dotcms.spring/src/main/java/com/dotmarketing/osgi/spring/Activator.java
@@ -13,9 +13,6 @@ import org.springframework.web.servlet.DispatcherServlet;
  */
 public class Activator extends GenericBundleActivator {
 
-    private DispatcherServlet dispatcherServlet;
-    private ExtHttpService httpService;
-
     @SuppressWarnings ("unchecked")
     public void start ( BundleContext context ) throws Exception {
 
@@ -29,9 +26,9 @@ public class Activator extends GenericBundleActivator {
             //Publish bundle services
             publishBundleServices( context );
 
-            httpService = (ExtHttpService) context.getService( sRef );
+            ExtHttpService httpService = (ExtHttpService) context.getService( sRef );
             try {
-                dispatcherServlet = new DispatcherServlet();
+                DispatcherServlet dispatcherServlet = new DispatcherServlet();
                 dispatcherServlet.setContextConfigLocation( "spring/example-servlet.xml" );
                 httpService.registerServlet( "/spring", dispatcherServlet, null, null );
             } catch ( Exception e ) {
@@ -43,15 +40,10 @@ public class Activator extends GenericBundleActivator {
 
     public void stop ( BundleContext context ) throws Exception {
 
-        //Unregister the servlet
-        if ( httpService != null && dispatcherServlet != null ) {
-            httpService.unregisterServlet( dispatcherServlet );
-        }
-
         CMSFilter.removeExclude( "/app/spring" );
 
         //Unpublish bundle services
-        unpublishBundleServices();
+        unregisterServices( context );
     }
 
 }
