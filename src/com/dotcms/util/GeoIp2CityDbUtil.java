e814b34837a86db43af94436859e8d0b70ba3a81
Jose Castro <josecastro@joses-mbp.dotcms>
2015-04-17 14:25:40 -0600

    #7391 : Adding VisitorsCityConditionlet and VisitorsLanguageConditionlet. Adding other small modifications.

--- src/com/dotcms/util/GeoIp2CityDbUtil.java
+++ src/com/dotcms/util/GeoIp2CityDbUtil.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.dotcms.repackage.com.maxmind.geoip2.DatabaseReader;
 import com.dotcms.repackage.com.maxmind.geoip2.exception.GeoIp2Exception;
 import com.dotcms.repackage.com.maxmind.geoip2.model.CityResponse;
 import com.dotcms.repackage.com.maxmind.geoip2.record.Subdivision;
+import com.dotcms.repackage.org.apache.commons.lang.StringUtils;
 import com.dotmarketing.exception.DotRuntimeException;
 import com.dotmarketing.util.Config;
 import com.dotmarketing.util.Logger;
@@ -18,6 +21,13 @@ import com.dotmarketing.util.Logger;
  * address, which is ideal for displaying content to Website users based on
  * different criteria, such as, city, state/province, country, language, postal
  * code, etc.
+ * <p>
+ * By default, Tomcat returns IPv6 addresses via the {@link HttpServletRequest}
+ * object. This can be changed by adding the following parameter to the startup
+ * command line of your Tomcat server:
+ * <p>
+ * {@code -Djava.net.preferIPv4Stack=true}
+ * </p>
  * <p>
  * The GeoIP2 API is compatible with both IPv4 and IPv6 addresses. This
  * implementation uses a free of charge local <a
@@ -84,6 +94,57 @@ public class GeoIp2CityDbUtil {
 	 */
 	public static GeoIp2CityDbUtil getInstance() {
 		return SingletonHolder.INSTANCE;
+	}
+
+	/**
+	 * Retrieves the client's IP address from the {@link HttpServletRequest}
+	 * object based on the different available approaches. It's worth noting
+	 * that, depending on the server startup parameters, the resulting IP
+	 * address can be either IPv4 or IPv6.
+	 * 
+	 * @param request
+	 *            - The {@link HttpServletRequest} object.
+	 * @return The client's IP address.
+	 */
+	public String getClientIpAddress(HttpServletRequest request) {
+		String ip = request.getHeader("X-Forwarded-For");
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+		}
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_X_FORWARDED");
+		}
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_X_CLUSTER_CLIENT_IP");
+		}
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_CLIENT_IP");
+		}
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_FORWARDED_FOR");
+		}
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_FORWARDED");
+		}
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_VIA");
+		}
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("REMOTE_ADDR");
+		}
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("X-Real-IP");
+		}
+		if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		return ip;
 	}
 
 	/**
