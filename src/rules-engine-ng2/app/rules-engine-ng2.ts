1635e714baf751bdb31b0fc4005f56f021bb2665
ggranum <geoff.granum@gmail.com>
2015-06-10 10:06:12 -0700

    Holding pattern - Forms are broken in angualr2-alpha26, waiting on fixes before continuing down this path.

--- src/rules-engine-ng2/app/rules-engine-ng2.ts
+++ src/rules-engine-ng2/app/rules-engine-ng2.ts
@@ -5,8 +5,15 @@ import Reflect from 'reflect-metadata';
 
 import {bootstrap, NgFor, NgIf, EventEmitter, Component, View} from 'angular2/angular2';
 
-import {FormBuilder, Validators, formDirectives, ControlGroup, Control,
-    ControlArray} from 'angular2/forms';
+import {
+    FormBuilder,
+    Validators,
+    formDirectives,
+    ControlGroup,
+    Control,
+    ControlArray
+    } from 'angular2/forms';
+import {ObservableWrapper} from 'angular2/src/facade/async';
 
 import {Core} from 'src/dc/index.js'
 import * as RuleEngine from 'src/rules-engine/RuleEngine.js';
@@ -22,192 +29,192 @@ import ruleActionTemplate from './rule-action.tpl.html!text'
 import clauseGroupTemplate from './clause-group.tpl.html!text'
 import clauseTemplate from './clause.tpl.html!text'
 
-@Component({
-  selector: 'rule-action',
-  properties: {
-    "rule": "rule",
-    "ruleAction": "rule-action",
-    "index": "index"
-  }
-})
-@View({
-  template: ruleActionTemplate,
-  directives: [formDirectives, NgIf, NgFor],
-  appInjector: [FormBuilder]
-})
-class RuleActionComponent {
-  _ruleAction:any;
-  form:ControlGroup;
-  rule:any;
-  index:number;
-  builder:FormBuilder;
-
-  constructor(b:FormBuilder) {
-    this.builder = b;
-  }
-
-  get ruleAction():any {
-    return this._ruleAction;
-  }
-
-  set ruleAction(ruleAction:any) {
-    var ruleActionControl = this.builder.group({
-      "name": [ruleAction.name, Validators.required]
-    });
-    ruleActionControl.controls.name.valueChanges.toRx().debounce(500).subscribe(
-      (v) => {
-        this._ruleAction = this._ruleAction.clone().withName(v).build()
-        this.saveChanges()
-      })
-    this.form = ruleActionControl
-    this._ruleAction = ruleAction
-  }
-
-  addRuleAction() {
-    //RuleActionActionCreators.addRuleAction(RuleActionBuilder.fromJson({owningRule: this.rule.$key.value}).build(), this.rule)
-  }
-
-  saveChanges() {
-    //RuleActionActionCreators.updateRuleAction(this._ruleAction)
-  }
-
-  onChange(action) {
-  }
-
-  removeRuleAction() {
-    //RuleActionActionCreators.removeRuleAction(this._ruleAction.$key)
-  }
-}
-
-
-@Component({
-  selector: 'clause',
-  properties: {
-    "rule": "rule",
-    "group": "group",
-    "clause": "clause",
-    "index": "index"
-  }
-})
-@View({
-  template: clauseTemplate,
-  directives: [formDirectives, NgIf, NgFor]
-})
-class ClauseComponent {
-  _clause:any;
-  group:any;
-  rule:any;
-  index:number;
-  clauseTypes:Array;
-  comparisons:Array;
-
-  constructor() {
-    //ClauseStore.addChangeListener(this.onChange.bind(this))
-    this.clauseTypes = [{id: 'hello', text: 'foo'}, {id: 'goodbye', text: 'bar'}]
-    this.comparisons = [{id: 'compHello', text: 'cFoo'}, {id: 'compGoodbye', text: 'cBar'}];
-  }
-
-  get clause() {
-    return this._clause;
-  }
-
-  set clause(clause) {
-    this._clause = clause
-  }
-
-  onChange(action) {
-  }
-
-  addClause(rule, group) {
-    //let clause = ClauseBuilder.fromCfg({owningGroup: group.$key}).build()
-    //ClauseActionCreators.addClause(clause, rule.$key)
-  }
-
-  removeClause() {
-    //ClauseActionCreators.removeClause(this._clause)
-  }
-
-  toggleOperator() {
-    //ClauseActionCreators.toggleOperator(this._clause)
-  }
-}
-
-
-@Component({
-  selector: 'clause-group',
-  properties: {
-    "rule": "rule",
-    "group": "group",
-    "index": "index"
-  }
-})
-@View({
-  template: clauseGroupTemplate,
-  directives: [ClauseComponent, formDirectives, NgIf, NgFor]
-})
-class ClauseGroupComponent {
-  _group;
-  rule;
-  index:number;
-  clauses:Array = null;
-  isCollapse: any;
-
-  constructor() {
-    //ClauseStore.addChangeListener(this.onChange.bind(this))
-    this.isCollapse = true;
-  }
-
-  get group() {
-    return this._group;
-  }
-
-  set group(group) {
-    this._group = group
-    var idx = 1
-    // TODO: remove this :)
-    this.clauses = [
-      {
-        $key: 'rule0' + idx + '-group02-clause01',
-        type: 'IsAuthenticated',
-        name: 'User is authenticated',
-        owningGroup: 'rule0' + idx + '-group02',
-        value: true,
-        operator: 'AND'
-      },
-      {
-        $key: 'rule0' + idx + '-group02-clause02',
-        type: 'VisitorLocation',
-        name: 'User is visiting from france',
-        owningGroup: 'rule0' + idx + '-group02',
-        value: 'CA',
-        operator: 'AND'
-      }
-    ]
-    //debugger
-    //this.clauses = ClauseStore.getAll(this._group)
-  }
-
-  onChange(action) {
-    //this.clauses = ClauseStore.getAll(this._group);
-  }
-
-  addGroup(rule:any) {
-    //RuleActionCreators.addGroup(rule.$key, new ClauseGroupBuilder().build())
-  }
-
-  removeGroup() {
-    //RuleActionCreators.removeGroup(this.rule.$key, this._group.$key)
-  }
-
-  toggleOperator(rule, group) {
-    //RuleActionCreators.toggleClauseGroupOperator(this.rule, this._group)
-    //log("Toggle group operator", rule, group)
-  }
-
-  toggleCollapse() {
-    this.isCollapse = !this.isCollapse
-  }
-}
+//@Component({
+//  selector: 'rule-action',
+//  properties: {
+//    "rule": "rule",
+//    "ruleAction": "rule-action",
+//    "index": "index"
+//  }
+//})
+//@View({
+//  template: ruleActionTemplate,
+//  directives: [formDirectives, NgIf, NgFor],
+//  appInjector: [FormBuilder]
+//})
+//class RuleActionComponent {
+//  _ruleAction:any;
+//  form:ControlGroup;
+//  rule:any;
+//  index:number;
+//  builder:FormBuilder;
+//
+//  constructor(b:FormBuilder) {
+//    this.builder = b;
+//  }
+//
+//  get ruleAction():any {
+//    return this._ruleAction;
+//  }
+//
+//  set ruleAction(ruleAction:any) {
+//    var ruleActionControl = this.builder.group({
+//      "name": [ruleAction.name, Validators.required]
+//    });
+//    ruleActionControl.controls.name.valueChanges.toRx().debounce(500).subscribe(
+//      (v) => {
+//        this._ruleAction = this._ruleAction.clone().withName(v).build()
+//        this.saveChanges()
+//      })
+//    this.form = ruleActionControl
+//    this._ruleAction = ruleAction
+//  }
+//
+//  addRuleAction() {
+//    //RuleActionActionCreators.addRuleAction(RuleActionBuilder.fromJson({owningRule: this.rule.$key.value}).build(), this.rule)
+//  }
+//
+//  saveChanges() {
+//    //RuleActionActionCreators.updateRuleAction(this._ruleAction)
+//  }
+//
+//  onChange(action) {
+//  }
+//
+//  removeRuleAction() {
+//    //RuleActionActionCreators.removeRuleAction(this._ruleAction.$key)
+//  }
+//}
+//
+//
+//@Component({
+//  selector: 'clause',
+//  properties: {
+//    "rule": "rule",
+//    "group": "group",
+//    "clause": "clause",
+//    "index": "index"
+//  }
+//})
+//@View({
+//  template: clauseTemplate,
+//  directives: [formDirectives, NgIf, NgFor]
+//})
+//class ClauseComponent {
+//  _clause:any;
+//  group:any;
+//  rule:any;
+//  index:number;
+//  clauseTypes:Array;
+//  comparisons:Array;
+//
+//  constructor() {
+//    //ClauseStore.addChangeListener(this.onChange.bind(this))
+//    this.clauseTypes = [{id: 'hello', text: 'foo'}, {id: 'goodbye', text: 'bar'}]
+//    this.comparisons = [{id: 'compHello', text: 'cFoo'}, {id: 'compGoodbye', text: 'cBar'}];
+//  }
+//
+//  get clause() {
+//    return this._clause;
+//  }
+//
+//  set clause(clause) {
+//    this._clause = clause
+//  }
+//
+//  onChange(action) {
+//  }
+//
+//  addClause(rule, group) {
+//    //let clause = ClauseBuilder.fromCfg({owningGroup: group.$key}).build()
+//    //ClauseActionCreators.addClause(clause, rule.$key)
+//  }
+//
+//  removeClause() {
+//    //ClauseActionCreators.removeClause(this._clause)
+//  }
+//
+//  toggleOperator() {
+//    //ClauseActionCreators.toggleOperator(this._clause)
+//  }
+//}
+//
+//
+//@Component({
+//  selector: 'clause-group',
+//  properties: {
+//    "rule": "rule",
+//    "group": "group",
+//    "index": "index"
+//  }
+//})
+//@View({
+//  template: clauseGroupTemplate,
+//  directives: [ClauseComponent, formDirectives, NgIf, NgFor]
+//})
+//class ClauseGroupComponent {
+//  _group;
+//  rule;
+//  index:number;
+//  clauses:Array = null;
+//  isCollapse: any;
+//
+//  constructor() {
+//    //ClauseStore.addChangeListener(this.onChange.bind(this))
+//    this.isCollapse = true;
+//  }
+//
+//  get group() {
+//    return this._group;
+//  }
+//
+//  set group(group) {
+//    this._group = group
+//    var idx = 1
+//    // TODO: remove this :)
+//    this.clauses = [
+//      {
+//        $key: 'rule0' + idx + '-group02-clause01',
+//        type: 'IsAuthenticated',
+//        name: 'User is authenticated',
+//        owningGroup: 'rule0' + idx + '-group02',
+//        value: true,
+//        operator: 'AND'
+//      },
+//      {
+//        $key: 'rule0' + idx + '-group02-clause02',
+//        type: 'VisitorLocation',
+//        name: 'User is visiting from france',
+//        owningGroup: 'rule0' + idx + '-group02',
+//        value: 'CA',
+//        operator: 'AND'
+//      }
+//    ]
+//    //debugger
+//    //this.clauses = ClauseStore.getAll(this._group)
+//  }
+//
+//  onChange(action) {
+//    //this.clauses = ClauseStore.getAll(this._group);
+//  }
+//
+//  addGroup(rule:any) {
+//    //RuleActionCreators.addGroup(rule.$key, new ClauseGroupBuilder().build())
+//  }
+//
+//  removeGroup() {
+//    //RuleActionCreators.removeGroup(this.rule.$key, this._group.$key)
+//  }
+//
+//  toggleOperator(rule, group) {
+//    //RuleActionCreators.toggleClauseGroupOperator(this.rule, this._group)
+//    //log("Toggle group operator", rule, group)
+//  }
+//
+//  toggleCollapse() {
+//    this.isCollapse = !this.isCollapse
+//  }
+//}
 
 
 @Component({
@@ -222,33 +229,35 @@ class ClauseGroupComponent {
 })
 @View({
   template: ruleTemplate,
-  directives: [RuleActionComponent, ClauseGroupComponent, formDirectives, NgIf, NgFor]
+  directives: [formDirectives, NgIf, NgFor]
 })
 class RuleComponent {
-  _rule:Object;
+  _rule:String;
   form:ControlGroup;
   index:number;
-  builder:FormBuilder;
   isCollapse: boolean;
 
-  constructor(b:FormBuilder) {
-    this._rule = {};
-    this.builder = b;
+  constructor( public builder:FormBuilder) {
+    this._rule = "bad!";
     this.isCollapse = true;
+    this.form =  builder.group({
+      "name": ["", Validators.required]
+    });
+    //this.form.controls['name'].valueChanges.toRx().subscribe((v) => log("it changed: ", v))
+    ObservableWrapper.subscribe(this.form.controls['name'].valueChanges, (v) => {
+      log("wtf?", v)
+    });
 
   }
 
-  get rule():Object {
+  get rule():String {
+    log('getting rule')
     return this._rule
   }
 
-  set rule(rule:Object) {
+  set rule(rule:String) {
+    log('setting rule')
     this._rule = rule
-    var ruleControl = this.builder.group({
-      "name": [rule['name'], Validators.required]
-    });
-    ruleControl.controls.name.valueChanges.toRx().subscribe((v) => log("it changed: ", v))
-    this.form = ruleControl
   }
 
   addRule() {
@@ -285,7 +294,7 @@ class RulesEngine {
   rules:Array;
 
   constructor() {
-    this.rules = []
+    this.rules = ["Bob"]
     log("creating rules engine");
     RuleEngine.store.addChangeListener(this.onChange.bind(this))
     RuleEngine.store.init()
@@ -294,7 +303,7 @@ class RulesEngine {
   onChange(event) {
     RuleEngine.store.get().then((rulesAry)=> {
       log("hmmm?", rulesAry)
-      this.rules = [{name: "Hello"}, {name: "World"}]
+      this.rules = ["Hello", "World"]
     })
   }
 
