deb30a34f569ebb8e398b8506e53d14f535ec0f8
Daniel Silva <daniel.silva@dotcms.com>
2015-06-26 11:59:08 -0430

    #7391

--- src/com/dotcms/rest/api/v1/sites/rules/ConditionGroupTransform.java
+++ src/com/dotcms/rest/api/v1/sites/rules/ConditionGroupTransform.java
@@ -2,66 +2,65 @@ package com.dotcms.rest.api.v1.sites.rules;
 
 import com.dotcms.repackage.com.google.common.base.Function;
 import com.dotcms.repackage.com.google.common.collect.Lists;
+import com.dotmarketing.portlets.rules.model.Condition;
 import com.dotmarketing.portlets.rules.model.ConditionGroup;
 import com.dotmarketing.portlets.rules.model.Rule;
+
 import java.util.List;
 
 /**
  * @author Geoff M. Granum
  */
 public class ConditionGroupTransform {
-    private final Function<Rule, RestRule> toRest = new  AppToRest();
-    public ConditionGroupTransform() {}
-
-    public Rule restToApp(RestRule rest) {
-        Rule app = new Rule();
-        return applyRestToApp(rest, app);
-    }
-
-    public Rule applyRestToApp(RestRule rest, Rule app) {
-        app.setName(rest.name);
-        app.setFireOn(Rule.FireOn.valueOf(rest.fireOn));
-        app.setPriority(rest.priority);
-        app.setShortCircuit(rest.shortCircuit);
-        app.setEnabled(rest.enabled);
-        return app;
-    }
-
-    public RestRule appToRest(Rule app) {
-        return toRest.apply(app);
-    }
-
-    public Function<Rule, RestRule> appToRestFn() {
-        return toRest;
-    }
-
-    public static class AppToRest implements Function<Rule, RestRule> {
-
-        @Override
-        public RestRule apply(Rule app) {
-            List<String> groupIds = Lists.newArrayList();
-            for (ConditionGroup group : app.getGroups()) {
-                groupIds.add(group.getId());
-            }
-
-            RestRule rest = new RestRule.Builder()
-                                    .key(app.getId())
-                                    .name(app.getName())
-                                    .fireOn(app.getFireOn().getCamelCaseName())
-                                    .shortCircuit(app.isShortCircuit())
-                                    .priority(app.getPriority())
-                                    .enabled(app.isEnabled())
-                                    .groups(groupIds)
-                                    .build();
-
-            //        List<RuleAction> actions = rulesAPI.getRuleActionsByRule(rule.getId(), user, false);
-            //
-            //        for (RuleAction action : actions) {
-            //            groupsJSON.put(action.getId(), new com.dotmarketing.util.json.JSONObject(action, new String[]{"priority"}));
-            //        }
-
-            return rest;
-        }
-    }
+//    private final Function<ConditionGroup, RestConditionGroup> toRest = new  AppToRest();
+//    public ConditionGroupTransform() {}
+//
+//    public Rule restToApp(RestRule rest) {
+//        Rule app = new Rule();
+//        return applyRestToApp(rest, app);
+//    }
+//
+//    public Rule applyRestToApp(RestConditionGroup rest, ConditionGroup app) {
+//        app.setOperator(Condition.Operator.valueOf(rest.operator));
+//        app.setPriority(rest.priority);
+//        return app;
+//    }
+//
+//    public RestCondition appToRest(Condition app) {
+//        return toRest.apply(app);
+//    }
+//
+//    public Function<Condition, RestCondition> appToRestFn() {
+//        return toRest;
+//    }
+//
+//    public static class AppToRest implements Function<Condition, RestCondition> {
+//
+//        @Override
+//        public RestCondition apply(Condition app) {
+//            List<String> groupIds = Lists.newArrayList();
+//            for (ConditionGroup group : app.getGroups()) {
+//                groupIds.add(group.getId());
+//            }
+//
+//            RestRule rest = new RestRule.Builder()
+//                                    .key(app.getId())
+//                                    .name(app.getName())
+//                                    .fireOn(app.getFireOn().getCamelCaseName())
+//                                    .shortCircuit(app.isShortCircuit())
+//                                    .priority(app.getPriority())
+//                                    .enabled(app.isEnabled())
+//                                    .groups(groupIds)
+//                                    .build();
+//
+//            //        List<RuleAction> actions = rulesAPI.getRuleActionsByRule(rule.getId(), user, false);
+//            //
+//            //        for (RuleAction action : actions) {
+//            //            groupsJSON.put(action.getId(), new com.dotmarketing.util.json.JSONObject(action, new String[]{"priority"}));
+//            //        }
+//
+//            return rest;
+//        }
+//    }
 }
 
