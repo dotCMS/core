a1a976b61be3849db4409b0ef2166b4efa00975a
ggranum <geoff.granum@gmail.com>
2015-07-07 13:57:41 -0700

    Add comparisons to conditionlet output.

--- src/com/dotcms/rest/api/v1/sites/ruleengine/ConditionletTransform.java
+++ src/com/dotcms/rest/api/v1/sites/ruleengine/ConditionletTransform.java
@@ -2,27 +2,30 @@ package com.dotcms.rest.api.v1.sites.ruleengine;
 
 import com.dotmarketing.portlets.rules.conditionlet.Conditionlet;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class ConditionletTransform {
 
     private final Function<Conditionlet, RestConditionlet> toRest = (app) -> {
-
+        ComparisonTransform comparisonTransform = new ComparisonTransform();
         RestConditionlet rest = new RestConditionlet.Builder()
                                         .id(app.getName())
                                         .localizedName(app.getLocalizedName())
                                         .languageId(app.getLanguageId())
+                                        .comparisons(app.getComparisons()
+                                                        .stream()
+                                                        .map(comparisonTransform.appToRestFn())
+                                                        .collect(Collectors.toList()))
                                         .build();
 
         return rest;
     };
 
     public Conditionlet applyRestToApp(RestConditionlet rest, Conditionlet app) {
-
-        return app;
+        throw new IllegalAccessError("Conditionlets are not editable");
     }
 
     public RestConditionlet appToRest(Conditionlet c) {
-
         return toRest.apply(c);
     }
 
