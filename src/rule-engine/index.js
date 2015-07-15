c7310cdbdc4a743903555fe8bbb952451c92def6
ggranum <geoff.granum@gmail.com>
2015-07-15 08:14:10 -0700

    Remove deprecated code.

--- src/rule-engine/index.js
+++ src/rule-engine/index.js
@@ -1,5 +1,1 @@
-export * from './dispatcher/AppDispatcher.js';
-export * from './api/RuleEngineAPI.js';
-export * from './actions/RuleEngineActionCreators.js';
-export * from './stores/RuleStore.js';
 export * from './api/RuleEngineTypes.js'
--- typedefs/rules-engine.d.ts
+++ typedefs/rules-engine.d.ts
@@ -1,21 +1,8 @@
 import Dispatcher from 'flux';
 
-declare class AppDispatcher extends Dispatcher.Dispatcher {  }
-
 declare module Core {
   declare module Collections {
     declare function asArray(any, Function?)
   }
 
-}
-declare module RuleEngine {
-    declare module ruleRepo {
-      declare function push(rule:any):Promise
-      declare function set(rule:any):Promise
-      declare function remove(rule:any):Promise
-  }
-}
-
-declare class EntityMeta {
-  once(eventType:string, callback:Function):Promise;
 }
