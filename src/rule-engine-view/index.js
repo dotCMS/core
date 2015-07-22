b28c8346b8c10f879ae58968517884fc04a36a73
ggranum <geoff.granum@gmail.com>
2015-07-22 13:13:09 -0700

    Light theme.

--- src/rule-engine-view/index.js
+++ src/rule-engine-view/index.js
@@ -1,5 +1,5 @@
 import "bootstrap/css/bootstrap.css!";
 import "./app/styles/rule-engine.css!";
-import "./app/styles/theme-dark.css!";
+import "./app/styles/theme-light.css!";
 
 export * from './app/rule-engine';
