8e09e64ffa43f536157aed1f070912676ced263a
ggranum <geoff.granum@gmail.com>
2015-07-29 09:43:03 -0700

    Trying to work with ObservableWrapper, not succeeding - sharing the joy.

--- src/rule-engine-view/app/conditionlets/conditionlet-component.ts
+++ src/rule-engine-view/app/conditionlets/conditionlet-component.ts
@@ -2,26 +2,42 @@
 /// <reference path="../../../../typings/angular2/angular2.d.ts" />
 
 import {Directive, LifecycleEvent, Attribute, Ancestor, EventEmitter, NgFor, NgIf, Component, View} from 'angular2/angular2';
+import { ObservableWrapper } from 'angular2/src/facade/async';
 
 @Directive({
   selector: 'conditionlet',
   properties: ["value", "condition", "conditionlet"],
-  events: ['change']
+  events: ['valueChange', 'comparisonChange']
 })
 export class ConditionletDirective {
   value:string;
   condition:any;
   conditionlet:any;
-  inputHasValue:boolean;
-  inputHasFocus:boolean;
-  change = new EventEmitter();
+  child:any;
+  valueChange: EventEmitter;
+  comparisonChange:EventEmitter;
+
 
   constructor(@Attribute('value') value:string, @Attribute('condition') condition:any, @Attribute('conditionlet') conditionlet:any, @Attribute('id') id:string) {
     this.value = value ? value : ''
     this.condition = condition ? condition : null
     this.conditionlet = conditionlet ? conditionlet : null
-    this.inputHasValue = false;
-    this.inputHasFocus = false;
+    this.valueChange = new EventEmitter();
+    this.comparisonChange = new EventEmitter();
+  }
+
+  /** Forward events from the child conditionlet. */
+  register(child) {
+    this.child = child;
+    ObservableWrapper.subscribe(child.valueChange, (event) => {
+          debugger;
+          this.valueChange.next(event)
+        }
+    );
+
+    ObservableWrapper.subscribe(child.comparisonChange, (event) => {
+      this.comparisonChange.next(event)
+    });
   }
 
 
--- src/rule-engine-view/app/conditionlets/users-page-visits.ts
+++ src/rule-engine-view/app/conditionlets/users-page-visits.ts
@@ -2,6 +2,8 @@
 /// <reference path="../../../../typings/angular2/angular2.d.ts" />
 
 import {Directive, LifecycleEvent, Attribute, Ancestor, ObservableWrapper, EventEmitter, NgFor, NgIf, Component, View} from 'angular2/angular2';
+import { ObservableWrapper } from 'angular2/src/facade/async';
+
 import {ConditionletDirective} from './conditionlet-component';
 
 @Component({
@@ -27,21 +29,28 @@ export class UsersPageVisitsConditionlet {
   conditionletDir:ConditionletDirective;
   inputHasValue:boolean;
   inputHasFocus:boolean;
-  change = new EventEmitter();
+  valueChange: EventEmitter;
+  comparisonChange:EventEmitter;
+
 
   constructor(@Ancestor() conditionletDir:ConditionletDirective, @Attribute('id') id:string) {
     this.conditionletDir = conditionletDir
     this.inputHasValue = false;
     this.inputHasFocus = false;
+    this.valueChange = new EventEmitter();
+    this.comparisonChange = new EventEmitter();
+
+    this.conditionletDir.register(this)
   }
 
   setComparison(value:string){
-    this.change.next({key: 'comparison', was: this.conditionletDir.condition.comparison,  isNow: event.target.value})
+    ObservableWrapper.callNext(this.comparisonChange, {was: this.conditionletDir.condition.comparison,  isNow: event.target.value})
   }
 
   updateValue(event) {
-    this.change.next({key: 'value', was: this.conditionletDir.value,  isNow: event.target.value})
+    ObservableWrapper.callNext(this.valueChange, {was: this.conditionletDir.value,  isNow: event.target.value})
   }
+
 
   setHasFocus(hasFocus:boolean) {
     this.inputHasFocus = hasFocus
