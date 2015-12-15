4b54110c7bedad49ea0c04a2363afc2d6ae3e7c9
Will Ezell <will@dotcms.com>
2015-12-15 14:04:27 -0500

    card #564 fixes crappy sql

--- src/com/dotmarketing/portlets/personas/business/PersonaFactoryImpl.java
+++ src/com/dotmarketing/portlets/personas/business/PersonaFactoryImpl.java
@@ -7,15 +7,17 @@ import com.dotmarketing.beans.Host;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.business.UserAPI;
 import com.dotmarketing.common.db.DotConnect;
+import com.dotmarketing.db.HibernateUtil;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.portlets.folders.business.FolderAPI;
 import com.dotmarketing.portlets.structure.factories.FieldFactory;
 import com.dotmarketing.portlets.structure.model.Field;
 import com.dotmarketing.portlets.structure.model.Structure;
+import com.dotmarketing.util.Logger;
 
 public class PersonaFactoryImpl implements PersonaFactory {
 
-	private final String INSERT_DEFAULT_STRUC_QUERY = "insert into structure (inode, name, description, structuretype, system, fixed, velocity_var_name, host, folder, mod_date, default_structure) value ( ?, ?, ?, ?, ?, ?, ?, ?, ? ,?,?)";
+	private final String INSERT_DEFAULT_STRUC_QUERY = "insert into structure (inode, name, description, structuretype, system, fixed, velocity_var_name, host, folder, mod_date, default_structure) values ( ?, ?, ?, ?, ?, ?, ?, ?, ? ,?,?)";
 	private final String INSERT_DEFAULT_STRUC_INODE_QUERY = "insert into inode (inode, owner, idate, type) values (?, ?, ?, ?)";
 	
 	// make sure we have inode and structure entries
@@ -34,49 +36,70 @@ public class PersonaFactoryImpl implements PersonaFactory {
 	@Override
 	public void createDefualtPersonaStructure() throws DotDataException {
 
+		
+		
+		
+		
+		
+		boolean localTransaction = false;
 		DotConnect dc = new DotConnect();
-		dc.setSQL(SELECT_DEFAULT_STRUC_QUERY);
-		dc.addParam(PersonaAPI.DEFAULT_PERSONAS_STRUCTURE_INODE);
-		if (dc.loadResults().size() > 0) {
-			return;
+
+		try {
+			localTransaction = HibernateUtil.startLocalTransactionIfNeeded();
+		
+			
+
+			dc.setSQL(SELECT_DEFAULT_STRUC_QUERY);
+			dc.addParam(PersonaAPI.DEFAULT_PERSONAS_STRUCTURE_INODE);
+			if (dc.loadResults().size() > 0) {
+				return;
+			}
+	
+			dc.setSQL(INSERT_DEFAULT_STRUC_INODE_QUERY);
+			dc.addParam(PersonaAPI.DEFAULT_PERSONAS_STRUCTURE_INODE);
+			dc.addParam(UserAPI.SYSTEM_USER_ID);
+			dc.addParam(new Date());
+			dc.addParam(new Structure().getType());
+			dc.loadResult();
+	
+	
+			/**
+			 * inode name description structuretype system fixed velocity_var_name
+			 * host folder mod_date
+			 */
+			 dc = new DotConnect();
+			dc.setSQL(INSERT_DEFAULT_STRUC_QUERY);
+			dc.addParam(PersonaAPI.DEFAULT_PERSONAS_STRUCTURE_INODE);
+			dc.addParam(PersonaAPI.DEFAULT_PERSONAS_STRUCTURE_NAME);
+			dc.addParam(PersonaAPI.DEFAULT_PERSONAS_STRUCTURE_DESCRIPTION);
+			dc.addParam(Structure.STRUCTURE_TYPE_PERSONA);
+			dc.addParam(false);
+			dc.addParam(false);
+			dc.addParam(PersonaAPI.DEFAULT_PERSONAS_STRUCTURE_VARNAME);
+			dc.addParam(Host.SYSTEM_HOST);
+			dc.addParam(FolderAPI.SYSTEM_FOLDER);
+			dc.addParam(new Date());
+			dc.addParam(false);
+			dc.loadResult();
+	
+			
+			
+			Structure proxy = new Structure();
+			proxy.setInode(PersonaAPI.DEFAULT_PERSONAS_STRUCTURE_INODE);
+			List<Field> fields = APILocator.getPersonaAPI().getBasePersonaFields(proxy);
+			int i = 0;
+			for (Field f : fields) {
+				f.setInode(DEFAUTL_PERSONA_FIELD_INODES[i++]);
+				FieldFactory.saveField(f, f.getInode());
+			}
+		} catch (final Exception e) {
+			if (localTransaction) {
+				HibernateUtil.rollbackTransaction();
+			}
+			Logger.error(this, "deleteWorkflowTask failed:" + e, e);
+			throw new DotDataException(e.toString());
 		}
 
-		dc.setSQL(INSERT_DEFAULT_STRUC_INODE_QUERY);
-		dc.addParam(PersonaAPI.DEFAULT_PERSONAS_STRUCTURE_INODE);
-		dc.addParam(UserAPI.SYSTEM_USER_ID);
-		dc.addParam(new Date());
-		dc.addParam(new Structure().getType());
-		dc.loadResult();
-
-
-		/**
-		 * inode name description structuretype system fixed velocity_var_name
-		 * host folder mod_date
-		 */
-		dc.setSQL(INSERT_DEFAULT_STRUC_QUERY);
-		dc.addParam(PersonaAPI.DEFAULT_PERSONAS_STRUCTURE_INODE);
-		dc.addParam(PersonaAPI.DEFAULT_PERSONAS_STRUCTURE_NAME);
-		dc.addParam(PersonaAPI.DEFAULT_PERSONAS_STRUCTURE_DESCRIPTION);
-		dc.addParam(Structure.STRUCTURE_TYPE_PERSONA);
-		dc.addParam(false);
-		dc.addParam(false);
-		dc.addParam(PersonaAPI.DEFAULT_PERSONAS_STRUCTURE_VARNAME);
-		dc.addParam(Host.SYSTEM_HOST);
-		dc.addParam(FolderAPI.SYSTEM_FOLDER);
-		dc.addParam(new Date());
-		dc.addParam(false);
-		dc.loadResult();
-
-		
-		
-		Structure proxy = new Structure();
-		proxy.setInode(PersonaAPI.DEFAULT_PERSONAS_STRUCTURE_INODE);
-		List<Field> fields = APILocator.getPersonaAPI().getBasePersonaFields(proxy);
-		int i = 0;
-		for (Field f : fields) {
-			f.setInode(DEFAUTL_PERSONA_FIELD_INODES[i++]);
-			FieldFactory.saveField(f, f.getInode());
-		}
 
 	}
 
