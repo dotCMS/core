247e3c2c14005231f83637293711c379a3443c9e
Erick Gonzalez <erick.gonzalez@dotcms.com>
2016-01-05 14:19:01 -0600

    card 449 Create Numeric Input and Comparison methods for site visits conditionlet

---
+++ src/com/dotmarketing/portlets/rules/parameter/display/NumericInput.java
@@ -0,0 +1,28 @@
+package com.dotmarketing.portlets.rules.parameter.display;
+
+import com.dotmarketing.portlets.rules.parameter.type.NumericType;
+
+/**
+ * @author Erick Gonzalez
+ */
+public class NumericInput<T extends NumericType> extends Input<T> {
+
+	private String placeholder = "";
+
+    public NumericInput(T dataType) {
+        super("numeric", dataType);
+    }
+
+    protected NumericInput(String id, T dataType) {
+        super(id, dataType);
+    }
+
+    public NumericInput placeholder(String placeholder) {
+        this.placeholder = placeholder;
+        return this;
+    }
+
+    public String getPlaceholder() {
+        return placeholder;
+    }
+}
