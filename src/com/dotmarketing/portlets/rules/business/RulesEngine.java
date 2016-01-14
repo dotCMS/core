808f5d7e92fb5b20b8b1e526baac6d563ebf105f
Oswaldo Gallango <oswaldo@dotcms.com>
2016-01-14 09:35:16 -0600

    card433 adding log

--- src/com/dotmarketing/portlets/rules/business/RulesEngine.java
+++ src/com/dotmarketing/portlets/rules/business/RulesEngine.java
@@ -6,6 +6,7 @@ import com.dotmarketing.business.web.WebAPILocator;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.portlets.rules.exception.RuleEngineException;
 import com.dotmarketing.portlets.rules.model.Rule;
+import com.dotmarketing.util.Config;
 import com.dotmarketing.util.Logger;
 import com.liferay.portal.model.User;
 import java.util.Set;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public final class RulesEngine {
+	private static int SLOW_RULE_LOG_MIN=Config.getIntProperty("SLOW_RULE_LOG_MIN", 1000);
 
     public static void fireRules(HttpServletRequest req, HttpServletResponse res, Rule.FireOn fireOn) {
 
@@ -40,8 +42,13 @@ public final class RulesEngine {
 
             for (Rule rule : rules) {
                 try {
+                	long before = System.currentTimeMillis();
                     rule.checkValid(); // @todo ggranum: this should actually be done on writing to the DB, or at worst reading from.
                     rule.evaluate(req, res);
+                    long after = System.currentTimeMillis();
+        			if((after - before) > SLOW_RULE_LOG_MIN) {
+        				Logger.warn(RulesEngine.class, "Rule ID:"+rule.getId()+" is running too slow. The rule is fired on: "+ rule.getFireOn().name());
+        			}
                 } catch (RuleEngineException e) {
                     Logger.error(RulesEngine.class, "Rule could not be evaluated. Rule Id: " + rule.getId(), e);
                 }
