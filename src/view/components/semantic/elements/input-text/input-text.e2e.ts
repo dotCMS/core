907f1d2f22feefb260a61d84f4591778f60b1e8a
ggranum <geoff.granum@gmail.com>
2016-01-21 17:57:14 +0000

    Fix remaining demo pages, input-text e2e.

--- src/view/components/semantic/elements/input-text/input-text.e2e.ts
+++ src/view/components/semantic/elements/input-text/input-text.e2e.ts
@@ -2,7 +2,7 @@
 
 describe('The input-text demo', function() {
   it('should have a title.', function() {
-    browser.get('http://localhost:9000/build/view/components/semantic/elements/input-text/index.html');
+    browser.get('http://localhost:9000/build/view/components/semantic/elements/input-text.html');
     expect(browser.getTitle()).toEqual('Input-Text Demo');
   });
 
