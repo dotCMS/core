169f9f1641ea385294640f1ab736359864ccd084
Will Ezell <will@dotcms.com>
2016-01-26 12:24:26 -0500

    working on card 623

--- src/com/dotmarketing/common/model/ContentletSearch.java
+++ src/com/dotmarketing/common/model/ContentletSearch.java
@@ -4,6 +4,13 @@ package com.dotmarketing.common.model;
 public class  ContentletSearch {
 	private String inode;
 	private String identifier;
+	float score;
+	public float getScore() {
+		return score;
+	}
+	public void setScore(float score) {
+		this.score = score;
+	}
 	public void setIdentifier(String identifier) {
 		this.identifier = identifier;
 	}
