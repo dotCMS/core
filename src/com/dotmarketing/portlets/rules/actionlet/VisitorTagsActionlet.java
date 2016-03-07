568d89a1ad9f063058f9442aa82733ca6bbc5692
mrizo <mauricio.rizo@dotcms.com>
2016-03-07 08:40:45 -0600

    Card770 updates to messages

--- src/com/dotmarketing/portlets/rules/actionlet/VisitorTagsActionlet.java
+++ src/com/dotmarketing/portlets/rules/actionlet/VisitorTagsActionlet.java
@@ -98,7 +98,7 @@ public class VisitorTagsActionlet extends RuleActionlet<VisitorTagsActionlet.Ins
          * @throws InvalidRuleParameterException
          */
         private String checkValid(final Map<String, ParameterModel> parameters) throws InvalidRuleParameterException{
-        	if(parameters == null || parameters.size()!=1){
+        	if(parameters == null || parameters.size() != 1	){
         		throw new InvalidRuleParameterException("This actionlet only allows '%s' as parameter",TAGS_KEY);
         	}
         	String value = parameters.get(TAGS_KEY).getValue();
@@ -107,13 +107,13 @@ public class VisitorTagsActionlet extends RuleActionlet<VisitorTagsActionlet.Ins
         	}
         	String trimmedValue = value.trim();
         	if(trimmedValue.indexOf('\"') > -1 || trimmedValue.indexOf('\'') > -1)
-        		throw new InvalidRuleParameterException("Quates and doble quotes are not allowed");
+        		throw new InvalidRuleParameterException("Single or double quotes are not allowed");
         	if(trimmedValue.startsWith(",") || trimmedValue.endsWith(",")){
         		throw new InvalidRuleParameterException("Empty tag values are not allowed");
         	}
             if(REQUIRED){
             	if(trimmedValue.isEmpty())
-            		throw new InvalidRuleParameterException("The tags parameter requieres values. Empty values not allowed.");
+            		throw new InvalidRuleParameterException("The tags parameter requieres values. Empty values are not allowed.");
     	        String[] values = trimmedValue.trim().split(",");
     			Set<String> uniqueValues = new HashSet<String>();
     			for (String currentValue : values){
