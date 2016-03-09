545136edc1ab112f95d9cf9de0f1c5e533ec164f
Oscar Arrieta <oscar.arrieta@dotcms.com>
2016-03-09 07:39:37 -0600

    card762: Also remove from container-structures table

--- src/com/dotmarketing/business/BaseWebAssetAPI.java
+++ src/com/dotmarketing/business/BaseWebAssetAPI.java
@@ -499,6 +499,11 @@ public abstract class BaseWebAssetAPI extends BaseInodeAPI {
         dc.addParam(date);
         dc.loadResult();
 
+		String deleteContainerStructures = "delete from container_structures where container_inode in ("+inodesToDelete+")";
+		dc.setSQL(deleteContainerStructures);
+		dc.addParam(date);
+		dc.loadResult();
+
         String deleteContentletSQL = "delete from "+assetType+" where  "+condition;
         dc.setSQL(deleteContentletSQL);
         dc.addParam(date);
