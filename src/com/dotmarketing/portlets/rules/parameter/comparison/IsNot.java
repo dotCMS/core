cc482652fcf95fe5a3b773e9a36ba36a1086b215
Daniel Silva <daniel.silva@dotcms.com>
2016-03-22 15:02:01 -0430

    card-789:Conditionlet, Automated Test, Changes for "Is" Comparison

--- src/com/dotmarketing/portlets/rules/parameter/comparison/IsNot.java
+++ src/com/dotmarketing/portlets/rules/parameter/comparison/IsNot.java
@@ -3,14 +3,14 @@ package com.dotmarketing.portlets.rules.parameter.comparison;
 /**
  * @author Geoff M. Granum
  */
-public class IsNot extends Comparison<Comparable> {
+public class IsNot extends Comparison<Object> {
 
     public IsNot() {
         super("isNot");
     }
 
     @Override
-    public boolean perform(Comparable left, Comparable right) {
+    public boolean perform(Object left, Object right) {
         return !java.util.Objects.equals(left, right);
     }
 }
