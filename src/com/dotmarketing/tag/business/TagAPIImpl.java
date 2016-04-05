7f175e20f8f41ee5212f9ad4bebde045b4f4c23c
Erick Gonzalez <erick.gonzalez@dotcms.com>
2016-04-05 09:14:10 -0600

    card867 Check Tag Storage

--- src/com/dotmarketing/tag/business/TagAPIImpl.java
+++ src/com/dotmarketing/tag/business/TagAPIImpl.java
@@ -248,15 +248,19 @@ public class TagAPIImpl implements TagAPI {
                 hostId = Host.SYSTEM_HOST;
             }
             tag.setHostId(hostId);
-
-            Tag createdTag = tagFactory.createTag(tag);
+            
+            Tag foundTagInStorage = tagFactory.getTagByNameAndHost(tag.getTagName(),tag.getHostId());
+            
+            if(foundTagInStorage == null){
+            	foundTagInStorage = tagFactory.createTag(tag);
+            }
 
             //Everything ok..., committing the transaction
             if ( localTransaction ) {
                 HibernateUtil.commitTransaction();
             }
 
-            return createdTag;
+            return foundTagInStorage;
         } catch ( Exception e ) {
             if ( localTransaction ) {
                 HibernateUtil.rollbackTransaction();
