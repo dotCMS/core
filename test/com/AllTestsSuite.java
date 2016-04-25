16071e18868cbf65cf0fef1a120a92d74b882356
Oscar Arrieta <oscar.arrieta@dotcms.com>
2016-04-25 11:49:50 -0600

    #8918: Adding a little order

--- test/com/AllTestsSuite.java
+++ test/com/AllTestsSuite.java
@@ -1,14 +1,5 @@
 package com;
 
-import com.dotmarketing.portlets.rules.RulesUnderPageAssetsFTest;
-import com.dotmarketing.portlets.rules.actionlet.VisitorsTagsActionletFTest;
-import com.dotmarketing.portlets.rules.business.RulesAPIFTest;
-
-import com.dotmarketing.portlets.rules.business.RulesCacheFTest;
-import com.dotmarketing.portlets.rules.conditionlet.*;
-
-import org.apache.velocity.runtime.parser.node.SimpleNodeTest;
-
 import com.dotcms.cmis.DotCMSCMISTest;
 import com.dotcms.content.elasticsearch.business.ESContentFactoryImplTest;
 import com.dotcms.content.elasticsearch.business.ESContentletIndexAPITest;
@@ -19,15 +10,13 @@ import com.dotcms.csspreproc.SassCompilerTest;
 import com.dotcms.notification.business.NotificationAPITest;
 import com.dotcms.publisher.ajax.RemotePublishAjaxActionTest;
 import com.dotcms.publisher.endpoint.business.PublishingEndPointAPITest;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 import com.dotcms.rest.ContentResourceTest;
 import com.dotcms.rest.RoleResourceTest;
 import com.dotcms.rest.WebResourceTest;
+import com.dotcms.rest.api.v1.sites.rules.ActionResourceFTest;
 import com.dotcms.rest.api.v1.sites.rules.ConditionGroupResourceFTest;
 import com.dotcms.rest.api.v1.sites.rules.RuleResourceFTest;
 import com.dotcms.rest.api.v1.system.ruleengine.ActionletResourceFTest;
-import com.dotcms.rest.api.v1.sites.rules.ActionResourceFTest;
 import com.dotmarketing.business.IdentifierAPITest;
 import com.dotmarketing.business.LanguageAPITest;
 import com.dotmarketing.business.PermissionAPITest;
@@ -45,9 +34,14 @@ import com.dotmarketing.portlets.folder.business.FolderAPITest;
 import com.dotmarketing.portlets.htmlpages.business.HTMLPageAPITest;
 import com.dotmarketing.portlets.linkchecker.business.LinkCheckerAPITest;
 import com.dotmarketing.portlets.links.business.MenuLinkAPITest;
+import com.dotmarketing.portlets.rules.RulesUnderPageAssetsFTest;
 import com.dotmarketing.portlets.rules.actionlet.PersonaActionletFTest;
 import com.dotmarketing.portlets.rules.actionlet.SetResponseHeaderActionletFTest;
 import com.dotmarketing.portlets.rules.actionlet.SetSessionAttributeActionletFTest;
+import com.dotmarketing.portlets.rules.actionlet.VisitorsTagsActionletFTest;
+import com.dotmarketing.portlets.rules.business.RulesAPIFTest;
+import com.dotmarketing.portlets.rules.business.RulesCacheFTest;
+import com.dotmarketing.portlets.rules.conditionlet.*;
 import com.dotmarketing.portlets.structure.business.FieldAPITest;
 import com.dotmarketing.portlets.structure.business.URLMapTest;
 import com.dotmarketing.portlets.structure.factories.FieldFactoryTest;
@@ -55,10 +49,13 @@ import com.dotmarketing.portlets.structure.factories.StructureFactoryTest;
 import com.dotmarketing.portlets.templates.business.TemplateAPITest;
 import com.dotmarketing.portlets.workflows.business.WorkflowAPITest;
 import com.dotmarketing.portlets.workflows.model.WorkflowSearcherTest;
-import com.dotmarketing.util.ImportUtilTest;
 import com.dotmarketing.sitesearch.ajax.SiteSearchAjaxActionTest;
 import com.dotmarketing.tag.business.TagAPITest;
+import com.dotmarketing.util.ImportUtilTest;
 import com.dotmarketing.webdav.WebDavTest;
+import org.apache.velocity.runtime.parser.node.SimpleNodeTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
 /**
  * @author Jonathan Gamba.
@@ -66,76 +63,82 @@ import com.dotmarketing.webdav.WebDavTest;
  */
 @RunWith (Suite.class)
 @Suite.SuiteClasses ({
-	HostAPITest.class, //Needs Enterprise License
-    LinkCheckerAPITest.class, //Needs Enterprise License
-    TemplateAPITest.class,
-    HTMLPageAPITest.class,
-    CategoryAPITest.class,
-    MenuLinkAPITest.class,
-    ContentletFactoryTest.class,
-    ContentletAPITest.class,
-    ContainerAPITest.class,
-    FieldFactoryTest.class,
-    StructureFactoryTest.class,
-    FieldAPITest.class,
-    URLMapTest.class,
-    PermissionAPITest.class,
-    PublishingEndPointAPITest.class,
-    ESContentletIndexAPITest.class,
-    DotCMSCMISTest.class,
-    WebResourceTest.class,
-    RoleResourceTest.class,
-    ESIndexSpeedTest.class,
-    PluginMergerTest.class,
-    WebDavTest.class,
-    ContentResourceTest.class, //Needs Enterprise License
-    RoleAPITest.class,
-    LanguageAPITest.class,
-    FolderAPITest.class,
-    WorkflowSearcherTest.class,
-    NotificationAPITest.class,
-    SassCompilerTest.class,
-    LessCompilerTest.class,
-    IdentifierAPITest.class,
-    ImportUtilTest.class,
-    SiteSearchAjaxActionTest.class,
-    CSSPreProcessServletTest.class, //Needs Enterprise License
-    ESContentFactoryImplTest.class,
-    HibernateUtilTest.class,
-    WorkflowAPITest.class,
-    ContentletAjaxTest.class,
-    SimpleNodeTest.class,
-    DbConnectionFactoryUtilTest.class,
-    RulesAPIFTest.class,
-    RuleResourceFTest.class,
-    ConditionGroupResourceFTest.class,
-    RemotePublishAjaxActionTest.class, //Needs Enterprise License
-    ActionletResourceFTest.class,
-    SetSessionAttributeActionletFTest.class,
-    SetResponseHeaderActionletFTest.class,
-    ActionResourceFTest.class,
-    RulesCacheFTest.class,
-    PersonaActionletFTest.class, //Needs Enterprise License
-	UsersBrowserLanguageConditionletFTest.class,
-	UsersBrowserLanguageConditionletTest.class,
-    VisitedUrlConditionletTest.class,
-    VisitedUrlConditionletFTest.class,
-	CurrentSessionLanguageConditionletFTest.class,
-	CurrentSessionLanguageConditionletTest.class,
-	TagAPITest.class,
-	RulesUnderPageAssetsFTest.class,
-	VisitorsTagsActionletFTest.class,
-    ConditionletFTest.class,
-    ConditionletOSGIFTest.class,
-    NumberOfTimesPreviouslyVisitedConditionletFTest.class,
-    NumberOfTimesPreviouslyVisitedConditionletTest.class,
-    PagesViewedConditionletFTest.class,
-    UsersSiteVisitsConditionletFTest.class,
-    UsersSiteVisitsConditionletTest.class,
-    VisitorOperatingSystemConditionletFTest.class,
-    VisitorOperatingSystemConditionletTest.class,
-    VisitorsCurrentUrlConditionletFTest.class
-})
-public class AllTestsSuite {
 
-}
+        HostAPITest.class, //Needs Enterprise License
+        LinkCheckerAPITest.class, //Needs Enterprise License
+        TemplateAPITest.class,
+        HTMLPageAPITest.class,
+        CategoryAPITest.class,
+        MenuLinkAPITest.class,
+        ContentletFactoryTest.class,
+        ContentletAPITest.class,
+        ContainerAPITest.class,
+        FieldFactoryTest.class,
+        StructureFactoryTest.class,
+        FieldAPITest.class,
+        URLMapTest.class,
+        PermissionAPITest.class,
+        PublishingEndPointAPITest.class,
+        ESContentletIndexAPITest.class,
+        DotCMSCMISTest.class,
+        WebResourceTest.class,
+        RoleResourceTest.class,
+        ESIndexSpeedTest.class,
+        PluginMergerTest.class,
+        WebDavTest.class,
+        ContentResourceTest.class, //Needs Enterprise License
+        RoleAPITest.class,
+        LanguageAPITest.class,
+        FolderAPITest.class,
+        WorkflowSearcherTest.class,
+        NotificationAPITest.class,
+        SassCompilerTest.class,
+        LessCompilerTest.class,
+        IdentifierAPITest.class,
+        ImportUtilTest.class,
+        SiteSearchAjaxActionTest.class,
+        CSSPreProcessServletTest.class, //Needs Enterprise License
+        ESContentFactoryImplTest.class,
+        HibernateUtilTest.class,
+        WorkflowAPITest.class,
+        ContentletAjaxTest.class,
+        SimpleNodeTest.class,
+        DbConnectionFactoryUtilTest.class,
+        RuleResourceFTest.class,
+        ConditionGroupResourceFTest.class,
+        RemotePublishAjaxActionTest.class, //Needs Enterprise License
+        ActionResourceFTest.class,
+        TagAPITest.class,
+
+        //Rules.
+        RulesAPIFTest.class,
+        RulesCacheFTest.class,
+        RulesUnderPageAssetsFTest.class,
+
+        //Rules:Actionlets.
+        ActionletResourceFTest.class,
+        PersonaActionletFTest.class, //Needs Enterprise License
+        SetResponseHeaderActionletFTest.class,
+        SetSessionAttributeActionletFTest.class,
+        VisitorsTagsActionletFTest.class,
+
+        //Rules:Conditionlets.
+        ConditionletFTest.class,
+        ConditionletOSGIFTest.class,
+        CurrentSessionLanguageConditionletFTest.class,
+        CurrentSessionLanguageConditionletTest.class,
+        NumberOfTimesPreviouslyVisitedConditionletFTest.class,
+        NumberOfTimesPreviouslyVisitedConditionletTest.class,
+        PagesViewedConditionletFTest.class,
+        UsersBrowserLanguageConditionletFTest.class,
+        UsersBrowserLanguageConditionletTest.class,
+        UsersSiteVisitsConditionletFTest.class,
+        UsersSiteVisitsConditionletTest.class,
+        VisitedUrlConditionletTest.class,
+        VisitedUrlConditionletFTest.class,
+        VisitorOperatingSystemConditionletFTest.class,
+        VisitorOperatingSystemConditionletTest.class,
+        VisitorsCurrentUrlConditionletFTest.class
+})
+
+public class AllTestsSuite {}
