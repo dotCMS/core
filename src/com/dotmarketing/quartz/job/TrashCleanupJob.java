00f248d00761c421bcf8d31261102fa6456328ec
Will Ezell <will@dotcms.com>
2016-06-10 11:48:32 -0400

    #9200 removing old JBoss crapcache and cleaning up the unnused TrashUtils

--- src/com/dotmarketing/quartz/job/TrashCleanupJob.java
+++ src/com/dotmarketing/quartz/job/TrashCleanupJob.java
@@ -3,21 +3,11 @@
  */
 package com.dotmarketing.quartz.job;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-
-import com.dotcms.repackage.org.apache.commons.io.FileUtils;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
-import com.dotmarketing.util.Config;
-import com.dotmarketing.util.Logger;
 import com.dotmarketing.util.TrashUtils;
-import com.liferay.util.FileUtil;
 
 /**
  * This job will clean up the trash folder created under the dotsecure directory.  It will cleanup files older than 1 hour by default.
@@ -33,19 +23,7 @@ public class TrashCleanupJob implements Job {
 	
 	public void execute(JobExecutionContext ctx) throws JobExecutionException {
 		
-		File trashFolder = TrashUtils.getTrashFolder();
-		if(!trashFolder.exists()){
-			Logger.debug(this,"Trash folder "+ trashFolder.getPath() + " not found exiting job");
-			return;
-		}
-		File[] files = trashFolder.listFiles();
-		for (File file : files) {
-			if(file.isDirectory()){
-				FileUtil.deltree(file);
-			}else{
-				file.delete();
-			}
-		}
+		TrashUtils.emptyTrash();
 		
 	}
 
--- src/com/dotmarketing/util/TrashUtils.java
+++ src/com/dotmarketing/util/TrashUtils.java
@@ -4,15 +4,17 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 
+import com.liferay.util.FileUtil;
+
 public class TrashUtils {
 
-	private static final String TRASH_FOLDER = "dottrash";
+	private static final String TRASH_FOLDER = "trash";
 	
 	public static File getTrashFolder () {
 		return new File(ConfigUtils.getDynamicContentPath() + File.separator+TRASH_FOLDER);
 	}
 	
-	public static boolean moveFileToThrash(File[] filesToMove, String trashCategory) throws IOException {
+	public static boolean moveFileToTrash(File[] filesToMove, String trashCategory) throws IOException {
 		
 		trashCategory = trashCategory == null?File.separator:File.separator + RegEX.replaceAll(trashCategory, "_", "[^A-Za-z0-9]");
 		String trashSubFolder = getTrashFolder().getCanonicalPath() + trashCategory + File.separator + DateUtil.formatDate(new Date(), "yyyyMMddHHmmssSSS");
@@ -23,31 +25,45 @@ public class TrashUtils {
 		}
 		for(File file : filesToMove) {
 			File newDest = new File(f, file.getName());
-			if (file.exists()) {
-				if(!file.renameTo(newDest)) {
-					return false;
-				}
-			}
+			FileUtil.move(file, newDest);
 		}
 		return true;
 	}
 	
-	public static boolean moveFileToThrash(String[] filePaths, String trashCategory) throws IOException {
+	public static boolean moveFileToTrash(String[] filePaths, String trashCategory) throws IOException {
 		File[]filesToMove = new File[filePaths.length];
 		int i = 0;
 		for(String filePath : filePaths) {
 			File fileToMove = new File(filePath);
 			filesToMove[i++] = fileToMove;
 		}
-		return moveFileToThrash(filesToMove, trashCategory);
+		return moveFileToTrash(filesToMove, trashCategory);
 	}
 
-	public static boolean moveFileToThrash(File fileToMove, String trashCategory) throws IOException {
-		return moveFileToThrash(new File[] { fileToMove }, trashCategory);
+	public static boolean moveFileToTrash(File fileToMove, String trashCategory) throws IOException {
+		return moveFileToTrash(new File[] { fileToMove }, trashCategory);
 	}
 	
-	public static boolean moveFileToThrash(String filePath, String trashCategory) throws IOException {
+	public static boolean moveFileToTrash(String filePath, String trashCategory) throws IOException {
 		File fileToMove = new File(filePath);
-		return moveFileToThrash(fileToMove, trashCategory);
+		return moveFileToTrash(fileToMove, trashCategory);
+	}
+	
+	public static boolean emptyTrash(){
+	
+		File trashFolder = getTrashFolder();
+		if(!trashFolder.exists()){
+			Logger.debug(TrashUtils.class,"Trash folder "+ trashFolder.getPath() + " not found exiting job");
+			return false;
+		}
+		File[] files = trashFolder.listFiles();
+		for (File file : files) {
+			if(file.isDirectory()){
+				FileUtil.deltree(file);
+			}else{
+				file.delete();
+			}
+		}
+		return true;
 	}
 }
