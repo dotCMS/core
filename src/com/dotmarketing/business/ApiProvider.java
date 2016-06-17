6980efeeffc9f7f8f806003dd216bb9245255e0e
Daniel Silva <daniel.silva@dotcms.com>
2016-06-17 10:08:06 -0400

    #9037 Add new methods to ApiProvider.

--- src/com/dotmarketing/business/ApiProvider.java
+++ src/com/dotmarketing/business/ApiProvider.java
@@ -2,6 +2,7 @@ package com.dotmarketing.business;
 
 import com.dotmarketing.business.web.UserWebAPI;
 import com.dotmarketing.business.web.WebAPILocator;
+import com.dotmarketing.portlets.categories.business.CategoryAPI;
 import com.dotmarketing.portlets.contentlet.business.ContentletAPI;
 import com.dotmarketing.portlets.contentlet.business.HostAPI;
 import com.dotcms.enterprise.rules.RulesAPI;
@@ -40,6 +41,10 @@ public class ApiProvider {
     public LanguageAPI languageAPI() { return APILocator.getLanguageAPI(); }
 
     public ContentletAPI contentletAPI() { return APILocator.getContentletAPI(); }
+
+    public PermissionAPI permissionAPI() { return APILocator.getPermissionAPI(); }
+
+    public CategoryAPI categoryAPI() { return APILocator.getCategoryAPI(); }
 
     /*******  Web API Accessors ******/
     public UserWebAPI userWebAPI() {
