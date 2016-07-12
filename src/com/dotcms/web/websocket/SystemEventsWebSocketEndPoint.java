313892d04005f124d4ffb98cad9612fde05c288a
Jonathan <jsanca@Jonathans-MBP-2.dotCMS>
2016-07-12 16:51:28 -0600

    #9284 Added the Web Socket for the System Events

--- src/com/dotcms/web/websocket/SystemEventsWebSocketEndPoint.java
+++ src/com/dotcms/web/websocket/SystemEventsWebSocketEndPoint.java
@@ -1,6 +1,15 @@
 package com.dotcms.web.websocket;
 
+import com.dotcms.api.system.event.SystemEvent;
+import com.dotcms.util.marshal.MarshalFactory;
+import com.dotcms.util.marshal.MarshalUtils;
+
+import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * 
@@ -9,7 +18,55 @@ import javax.websocket.server.ServerEndpoint;
  * @since Jul 11, 2016
  *
  */
-@ServerEndpoint("/system/events")
-public class SystemEventsWebSocketEndPoint {
+@ServerEndpoint(value = "/system/events",
+        encoders = { SystemEventEncoder.class },
+        configurator = DotCmsWebSocketConfigurator.class)
+public class SystemEventsWebSocketEndPoint implements Serializable {
 
-}
+    private final Queue<Session> queue =
+            new ConcurrentLinkedQueue<>();
+
+    @OnOpen
+    public void open(Session session) {
+
+        queue.add(session);
+        //todo: log me System.out.println("New session opened: " + session.getId());
+    }
+
+    @OnError
+    public void error(Session session, Throwable t) {
+        queue.remove(session);
+        //todo: log me System.err.println("Error on session " + session.getId());
+    }
+
+    @OnClose
+    public void closedConnection(Session session) {
+        queue.remove(session);
+        // todo log me System.out.println("session closed: " + session.getId());
+    }
+
+    public void sendSystemEvent(final SystemEvent event) {
+
+        try {
+			/* Send the new rate to all open WebSocket sessions */
+            final ArrayList<Session> closedSessions = new ArrayList<>();
+            for (Session session : queue) {
+                if (!session.isOpen()) {
+                    // todo log me System.err.println("Closed session: " + session.getId());
+
+                    closedSessions.add(session);
+                } else {
+
+                    session.getBasicRemote().sendObject(event);
+                }
+            }
+
+            queue.removeAll(closedSessions);
+            // log me System.out.println("Sending " + msg + " to " + queue.size() + " clients");
+        } catch (Throwable e) {
+
+            // todo log an error
+        }
+    }
+
+} // E:O:F:SystemEventsWebSocketEndPoint.
