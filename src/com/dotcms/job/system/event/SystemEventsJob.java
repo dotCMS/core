8480436bee0de558f43ae19bdb5dcb63868638e0
Jose Castro <jose.castro@dotcms.com>
2016-07-19 12:13:37 -0600

    #9284 : Adding Job delegate classes. Small changes to existing classes.

--- src/com/dotcms/job/system/event/SystemEventsJob.java
+++ src/com/dotcms/job/system/event/SystemEventsJob.java
@@ -1,9 +1,17 @@
 package com.dotcms.job.system.event;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import com.dotcms.web.websocket.JobDelegate;
+import com.dotcms.web.websocket.delegate.SystemEventsJobDelegate;
+import com.dotcms.web.websocket.delegate.bean.JobDelegateDataBean;
 import com.dotmarketing.quartz.DotJob;
+import com.dotmarketing.util.Logger;
 
 /**
  * 
@@ -14,10 +22,37 @@ import com.dotmarketing.quartz.DotJob;
  */
 public class SystemEventsJob extends DotJob {
 
+	private static volatile long lastCallback;
+	private List<JobDelegate> delegates;
+
 	@Override
 	public void run(JobExecutionContext jobContext) throws JobExecutionException {
-		// TODO Auto-generated method stub
+		Logger.info(this, "=======================================================================");
+		Logger.info(this, "==== RUNNING THE SystemEventsJob at " + new Date() + " ====");
+		Logger.info(this, "=======================================================================");
+		
+		List<JobDelegate> delegateList = this.getDelegates();
+		if (delegateList != null && !delegateList.isEmpty()) {
+			if (lastCallback > 0) {
+				for (JobDelegate delegate : delegateList) {
+					JobDelegateDataBean dataBean = new JobDelegateDataBean(jobContext, lastCallback);
+					delegate.execute(dataBean);
+				}
+			}
+			lastCallback = new Date().getTime();
+		}
+	}
 
+	/**
+	 * 
+	 * @return
+	 */
+	private List<JobDelegate> getDelegates() {
+		if (this.delegates == null) {
+			this.delegates = new ArrayList<>();
+			this.delegates.add(new SystemEventsJobDelegate());
+		}
+		return this.delegates;
 	}
 
 }
