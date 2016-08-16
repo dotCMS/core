13c8ca45684e729b86405e80cfdbc9f6c2ea3daa
Freddy Rodriguez <freddy0309@gmail.com>
2016-08-16 12:17:25 -0400

    dotCMS/core#9570 new dropdown-component

---
+++ src/view/components/common/dropdown-component/dropdown-component.ts
@@ -0,0 +1,66 @@
+import {Component, EventEmitter, Input, Output, ViewEncapsulation, ElementRef} from '@angular/core';
+
+
+import { Router } from '@ngrx/router';
+import {LoginService} from '../../../../api/services/login-service';
+
+
+@Component({
+    directives: [],
+    encapsulation: ViewEncapsulation.Emulated,
+    moduleId: __moduleName, // REQUIRED to use relative path in styleUrls
+    pipes: [],
+    providers: [],
+    selector: 'dot-dropdown-component',
+    styleUrls: ['dropdown-component.css'],
+    templateUrl: ['dropdown-component.html'],
+    host: {
+        '(document:click)': 'handleClick($event)',
+    }
+})
+
+/**
+ * Dropdown component
+ *
+ */
+export class DropdownComponent {
+    @Input() disabled:boolean = false;
+    @Input() icon:string = null;
+    @Input() title:string = null;
+    @Input() alignRight:boolean = false;
+
+    @Output() open = new EventEmitter<>();
+    @Output() toggle = new EventEmitter<boolean>();
+    @Output() close = new EventEmitter<>();
+
+    private show:boolean = false;
+
+    constructor (private elementRef: ElementRef){}
+
+    private onToggle():void{
+        this.show = !this.show;
+
+        if (this.show){
+            this.open.emit();
+        }else{
+            this.close.emit();
+        }
+
+        this.toggle.emit( this.show );
+    }
+
+    private handleClick($event) {
+        let clickedComponent = $event.target;
+        let inside = false;
+        do {
+            if (clickedComponent === this.elementRef.nativeElement) {
+                inside = true;
+            }
+            clickedComponent = clickedComponent.parentNode;
+        } while (clickedComponent);
+
+        if (!inside) {
+            this.show = false;
+        }
+    }
+}
--- src/view/components/common/pattern-library/pattern-library.ts
+++ src/view/components/common/pattern-library/pattern-library.ts
@@ -5,10 +5,11 @@ import {MD_INPUT_DIRECTIVES} from '@angular2-material/input/input';
 import {MdCheckbox} from '@angular2-material/checkbox/checkbox';
 import { MdUniqueSelectionDispatcher } from '@angular2-material/core';
 import { MD_RADIO_DIRECTIVES } from '@angular2-material/radio';
+import {DropdownComponent} from "../dropdown-component/dropdown-component";
 
 
 @Component({
-    directives: [MdButton, MD_INPUT_DIRECTIVES, MdCheckbox, MD_RADIO_DIRECTIVES],
+    directives: [MdButton, MD_INPUT_DIRECTIVES, MdCheckbox, MD_RADIO_DIRECTIVES, DropdownComponent],
     encapsulation: ViewEncapsulation.Emulated,
     moduleId: __moduleName, // REQUIRED to use relative path in styleUrls
     providers: [MdUniqueSelectionDispatcher],
@@ -20,5 +21,13 @@ import { MD_RADIO_DIRECTIVES } from '@angular2-material/radio';
 export class PatternLibrary {
     constructor() {
         console.log('PL')
+    }
+
+    public print(message:string, argument:any){
+        if (argument) {
+            console.log(message, argument);
+        }else{
+            console.log(message);
+        }
     }
 }
