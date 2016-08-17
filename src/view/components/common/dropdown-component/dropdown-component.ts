d497bcee02532850ec730b7c62fdfb822847d2bc
Freddy Rodriguez <freddy0309@gmail.com>
2016-08-17 10:29:49 -0400

    add doc

--- src/view/components/common/dropdown-component/dropdown-component.ts
+++ src/view/components/common/dropdown-component/dropdown-component.ts
@@ -20,8 +20,32 @@ import {LoginService} from '../../../../api/services/login-service';
 })
 
 /**
- * Dropdown component
+ * Dropdown component.<br>
  *
+ * It has the follow attributes:<br>
+ *     <ul>
+ *         <li>disabled: disabled if it is true</li>
+ *         <li>icon: icon to show in the dropdown, just allow icon define in angular-material</li>
+ *         <li>title: title to show in the dropdown</li>
+ *         <li>alignRight: if it is true align the content to the right, by default the content is align to the left</li>
+ *      </ul>
+ *
+ * It has the follow events:<br>
+ *     <ul>
+ *         <li>open: fire when the dropdown is opened</li>
+ *         <li>close: fire when the dropdown is closed</li>
+ *         <li>toggle: fire when thw dropdown is closed or opened, if the dropdown is opened the event object is true
+ *         it is false otherwise</li>
+ *      </ul>
+ *
+ * <b>Example:</b>
+ *
+ * <pre>
+ * <dot-dropdown-component title="Dropdowns example"
+ *                          (open)="print('Text Dropdown is open')"
+ *                          (close)="print('Text Dropdown is close')"
+ *                          (toggle)="print('Text Dropdown is', $event)">
+ * </pre>
  */
 export class DropdownComponent {
     @Input() disabled:boolean = false;
