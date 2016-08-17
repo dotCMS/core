2872aae390f9217611af39745e033f19d6dc8599
Freddy Rodriguez <freddy0309@gmail.com>
2016-08-17 13:38:04 -0400

    dotCMS/core#9570 improvement code style

--- src/view/components/common/dropdown-component/dropdown-component.ts
+++ src/view/components/common/dropdown-component/dropdown-component.ts
@@ -1,6 +1,4 @@
 import {Component, EventEmitter, Input, Output, ViewEncapsulation, ElementRef} from '@angular/core';
-
-
 import { Router } from '@ngrx/router';
 import {LoginService} from '../../../../api/services/login-service';
 
@@ -19,34 +17,6 @@ import {LoginService} from '../../../../api/services/login-service';
     }
 })
 
-/**
- * Dropdown component.<br>
- *
- * It has the follow attributes:<br>
- *     <ul>
- *         <li>disabled: disabled if it is true</li>
- *         <li>icon: icon to show in the dropdown, just allow icon define in angular-material</li>
- *         <li>title: title to show in the dropdown</li>
- *         <li>alignRight: if it is true align the content to the right, by default the content is align to the left</li>
- *      </ul>
- *
- * It has the follow events:<br>
- *     <ul>
- *         <li>open: fire when the dropdown is opened</li>
- *         <li>close: fire when the dropdown is closed</li>
- *         <li>toggle: fire when thw dropdown is closed or opened, if the dropdown is opened the event object is true
- *         it is false otherwise</li>
- *      </ul>
- *
- * <b>Example:</b>
- *
- * <pre>
- * <dot-dropdown-component title="Dropdowns example"
- *                          (open)="print('Text Dropdown is open')"
- *                          (close)="print('Text Dropdown is close')"
- *                          (toggle)="print('Text Dropdown is', $event)">
- * </pre>
- */
 export class DropdownComponent {
     @Input() disabled:boolean = false;
     @Input() icon:string = null;
@@ -66,13 +36,14 @@ export class DropdownComponent {
 
         if (this.show){
             this.open.emit();
-        }else{
+        } else {
             this.close.emit();
         }
 
         this.toggle.emit( this.show );
     }
 
+    //TODO: we need doing this globally for all the components that need to detect if the click was outside it.
     private handleClick($event) {
         let clickedComponent = $event.target;
         let inside = false;
