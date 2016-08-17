ac2cce4dbf5b6449e064b005444e1238653fad87
Freddy Montes <freddy@dotcms.com>
2016-08-17 08:05:16 -0600

    dotCMS/core#9431 UI: create angular-material <select> component
    Add ControlValueAccessor
    
    Create disabled state
    
    Add selected state to options
    
    Create change event emit

---
+++ src/view/components/common/dot-select/dot-select.ts
@@ -0,0 +1,150 @@
+import {Component, Input, Output, EventEmitter, ElementRef, ViewChild, forwardRef} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {MdIcon} from '@angular2-material/icon/icon';
+
+@Component({
+    directives: [MdIcon],
+    host: {
+        '(document:click)': 'handleClick($event)',
+    },
+    moduleId: __moduleName,
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => DotSelect),
+            multi: true
+        }
+    ],
+    selector: 'dot-select',
+    styleUrls: ['dot-select.css'],
+    templateUrl: ['dot-select.html']
+})
+export class DotSelect implements ControlValueAccessor {
+    private elementRef;
+    private isHover:boolean = false;
+    private isOpen:boolean = false;
+    private options:Array<DotOption> = [];
+    private selectedText:string;
+
+    propagateChange = (_: any) => {};
+
+    @Input('value') _innerValue;
+    @Input() disabled;
+
+    @Output() change = new EventEmitter();
+
+    @ViewChild('selectOptions') selectOptions: ElementRef;
+    @ViewChild('dotSelect') dotSelect: ElementRef;
+
+    constructor(myElement: ElementRef) {
+        this.elementRef = myElement;
+    }
+
+    set innerValue(val) {
+        this._innerValue = val;
+        this.selectedText = this.options.filter((option) => option.value === val)[0].text || '';
+        this.propagateChange(val);
+    }
+
+    get innerValue() {
+        return this._innerValue;
+    }
+
+    ngOnInit() {
+        this.dotSelect.nativeElement.style.width = this.selectOptions.nativeElement.offsetWidth + 'px';
+        this.disabled = typeof(this.disabled) === 'string' ? true : this.disabled;
+    }
+
+    addOption(option:DotOption):void {
+        this.options.push(option);
+    }
+
+    openClose():void {
+        if (this.disabled) {
+            return;
+        }
+        this.isOpen = !this.isOpen;
+    }
+
+    setValue(value:string):void {
+        this.innerValue = value;
+    }
+
+    setOption(option):void {
+        this.openClose();
+        this.innerValue = option.value;
+
+        this.options.forEach((o) => {
+            o.selected = o.value === option.value;
+        })
+
+        this.change.emit(option);
+    }
+
+    toggleHoverState() {
+        this.isHover = !this.isHover;
+    }
+
+    // Implementing ControlValueAccessor methods
+    writeValue(value: any) {
+        if (value) {
+            this.innerValue = value;
+        }
+    }
+
+    registerOnChange(fn) {
+        this.propagateChange = fn;
+    }
+
+    registerOnTouched() {}
+
+    // TODO: need to make this global because will be use in a lot of places
+    private handleClick($event) {
+        let clickedComponent = $event.target;
+        let inside = false;
+        do {
+            if (clickedComponent === this.elementRef.nativeElement) {
+                inside = true;
+            }
+            clickedComponent = clickedComponent.parentNode;
+        } while (clickedComponent);
+
+        if (!inside) {
+            this.isOpen = false;
+        }
+    }
+
+}
+
+@Component({
+    moduleId: __moduleName,
+    selector: 'dot-option',
+    templateUrl: ['dot-option.html'],
+    styleUrls: ['dot-option.css'],
+    host: {
+        '(click)': 'onClick($event)',
+    }
+})
+
+export class DotOption {
+    @Input() value:string;
+    @Input() selected:boolean;
+    public node:ElementRef;
+    public text:string;
+
+    constructor(private select: DotSelect, myElement: ElementRef) {
+        this.node = myElement;
+    }
+
+    ngOnInit() {
+        this.select.addOption(this);
+        this.text = this.node.nativeElement.firstChild.innerHTML.trim();
+    }
+
+    onClick() {
+        this.select.setOption({
+            value: this.value,
+            text: this.node.nativeElement.firstChild.innerHTML.trim()
+        });
+    }
+}
--- src/view/components/common/pattern-library/pattern-library.ts
+++ src/view/components/common/pattern-library/pattern-library.ts
@@ -1,15 +1,15 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 
-import {MdButton} from '@angular2-material/button';
-import {MD_INPUT_DIRECTIVES} from '@angular2-material/input/input';
-import {MdCheckbox} from '@angular2-material/checkbox/checkbox';
-import { MdUniqueSelectionDispatcher } from '@angular2-material/core';
-import { MD_RADIO_DIRECTIVES } from '@angular2-material/radio';
+import {DotSelect, DotOption} from '../dot-select/dot-select';
 import {DropdownComponent} from "../dropdown-component/dropdown-component";
-
+import {MD_INPUT_DIRECTIVES} from '@angular2-material/input/input';
+import {MD_RADIO_DIRECTIVES} from '@angular2-material/radio';
+import {MdButton} from '@angular2-material/button';
+import {MdCheckbox} from '@angular2-material/checkbox/checkbox';
+import {MdUniqueSelectionDispatcher} from '@angular2-material/core';
 
 @Component({
-    directives: [MdButton, MD_INPUT_DIRECTIVES, MdCheckbox, MD_RADIO_DIRECTIVES, DropdownComponent],
+    directives: [MdButton, MD_INPUT_DIRECTIVES, MdCheckbox, MD_RADIO_DIRECTIVES, DropdownComponent, DotSelect, DotOption],
     encapsulation: ViewEncapsulation.Emulated,
     moduleId: __moduleName, // REQUIRED to use relative path in styleUrls
     providers: [MdUniqueSelectionDispatcher],
