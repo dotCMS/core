466bb0603b282bfd4624456e71aa5ae7c218b076
Will Ezell <will@dotcms.com>
2016-08-24 16:34:22 -0400

    #9272 db testing changes forgot this commit

--- src/com/dotmarketing/db/test/DataSourcesForTesting.java
+++ src/com/dotmarketing/db/test/DataSourcesForTesting.java
@@ -3,6 +3,7 @@ package com.dotmarketing.db.test;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
@@ -11,103 +12,113 @@ import javax.sql.DataSource;
 
 import com.dotcms.repackage.org.apache.commons.dbcp.BasicDataSource;
 import com.dotmarketing.business.DotStateException;
-import com.dotmarketing.db.DbConnectionFactory;
 import com.dotmarketing.viewtools.XmlTool;
 import com.liferay.util.FileUtil;
 
-public class DataSourceForTesting {
+public class DataSourcesForTesting {
+
+   final File contextFile;
+
+   public DataSourcesForTesting(File useMe) {
+      this.contextFile = useMe;
+   }
+
+   public DataSourcesForTesting() {
+      this.contextFile = findContextFile();
+   }
+
+   public List<DataSource> dataSources() throws FileNotFoundException, Exception {
 
 
-	public void setup() throws FileNotFoundException, Exception{
-		
-		if(DbConnectionFactory.ready()){
-			return ;
-		}
+      List<DataSource> sources = new ArrayList<DataSource>();
 
-		
-		XmlTool xml = new XmlTool().parse(getContextAsString());
-		xml = xml.children().find("Resource");
-		Iterator<XmlTool> it = xml.iterator();
-		XmlTool source = new XmlTool();
-		while (it.hasNext()) {
-			XmlTool tool = it.next();
-			if (tool.attr("name").equals("jdbc/dotCMSPool")) {
-				source = tool;
-				break;
-			}
-		}
-		String driver = source.attr("driverClassName");
-		String url = source.attr("url");
-		String username = source.attr("username");
-		String password = source.attr("password");
-		
+      XmlTool xml = new XmlTool().parse(getContextAsString());
+      xml = xml.children().find("Resource");
+      Iterator<XmlTool> it = xml.iterator();
+      while (it.hasNext()) {
+         XmlTool tool = it.next();
+         if (tool.attr("name").equals("jdbc/dotCMSPool")) {
+            String driver = tool.attr("driverClassName");
+            String url = tool.attr("url");
+            String username = tool.attr("username");
+            String password = tool.attr("password");
+            sources.add(buildDS(driver, url, username, password));
+         }
+      }
 
-		System.out.println("Building test datasource : " +url );
-		
-		
+      return sources;
 
-		DbConnectionFactory.overrideDefaultDatasource(getDataSource(driver, url, username, password));
-		
-	}
-	
+   }
 
-	
-	private static DataSource getDataSource(String driver, String url, String username, String password) throws Exception {
-		BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(driver);
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        dataSource.setMaxActive(100);
-        return dataSource;
-	}
 
-	private String getContextAsString() throws FileNotFoundException{
-		FileFilter ff = new FileFilter() {
-			@Override
-			public boolean accept(File pathname) {
-				return ("context.xml".equals(pathname.getName()) || pathname.isDirectory());
-			}
-		};
 
-		File root = findProjectRoot(new File("."), 0);
-		File context = null;
-		List<File> files = FileUtil.listFilesRecursively(root, ff);
-		for(File f: files){
-			if(f.isFile()){
-				context=f;
-				break;
-			}
-		}
-		if(context==null){
-			throw new DotStateException("Unable to find the context.xml");
-		}
-		System.out.println("Using context file:" + context.getAbsolutePath());
-		try (Scanner scan = new Scanner(context)){
-			return scan.useDelimiter("\\Z").next();
-		}
+   private static DataSource buildDS(String driver, String url, String username,
+         String password) throws Exception {
+      BasicDataSource dataSource = new BasicDataSource();
+      dataSource.setDriverClassName(driver);
+      dataSource.setUrl(url);
+      dataSource.setUsername(username);
+      dataSource.setPassword(password);
+      dataSource.setMaxActive(100);
+      return dataSource;
+   }
 
-		
-	}
-	
-	
-	private File findProjectRoot(File start, int i){
-		File[] files = start.listFiles();
-		for(File file : files){
-			if(file.isDirectory() && "META-INF".equals(file.getName())){
-				return file;
-			}
-			if(file.isDirectory() && "extra".equals(file.getName())){
-				return file;
-			}
-		}
-		File parent = start.getParentFile();
-		if(parent ==null || parent.equals(start) || i > 500){
-			throw new DotStateException("Unable to find the directory root");
-		}
-		return findProjectRoot(start.getParentFile(), ++i);
-	}
-	
-	
-	
+   private File findContextFile() {
+
+      FileFilter ff = new FileFilter() {
+         @Override
+         public boolean accept(File pathname) {
+            return ("context.xml".equals(pathname.getName()) || pathname.isDirectory());
+         }
+      };
+
+      File root = findProjectRoot(new File("."), 0);
+
+      List<File> files;
+      try {
+         files = FileUtil.listFilesRecursively(root, ff);
+
+         for (File f : files) {
+            if (f.isFile()) {
+               return f;
+            }
+         }
+      } catch (FileNotFoundException e) {
+         throw new DotStateException("cannot find context.xml");
+      }
+      throw new DotStateException("cannot find context.xml");
+   }
+
+
+   private String getContextAsString() throws FileNotFoundException {
+
+
+      System.out.println("Using context file:" + this.contextFile.getAbsolutePath());
+      try (Scanner scan = new Scanner(this.contextFile)) {
+         return scan.useDelimiter("\\Z").next();
+      }
+
+
+   }
+
+
+   private File findProjectRoot(File start, int i) {
+      File[] files = start.listFiles();
+      for (File file : files) {
+         if (file.isDirectory() && "META-INF".equals(file.getName())) {
+            return file;
+         }
+         if (file.isDirectory() && "extra".equals(file.getName())) {
+            return file;
+         }
+      }
+      File parent = start.getParentFile();
+      if (parent == null || parent.equals(start) || i > 500) {
+         throw new DotStateException("Unable to find the directory root");
+      }
+      return findProjectRoot(start.getParentFile(), ++i);
+   }
+
+
+
 }
