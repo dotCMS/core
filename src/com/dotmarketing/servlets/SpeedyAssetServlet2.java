55476ef27744d9ade90bec2df4c946965360be68
Will Ezell <will@dotcms.com>
2016-09-19 16:48:37 -0400

    #9800 added api/cache/unit test

--- src/com/dotmarketing/servlets/SpeedyAssetServlet2.java
+++ src/com/dotmarketing/servlets/SpeedyAssetServlet2.java
@@ -1,7 +1,9 @@
 package com.dotmarketing.servlets;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import java.util.StringTokenizer;
 
 import javax.servlet.ServletException;
@@ -10,15 +12,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.dotcms.uuid.shorty.ShortType;
+import com.dotcms.uuid.shorty.ShortyId;
+import com.dotcms.uuid.shorty.ShortyIdApiImpl;
+
 import com.dotmarketing.beans.Host;
 import com.dotmarketing.business.APILocator;
+import com.dotmarketing.business.CacheLocator;
+import com.dotmarketing.business.DotStateException;
 import com.dotmarketing.business.VersionableAPI;
 import com.dotmarketing.business.web.WebAPILocator;
+import com.dotmarketing.cache.ContentTypeCache;
+import com.dotmarketing.cache.FieldsCache;
+import com.dotmarketing.db.DbConnectionFactory;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.portlets.contentlet.business.ContentletAPI;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.portlets.contentlet.model.ContentletVersionInfo;
+import com.dotmarketing.portlets.languagesmanager.model.Language;
+import com.dotmarketing.portlets.structure.factories.StructureFactory;
 import com.dotmarketing.portlets.structure.model.Field;
 import com.dotmarketing.portlets.structure.model.Structure;
 import com.dotmarketing.util.Config;
@@ -29,134 +42,114 @@ import com.liferay.portal.model.User;
 
 public class SpeedyAssetServlet2 extends HttpServlet {
 
-   private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+
+    final static String NOT_FOUND="NOT_FOUND";
+
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            _serve(request, response);
+        } catch (Throwable t) {
+            throw new ServletException(t);
+        } finally {
+            DbConnectionFactory.closeSilently();
+        }
+    }
+
+
+    private void _serve(final HttpServletRequest request, final HttpServletResponse response)
+            throws Exception {
+
+        Host host = WebAPILocator.getHostWebAPI().getCurrentHost(request);
+
+
+        HttpSession session = request.getSession(false);
+        boolean ADMIN_MODE = PageRequestModeUtil.isAdminMode(session);
+        boolean PREVIEW_MODE = PageRequestModeUtil.isPreviewMode(session);
+        boolean EDIT_MODE = PageRequestModeUtil.isEditMode(session);
+
+
+        // Checking if host is active
+        if (!ADMIN_MODE && !APILocator.getVersionableAPI().hasLiveVersion(host)) {
+            response.sendError(HttpServletResponse.SC_SERVICE_UNAVAILABLE,
+                    LanguageUtil.get("server-unavailable-error-message"));
+            return;
+        }
 
 
 
-   protected void service(HttpServletRequest request, HttpServletResponse response)
-         throws ServletException, IOException {
-      try {
-         _serve(request, response);
-      } catch (Throwable t) {
-         throw new ServletException(t);
-      } finally {
-         UtilMethods.closeDbSilently();
-      }
-   }
-
-
-   private void _serve(final HttpServletRequest request, final HttpServletResponse response)
-         throws Exception {
-
-      Host host = WebAPILocator.getHostWebAPI().getCurrentHost(request);
-
-
-      HttpSession session = request.getSession(false);
-      boolean ADMIN_MODE = PageRequestModeUtil.isAdminMode(session);
-      boolean PREVIEW_MODE = PageRequestModeUtil.isPreviewMode(session);
-      boolean EDIT_MODE = PageRequestModeUtil.isEditMode(session);
-
-
-      // Checking if host is active
-      if (!ADMIN_MODE && !APILocator.getVersionableAPI().hasLiveVersion(host)) {
-         response.sendError(HttpServletResponse.SC_SERVICE_UNAVAILABLE,
-               LanguageUtil.get("server-unavailable-error-message"));
-         return;
-      }
-
-      User user = WebAPILocator.getUserWebAPI().getLoggedInFrontendUser(request);
-
-      boolean live = (user != null) ? (EDIT_MODE || PREVIEW_MODE) ? false : true : true;
+        StringTokenizer tokens = new StringTokenizer(request.getRequestURI(), "/");
+        if (tokens.countTokens() < 2) {
+            response.sendError(404);
+            return;
+        }
+        tokens.nextToken();
+        String id = tokens.nextToken();
+        String fieldName = tokens.hasMoreTokens() ? tokens.nextToken() : NOT_FOUND;
 
 
 
-      StringTokenizer tokens = new StringTokenizer(request.getRequestURI(), "/");
-      if (tokens.countTokens() < 2) {
-         response.sendError(404);
-         return;
-      }
-      tokens.nextToken();
-      String id = tokens.nextToken();
+        Optional<ShortyId> shortOpt = new ShortyIdApiImpl().getShorty(id);
+        User user = WebAPILocator.getUserWebAPI().getLoggedInFrontendUser(request);
+        boolean live = (user != null) ? (EDIT_MODE || PREVIEW_MODE) ? false : true : true;
+        if (!live) {
+            response.setHeader("Pragma", "no-cache");
+            response.setHeader("Cache-Control", "no-cache");
+            response.setDateHeader("Expires", 0);
+        }
 
-      long languageId = WebAPILocator.getLanguageWebAPI().getLanguage(request).getId();
-
-
-      Contentlet con = resolveContentlet(id, languageId, live, user);
-      if (con == null) {
-         response.sendError(404);
-         return;
-      }
-
+        if (!shortOpt.isPresent()) {
+            response.sendError(404);
+            return;
+        }
+        ShortyId shorty = shortOpt.get();
+        if (shorty.subType == ShortType.CONTENTLET) {
 
 
-      if (!live) {
-         response.setHeader("Pragma", "no-cache");
-         response.setHeader("Cache-Control", "no-cache");
-         response.setDateHeader("Expires", 0);
-      }
+            if (shorty.type == ShortType.IDENTIFIER) {
+                Contentlet con = APILocator.getContentletAPI().findContentletByIdentifier(
+                        shorty.longId, false, -1, APILocator.getUserAPI().getSystemUser(), false);
+
+                String field = resolveField(con, fieldName);
 
 
+                request.getRequestDispatcher(
+                        "/contentAsset/raw-data/" + shorty.longId + "/" + field)
+                        .forward(request, response);
+            } else {
+                Contentlet con = APILocator.getContentletAPI().find(shorty.longId,
+                        APILocator.getUserAPI().getSystemUser(), false);
 
-      request.getRequestDispatcher(
-            "/contentAsset/raw-data/" + con.getInode() + "/" + resolveField(con) + "/byInode/true")
-            .forward(request, response);
+                String field = resolveField(con, fieldName);
 
-
-
-   }
-
-
-   private Contentlet resolveContentlet(String id, long lang, boolean live, User user)
-         throws DotDataException, DotSecurityException {
-
-      long defualtLang = APILocator.getLanguageAPI().getDefaultLanguage().getId();
-      ContentletAPI capi = APILocator.getContentletAPI();
-      VersionableAPI vapi = APILocator.getVersionableAPI();
-      Contentlet con = null;
-
-      // if we have a shorty, use the index
-      if (id.length() == 8) {
-         StringBuilder query =
-               new StringBuilder("+(identifier:").append(id).append("* inode:").append(id)
-                     .append("*) ");
-
-
-         if (live) {
-            query.append("+live:true ");
-         } else {
-            query.append("+working:true ");
-         }
-         query.append("languageId:").append(lang).append("^10 ");
-         query.append("languageId:").append(defualtLang);
-         List<Contentlet> cons = capi.search(query.toString(), 1, 0, "score", user, true);
-         con = (cons.size() > 0) ? cons.get(0) : con;
-      } else {
-         ContentletVersionInfo cvi = vapi.getContentletVersionInfo(id, lang);
-         if (cvi == null && Config.getBooleanProperty("DEFAULT_FILE_TO_DEFAULT_LANGUAGE", false)
-               && lang != defualtLang) {
-            cvi = vapi.getContentletVersionInfo(id, defualtLang);
-
-         }
-         if (cvi != null) {
-            id = (live) ? cvi.getLiveInode() : cvi.getWorkingInode();
-         }
-         con = capi.find(id, user, true);
-
-      }
-      return con;
-   }
-
-   private String resolveField(Contentlet con) {
-      String field = "fileAsset";
-      if (con.getStructure().getStructureType() != Structure.STRUCTURE_TYPE_FILEASSET) {
-         for (Field f : con.getStructure().getFields()) {
-            if ("binary".equals(f.getFieldType())) {
-               field = f.getVelocityVarName();
-               break;
+                request.getRequestDispatcher(
+                        "/contentAsset/raw-data/" + shorty.longId + "/" + field + "/byInode/true")
+                        .forward(request, response);
             }
-         }
-      }
-      return field;
-   }
+        }
+
+    }
+
+
+
+    private String resolveField(Contentlet con, final String tryField) {
+        if(!NOT_FOUND.equals(tryField)){
+            Object obj = con.getMap().get(tryField);
+            if (obj instanceof File) {
+                return tryField;
+            }
+           return NOT_FOUND;
+        }
+
+
+        for (Field f : FieldsCache.getFieldsByStructureInode(con.getStructureInode())) {
+            if ("binary".equals(f.getFieldType())) {
+                return f.getVelocityVarName();
+            }
+        }
+
+        return tryField;
+    }
 
 }
---
+++ test/com/dotcms/uuid/shorty/TestShortyIdApi.java
@@ -0,0 +1,228 @@
+package com.dotcms.uuid.shorty;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.dotcms.repackage.com.google.common.collect.ImmutableList;
+import com.dotmarketing.business.APILocator;
+import com.dotmarketing.business.CacheLocator;
+import com.dotmarketing.db.test.TestingJndiDatasource;
+import com.dotmarketing.util.ConfigTestHelper;
+
+public class TestShortyIdApi {
+
+    List<String[]> expecteds = ImmutableList.<String[]>builder()
+            .add(new String[] {"f6b80fab-8671-4764-af0d-79b716725ffd", "inode", "containers"})
+            .add(new String[] {"fc193c82-8c32-4abe-ba8a-49522328c93e", "identifier", "containers"})
+
+            
+            .add(new String[] {"ff9d9f72-3650-4bca-9e0f-6ce18150d51e", "identifier", "contentlet"})
+            .add(new String[] {"ffa0b494-cbb2-4634-b747-8795b5995d74", "identifier", "folder"})
+            .add(  new String[] {"1aeb328b-57b5-46e8-9eec-2cb2b4c24953", "identifier", "htmlpage"})
+            .add( new String[] {"b12f30da-0f0c-4376-88a0-a17d1ffe39f9", "identifier", "links"})
+            .add( new String[] {"fdb3f906-e9c4-46c4-b7e4-148201271d04", "identifier", "template"})
+
+
+
+            .add(new String[] {"ffe266d8-0683-45f3-9b08-3d7804ae3280", "inode", "contentlet"})
+            .add(new String[] {"975964f8-ef1f-451a-aa23-10a44c39ce98", "inode", "htmlpage"})
+            .add(new String[] {"fe654925-f011-487c-b5db-d7cb4ed2553a", "inode", "template"}) 
+
+            .add(new String[] {"ede13663-ff06-4d01-ab99-a8976e995010", "inode", "category"})
+            .add(new String[] {"fff86d86-7908-4922-aaed-a3e8b0c6aae4", "inode", "field"})
+            .add(new String[] {"008dab22-8bc3-4eb2-93a0-79e3d5d0a4ab", "inode", "folder"})
+            .add(new String[] {"b1bb57cf-6f98-486d-a3b9-83f96d3dab22", "inode", "links"})
+            .add(new String[] {"a2446e0c-0e8f-4308-8b17-9d0e907aaff5", "inode", "relationship"})
+            .add(new String[] {"868c07ca-c2af-4668-8fe8-75d5b48bd6a5", "inode", "virtual_link"})
+            
+            
+            
+            
+            
+            
+            
+            
+            .build();
+
+    /**
+     * | fc193c82-8c32-4abe-ba8a-49522328c93e | containers | | ff9d9f72-3650-4bca-9e0f-6ce18150d51e
+     * | contentlet | | ffa0b494-cbb2-4634-b747-8795b5995d74 | folder | |
+     * 1aeb328b-57b5-46e8-9eec-2cb2b4c24953 | htmlpage | | b12f30da-0f0c-4376-88a0-a17d1ffe39f9 |
+     * links | | fdb3f906-e9c4-46c4-b7e4-148201271d04 | template | |
+     * fba1b937-5c06-40a0-94c6-a830425d3875 | identifer |
+     **/
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        new TestingJndiDatasource().init();
+        ConfigTestHelper._setupFakeTestingContext();
+        APILocator.getBundleAPI();
+        CacheLocator.init();
+        
+    }
+
+    @Before
+    public void setUp() throws Exception {}
+
+    @Test
+    public void testShortyLookup() {
+        ShortyId shorty = null;
+        ShortType shortType = null;
+        ShortType shortSubType = null;
+        String[] val =null;
+        // run 10 times
+        for(int i=0;i<10;i++){
+            try {
+                for (String[] values :  expecteds){
+                    val = values;
+                    String key = val[0].substring(0, 8);
+                    shortType = ShortType.fromString(val[1]);
+                    shortSubType = ShortType.fromString(val[2]);
+                    Optional<ShortyId> opt = new ShortyIdApiImpl().getShorty(key);
+                    shorty = opt.get();
+                    
+                    assert (shorty.longId.equals(val[0]));
+                    assert (shortType == shorty.type);
+                    assert (shortSubType == shorty.subType);
+                }
+            } catch (Throwable t) {
+                t.printStackTrace();
+                System.out.println("val[0]:" + val[0]);
+                System.out.println("val[1]:" + val[1]);
+                System.out.println("val[2]:" + val[2]);
+                
+                
+                System.out.println("bad shorty:" + shorty);
+                System.out.println("looking shortType:" + shortType);
+                System.out.println("looking shortSubType:" + shortSubType);
+                throw t;
+            }
+        }
+
+
+    }
+    
+    @Test
+    public void testShortyCache() {
+        ShortyIdApiImpl api = new ShortyIdApiImpl();
+
+        String[] val =null;
+
+        // load cache
+        for (String[] values :  expecteds){
+            val = values;
+            String key = val[0].substring(0, 8);
+            Optional<ShortyId> opt = api.getShorty(key);
+        }
+
+        long dbQueries =api.dbHits;
+
+        for(int i=0;i<10;i++){
+
+            for (String[] values :  expecteds){
+                val = values;
+                String key = val[0].substring(0, 8);
+                Optional<ShortyId> opt = api.getShorty(key);
+            }
+        }
+        
+        // test that we are loading from cache
+        long dbQueries2 = api.getDbHits();
+        assert (dbQueries2 == dbQueries );
+        
+        
+        CacheLocator.getCacheAdministrator().flushAll();
+        
+        // load cache
+        for (String[] values :  expecteds){
+            val = values;
+            String key = val[0].substring(0, 8);
+            Optional<ShortyId> opt = api.getShorty(key);
+        }
+
+        
+        long dbQueries3 = api.getDbHits();
+        assert (dbQueries3 == dbQueries2 + expecteds.size() );
+    }
+    
+    
+    
+    @Test
+    public void testValidShorty() {
+        
+        
+        String[] invalids = new String[]{"!", ":",";","\"","'", "*", "_",null};
+        String[] valids = new String[]{"-", "a","4","9","A", "Z"};
+        ShortyIdApiImpl api = new ShortyIdApiImpl();
+
+        
+        for(String x : invalids){
+            try{
+                api.validShorty(x);
+                assert(false);
+            }
+            catch(Exception e){
+                if(e instanceof ShortyException){
+                    assert(true);
+                }else{
+                    assert(false);
+                }
+                
+            }
+        }
+        for(String x : valids){
+            try{
+                api.validShorty(x);
+                assert(true);
+            }
+            catch(Exception e){
+                assert(false);
+            }
+        }
+        
+        
+    }
+    
+    @Test
+    public void test404Cache() {
+        ShortyIdApiImpl api = new ShortyIdApiImpl();
+
+        String[] fourOhFours ={"asdas", "QEFQF","SFADSF","ZASDSDSAD","11111"};
+
+        // load cache
+        for (String key :  fourOhFours){
+            Optional<ShortyId> opt = api.getShorty(key);
+        }
+
+        long dbQueries =api.dbHits;
+
+        for(int i=0;i<10;i++){
+
+            for (String key :  fourOhFours){
+                Optional<ShortyId> opt = api.getShorty(key);
+            }
+        }
+        
+        // test that we are loading from cache
+        long dbQueries2 = api.getDbHits();
+        assert (dbQueries2 == dbQueries );
+        
+        
+        CacheLocator.getCacheAdministrator().flushAll();
+        
+        for (String key :  fourOhFours){
+            Optional<ShortyId> opt = api.getShorty(key);
+        }
+
+        
+        long dbQueries3 = api.getDbHits();
+        assert (dbQueries3 == dbQueries2 + fourOhFours.length );
+        
+        
+        
+    }
+}
