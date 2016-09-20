26fc6a94099c3555e12c462123fc42c26fa4202f
Will Ezell <will@dotcms.com>
2016-09-20 10:56:03 -0400

    #9800 oops, here is the actual code

---
+++ src/com/dotcms/uuid/shorty/ShortyIdSql.java
@@ -0,0 +1,21 @@
+package com.dotcms.uuid.shorty;
+
+import com.dotmarketing.db.DbConnectionFactory;
+
+public class ShortyIdSql {
+
+    static protected ShortyIdSql getInstance() {
+        if (DbConnectionFactory.isMsSql()) {
+            return new ShortyIdMSSql();
+        } 
+        else{
+            return new ShortyIdSql();
+        }
+    }
+
+
+    static String SELECT_SHORTY_SQL="select inode as id, 'inode' as type, type as subtype from inode where inode like ? union select id,'identifier', asset_type from identifier where id like ? limit 1";
+    
+    
+
+}
