e04cd7b2970e33da944f2e53420e6e34822c5fcd
Oswaldo Gallango <oswaldo@dotcms.com>
2016-09-22 08:52:46 -0400

    dotCMS/core#9808 updated with pull request comments

--- src/view/components/common/login/reset-password-component/reset-password-container.ts
+++ src/view/components/common/login/reset-password-component/reset-password-container.ts
@@ -19,24 +19,24 @@ import {ResetPasswordComponent} from './reset-password-component';
         </dot-reset-password-component>
     `
 })
-export class ResetPasswordContainer{
+export class ResetPasswordContainer {
 
-    private message:string = '';
-    private login:string = '';
-    private token:string = '';
+    private message: string = '';
+    private login: string = '';
+    private token: string = '';
 
-    private changePasswordSuccessfully:string;
+    private changePasswordSuccessfully: string;
 
     constructor( private loginService: LoginService, private router: Router,
                  private queryParams: QueryParams, private routeParams: RouteParams) {
 
     }
 
-    ngOnInit(){
+    ngOnInit() {
          this.queryParams.pluck<string>('token').distinctUntilChanged()
             .forEach(token => this.token = token);
 
-        this.loginService.getLoginFormInfo('', ['message.forgot.password.password.updated']).subscribe((data) => {
+         this.loginService.getLoginFormInfo('', ['message.forgot.password.password.updated']).subscribe((data) => {
             let dataI18n = data.i18nMessagesMap;
             let entity = data.entity;
 
@@ -46,7 +46,7 @@ export class ResetPasswordContainer{
         });
     }
 
-    public changePassword(changePasswordData:ChangePasswordData):void{
+    public changePassword(changePasswordData: ChangePasswordData): void {
         this.cleanMessage();
         this.loginService.changePassword(changePasswordData.password, changePasswordData.token)
             .subscribe( result =>{
@@ -59,16 +59,16 @@ export class ResetPasswordContainer{
             });
     }
 
-    private goToLogin():void{
+    private goToLogin(): void {
         this.router.go('/public/login');
     }
 
-    private cleanMessage():void{
+    private cleanMessage(): void {
         this.message = '';
     }
 }
 
 export interface ChangePasswordData{
-    token:string,
-    password:string
+    token: string,
+    password: string
 }
