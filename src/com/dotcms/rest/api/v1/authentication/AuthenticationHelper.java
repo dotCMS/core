5e22121281bf2df6b585c398ff747601a82c1841
Oswaldo Gallango <oswaldo@dotcms.com>
2016-09-22 08:57:50 -0400

    #9808 cleaning unnecessary imports

--- src/com/dotcms/rest/api/v1/authentication/AuthenticationHelper.java
+++ src/com/dotcms/rest/api/v1/authentication/AuthenticationHelper.java
@@ -1,21 +1,18 @@
 package com.dotcms.rest.api.v1.authentication;
 
-import com.dotcms.exception.InternationalizationExceptionSupport;
-import com.dotcms.repackage.javax.ws.rs.core.Response;
-import com.dotcms.rest.ErrorResponseHelper;
-import com.dotmarketing.util.Logger;
-import com.dotmarketing.util.SecurityLogger;
-import com.dotmarketing.util.UtilMethods;
-import com.liferay.portal.language.LanguageException;
-import com.liferay.portal.language.LanguageUtil;
-import com.liferay.portal.model.User;
-
-import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
 import java.text.MessageFormat;
 import java.util.Locale;
 
-import static com.dotcms.util.CollectionsUtils.mapEntries;
+import javax.servlet.http.HttpServletRequest;
+
+import com.dotcms.repackage.javax.ws.rs.core.Response;
+import com.dotcms.rest.ErrorResponseHelper;
+import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.UtilMethods;
+import com.liferay.portal.language.LanguageException;
+import com.liferay.portal.language.LanguageUtil;
+import com.liferay.portal.model.User;
 
 /**
  * Just a helper to encapsulate AuthenticationResource functionality.
@@ -23,34 +20,34 @@ import static com.dotcms.util.CollectionsUtils.mapEntries;
  */
 public class AuthenticationHelper implements Serializable {
 
-    public static AuthenticationHelper INSTANCE =
-            new AuthenticationHelper();
+	public static AuthenticationHelper INSTANCE =
+			new AuthenticationHelper();
 
 
-    private AuthenticationHelper () {
+	private AuthenticationHelper () {
 
-    }
+	}
 
-    /**
-     * Get Error response based on a status and message key
-     * This support is a single message
-     *
-     * @param request
-     * @param status
-     * @param userId
-     * @param messageKey
-     * @return Response
-     */
-    public Response getErrorResponse(final HttpServletRequest request,
-                                     final Response.Status status,
-                                     final Locale locale,
-                                     final String userId,
-                                     final String messageKey) {
+	/**
+	 * Get Error response based on a status and message key
+	 * This support is a single message
+	 *
+	 * @param request
+	 * @param status
+	 * @param userId
+	 * @param messageKey
+	 * @return Response
+	 */
+	public Response getErrorResponse(final HttpServletRequest request,
+			final Response.Status status,
+			final Locale locale,
+			final String userId,
+			final String messageKey) {
 
-        return ErrorResponseHelper.INSTANCE.getErrorResponse(status, locale, messageKey);
-    }
-    
-   
+		return ErrorResponseHelper.INSTANCE.getErrorResponse(status, locale, messageKey);
+	}
+
+
 	/**
 	 * Get the translation of the message key in the specified locale, if the locale is null
 	 * then the message is translated into the default user language
@@ -66,7 +63,7 @@ public class AuthenticationHelper implements Serializable {
 					LanguageUtil.get(locale, messageKey):
 						LanguageUtil.get((User)null, messageKey);
 
-			return MessageFormat.format(message, messageArguments);
+					return MessageFormat.format(message, messageArguments);
 		} catch (LanguageException e) {
 			Logger.error(AuthenticationHelper.class, e.getMessage(), e);
 			throw new RuntimeException(e);
