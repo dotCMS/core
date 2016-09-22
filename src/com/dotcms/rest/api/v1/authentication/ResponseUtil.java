0ddf8adf6dce3eee21a6ad85de2cfc1137ba4826
Jonathan Gamba <jonathan.gamba@dotcms.com>
2016-09-22 10:47:48 -0600

    Merge remote-tracking branch 'remotes/origin/master-3.7' into issue-9721-DotConfig-User-and-i18n
    # Conflicts:
    #	src/com/dotcms/rest/annotation/RequestFilter.java
    #  src/com/dotcms/rest/api/v1/authentication/AuthenticationHelper.java
    #  src/com/dotcms/rest/api/v1/authentication/ResetPasswordResource.java

--- src/com/dotcms/rest/api/v1/authentication/ResponseUtil.java
+++ src/com/dotcms/rest/api/v1/authentication/ResponseUtil.java
@@ -2,12 +2,16 @@ package com.dotcms.rest.api.v1.authentication;
 
 import com.dotcms.repackage.javax.ws.rs.core.Response;
 import com.dotcms.rest.ErrorResponseHelper;
+import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.UtilMethods;
+import com.liferay.portal.language.LanguageException;
+import com.liferay.portal.language.LanguageUtil;
+import com.liferay.portal.model.User;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
+import java.text.MessageFormat;
 import java.util.Locale;
-
-import static com.dotcms.util.CollectionsUtils.mapEntries;
 
 /**
  * Just a helper to encapsulate AuthenticationResource functionality.
@@ -40,6 +44,29 @@ public class ResponseUtil implements Serializable {
                                      final String messageKey) {
 
         return ErrorResponseHelper.INSTANCE.getErrorResponse(status, locale, messageKey);
+    }
+
+    /**
+     * Get the translation of the message key in the specified locale, if the locale is null
+     * then the message is translated into the default user language
+     *
+     * @param locale           Current user language
+     * @param messageKey       Message key to be translated
+     * @param messageArguments (Optional) if the message require some argument
+     * @return
+     */
+    public String getFormattedMessage(Locale locale, String messageKey, Object... messageArguments) {
+        String message;
+        try {
+            message = (UtilMethods.isSet(locale)) ?
+                    LanguageUtil.get(locale, messageKey) :
+                    LanguageUtil.get((User) null, messageKey);
+
+            return MessageFormat.format(message, messageArguments);
+        } catch (LanguageException e) {
+            Logger.error(AuthenticationHelper.class, e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
     }
 
 } // E:O:F:ResponseUtil.
