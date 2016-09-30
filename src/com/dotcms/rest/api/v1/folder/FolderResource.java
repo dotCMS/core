b490e907505b2f5d351d20ea08928393fd9f1a9b
Jason Tesser <jasontesser@gmail.com>
2016-09-30 10:18:00 -0400

    #9847 folder resource commit

--- src/com/dotcms/rest/api/v1/folder/FolderResource.java
+++ src/com/dotcms/rest/api/v1/folder/FolderResource.java
@@ -1,10 +1,8 @@
-package com.dotcms.rest.api.v1.browsertree;
+package com.dotcms.rest.api.v1.folder;
 
 import com.dotcms.repackage.com.google.common.annotations.VisibleForTesting;
-import com.dotcms.repackage.javax.ws.rs.GET;
-import com.dotcms.repackage.javax.ws.rs.Path;
-import com.dotcms.repackage.javax.ws.rs.PathParam;
-import com.dotcms.repackage.javax.ws.rs.Produces;
+import com.dotcms.repackage.edu.emory.mathcs.backport.java.util.Arrays;
+import com.dotcms.repackage.javax.ws.rs.*;
 import com.dotcms.repackage.javax.ws.rs.core.Context;
 import com.dotcms.repackage.javax.ws.rs.core.MediaType;
 import com.dotcms.repackage.javax.ws.rs.core.Response;
@@ -17,13 +15,11 @@ import com.dotcms.rest.exception.mapper.ExceptionMapperUtil;
 import com.dotcms.util.I18NUtil;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.business.LayoutAPI;
-import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotRuntimeException;
-import com.dotmarketing.exception.DotSecurityException;
+import com.dotmarketing.portlets.folders.model.Folder;
 import com.dotmarketing.util.Logger;
 import com.liferay.portal.model.User;
 import com.liferay.util.LocaleUtil;
-import static com.dotcms.util.CollectionsUtils.map;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
@@ -32,74 +28,74 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static com.dotcms.util.CollectionsUtils.map;
+
 /**
  * Created by jasontesser on 9/28/16.
  */
-@Path("/v1/browsertree")
-public class BrowserTreeResource implements Serializable {
+@Path("/v1/folder")
+public class FolderResource implements Serializable {
     private final WebResource webResource;
-    private final BrowserTreeHelper browserTreeHelper;
+    private final FolderHelper folderHelper;
     private final LayoutAPI layoutAPI;
     private final I18NUtil i18NUtil;
 
-    public BrowserTreeResource() {
+    public FolderResource() {
         this(new WebResource(),
-                BrowserTreeHelper.getInstance(),
+                FolderHelper.getInstance(),
                 APILocator.getLayoutAPI(),
                 I18NUtil.INSTANCE);
     }
 
     @VisibleForTesting
-    public BrowserTreeResource(final WebResource webResource,
-                               final BrowserTreeHelper browserTreeHelper,
-                               final LayoutAPI layoutAPI,
-                               final I18NUtil i18NUtil) {
+    public FolderResource(final WebResource webResource,
+                          final FolderHelper folderHelper,
+                          final LayoutAPI layoutAPI,
+                          final I18NUtil i18NUtil) {
 
         this.webResource = webResource;
-        this.browserTreeHelper  = browserTreeHelper;
-        this.layoutAPI   = layoutAPI;
-        this.i18NUtil    = i18NUtil;
+        this.folderHelper = folderHelper;
+        this.layoutAPI = layoutAPI;
+        this.i18NUtil = i18NUtil;
     }
 
-    @GET
-    @Path ("/sitename/{sitename}/uri/{uri : .+}")
+    @POST
+    @Path("/createfolders")
     @JSONP
     @NoCache
     @Produces({MediaType.APPLICATION_JSON, "application/javascript"})
-    public final Response loadAssetsUnder(
-            @Context final HttpServletRequest req,
-            @PathParam("sitename")   final String sitename,
-            @PathParam("uri") final String uri
-    ) {
-
+    public final Response createFolders(@Context final HttpServletRequest req,
+                                        @PathParam("paths") final List<String> paths,
+                                        @PathParam("siteName") final String siteName) {
         Response response = null;
         final InitDataObject initData = this.webResource.init(null, true, req, true, null);
         final User user = initData.getUser();
-        final List<Map<String, Object>> assetResults;
+        final List<Map<String, Object>> folderResults;
 
         try {
             Locale locale = LocaleUtil.getLocale(user, req);
-
-            assetResults = browserTreeHelper.getTreeablesUnder(sitename,user,uri)
+            FolderHelper.FolderResults results = folderHelper.createFolders(paths, siteName, user);
+            folderResults = results.folders
                     .stream()
-                    .map(treeable -> {
+                    .map(folder -> {
                         try {
-                            return treeable.getMap();
+                            return folder.getMap();
                         } catch (Exception e) {
-                            Logger.error(this,"Data Exception while converting to map", e);
-                            throw new DotRuntimeException("Data Exception while converting to map",e);
+                            Logger.error(this, "Data Exception while converting to map", e);
+                            throw new DotRuntimeException("Data Exception while converting to map", e);
                         }
                     })
-                    .collect(Collectors.toList());;
+                    .collect(Collectors.toList());
+            ;
 
             response = Response.ok(new ResponseEntityView
-                    (map(   "result",         assetResults
-                    ),
+                    (map("result", folderResults),
+                            results.getErrorEntities(), null,
                             this.i18NUtil.getMessagesMap(locale, "Invalid-option-selected",
-                                    "cancel", "Change-Host"))
-            ).build(); // 200
+                                    "cancel")
+                    )).build(); // 200
         } catch (Exception e) { // this is an unknown error, so we report as a 500.
-            Logger.error(this,"Error handling loadAssetsUnder Get Request", e);
+            Logger.error(this, "Error handling Save Folder Post Request", e);
             response = ExceptionMapperUtil.createResponse(e, Response.Status.INTERNAL_SERVER_ERROR);
         }
         return response;
