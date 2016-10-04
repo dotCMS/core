6ce4af2cb94ef4af78eff457154719c5726df5d0
Jose Orsini <jose@dotcms.com>
2016-10-04 16:33:59 -0400

    fixes #9415 #9382 fix data type for IR tables with language_id for mssql

--- src/com/dotmarketing/startup/runonce/Task03710AddFKForIntegrityCheckerTables.java
+++ src/com/dotmarketing/startup/runonce/Task03710AddFKForIntegrityCheckerTables.java
@@ -12,44 +12,55 @@ public class Task03710AddFKForIntegrityCheckerTables implements StartupTask {
 
     private void createMissingFKForIRTables(DotConnect dc) throws SQLException, DotDataException {
         if(DbConnectionFactory.isMsSql()) {
+            //truncate tables before applying FKs
             dc.executeStatement("truncate table folders_ir;");
             dc.executeStatement("truncate table structures_ir;");
             dc.executeStatement("truncate table schemes_ir;");
             dc.executeStatement("truncate table htmlpages_ir;");
             dc.executeStatement("truncate table fileassets_ir;");
+            //fix datatype for htmlpages_ir table
+            dc.executeStatement("ALTER TABLE htmlpages_ir ALTER COLUMN language_id bigint;");
+            dc.executeStatement("ALTER TABLE fileassets_ir ALTER COLUMN language_id bigint;");
+            //add FKS
             dc.executeStatement("alter table folders_ir add constraint FK_folder_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
             dc.executeStatement("alter table structures_ir add constraint FK_structure_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
             dc.executeStatement("alter table schemes_ir add constraint FK_scheme_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
             dc.executeStatement("alter table htmlpages_ir add constraint FK_page_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
             dc.executeStatement("alter table fileassets_ir add constraint FK_file_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
         }else if(DbConnectionFactory.isOracle()) {
+          //truncate tables before applying FKs
             dc.executeStatement("truncate table folders_ir;");
             dc.executeStatement("truncate table structures_ir;");
             dc.executeStatement("truncate table schemes_ir;");
             dc.executeStatement("truncate table htmlpages_ir;");
             dc.executeStatement("truncate table fileassets_ir;");
+            //add FKS
             dc.executeStatement("alter table folders_ir add constraint FK_folder_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
             dc.executeStatement("alter table structures_ir add constraint FK_structure_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
             dc.executeStatement("alter table schemes_ir add constraint FK_scheme_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
             dc.executeStatement("alter table htmlpages_ir add constraint FK_page_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
             dc.executeStatement("alter table fileassets_ir add constraint FK_file_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
         }else if(DbConnectionFactory.isMySql()) {
+            //truncate tables before applying FKs
             dc.executeStatement("truncate table folders_ir;");
             dc.executeStatement("truncate table structures_ir;");
             dc.executeStatement("truncate table schemes_ir;");
             dc.executeStatement("truncate table htmlpages_ir;");
             dc.executeStatement("truncate table fileassets_ir;");
+            //add FKS
             dc.executeStatement("alter table folders_ir add constraint FK_folder_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
             dc.executeStatement("alter table structures_ir add constraint FK_structure_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
             dc.executeStatement("alter table schemes_ir add constraint FK_scheme_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
             dc.executeStatement("alter table htmlpages_ir add constraint FK_page_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
             dc.executeStatement("alter table fileassets_ir add constraint FK_file_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
         }else if(DbConnectionFactory.isPostgres()) {
+            //truncate tables before applying FKs
             dc.executeStatement("truncate table folders_ir;");
             dc.executeStatement("truncate table structures_ir;");
             dc.executeStatement("truncate table schemes_ir;");
             dc.executeStatement("truncate table htmlpages_ir;");
             dc.executeStatement("truncate table fileassets_ir;");
+            //add FKS
             dc.executeStatement("alter table folders_ir add constraint FK_folder_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
             dc.executeStatement("alter table structures_ir add constraint FK_structure_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
             dc.executeStatement("alter table schemes_ir add constraint FK_scheme_ir_ep foreign key (endpoint_id) references publishing_end_point(id);");
