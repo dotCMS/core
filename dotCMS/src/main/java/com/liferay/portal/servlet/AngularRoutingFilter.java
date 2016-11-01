1bb1012f2b5e4f16aa87c31fd792071ff29328b4
Freddy Rodriguez <freddy0309@gmail.com>
2016-11-01 11:12:36 -0400

    Fix error when load the rules from a specific page/ AngularRoutingFilter refactoring

--- dotCMS/src/main/java/com/liferay/portal/servlet/AngularRoutingFilter.java
+++ dotCMS/src/main/java/com/liferay/portal/servlet/AngularRoutingFilter.java
@@ -1,5 +1,10 @@
 package com.liferay.portal.servlet;
 
+import com.dotcms.repackage.com.google.common.collect.ImmutableList;
+import com.dotcms.repackage.org.apache.commons.io.IOUtils;
+import com.dotmarketing.portlets.rules.conditionlet.Conditionlet;
+import com.dotmarketing.util.Config;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,12 +12,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
 
 /**
  * If the request is a Angular routing request then it response with a 304 Http code.
  */
 public class AngularRoutingFilter implements Filter {
-
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -23,29 +28,15 @@ public class AngularRoutingFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
         String path = ((HttpServletRequest) servletRequest).getServletPath();
+        String angularRoutingUrlRex = Config.getStringProperty("ANGULAR_ROUTING_URL_REX", "^\\/html\\/ng\\/(public|dotCMS)\\/.*");
 
-        if (!path.toLowerCase().endsWith("js")
-                && !path.toLowerCase().endsWith("css")
-                && !path.toLowerCase().endsWith("html")
-                && !path.toLowerCase().endsWith("js.map")
-                && !path.toLowerCase().endsWith("ttf")
-                && !path.toLowerCase().endsWith("woff2")
-                && !path.toLowerCase().endsWith("woff")) {
+        if (path.matches(angularRoutingUrlRex)) {
 
             ServletContext context = servletRequest.getServletContext();
-            String indexRealPath = context.getRealPath("/html/ng/index.html");
+            String indexURL = Config.getStringProperty("ANGULAR_INDEX_URL", "/html/ng/index.html");
+            String indexRealPath = context.getRealPath(indexURL);
 
-            try( OutputStream out = servletResponse.getOutputStream();
-                 FileInputStream in = new FileInputStream(indexRealPath)){
-
-                byte[] buffer = new byte[4096];
-                int length;
-                while ((length = in.read(buffer)) > 0) {
-                    out.write(buffer, 0, length);
-                }
-                in.close();
-                out.flush();
-            }
+            IOUtils.copy(new FileInputStream(indexRealPath), servletResponse.getOutputStream());
         }else{
             filterChain.doFilter(servletRequest, servletResponse);
         }
