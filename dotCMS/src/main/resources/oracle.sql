3353627db8929f7f96ba319a2c87219426c8be8f
Oscar Arrieta <oscar.arrieta@dotcms.com>
2016-11-30 11:31:39 -0600

    #10203: Add LOWER Index and other fixes.
    gitignore: Adding generated folder, part of the immutable lib process.
    dependencies.gradle: Updating ee and lic Snapshots to 3.8.0.
    ContentTye*.java: Allow override of SQL for each DB.
    Task03800AddIndexLowerStructureTable.java: Add LOWER index in upgrade.
    TaskLocatorUtil.java: Add Task03800AddIndexLowerStructureTable.java task.
    *.sql: Add index in vanilla instance.

--- dotCMS/src/main/resources/oracle.sql
+++ dotCMS/src/main/resources/oracle.sql
@@ -2655,3 +2655,6 @@ CREATE INDEX idx_system_event ON system_event (created);
 -- Delete User
 ALTER TABLE user_ ADD delete_in_progress number(1,0) default 0;
 ALTER TABLE user_ ADD delete_date DATE;
+
+--Content Types improvement
+CREATE INDEX idx_lower_structure_name ON structure (LOWER(velocity_var_name));
