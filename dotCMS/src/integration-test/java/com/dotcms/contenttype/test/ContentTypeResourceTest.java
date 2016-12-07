9c26822a47e7e57d98d7b2fbe97d087e488a9c5b
agomez-dotcms <anibalgomez@192.168.1.107>
2016-12-07 16:57:49 -0600

    #10245: Fixed integration-tests failing against postgres in local-env

--- dotCMS/src/integration-test/java/com/dotcms/contenttype/test/ContentTypeResourceTest.java
+++ dotCMS/src/integration-test/java/com/dotcms/contenttype/test/ContentTypeResourceTest.java
@@ -3,7 +3,6 @@ package com.dotcms.contenttype.test;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
@@ -11,12 +10,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.io.IOUtils;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.dotcms.contenttype.business.ContentTypeApi;
-import com.dotcms.contenttype.business.ContentTypeFactory;
-import com.dotcms.contenttype.business.ContentTypeFactoryImpl;
 import com.dotcms.contenttype.exception.NotFoundInDbException;
 import com.dotcms.contenttype.model.type.ContentType;
 import com.dotcms.contenttype.transform.contenttype.JsonContentTypeTransformer;
@@ -27,86 +22,67 @@ import com.dotcms.mock.request.MockSessionRequest;
 import com.dotcms.repackage.javax.ws.rs.core.Response;
 import com.dotcms.repackage.org.glassfish.jersey.internal.util.Base64;
 import com.dotcms.rest.api.v1.contenttype.ContentTypeResource;
-import com.dotmarketing.business.APILocator;
 import com.dotmarketing.util.Logger;
 
-public class ContentTypeResourceTest {
+public class ContentTypeResourceTest extends ContentTypeBaseTest {
+	final String base = "/com/dotcms/contenttype/test/";
 
-  final ContentTypeFactory factory = new ContentTypeFactoryImpl();
-  static ContentTypeApi api;
+	@Test
+	public void jsonTests() throws Exception {
 
-  @BeforeClass
-  public static void SetUpTests() throws FileNotFoundException, Exception {
-    SuperContentTypeTest.SetUpTests();
-    api = APILocator.getContentTypeAPI2(APILocator.systemUser());
-  }
+		URL resource = this.getClass().getResource(base);
+		File directory = new File(resource.toURI());
+		assertThat("we have a testing directory with json in it", directory != null);
 
-  /**
-   * BasicAuth
-   * 
-   * @return
-   */
-  private HttpServletRequest getHttpRequest() {
-    MockHeaderRequest request = new MockHeaderRequest(
-        new MockSessionRequest(new MockAttributeRequest(new MockHttpRequest("localhost", "/").request()).request())
-            .request());
+		for (String file : directory.list()) {
+			if (file.endsWith(".json")) {
+				testJson(file);
+			}
+		}
+	}
 
-    request.setHeader("Authorization", "Basic " + new String(Base64.encode("admin@dotcms.com:admin".getBytes())));
-
-    return request;
-  }
-
-  final String base = "/com/dotcms/contenttype/test/";
-
-  @Test
-  public void jsonTests() throws Exception {
-
-    URL resource = this.getClass().getResource(base);
-    File directory = new File(resource.toURI());
-    assertThat("we have a testing directory with json in it", directory != null);
-
-    for (String file : directory.list()) {
-      if (file.endsWith(".json")) {
-        testJson(file);
-      }
-    }
-  }
+	public void testJson(String jsonFile) throws Exception {
+		Logger.info(this.getClass(), "testing:" + jsonFile);
 
 
+		Logger.info(this.getClass(), "testing:" + base + jsonFile);
+		InputStream stream = this.getClass().getResourceAsStream(base + jsonFile);
+		String json = IOUtils.toString(stream);
+		stream.close();
+		List<ContentType> delTypes = new JsonContentTypeTransformer(json).asList();
+		for(ContentType delType:delTypes){
+			try {
+				contentTypeApi.delete(contentTypeApi.find(delType.id()));
+			} catch (NotFoundInDbException e) {
 
-  public void testJson(String jsonFile) throws Exception {
-    Logger.info(this.getClass(), "testing:" + jsonFile);
+			}
+			try {
+				contentTypeApi.delete(contentTypeApi.find(delType.variable()));
+			} catch (NotFoundInDbException ee) {
 
-    
-    Logger.info(this.getClass(), "testing:" + base + jsonFile);
-    InputStream stream = this.getClass().getResourceAsStream(base + jsonFile);
-    String json = IOUtils.toString(stream);
-    stream.close();
-    List<ContentType> delTypes = new JsonContentTypeTransformer(json).asList();
-      for(ContentType delType:delTypes){
-      try {
-        api.delete(api.find(delType.id()));
-      } catch (NotFoundInDbException e) {
-  
-      }
-      try {
-        api.delete(api.find(delType.variable()));
-      } catch (NotFoundInDbException ee) {
-  
-      }
-    }
-    
-    
-    
-    
-    
-    ContentTypeResource resource = new ContentTypeResource();
+			}
+		}
 
-    Response response = resource.saveType(getHttpRequest(), json);
+		ContentTypeResource resource = new ContentTypeResource();
 
-    int x = response.getStatus();
-    assertThat("result:200 with json " + jsonFile, x == 200);
-  }
+		Response response = resource.saveType(getHttpRequest(), json);
 
+		int x = response.getStatus();
+		assertThat("result:200 with json " + jsonFile, x == 200);
+	}
 
+	/**
+	 * BasicAuth
+	 * 
+	 * @return
+	 */
+	private HttpServletRequest getHttpRequest() {
+		MockHeaderRequest request = new MockHeaderRequest(
+				new MockSessionRequest(new MockAttributeRequest(new MockHttpRequest("localhost", "/").request()).request())
+				.request());
+
+		request.setHeader("Authorization", "Basic " + new String(Base64.encode("admin@dotcms.com:admin".getBytes())));
+
+		return request;
+	}
 }
--- dotCMS/src/integration-test/java/com/dotcms/contenttype/test/JsonContentTypeTransformerTest.java
+++ dotCMS/src/integration-test/java/com/dotcms/contenttype/test/JsonContentTypeTransformerTest.java
@@ -2,136 +2,110 @@ package com.dotcms.contenttype.test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.io.FileNotFoundException;
 import java.util.List;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.dotcms.contenttype.business.ContentTypeApi;
-import com.dotcms.contenttype.business.ContentTypeFactory;
-import com.dotcms.contenttype.business.ContentTypeFactoryImpl;
 import com.dotcms.contenttype.model.field.Field;
 import com.dotcms.contenttype.model.type.ContentType;
 import com.dotcms.contenttype.transform.contenttype.JsonContentTypeTransformer;
 import com.dotcms.contenttype.transform.field.JsonFieldTransformer;
-import com.dotmarketing.business.APILocator;
 import com.dotmarketing.util.Logger;
 
-public class JsonContentTypeTransformerTest {
+public class JsonContentTypeTransformerTest extends ContentTypeBaseTest {
 
-  final ContentTypeFactory factory = new ContentTypeFactoryImpl();
-  static ContentTypeApi api ;
-  
-  @BeforeClass
-  public static void SetUpTests() throws FileNotFoundException, Exception {
-    SuperContentTypeTest.SetUpTests();
-    api =APILocator.getContentTypeAPI2(APILocator.systemUser());
-  }
+	@Test
+	public void testContentTypeSerialization() throws Exception {
+		List<ContentType> types = contentTypeFactory.findAll();
+		for (ContentType type : types) {
+			ContentType type2 = null;
+			String json = null;
+			try {
+				json = new JsonContentTypeTransformer(type).jsonObject().toString();
+				type2 = new JsonContentTypeTransformer(json).from();
 
-  @Test
-  public void testContentTypeSerialization() throws Exception {
-    List<ContentType> types = factory.findAll();
-    for (ContentType type : types) {
-      ContentType type2 = null;
-      String json = null;
-      try {
-        json = new JsonContentTypeTransformer(type).jsonObject().toString();
-        type2 = new JsonContentTypeTransformer(json).from();
+				assertThat("ContentType == ContentType2", type.equals(type2));
+			} catch (Throwable t) {
+				System.out.println(type);
+				System.out.println(type2);
+				System.out.println(json);
+				throw t;
+			}
+		}
+	}
 
-        assertThat("ContentType == ContentType2", type.equals(type2));
-      } catch (Throwable t) {
-        System.out.println(type);
-        System.out.println(type2);
-        System.out.println(json);
-        throw t;
-      }
-    }
-  }
+	@Test
+	public void testContentTypeArraySerialization() throws Exception {
+		List<ContentType> types = contentTypeFactory.findAll();
 
-  @Test
-  public void testContentTypeArraySerialization() throws Exception {
-    List<ContentType> types = factory.findAll();
+		List<ContentType> types2 = null;
+		String json = null;
+		try {
+			json = new JsonContentTypeTransformer(types).jsonArray().toString();
+			types2 = new JsonContentTypeTransformer(json).asList();
 
-    List<ContentType> types2 = null;
-    String json = null;
-    try {
-      json = new JsonContentTypeTransformer(types).jsonArray().toString();
-      types2 = new JsonContentTypeTransformer(json).asList();
+			for (int i = 0; i < types.size(); i++) {
+				assertThat("ContentType == ContentType2", types.get(i).equals(types2.get(i)));
+			}
+		} catch (Throwable t) {
+			System.out.println(types);
+			System.out.println(types2);
+			System.out.println(json);
+			throw t;
+		}
+	}
 
-      for (int i = 0; i < types.size(); i++) {
-        assertThat("ContentType == ContentType2", types.get(i).equals(types2.get(i)));
-      }
-    } catch (Throwable t) {
-      System.out.println(types);
-      System.out.println(types2);
-      System.out.println(json);
-      throw t;
-    }
-  }
+	@Test
+	public void testFieldSerialization() throws Exception {
+		List<ContentType> types = contentTypeFactory.findAll();
+		for (ContentType type : types) {
+			Field field2 = null;
+			String json = null;
+			for (Field field : type.fields()) {
 
-  @Test
-  public void testFieldSerialization() throws Exception {
-    List<ContentType> types = factory.findAll();
-    for (ContentType type : types) {
-      Field field2 = null;
-      String json = null;
-      for (Field field : type.fields()) {
+				try {
+					json = new JsonFieldTransformer(field).jsonObject().toString();
 
-        try {
-          json = new JsonFieldTransformer(field).jsonObject().toString();
-
-          field2 = new JsonFieldTransformer(json).from();
+					field2 = new JsonFieldTransformer(json).from();
 
 
-          assertThat("Field1 == json Field2", field.equals(field2));
-        } catch (Throwable t) {
-          Logger.error(this.getClass(), "Asdasdsa", t);
-          System.out.println(json);
-          System.out.println(field);
-          System.out.println(field2);
-          System.out.println(t);
-          throw t;
-        }
-      }
-    }
-  }
+					assertThat("Field1 == json Field2", field.equals(field2));
+				} catch (Throwable t) {
+					Logger.error(this.getClass(), "Asdasdsa", t);
+					System.out.println(json);
+					System.out.println(field);
+					System.out.println(field2);
+					System.out.println(t);
+					throw t;
+				}
+			}
+		}
+	}
 
-  
-  @Test
-  public void testFieldArraySerialization() throws Exception {
-    List<ContentType> types = factory.findAll();
+	@Test
+	public void testFieldArraySerialization() throws Exception {
+		List<ContentType> types = contentTypeFactory.findAll();
 
-    List<Field> fields = null;
-    List<Field> fields2 = null;
-    String json = null;
-    try {
-      for (ContentType type : types) {
-        fields = type.fields();
-        json = new JsonFieldTransformer(type.fields()).jsonArray().toString();
-        fields2 = new JsonFieldTransformer(json).asList();
+		List<Field> fields = null;
+		List<Field> fields2 = null;
+		String json = null;
+		try {
+			for (ContentType type : types) {
+				fields = type.fields();
+				json = new JsonFieldTransformer(type.fields()).jsonArray().toString();
+				fields2 = new JsonFieldTransformer(json).asList();
 
-        for (int i = 0; i < type.fields().size(); i++) {
-          assertThat("Field1 == Field2", fields.get(i).equals(type.fields().get(i)));
-        }
-        for (int i = 0; i < type.fields().size(); i++) {
-          assertThat("Field1 == Field2", fields2.get(i).equals(type.fields().get(i)));
-        }
-      }
-    } catch (Throwable t) {
-      System.out.println(fields);
-      System.out.println(json);
-      throw t;
-    }
-  }
-
-  
-
-
-  
-  
-  
-  
-  
-
+				for (int i = 0; i < type.fields().size(); i++) {
+					assertThat("Field1 == Field2", fields.get(i).equals(type.fields().get(i)));
+				}
+				for (int i = 0; i < type.fields().size(); i++) {
+					assertThat("Field1 == Field2", fields2.get(i).equals(type.fields().get(i)));
+				}
+			}
+		} catch (Throwable t) {
+			System.out.println(fields);
+			System.out.println(json);
+			throw t;
+		}
+	}
 }
