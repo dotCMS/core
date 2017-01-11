34ac0f5de61cd8e54a40030f9384595a7349d742
Brent Griffin <brent.griffin@dotcms.com>
2017-01-11 16:06:35 -0600

    #9996 fix brought in

--- src/com/dotmarketing/init/DotInitScheduler.java
+++ src/com/dotmarketing/init/DotInitScheduler.java
@@ -742,10 +742,9 @@ public class DotInitScheduler {
             String jobGroup = "dotcms_jobs";
             String triggerName = "trigger23";
             String triggerGroup = "group23";
-            if(Config.getBooleanProperty("ENABLE_REMOVE_INACTIVE_CLUSTER_SERVER", true)) {
+            if(Config.getBooleanProperty("ENABLE_REMOVE_INACTIVE_CLUSTER_SERVER", true) &&  Config.getBooleanProperty( "ENABLE_SERVER_HEARTBEAT", true )) {
 				try {
 					isNew = false;
-					
 					try {
 						if ((job = sched.getJobDetail(jobName, jobGroup)) == null) {
 							job = new JobDetail(jobName, jobGroup, DeleteInactiveClusterServersJob.class);
@@ -765,10 +764,12 @@ public class DotInitScheduler {
 						sched.scheduleJob(trigger);
 					else
 						sched.rescheduleJob(triggerName, triggerGroup, trigger);
+					Logger.info(DotInitScheduler.class, "DeleteInactiveClusterServersJob on");
 				} catch (Exception e) {
 					Logger.error(DotInitScheduler.class, e.getMessage(),e);
 				}
 			} else {
+				Logger.info(DotInitScheduler.class, "DeleteInactiveClusterServersJob off”);
 				if ((job = sched.getJobDetail(jobName, jobGroup)) != null) {
 					sched.deleteJob(jobName, jobGroup);
 				}
