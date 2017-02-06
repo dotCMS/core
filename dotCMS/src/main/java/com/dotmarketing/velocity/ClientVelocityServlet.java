2ad1f7fa1b7344e041946228a209c03b9744f1b4
Freddy Rodriguez <freddy0309@gmail.com>
2017-02-06 12:57:36 -0600

    refactoring

--- dotCMS/src/main/java/com/dotmarketing/velocity/ClientVelocityServlet.java
+++ dotCMS/src/main/java/com/dotmarketing/velocity/ClientVelocityServlet.java
@@ -62,13 +62,13 @@ public class ClientVelocityServlet extends VelocityServlet {
      */
     private boolean notRedirect(HttpServletRequest req) {
         String refererValue = req.getHeader(Constants.REFERER_URL_HTTP_HEADER);
-        boolean faParameter = Boolean.parseBoolean(req.getParameter("container"));
+        boolean containerParameter = Boolean.parseBoolean(req.getParameter("container"));
 
         return (refererValue != null && (refererValue.contains( "host_id=" )
                     || refererValue.contains( "container=true" )
                     || refererValue.contains(PortletURLUtil.URL_ADMIN_PREFIX )
                     || refererValue.contains( "p_p_id=" + PortletID.SITE_BROWSER )))
-                || faParameter;
+                || containerParameter;
     }
 
     //EACH CLIENT MAY HAVE ITS OWN VARIABLES
