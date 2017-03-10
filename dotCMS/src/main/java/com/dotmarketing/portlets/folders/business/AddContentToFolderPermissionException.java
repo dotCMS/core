3ccc144728c3f2c9465cf43704c030dff3e30a57
Freddy Rodriguez <freddy0309@gmail.com>
2017-03-10 15:29:50 -0400

    #10984 change to field final

--- dotCMS/src/main/java/com/dotmarketing/portlets/folders/business/AddContentToFolderPermissionException.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/folders/business/AddContentToFolderPermissionException.java
@@ -4,8 +4,8 @@ import com.dotmarketing.exception.DotSecurityException;
 
 public class AddContentToFolderPermissionException extends DotSecurityException {
 
-    private String userId;
-    private String folderPath;
+    private final String userId;
+    private final String folderPath;
 
     AddContentToFolderPermissionException(String userId, String folderPath){
         super(String.format("User %s does not have permission to add to Folder %s", userId, folderPath));
