a9d7988d7b7e9d64fa933094368728321bbfeb15
Erick Gonzalez <erick.gonzalez@dotcms.com>
2017-03-17 13:14:37 -0600

    restore removed code and reference new path

--- dotCMS/src/main/java/com/dotcms/publisher/pusher/wrapper/ContentTypeWrapper.java
+++ dotCMS/src/main/java/com/dotcms/publisher/pusher/wrapper/ContentTypeWrapper.java
@@ -5,12 +5,14 @@ import java.util.List;
 import com.dotcms.contenttype.model.field.Field;
 import com.dotcms.contenttype.model.field.FieldVariable;
 import com.dotcms.contenttype.model.type.ContentType;
+import com.dotcms.publishing.PublisherConfig.Operation;
 
 public class ContentTypeWrapper {
 	private ContentType contentType;
 	private List<Field> fields;
 	private String workflowSchemaId;
 	private String workflowSchemaName;
+	private Operation operation;
 	private List<FieldVariable> fieldVariables;
 	
     public ContentTypeWrapper() {}
@@ -52,6 +54,14 @@ public class ContentTypeWrapper {
 	public void setFields(List<Field> fields) {
 		this.fields = fields;
 	}
+
+    public Operation getOperation() {		
+        return operation;		
+    }		
+ 		
+    public void setOperation(Operation operation) {		
+        this.operation = operation;		
+    }
 
     public String getWorkflowSchemaId() {
         return workflowSchemaId;
