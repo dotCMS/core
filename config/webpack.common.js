9dbf8400cd45545317fd97d085957fccf6dc8fc2
Freddy Montes <freddy@dotcms.com>
2017-03-30 12:01:43 -0600

    dotCMS/core#11078 remove meta asset from build

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -282,8 +282,7 @@ module.exports = function (options) {
        * See: https://www.npmjs.com/package/copy-webpack-plugin
        */
       new CopyWebpackPlugin([
-        { from: 'src/assets', to: 'assets' },
-        { from: 'src/meta'}
+        { from: 'src/assets', to: 'assets' }
       ]),
 
 
