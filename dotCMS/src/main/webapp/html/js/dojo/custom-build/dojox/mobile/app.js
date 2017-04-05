7a6e55704513aca375dc1b3bebf312a9827d4b78
Freddy Montes <freddymontes@gmail.com>
2017-04-05 09:02:59 -0600

    dotCMS/core#11218 Minifiy Dojo

---
+++ dotCMS/src/main/webapp/html/js/dojo/custom-build/dojox/mobile/app.js
@@ -0,0 +1,14 @@
+define("dojox/mobile/app", [
+	"./app/_base"
+], function(appBase){
+	
+	/*=====
+	return {
+		// summary:
+		//		Loads dojox/mobile/app/_base. 
+		// tags:
+		//		private
+	};
+	=====*/
+	return appBase;
+});
