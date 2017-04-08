9677db9e50d0b3978f94d65e4fc6de4dbb402ba2
Anibal Gomez <anibal.gomez@dotcms.com>
2017-04-07 21:10:08 -0600

    #11156: Refactored field-resource-test for extensibility

--- dotCMS/src/integration-test/java/com/dotcms/rest/api/v1/contenttype/FieldResourceTest.java
+++ dotCMS/src/integration-test/java/com/dotcms/rest/api/v1/contenttype/FieldResourceTest.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.dotcms.contenttype.model.field.DateField;
 import com.dotcms.contenttype.model.field.Field;
 import com.dotcms.contenttype.model.field.TextField;
 import com.dotcms.contenttype.model.type.ContentType;
@@ -32,19 +33,17 @@ import org.junit.Test;
 
 public class FieldResourceTest {
 
-	private final ObjectMapper mapper = new ObjectMapper();
+	private static final ObjectMapper mapper = new ObjectMapper();
 
 
 	@BeforeClass
 	public static void prepare() throws Exception{
-		//Setting web app environment
         IntegrationTestInitService.getInstance().init();
 	}
 
 
-	@SuppressWarnings("unchecked")
 	@Test
-	public void testExistingFields() throws Exception {
+	public void testFieldsList() throws Exception {
 		final FieldResource resource = new FieldResource();
 
         ContentType contentType = getContentType();
@@ -66,181 +65,300 @@ public class FieldResourceTest {
 		}
 	}
 
-	@SuppressWarnings("unchecked")
+
 	@Test
-	public void testNewFieldTextField() throws Exception {
+	public void testFieldText() throws Exception {
+		testField(
+			new AbstractFieldTester(){
+				@Override
+				protected String getJsonFieldCreate() {
+					return "{"+
+						// IDENTITY VALUES
+						"	\"clazz\" : \"com.dotcms.contenttype.model.field.ImmutableTextField\","+
+						"	\"contentTypeId\" : \"CONTENT_TYPE_ID\","+
+						"	\"dataType\" : \"TEXT\","+
+						"	\"name\" : \"The Field 1\","+
 
-		final FieldResource resource = new FieldResource();
+						// MANDATORY VALUES
+						"	\"defaultValue\" : \"THE DEFAULT VALUE\","+
+						"	\"regexCheck\" : \"THE VALIDATION REGEX\","+
+						"	\"hint\" : \"THE HINT\","+
 
-        final ContentType contentType = getContentType();
+						"	\"required\" : \"true\","+
+						"	\"searchable\" : \"true\","+
+						"	\"indexed\" : \"true\","+
+						"	\"listed\" : \"true\","+
+						"	\"unique\" : \"false\","+
 
-		Response response = resource.createContentTypeField(
-			contentType.id(), JSON_NEW_FIELD_TEXT.replace("CONTENT_TYPE_ID", contentType.id()), getHttpRequest()
+						// OPTIONAL VALUES
+						"	\"readOnly\" : \"false\","+
+						"	\"fixed\" : \"false\","+
+						"	\"sortOrder\" : 11"+
+					"	}";
+				}
+
+				@Override
+				protected void assertFieldCreate(Field field) {
+					assertTrue(field instanceof TextField);
+					assertNotNull(field.id());
+					assertEquals("theField1", field.variable());
+
+					assertEquals("THE DEFAULT VALUE", field.defaultValue());
+					assertEquals("THE VALIDATION REGEX", field.regexCheck());
+					assertEquals("THE HINT", field.hint());
+
+					assertTrue(field.required());
+					assertTrue(field.searchable());
+					assertTrue(field.indexed());
+					assertTrue(field.listed());
+					assertFalse(field.unique());
+
+					assertFalse(field.readOnly());
+					assertFalse(field.fixed());
+					assertEquals(11, field.sortOrder());		
+				}
+
+				@Override
+				protected String getJsonFieldUpdate() {
+					return "{"+
+						// IDENTITY VALUES
+						"	\"clazz\" : \"com.dotcms.contenttype.model.field.ImmutableTextField\","+
+						"	\"contentTypeId\" : \"CONTENT_TYPE_ID\","+
+						"	\"id\" : \"CONTENT_TYPE_FIELD_ID\","+
+						"	\"dataType\" : \"TEXT\","+
+						"	\"name\" : \"The Field 1\","+
+
+						// MANDATORY VALUES
+						"	\"variable\" : \"theField1\","+
+						"	\"sortOrder\":\"12\","+
+
+						"	\"defaultValue\" : \"THE DEFAULT VALUE 2\","+
+						"	\"regexCheck\" : \"THE VALIDATION REGEX 2\","+
+						"	\"hint\" : \"THE HINT 2\","+
+
+						"	\"required\" : \"false\","+
+						"	\"searchable\" : \"false\","+
+						"	\"indexed\" : \"false\","+
+						"	\"listed\" : \"false\","+
+						"	\"unique\" : \"false\""+
+					"	}";
+				}
+
+				@Override
+				protected void assertFieldUpdate(Field field) {
+					assertTrue(field instanceof TextField);
+					assertNotNull(field.id());
+					assertEquals("theField1", field.variable());
+
+					assertEquals("THE DEFAULT VALUE 2", field.defaultValue());
+					assertEquals("THE VALIDATION REGEX 2", field.regexCheck());
+					assertEquals("THE HINT 2", field.hint());
+
+					assertFalse(field.required());
+					assertFalse(field.searchable());
+					assertFalse(field.indexed());
+					assertFalse(field.listed());
+					assertFalse(field.unique());
+
+					assertFalse(field.readOnly());
+					assertFalse(field.fixed());
+					assertEquals(12, field.sortOrder());		
+				}
+			}
 		);
-
-		assertResponse_OK(response);
-
-
-		Map<String, Object> fieldMap = (Map<String, Object>) ((ResponseEntityView) response.getEntity()).getEntity();
-
-		assertNotNull(fieldMap);
-
-		try {
-			Field field = convertMapToField((Map<String, Object>) fieldMap);
-
-			assertNewFieldText( field );
-
-			response = resource.getContentTypeFieldById(contentType.id(), (String) fieldMap.get("id"), getHttpRequest());
-
-			assertNewFieldText( field );
-
-		} finally {
-
-			response = resource.deleteContentTypeFieldById(contentType.id(), (String) fieldMap.get("id"), getHttpRequest());
-
-			assertResponse_OK(response);
-
-			response = resource.getContentTypeFieldById(contentType.id(), (String) fieldMap.get("id"), getHttpRequest());
-
-			assertResponse_NOT_FOUND(response);
-		}
 	}
 
-	final String JSON_NEW_FIELD_TEXT = "{"+
-		// IDENTITY VALUES
-		"	\"clazz\" : \"com.dotcms.contenttype.model.field.ImmutableTextField\","+
-		"	\"contentTypeId\" : \"CONTENT_TYPE_ID\","+
-		"	\"dataType\" : \"TEXT\","+
-		"	\"name\" : \"The Field 1\","+
+	@Test
+	public void testFieldDate() throws Exception {
+		testField(
+			new AbstractFieldTester(){
+				@Override
+				protected String getJsonFieldCreate() {
+					return "{"+
+						// IDENTITY VALUES
+						"	\"clazz\" : \"com.dotcms.contenttype.model.field.ImmutableDateField\","+
+						"	\"contentTypeId\" : \"CONTENT_TYPE_ID\","+
+						"	\"dataType\" : \"DATE\","+
+						"	\"name\" : \"The Field 1\","+
 
-		// MANDATORY VALUES
-		"	\"defaultValue\" : \"THE DEFAULT VALUE\","+
-		"	\"regexCheck\" : \"THE VALIDATION REGEX\","+
-		"	\"hint\" : \"THE HINT\","+
+						// MANDATORY VALUES
+						"	\"defaultValue\" : \"1995-12-05\","+
+						"	\"hint\" : \"THE HINT\","+
 
-		"	\"required\" : \"true\","+
-		"	\"searchable\" : \"true\","+
-		"	\"indexed\" : \"true\","+
-		"	\"listed\" : \"true\","+
-		"	\"unique\" : \"false\","+
+						"	\"required\" : \"true\","+
+						"	\"searchable\" : \"true\","+
+						"	\"indexed\" : \"true\","+
+						"	\"listed\" : \"true\","+
 
-		// OPTIONAL VALUES
-		"	\"readOnly\" : \"false\","+
-		"	\"fixed\" : \"false\","+
-		"	\"sortOrder\" : 11"+
-	"	}";
+						// OPTIONAL VALUES
+						"	\"readOnly\" : \"false\","+
+						"	\"fixed\" : \"false\","+
+						"	\"sortOrder\" : 11"+
+					"	}";
+				}
 
-	private void assertNewFieldText(Field field) {
-		assertTrue(field instanceof TextField);
-		assertNotNull(field.id());
-		assertEquals("theField1", field.variable());
+				@Override
+				protected void assertFieldCreate(Field field) {
+					assertTrue(field instanceof DateField);
+					assertNotNull(field.id());
+					assertEquals("theField1", field.variable());
 
-		assertEquals("THE DEFAULT VALUE", field.defaultValue());
-		assertEquals("THE VALIDATION REGEX", field.regexCheck());
-		assertEquals("THE HINT", field.hint());
+					assertEquals("1995-12-05", field.defaultValue());
+					assertEquals("THE HINT", field.hint());
 
-		assertTrue(field.required());
-		assertTrue(field.searchable());
-		assertTrue(field.indexed());
-		assertTrue(field.listed());
-		assertFalse(field.unique());
+					assertTrue(field.required());
+					assertTrue(field.searchable());
+					assertTrue(field.indexed());
+					assertTrue(field.listed());
 
-		assertFalse(field.readOnly());
-		assertFalse(field.fixed());
-		assertEquals(11, field.sortOrder());		
+					assertFalse(field.readOnly());
+					assertFalse(field.fixed());
+					assertEquals(11, field.sortOrder());		
+				}
+
+				@Override
+				protected String getJsonFieldUpdate() {
+					return "{"+
+						// IDENTITY VALUES
+						"	\"clazz\" : \"com.dotcms.contenttype.model.field.ImmutableDateField\","+
+						"	\"contentTypeId\" : \"CONTENT_TYPE_ID\","+
+						"	\"id\" : \"CONTENT_TYPE_FIELD_ID\","+
+						"	\"dataType\" : \"DATE\","+
+						"	\"name\" : \"The Field 1\","+
+
+						// MANDATORY VALUES
+						"	\"variable\" : \"theField1\","+
+						"	\"sortOrder\":\"12\","+
+
+						"	\"defaultValue\" : \"1980-03-31\","+
+						"	\"hint\" : \"THE HINT 2\","+
+
+						"	\"required\" : \"false\","+
+						"	\"searchable\" : \"false\","+
+						"	\"indexed\" : \"false\","+
+						"	\"listed\" : \"false\""+
+					"	}";
+				}
+
+				@Override
+				protected void assertFieldUpdate(Field field) {
+					assertTrue(field instanceof DateField);
+					assertNotNull(field.id());
+					assertEquals("theField1", field.variable());
+
+					assertEquals("1980-03-31", field.defaultValue());
+					assertEquals("THE HINT 2", field.hint());
+
+					assertFalse(field.required());
+					assertFalse(field.searchable());
+					assertFalse(field.indexed());
+					assertFalse(field.listed());
+
+					assertFalse(field.readOnly());
+					assertFalse(field.fixed());
+					assertEquals(12, field.sortOrder());		
+				}
+			}
+		);
 	}
 
-	@SuppressWarnings("unchecked")
-	@Test
-	public void testUpdatedFieldTextField() throws Exception {
 
-		final FieldResource resource = new FieldResource();
+	private static abstract class AbstractFieldTester {
 
-        final ContentType contentType = getContentType();
+		protected abstract String getJsonFieldCreate();
+		protected abstract String getJsonFieldUpdate();
+		
+		protected abstract void assertFieldCreate(Field field);
+		protected abstract void assertFieldUpdate(Field field);
 
-		Response response = resource.createContentTypeField(
-			contentType.id(), JSON_NEW_FIELD_TEXT.replace("CONTENT_TYPE_ID", contentType.id()), getHttpRequest()
-		);
+		public void run() throws Exception {
 
-		assertResponse_OK(response);
+			final FieldResource resource = new FieldResource();
 
+	        final ContentType contentType = getContentType();
 
-		Map<String, Object> fieldMap = (Map<String, Object>) ((ResponseEntityView) response.getEntity()).getEntity();
+	        Response response;
+	        Map<String, Object> fieldMap;
 
-		assertNotNull(fieldMap);
-
-		try {
-			response = resource.updateContentTypeFieldById(
-				contentType.id(), (String) fieldMap.get("id"),
-				JSON_UPDATED_FIELD_TEXT.replace("CONTENT_TYPE_ID", contentType.id()).replace("CONTENT_TYPE_FIELD_ID", (String) fieldMap.get("id")),
-				getHttpRequest()
+	        // Test Field Creation
+			assertResponse_OK(
+				response = resource.createContentTypeField(
+					contentType.id(), getJsonFieldCreate().replace("CONTENT_TYPE_ID", contentType.id()), getHttpRequest()
+				)
 			);
 
-			fieldMap = (Map<String, Object>) ((ResponseEntityView) response.getEntity()).getEntity();
+			assertNotNull(
+				fieldMap = (Map<String, Object>) ((ResponseEntityView) response.getEntity()).getEntity()
+			);
 
-			assertNotNull(fieldMap);
+			try {
+				assertFieldCreate(
+					convertMapToField((Map<String, Object>) fieldMap)
+				);
 
-			Field field = convertMapToField((Map<String, Object>) fieldMap);
+				// Test Field Retrieval by Id
+				assertResponse_OK(
+					response = resource.getContentTypeFieldById(contentType.id(), (String) fieldMap.get("id"), getHttpRequest())
+				);
 
-			assertUpdatedFieldText( field );
+				assertNotNull(
+					fieldMap = (Map<String, Object>) ((ResponseEntityView) response.getEntity()).getEntity()
+				);
 
-		} finally {
+				assertFieldCreate(
+					convertMapToField((Map<String, Object>) fieldMap)
+				);
 
-			response = resource.deleteContentTypeFieldById(contentType.id(), (String) fieldMap.get("id"), getHttpRequest());
+				// Test Field Update
+				assertResponse_OK(
+					response = resource.updateContentTypeFieldById(
+						contentType.id(), (String) fieldMap.get("id"),
+						getJsonFieldUpdate().replace("CONTENT_TYPE_ID", contentType.id()).replace("CONTENT_TYPE_FIELD_ID", (String) fieldMap.get("id")),
+						getHttpRequest()
+					)
+				);
 
-			assertResponse_OK(response);
+				assertNotNull(
+					fieldMap = (Map<String, Object>) ((ResponseEntityView) response.getEntity()).getEntity()
+				);
 
-			response = resource.getContentTypeFieldById(contentType.id(), (String) fieldMap.get("id"), getHttpRequest());
+				assertFieldUpdate(
+					convertMapToField((Map<String, Object>) fieldMap)
+				);
 
-			assertResponse_NOT_FOUND(response);
+				// Test Field Retrieval by Var
+				assertResponse_OK(
+					response = resource.getContentTypeFieldByVar(contentType.id(), (String) fieldMap.get("variable"), getHttpRequest())
+				);
+
+				assertNotNull(
+					fieldMap = (Map<String, Object>) ((ResponseEntityView) response.getEntity()).getEntity()
+				);
+
+				assertFieldUpdate(
+					convertMapToField((Map<String, Object>) fieldMap)
+				);
+
+			} finally {
+
+				// Test Field Deletion
+				assertResponse_OK(
+					response = resource.deleteContentTypeFieldById(contentType.id(), (String) fieldMap.get("id"), getHttpRequest())
+				);
+
+				assertResponse_NOT_FOUND(
+					response = resource.getContentTypeFieldById(contentType.id(), (String) fieldMap.get("id"), getHttpRequest())
+				);
+			}
 		}
 	}
 
-	final String JSON_UPDATED_FIELD_TEXT = "{"+
-			// IDENTITY VALUES
-		"	\"clazz\" : \"com.dotcms.contenttype.model.field.ImmutableTextField\","+
-		"	\"contentTypeId\" : \"CONTENT_TYPE_ID\","+
-		"	\"id\" : \"CONTENT_TYPE_FIELD_ID\","+
-		"	\"dataType\" : \"TEXT\","+
-		"	\"name\" : \"The Field 1\","+
-
-		// MANDATORY VALUES
-		"	\"variable\" : \"theField1\","+
-		"	\"sortOrder\":\"12\","+
-
-		"	\"defaultValue\" : \"THE DEFAULT VALUE 2\","+
-		"	\"regexCheck\" : \"THE VALIDATION REGEX 2\","+
-		"	\"hint\" : \"THE HINT 2\","+
-
-		"	\"required\" : \"false\","+
-		"	\"searchable\" : \"false\","+
-		"	\"indexed\" : \"false\","+
-		"	\"listed\" : \"false\","+
-		"	\"unique\" : \"false\""+
-	"	}";
-
-	private void assertUpdatedFieldText(Field field) {
-		assertTrue(field instanceof TextField);
-		assertNotNull(field.id());
-		assertEquals("theField1", field.variable());
-
-		assertEquals("THE DEFAULT VALUE 2", field.defaultValue());
-		assertEquals("THE VALIDATION REGEX 2", field.regexCheck());
-		assertEquals("THE HINT 2", field.hint());
-
-		assertFalse(field.required());
-		assertFalse(field.searchable());
-		assertFalse(field.indexed());
-		assertFalse(field.listed());
-		assertFalse(field.unique());
-
-		assertFalse(field.readOnly());
-		assertFalse(field.fixed());
-		assertEquals(12, field.sortOrder());		
+	private void testField(AbstractFieldTester fieldTester) throws Exception {
+		fieldTester.run();
 	}
 
 
-	private void assertResponse_OK(Response response){
+	private static void assertResponse_OK(Response response){
 		assertNotNull(response);
 		assertEquals(response.getStatus(), 200);
 		assertNotNull(response.getEntity());
@@ -251,14 +369,14 @@ public class FieldResourceTest {
 		);
 	}
 
-	private void assertResponse_NOT_FOUND(Response response){
+	private static void assertResponse_NOT_FOUND(Response response){
 		assertNotNull(response);
 		assertEquals(response.getStatus(), 404);
 		assertNotNull(response.getEntity());
 	}
 
 
-	private Field convertMapToField(Map<String, Object> fieldMap) {
+	private static Field convertMapToField(Map<String, Object> fieldMap) {
 		try {
 			return mapper.readValue(
 				mapper.writeValueAsString(fieldMap),
@@ -269,13 +387,13 @@ public class FieldResourceTest {
 		}
 	}
 
-	private ContentType getContentType() throws DotDataException {
+	private static ContentType getContentType() throws DotDataException {
 		User user = APILocator.getUserAPI().getSystemUser();
 
 		return APILocator.getContentTypeAPI(user).search(" velocity_var_name = 'Employee'").get(0);
 	}
 
-	private HttpServletRequest getHttpRequest() {
+	private static HttpServletRequest getHttpRequest() {
 		MockHeaderRequest request = new MockHeaderRequest(
 			(
 				new MockSessionRequest(new MockAttributeRequest(new MockHttpRequest("localhost", "/").request()).request())
