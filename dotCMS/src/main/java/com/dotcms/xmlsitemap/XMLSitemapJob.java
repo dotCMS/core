4d124c40e43a8a988627d53976c3f231dd5825bf
Oswaldo Gallango <oswaldo@dotcms.com>
2017-05-02 13:47:58 -0400

    #10233 exclude url if the uri value is empty

--- dotCMS/src/main/java/com/dotcms/xmlsitemap/XMLSitemapJob.java
+++ dotCMS/src/main/java/com/dotcms/xmlsitemap/XMLSitemapJob.java
@@ -18,6 +18,7 @@ import org.quartz.StatefulJob;
 
 import com.dotcms.contenttype.model.type.ContentType;
 import com.dotcms.contenttype.transform.contenttype.StructureTransformer;
+import com.dotcms.repackage.org.apache.commons.lang.StringUtils;
 import com.dotmarketing.beans.Host;
 import com.dotmarketing.beans.Identifier;
 import com.dotmarketing.beans.Inode;
@@ -305,21 +306,22 @@ public class XMLSitemapJob implements Job, StatefulJob {
 										uri = page.getURI() + "?id=" + contenlet.getInode();
 									}
 								}
-								String urlRelacementText = getUrlPatternReplacementText( host, stVelocityVarName);
+								String urlReplacementText = getUrlPatternReplacementText( host, stVelocityVarName);
 
-								uri = uri.replaceAll(urlRelacementText, "");
+								uri = uri.replaceAll(urlReplacementText, "");
 
 								Logger.debug(this,
 										"Performing URL replacement - urlRelacementText ["
-												+ urlRelacementText
+												+ urlReplacementText
 												+ "], uri [" + uri + "]");
-
-								stringbuf = "<url><loc>"
+								if(StringUtils.isNotEmpty(uri)){
+									stringbuf = "<url><loc>"
 										+ XMLUtils.xmlEscape("http://"
 												+ host.getHostname() + uri)
 										+ "</loc><lastmod>"
 										+ modifiedDateStringValue
 										+ "</lastmod><changefreq>daily</changefreq></url>\n";
+								}
 							} else {
 								stringbuf = "<url><loc>"
 										+ XMLUtils.xmlEscape("http://"
