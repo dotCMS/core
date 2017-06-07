0a3486345eac57e07289527a85b9f51be13adc0f
Oscar Arrieta <oscar.arrieta@dotcms.com>
2017-06-07 08:16:01 -0600

    #11813: Fixing V No. 1 (#11832)
    * #11813: Fixing V No. 1
    ContentTypeFactoryImp.java: Sanitize condition and search before adding to DotConnect.
    SQLUtil.java: Separate Condition and Parameter evil words just as the sanitize methods.
    SQLUtilTest.java: Add tests to check new condition and parameter evil words and methods.
    
    * #11813: Feedback from Code Review:
    ContentTypeFactoryImp.java: As long as we use addParam method we don't need to sanitize it. Driver will take care of that.
    
    * #11813: Feedback from Code Review:
    SQLUtilTest.java: Test should be using sanitizeCondition method.

--- dotCMS/src/main/java/com/dotcms/contenttype/business/ContentTypeFactoryImpl.java
+++ dotCMS/src/main/java/com/dotcms/contenttype/business/ContentTypeFactoryImpl.java
@@ -5,21 +5,12 @@ import com.dotcms.contenttype.exception.NotFoundInDbException;
 import com.dotcms.contenttype.model.field.Field;
 import com.dotcms.contenttype.model.field.FieldBuilder;
 import com.dotcms.contenttype.model.field.HostFolderField;
-import com.dotcms.contenttype.model.type.BaseContentType;
-import com.dotcms.contenttype.model.type.ContentType;
-import com.dotcms.contenttype.model.type.ContentTypeBuilder;
-import com.dotcms.contenttype.model.type.Expireable;
-import com.dotcms.contenttype.model.type.FileAssetContentType;
-import com.dotcms.contenttype.model.type.UrlMapable;
+import com.dotcms.contenttype.model.type.*;
 import com.dotcms.contenttype.transform.contenttype.DbContentTypeTransformer;
 import com.dotcms.contenttype.transform.contenttype.ImplClassContentTypeTransformer;
 import com.dotcms.repackage.javax.validation.constraints.NotNull;
 import com.dotcms.repackage.org.apache.commons.lang.time.DateUtils;
-import com.dotmarketing.business.APILocator;
-import com.dotmarketing.business.CacheLocator;
-import com.dotmarketing.business.DotStateException;
-import com.dotmarketing.business.DotValidationException;
-import com.dotmarketing.business.FactoryLocator;
+import com.dotmarketing.business.*;
 import com.dotmarketing.common.db.DotConnect;
 import com.dotmarketing.common.util.SQLUtil;
 import com.dotmarketing.db.LocalTransaction;
@@ -30,21 +21,11 @@ import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.portlets.fileassets.business.FileAssetAPI;
 import com.dotmarketing.portlets.structure.model.Relationship;
 import com.dotmarketing.portlets.workflows.business.WorkFlowFactory;
-import com.dotmarketing.util.Config;
-import com.dotmarketing.util.Logger;
-import com.dotmarketing.util.UUIDUtil;
-import com.dotmarketing.util.UtilMethods;
-import com.dotmarketing.util.VelocityUtil;
-
+import com.dotmarketing.util.*;
 import org.elasticsearch.indices.IndexMissingException;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
 
 public class ContentTypeFactoryImpl implements ContentTypeFactory {
 
@@ -517,12 +498,12 @@ public class ContentTypeFactoryImpl implements ContentTypeFactory {
     	orderBy = "mod_date";
     }
     DotConnect dc = new DotConnect();
-    dc.setSQL(String.format(this.contentTypeSql.SELECT_QUERY_CONDITION, searchCondition.condition,orderBy));
+    dc.setSQL( String.format( this.contentTypeSql.SELECT_QUERY_CONDITION, SQLUtil.sanitizeCondition( searchCondition.condition ), orderBy ) );
     dc.setMaxRows(limit);
     dc.setStartRow(offset);
-    dc.addParam(searchCondition.search);
-    dc.addParam(searchCondition.search);
-    dc.addParam(searchCondition.search);
+    dc.addParam( searchCondition.search );
+    dc.addParam( searchCondition.search );
+    dc.addParam( searchCondition.search );
     dc.addParam(bottom);
     dc.addParam(top);
     
@@ -539,10 +520,10 @@ public class ContentTypeFactoryImpl implements ContentTypeFactory {
     SearchCondition searchCondition = new SearchCondition(search);
 
     DotConnect dc = new DotConnect();
-    dc.setSQL(String.format(this.contentTypeSql.SELECT_COUNT_CONDITION, searchCondition.condition));
-    dc.addParam(searchCondition.search);
-    dc.addParam(searchCondition.search);
-    dc.addParam(searchCondition.search);
+    dc.setSQL( String.format( this.contentTypeSql.SELECT_COUNT_CONDITION, SQLUtil.sanitizeCondition( searchCondition.condition ) ) );
+    dc.addParam( searchCondition.search );
+    dc.addParam( searchCondition.search );
+    dc.addParam( searchCondition.search );
     dc.addParam(bottom);
     dc.addParam(top);
     return dc.getInt("test");
