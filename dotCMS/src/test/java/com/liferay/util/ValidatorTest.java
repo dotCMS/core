7081e9f555689f89626dcec44d10a9fc32a5307c
Oscar Arrieta <oscar.arrieta@dotcms.com>
2017-06-20 10:32:42 -0600

    #11565: Adding 2 more emails to test

--- dotCMS/src/test/java/com/liferay/util/ValidatorTest.java
+++ dotCMS/src/test/java/com/liferay/util/ValidatorTest.java
@@ -17,7 +17,9 @@ public class ValidatorTest extends UnitTestBase {
     @Test
     public void testValidEmails(){
 
-        final List<String> validEmails = Arrays.asList( "john.doe@dotcms.com", "james.sa'd@test.com" );
+        final List<String> validEmails =
+                Arrays.asList( "john.doe@dotcms.com", "james.sa'd@test.com",
+                        "john+test@dotcms.com", "john+test@test.test.test.test.ag" );
 
         for ( String validEmail : validEmails ) {
             assertTrue( Validator.isEmailAddress( validEmail ) );
