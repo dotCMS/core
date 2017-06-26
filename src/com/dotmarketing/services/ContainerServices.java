0208dfd52072f5321e0bfe0d78d74071c40c377a
Jose Orsini <jose@dotcms.com>
2017-06-26 13:43:21 -0600

    dotCMS/core#11597 backport fix dotCMS/core#10869 for 3.7.2

--- src/com/dotmarketing/services/ContainerServices.java
+++ src/com/dotmarketing/services/ContainerServices.java
@@ -164,10 +164,6 @@ public class ContainerServices {
                	}
 
                	String code = "";
-                
-                sb.append("#set ($structureCode" ).append(
-                		" = $containerAPI.getStructureCode(\"").append( container.getIdentifier() ).append("\", \"$ContentletStructure\"))" );
-
 
                 //### HEADER ###
                 String startTag = "${contentletStart}";
