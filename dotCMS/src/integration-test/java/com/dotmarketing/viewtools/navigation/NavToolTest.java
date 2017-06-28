5fadf72e48f6bc02ae49f1b053aac0eb8de8feb6
Daniel Laguna <dan.laguna@dotcms.com>
2017-06-28 16:05:20 -0600

    #11640: Unit test modified to fit the current DB structure and use the DEFAULT_PAGE_TO_DEFAULT_LANGUAGE property accordingly

--- dotCMS/src/integration-test/java/com/dotmarketing/viewtools/navigation/NavToolTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/viewtools/navigation/NavToolTest.java
@@ -8,6 +8,7 @@ import com.dotmarketing.business.APILocator;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.portlets.folders.model.Folder;
 import com.dotmarketing.portlets.htmlpageasset.model.IHTMLPage;
+import com.dotmarketing.viewtools.LanguageWebAPI;
 import com.liferay.portal.model.User;
 
 import org.junit.BeforeClass;
@@ -17,41 +18,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
+ * NavToolTest
  * Created by Oscar Arrieta on 5/4/15.
  */
 public class NavToolTest extends IntegrationTestBase{
-	
-	@BeforeClass
+
+    private static boolean DEFAULT_PAGE_TO_DEFAULT_LANGUAGE;
+
+    @BeforeClass
     public static void prepare() throws Exception {
         //Setting web app environment
         IntegrationTestInitService.getInstance().init();
-	}
+        DEFAULT_PAGE_TO_DEFAULT_LANGUAGE = LanguageWebAPI.canDefaultPageToDefaultLanguage();
+    }
 
     @Test
     public void testAboutUs() throws Exception { // https://github.com/dotCMS/core/issues/7678
 
         //Using System User.
         User user = APILocator.getUserAPI().getSystemUser();
+
         //Using demo.dotcms.com host.
         Host demoHost = APILocator.getHostAPI().findByName("demo.dotcms.com", user, false);
+
         //Using about us Folder.
         Folder aboutUsFolder = APILocator.getFolderAPI().findFolderByPath("/about-us/", demoHost, user, false);
+
         //Using Identifier to get the path.
         Identifier aboutUsIdentifier=APILocator.getIdentifierAPI().find(aboutUsFolder);
 
-        NavTool navTool = new NavTool();
+        NavResult navResult = NavTool.getNav(demoHost, aboutUsIdentifier.getPath(), 1, user);
+        assertNotNull(navResult);
 
-        NavResult navResult = navTool.getNav(demoHost, aboutUsIdentifier.getPath(), 1, user);
-
-        //We are expecting 4 children result for English Language.
+        //We are expecting 3 children result for English Language.
         int englishResultChildren = navResult.getChildren().size();
+        assertEquals(englishResultChildren, 3);
 
-        navResult = navTool.getNav(demoHost, aboutUsIdentifier.getPath(), 2, user);
+        navResult = NavTool.getNav(demoHost, aboutUsIdentifier.getPath(), 2, user);
+        assertNotNull(navResult);
 
-        //We are expecting 2 children result for Spanish Language.
         int spanishResultChildren = navResult.getChildren().size();
+        if (DEFAULT_PAGE_TO_DEFAULT_LANGUAGE) {
+            //We are expecting 3 children result for Spanish Language.
+            assertEquals(spanishResultChildren, 3);
+        } else {
+            //We are expecting 2 children result for Spanish Language.
+            assertEquals(spanishResultChildren, 2);
+        }
 
         List<IHTMLPage> liveHTMLPages = APILocator.getHTMLPageAssetAPI().getLiveHTMLPages(aboutUsFolder, user, false);
 
@@ -69,7 +86,7 @@ public class NavToolTest extends IntegrationTestBase{
             htmlPageContentlet.getMap().remove("modUser");
 
             htmlPageContentlet.getMap().put("inode", "");
-            htmlPageContentlet.getMap().put("languageId", new Long(2));
+            htmlPageContentlet.getMap().put("languageId", 2L);
 
             //Checkin and Publish.
             Contentlet working = APILocator.getContentletAPI().checkin(htmlPageContentlet, user, false);
@@ -79,10 +96,16 @@ public class NavToolTest extends IntegrationTestBase{
             contentletsCreated.add(working);
         }
 
-        navResult = navTool.getNav(demoHost, aboutUsIdentifier.getPath(), 2, user);
+        navResult = NavTool.getNav(demoHost, aboutUsIdentifier.getPath(), 2, user);
+        assertNotNull(navResult);
 
-        //Now We are expecting same children result for Spanish Language that English Language.
-        assertEquals(englishResultChildren, navResult.getChildren().size());
+        if (DEFAULT_PAGE_TO_DEFAULT_LANGUAGE) {
+            //Now We are expecting more children result for Spanish Language than English Language.
+            assertTrue(englishResultChildren <= navResult.getChildren().size());
+        } else {
+            //Now We are expecting same children result for Spanish Language and English Language.
+            assertEquals(englishResultChildren, navResult.getChildren().size());
+        }
 
         //Now remove all the pages that we created for this tests.
         APILocator.getContentletAPI().unpublish(contentletsCreated, user, false);
@@ -90,7 +113,8 @@ public class NavToolTest extends IntegrationTestBase{
         APILocator.getContentletAPI().delete(contentletsCreated, user, false);
 
         //We should back to 2 in Spanish Nav.
-        navResult = navTool.getNav(demoHost, aboutUsIdentifier.getPath(), 2, user);
+        navResult = NavTool.getNav(demoHost, aboutUsIdentifier.getPath(), 2, user);
+        assertNotNull(navResult);
 
         //Now We are expecting original amount children result for Spanish Language.
         assertEquals(spanishResultChildren, navResult.getChildren().size());
