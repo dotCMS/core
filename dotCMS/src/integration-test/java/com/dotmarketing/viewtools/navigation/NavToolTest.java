a8ad30bc837b2a7a1519fb38aa0962a410ea5909
Daniel Laguna <dan.laguna@dotcms.com>
2017-06-28 17:07:02 -0600

    #11640: New tests included for true and false on the DEFAULT_PAGE_TO_DEFAULT_LANGUAGE property

--- dotCMS/src/integration-test/java/com/dotmarketing/viewtools/navigation/NavToolTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/viewtools/navigation/NavToolTest.java
@@ -5,14 +5,19 @@ import com.dotcms.util.IntegrationTestInitService;
 import com.dotmarketing.beans.Host;
 import com.dotmarketing.beans.Identifier;
 import com.dotmarketing.business.APILocator;
+import com.dotmarketing.business.CacheLocator;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.portlets.folders.model.Folder;
 import com.dotmarketing.portlets.htmlpageasset.model.IHTMLPage;
+import com.dotmarketing.util.Config;
 import com.dotmarketing.viewtools.LanguageWebAPI;
 import com.liferay.portal.model.User;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,19 +30,30 @@ import static org.junit.Assert.assertTrue;
  * NavToolTest
  * Created by Oscar Arrieta on 5/4/15.
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class NavToolTest extends IntegrationTestBase{
 
-    private static boolean DEFAULT_PAGE_TO_DEFAULT_LANGUAGE;
+    private static boolean ORIGINAL_DEFAULT_PAGE_TO_DEFAULT_LANGUAGE;
 
     @BeforeClass
     public static void prepare() throws Exception {
         //Setting web app environment
         IntegrationTestInitService.getInstance().init();
-        DEFAULT_PAGE_TO_DEFAULT_LANGUAGE = LanguageWebAPI.canDefaultPageToDefaultLanguage();
+        ORIGINAL_DEFAULT_PAGE_TO_DEFAULT_LANGUAGE = LanguageWebAPI.canDefaultPageToDefaultLanguage();
+    }
+
+    @AfterClass
+    public static void restoreProperty() {
+        Config.setProperty("DEFAULT_PAGE_TO_DEFAULT_LANGUAGE", ORIGINAL_DEFAULT_PAGE_TO_DEFAULT_LANGUAGE);
+        assertEquals(LanguageWebAPI.canDefaultPageToDefaultLanguage(), ORIGINAL_DEFAULT_PAGE_TO_DEFAULT_LANGUAGE);
     }
 
     @Test
-    public void testAboutUs() throws Exception { // https://github.com/dotCMS/core/issues/7678
+    public void testAboutUsDefaultPageToLanguageTrue() throws Exception { // https://github.com/dotCMS/core/issues/7678
+        CacheLocator.getCacheAdministrator().flushAll();
+
+        Config.setProperty("DEFAULT_PAGE_TO_DEFAULT_LANGUAGE", true);
+        assertEquals(LanguageWebAPI.canDefaultPageToDefaultLanguage(), true);
 
         //Using System User.
         User user = APILocator.getUserAPI().getSystemUser();
@@ -62,19 +78,104 @@ public class NavToolTest extends IntegrationTestBase{
         assertNotNull(navResult);
 
         int spanishResultChildren = navResult.getChildren().size();
-        if (DEFAULT_PAGE_TO_DEFAULT_LANGUAGE) {
-            //We are expecting 3 children result for Spanish Language.
-            assertEquals(spanishResultChildren, 3);
-        } else {
-            //We are expecting 2 children result for Spanish Language.
-            assertEquals(spanishResultChildren, 2);
-        }
+        //We are expecting 3 children result for Spanish Language.
+        assertEquals(spanishResultChildren, 3);
 
         List<IHTMLPage> liveHTMLPages = APILocator.getHTMLPageAssetAPI().getLiveHTMLPages(aboutUsFolder, user, false);
 
         //List of contentlets created for this test.
         List<Contentlet> contentletsCreated = new ArrayList<>();
 
+        createSpanishPagesCopy(user, liveHTMLPages, contentletsCreated);
+
+        navResult = NavTool.getNav(demoHost, aboutUsIdentifier.getPath(), 2, user);
+        assertNotNull(navResult);
+
+        //Now We are expecting more children result for Spanish Language than English Language.
+        assertTrue(englishResultChildren <= navResult.getChildren().size());
+
+        //Now remove all the pages that we created for this tests.
+        APILocator.getContentletAPI().unpublish(contentletsCreated, user, false);
+        APILocator.getContentletAPI().archive(contentletsCreated, user, false);
+        APILocator.getContentletAPI().delete(contentletsCreated, user, false);
+
+        //We should back to 2 in Spanish Nav.
+        navResult = NavTool.getNav(demoHost, aboutUsIdentifier.getPath(), 2, user);
+        assertNotNull(navResult);
+
+        //Now We are expecting original amount children result for Spanish Language.
+        assertEquals(spanishResultChildren, navResult.getChildren().size());
+    }
+
+    @Test
+    public void testAboutUsDefaultPageToLanguageFalse() throws Exception { // https://github.com/dotCMS/core/issues/7678
+        CacheLocator.getCacheAdministrator().flushAll();
+
+        Config.setProperty("DEFAULT_PAGE_TO_DEFAULT_LANGUAGE", false);
+        assertEquals(LanguageWebAPI.canDefaultPageToDefaultLanguage(), false);
+
+        //Using System User.
+        User user = APILocator.getUserAPI().getSystemUser();
+
+        //Using demo.dotcms.com host.
+        Host demoHost = APILocator.getHostAPI().findByName("demo.dotcms.com", user, false);
+
+        //Using about us Folder.
+        Folder aboutUsFolder = APILocator.getFolderAPI().findFolderByPath("/about-us/", demoHost, user, false);
+
+        //Using Identifier to get the path.
+        Identifier aboutUsIdentifier=APILocator.getIdentifierAPI().find(aboutUsFolder);
+
+        NavResult navResult = NavTool.getNav(demoHost, aboutUsIdentifier.getPath(), 1, user);
+        assertNotNull(navResult);
+
+        //We are expecting 3 children result for English Language.
+        int englishResultChildren = navResult.getChildren().size();
+        assertEquals(englishResultChildren, 3);
+
+        navResult = NavTool.getNav(demoHost, aboutUsIdentifier.getPath(), 2, user);
+        assertNotNull(navResult);
+
+        int spanishResultChildren = navResult.getChildren().size();
+        //We are expecting 2 children result for Spanish Language.
+        assertEquals(spanishResultChildren, 2);
+
+        List<IHTMLPage> liveHTMLPages = APILocator.getHTMLPageAssetAPI().getLiveHTMLPages(aboutUsFolder, user, false);
+
+        //List of contentlets created for this test.
+        List<Contentlet> contentletsCreated = new ArrayList<>();
+
+        createSpanishPagesCopy(user, liveHTMLPages, contentletsCreated);
+
+        navResult = NavTool.getNav(demoHost, aboutUsIdentifier.getPath(), 2, user);
+        assertNotNull(navResult);
+
+        //Now We are expecting same children result for Spanish Language and English Language.
+        assertEquals(englishResultChildren, navResult.getChildren().size());
+
+        //Now remove all the pages that we created for this tests.
+        APILocator.getContentletAPI().unpublish(contentletsCreated, user, false);
+        APILocator.getContentletAPI().archive(contentletsCreated, user, false);
+        APILocator.getContentletAPI().delete(contentletsCreated, user, false);
+
+        //We should back to 2 in Spanish Nav.
+        navResult = NavTool.getNav(demoHost, aboutUsIdentifier.getPath(), 2, user);
+        assertNotNull(navResult);
+
+        //Now We are expecting original amount children result for Spanish Language.
+        assertEquals(spanishResultChildren, navResult.getChildren().size());
+
+    }
+
+    /**
+     * Create a copy of the spanish pages
+     *
+     * @param user                      The user
+     * @param liveHTMLPages         The live HTML pages
+     * @param contentletsCreated    The contentlets created
+     * @throws Exception            Any Exception
+     */
+    private void createSpanishPagesCopy(User user, List<IHTMLPage> liveHTMLPages, List<Contentlet> contentletsCreated) throws Exception {
         //We need to create a new copy of pages for Spanish.
         for(IHTMLPage liveHTMLPage : liveHTMLPages){
             Contentlet htmlPageContentlet = APILocator.getContentletAPI().find( liveHTMLPage.getInode(), user, false );
@@ -95,30 +196,5 @@ public class NavToolTest extends IntegrationTestBase{
 
             contentletsCreated.add(working);
         }
-
-        navResult = NavTool.getNav(demoHost, aboutUsIdentifier.getPath(), 2, user);
-        assertNotNull(navResult);
-
-        if (DEFAULT_PAGE_TO_DEFAULT_LANGUAGE) {
-            //Now We are expecting more children result for Spanish Language than English Language.
-            assertTrue(englishResultChildren <= navResult.getChildren().size());
-        } else {
-            //Now We are expecting same children result for Spanish Language and English Language.
-            assertEquals(englishResultChildren, navResult.getChildren().size());
-        }
-
-        //Now remove all the pages that we created for this tests.
-        APILocator.getContentletAPI().unpublish(contentletsCreated, user, false);
-        APILocator.getContentletAPI().archive(contentletsCreated, user, false);
-        APILocator.getContentletAPI().delete(contentletsCreated, user, false);
-
-        //We should back to 2 in Spanish Nav.
-        navResult = NavTool.getNav(demoHost, aboutUsIdentifier.getPath(), 2, user);
-        assertNotNull(navResult);
-
-        //Now We are expecting original amount children result for Spanish Language.
-        assertEquals(spanishResultChildren, navResult.getChildren().size());
-
     }
-
 }
