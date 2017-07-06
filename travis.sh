ad8ac5738cd7dbdca0e4a0a3a7d65f598df65f52
Jonathan Gamba <jonathan.gamba@dotcms.com>
2017-07-06 07:11:02 -0600

    Travis/SonarCloud tests

--- travis.sh
+++ travis.sh
@@ -4,11 +4,8 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   echo "Running sonar scanner in change preview mode"
 
   sonar-scanner \
-    -Dsonar.host.url=$SONAR_URL \
     -Dsonar.analysis.mode=preview \
-    -Dsonar.login=$SONAR_TOKEN \
     -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-    -Dsonar.github.oauth=$SONAR_GITHUB_TOKEN \
     -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
     -Dsonar.scanner.skip=false
 
