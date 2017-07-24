eb7102b7d508a99152705fb4fee3aa488d985cc3
Nollymar Longa <nollymar@gmail.com>
2017-07-24 13:12:52 -0500

    Origin/issue 12141 pp event listener bus poc (#12192)
    * #12141 Loca system events first poc
    
    * #12141 Added first unit test
    
    * #12141 Added some minor changes and some improvements to handle orphan events
    
    * #12141 Solving sonar violations
    
    * #12141 Rollback of last change
    
    * #12141 Solving sonar violations

---
+++ dotCMS/src/test/java/com/dotcms/api/system/event/local/TestAnnotatedForDeleteSubscriber.java
@@ -0,0 +1,20 @@
+package com.dotcms.api.system.event.local;
+
+/**
+ * Just for testing
+ */
+public class TestAnnotatedForDeleteSubscriber {
+
+    private boolean called = false;
+
+    @Subscriber
+    public void test(TestEventType1 testEventType1) {
+
+        System.out.println(testEventType1.getMsg());
+        called = true;
+    }
+
+    public boolean isCalled() {
+        return called;
+    }
+}
