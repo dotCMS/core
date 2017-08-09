2852ff5e8143994ce21c9fc75092a6ba83c834f6
Daniel Silva <daniel.silva@dotcms.com>
2017-08-09 18:33:43 -0500

    #12276 Defensive copy to prevent mutation.

--- dotCMS/src/main/java/com/dotmarketing/common/db/DotTimezonedTimestamp.java
+++ dotCMS/src/main/java/com/dotmarketing/common/db/DotTimezonedTimestamp.java
@@ -20,7 +20,7 @@ public class DotTimezonedTimestamp {
     }
 
     public Timestamp getTimestamp() {
-        return timestamp;
+        return new Timestamp(timestamp.getTime());
     }
 
     public String getTimezone() {
