5e56c118347fa394c08a6f18f32600e04ff6b5bd
Jonathan <jsanca@Jonathans-MacBook-Pro-2.local>
2017-08-14 16:29:09 -0600

    #12337 Added a refactor to do a new approach for cache vanity key

--- dotCMS/src/main/java/com/dotcms/util/VanityUrlUtil.java
+++ dotCMS/src/main/java/com/dotcms/util/VanityUrlUtil.java
@@ -1,12 +1,7 @@
 package com.dotcms.util;
 
-import com.dotcms.contenttype.model.type.VanityUrlContentType;
-import com.dotmarketing.business.APILocator;
-import com.dotmarketing.exception.DotDataException;
-import com.dotmarketing.exception.DotSecurityException;
-import com.dotmarketing.portlets.contentlet.business.HostAPI;
-import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.util.Logger;
+
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
@@ -20,61 +15,9 @@ import java.util.regex.PatternSyntaxException;
  */
 public class VanityUrlUtil {
 
-    private static final HostAPI hostAPI = APILocator.getHostAPI();
 
     private VanityUrlUtil() {
 
-    }
-
-    /**
-     * Generate the sanitized cache key
-     *
-     * @param hostId The current host Identifier
-     * @param uri The current URI
-     * @param languageId The current languageId
-     * @return String with the sanitized key name
-     */
-    public static String sanitizeKey(final String hostId, final String uri, final long languageId) {
-        return hostId + "|" + uri.replace('/', '|') + "|lang_" + languageId;
-    }
-
-    /**
-     * Generate the sanitized cache key
-     *
-     * @param vanityUrl The vanity Url contentlet
-     * @return String with the sanitized key name
-     */
-    public static String sanitizeKey(final Contentlet vanityUrl)
-            throws DotDataException, DotSecurityException {
-
-        return sanitizeKey(vanityUrl.getStringProperty(VanityUrlContentType.SITE_FIELD_VAR),
-                vanityUrl.getStringProperty(VanityUrlContentType.URI_FIELD_VAR),
-                vanityUrl.getLanguageId());
-    }
-
-    /**
-     * Generate the sanitized cache key for the second cache
-     *
-     * @param hostId The current host Identifier
-     * @param languageId The current languageId
-     * @return String with the sanitized key name
-     */
-    public static String sanitizeSecondCacheKey(final String hostId, final long languageId) {
-        return hostId + "| lang_" + languageId;
-    }
-
-    /**
-     * Generate the sanitized cache key
-     *
-     * @param vanityUrl The vanity Url contentlet
-     * @return String with the sanitized key name
-     */
-    public static String sanitizeSecondCacheKey(final Contentlet vanityUrl)
-            throws DotDataException, DotSecurityException {
-
-        return sanitizeSecondCacheKey(
-                vanityUrl.getStringProperty(VanityUrlContentType.SITE_FIELD_VAR),
-                vanityUrl.getLanguageId());
     }
 
     /**
