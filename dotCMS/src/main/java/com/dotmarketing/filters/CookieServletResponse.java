92805c7a386c040e64e99a3d7c38283f376271b7
Erick Gonzalez <erick.gonzalez@dotcms.com>
2017-08-22 16:14:59 -0600

    #11912 format class

--- dotCMS/src/main/java/com/dotmarketing/filters/CookieServletResponse.java
+++ dotCMS/src/main/java/com/dotmarketing/filters/CookieServletResponse.java
@@ -1,7 +1,8 @@
 /*
  * ServletResponseCharacterEncoding.java
  *
- * Created on 24 October 2007
+ * Created on 8/10/2017
+ * @author Will Ezell
  */
 package com.dotmarketing.filters;
 
@@ -21,30 +22,22 @@ import com.dotmarketing.util.Config;
 public class CookieServletResponse extends HttpServletResponseWrapper {
 
 
-	public CookieServletResponse(HttpServletRequest request, HttpServletResponse response) {
-		super(response);
-	}
+    public CookieServletResponse(HttpServletRequest request, HttpServletResponse response) {
+        super(response);
+    }
 
     public CookieServletResponse(ServletRequest request, ServletResponse response) {
-        this((HttpServletRequest) request,(HttpServletResponse) response);
+        this((HttpServletRequest) request, (HttpServletResponse) response);
     }
-    
+
     @Override
     public void addCookie(Cookie cookie) {
 
-
         if (Config.getBooleanProperty(COOKIES_HTTP_ONLY, false)) {
-            cookie.setHttpOnly(true);          
+            cookie.setHttpOnly(true);
         }
-        
-        
+
         super.addCookie(cookie);
     }
 
-	
-
-	
-	
-	
-	
 }
