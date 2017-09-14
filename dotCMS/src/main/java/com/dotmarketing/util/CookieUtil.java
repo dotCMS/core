a8a68b0652e8f6796b154e457300c7536415ca08
erickgonzalez <erick.gonzalez@dotcms.com>
2017-09-14 09:29:37 -0600

    #12572 secure flag verification (#12589)

--- dotCMS/src/main/java/com/dotmarketing/util/CookieUtil.java
+++ dotCMS/src/main/java/com/dotmarketing/util/CookieUtil.java
@@ -21,8 +21,8 @@ import static com.liferay.util.CookieUtil.COOKIES_SECURE_FLAG;
  */
 public class CookieUtil {
 
-    private static final String ALWAYS = "always";
-    private static final String HTTPS = "https";
+    public static final String ALWAYS = "always";
+    public static final String HTTPS = "https";
     private static final String URI = "/";
     private static final int MAX_AGE_DAY_MILLIS = 60 * 60 * 24;
     public static final int DEFAULT_JWT_MAX_AGE_DAYS = 14;
