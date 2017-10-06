2c8b071f4bc38873a88772021fdca11f74d18630
Oscar Arrieta <oscar.arrieta@dotcms.com>
2017-10-06 10:09:50 -0600

    #12671: path to lowe case and refactor

--- dotCMS/src/main/java/com/dotmarketing/webdav/BasicFolderResourceImpl.java
+++ dotCMS/src/main/java/com/dotmarketing/webdav/BasicFolderResourceImpl.java
@@ -12,7 +12,6 @@ import com.dotcms.repackage.org.dts.spell.utils.FileUtils;
 import com.dotmarketing.beans.Host;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.exception.DotRuntimeException;
-import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.portlets.fileassets.business.IFileAsset;
 import com.dotmarketing.util.Logger;
 import com.liferay.portal.model.User;
@@ -31,7 +30,7 @@ public abstract class BasicFolderResourceImpl implements FolderResource {
     protected long lang = APILocator.getLanguageAPI().getDefaultLanguage().getId();
     
     public BasicFolderResourceImpl(String path) {
-        this.path=path;
+        this.path=path.toLowerCase();
         try {
             this.host=APILocator.getHostAPI().findByName(
                     dotDavHelper.getHostName(path),APILocator.getUserAPI().getSystemUser(),false);
@@ -58,36 +57,26 @@ public abstract class BasicFolderResourceImpl implements FolderResource {
         if(!path.endsWith("/")){
             path = path + "/";
         }
-        if(!dotDavHelper.isTempResource(newName)){
 
+        if(!dotDavHelper.isTempResource(newName)){
             try {
-            	IFileAsset f = null;
             	dotDavHelper.setResourceContent(path + newName, in, contentType, null, java.util.Calendar.getInstance().getTime(), user, isAutoPub);
-                f = dotDavHelper.loadFile(path + newName,user);
-                FileResourceImpl fr = new FileResourceImpl(f, f.getFileName());
+                IFileAsset f = dotDavHelper.loadFile(path + newName,user);
+                Resource fr = new FileResourceImpl(f, f.getFileName());
                 return fr;
                 
-            }catch (DotSecurityException dotE){
+            }catch (Exception e){
             	Logger.error(this, "An error occurred while creating new file: " + (newName != null ? newName : "Unknown") 
                 		+ " in this path: " + (path != null ? path : "Unknown") + " " 
-                		+ dotE.getMessage(), dotE);
-            	throw new DotRuntimeException(dotE.getMessage(), dotE);
-            	
-            }catch (Exception e) {
-                Logger.error(this, "An error occurred while creating new file: " + (newName != null ? newName : "Unknown") 
-                		+ " in this path: " + (path != null ? path : "Unknown") + " " 
                 		+ e.getMessage(), e);
+            	throw new DotRuntimeException(e.getMessage(), e);
             }
+        } else {
+            File f = dotDavHelper.createTempFile("/" + host.getHostname() + path + newName);
+            FileUtils.copyStreamToFile(f, in, null);
+            Resource tr = new TempFileResourceImpl(f, path + newName, isAutoPub);
+            return tr;
         }
-        
-        String p = path;
-        if(!p.endsWith("/")){
-            p = p + "/";
-        }
-        File f = dotDavHelper.createTempFile("/" + host.getHostname() + p + newName);
-        FileUtils.copyStreamToFile(f, in, null);
-        TempFileResourceImpl tr = new TempFileResourceImpl(f, path + newName, isAutoPub);
-        return tr;
     }
 
     
