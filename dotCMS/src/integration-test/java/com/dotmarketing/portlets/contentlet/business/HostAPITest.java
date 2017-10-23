7efcdba93eee9368649b9047861105b3f7d23d4c
Jose Castro <jose.castro@dotcms.com>
2017-10-23 09:55:22 -0600

    #12881 : Creating two types of commit listeners to easily handle which are synchronous and asynchronous. Adding Javadoc. For deleting users, we needed the synchronous listener because we needed to use the main dotCMS thread to access the temporary table.

--- dotCMS/src/integration-test/java/com/dotmarketing/portlets/contentlet/business/HostAPITest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/portlets/contentlet/business/HostAPITest.java
@@ -113,86 +113,82 @@ public class HostAPITest {
     
     @Test
     public void makeDefault() throws Exception {
-	    try {
             User user = APILocator.getUserAPI().getSystemUser();
     	/*
     	 * Get the current Default host
     	 */
-            Host hdef = APILocator.getHostAPI().findDefaultHost(user, false);
-    	
+        Host hdef = APILocator.getHostAPI().findDefaultHost(user, false);
+
     	/*
     	 * Create a new Host and make it default
     	 */
-            Host host = new Host();
-            host.setHostname("test" + System.currentTimeMillis() + ".demo.dotcms.com");
-            host.setDefault(false);
-            try {
-                HibernateUtil.startTransaction();
-                host = APILocator.getHostAPI().save(host, user, false);
-                HibernateUtil.closeAndCommitTransaction();
-            } catch (Exception e) {
-                HibernateUtil.rollbackTransaction();
-                Logger.error(HostAPITest.class, e.getMessage());
-            }
-            APILocator.getHostAPI().publish(host, user, false);
-            APILocator.getHostAPI().makeDefault(host, user, false);
+        Host host = new Host();
+        host.setHostname("test" + System.currentTimeMillis() + ".demo.dotcms.com");
+        host.setDefault(false);
+        try {
+            HibernateUtil.startTransaction();
+            host = APILocator.getHostAPI().save(host, user, false);
+            HibernateUtil.closeAndCommitTransaction();
+        } catch (Exception e) {
+            HibernateUtil.rollbackTransaction();
+            Logger.error(HostAPITest.class, e.getMessage());
+        }
+        APILocator.getHostAPI().publish(host, user, false);
+        APILocator.getHostAPI().makeDefault(host, user, false);
 
-            host = APILocator.getHostAPI().find(host.getIdentifier(), user, false);
-            APILocator.getContentletAPI().isInodeIndexed(host.getInode());
-            APILocator.getContentletAPI().isInodeIndexed(host.getInode(), true);
-            hdef = APILocator.getHostAPI().find(hdef.getIdentifier(), user, false);
-            APILocator.getContentletAPI().isInodeIndexed(hdef.getInode());
-            APILocator.getContentletAPI().isInodeIndexed(hdef.getInode(), true);
-        
+        host = APILocator.getHostAPI().find(host.getIdentifier(), user, false);
+        APILocator.getContentletAPI().isInodeIndexed(host.getInode());
+        APILocator.getContentletAPI().isInodeIndexed(host.getInode(), true);
+        hdef = APILocator.getHostAPI().find(hdef.getIdentifier(), user, false);
+        APILocator.getContentletAPI().isInodeIndexed(hdef.getInode());
+        APILocator.getContentletAPI().isInodeIndexed(hdef.getInode(), true);
+
         /*
          * Validate if the previous default host. Is live and not default
          */
-            Assert.assertTrue(hdef.isLive());
-            Assert.assertFalse(hdef.isDefault());
-        
+        Assert.assertTrue(hdef.isLive());
+        Assert.assertFalse(hdef.isDefault());
+
         /*
          * get Back to default the previous host
          */
-            APILocator.getHostAPI().makeDefault(hdef, user, false);
+        APILocator.getHostAPI().makeDefault(hdef, user, false);
 
-            host = APILocator.getHostAPI().find(host.getIdentifier(), user, false);
-            APILocator.getContentletAPI().isInodeIndexed(host.getInode());
-            APILocator.getContentletAPI().isInodeIndexed(host.getInode(), true);
-            hdef = APILocator.getHostAPI().find(hdef.getIdentifier(), user, false);
-            APILocator.getContentletAPI().isInodeIndexed(hdef.getInode());
-            APILocator.getContentletAPI().isInodeIndexed(hdef.getInode(), true);
-        
+        host = APILocator.getHostAPI().find(host.getIdentifier(), user, false);
+        APILocator.getContentletAPI().isInodeIndexed(host.getInode());
+        APILocator.getContentletAPI().isInodeIndexed(host.getInode(), true);
+        hdef = APILocator.getHostAPI().find(hdef.getIdentifier(), user, false);
+        APILocator.getContentletAPI().isInodeIndexed(hdef.getInode());
+        APILocator.getContentletAPI().isInodeIndexed(hdef.getInode(), true);
+
         /*
          * Validate if the new host is not default anymore and if its live
          */
-            Assert.assertFalse(host.isDefault());
-            Assert.assertTrue(host.isLive());
+        Assert.assertFalse(host.isDefault());
+        Assert.assertTrue(host.isLive());
 
-            Assert.assertTrue(hdef.isLive());
-            Assert.assertTrue(hdef.isDefault());
-        
+        Assert.assertTrue(hdef.isLive());
+        Assert.assertTrue(hdef.isDefault());
+
         /*
          * Delete the new test host
          */
-            Thread.sleep(600); // wait a bit for the index
-            try {
-                HibernateUtil.startTransaction();
-                APILocator.getHostAPI().archive(host, user, false);
-                APILocator.getHostAPI().delete(host, user, false);
-                HibernateUtil.closeAndCommitTransaction();
-            } catch (Exception e) {
-                HibernateUtil.rollbackTransaction();
-                Logger.error(HostAPITest.class, e.getMessage());
-            }
-            Thread.sleep(600); // wait a bit for the index
+        Thread.sleep(600); // wait a bit for the index
+        try {
+            HibernateUtil.startTransaction();
+            APILocator.getHostAPI().archive(host, user, false);
+            APILocator.getHostAPI().delete(host, user, false);
+            HibernateUtil.closeAndCommitTransaction();
+        } catch (Exception e) {
+            HibernateUtil.rollbackTransaction();
+            Logger.error(HostAPITest.class, e.getMessage());
+        }
+        Thread.sleep(600); // wait a bit for the index
         /*
          * Validate if the current Original default host is the current default one
          */
-            host = APILocator.getHostAPI().findDefaultHost(user, false);
-            Assert.assertEquals(hdef.getIdentifier(), host.getIdentifier());
-        } finally {
-            HibernateUtil.setAsyncCommitListenersFinalization(true);
-        }
+        host = APILocator.getHostAPI().findDefaultHost(user, false);
+        Assert.assertEquals(hdef.getIdentifier(), host.getIdentifier());
     }
     
     @Test
