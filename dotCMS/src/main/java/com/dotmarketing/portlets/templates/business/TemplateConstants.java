b32a9d246f84815cbd8832d7c9d324a7db808909
Andre Curione <andre.curione@dotcms.com>
2017-12-06 10:39:37 -0600

    #13097 - Fix in Integration Test

--- dotCMS/src/main/java/com/dotmarketing/portlets/templates/business/TemplateConstants.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/templates/business/TemplateConstants.java
@@ -5,4 +5,7 @@ public class TemplateConstants {
     public static final String TEMPLATE_DELETE_ERROR = "message.template.full_delete.error";
     public static final int TEMPLATE_DEPENDENCY_SEARCH_LIMIT = 10;
 
+    private TemplateConstants() {
+        throw new IllegalStateException("Constants class");
+    }
 }
