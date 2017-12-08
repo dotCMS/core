30a5211b57dc1efe1678b0993c540f7702e6067e
Andre Curione <andre.curione@dotcms.com>
2017-12-08 11:04:18 -0600

    #13169 - Push Publish - Allow site bundle creation when default language has been changed

--- dotCMS/src/main/java/com/dotmarketing/portlets/rules/util/RulePermissionableUtil.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/rules/util/RulePermissionableUtil.java
@@ -37,9 +37,12 @@ public class RulePermissionableUtil {
                 if(iden.getAssetType().equals("folder")){
                     permissionableParent = APILocator.getFolderAPI().find(parent,systemUser,false);
                 }else{
-					Contentlet contentlet = APILocator.getContentletAPI().findContentletByIdentifier(parent, false,
-							APILocator.getLanguageAPI().getDefaultLanguage().getId(), systemUser, false);
-					if (contentlet.isHost()) {
+					//Contentlet contentlet = APILocator.getContentletAPI().findContentletByIdentifier(parent, false,
+					//		APILocator.getLanguageAPI().getDefaultLanguage().getId(), systemUser, false);
+                    Contentlet contentlet = APILocator.getContentletAPI()
+                            .search("+identifier:" + parent + " +working:true", 1, 0, null, systemUser, false)
+                            .get(0);
+                    if (contentlet.isHost()) {
 						permissionableParent = contentlet;
 					} else {
 						permissionableParent = contentlet.getParentPermissionable();
