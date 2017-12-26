181f1e8daed8ec36ee2d6e09da0a7143c0b30f93
freddyrodriguez <freddy0309@gmail.com>
2017-12-26 09:58:20 -0600

    refactoring

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/contenttype/ContentTypeForm.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/contenttype/ContentTypeForm.java
@@ -3,21 +3,17 @@ package com.dotcms.rest.api.v1.contenttype;
 import com.dotcms.contenttype.model.type.ContentType;
 import com.dotcms.contenttype.transform.contenttype.JsonContentTypeTransformer;
 import com.dotcms.repackage.com.fasterxml.jackson.core.JsonParser;
-import com.dotcms.repackage.com.fasterxml.jackson.core.JsonProcessingException;
 import com.dotcms.repackage.com.fasterxml.jackson.databind.DeserializationContext;
 import com.dotcms.repackage.com.fasterxml.jackson.databind.JsonDeserializer;
 import com.dotcms.repackage.com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.dotcms.repackage.com.google.common.annotations.VisibleForTesting;
-import com.dotmarketing.business.DotStateException;
 import com.dotmarketing.exception.DotRuntimeException;
 import com.dotmarketing.util.json.JSONArray;
 import com.dotmarketing.util.json.JSONException;
 import com.dotmarketing.util.json.JSONObject;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -46,10 +42,13 @@ public class ContentTypeForm  {
 
     public static final class ContentTypeFormDeserialize extends JsonDeserializer<ContentTypeForm> {
 
-        @Override
-        public ContentTypeForm deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
-            String json = jsonParser.readValueAsTree().toString();
+        public static final String WORKFLOW_ATTRIBUTE_NAME = "workflow";
 
+        @Override
+        public ContentTypeForm deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+                throws IOException {
+
+            String json = jsonParser.readValueAsTree().toString();
             return buildForm(json);
         }
 
@@ -63,7 +62,9 @@ public class ContentTypeForm  {
             return new ContentTypeForm(entries);
         }
 
-        private List<ContentTypeFormEntry> getContentTypeFormEntries(List<ContentType> typesToSave, List<List<String>> workflows) {
+        private List<ContentTypeFormEntry> getContentTypeFormEntries(List<ContentType> typesToSave,
+                                                                     List<List<String>> workflows) {
+
             List<ContentTypeFormEntry> entries = new ArrayList<>();
 
             for (int i = 0; i < workflows.size(); i++) {
@@ -75,44 +76,45 @@ public class ContentTypeForm  {
             }
             return entries;
         }
-    }
 
-    private static List<List<String>> getWorkflowIdsFromJson(String json) {
-        final List<List<String>> workflows = new ArrayList<List<String>>();
+        private static List<List<String>> getWorkflowIdsFromJson(String json) {
+            final List<List<String>> workflows = new ArrayList<>();
 
-        try {
-            JSONArray jarr = new JSONArray(json);
-
-            for (int i = 0; i < jarr.size(); i++) {
-                final JSONObject fieldJsonObject = (JSONObject) jarr.get(i);
-
-                if (fieldJsonObject.has("workflow")){
-                    getWorkflowsId(workflows, fieldJsonObject);
-                }
-            }
-        } catch (Exception e) {
             try {
-                final JSONObject fieldJsonObject = new JSONObject(json);
+                JSONArray jarr = new JSONArray(json);
 
-                if (fieldJsonObject.has("workflow")){
-                    getWorkflowsId(workflows, fieldJsonObject);
+                for (int i = 0; i < jarr.size(); i++) {
+                    final JSONObject fieldJsonObject = (JSONObject) jarr.get(i);
+
+                    if (fieldJsonObject.has("workflow")){
+                        getWorkflowsId(workflows, fieldJsonObject);
+                    }
                 }
-            } catch (Exception ex) {
-                throw new DotRuntimeException(ex);
+            } catch (JSONException e) {
+
+                try {
+                    final JSONObject  fieldJsonObject = new JSONObject(json);
+
+                    if (fieldJsonObject.has(WORKFLOW_ATTRIBUTE_NAME)){
+                        getWorkflowsId(workflows, fieldJsonObject);
+                    }
+                } catch (JSONException e1) {
+                    throw new DotRuntimeException(e1);
+                }
             }
-        }
-        return workflows;
-    }
-
-    private static void getWorkflowsId(List<List<String>> workflows, JSONObject fieldJsonObject) throws JSONException {
-        JSONArray workflowsJaonArray = (JSONArray) fieldJsonObject.get("workflow");
-        List<String> worflowsArray = new ArrayList<>(workflowsJaonArray.size());
-
-        for (int k = 0; k < workflowsJaonArray.size(); k++) {
-            worflowsArray.add((String) workflowsJaonArray.get(k));
+            return workflows;
         }
 
-        workflows.add(worflowsArray);
+        private static void getWorkflowsId(List<List<String>> workflows, JSONObject fieldJsonObject) throws JSONException {
+            JSONArray workflowsJaonArray = (JSONArray) fieldJsonObject.get(WORKFLOW_ATTRIBUTE_NAME);
+            List<String> worflowsArray = new ArrayList<>(workflowsJaonArray.size());
+
+            for (int k = 0; k < workflowsJaonArray.size(); k++) {
+                worflowsArray.add((String) workflowsJaonArray.get(k));
+            }
+
+            workflows.add(worflowsArray);
+        }
     }
 
     public static class ContentTypeFormEntry {
