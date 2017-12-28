bb1f2e2ab49cad89a0fa152c5b5a0c4fb1e38a35
acurionedotcms <32649325+acurionedotcms@users.noreply.github.com>
2017-12-28 08:13:29 -0600

    #13237 - Oracle - Transformers - Integer cast fixes (#13264)

--- dotCMS/src/main/java/com/dotmarketing/portlets/containers/transform/ContainerTransformer.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/containers/transform/ContainerTransformer.java
@@ -47,9 +47,9 @@ public class ContainerTransformer implements DBTransformer {
         container.setTitle((String) map.get("title"));
         container.setModDate((Date) map.get("mod_date"));
         container.setModUser((String) map.get("mod_user"));
-        container.setSortOrder((Integer) map.getOrDefault("sort_order",0));
+        container.setSortOrder(ConversionUtils.toInt(map.get("sort_order"),0));
         container.setFriendlyName((String) map.get("friendly_name"));
-        container.setMaxContentlets((Integer) map.getOrDefault("max_contentlets",0));
+        container.setMaxContentlets(ConversionUtils.toInt(map.get("max_contentlets"),0));
         container.setUseDiv(ConversionUtils.toBooleanFromDb(map.getOrDefault("use_div",false)));
         container.setStaticify(ConversionUtils.toBooleanFromDb(map.getOrDefault("staticify",false)));
         container.setSortContentletsBy((String) map.get("sort_contentlets_by"));
