d8ec8c3f1968c45b33ca071163aad0d185badb64
Nollymar Longa <nollymar.longa@dotcms.com>
2018-01-04 16:42:06 -0500

    Just for testing

--- dotCMS/src/integration-test/java/com/dotcms/util/TestInitialContext.java
+++ dotCMS/src/integration-test/java/com/dotcms/util/TestInitialContext.java
@@ -23,7 +23,7 @@ public class TestInitialContext extends InitialContext {
     private static TestInitialContext context;
 
     private TestInitialContext() throws NamingException {
-        //xd
+        //x
         String dbType = "postgres.";
 
         if (System.getProperty("databaseType")!=null){
