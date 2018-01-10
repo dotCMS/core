a58a93040e3e08fb04f28ef66e59134f18ba24d7
José Orsini <jose@dotcms.com>
2018-01-10 08:29:17 -0600

    #12266 fixes (#13228)
    * #12266 fixes
    
    * #12266 fixes

--- dotCMS/src/main/java/com/dotmarketing/portlets/cmsmaintenance/util/AssetFileNameFilter.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/cmsmaintenance/util/AssetFileNameFilter.java
@@ -1,13 +1,19 @@
 package com.dotmarketing.portlets.cmsmaintenance.util;
 
+import com.google.common.collect.ImmutableSet;
 import java.io.File;
 import java.io.FileFilter;
 
 import com.dotmarketing.util.Config;
 import com.dotmarketing.util.Logger;
 import com.liferay.util.FileUtil;
+import java.util.Set;
 
 public class AssetFileNameFilter implements FileFilter {
+
+    private static final Set<String> EXCLUDE_FOLDERS_LIST = ImmutableSet
+            .of("license", "bundles", "tmp_upload",
+                    "timemachine", "integrity", "server", "dotGenerated");
 
 	@Override
 	public boolean accept(File dir) {
@@ -15,48 +21,35 @@ public class AssetFileNameFilter implements FileFilter {
 			return false;
 		}
 
-
 		if(dir.getAbsolutePath().contains("dotGenerated") ){
 			return false;
 		}
+
 		String name = dir.getName();
-		String osname = System.getProperty("os.name");
-		String[] path = null;
+		final String osName = System.getProperty("os.name");
 
-		if (osname.startsWith("Windows"))
+		String[] path;
+
+		if (osName.startsWith("Windows")) {
 			path = dir.getAbsolutePath().split("\\\\");
-		else
+		}
+		else {
 			path = dir.getAbsolutePath().split(File.separator);
-
+		}
 
 		String[] test = new String[0];
-		String assetPath=null;
+
+		String assetPath;
+
         try {
         	assetPath = Config.getStringProperty("ASSET_REAL_PATH", FileUtil.getRealPath(Config.getStringProperty("ASSET_PATH")));
         	test = new File(assetPath).getAbsolutePath().split(File.separator);
         } catch (Exception e) {
         	Logger.debug(this.getClass(), e.getMessage());
         }
-        if(test.length +1 == path.length){
 
-			if( "license".equals(name)){
-				return false;
-			}
-			else if( "bundles".equals(name)){
-				return false;
-			}
-			else if( "tmp_upload".equals(name)){
-				return false;
-			}
-			else if( name.startsWith(".")){
-				return false;
-	        }
-			else if(name.equals("timemachine")) {
-			    return false;
-			}
-        }
-		return true;
-
+        return test.length + 1 != path.length || (name.charAt(0) != '.' && !EXCLUDE_FOLDERS_LIST.contains(name));
+        
 	}
 
 }
