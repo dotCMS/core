6390b68bc283c9031cde7ab50097c2abd97f7ab7
Nollymar Longa <nollymar.longa@dotcms.com>
2018-01-11 13:00:55 -0500

    Fixing test ContentletCheckInTest.checkInvalidFileContent

--- dotCMS/src/integration-test/java/com/dotmarketing/portlets/contentlet/business/ContentletCheckInTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/portlets/contentlet/business/ContentletCheckInTest.java
@@ -2,6 +2,7 @@ package com.dotmarketing.portlets.contentlet.business;
 
 import com.dotcms.repackage.org.apache.commons.io.FileUtils;
 
+import com.dotcms.uuid.shorty.ShortyIdAPI;
 import com.dotmarketing.beans.Host;
 import com.dotmarketing.beans.Identifier;
 import com.dotmarketing.business.APILocator;
@@ -14,7 +15,9 @@ import com.dotmarketing.portlets.ContentletBaseTest;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.portlets.fileassets.business.FileAsset;
 import com.dotmarketing.portlets.fileassets.business.FileAssetAPI;
+import com.dotmarketing.portlets.folders.business.FolderAPI;
 import com.dotmarketing.portlets.folders.model.Folder;
+import com.dotmarketing.portlets.languagesmanager.business.LanguageAPI;
 import com.dotmarketing.portlets.languagesmanager.model.Language;
 import com.dotmarketing.util.Logger;
 
@@ -33,6 +36,13 @@ import com.liferay.portal.model.User;
  * Time: 12:12 PM
  */
 public class ContentletCheckInTest {
+
+    private static ContentletAPI contentletAPI;
+    private static FileAssetAPI fileAssetAPI;
+    private static FolderAPI folderAPI;
+    private static HostAPI hostAPI;
+    private static LanguageAPI languageAPI;
+    private static ShortyIdAPI shortyIdAPI;
   
   public ContentletCheckInTest() {
       
@@ -40,63 +50,76 @@ public class ContentletCheckInTest {
   
   @BeforeClass
   public static void prepare () throws Exception{
-    
-  
 
-      
-      
-      
-    ContentletBaseTest.prepare();
-    
+      ContentletBaseTest.prepare();
+
+      hostAPI          = APILocator.getHostAPI();
+      folderAPI        = APILocator.getFolderAPI();
+      languageAPI      = APILocator.getLanguageAPI();
+      contentletAPI    = APILocator.getContentletAPI();
+      fileAssetAPI     = APILocator.getFileAssetAPI();
+      shortyIdAPI      = APILocator.getShortyAPI();
   }
   
   @Test
   public void checkinInvalidFileContent () throws Exception {
-      ContentletAPI capi= APILocator.getContentletAPI();
+
+      Folder folder1  = null;
+      Contentlet con2 = null;
+
+
       boolean respectFrontendRoles=false;
       String fileTypeId=APILocator.getContentTypeAPI(APILocator.systemUser()).find("fileAsset").id();
-      String uuid1 = APILocator.getShortyAPI().randomShorty();
-      String uuid2 = APILocator.getShortyAPI().randomShorty();
-      String uuid3 = APILocator.getShortyAPI().randomShorty();
+      String uuid1 = shortyIdAPI.randomShorty();
+      String uuid3 = shortyIdAPI.randomShorty();
       
       User user = APILocator.systemUser();
-      Host host = APILocator.getHostAPI().findDefaultHost(user, false);   
-      Folder folder1 = APILocator.getFolderAPI().createFolders("/" + uuid1, host, user, false);
-      Language lang = APILocator.getLanguageAPI().getDefaultLanguage();
-      folder1.setFilesMasks("*.txt");
-      folder1.setDefaultFileType(fileTypeId);
-      APILocator.getFolderAPI().save(folder1, user, false);
-      
-      //Folder folder2 = APILocator.getFolderAPI().createFolders("/" + uuid2, host, user, false);
-      File file = File.createTempFile(uuid3, ".txt");
-      try(FileWriter writer = new FileWriter(file)){
-          while(file.length()< 1024*10) {
-              writer.write("Im writing\n");
+      Host host = hostAPI.findDefaultHost(user, false);
+      try {
+          folder1 = folderAPI.createFolders("/" + uuid1, host, user, false);
+          Language lang = languageAPI.getDefaultLanguage();
+          folder1.setFilesMasks("*.txt");
+          folder1.setDefaultFileType(fileTypeId);
+          folderAPI.save(folder1, user, false);
+
+          File file = File.createTempFile(uuid3, ".txt");
+          try (FileWriter writer = new FileWriter(file)) {
+              while (file.length() < 1024 * 10) {
+                  writer.write("Im writing\n");
+              }
+          }
+
+          FileAsset asset = new FileAsset();
+          asset.setLanguageId(lang.getId());
+          asset.setBinary("fileAsset", file);
+          asset.setTitle(file.getName());
+          asset.setFileName(file.getName());
+          asset.setContentTypeId(fileTypeId);
+          asset.setHost(host.getIdentifier());
+          asset.setFolder(folder1.getIdentifier());
+          Contentlet con = contentletAPI.checkin(asset, user, true);
+
+          con2 = contentletAPI.find(con.getInode(), user, true);
+
+          assert (con.equals(con2));
+
+          FileAsset fileAsset = fileAssetAPI.fromContentlet(con2);
+
+          // this fails becuase the name does not match the folder's file mask
+          // but it leaves the original file removed from the index
+          fileAssetAPI.renameFile(fileAsset, "test.fail", user, respectFrontendRoles);
+          assert (!folderAPI.getContent(folder1, user, respectFrontendRoles)
+                  .isEmpty());
+
+      }finally{
+          if (folder1 != null) {
+              folderAPI.delete(folder1, user, false);
+          }
+
+          if (con2 != null && con2.getInode() != null) {
+              contentletAPI.delete(con2, user, false);
           }
       }
-      
-      FileAsset asset = new FileAsset();
-      asset.setLanguageId(lang.getId());
-      asset.setBinary("fileAsset", file);
-      asset.setTitle(file.getName());
-      asset.setFileName(file.getName());
-      asset.setContentTypeId(fileTypeId);
-      asset.setHost(host.getIdentifier());
-      asset.setFolder(folder1.getIdentifier());
-      Contentlet con = capi.checkin(asset, user, true);
-
-      Contentlet con2 = capi.find(con.getInode(), user, true);
-      
-      assert(con.equals(con2));
-      
-      FileAsset fileAsset = APILocator.getFileAssetAPI().fromContentlet(con2);
-      
-      // this fails becuase the name does not match the folder's file mask
-      // but it leaves the original file removed from the index
-      APILocator.getFileAssetAPI().renameFile(fileAsset, "test.fail", user, respectFrontendRoles);
-      assert(!APILocator.getFolderAPI().getContent(folder1, user, respectFrontendRoles).isEmpty());
-      
-      
   }
 
   // Mostly borrowed from com.dotmarketing.portlets.fileassets.business.FileAssetAPIImpl.renameFile(Contentlet, String, User, boolean)
