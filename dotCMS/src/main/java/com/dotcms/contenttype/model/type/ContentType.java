00e5b396f10759ff1d096e3bb378dcc23f5c7259
erickgonzalez <erick.gonzalez@dotcms.com>
2018-02-02 17:07:04 -0600

    Fix conflicts for #13180 into 4.3

--- dotCMS/src/main/java/com/dotcms/contenttype/model/type/ContentType.java
+++ dotCMS/src/main/java/com/dotcms/contenttype/model/type/ContentType.java
@@ -25,6 +25,7 @@ import com.dotmarketing.business.PermissionAPI;
 import com.dotmarketing.business.PermissionSummary;
 import com.dotmarketing.business.Permissionable;
 import com.dotmarketing.business.RelatedPermissionableGroup;
+import com.dotmarketing.common.db.DotConnect;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotRuntimeException;
 import com.dotmarketing.portlets.folders.business.FolderAPI;
@@ -38,25 +39,25 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import com.google.common.collect.ImmutableMap;
 
 @JsonTypeInfo(
-	use = Id.CLASS,
-	include = JsonTypeInfo.As.PROPERTY,
-	property = "clazz"
+        use = Id.CLASS,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "clazz"
 )
 @JsonSubTypes({
-	@Type(value = FileAssetContentType.class),
-	@Type(value = FormContentType.class),
-	@Type(value = PageContentType.class),
-	@Type(value = PersonaContentType.class),
-	@Type(value = SimpleContentType.class),
-	@Type(value = WidgetContentType.class),
-	@Type(value = VanityUrlContentType.class),
-	@Type(value = KeyValueContentType.class),
+        @Type(value = FileAssetContentType.class),
+        @Type(value = FormContentType.class),
+        @Type(value = PageContentType.class),
+        @Type(value = PersonaContentType.class),
+        @Type(value = SimpleContentType.class),
+        @Type(value = WidgetContentType.class),
+        @Type(value = VanityUrlContentType.class),
+        @Type(value = KeyValueContentType.class),
 })
 public abstract class ContentType implements Serializable, Permissionable, ContentTypeIf {
 
   @Value.Check
   protected void check() {
-	Preconditions.checkArgument(StringUtils.isNotEmpty(name()), "Name cannot be empty for " + this.getClass());
+    Preconditions.checkArgument(StringUtils.isNotEmpty(name()), "Name cannot be empty for " + this.getClass());
 
     if (!(this instanceof UrlMapable)) {
       Preconditions.checkArgument(detailPage() == null, "Detail Page cannot be set for " + this.getClass());
@@ -220,32 +221,26 @@ public abstract class ContentType implements Serializable, Permissionable, Conte
   @Override
   public List<PermissionSummary> acceptedPermissions() {
     return ImmutableList.of(new PermissionSummary("view", "view-permission-description", PermissionAPI.PERMISSION_READ),
-        new PermissionSummary("edit", "edit-permission-description", PermissionAPI.PERMISSION_WRITE),
-        new PermissionSummary("publish", "publish-permission-description", PermissionAPI.PERMISSION_PUBLISH),
-        new PermissionSummary("edit-permissions", "edit-permissions-permission-description",
-            PermissionAPI.PERMISSION_EDIT_PERMISSIONS));
+            new PermissionSummary("edit", "edit-permission-description", PermissionAPI.PERMISSION_WRITE),
+            new PermissionSummary("publish", "publish-permission-description", PermissionAPI.PERMISSION_PUBLISH),
+            new PermissionSummary("edit-permissions", "edit-permissions-permission-description",
+                    PermissionAPI.PERMISSION_EDIT_PERMISSIONS));
 
   }
 
   @JsonIgnore
   @Value.Lazy
   public Permissionable getParentPermissionable() {
-    try {
-
-      PermissionableProxy pp = new PermissionableProxy();
-
+    try{
       if (FolderAPI.SYSTEM_FOLDER.equals(this.folder())) {
-        pp.setIdentifier(this.host());
-        pp.setInode(this.host());
-        pp.setType(Host.class.getCanonicalName());
+        PermissionableProxy host = new PermissionableProxy();
+        host.setIdentifier(this.host());
+        host.setInode(this.host());
+        return host;
       } else {
-        pp.setIdentifier(this.folder());
-        pp.setInode(this.folder());
-        pp.setType(Folder.class.getCanonicalName());
+        return APILocator.getFolderAPI().find(this.folder(), APILocator.systemUser(), false);
       }
-
-      return pp;
-    } catch (Exception e) {
+    }catch (Exception e) {
       throw new DotRuntimeException(e.getMessage(), e);
     }
   }
