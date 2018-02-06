439f38c0577aac15eb76294ffbb8b2a9a7c5756a
Jonathan <jsanca@Jonathans-MacBook-Pro-2.local>
2018-02-06 12:54:10 -0600

    #13563 now if the content is new, but you have a save content actionlet in your step your content will be saved

--- dotCMS/src/main/java/com/dotmarketing/portlets/workflows/business/WorkflowAPIImpl.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/workflows/business/WorkflowAPIImpl.java
@@ -1099,38 +1099,6 @@ public class WorkflowAPIImpl implements WorkflowAPI, WorkflowAPIOsgiService {
 
 			processor.getContentlet().setStringProperty("wfActionId", processor.getAction().getId());
 
-
-
-			WorkflowTask task = processor.getTask();
-			if(task != null){
-				Role r = roleAPI.getUserRole(processor.getUser());
-				if(task.isNew()){
-
-					task.setCreatedBy(r.getId());
-					task.setWebasset(processor.getContentlet().getIdentifier());
-					task.setLanguageId(processor.getContentlet().getLanguageId());
-					if(processor.getWorkflowMessage() != null){
-						task.setDescription(processor.getWorkflowMessage());
-					}
-				}
-				task.setTitle(processor.getContentlet().getTitle());
-				task.setModDate(new java.util.Date());
-				if(processor.getNextAssign() != null)
-					task.setAssignedTo(processor.getNextAssign().getId());
-				task.setStatus(processor.getNextStep().getId());
-
-				saveWorkflowTask(task,processor);
-				if(processor.getWorkflowMessage() != null){
-					WorkflowComment comment = new WorkflowComment();
-					comment.setComment(processor.getWorkflowMessage());
-
-					comment.setWorkflowtaskId(task.getId());
-					comment.setCreationDate(new Date());
-					comment.setPostedBy(r.getId());
-					saveComment(comment);
-				}
-			}
-
 			List<WorkflowActionClass> actionClasses = processor.getActionClasses();
 			if(actionClasses != null){
 				for(WorkflowActionClass actionClass : actionClasses){
@@ -1144,6 +1112,9 @@ public class WorkflowAPIImpl implements WorkflowAPI, WorkflowAPIOsgiService {
 					}
 				}
 			}
+
+			this.saveWorkflowTask(processor);
+
 			if(UtilMethods.isSet(processor.getContentlet())){
 			    APILocator.getContentletAPI().refresh(processor.getContentlet());
 			}
@@ -1165,6 +1136,41 @@ public class WorkflowAPIImpl implements WorkflowAPI, WorkflowAPIOsgiService {
 				HibernateUtil.closeSessionSilently();
 			}
 		}
+	}
+
+	private void saveWorkflowTask(final WorkflowProcessor processor) throws DotDataException {
+
+		final WorkflowTask task = processor.getTask();
+		if(task != null){
+            Role r = roleAPI.getUserRole(processor.getUser());
+            if(task.isNew()){
+
+            	DotPreconditions.notNull(processor.getContentlet().getIdentifier(),
+						()-> "Workflow-Content-Needs-Be-Saved", DotWorkflowException.class);
+                task.setCreatedBy(r.getId());
+                task.setWebasset(processor.getContentlet().getIdentifier());
+                task.setLanguageId(processor.getContentlet().getLanguageId());
+                if(processor.getWorkflowMessage() != null){
+                    task.setDescription(processor.getWorkflowMessage());
+                }
+            }
+            task.setTitle(processor.getContentlet().getTitle());
+            task.setModDate(new Date());
+            if(processor.getNextAssign() != null)
+                task.setAssignedTo(processor.getNextAssign().getId());
+            task.setStatus(processor.getNextStep().getId());
+
+            saveWorkflowTask(task,processor);
+            if(processor.getWorkflowMessage() != null){
+                WorkflowComment comment = new WorkflowComment();
+                comment.setComment(processor.getWorkflowMessage());
+
+                comment.setWorkflowtaskId(task.getId());
+                comment.setCreationDate(new Date());
+                comment.setPostedBy(r.getId());
+                saveComment(comment);
+            }
+        }
 	}
 
 	// todo: note; this method is not referer by anyone, should it be removed?
