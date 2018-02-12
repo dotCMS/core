4073d35d26f81328162eb5b394dba7b118e75d21
Jonathan <jsanca@Jonathans-MacBook-Pro-2.local>
2018-02-12 15:01:26 -0600

    #13174 minor changes to the Task04340AddSystemWorkflowToContentType

--- dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task04340AddSystemWorkflowToContentType.java
+++ dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task04340AddSystemWorkflowToContentType.java
@@ -21,9 +21,9 @@ import java.util.Map;
  */
 public class Task04340AddSystemWorkflowToContentType implements StartupTask {
 
-    public static final String SYSTEM_WORKFLOW_ID = "d61a59e1-a49c-46f2-a929-db2b4bfa88b2";
-    protected static String SELECT_CONTENT_TYPES     = "select inode, name from structure";
-    protected static String INSERT_SCHEME_FOR_STRUCT = "insert into workflow_scheme_x_structure (id, scheme_id, structure_id) values ( ?, ?, ?)";
+    public static final String SYSTEM_WORKFLOW_ID          = "d61a59e1-a49c-46f2-a929-db2b4bfa88b2";
+    protected static String SELECT_CONTENT_TYPES           = "select inode, name from structure";
+    protected static String INSERT_SCHEME_FOR_CONTENT_TYPE = "insert into workflow_scheme_x_structure (id, scheme_id, structure_id) values ( ?, ?, ?)";
 
     @Override
     public boolean forceRun() {
@@ -49,7 +49,7 @@ public class Task04340AddSystemWorkflowToContentType implements StartupTask {
             Logger.info(this, "Adding the system workflow to the content type "
                     + contentTypeId + ", name " + contentResult.get("name"));
 
-            new DotConnect().setSQL(INSERT_SCHEME_FOR_STRUCT)
+            new DotConnect().setSQL(INSERT_SCHEME_FOR_CONTENT_TYPE)
                     .addParam(UUIDGenerator.generateUuid())
                     .addParam(contentTypeId)
                     .addParam(SYSTEM_WORKFLOW_ID)
