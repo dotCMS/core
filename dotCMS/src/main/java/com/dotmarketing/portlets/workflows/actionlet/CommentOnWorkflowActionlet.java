dfbe0f492dc722a15e054169b112d062868d6df4
Will Ezell <will@dotcms.com>
2018-03-11 19:12:41 -0400

    #13835 comment now uses the right user

--- dotCMS/src/main/java/com/dotmarketing/portlets/workflows/actionlet/CommentOnWorkflowActionlet.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/workflows/actionlet/CommentOnWorkflowActionlet.java
@@ -4,7 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dotcms.mock.request.MockHttpRequest;
+import com.dotcms.mock.response.BaseResponse;
+
+import com.dotmarketing.beans.Host;
 import com.dotmarketing.business.APILocator;
+import com.dotmarketing.business.Role;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.portlets.workflows.model.WorkflowActionClassParameter;
 import com.dotmarketing.portlets.workflows.model.WorkflowActionFailureException;
@@ -13,6 +21,8 @@ import com.dotmarketing.portlets.workflows.model.WorkflowComment;
 import com.dotmarketing.portlets.workflows.model.WorkflowProcessor;
 import com.dotmarketing.portlets.workflows.model.WorkflowStep;
 import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.UtilMethods;
+import com.dotmarketing.util.VelocityUtil;
 
 public class CommentOnWorkflowActionlet extends WorkFlowActionlet {
 
@@ -40,8 +50,28 @@ public class CommentOnWorkflowActionlet extends WorkFlowActionlet {
 	public void executeAction(WorkflowProcessor processor,Map<String,WorkflowActionClassParameter>  params) throws WorkflowActionFailureException {
 		WorkflowActionClassParameter commentParam =  params.get("comment");
 		WorkflowComment comment = new WorkflowComment();
-		comment.setPostedBy(processor.getUser().getUserId());
-		comment.setComment(commentParam.getValue());
+		Role role;
+        try {
+            role = APILocator.getRoleAPI().getUserRole(processor.getUser());
+            comment.setPostedBy(role.getId());
+        } catch (DotDataException e1) {
+            throw new WorkflowActionFailureException("unable to load role:" + processor.getUser(), e1);
+        }
+
+
+        org.apache.velocity.context.Context ctx = VelocityUtil.getBasicContext();
+        ctx.put("workflow", processor);
+        ctx.put("user", processor.getUser());
+        ctx.put("contentlet", processor.getContentlet());
+        ctx.put("content", processor.getContentlet());
+
+        try {
+            comment.setComment(VelocityUtil.eval(commentParam.getValue(), ctx));
+        } catch (Exception e1) {
+            Logger.warn(this.getClass(), "unable to parse comment, falling back" + e1);
+            comment.setComment(commentParam.getValue());
+        }
+
 		comment.setWorkflowtaskId(processor.getTask().getId());
 		try {
 			APILocator.getWorkflowAPI().saveComment(comment);
