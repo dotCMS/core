7a1c9fe350b037043a3a9a3f55377513c7445bec
Will Ezell <will@dotcms.com>
2018-03-11 21:30:55 -0400

    #13835 one fix for invalid workflow task id in comment

--- dotCMS/src/main/java/com/dotmarketing/portlets/workflows/actionlet/CommentOnWorkflowActionlet.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/workflows/actionlet/CommentOnWorkflowActionlet.java
@@ -1,56 +1,59 @@
 package com.dotmarketing.portlets.workflows.actionlet;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.dotcms.mock.request.MockHttpRequest;
-import com.dotcms.mock.response.BaseResponse;
-
-import com.dotmarketing.beans.Host;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.business.Role;
+import com.dotmarketing.db.DbConnectionFactory;
+import com.dotmarketing.db.HibernateUtil;
 import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotHibernateException;
+import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.portlets.workflows.model.WorkflowActionClassParameter;
 import com.dotmarketing.portlets.workflows.model.WorkflowActionFailureException;
 import com.dotmarketing.portlets.workflows.model.WorkflowActionletParameter;
 import com.dotmarketing.portlets.workflows.model.WorkflowComment;
 import com.dotmarketing.portlets.workflows.model.WorkflowProcessor;
 import com.dotmarketing.portlets.workflows.model.WorkflowStep;
+import com.dotmarketing.portlets.workflows.model.WorkflowTask;
 import com.dotmarketing.util.Logger;
-import com.dotmarketing.util.UtilMethods;
 import com.dotmarketing.util.VelocityUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class CommentOnWorkflowActionlet extends WorkFlowActionlet {
 
 
-	private static List<WorkflowActionletParameter> paramList = null; 
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private static List<WorkflowActionletParameter> paramList = null;
 
-	@Override
-	public synchronized List<WorkflowActionletParameter> getParameters() {
-		if(paramList ==null){
-			paramList = new ArrayList<WorkflowActionletParameter>();
-			paramList.add(new WorkflowActionletParameter("comment", "Workflow Comment", null, true));
-		}
-		return paramList;
-	}
+    @Override
+    public synchronized List<WorkflowActionletParameter> getParameters() {
+        if (paramList == null) {
+            paramList = new ArrayList<WorkflowActionletParameter>();
+            paramList.add(new WorkflowActionletParameter("comment", "Workflow Comment", null, true));
+        }
+        return paramList;
+    }
 
-	public String getName() {
-		return "Comment on Workflow";
-	}
+    public String getName() {
+        return "Comment on Workflow";
+    }
 
-	public String getHowTo() {
+    public String getHowTo() {
 
-		return "This actionlet allows you to add a comment on the workflow task.";
-	}
+        return "This actionlet allows you to add a comment on the workflow task.";
+    }
 
-	public void executeAction(WorkflowProcessor processor,Map<String,WorkflowActionClassParameter>  params) throws WorkflowActionFailureException {
-		WorkflowActionClassParameter commentParam =  params.get("comment");
-		WorkflowComment comment = new WorkflowComment();
-		Role role;
+    public void executeAction(WorkflowProcessor processor, Map<String, WorkflowActionClassParameter> params)
+            throws WorkflowActionFailureException {
+        WorkflowActionClassParameter commentParam = params.get("comment");
+        WorkflowComment comment = new WorkflowComment();
+        final Contentlet con = processor.getContentlet();
+        Role role;
         try {
             role = APILocator.getRoleAPI().getUserRole(processor.getUser());
             comment.setPostedBy(role.getId());
@@ -72,17 +75,40 @@ public class CommentOnWorkflowActionlet extends WorkFlowActionlet {
             comment.setComment(commentParam.getValue());
         }
 
-		comment.setWorkflowtaskId(processor.getTask().getId());
-		try {
-			APILocator.getWorkflowAPI().saveComment(comment);
-		} catch (DotDataException e) {
-			Logger.error(CommentOnWorkflowActionlet.class,e.getMessage(),e);
-		}
+        comment.setWorkflowtaskId(processor.getTask().getId());
 
-	}
+        if (processor.getTask().isNew()) {
+            try {
+                HibernateUtil.addAsyncCommitListener(() -> {
+                    try {
+                        WorkflowTask task = APILocator.getWorkflowAPI().findTaskByContentlet(con);
+                        comment.setWorkflowtaskId(task.getId());
+                        APILocator.getWorkflowAPI().saveComment(comment);
+                        DbConnectionFactory.closeAndCommit();
+                    } catch (Exception e) {
+                        Logger.warn(CommentOnWorkflowActionlet.class, "unable to save comment");
+                    }
+                });
+            } catch (DotHibernateException e1) {
 
-	public WorkflowStep getNextStep() {
-		return null;
-	}
+                Logger.warn(CommentOnWorkflowActionlet.class, e1.getMessage());
+            }
+        } else {
+            try {
+                APILocator.getWorkflowAPI().saveComment(comment);
+            } catch (DotDataException e) {
+                Logger.error(CommentOnWorkflowActionlet.class, e.getMessage(), e);
+            }
+        }
 
+    }
+
+    public WorkflowStep getNextStep() {
+        return null;
+    }
+
+    
+    
+    
+    
 }
