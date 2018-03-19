5071e8207da124c4fd4b86a12991e6b76064720c
Freddy Rodriguez <freddy0309@gmail.com>
2018-03-19 11:34:13 -0600

    dotCMS/core#13531 Unable to see the content types permissions
    * Send angularCurrentPortlet query parameter (#578)

--- src/app/view/pipes/safe-url.pipe.ts
+++ src/app/view/pipes/safe-url.pipe.ts
@@ -1,20 +1,27 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { DotRouterService } from '../../api/services/dot-router/dot-router.service';
 
 @Pipe({ name: 'safe' })
 export class SafePipe implements PipeTransform {
-    constructor(private sanitizer: DomSanitizer) {}
+    constructor(private sanitizer: DomSanitizer, private dotRouterService: DotRouterService) {}
 
     transform(url) {
         if (url) {
-            let urlWithParameters = url;
-            urlWithParameters += urlWithParameters.indexOf('?') === -1 ? '?' : '&';
-            urlWithParameters +=
-                urlWithParameters.indexOf('in_frame') === -1 ? 'in_frame=true&frame=detailFrame&container=true' : '';
-
+            const urlWithParameters = this.addURLWithParameters(url);
             return this.sanitizer.bypassSecurityTrustResourceUrl(urlWithParameters);
         } else {
             return this.sanitizer.bypassSecurityTrustResourceUrl('');
         }
+    }
+
+    private addURLWithParameters(url: string): string {
+        let urlWithParameters = url;
+        urlWithParameters += url.indexOf('?') === -1 ? '?' : '&';
+        urlWithParameters += urlWithParameters.indexOf('in_frame') === -1 ?
+            `in_frame=true&frame=detailFrame&container=true&angularCurrentPortlet=${this.dotRouterService.currentPortlet.id}` :
+            '';
+
+        return urlWithParameters;
     }
 }
