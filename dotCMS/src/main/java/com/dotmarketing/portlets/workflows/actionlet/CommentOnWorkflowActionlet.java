19fce83d30fae275775a756d79b4b6af32fee745
Jonathan <jsanca@Jonathans-MacBook-Pro-2.local>
2018-03-21 15:34:51 -0600

    #13835 formatting and trx handling

--- dotCMS/src/main/java/com/dotmarketing/portlets/workflows/actionlet/CommentOnWorkflowActionlet.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/workflows/actionlet/CommentOnWorkflowActionlet.java
@@ -1,21 +1,16 @@
 package com.dotmarketing.portlets.workflows.actionlet;
 
+import com.dotcms.business.WrapInTransaction;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.business.Role;
-import com.dotmarketing.db.DbConnectionFactory;
 import com.dotmarketing.db.HibernateUtil;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotHibernateException;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
-import com.dotmarketing.portlets.workflows.model.WorkflowActionClassParameter;
-import com.dotmarketing.portlets.workflows.model.WorkflowActionFailureException;
-import com.dotmarketing.portlets.workflows.model.WorkflowActionletParameter;
-import com.dotmarketing.portlets.workflows.model.WorkflowComment;
-import com.dotmarketing.portlets.workflows.model.WorkflowProcessor;
-import com.dotmarketing.portlets.workflows.model.WorkflowStep;
-import com.dotmarketing.portlets.workflows.model.WorkflowTask;
+import com.dotmarketing.portlets.workflows.model.*;
 import com.dotmarketing.util.Logger;
 import com.dotmarketing.util.VelocityUtil;
+import org.apache.velocity.context.Context;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,46 +43,22 @@ public class CommentOnWorkflowActionlet extends WorkFlowActionlet {
         return "This actionlet allows you to add a comment on the workflow task.";
     }
 
-    public void executeAction(WorkflowProcessor processor, Map<String, WorkflowActionClassParameter> params)
+    public void executeAction(final WorkflowProcessor processor,
+                              final Map<String, WorkflowActionClassParameter> params)
             throws WorkflowActionFailureException {
-        WorkflowActionClassParameter commentParam = params.get("comment");
-        WorkflowComment comment = new WorkflowComment();
-        final Contentlet con = processor.getContentlet();
-        Role role;
-        try {
-            role = APILocator.getRoleAPI().getUserRole(processor.getUser());
-            comment.setPostedBy(role.getId());
-        } catch (DotDataException e1) {
-            throw new WorkflowActionFailureException("unable to load role:" + processor.getUser(), e1);
-        }
 
+        final WorkflowActionClassParameter commentParam = params.get("comment");
+        final WorkflowComment              comment      = new WorkflowComment();
+        final Contentlet                   contentlet   = processor.getContentlet();
 
-        org.apache.velocity.context.Context ctx = VelocityUtil.getBasicContext();
-        ctx.put("workflow", processor);
-        ctx.put("user", processor.getUser());
-        ctx.put("contentlet", processor.getContentlet());
-        ctx.put("content", processor.getContentlet());
-
-        try {
-            comment.setComment(VelocityUtil.eval(commentParam.getValue(), ctx));
-        } catch (Exception e1) {
-            Logger.warn(this.getClass(), "unable to parse comment, falling back" + e1);
-            comment.setComment(commentParam.getValue());
-        }
-
+        this.setRole(processor, comment);
+        this.processCommentValue(processor, commentParam, comment);
         comment.setWorkflowtaskId(processor.getTask().getId());
 
         if (processor.getTask().isNew()) {
             try {
                 HibernateUtil.addAsyncCommitListener(() -> {
-                    try {
-                        WorkflowTask task = APILocator.getWorkflowAPI().findTaskByContentlet(con);
-                        comment.setWorkflowtaskId(task.getId());
-                        APILocator.getWorkflowAPI().saveComment(comment);
-                        DbConnectionFactory.closeAndCommit();
-                    } catch (Exception e) {
-                        Logger.warn(CommentOnWorkflowActionlet.class, "unable to save comment");
-                    }
+                    this.saveComment(contentlet, comment);
                 });
             } catch (DotHibernateException e1) {
 
@@ -101,6 +72,48 @@ public class CommentOnWorkflowActionlet extends WorkFlowActionlet {
             }
         }
 
+    }
+
+    private void setRole(final WorkflowProcessor processor,
+                         final WorkflowComment comment) {
+        Role role;
+
+        try {
+            role = APILocator.getRoleAPI().getUserRole(processor.getUser());
+            comment.setPostedBy(role.getId());
+        } catch (DotDataException e1) {
+            throw new WorkflowActionFailureException("unable to load role:" + processor.getUser(), e1);
+        }
+    }
+
+    private void processCommentValue(final WorkflowProcessor processor,
+                                     final WorkflowActionClassParameter commentParam,
+                                     final WorkflowComment comment) {
+
+        final Context velocityContext = VelocityUtil.getBasicContext();
+        velocityContext.put("workflow", processor);
+        velocityContext.put("user", processor.getUser());
+        velocityContext.put("contentlet", processor.getContentlet());
+        velocityContext.put("content", processor.getContentlet());
+
+        try {
+            comment.setComment(VelocityUtil.eval(commentParam.getValue(), velocityContext));
+        } catch (Exception e1) {
+            Logger.warn(this.getClass(), "unable to parse comment, falling back" + e1);
+            comment.setComment(commentParam.getValue());
+        }
+    }
+
+    @WrapInTransaction
+    private void saveComment (final Contentlet contentlet, final WorkflowComment comment) {
+
+        try {
+            final WorkflowTask task = APILocator.getWorkflowAPI().findTaskByContentlet(contentlet);
+            comment.setWorkflowtaskId(task.getId());
+            APILocator.getWorkflowAPI().saveComment(comment);
+        } catch (Exception e) {
+            Logger.warn(CommentOnWorkflowActionlet.class, "unable to save comment");
+        }
     }
 
     public WorkflowStep getNextStep() {
