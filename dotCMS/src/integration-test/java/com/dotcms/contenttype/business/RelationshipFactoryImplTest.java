5cb4231753fa123c664cf671eb97804d6bb5c9f8
erickgonzalez <erick.gonzalez@dotcms.com>
2018-03-23 16:28:50 -0600

    Issue 13809 more info loggers (#13976)
    * loggers more info importUtilTest and RelationshipFactoryTest
    
    * #13809 delete relationships when test finishes
    
    * more logging importUtilTest
    
    * #13809 use correct test name and variables
    
    * more logs
    
    * #13809 use correct test name and variables

--- dotCMS/src/integration-test/java/com/dotcms/contenttype/business/RelationshipFactoryImplTest.java
+++ dotCMS/src/integration-test/java/com/dotcms/contenttype/business/RelationshipFactoryImplTest.java
@@ -17,21 +17,14 @@ import org.junit.Test;
 public class RelationshipFactoryImplTest extends ContentTypeBaseTest{
 
     @Test
-    public void testByTypeValue() throws DotHibernateException {
-        RelationshipFactory fac = FactoryLocator.getRelationshipFactory();
-        List<Relationship> rels = fac.dbAll();
-
-        for (Relationship ship : rels) {
-            Relationship newRel = fac.byTypeValue(ship.getRelationTypeValue().toUpperCase());
-            Logger.info(this, "ship: " + ship.getRelationTypeValue());
-            Logger.info(this, "newRel: " + newRel.getRelationTypeValue());
-            assertEquals(ship,newRel);
-
+    public void testByTypeValue_RelationshipsShouldBeSameRegardlessCase_SameRelationship() throws DotHibernateException {
+        RelationshipFactory relationshipFactory = FactoryLocator.getRelationshipFactory();
+        List<Relationship> relationshipList = relationshipFactory.dbAll();
+        for (Relationship relationship : relationshipList) {
+            Relationship relationshipWithUpperCase = relationshipFactory.byTypeValue(relationship.getRelationTypeValue().toUpperCase());
+            assertEquals(relationship,relationshipWithUpperCase);
 
         }
-
-
-
     }
 
 }
