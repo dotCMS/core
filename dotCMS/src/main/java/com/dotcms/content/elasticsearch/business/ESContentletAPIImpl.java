f68217d51ddce25df13631c26d5f4e76ed779797
freddyrodriguez <freddy0309@gmail.com>
2018-03-26 09:13:04 -0600

    remove archive contentñet from cache

--- dotCMS/src/main/java/com/dotcms/content/elasticsearch/business/ESContentletAPIImpl.java
+++ dotCMS/src/main/java/com/dotcms/content/elasticsearch/business/ESContentletAPIImpl.java
@@ -2052,6 +2052,7 @@ public class ESContentletAPIImpl implements ContentletAPI {
                 new ContentletLoader().invalidate(contentlet);
 
                 publishRelatedHtmlPages(contentlet);
+                CacheLocator.getContentletCache().remove(contentlet);
                 contentletSystemEventUtil.pushArchiveEvent(this.find(contentlet.getInode(), user, respectFrontendRoles));
             }else{
                 throw new DotContentletStateException("Contentlet is locked: Unable to archive");
