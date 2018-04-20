afd4d67125a29585418085ff4ea959c71247e2ef
Daniel Silva <daniel.silva@dotcms.com>
2018-04-20 12:52:31 -0700

    #13927 use constant for license level

--- dotCMS/src/main/java/com/dotcms/cluster/ClusterUtils.java
+++ dotCMS/src/main/java/com/dotcms/cluster/ClusterUtils.java
@@ -2,6 +2,7 @@ package com.dotcms.cluster;
 
 import com.dotcms.cluster.business.ReplicasMode;
 import com.dotcms.enterprise.LicenseUtil;
+import com.dotcms.enterprise.license.LicenseLevel;
 import com.dotmarketing.util.Config;
 import com.dotmarketing.util.UtilMethods;
 
@@ -13,15 +14,18 @@ import com.dotmarketing.util.UtilMethods;
 public class ClusterUtils {
 
 	public static boolean isTransportAutoWire(){
-		return Config.getBooleanProperty("AUTOWIRE_CLUSTER_TRANSPORT", true) && LicenseUtil.getLevel()> 200;
+		return Config.getBooleanProperty("AUTOWIRE_CLUSTER_TRANSPORT", true)
+			&& LicenseUtil.getLevel()> LicenseLevel.STANDARD.level;
 	}
 
 	public static boolean isESAutoWire(){
-		return Config.getBooleanProperty("AUTOWIRE_CLUSTER_ES", true)&& LicenseUtil.getLevel()> 200;
+		return Config.getBooleanProperty("AUTOWIRE_CLUSTER_ES", true)
+			&& LicenseUtil.getLevel()> LicenseLevel.STANDARD.level;
 	}
 
 	public static boolean isReplicasSet(){
-		return UtilMethods.isSet(Config.getStringProperty("ES_INDEX_REPLICAS", null)) && LicenseUtil.getLevel()> 200;
+		return UtilMethods.isSet(Config.getStringProperty("ES_INDEX_REPLICAS", null))
+			&& LicenseUtil.getLevel()> LicenseLevel.STANDARD.level;
 	}
 	
 	
