9970362ca6fbc98b4a66044944780c9aac659028
Andre Curione <andre.curione@dotcms.com>
2018-05-11 10:14:45 -0600

    #14296 - Task04365 - Oracle query

--- dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task04365RelationshipUniqueConstraint.java
+++ dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task04365RelationshipUniqueConstraint.java
@@ -1,6 +1,13 @@
 package com.dotmarketing.startup.runonce;
 
+import com.dotmarketing.common.db.DotConnect;
+import com.dotmarketing.db.DbConnectionFactory;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotRuntimeException;
 import com.dotmarketing.startup.AbstractJDBCStartupTask;
+import com.dotmarketing.util.Logger;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
 
@@ -17,9 +24,11 @@ public class Task04365RelationshipUniqueConstraint extends AbstractJDBCStartupTa
         return Collections.emptyList();
     }
 
+    private static final String ORACLE_ADD_UNIQUE_CONSTRAINT = "ALTER TABLE relationship ADD CONSTRAINT unique_relation_type_value UNIQUE (relation_type_value)";
+
     @Override
     public String getH2Script() {
-        StringBuilder builder = new StringBuilder()
+        final StringBuilder builder = new StringBuilder()
             //Delete duplicates
             .append("DELETE FROM relationship WHERE inode NOT IN ( ")
             .append("  SELECT MAX (inode) FROM relationship GROUP BY relation_type_value ); ")
@@ -31,7 +40,7 @@ public class Task04365RelationshipUniqueConstraint extends AbstractJDBCStartupTa
 
     @Override
     public String getPostgresScript() {
-        StringBuilder builder = new StringBuilder()
+        final StringBuilder builder = new StringBuilder()
             //Delete duplicates
             .append("DELETE FROM relationship R1 ")
             .append("USING relationship R2 ")
@@ -45,7 +54,7 @@ public class Task04365RelationshipUniqueConstraint extends AbstractJDBCStartupTa
 
     @Override
     public String getMySQLScript() {
-        StringBuilder builder = new StringBuilder()
+        final StringBuilder builder = new StringBuilder()
             //Delete duplicates
             .append("DELETE R1 ")
             .append("FROM relationship AS R1, relationship AS R2 ")
@@ -59,7 +68,7 @@ public class Task04365RelationshipUniqueConstraint extends AbstractJDBCStartupTa
 
     @Override
     public String getMSSQLScript() {
-        StringBuilder builder = new StringBuilder()
+        final StringBuilder builder = new StringBuilder()
             //Delete duplicates
             .append("DELETE R1 FROM ( ")
             .append("  Select *, ROW_NUMBER() over (Partition by relation_type_value order by relation_type_value) as rowNumber ")
@@ -73,17 +82,47 @@ public class Task04365RelationshipUniqueConstraint extends AbstractJDBCStartupTa
 
     @Override
     public String getOracleScript() {
-        this.runInSingleTransaction = false;
-        StringBuilder builder = new StringBuilder()
+        final StringBuilder builder = new StringBuilder()
             //Delete duplicates
             .append("DELETE FROM relationship R1 ")
             .append("WHERE R1.ROWID > ANY ( ")
             .append("  SELECT R2.ROWID ")
             .append("  FROM relationship R2 ")
-            .append("  WHERE R1.relation_type_value = R2.relation_type_value ); ")
+            .append("  WHERE R1.relation_type_value = R2.relation_type_value ); ");
 
-            //Create Unique Key
-            .append("ALTER TABLE relationship ADD CONSTRAINT unique_relation_type_value UNIQUE (relation_type_value); ");
+            //Create Unique Key deferred to method executeUpgrade below, because DML and DDL require different connections
         return builder.toString();
+    }
+
+    @Override
+    public void executeUpgrade() throws DotDataException, DotRuntimeException {
+        //Run upgrade as usual
+        super.executeUpgrade();
+
+        //For Oracle the Alter (DDL) needs to be executed on a different connection
+        Connection conn = null;
+        try {
+            if (DbConnectionFactory.isOracle()) {
+
+                conn = DbConnectionFactory.getDataSource().getConnection();
+                conn.setAutoCommit(true);
+
+                Logger.info(this, "Executing: "+ ORACLE_ADD_UNIQUE_CONSTRAINT);
+                final DotConnect db = new DotConnect();
+                db.setSQL(ORACLE_ADD_UNIQUE_CONSTRAINT);
+                db.loadResult(conn);
+                Logger.info(this, "Finished Executing: "+ ORACLE_ADD_UNIQUE_CONSTRAINT);
+            }
+        } catch(Exception e) {
+            throw new DotDataException(e);
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                throw new DotDataException(e.getMessage(), e);
+            }
+        }
     }
 }
