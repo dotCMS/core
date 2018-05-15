c55a209fb00f06863a2740757010d0e53ff6893e
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2018-05-14 23:09:23 -0600

    #13871 fixing small err in test

--- dotCMS/src/integration-test/java/com/dotmarketing/portlets/workflows/actionlet/copy/ContentValidationStrategy.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/portlets/workflows/actionlet/copy/ContentValidationStrategy.java
@@ -14,10 +14,16 @@ public class ContentValidationStrategy extends AbstractContentletValidationStrat
         assertEquals("body", originalValue, copyValue);
     };
 
+    AssertionStrategy commentAssertion = (final Contentlet original, final Contentlet copy) -> {
+        final String originalValue = (String) original.get("comment");
+        final String copyValue = (String) copy.get("comment");
+        assertEquals("comment", originalValue, copyValue);
+    };
+
     private final ImmutableMap<String, AssertionStrategy> assertionStrategyMap = ImmutableMap.<String, AssertionStrategy>builder()
             .put("folder",folderAssertion)
             .put("title",titleAssertion)
-            .put("comment",titleAssertion)
+            .put("comment",commentAssertion)
             .put("email",emailAssertion)
             .put("body",bodyAssertion)
             .build();
