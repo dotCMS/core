a5c99752b6d0cc0c6bcea27139f09f31cb9c0178
Nollymar Longa <nollymar@gmail.com>
2018-05-30 13:48:41 -0500

    Issue 14516 personas feature is not working (#14517)
    * #14516 Tags were not saved on index documents
    
    * #14516 Fixing tests

--- dotCMS/src/integration-test/java/com/dotcms/content/elasticsearch/business/ES6UpgradeTest.java
+++ dotCMS/src/integration-test/java/com/dotcms/content/elasticsearch/business/ES6UpgradeTest.java
@@ -16,7 +16,6 @@ import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
