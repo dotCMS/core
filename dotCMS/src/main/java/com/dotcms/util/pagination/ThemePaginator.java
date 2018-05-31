79e3b8ec55be277f0944f020f8a7218859cfc8e0
Nollymar Longa <nollymar.longa@dotcms.com>
2018-05-31 16:55:06 -0500

    #14456 Adding support for additional query params

--- dotCMS/src/main/java/com/dotcms/util/pagination/ThemePaginator.java
+++ dotCMS/src/main/java/com/dotcms/util/pagination/ThemePaginator.java
@@ -1,6 +1,5 @@
 package com.dotcms.util.pagination;
 
-import com.dotcms.content.elasticsearch.business.ESContentFactoryImpl;
 import com.dotcms.repackage.com.google.common.annotations.VisibleForTesting;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.common.model.ContentletSearch;
@@ -8,14 +7,11 @@ import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.portlets.contentlet.business.ContentletAPI;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
-import com.dotmarketing.util.Logger;
 import com.dotmarketing.util.PaginatedArrayList;
+import com.dotmarketing.util.UtilMethods;
 import com.liferay.portal.model.User;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.SearchHits;
 
 import static com.dotcms.util.CollectionsUtils.map;
-import static org.mockito.Mockito.when;
 
 import java.util.List;
 import java.util.Map;
@@ -27,9 +23,10 @@ import java.util.stream.Collectors;
 public class ThemePaginator implements Paginator<Contentlet> {
 
     public final static String HOST_ID_PARAMETER_NAME = "host-id";
+    public final static String SEARCH_PARAMETER = "search-parameter";
 
     private static final String LUCENE_QUERY_WITH_HOST = "+parentpath:/application/themes/* +title:template.vtl +conhost:%s";
-    private static final String LUCENE_QUERY_WITH_OUT_HOST = "+parentpath:/application/themes/* +title:template.vtl";
+    private static final String LUCENE_QUERY_WITHOUT_HOST = "+parentpath:/application/themes/* +title:template.vtl";
 
     private ContentletAPI contentletAPI;
 
@@ -50,14 +47,23 @@ public class ThemePaginator implements Paginator<Contentlet> {
                 (OrderDirection) params.get(ORDER_DIRECTION_PARAM_NAME) :
                 OrderDirection.ASC;
 
-        final String hostId = params != null ? (String) params.get(HOST_ID_PARAMETER_NAME) : null;
-        final String query = String.format(hostId == null ? LUCENE_QUERY_WITH_OUT_HOST : LUCENE_QUERY_WITH_HOST, hostId);
+        final String hostId       = params != null ? (String) params.get(HOST_ID_PARAMETER_NAME) : null;
+        final String searchParams = params != null ? (String) params.get(SEARCH_PARAMETER) : null;
+        StringBuilder query       = new StringBuilder(String.format(hostId == null ? LUCENE_QUERY_WITHOUT_HOST : LUCENE_QUERY_WITH_HOST, hostId));
+
+
+        if (UtilMethods.isSet(searchParams)){
+            query.append("+catchall:");
+            query.append(searchParams);
+            query.append("*");
+        }
+
         final String sortBy = String.format("parentPath %s", direction.toString().toLowerCase());
 
         try {
 
             final PaginatedArrayList<ContentletSearch> contentletSearches = (PaginatedArrayList<ContentletSearch>)
-                    contentletAPI.searchIndex(query, limit, offset, sortBy, user, false);
+                    contentletAPI.searchIndex(query.toString(), limit, offset, sortBy, user, false);
 
             final List<String>  inodes = contentletSearches.stream()
                     .map(contentletSearch -> contentletSearch.getInode())
@@ -65,7 +71,7 @@ public class ThemePaginator implements Paginator<Contentlet> {
 
             final PaginatedArrayList<Contentlet> result = new PaginatedArrayList();
             result.setTotalResults(contentletSearches.getTotalResults());
-            result.setQuery(query);
+            result.setQuery(query.toString());
             result.addAll(contentletAPI.findContentlets(inodes));
             return result;
         } catch (DotSecurityException | DotDataException e) {
