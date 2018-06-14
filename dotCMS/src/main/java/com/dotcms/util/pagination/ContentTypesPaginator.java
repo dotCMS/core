5d9734a187254577865803632bde00174cfa3083
Will Ezell <will@dotcms.com>
2018-06-14 12:23:45 -0400

    Issue 14560 allow filtering basetype (#14561)
    * #14560 allows filtering by base content type
    
    * #14560 allows filtering by base content type
    
    * #14560 Adding IT for ContentTypesPaginator
    
    * #14560 Applying Codacy suggestions

--- dotCMS/src/main/java/com/dotcms/util/pagination/ContentTypesPaginator.java
+++ dotCMS/src/main/java/com/dotcms/util/pagination/ContentTypesPaginator.java
@@ -1,6 +1,7 @@
 package com.dotcms.util.pagination;
 
 
+import com.dotcms.contenttype.model.type.BaseContentType;
 import com.dotcms.contenttype.model.type.ContentType;
 import com.dotcms.contenttype.transform.contenttype.JsonContentTypeTransformer;
 import com.dotcms.contenttype.transform.contenttype.StructureTransformer;
@@ -13,8 +14,14 @@ import com.dotmarketing.portlets.structure.model.Structure;
 import com.dotmarketing.util.PaginatedArrayList;
 import com.dotmarketing.util.UtilMethods;
 import com.liferay.portal.model.User;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 import org.apache.commons.lang.StringUtils;
 
@@ -52,7 +59,7 @@ public class ContentTypesPaginator implements PaginatorOrdered<Map<String, Objec
 
         try {
             List<Structure> structures = this.structureAPI.find(user, false, false, queryCondition,
-                    orderby, limit, offset, direction.toString().toLowerCase());
+                    orderby, limit, offset, UtilMethods.isSet(direction)?direction.toString().toLowerCase(): OrderDirection.ASC.name());
 
             List<ContentType> contentTypes = new StructureTransformer(structures).asList();
             List<Map<String, Object>> contentTypesTransform = transformContentTypesToMap(contentTypes);
@@ -106,7 +113,36 @@ public class ContentTypesPaginator implements PaginatorOrdered<Map<String, Objec
                         .collect(Collectors.toList());
     }
 
-    private String getQueryCondition(String filter){
-        return UtilMethods.isSet(filter) ? String.format("(upper(name) like '%%%s%%')", filter.toUpperCase()) : StringUtils.EMPTY;
+  private String getQueryCondition(final String filter) {
+
+    if (!UtilMethods.isSet(filter)) {
+      return StringUtils.EMPTY;
     }
+    final String filterUpper = filter.toUpperCase();
+
+
+    final List<String> andClauses = new ArrayList<>();
+
+
+    final StringTokenizer st = new StringTokenizer(filterUpper, " :,-");
+    while (st.hasMoreTokens()) {
+      final String tok = st.nextToken();
+      final Set<String> orClauses = new HashSet<>();
+      for (final BaseContentType btype : BaseContentType.values()) {
+        if (btype.name().equals(tok)) {
+          orClauses.add("structuretype=" + btype.getType());
+          break;
+        } else if (btype.name().startsWith(tok)) {
+          orClauses.add("structuretype=" + btype.getType());
+          orClauses.add(String.format("upper(name) like '%%%s%%'", tok));
+        } else {
+          orClauses.add(String.format("upper(name) like '%%%s%%'", tok));
+        }
+      }
+      andClauses.add('(' + String.join(" or ", orClauses) +')');
+    }
+    
+    return '(' + String.join(" and ", andClauses) + ')';
+
+  }
 }
