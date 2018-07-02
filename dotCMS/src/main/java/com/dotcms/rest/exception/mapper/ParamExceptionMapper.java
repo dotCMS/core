2dd3861766f831f2ae85aeff2b679da2beef2295
Jonathan <jsanca@Jonathans-MacBook-Pro-2.local>
2018-07-02 11:01:06 -0600

    #14545 adding support for invalid parameter on url to return 400 instead of the default behavior 404

---
+++ dotCMS/src/main/java/com/dotcms/rest/exception/mapper/ParamExceptionMapper.java
@@ -0,0 +1,32 @@
+package com.dotcms.rest.exception.mapper;
+
+import com.dotcms.exception.ExceptionUtil;
+import com.dotcms.repackage.javax.ws.rs.core.Response;
+import com.dotcms.repackage.javax.ws.rs.ext.ExceptionMapper;
+import com.dotcms.repackage.javax.ws.rs.ext.Provider;
+import com.dotcms.repackage.org.glassfish.jersey.server.ParamException;
+import com.dotmarketing.util.Logger;
+
+/**
+ *
+ * Instead of returning 404 error, on {@link ParamException} we want to return 400
+ */
+@Provider
+public class ParamExceptionMapper implements ExceptionMapper<ParamException> {
+
+    @Override
+    public Response toResponse(ParamException exception)
+    {
+        //Log into our logs first.
+        Logger.warn(this.getClass(), exception.getMessage(), exception);
+
+        //Create the message.
+        final String message = ExceptionUtil.getRootCause(exception).getMessage();
+
+        //Creating the message in JSON format.
+        final String entity = ExceptionMapperUtil.getJsonErrorAsString(exception.getParameterName(), message);
+
+        //Return 4xx message to the client.
+        return ExceptionMapperUtil.createResponse(entity, message);
+    }
+}
