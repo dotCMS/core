4460ee96c5035305257c2ecca365687cdc82d896
freddyrodriguez <freddy0309@gmail.com>
2018-07-03 12:35:35 -0600

    sending categories fields into content types

--- dotCMS/src/main/java/com/dotcms/contenttype/transform/field/JsonFieldTransformer.java
+++ dotCMS/src/main/java/com/dotcms/contenttype/transform/field/JsonFieldTransformer.java
@@ -8,10 +8,13 @@ import java.util.Map;
 
 import com.dotcms.contenttype.model.field.Field;
 import com.dotcms.contenttype.model.field.FieldVariable;
+import com.dotcms.contenttype.model.field.ImmutableCategoryField;
 import com.dotcms.contenttype.model.field.ImmutableFieldVariable;
 import com.dotcms.contenttype.transform.JsonTransformer;
 import com.dotcms.repackage.com.google.common.collect.ImmutableList;
+import com.dotmarketing.business.APILocator;
 import com.dotmarketing.business.DotStateException;
+import com.dotmarketing.portlets.categories.model.Category;
 import com.dotmarketing.util.json.JSONArray;
 import com.dotmarketing.util.json.JSONException;
 import com.dotmarketing.util.json.JSONObject;
@@ -148,6 +151,12 @@ public class JsonFieldTransformer implements FieldTransformer, JsonTransformer {
       field.put("fieldVariables", new JsonFieldVariableTransformer(f.fieldVariables()).mapList());
       field.remove("acceptedDataTypes");
       field.remove("dbColumn");
+
+      if (ImmutableCategoryField.class.getName().equals(field.get("clazz"))) {
+        final Category category = APILocator.getCategoryAPI()
+                .find(field.get("values").toString(), APILocator.getLoginServiceAPI().getLoggedInUser(), false);
+        field.put(CATEGORIES_PROPERTY_NAME, category.getMap());
+      }
 
       return field;
     } catch (Exception e) {
