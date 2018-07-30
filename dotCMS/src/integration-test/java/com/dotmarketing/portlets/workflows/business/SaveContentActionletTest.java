1868e12fc8e07d69845a680ad89d9905ae8e53e1
Jonathan <jsanca@Jonathans-MacBook-Pro-2.local>
2018-07-30 15:34:07 -0600

    #14988 adding a integration test for expire never expire fields on content types and save content actionlet

--- dotCMS/src/integration-test/java/com/dotmarketing/portlets/workflows/business/SaveContentActionletTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/portlets/workflows/business/SaveContentActionletTest.java
@@ -14,6 +14,7 @@ import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.portlets.contentlet.business.ContentletAPI;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
+import com.dotmarketing.portlets.contentlet.model.ContentletDependencies;
 import com.dotmarketing.portlets.folders.business.FolderAPI;
 import com.dotmarketing.portlets.workflows.actionlet.SaveContentActionlet;
 import com.dotmarketing.portlets.workflows.model.WorkflowProcessor;
@@ -35,7 +36,9 @@ public class SaveContentActionletTest extends BaseWorkflowIntegrationTest {
     private static WorkflowAPI                  workflowAPI            = null;
     private static ContentletAPI                contentletAPI          = null;
     private static ContentType                  type                   = null;
+    private static ContentType                  type2                  = null;
     private static Contentlet                   contentlet             = null;
+    private static Contentlet                   contentlet2            = null;
 
 
     @BeforeClass
@@ -53,9 +56,13 @@ public class SaveContentActionletTest extends BaseWorkflowIntegrationTest {
 
         // creates the type to trigger the scheme
         SaveContentActionletTest.createTestType(contentTypeAPI);
+        SaveContentActionletTest.createTestType2(contentTypeAPI);
 
         // associated the scheme to the type
         SaveContentActionletTest.workflowAPI.saveSchemesForStruct(new StructureTransformer(SaveContentActionletTest.type).asStructure(),
+                Arrays.asList(SaveContentActionletTest.schemeStepActionResult.getScheme()));
+
+        SaveContentActionletTest.workflowAPI.saveSchemesForStruct(new StructureTransformer(SaveContentActionletTest.type2).asStructure(),
                 Arrays.asList(SaveContentActionletTest.schemeStepActionResult.getScheme()));
 
     }
@@ -79,6 +86,34 @@ public class SaveContentActionletTest extends BaseWorkflowIntegrationTest {
         SaveContentActionletTest.type = contentTypeAPI.save(SaveContentActionletTest.type, fields);
     }
 
+    private static void createTestType2(final ContentTypeAPI contentTypeAPI) throws DotDataException, DotSecurityException {
+
+        SaveContentActionletTest.type2 = contentTypeAPI.save(
+                ContentTypeBuilder.builder(BaseContentType.CONTENT.immutableClass())
+                        .expireDateVar(null).folder(FolderAPI.SYSTEM_FOLDER).host(Host.SYSTEM_HOST)
+                        .description("DotSaveContentActionletTest2...")
+                        .name("DotSaveContentActionletTest2").owner(APILocator.systemUser().toString())
+                        .variable("DotSaveContentActionletTest2").build());
+
+        final List<Field> fields = new ArrayList<>(SaveContentActionletTest.type2.fields());
+
+        fields.add(FieldBuilder.builder(TextField.class).name("title").variable("title")
+                .contentTypeId(SaveContentActionletTest.type2.id()).dataType(DataTypes.TEXT).indexed(true).build());
+        fields.add(FieldBuilder.builder(TextField.class).name("txt").variable("txt")
+                .contentTypeId(SaveContentActionletTest.type2.id()).dataType(DataTypes.TEXT).indexed(true).build());
+        fields.add(FieldBuilder.builder(DateTimeField.class).name("publish").variable("publish")
+                .contentTypeId(SaveContentActionletTest.type2.id()).dataType(DataTypes.DATE).indexed(true).build());
+        fields.add(FieldBuilder.builder(DateTimeField.class).name("expire").variable("expire")
+                .contentTypeId(SaveContentActionletTest.type2.id()).dataType(DataTypes.DATE).indexed(true).build());
+
+        SaveContentActionletTest.type2 = contentTypeAPI.save(SaveContentActionletTest.type2, fields);
+
+        final ContentTypeBuilder contentTypeBuilder = ContentTypeBuilder.builder(SaveContentActionletTest.type2);
+        SaveContentActionletTest.type2 = contentTypeBuilder.expireDateVar("expire").publishDateVar("publish").build();
+
+        SaveContentActionletTest.type2 = contentTypeAPI.save(SaveContentActionletTest.type2);
+    }
+
     /**
      * Remove the content type and workflows created
      */
@@ -91,6 +126,12 @@ public class SaveContentActionletTest extends BaseWorkflowIntegrationTest {
 
                 SaveContentActionletTest.contentletAPI.archive(SaveContentActionletTest.contentlet, APILocator.systemUser(), false);
                 SaveContentActionletTest.contentletAPI.delete(SaveContentActionletTest.contentlet, APILocator.systemUser(), false);
+            }
+
+            if (null != SaveContentActionletTest.contentlet2) {
+
+                SaveContentActionletTest.contentletAPI.archive(SaveContentActionletTest.contentlet2, APILocator.systemUser(), false);
+                SaveContentActionletTest.contentletAPI.delete(SaveContentActionletTest.contentlet2, APILocator.systemUser(), false);
             }
         } finally {
 
@@ -106,6 +147,12 @@ public class SaveContentActionletTest extends BaseWorkflowIntegrationTest {
 
                     ContentTypeAPI contentTypeAPI = APILocator.getContentTypeAPI(APILocator.systemUser());
                     contentTypeAPI.delete(SaveContentActionletTest.type);
+                }
+
+                if (null != SaveContentActionletTest.type2) {
+
+                    ContentTypeAPI contentTypeAPI = APILocator.getContentTypeAPI(APILocator.systemUser());
+                    contentTypeAPI.delete(SaveContentActionletTest.type2);
                 }
             }
         }
@@ -154,6 +201,54 @@ public class SaveContentActionletTest extends BaseWorkflowIntegrationTest {
         Assert.assertFalse  (contentlet2.getInode().equals(firstInode));
         Assert.assertEquals ("Test Save 2",      contentlet2.getStringProperty("title"));
         Assert.assertEquals ("Test Save Text 2", contentlet2.getStringProperty("txt"));
+    }
 
+    @Test
+    public void save_Content_with_Publish_Expire_NerverExpire_Save_Success_Test() throws DotDataException, DotSecurityException {
+
+        final long       languageId = APILocator.getLanguageAPI().getDefaultLanguage().getId();
+        final Contentlet contentlet = new Contentlet();
+        final User       user       = APILocator.systemUser();
+        contentlet.setContentTypeId(SaveContentActionletTest.type2.id());
+        contentlet.setOwner(APILocator.systemUser().toString());
+        contentlet.setModDate(new Date());
+        contentlet.setLanguageId(languageId);
+        contentlet.setStringProperty("title", "Test Save");
+        contentlet.setStringProperty("txt",   "Test Save Text");
+        contentlet.setDateProperty("publish",   new Date());
+        contentlet.setStringProperty("NeverExpire","NeverExpire");
+        contentlet.setHost(Host.SYSTEM_HOST);
+        contentlet.setFolder(FolderAPI.SYSTEM_FOLDER);
+
+        // first save
+        final Contentlet contentlet1 = SaveContentActionletTest.contentletAPI.checkin(contentlet, user, false);
+        final String firstInode      = contentlet1.getInode();
+        SaveContentActionletTest.contentlet2              = contentlet1;
+
+        // triggering the save content action
+        contentlet1.setActionId(
+                this.schemeStepActionResult.getAction().getId());
+        contentlet1.setStringProperty("title", "Test Save 2");
+        contentlet1.setStringProperty("txt",   "Test Save Text 2");
+
+        final Contentlet contentlet2  =
+                SaveContentActionletTest.workflowAPI.fireContentWorkflow(contentlet1,
+                        new ContentletDependencies.Builder().respectAnonymousPermissions(false)
+                        .modUser(user)
+                        .workflowActionId(this.schemeStepActionResult.getAction().getId())
+                        .generateSystemEvent(false).build());
+
+        contentletAPI.isInodeIndexed(contentlet2.getInode());
+
+        final Contentlet contentlet3  = SaveContentActionletTest.contentletAPI.findContentletByIdentifier
+                (SaveContentActionletTest.contentlet2.getIdentifier(),
+                        false, languageId, user, false);
+
+        // the contentlet save by the action must be not null, should has a new version.
+        Assert.assertNotNull(contentlet3);
+        Assert.assertNotNull(contentlet3.getInode());
+        Assert.assertFalse  (contentlet3.getInode().equals(firstInode));
+        Assert.assertEquals ("Test Save 2",      contentlet3.getStringProperty("title"));
+        Assert.assertEquals ("Test Save Text 2", contentlet3.getStringProperty("txt"));
     }
 }
