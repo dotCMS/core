3b45d4c2f262844d253509d2b8fe769280cd144f
Jonathan Gamba <jonathan.gamba@dotcms.com>
2018-08-20 12:31:15 -0600

    dotCMS/core#15145

--- dotCMS/src/main/java/com/dotcms/spring/web/DotView.java
+++ dotCMS/src/main/java/com/dotcms/spring/web/DotView.java
@@ -10,37 +10,35 @@ import org.springframework.web.servlet.View;
 
 public class DotView implements View {
 
-	String pagePath;
-	
-	
-	public DotView(String pagePath) {
-		super();
-		this.pagePath = pagePath;
-	}
+    String pagePath;
 
+    public DotView(String pagePath) {
+        super();
+        this.pagePath = pagePath;
+    }
 
+    public String getContentType() {
+        return null;
+    }
 
-
-	public String getContentType() {
-		return null;
-	}
-
-	public void render(Map<String, ?> map, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(Map<String, ?> map, HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
         // get the VelocityContext
         VelocityContext ctx = VelocityUtil.getWebContext(request, response);
 
         if (!pagePath.startsWith("redirect:")) {
+
             // add the Spring map to the context
-            for(String x : map.keySet()){
+            for (String x : map.keySet()) {
                 ctx.put(x, map.get(x));
             }
 
-
-            
+            // add the context to the request.attr where it will be picked up and used by the VelocityServlet
+            request.setAttribute(com.dotcms.rendering.velocity.Constants.VELOCITY_CONTEXT, ctx);
             // override the page path
             request.setAttribute(Constants.CMS_FILTER_URI_OVERRIDE, pagePath);
 
-            request.getRequestDispatcher("/servlets/VelocityLiveServlet").forward(request, response);
+            request.getRequestDispatcher("/servlets/VelocityServlet").forward(request, response);
 
         } else {
             pagePath = pagePath.replaceFirst("redirect:", "");
