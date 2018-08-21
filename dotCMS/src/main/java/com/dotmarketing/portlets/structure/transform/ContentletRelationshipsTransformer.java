30d26eec7e55913e31d40cdeb1e48dc953babd97
Jonathan <jonathan.sanchez@dotcms.com>
2018-08-21 10:58:34 -0600

    #15053 now if the action has a save actionlet, the checkin is not cal… (#15127)
    * #15053 now if the action has a save actionlet, the checkin is not called, fireContentletWorkflow instead
    
    * #15053 refactoring the ContentletRelationships in order to keep the transforming code centralized
    
    * #15053 codacy feedback

---
+++ dotCMS/src/main/java/com/dotmarketing/portlets/structure/transform/ContentletRelationshipsTransformer.java
@@ -0,0 +1,81 @@
+package com.dotmarketing.portlets.structure.transform;
+
+import com.dotcms.util.transform.DBTransformer;
+import com.dotmarketing.business.CacheLocator;
+import com.dotmarketing.business.FactoryLocator;
+import com.dotmarketing.portlets.contentlet.model.Contentlet;
+import com.dotmarketing.portlets.structure.model.ContentletRelationships;
+import com.dotmarketing.portlets.structure.model.Relationship;
+import com.dotmarketing.portlets.structure.model.Structure;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * ContentletRelationships transformer
+ * @author jsanca
+ */
+public class ContentletRelationshipsTransformer implements DBTransformer<ContentletRelationships> {
+
+    private final Contentlet contentlet;
+    private final Map<Relationship, List<Contentlet>> contentRelationships;
+
+    public ContentletRelationshipsTransformer(final Contentlet contentlet,
+            final Map<Relationship, List<Contentlet>> contentRelationships) {
+
+        this.contentlet           = contentlet;
+        this.contentRelationships = contentRelationships;
+    }
+
+    @Override
+    public List<ContentletRelationships> asList() {
+
+        final List<ContentletRelationships> relationshipsList = new ArrayList<>();
+
+        final ContentletRelationships contentletRelationships =
+                this.getContentletRelationshipsFromMap(this.contentlet,
+                        this.contentRelationships);
+
+        if (null != contentletRelationships) {
+            relationshipsList.add(contentletRelationships);
+        }
+
+        return relationshipsList;
+    }
+
+    private ContentletRelationships getContentletRelationshipsFromMap(final Contentlet contentlet,
+                                                                      final Map<Relationship, List<Contentlet>> contentRelationships) {
+
+        if(contentRelationships == null) {
+
+            return null;
+        }
+
+        final Structure structure = CacheLocator.getContentTypeCache()
+                .getStructureByInode(contentlet.getStructureInode());
+        final ContentletRelationships relationshipsData = new ContentletRelationships(contentlet);
+        final List<ContentletRelationships.ContentletRelationshipRecords> relationshipsRecords = new ArrayList<ContentletRelationships.ContentletRelationshipRecords>();
+        relationshipsData.setRelationshipsRecords(relationshipsRecords);
+
+        for(final Map.Entry<Relationship, List<Contentlet>> relEntry : contentRelationships.entrySet()) {
+
+            final Relationship relationship = relEntry.getKey();
+            final boolean hasChildren       = FactoryLocator.getRelationshipFactory().isChild(relationship, structure);
+            boolean hasParent               = FactoryLocator.getRelationshipFactory().isParent(relationship, structure);
+
+            // self-join (same CT for parent and child) relationships return true to both, so since we can't
+            // determine if it's parent or child we always assume child (e.g. Coming from the Content REST API)
+            if (hasParent && hasChildren) {
+                hasParent = false;
+            }
+            
+            final ContentletRelationships.ContentletRelationshipRecords
+                    records = relationshipsData.new ContentletRelationshipRecords(relationship, hasParent);
+            records.setRecords(relEntry.getValue());
+            relationshipsRecords.add(records);
+        }
+
+        return relationshipsData;
+    }
+}
