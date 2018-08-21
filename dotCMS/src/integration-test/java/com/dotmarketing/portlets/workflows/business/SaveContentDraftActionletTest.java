1c5336909ae70f852868d3dc1a2bba00ff193781
Jonathan <jsanca@Jonathans-MBP-2.Home>
2018-08-21 00:10:07 -0600

    #15137 unit test for saving draft with relationships and categories

--- dotCMS/src/integration-test/java/com/dotmarketing/portlets/workflows/business/SaveContentDraftActionletTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/portlets/workflows/business/SaveContentDraftActionletTest.java
@@ -12,13 +12,19 @@ import com.dotcms.contenttype.transform.contenttype.StructureTransformer;
 import com.dotcms.util.IntegrationTestInitService;
 import com.dotmarketing.beans.Host;
 import com.dotmarketing.business.APILocator;
-import com.dotmarketing.db.HibernateUtil;
+import com.dotmarketing.business.CacheLocator;
+import com.dotmarketing.business.FactoryLocator;
 import com.dotmarketing.exception.AlreadyExistException;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotSecurityException;
+import com.dotmarketing.portlets.categories.model.Category;
 import com.dotmarketing.portlets.contentlet.business.ContentletAPI;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
+import com.dotmarketing.portlets.contentlet.model.ContentletDependencies;
 import com.dotmarketing.portlets.folders.business.FolderAPI;
+import com.dotmarketing.portlets.structure.model.ContentletRelationships;
+import com.dotmarketing.portlets.structure.model.Relationship;
+import com.dotmarketing.portlets.structure.model.Structure;
 import com.dotmarketing.portlets.workflows.actionlet.SaveContentAsDraftActionlet;
 import com.dotmarketing.util.UUIDGenerator;
 import com.liferay.portal.model.User;
@@ -27,10 +33,7 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class SaveContentDraftActionletTest extends BaseWorkflowIntegrationTest {
 
@@ -39,6 +42,8 @@ public class SaveContentDraftActionletTest extends BaseWorkflowIntegrationTest {
     private static ContentletAPI                contentletAPI          = null;
     private static ContentType                  type                   = null;
     private static Contentlet                   contentlet             = null;
+    private static Contentlet                   contentlet2             = null;
+    private static Relationship                 relationship           = null;
 
 
     @BeforeClass
@@ -61,6 +66,13 @@ public class SaveContentDraftActionletTest extends BaseWorkflowIntegrationTest {
         SaveContentDraftActionletTest.workflowAPI.saveSchemesForStruct(new StructureTransformer(SaveContentDraftActionletTest.type).asStructure(),
                 Arrays.asList(SaveContentDraftActionletTest.schemeStepActionResult.getScheme()));
 
+        relationship = new Relationship();
+        relationship.setParentRelationName("Parent");
+        relationship.setChildRelationName("Child");
+        relationship.setRelationTypeValue("IT-Parent-Child" + System.currentTimeMillis());
+        relationship.setParentStructureInode(SaveContentDraftActionletTest.type.inode());
+        relationship.setChildStructureInode(SaveContentDraftActionletTest.type.inode());
+        APILocator.getRelationshipAPI().create(relationship);
     }
 
     private static void createTestType(final ContentTypeAPI contentTypeAPI) throws DotDataException, DotSecurityException {
@@ -95,6 +107,12 @@ public class SaveContentDraftActionletTest extends BaseWorkflowIntegrationTest {
                 SaveContentDraftActionletTest.contentletAPI.archive(SaveContentDraftActionletTest.contentlet, APILocator.systemUser(), false);
                 SaveContentDraftActionletTest.contentletAPI.delete(SaveContentDraftActionletTest.contentlet, APILocator.systemUser(), false);
             }
+
+            if (null != SaveContentDraftActionletTest.contentlet2) {
+
+                SaveContentDraftActionletTest.contentletAPI.archive(SaveContentDraftActionletTest.contentlet2, APILocator.systemUser(), false);
+                SaveContentDraftActionletTest.contentletAPI.delete(SaveContentDraftActionletTest.contentlet2, APILocator.systemUser(), false);
+            }
         } finally {
 
             try {
@@ -104,6 +122,10 @@ public class SaveContentDraftActionletTest extends BaseWorkflowIntegrationTest {
                     SaveContentDraftActionletTest.cleanScheme(SaveContentDraftActionletTest.schemeStepActionResult.getScheme());
                 }
             } finally {
+
+                if (relationship != null) {
+                    APILocator.getRelationshipAPI().delete(relationship);
+                }
 
                 if (null != SaveContentDraftActionletTest.type) {
 
@@ -174,7 +196,105 @@ public class SaveContentDraftActionletTest extends BaseWorkflowIntegrationTest {
         Assert.assertTrue(contentlet5.getInode().equals(contentlet3.getInode()));
         Assert.assertEquals("Test Save 3", contentlet5.getStringProperty("title"));
         Assert.assertEquals("Test Save Text 3", contentlet5.getStringProperty("txt"));
+    }
 
+    @Test
+    public void save_content_draft_test_with_categories_success() throws DotDataException, DotSecurityException {
 
+        final long       languageId = APILocator.getLanguageAPI().getDefaultLanguage().getId();
+        final Contentlet contentlet = new Contentlet();
+        final User       user       = APILocator.systemUser();
+        contentlet.setContentTypeId(SaveContentDraftActionletTest.type.id());
+        contentlet.setOwner(user.toString());
+        contentlet.setModUser(user.getUserId());
+        contentlet.setModDate(new Date());
+        contentlet.setLanguageId(languageId);
+        contentlet.setStringProperty("title", "Test Saves");
+        contentlet.setStringProperty("txt",   "Test Saves Text");
+        contentlet.setHost(Host.SYSTEM_HOST);
+        contentlet.setFolder(FolderAPI.SYSTEM_FOLDER);
+
+        // first save
+        final Contentlet contentlet1 = SaveContentDraftActionletTest.contentletAPI.checkin(contentlet, user, false);
+        SaveContentDraftActionletTest.contentlet2 = contentlet1;
+
+        contentlet1.setStringProperty("title", "Test Saves 2");
+        contentlet1.setStringProperty("txt", "Test Saves Text 2");
+
+        contentletAPI.isInodeIndexed(contentlet1.getInode());
+
+        final Contentlet contentlet2 = SaveContentDraftActionletTest.contentletAPI.checkout(contentlet1.getInode(), user, false);
+
+        contentlet2.setStringProperty("title", "Test Saves 2");
+        contentlet2.setStringProperty("txt", "Test Saves Text 2");
+
+        final Contentlet contentlet3 = SaveContentDraftActionletTest.contentletAPI.checkin(contentlet2, user, false);
+
+        contentletAPI.isInodeIndexed(contentlet3.getInode());
+
+        final Contentlet contentlet4 = SaveContentDraftActionletTest.contentletAPI.
+                find(contentlet3.getInode(), user, false);
+        // triggering the save content action
+
+        contentlet4.setActionId(
+                this.schemeStepActionResult.getAction().getId());
+        contentlet4.setStringProperty("title", "Test Saves 3");
+        contentlet4.setStringProperty("txt", "Test Saves Text 3");
+
+        if (SaveContentDraftActionletTest.contentletAPI.canLock(contentlet4, user) && contentlet4.isLocked()) {
+            SaveContentDraftActionletTest.contentletAPI.unlock(contentlet4, user, false);
+        }
+
+        final List<Category> categories    =  APILocator.getCategoryAPI().findAll(user, false);
+        final Relationship relationship    = SaveContentDraftActionletTest.relationship;
+        final List<Contentlet> contentlets = APILocator.getRelationshipAPI().dbRelatedContent(relationship, contentlet4);
+        final Map<Relationship, List<Contentlet>> contentRelationshipsMap = new HashMap<>();
+        contentRelationshipsMap.put(relationship, contentlets);
+        final ContentletRelationships contentletRelationships = getContentletRelationshipsFromMap(contentlet4, contentRelationshipsMap);
+
+        SaveContentDraftActionletTest.workflowAPI.fireContentWorkflow(contentlet4,
+                new ContentletDependencies.Builder().categories(categories).
+                relationships(contentletRelationships).modUser(user)
+                .respectAnonymousPermissions(false).generateSystemEvent(false).build());
+
+        final Contentlet contentlet5 = SaveContentDraftActionletTest.contentletAPI.findContentletByIdentifier
+                (SaveContentDraftActionletTest.contentlet2.getIdentifier(),
+                        false, languageId, user, false);
+
+        // the contentlet save by the action must be not null, should has the same version.
+        Assert.assertNotNull(contentlet5);
+        Assert.assertNotNull(contentlet5.getInode());
+        Assert.assertTrue(contentlet5.getInode().equals(contentlet3.getInode()));
+        Assert.assertEquals("Test Saves 3", contentlet5.getStringProperty("title"));
+        Assert.assertEquals("Test Saves Text 3", contentlet5.getStringProperty("txt"));
+    }
+
+    private static ContentletRelationships getContentletRelationshipsFromMap(Contentlet contentlet,
+                                                                      Map<Relationship, List<Contentlet>> contentRelationships) {
+
+        if(contentRelationships == null) {
+            return null;
+        }
+
+        Structure st = CacheLocator.getContentTypeCache().getStructureByInode(contentlet.getStructureInode());
+        ContentletRelationships relationshipsData = new ContentletRelationships(contentlet);
+        List<ContentletRelationships.ContentletRelationshipRecords> relationshipsRecords = new ArrayList<ContentletRelationships.ContentletRelationshipRecords>();
+        relationshipsData.setRelationshipsRecords(relationshipsRecords);
+        for(Map.Entry<Relationship, List<Contentlet>> relEntry : contentRelationships.entrySet()) {
+            Relationship relationship = relEntry.getKey();
+            boolean hasParent = FactoryLocator.getRelationshipFactory().isParent(relationship, st);
+            boolean hasChildren = FactoryLocator.getRelationshipFactory().isChild(relationship, st);
+
+            // self-join (same CT for parent and child) relationships return true to both, so since we can't
+            // determine if it's parent or child we always assume child (e.g. Coming from the Content REST API)
+            if (hasParent && hasChildren) {
+                hasParent = false;
+            }
+            ContentletRelationships.ContentletRelationshipRecords
+                    records = relationshipsData.new ContentletRelationshipRecords(relationship, hasParent);
+            records.setRecords(relEntry.getValue());
+            relationshipsRecords.add(records);
+        }
+        return relationshipsData;
     }
 }
