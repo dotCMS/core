3f1db6846f05f06b504b8def7c552519ccfa3593
erickgonzalez <erick.gonzalez@dotcms.com>
2018-08-27 03:46:42 -0600

    #15013 Integration Test (#15189)
    * #15013 Integration Test
    
    * #15013 code review
    
    * #15013 code review v2

---
+++ dotCMS/src/integration-test/java/com/dotcms/enterprise/publishing/remote/HostBundlerHandlerTest.java
@@ -0,0 +1,158 @@
+package com.dotcms.enterprise.publishing.remote;
+
+import com.dotcms.IntegrationTestBase;
+import com.dotcms.LicenseTestUtil;
+import com.dotcms.enterprise.publishing.remote.bundler.HostBundler;
+import com.dotcms.enterprise.publishing.remote.handler.HostHandler;
+import com.dotcms.publisher.pusher.PushPublisherConfig;
+import com.dotcms.publishing.BundlerStatus;
+import com.dotcms.publishing.PublisherConfig.Operation;
+import com.dotcms.util.IntegrationTestInitService;
+import com.dotmarketing.beans.Host;
+import com.dotmarketing.business.APILocator;
+import com.dotmarketing.db.HibernateUtil;
+import com.dotmarketing.portlets.contentlet.business.HostAPI;
+import com.dotmarketing.util.Config;
+import com.liferay.portal.model.User;
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class HostBundlerHandlerTest extends IntegrationTestBase {
+
+    private static User user;
+    private static HostAPI hostAPI;
+    private static int originalHostSize;
+
+    @BeforeClass
+    public static void prepare() throws Exception {
+
+        //Setting web app environment
+        IntegrationTestInitService.getInstance().init();
+        LicenseTestUtil.getLicense();
+
+        hostAPI = APILocator.getHostAPI();
+        user = APILocator.getUserAPI().getSystemUser();
+        originalHostSize = APILocator.getHostAPI().findAll(user, false).size();
+    }
+
+    /**
+     * This test creates a host and add it to a bundle as an unpublish operation (push-remove option).
+     * After a couple of seconds, the HostHandler handle this bundle and apply the operation (removes the host)
+     */
+    @Test
+    public void testBundlerHandler_UnpublishHost_Success() throws Exception {
+        final BundlerStatus status;
+        final String assetRealPath = Config.getStringProperty("ASSET_REAL_PATH", "test-resources");
+        final File tempDir = new File(assetRealPath + "/bundles/" + System.currentTimeMillis());
+        final HostBundler hostBundler;
+        final PushPublisherConfig config;
+        final Set<String> contentSet;
+
+        try {
+
+            contentSet = new HashSet();
+            Host host = new Host();
+            hostBundler = new HostBundler();
+            status = new BundlerStatus(HostBundler.class.getName());
+
+            //Creating host
+            host.setHostname("hostUnpublish" + System.currentTimeMillis() + ".dotcms.com");
+            host.setDefault(false);
+
+            HibernateUtil.startTransaction();
+            host = APILocator.getHostAPI().save(host, user, false);
+            HibernateUtil.closeAndCommitTransaction();
+
+            APILocator.getContentletAPI().isInodeIndexed(host.getInode());
+
+            Assert.assertEquals(originalHostSize + 1, APILocator.getHostAPI().findAll(user, false).size());
+
+            contentSet.add(host.getIdentifier());
+
+            //Mocking Push Publish configuration
+            config = Mockito.mock(PushPublisherConfig.class);
+            Mockito.when(config.getHostSet()).thenReturn(contentSet);
+            Mockito.when(config.isDownloading()).thenReturn(true);
+            Mockito.when(config.getOperation()).thenReturn(Operation.UNPUBLISH);
+            hostBundler.setConfig(config);
+
+            //Creating temp bundle dir
+
+            if (!tempDir.exists()) {
+                tempDir.mkdirs();
+            }
+
+            hostBundler.generate(tempDir, status);
+            Assert.assertEquals(1, status.getCount()); //Only 1 content in the bundler
+
+            Thread.sleep(5000); //Let's wait a couple of seconds before running the Hanlder
+
+            //Handler
+            final HostHandler hostHandler = new HostHandler(config);
+            hostHandler.handle(tempDir);
+
+            Assert.assertEquals(originalHostSize, APILocator.getHostAPI().findAll(user, false).size());
+        } finally {
+            tempDir.delete();
+        }
+    }
+
+    @Test
+    public void testGenerate_success_when_liveContentletIsNotFound()
+            throws Exception {
+        final BundlerStatus status;
+        final String assetRealPath = Config.getStringProperty("ASSET_REAL_PATH", "test-resources");
+        final File tempDir = new File(assetRealPath + "/bundles/" + System.currentTimeMillis());
+        final HostBundler hostBundler;
+        final PushPublisherConfig config;
+        final Set<String> contentSet;
+        contentSet = new HashSet();
+        Host host = new Host();
+
+        try {
+
+            hostBundler = new HostBundler();
+            status = new BundlerStatus(HostBundler.class.getName());
+
+            //Creating host
+            host.setHostname("hostGenerate" + System.currentTimeMillis() + ".dotcms.com");
+            host.setDefault(false);
+
+            HibernateUtil.startTransaction();
+            host = APILocator.getHostAPI().save(host, user, false);
+            HibernateUtil.closeAndCommitTransaction();
+
+            APILocator.getContentletAPI().isInodeIndexed(host.getInode());
+
+            Assert.assertEquals(originalHostSize + 1, APILocator.getHostAPI().findAll(user, false).size());
+
+            contentSet.add(host.getIdentifier());
+
+            //Mocking Push Publish configuration
+            config = Mockito.mock(PushPublisherConfig.class);
+            Mockito.when(config.getHostSet()).thenReturn(contentSet);
+            Mockito.when(config.isDownloading()).thenReturn(true);
+            Mockito.when(config.getOperation()).thenReturn(Operation.PUBLISH);
+            hostBundler.setConfig(config);
+
+            //Creating temp bundle dir
+
+            if (!tempDir.exists()) {
+                tempDir.mkdirs();
+            }
+
+            hostBundler.generate(tempDir, status);
+            Assert.assertEquals(1, status.getCount()); //Only 1 content in the bundler
+        } finally {
+            tempDir.delete();
+            hostAPI.archive(host, user, false);
+            hostAPI.delete(host, user, false);
+        }
+    }
+
+}
