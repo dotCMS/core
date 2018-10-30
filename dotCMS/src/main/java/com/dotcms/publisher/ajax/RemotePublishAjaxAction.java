12e18de9f81ef71b9aa578a493ce719082e2fc62
Jose Castro <jose.castro@dotcms.com>
2018-10-30 13:11:50 -0600

    #15449 : Sanitize bundle file name when uploading bundle using Microsoft Edge on Windows 10

--- dotCMS/src/main/java/com/dotcms/publisher/ajax/RemotePublishAjaxAction.java
+++ dotCMS/src/main/java/com/dotcms/publisher/ajax/RemotePublishAjaxAction.java
@@ -713,19 +713,18 @@ public class RemotePublishAjaxAction extends AjaxAction {
 
         FileItemFactory factory = new DiskFileItemFactory();
         ServletFileUpload upload = new ServletFileUpload( factory );
-        @SuppressWarnings ("unchecked")
-        List<FileItem> items = (List<FileItem>) upload.parseRequest( request );
-
-        InputStream bundle = items.get( 0 ).getInputStream();
-        String bundleName = items.get( 0 ).getName();
-        String bundlePath = ConfigUtils.getBundlePath() + File.separator;
-        String bundleFolder = bundleName.substring( 0, bundleName.indexOf( ".tar.gz" ) );
-        String endpointId = getUser().getUserId();
-        response.setContentType( "text/html; charset=utf-8" );
         PrintWriter out = response.getWriter();
-
         PublishAuditStatus status;
+        String bundleName = null;
         try {
+            @SuppressWarnings ("unchecked")
+            List<FileItem> items = (List<FileItem>) upload.parseRequest( request );
+            InputStream bundle = items.get( 0 ).getInputStream();
+            bundleName = sanitizeBundleName(items.get(0).getName());
+            String bundlePath = ConfigUtils.getBundlePath() + File.separator;
+            String bundleFolder = bundleName.substring( 0, bundleName.indexOf( ".tar.gz" ) );
+            String endpointId = getUser().getUserId();
+            response.setContentType( "text/html; charset=utf-8" );
             status = PublishAuditAPI.getInstance().updateAuditTable( endpointId, endpointId, bundleFolder );
 
             // Write file on FS
@@ -738,9 +737,34 @@ public class RemotePublishAjaxAction extends AjaxAction {
             out.print( "<html><head><script>isLoaded = true;</script></head><body><textarea>{'status':'success'}</textarea></body></html>" );
 
         } catch ( DotPublisherException e ) {
+            Logger.error(this, String.format("An error occurred when uploading bundle '%s'", bundleName), e);
             out.print( "<html><head><script>isLoaded = true;</script></head><body><textarea>{'status':'error'}</textarea></body></html>" );
         }
 
+    }
+
+    /**
+     * Checks the name of the bundle file coming from the request to make sure that it is correct. For example, in
+     * Microsoft Edge on Windows 10, the name includes the absolute path to the file instead of the file name only. We
+     * need to strip the absolute path away.
+     *
+     * @param bundleName The name of the bundle file.
+     *
+     * @return The sanitized bundle file name.
+     */
+    private String sanitizeBundleName(String bundleName) throws DotPublisherException {
+        if (!UtilMethods.isSet(bundleName)) {
+            final String errorMsg = "The name for the uploaded bundle is null or empty";
+            Logger.error(this, errorMsg);
+            throw new DotPublisherException(errorMsg);
+        }
+        if (!bundleName.contains(File.separator)) {
+            return bundleName;
+        } else {
+            final int indexOfSeparator = bundleName.lastIndexOf(File.separator);
+            bundleName = bundleName.substring(indexOfSeparator + 1);
+            return bundleName;
+        }
     }
 
     /**
