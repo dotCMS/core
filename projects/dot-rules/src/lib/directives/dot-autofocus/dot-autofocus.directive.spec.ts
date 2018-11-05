dfbef61958645197d25a63f280fa820436462785
Humberto Morera <humberto.morera@dotcms.com>
2018-11-05 12:48:54 -0600

    remove tlint comment

--- projects/dot-rules/src/lib/directives/dot-autofocus/dot-autofocus.directive.spec.ts
+++ projects/dot-rules/src/lib/directives/dot-autofocus/dot-autofocus.directive.spec.ts
@@ -1,5 +1,3 @@
-/* tslint:disable:no-unused-variable */
-
 import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { DebugElement, Component } from '@angular/core';
 import { By } from '@angular/platform-browser';
