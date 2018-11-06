607a8037a48d5a10caa743b59cdbdb519ee66370
Freddy Rodriguez <freddy0309@gmail.com>
2018-11-06 12:21:20 -0600

    Navigate between page in LIVE mode with login user (#15516)
    * Navigate between page in LIVE mode with login user
    
    * refactoring

--- dotCMS/src/main/java/com/dotmarketing/util/PageMode.java
+++ dotCMS/src/main/java/com/dotmarketing/util/PageMode.java
@@ -1,6 +1,7 @@
 package com.dotmarketing.util;
 
 import com.dotcms.api.web.HttpServletRequestThreadLocal;
+import com.dotmarketing.business.APILocator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -117,8 +118,8 @@ public enum PageMode {
         PageMode sessionPageMode = (PageMode) ses.getAttribute(WebKeys.PAGE_MODE_SESSION);
         HttpServletRequest request = HttpServletRequestThreadLocal.INSTANCE.getRequest();
 
-        return  sessionPageMode != PageMode.LIVE &&
-                request != null &&
+        return  request != null &&
+                (sessionPageMode != PageMode.LIVE || APILocator.getLoginServiceAPI().isLoggedIn(request)) &&
                 request.getAttribute(WebKeys.PAGE_MODE_PARAMETER) == null ;
     }
 
