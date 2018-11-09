5778d393b75bcd9eb33eeb1674dce91891805dd4
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2018-11-09 08:57:19 -0600

    #15359 adding an upgrade task integration test

--- dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task05035LanguageTableIdentityOffTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task05035LanguageTableIdentityOffTest.java
@@ -1,5 +1,35 @@
 package com.dotmarketing.startup.runonce;
 
+import com.dotcms.util.IntegrationTestInitService;
+import com.dotmarketing.db.DbConnectionFactory;
+import com.dotmarketing.db.DbType;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotSecurityException;
+import com.dotmarketing.util.Logger;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class Task05035LanguageTableIdentityOffTest {
+
+    @BeforeClass
+    public static void prepare() throws Exception{
+        // Setting web app environment
+        IntegrationTestInitService.getInstance().init();
+    }
+
+    @Test
+    public void testExecuteUpgrade() throws DotDataException, DotSecurityException {
+       final DbType dbType = DbType.getDbType(DbConnectionFactory.getDBType());
+       try{
+         final Task05035LanguageTableIdentityOff languageTableIdentityOff = new Task05035LanguageTableIdentityOff();
+         languageTableIdentityOff.executeUpgrade();
+       } catch (Exception e) {
+           final String  errMessage = "Could not modify Language table on db of type: " + dbType;
+           Logger.error(getClass(),errMessage, e);
+           Assert.fail(errMessage);
+       }
+    }
+
 
 }
