c3c5717b79ecc921dd8b8401cc97a59f466befd5
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2018-12-04 13:44:31 -0600

    #15613 refactoring checkin  synchronized block to use stripe locks

--- dotCMS/src/main/java/com/dotcms/concurrent/lock/DotKeyLockManagerBuilder.java
+++ dotCMS/src/main/java/com/dotcms/concurrent/lock/DotKeyLockManagerBuilder.java
@@ -10,12 +10,12 @@ public class DotKeyLockManagerBuilder {
     private static final String DOTCMS_CONCURRENT_LOCK_STRIPS_TIMEUNIT = "dotcms.concurrent.locks.strips.timeunit";
 
 
-     static <R,K> DotKeyLockManager <R,K> newDefaultStripedLockManager() {
+    public static <R,K> DotKeyLockManager <R,K> newDefaultStripedLockManager() {
         final int strips = Config.getIntProperty(DOTCMS_CONCURRENT_LOCK_STRIPS, StripedLockImpl.DEFAULT_STRIPES);
         return new StripedLockImpl<>(strips);
      }
 
-    static <R,K> DotKeyLockManager <R,K> newStripedLockManager() {
+    public static <R,K> DotKeyLockManager <R,K> newStripedLockManager() {
         final int strips = Config.getIntProperty(DOTCMS_CONCURRENT_LOCK_STRIPS, StripedLockImpl.DEFAULT_STRIPES);
         final int time = Config.getIntProperty(DOTCMS_CONCURRENT_LOCK_STRIPS_TIME, StripedLockImpl.DEFAULT_TIME);
         final TimeUnit timeUnit = TimeUnit.valueOf(Config.getStringProperty(DOTCMS_CONCURRENT_LOCK_STRIPS_TIMEUNIT, StripedLockImpl.DEFAULT_TU.name()));
