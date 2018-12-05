617ddd4c72f13f3196e9a4b9d437e2ed65486562
Freddy Montes <freddy@dotcms.com>
2018-12-05 17:41:13 -0600

    Add label feature to the copy to clipboard button and add it to content types

--- src/app/view/components/dot-copy-button/dot-copy-button.component.spec.ts
+++ src/app/view/components/dot-copy-button/dot-copy-button.component.spec.ts
@@ -1,3 +1,5 @@
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DotCopyButtonComponent } from './dot-copy-button.component';
@@ -5,8 +7,6 @@ import { DotMessageService } from '@services/dot-messages-service';
 import { MockDotMessageService } from 'src/app/test/dot-message-service.mock';
 import { DotClipboardUtil } from 'src/app/api/util/clipboard/ClipboardUtil';
 import { DotIconButtonModule } from '@components/_common/dot-icon-button/dot-icon-button.module';
-import { DebugElement } from '@angular/core';
-import { By } from '@angular/platform-browser';
 
 const messageServiceMock = new MockDotMessageService({
     Copy: 'Copy',
@@ -19,6 +19,7 @@ describe('DotCopyButtonComponent', () => {
     let de: DebugElement;
     let dotClipboardUtil: DotClipboardUtil;
     let button: DebugElement;
+    let label: DebugElement;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -39,46 +40,91 @@ describe('DotCopyButtonComponent', () => {
         component = fixture.componentInstance;
         de = fixture.debugElement;
 
-        component.copy = 'Text to copy';
-        fixture.detectChanges();
-
         dotClipboardUtil = de.injector.get(DotClipboardUtil);
-        button = de.query(By.css('dot-icon-button'));
 
         spyOn(dotClipboardUtil, 'copy').and.callFake(() => {
             return new Promise((resolve) => {
                 resolve(true);
             });
         });
+
+        component.copy = 'Text to copy';
     });
 
-    it('should have dot-icon-button', () => {
-        expect(button.componentInstance.icon).toBe('file_copy');
-    });
-
-    it('should have tooltip', () => {
-        const tooltip: DebugElement = de.query(By.css('span'));
-        expect(tooltip.nativeElement.textContent).toBe('Copy');
-    });
-
-    it('should copy text to clipboard', () => {
-        button.triggerEventHandler('click', {});
-
-        expect(dotClipboardUtil.copy).toHaveBeenCalledWith('Text to copy');
-    });
-
-    it('should update tooltip text when copy', async(() => {
-        const spyTooltipText = spyOnProperty(component, 'tooltipText', 'set');
-
-        button.triggerEventHandler('click', {});
-
-        fixture.whenStable().then(() => {
-            setTimeout(() => {
-                expect([].concat.apply([], spyTooltipText.calls.allArgs())).toEqual([
-                    'Copied',
-                    'Copy'
-                ]);
-            }, 1000);
+    describe('with button', () => {
+        beforeEach(() => {
+            fixture.detectChanges();
+            button = de.query(By.css('dot-icon-button'));
         });
-    }));
+
+        it('should have dot-icon-button', () => {
+            expect(button.componentInstance.icon).toBe('file_copy');
+        });
+
+        it('should not have label', () => {
+            label = de.query(By.css('.label'));
+            expect(label).toBeNull();
+        });
+
+        it('should have tooltip', () => {
+            const tooltip: DebugElement = de.query(By.css('span'));
+            expect(tooltip.nativeElement.textContent).toBe('Copy');
+        });
+
+        it('should copy text to clipboard', () => {
+            const stopPropagation = jasmine.createSpy('stopPropagation');
+
+            button.triggerEventHandler('click', {
+                stopPropagation: stopPropagation
+            });
+
+            expect(dotClipboardUtil.copy).toHaveBeenCalledWith('Text to copy');
+            expect(stopPropagation).toHaveBeenCalledTimes(1);
+        });
+
+        it('should update tooltip text when copy', async(() => {
+            const spyTooltipText = spyOnProperty(component, 'tooltipText', 'set');
+
+            button.triggerEventHandler('click', {
+                stopPropagation: () => {}
+            });
+
+            fixture.whenStable().then(() => {
+                setTimeout(() => {
+                    expect([].concat.apply([], spyTooltipText.calls.allArgs())).toEqual([
+                        'Copied',
+                        'Copy'
+                    ]);
+                }, 1000);
+            });
+        }));
+    });
+
+    describe('with label', () => {
+        beforeEach(() => {
+            component.label = 'Label';
+            fixture.detectChanges();
+            label = de.query(By.css('.label'));
+        });
+
+        it('should show label', () => {
+            expect(label.nativeElement.textContent).toBe('Label');
+        });
+
+        it('should not show button', () => {
+            button = de.query(By.css('dot-icon-button'));
+            expect(button).toBeNull();
+        });
+
+        it('should copy text to clipboard', () => {
+            const stopPropagation = jasmine.createSpy('stopPropagation');
+
+            label.triggerEventHandler('click', {
+                stopPropagation: stopPropagation
+            });
+
+            expect(dotClipboardUtil.copy).toHaveBeenCalledWith('Text to copy');
+            expect(stopPropagation).toHaveBeenCalledTimes(1);
+        });
+    });
 });
