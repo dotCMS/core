a7fda282f51c8a58afe599a0d665fc015baf6dba
Humberto Morera <humberto.morera@dotcms.com>
2018-12-05 16:18:09 -0600

    progress field helper

--- src/app/portlets/content-types/form/content-types-form.component.spec.ts
+++ src/app/portlets/content-types/form/content-types-form.component.spec.ts
@@ -558,7 +558,6 @@ describe('ContentTypesFormComponent', () => {
         const pageSelector: DebugElement = de.query(By.css('dot-page-selector'));
         expect(pageSelector !== null).toBe(true);
         expect(pageSelector.componentInstance.label).toEqual('Detail Page');
-        expect(pageSelector.componentInstance.hostIdentifier).toEqual('123');
         expect(pageSelector.componentInstance.floatingLabel).toBe(true);
         expect(pageSelector.componentInstance.style).toEqual({ width: '100%' });
     });
---
+++ src/app/view/components/dot-field-helper/dot-field-helper.component.spec.ts
@@ -0,0 +1,64 @@
+import { ComponentFixture } from '@angular/core/testing';
+
+import { DotFieldHelperComponent } from './dot-field-helper.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { OverlayPanel, OverlayPanelModule } from 'primeng/primeng';
+import { DOTTestBed } from '@tests/dot-test-bed';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DotIconButtonModule } from '@components/_common/dot-icon-button/dot-icon-button.module';
+import { DotIconButtonComponent } from '@components/_common/dot-icon-button/dot-icon-button.component';
+
+fdescribe('DotFieldHelperComponent', () => {
+    let component: DotFieldHelperComponent;
+    let fixture: ComponentFixture<DotFieldHelperComponent>;
+    let de: DebugElement;
+
+    beforeEach(() => {
+        DOTTestBed.configureTestingModule({
+            declarations: [DotFieldHelperComponent],
+            imports: [BrowserAnimationsModule, DotIconButtonModule, OverlayPanelModule]
+        });
+
+        fixture = DOTTestBed.createComponent(DotFieldHelperComponent);
+        component = fixture.componentInstance;
+        de = fixture.debugElement;
+        component.message = 'Hello World';
+        fixture.detectChanges();
+    });
+
+    it('should display the overlay panel on click', () => {
+        const iconButton = de.query(By.css('dot-icon-button')).nativeElement;
+        const overlayPanel: OverlayPanel = de.query(By.css('p-overlayPanel')).componentInstance;
+
+        iconButton.dispatchEvent(new MouseEvent('click'));
+
+        expect(overlayPanel.visible).toEqual(true);
+
+    });
+
+    it('should hide the overlay panel on click', () => {
+        const iconButton = de.query(By.css('dot-icon-button')).nativeElement;
+        const overlayPanel: OverlayPanel = de.query(By.css('p-overlayPanel')).componentInstance;
+
+        iconButton.dispatchEvent(new MouseEvent('click'));
+        iconButton.dispatchEvent(new MouseEvent('click'));
+
+        expect(overlayPanel.visible).toEqual(false);
+    });
+
+    it('should have correct attributes  on button', () => {
+        const iconButton: DotIconButtonComponent = de.query(By.css('dot-icon-button'))
+            .componentInstance;
+
+        expect(iconButton.icon).toEqual('help_outline');
+    });
+
+    it('should have correct attributes on Overlay Panel', () => {
+        const overlayPanel: OverlayPanel = de.query(By.css('p-overlayPanel')).componentInstance;
+
+        expect(overlayPanel.style).toEqual({ width: '350px' });
+        expect(overlayPanel.appendTo).toEqual('body');
+        expect(overlayPanel.dismissable).toEqual(true);
+    });
+});
