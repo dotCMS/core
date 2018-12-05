664f391556ab36a436ca6e6501d236feb487e72d
Jonathan <jonathan.sanchez@dotcms.com>
2018-12-05 11:00:14 -0600

    Test issue 12943 containers as file (#15558)
    * #12943 adding second draft for the container as a file
    
    * #12943 codacy feedback
    
    * #12943 adding changes when a file is touch
    
    * #progress for the container, still missing deletes of the container.vtl reference and the folder container too, also needs to switch some queries to es
    
    * #12943 adding changes for #15568
    
    * #12943 when deleted a content type, everything is clear up #15569
    
    * #12943 fix to avoid serialization issues
    
    * #12943 fixes and refactoring for multitree

--- dotCMS/src/main/java/com/dotcms/system/event/local/business/LocalSystemEventSubscribersInitializer.java
+++ dotCMS/src/main/java/com/dotcms/system/event/local/business/LocalSystemEventSubscribersInitializer.java
@@ -2,7 +2,17 @@ package com.dotcms.system.event.local.business;
 
 import com.dotcms.config.DotInitializer;
 import com.dotcms.services.VanityUrlServices;
+import com.dotmarketing.beans.Host;
 import com.dotmarketing.business.APILocator;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotSecurityException;
+import com.dotmarketing.portlets.folders.business.ApplicationContainerFolderListener;
+import com.dotmarketing.portlets.folders.model.Folder;
+import com.dotmarketing.util.Constants;
+import com.dotmarketing.util.Logger;
+import com.liferay.portal.model.User;
+
+import java.util.List;
 
 /**
  * Initializer class that allow us to register Local System Events subscribers
@@ -13,7 +23,30 @@ public class LocalSystemEventSubscribersInitializer implements DotInitializer {
 
     @Override
     public void init() {
+
         APILocator.getLocalSystemEventsAPI().subscribe(VanityUrlServices.getInstance());
+        this.initApplicationContainerFolderListener();
+    }
+
+    public void initApplicationContainerFolderListener() {
+
+        try {
+
+            final User user  = APILocator.systemUser();
+            final List<Host> hosts = APILocator.getHostAPI().findAll(user, false);
+            for (final Host host : hosts) {
+
+                final Folder appContainerFolder = APILocator.getFolderAPI().findFolderByPath(Constants.CONTAINER_FOLDER_PATH,
+                        host, user, false);
+
+                APILocator.getFolderAPI().subscribeFolderListener(appContainerFolder, new ApplicationContainerFolderListener(),
+                        childName -> childName.endsWith(Constants.VELOCITY_FILE_EXTENSION));
+            }
+        } catch (DotDataException | DotSecurityException e) {
+
+            Logger.error(this, "Could not init the: " +
+                    ApplicationContainerFolderListener.class.getName() + ", msg: " + e.getMessage(), e);
+        }
     }
 
 }
