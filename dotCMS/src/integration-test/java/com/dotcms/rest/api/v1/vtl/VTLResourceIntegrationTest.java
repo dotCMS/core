5fbc5b455a5a922636490953d40e1672a1af580f
Daniel Silva <daniel.silva@dotcms.com>
2018-12-06 15:12:24 -0600

    #15433 Refactor tests

--- dotCMS/src/integration-test/java/com/dotcms/rest/api/v1/vtl/VTLResourceIntegrationTest.java
+++ dotCMS/src/integration-test/java/com/dotcms/rest/api/v1/vtl/VTLResourceIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.dotcms.rest.api.v1.vtl;
 
 import com.dotcms.datagen.FileAssetDataGen;
+import com.dotcms.repackage.com.fasterxml.jackson.core.JsonProcessingException;
 import com.dotcms.repackage.com.fasterxml.jackson.databind.ObjectMapper;
 import com.dotcms.repackage.javax.ws.rs.core.MultivaluedHashMap;
 import com.dotcms.repackage.javax.ws.rs.core.MultivaluedMap;
@@ -23,6 +24,7 @@ import com.liferay.util.FileUtil;
 import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.jetbrains.annotations.NotNull;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -45,8 +47,10 @@ import static org.mockito.Mockito.*;
 public class VTLResourceIntegrationTest {
 
     private final static String VALID_GET_VTL_DOTJSON_OUTPUT = "com/dotcms/rest/api/v1/vtl/valid_get_dotjson_response.vtl";
+    private final static String VALID_GET_VTL_RAW_OUTPUT = "com/dotcms/rest/api/v1/vtl/valid_get_raw_response.vtl";
     private final static String INVALID_GET_VTL = "com/dotcms/rest/api/v1/vtl/invalid_get.vtl";
     private final static String ONE_EMPLOYEE_JSON_RESPONSE = "com/dotcms/rest/api/v1/vtl/one_employee_json_response.json";
+    private final static String ONE_EMPLOYEE_XML_RESPONSE = "com/dotcms/rest/api/v1/vtl/one_employee_xml_response.xml";
 
     private static final String KNOWN_EMPLOYEE_ID = "37f93fcb-6124-46af-83b4-9ece6c1c5380";
 
@@ -75,13 +79,13 @@ public class VTLResourceIntegrationTest {
                         .setExpectedJSON(FileUtil.read(new File(ConfigTestHelper.getUrlToTestResource(ONE_EMPLOYEE_JSON_RESPONSE).toURI())))
                         .setExpectedOutput(null)
                         .build(),
-//                new VTLResourceTestCase.Builder().setVtlFile(VALID_GET_VTL_RAW_OUTPUT)
-//                        .setFolderName(folderName)
-//                        .setQueryParameters(queryParameters)
-//                        .setPathParameter("aaee9776-8fb7-4501-8048-844912a20405")
-//                        .setExpectedJSON(null)
-//                        .setExpectedOutput(VALID_GET_VTL_RAW_OUTPUT)
-//                        .build(),
+                new VTLResourceTestCase.Builder().setVtlFile(new File(ConfigTestHelper.getUrlToTestResource(VALID_GET_VTL_RAW_OUTPUT).toURI()))
+                        .setFolderName(folderName)
+                        .setQueryParameters(queryParameters)
+                        .setPathParameter(KNOWN_EMPLOYEE_ID)
+                        .setExpectedJSON(FileUtil.read(new File(ConfigTestHelper.getUrlToTestResource(ONE_EMPLOYEE_XML_RESPONSE).toURI())))
+                        .setExpectedOutput(null)
+                        .build(),
                 new VTLResourceTestCase.Builder().setVtlFile(new File(ConfigTestHelper.getUrlToTestResource(INVALID_GET_VTL).toURI()))
                         .setFolderName(folderName)
                         .setQueryParameters(queryParameters)
@@ -108,88 +112,102 @@ public class VTLResourceIntegrationTest {
 
         final Host demoSite = APILocator.getHostAPI().findDefaultHost(APILocator.systemUser(), false);
 
-        final Folder vtlFolder = APILocator.getFolderAPI()
-                .createFolders(VTL_PATH + "/" + testCase.getFolderName(),
-                        demoSite,
-                        APILocator.systemUser(), false);
+        final Folder vtlFolder = createVTLFolder(testCase.getFolderName(), demoSite);
 
         try {
-            final File getVTLFile = new File(Files.createTempDir(), "get.vtl");
-            FileUtil.copyFile(testCase.getVtlFile(), getVTLFile);
+            final Response response = getResponseFromVTLResource(testCase, vtlFolder);
 
-            final FileAssetDataGen fileAssetDataGen = new FileAssetDataGen(vtlFolder, getVTLFile);
-            final Contentlet getVTLFileAsset = fileAssetDataGen.nextPersisted();
-            getVTLFileAsset.setIndexPolicy(IndexPolicy.FORCE);
-            getVTLFileAsset.setIndexPolicyDependencies(IndexPolicy.FORCE);
-            getVTLFileAsset.setBoolProperty(Contentlet.IS_TEST_MODE, true);
-            APILocator.getContentletAPI().publish(getVTLFileAsset, systemUser, false);
-
-            final HttpServletRequest request = mock(HttpServletRequest.class);
-            when(request.getSession(false)).thenReturn(mock(HttpSession.class));
-            when(request.getSession()).thenReturn(mock(HttpSession.class));
-
-            final HttpServletResponse servletResponse = mock(HttpServletResponse.class);
-
-            final UriInfo uriInfo = mock(UriInfo.class);
-            when(uriInfo.getQueryParameters()).thenReturn(testCase.getQueryParameters());
-
-            final User requestingUser = APILocator.getUserAPI().loadUserById(testCase.getUserId(),
-                    APILocator.systemUser(), false);
-
-            final WebResource webResource = spy(WebResource.class);
-            doReturn(requestingUser).when(webResource).getCurrentUser(request,
-                    WebResource.buildParamsMap(testCase.getPathParameter()), false);
-
-            final VTLResource resource = new VTLResource(APILocator.getHostAPI(), APILocator.getIdentifierAPI(),
-                    APILocator.getContentletAPI(), webResource);
-            final Response response = resource.get(request, servletResponse, uriInfo, testCase.getFolderName(),
-                    testCase.getPathParameter(), null);
-
-            String expectedOutput;
-            String getOutput;
-
-            if(testCase.getExpectedException()>0) {
-                expectedOutput = Integer.toString(testCase.getExpectedException());
-                getOutput = Integer.toString(response.getStatus());
-            } else if(UtilMethods.isSet(testCase.getExpectedJSON())) {
-                expectedOutput = testCase.getExpectedJSON();
-                final ObjectMapper objectMapper = new ObjectMapper();
-                getOutput = objectMapper.writeValueAsString(response.getEntity());
-            } else {
-                expectedOutput = testCase.getExpectedOutput();
-                getOutput      = Map.class.isInstance(response.getEntity())?
-                    (String) Map.class.cast(response.getEntity()).get("message"):
-                    response.getEntity().toString();
-            }
-
-            assertEquals(expectedOutput, getOutput);
+            String expectedOutput = expectedOutput(testCase);
+            String actualOutput = actualOutput(testCase, response);
+            assertEquals(expectedOutput, actualOutput);
         } finally {
             APILocator.getFolderAPI().delete(vtlFolder, APILocator.systemUser(), false);
         }
-
-
     }
 
-//    private static final String VALID_GET_VTL_DOTJSON_OUTPUT = "## GETS NEWS BY ID, LOADS A FEW FIELD\n" +
-//            "#set($news = $dotcontent.find($pathParam))\n" +
-//            "\n" +
-//            "##  QUERY PARAMS\n" +
-//            "\n" +
-//            "$dotJSON.put(\"queryParam\", $queryParams)\n" +
-//            "\n" +
-//            "## NEW ARRAY\n" +
-//            "#set ($fields = [])   \n" +
-//            "$fields.add(\"title\")\n" +
-//            "$fields.add(\"urlTitle\")\n" +
-//            "$fields.add(\"story\")\n" +
-//            "\n" +
-//            "#foreach($field in $fields)\n" +
-//            "    #set($item = {})\n" +
-//            "    $item.put($field,${news.get(\"$field\")})\n" +
-//            "    $dotJSON.put(\"$field\",$item)\n" +
-//            "#end\n" +
-//            "\n" +
-//            "$dotJSON.put(\"cache\", 10)\n";
+    private Response getResponseFromVTLResource(final VTLResourceTestCase testCase, final Folder vtlFolder) throws IOException, DotSecurityException, DotDataException {
+        createVTLFile(testCase.getVtlFile(), vtlFolder);
 
-    private static final String VALID_GET_VTL_RAW_OUTPUT = "helloworld";
+        final HttpServletRequest request = getMockedRequest();
+
+        final HttpServletResponse servletResponse = mock(HttpServletResponse.class);
+
+        final UriInfo uriInfo = mock(UriInfo.class);
+        when(uriInfo.getQueryParameters()).thenReturn(testCase.getQueryParameters());
+
+        final WebResource webResource = getSpiedWebResource(testCase, request);
+
+        final VTLResource resource = new VTLResource(APILocator.getHostAPI(), APILocator.getIdentifierAPI(),
+                APILocator.getContentletAPI(), webResource);
+        return resource.get(request, servletResponse, uriInfo, testCase.getFolderName(),
+                testCase.getPathParameter(), null);
+    }
+
+    private String expectedOutput(final VTLResourceTestCase testCase) {
+        String expectedOutput;
+
+        if(testCase.getExpectedException()>0) {
+            expectedOutput = Integer.toString(testCase.getExpectedException());
+        } else if(UtilMethods.isSet(testCase.getExpectedJSON())) {
+            expectedOutput = testCase.getExpectedJSON();
+        } else {
+            expectedOutput = testCase.getExpectedOutput();
+        }
+
+        return expectedOutput;
+    }
+
+    private String actualOutput(final VTLResourceTestCase testCase, final Response response) throws JsonProcessingException {
+        final String output;
+        if(testCase.getExpectedException()>0) {
+            output = Integer.toString(response.getStatus());
+        } else if(UtilMethods.isSet(testCase.getExpectedJSON())) {
+            final ObjectMapper objectMapper = new ObjectMapper();
+            output = objectMapper.writeValueAsString(response.getEntity());
+        } else {
+            output      = Map.class.isInstance(response.getEntity())?
+                    (String) Map.class.cast(response.getEntity()).get("message"):
+                    response.getEntity().toString();
+        }
+
+        return  output;
+    }
+
+    private WebResource getSpiedWebResource(VTLResourceTestCase testCase, HttpServletRequest request) throws DotDataException, DotSecurityException {
+        final User requestingUser = APILocator.getUserAPI().loadUserById(testCase.getUserId(),
+                APILocator.systemUser(), false);
+
+        final WebResource webResource = spy(WebResource.class);
+        doReturn(requestingUser).when(webResource).getCurrentUser(request,
+                WebResource.buildParamsMap(testCase.getPathParameter()), false);
+        return webResource;
+    }
+
+    @NotNull
+    private HttpServletRequest getMockedRequest() {
+        final HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getSession(false)).thenReturn(mock(HttpSession.class));
+        when(request.getSession()).thenReturn(mock(HttpSession.class));
+        return request;
+    }
+
+    private void createVTLFile(final File vtlFile, final Folder vtlFolder) throws IOException, DotSecurityException, DotDataException {
+        final File getVTLFile = new File(Files.createTempDir(), "get.vtl");
+        FileUtil.copyFile(vtlFile, getVTLFile);
+
+        final FileAssetDataGen fileAssetDataGen = new FileAssetDataGen(vtlFolder, getVTLFile);
+        final Contentlet getVTLFileAsset = fileAssetDataGen.nextPersisted();
+        getVTLFileAsset.setIndexPolicy(IndexPolicy.FORCE);
+        getVTLFileAsset.setIndexPolicyDependencies(IndexPolicy.FORCE);
+        getVTLFileAsset.setBoolProperty(Contentlet.IS_TEST_MODE, true);
+        APILocator.getContentletAPI().publish(getVTLFileAsset, systemUser, false);
+    }
+
+    private Folder createVTLFolder(final String folderName, final Host demoSite) throws DotSecurityException, DotDataException {
+        return APILocator.getFolderAPI()
+                .createFolders(VTL_PATH + "/" + folderName,
+                        demoSite,
+                        APILocator.systemUser(), false);
+    }
+
 }
