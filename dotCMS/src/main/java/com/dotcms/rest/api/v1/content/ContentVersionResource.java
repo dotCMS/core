6953007e071b22f826402e00625e62e6a78e21a4
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2018-12-20 18:25:05 -0600

    #14659  adjustments per given feedback

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/content/ContentVersionResource.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/content/ContentVersionResource.java
@@ -31,15 +31,23 @@ import com.dotmarketing.portlets.contentlet.transform.ContentletToMapTransformer
 import com.dotmarketing.portlets.languagesmanager.business.LanguageAPI;
 import com.dotmarketing.portlets.languagesmanager.model.Language;
 import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.PageMode;
+import com.dotmarketing.util.UtilMethods;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableMap;
 import com.liferay.portal.model.User;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
 
 @Path("/v1/content/versions")
 public class ContentVersionResource {
@@ -47,7 +55,7 @@ public class ContentVersionResource {
     private static final String FIND_BY_ID_ERROR_MESSAGE_KEY = "Unable-to-find-contentlet-by-id";
     private static final String FIND_BY_INODE_ERROR_MESSAGE_KEY = "Unable-to-find-contentlet-by-inode";
     private static final String DATATYPE_MISSMATCH_ERROR_MESSAGE_KEY = "Data-Type-Missmatch";
-    private static final String DATATYPE_MISSMATCH_2_ERROR_MESSAGE_KEY = "Data-Type-Missmatch2";
+    private static final String BAD_REQUEST_ERROR_MESSAGE_KEY = "Bad-Request";
 
     private static final int MIN = 20;
     private static final int MAX = 100;
@@ -73,53 +81,57 @@ public class ContentVersionResource {
     @NoCache
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
-    public Response findVersions(@Context final HttpServletRequest request,
+    public Response findVersions(@Context final HttpServletRequest request, @QueryParam("inodes") final String inodes,
             @QueryParam("identifier") final String identifier, @QueryParam("groupByLang")final String groupByLangParam, @QueryParam("limit") final int limit)
             throws DotDataException, DotStateException, DotSecurityException {
 
         final boolean groupByLang = "1".equals(groupByLangParam) || BooleanUtils.toBoolean(groupByLangParam);
         final int showing = limit > MAX ? MAX : limit < MIN ? MIN : limit;
         final InitDataObject auth = webResource.init(true, request, true);
-
+        final boolean respectFrontendRoles = PageMode.get(request).respectAnonPerms;
         final User user = auth.getUser();
         try {
 
-            Logger.debug(this,
-                    "Getting versions for identifier: " + identifier + " grouping by language: '" + BooleanUtils.toStringYesNo(groupByLang)+ "' and limit: "+limit);
-
-            final ShortyId shorty = APILocator
-                    .getShortyAPI().getShorty(identifier)
-                    .orElseThrow(() -> new DoesNotExistException(
-                            getFormattedMessage(user.getLocale(), FIND_BY_ID_ERROR_MESSAGE_KEY,
-                                    identifier)));
-            final Identifier identifierObj =
-                    (shorty.type == ShortType.IDENTIFIER) ? APILocator.getIdentifierAPI()
-                            .find(shorty.longId)
-                            : APILocator.getIdentifierAPI().findFromInode(shorty.longId);
+           final Identifier identifierObj = getIdentifier(identifier, user);
 
            ResponseEntityView responseEntityView = null;
-           if(groupByLang){
-               final Map<String, List<Map<String, Object>>> versionsByLang = new HashMap<>();
-               final Map<Long, List<Contentlet>> contentByLangMap = contentletAPI
-                       .findAllVersions(identifierObj, user, false).stream()
-                       .limit(showing)
-                       .collect(Collectors.groupingBy(Contentlet::getLanguageId));
-               contentByLangMap.forEach((langId, contentlets) -> {
-                   final Language lang = languageAPI.getLanguage(langId);
-                   final List<Map<String, Object>> asMaps = contentlets.stream()
-                           .map(this::contentletToMap).collect(Collectors.toList());
-                   versionsByLang.put(lang.toString(), asMaps);
-               });
+           if(null != identifier){
 
-               responseEntityView = new ResponseEntityView(ImmutableMap.of("versions", versionsByLang));
+               Logger.debug(this,
+                       "Getting versions for identifier: " + identifier + " grouping by language: '" + BooleanUtils.toStringYesNo(groupByLang)+ "' and limit: "+limit);
 
+               if(groupByLang){
+                   final Map<String, List<Map<String, Object>>> versionsByLang = mapVersionsByLang(contentletAPI
+                           .findAllVersions(identifierObj, user, respectFrontendRoles), showing);
+                   responseEntityView = new ResponseEntityView(ImmutableMap.of("versions", versionsByLang));
+               } else {
+                   final List<Map<String, Object>> versions = mapVersions(contentletAPI
+                            .findAllVersions(identifierObj, user, respectFrontendRoles), showing);
+
+                   responseEntityView = new ResponseEntityView(ImmutableMap.of("versions", versions));
+               }
            } else {
-               final List<Map<String, Object>> versions = contentletAPI
-                       .findAllVersions(identifierObj, user, false).stream()
-                       .limit(showing)
-                       .map(this::contentletToMap).collect(Collectors.toList());
+               final Set<String> inodesSet =
+                       UtilMethods.isSet(inodes) ? Arrays.stream(inodes.split(",")).map(String::trim)
+                               .limit(showing).collect(Collectors.toSet()) : null;
 
-               responseEntityView = new ResponseEntityView(ImmutableMap.of("versions", versions));
+               if(null != inodesSet) {
+
+                   Logger.debug(this,
+                           "Getting versions for inodes: " + StringUtils.join(inodesSet, ',') + " grouping by language: '" + BooleanUtils.toStringYesNo(groupByLang)+ "' and limit: "+limit);
+
+                   if(groupByLang){
+                       final Map<String, List<Map<String, Object>>> versionsByLang = mapVersionsByLang(findByInodes(user, inodesSet, respectFrontendRoles), showing);
+                       responseEntityView = new ResponseEntityView(ImmutableMap.of("versions", versionsByLang));
+                   } else {
+                       final Map<String,Map<String,Object>> versions = mapVersionsByInode(findByInodes(user, inodesSet, respectFrontendRoles), showing);
+                       responseEntityView = new ResponseEntityView(ImmutableMap.of("versions", versions));
+                   }
+
+
+               } else {
+                    throw new BadRequestException(getFormattedMessage(user.getLocale(), BAD_REQUEST_ERROR_MESSAGE_KEY));
+               }
            }
 
             return Response.ok(responseEntityView).build();
@@ -133,6 +145,69 @@ public class ContentVersionResource {
 
     }
 
+    private Identifier getIdentifier(final String identifier, final User user ) throws DotDataException{
+        if(!UtilMethods.isSet(identifier)){
+          return null;
+        }
+        final ShortyId shorty = APILocator
+                .getShortyAPI().getShorty(identifier)
+                .orElseThrow(() -> new DoesNotExistException(
+                        getFormattedMessage(user.getLocale(), FIND_BY_ID_ERROR_MESSAGE_KEY,
+                                identifier)));
+        return
+                (shorty.type == ShortType.IDENTIFIER) ? APILocator.getIdentifierAPI()
+                        .find(shorty.longId)
+                        : APILocator.getIdentifierAPI().findFromInode(shorty.longId);
+    }
+
+    private List<Contentlet> findByInodes(final User user, final Set<String> inodes, boolean respectFrontendRoles)
+            throws DotStateException {
+
+        final Set<String> notFound = new HashSet<>();
+        final Set<ShortyId> mappedShorties = inodes.stream().map(inode -> {
+            final Optional<ShortyId> shortyOptional = APILocator.getShortyAPI().getShorty(inode);
+            return shortyOptional.orElseGet(() -> {
+                notFound.add(inode);
+                return null;
+            });
+        }).collect(Collectors.toSet());
+
+        if (!notFound.isEmpty()) {
+            throw new DoesNotExistException(
+                    getFormattedMessage(user.getLocale(), FIND_BY_INODE_ERROR_MESSAGE_KEY,
+                            StringUtils.join(notFound, ',')));
+        }
+
+        return mappedShorties.stream().map(shortyId -> {
+            try {
+                return APILocator.getContentletAPI().find(shortyId.longId, user, respectFrontendRoles);
+            } catch (DotDataException | DotSecurityException e) {
+                return null;
+            }
+        }).filter(Objects::nonNull).collect(Collectors.toList());
+    }
+
+    private List<Map<String, Object>> mapVersions(final List<Contentlet> input, final int limit){
+        return input.stream().limit(limit).map(this::contentletToMap).collect(Collectors.toList());
+    }
+
+    private Map<String,Map<String,Object>> mapVersionsByInode(final List<Contentlet> input, final int limit){
+        return input.stream().limit(limit).collect(Collectors.toMap(Contentlet::getInode,this::contentletToMap));
+    }
+
+    private Map<String, List<Map<String, Object>>> mapVersionsByLang(final List<Contentlet> input, final int limit){
+        final Map<String, List<Map<String, Object>>> versionsByLang = new HashMap<>();
+        final Map<Long, List<Contentlet>> contentByLangMap = input.stream().limit(limit).collect(Collectors.groupingBy(Contentlet::getLanguageId));
+        contentByLangMap.forEach((langId, contentlets) -> {
+            final Language lang = languageAPI.getLanguage(langId);
+            final List<Map<String, Object>> asMaps = contentlets.stream()
+                    .map(this::contentletToMap).collect(Collectors.toList());
+            versionsByLang.put(lang.toString(), asMaps);
+        });
+        return versionsByLang;
+    }
+
+
     @GET
     @JSONP
     @NoCache
@@ -141,6 +216,7 @@ public class ContentVersionResource {
     public Response findByInode(@Context final HttpServletRequest request,
             @PathParam("inode") final String inode)
             throws DotStateException {
+        final boolean respectFrontendRoles = PageMode.get(request).respectAnonPerms;
         final InitDataObject auth = webResource.init(true, request, true);
         final User user = auth.getUser();
         try {
@@ -156,7 +232,7 @@ public class ContentVersionResource {
                         getFormattedMessage(user.getLocale(), DATATYPE_MISSMATCH_ERROR_MESSAGE_KEY));
             }
 
-            final Contentlet contentlet = APILocator.getContentletAPI().find(inode, user, true);
+            final Contentlet contentlet = APILocator.getContentletAPI().find(inode, user, respectFrontendRoles);
             if (null == contentlet) {
                 throw new DoesNotExistException(
                         getFormattedMessage(user.getLocale(), FIND_BY_INODE_ERROR_MESSAGE_KEY,
@@ -173,59 +249,6 @@ public class ContentVersionResource {
             return ResponseUtil.mapExceptionResponse(ex);
         }
 
-    }
-
-    @GET
-    @JSONP
-    @NoCache
-    @Path("/diff/{inode1}/{inode2}")
-    @Produces({MediaType.APPLICATION_JSON, "application/javascript"})
-    public Response diff(@Context final HttpServletRequest request,
-            @PathParam("inode1") final String inode1, @PathParam("inode2") final String inode2)
-            throws DotStateException {
-
-        final InitDataObject auth = webResource.init(true, request, true);
-        final User user = auth.getUser();
-        try {
-
-            Logger.debug(this,
-                    "Building diff between version '" + inode1 + "' and '" + inode2 + "'"  );
-
-            final ShortyId shorty1 = APILocator
-                    .getShortyAPI().getShorty(inode1)
-                    .orElseThrow(() -> new DoesNotExistException(
-                            getFormattedMessage(user.getLocale(), FIND_BY_ID_ERROR_MESSAGE_KEY,
-                                    inode1)));
-            final ShortyId shorty2 = APILocator
-                    .getShortyAPI().getShorty(inode2)
-                    .orElseThrow(() -> new DoesNotExistException(
-                            getFormattedMessage(user.getLocale(), FIND_BY_ID_ERROR_MESSAGE_KEY,
-                                    inode2)));
-            if (shorty1.type != ShortType.INODE
-                    || shorty2.type != ShortType.INODE) {
-                throw new BadRequestException(
-                        getFormattedMessage(user.getLocale(), DATATYPE_MISSMATCH_2_ERROR_MESSAGE_KEY));
-            }
-
-            final Contentlet contentlet1 = APILocator
-                    .getContentletAPI().find(shorty1.longId, user, false);
-
-            final Contentlet contentlet2 = APILocator
-                    .getContentletAPI().find(shorty2.longId, user, false);
-
-            final Response.ResponseBuilder responseBuilder = Response.ok(new ResponseEntityView(
-                    ImmutableMap.of(
-                            shorty1.longId, contentletToMap(contentlet1),
-                            shorty2.longId, contentletToMap(contentlet2)
-                    ))
-            );
-            return responseBuilder.build();
-        } catch (Exception ex) {
-            Logger.error(this.getClass(),
-                    "Exception on method diff with exception message: " + ex
-                            .getMessage(), ex);
-            return ResponseUtil.mapExceptionResponse(ex);
-        }
     }
 
     private Map<String, Object> contentletToMap( final Contentlet con) {
