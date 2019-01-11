cd756bfca9580d42d1fcd8596e6eb70649d5c126
Alfredo Li <alfredo@dotcms.com>
2019-01-11 10:13:48 -0600

    fix

--- src/app/portlets/dot-edit-page/content/services/dot-edit-content-html/dot-edit-content-html.service.spec.ts
+++ src/app/portlets/dot-edit-page/content/services/dot-edit-content-html/dot-edit-content-html.service.spec.ts
@@ -278,18 +278,52 @@ describe('DotEditContentHtmlService', () => {
     });
 
     it('should render relocated contentlet', () => {
-        spyOn(this.dotEditContentHtmlService, 'renderRelocatedContentlet');
+        const dotContainerContentletService = this.injector.get(DotContainerContentletService);
+        spyOn(dotContainerContentletService, 'getContentletToContainer').and.callThrough();
+        spyOn(this.dotEditContentHtmlService, 'renderRelocatedContentlet').and.callThrough();
+
+        const dataObj = {
+            container: {
+                identifier: '123',
+                uuid: '456'
+            },
+            contentlet: {
+                identifier: '456',
+                inode: '456'
+            }
+        };
 
         this.dotEditContentHtmlService.contentletEvents$.next({
             name: 'relocate',
-            data: {
-                identifier: '456'
-            }
+            data: dataObj
         });
 
-        expect(this.dotEditContentHtmlService.renderRelocatedContentlet).toHaveBeenCalledWith({
-            identifier: '456'
+        expect(this.dotEditContentHtmlService.renderRelocatedContentlet).toHaveBeenCalledWith(dataObj);
+        expect(dotContainerContentletService.getContentletToContainer).toHaveBeenCalledWith(dataObj.container, dataObj.contentlet);
+    });
+
+    it('should not render relocated contentlet', () => {
+        this.dotEditContentHtmlService.remoteRendered = true;
+        spyOn(this.dotEditContentHtmlService, 'renderRelocatedContentlet').and.callThrough();
+
+        const dataObj = {
+            container: {
+                identifier: '123',
+                uuid: '456'
+            },
+            contentlet: {
+                identifier: '456',
+                inode: '456'
+            },
+            remoteRendered: true
+        };
+
+        this.dotEditContentHtmlService.contentletEvents$.next({
+            name: 'relocate',
+            data: dataObj
         });
+
+        expect(this.dotEditContentHtmlService.renderRelocatedContentlet).not.toHaveBeenCalled();
     });
 
     it('should render added contentlet', () => {
