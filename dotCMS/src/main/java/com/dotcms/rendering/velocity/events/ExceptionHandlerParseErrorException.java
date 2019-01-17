8f2c8b9b46296ff2f4738e3e2abd6b5a4bd0ed40
Jonathan <jonathan.sanchez@dotcms.com>
2019-01-17 14:26:53 -0600

    Issue 15640 velocity error msg handler 2 (#15837)
    * #15640 error handling for editmode
    
    * #15640 avoiding to show an error message when the socket has already reported it

--- dotCMS/src/main/java/com/dotcms/rendering/velocity/events/ExceptionHandlerParseErrorException.java
+++ dotCMS/src/main/java/com/dotcms/rendering/velocity/events/ExceptionHandlerParseErrorException.java
@@ -4,15 +4,18 @@ import com.dotcms.api.system.event.message.MessageSeverity;
 import com.dotcms.api.system.event.message.MessageType;
 import com.dotcms.api.system.event.message.SystemMessageEventUtil;
 import com.dotcms.api.system.event.message.builder.SystemMessageBuilder;
+import com.dotcms.api.web.HttpServletRequestThreadLocal;
 import com.dotcms.rendering.velocity.servlet.VelocityModeHandler;
 import com.dotmarketing.util.DateUtil;
 import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.PageMode;
 import com.dotmarketing.util.UtilMethods;
 import com.liferay.portal.auth.PrincipalThreadLocal;
 import com.liferay.util.Html;
 import org.apache.commons.lang.WordUtils;
 import org.apache.velocity.exception.ParseErrorException;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 
 /**
@@ -26,7 +29,7 @@ public class ExceptionHandlerParseErrorException implements ExceptionHandler<Par
     @Override
     public void handle(final ParseErrorException e) {
 
-        if (isEditMode(e)) {
+        if (isPreviewOrEditMode(e)) {
             try {
 
                 final String userId = PrincipalThreadLocal.getName();
@@ -64,23 +67,37 @@ public class ExceptionHandlerParseErrorException implements ExceptionHandler<Par
             } catch (Exception ex) {
                 Logger.error(VelocityModeHandler.class, "Parsing error on: " + e.getTemplateName() + ", msg: " + ex.getMessage(), ex);
             }
+
+            throw new PreviewEditParseErrorException(e);
         }
 
         throw e;
     }
 
-    boolean isEditMode(final Exception e) {
+    boolean isPreviewOrEditMode(final Exception e) {
 
-        boolean ret = false;
+        boolean isPreviewOrEdit          = false;
+        final HttpServletRequest request = HttpServletRequestThreadLocal.INSTANCE.getRequest();
 
-        for (final StackTraceElement ste : e.getStackTrace()) {
-            if (ste.getClassName().indexOf("EditMode") > -1 || ste.getMethodName().indexOf("EditMode") > -1) {
+        if (null != request) {
 
-                ret = true;
-                break;
+            final PageMode pageMode = PageMode.get(request);
+            isPreviewOrEdit = (PageMode.EDIT_MODE == pageMode || PageMode.PREVIEW_MODE == pageMode);
+        } else {
+
+
+            for (final StackTraceElement stackTraceElement : e.getStackTrace()) {
+                if (stackTraceElement.getClassName().indexOf("EditMode") > -1 ||
+                        stackTraceElement.getMethodName().indexOf("EditMode") > -1 ||
+                        stackTraceElement.getClassName().indexOf("PreviewMode") > -1 ||
+                        stackTraceElement.getMethodName().indexOf("PreviewMode") > -1) {
+
+                    isPreviewOrEdit = true;
+                    break;
+                }
             }
         }
 
-        return ret;
+        return isPreviewOrEdit;
     }
 }
