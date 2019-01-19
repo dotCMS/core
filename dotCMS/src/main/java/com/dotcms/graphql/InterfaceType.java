c5546a841a487d448c98d62de534daebd0ccbf17
Daniel Silva <daniel.silva@dotcms.com>
2019-01-19 01:19:00 -0600

    #15782 Add more interface types

--- dotCMS/src/main/java/com/dotcms/graphql/InterfaceType.java
+++ dotCMS/src/main/java/com/dotcms/graphql/InterfaceType.java
@@ -5,6 +5,7 @@ import com.dotcms.graphql.resolver.ContentResolver;
 import com.dotcms.graphql.util.TypeUtil;
 import com.dotmarketing.util.Logger;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -44,6 +45,12 @@ import static com.dotcms.contenttype.model.type.FileAssetContentType.FILEASSET_S
 import static com.dotcms.contenttype.model.type.FileAssetContentType.FILEASSET_SITE_OR_FOLDER_FIELD_VAR;
 import static com.dotcms.contenttype.model.type.FileAssetContentType.FILEASSET_SORT_ORDER_FIELD_VAR;
 import static com.dotcms.contenttype.model.type.FileAssetContentType.FILEASSET_TITLE_FIELD_VAR;
+import static com.dotcms.contenttype.model.type.FormContentType.FORM_EMAIL_FIELD_VAR;
+import static com.dotcms.contenttype.model.type.FormContentType.FORM_HOST_FIELD_VAR;
+import static com.dotcms.contenttype.model.type.FormContentType.FORM_RETURN_PAGE_FIELD_VAR;
+import static com.dotcms.contenttype.model.type.FormContentType.FORM_TITLE_FIELD_VAR;
+import static com.dotcms.contenttype.model.type.KeyValueContentType.KEY_VALUE_KEY_FIELD_VAR;
+import static com.dotcms.contenttype.model.type.KeyValueContentType.KEY_VALUE_VALUE_FIELD_VAR;
 import static com.dotcms.contenttype.model.type.PageContentType.PAGE_CACHE_TTL_FIELD_VAR;
 import static com.dotcms.contenttype.model.type.PageContentType.PAGE_FRIENDLY_NAME_FIELD_VAR;
 import static com.dotcms.contenttype.model.type.PageContentType.PAGE_HOST_FOLDER_FIELD_VAR;
@@ -62,6 +69,16 @@ import static com.dotcms.contenttype.model.type.PersonaContentType.PERSONA_KEY_T
 import static com.dotcms.contenttype.model.type.PersonaContentType.PERSONA_NAME_FIELD_VAR;
 import static com.dotcms.contenttype.model.type.PersonaContentType.PERSONA_OTHER_TAGS_FIELD_VAR;
 import static com.dotcms.contenttype.model.type.PersonaContentType.PERSONA_PHOTO_FIELD_VAR;
+import static com.dotcms.contenttype.model.type.VanityUrlContentType.ACTION_FIELD_VAR;
+import static com.dotcms.contenttype.model.type.VanityUrlContentType.FORWARD_TO_FIELD_VAR;
+import static com.dotcms.contenttype.model.type.VanityUrlContentType.ORDER_FIELD_VAR;
+import static com.dotcms.contenttype.model.type.VanityUrlContentType.SITE_FIELD_VAR;
+import static com.dotcms.contenttype.model.type.VanityUrlContentType.URI_FIELD_VAR;
+import static com.dotcms.contenttype.model.type.WidgetContentType.WIDGET_CODE_FIELD_VAR;
+import static com.dotcms.contenttype.model.type.WidgetContentType.WIDGET_PRE_EXECUTE_FIELD_VAR;
+import static com.dotcms.contenttype.model.type.WidgetContentType.WIDGET_TITLE_FIELD_VAR;
+import static com.dotcms.contenttype.model.type.WidgetContentType.WIDGET_USAGE_FIELD_VAR;
+import static com.dotcms.graphql.util.TypeUtil.createInterfaceType;
 import static graphql.Scalars.GraphQLBoolean;
 import static graphql.Scalars.GraphQLID;
 import static graphql.Scalars.GraphQLInt;
@@ -72,7 +89,11 @@ public enum InterfaceType {
     CONTENT,
     FILEASSET,
     HTMLPAGE,
-    PERSONA;
+    PERSONA,
+    WIDGET,
+    VANITY_URL,
+    KEY_VALUE,
+    FORM;
 
     private static Map<String, GraphQLInterfaceType> interfaceTypes = new HashMap<>();
 
@@ -103,7 +124,7 @@ public enum InterfaceType {
         contentFields.put(URL_MAP, GraphQLString);
         contentFields.put(CATEGORIES, GraphQLString);
 
-        interfaceTypes.put("CONTENT", TypeUtil.createInterfaceType("Content", contentFields, new ContentResolver()));
+        interfaceTypes.put("CONTENT", createInterfaceType("Content", contentFields, new ContentResolver()));
 
         final Map<String, GraphQLOutputType> fileAssetFields = new HashMap<>(contentFields);
         fileAssetFields.put(FILEASSET_FILE_NAME_FIELD_VAR, GraphQLString);
@@ -115,7 +136,7 @@ public enum InterfaceType {
         fileAssetFields.put(FILEASSET_SHOW_ON_MENU_FIELD_VAR, GraphQLBoolean);
         fileAssetFields.put(FILEASSET_SORT_ORDER_FIELD_VAR, GraphQLInt);
 
-        interfaceTypes.put("FILEASSET", TypeUtil.createInterfaceType("Fileasset", fileAssetFields, new ContentResolver()));
+        interfaceTypes.put("FILEASSET", createInterfaceType("Fileasset", fileAssetFields, new ContentResolver()));
 
         final Map<String, GraphQLOutputType> pageAssetFields = new HashMap<>(contentFields);
         pageAssetFields.put(PAGE_URL_FIELD_VAR, GraphQLString);
@@ -131,7 +152,7 @@ public enum InterfaceType {
         pageAssetFields.put(PAGE_SEO_KEYWORDS_FIELD_VAR, GraphQLString);
         pageAssetFields.put(PAGE_PAGE_METADATA_FIELD_VAR, GraphQLString);
 
-        interfaceTypes.put("HTMLPAGE", TypeUtil.createInterfaceType("Htmlpage", pageAssetFields, new ContentResolver()));
+        interfaceTypes.put("HTMLPAGE", createInterfaceType("Htmlpage", pageAssetFields, new ContentResolver()));
 
         final Map<String, GraphQLOutputType> personaFields = new HashMap<>(contentFields);
         personaFields.put(PERSONA_HOST_FOLDER_FIELD_VAR, CustomFieldType.SITE_OR_FOLDER.getType());
@@ -141,8 +162,38 @@ public enum InterfaceType {
         personaFields.put(PERSONA_OTHER_TAGS_FIELD_VAR, GraphQLString);
         personaFields.put(PERSONA_DESCRIPTION_FIELD_VAR, GraphQLString);
 
-        interfaceTypes.put("PERSONA", TypeUtil.createInterfaceType("Persona", personaFields, new ContentResolver()));
+        interfaceTypes.put("PERSONA", createInterfaceType("Persona", personaFields, new ContentResolver()));
 
+        final Map<String, GraphQLOutputType> widgetFields = new HashMap<>(contentFields);
+        widgetFields.put(WIDGET_TITLE_FIELD_VAR, GraphQLString);
+        widgetFields.put(WIDGET_CODE_FIELD_VAR, GraphQLString);
+        widgetFields.put(WIDGET_USAGE_FIELD_VAR, GraphQLString);
+        widgetFields.put(WIDGET_PRE_EXECUTE_FIELD_VAR, GraphQLString);
+
+        interfaceTypes.put("WIDGET", createInterfaceType("Widget", widgetFields, new ContentResolver()));
+
+        final Map<String, GraphQLOutputType> vanityUrlFields = new HashMap<>(contentFields);
+        vanityUrlFields.put(SITE_FIELD_VAR, CustomFieldType.SITE_OR_FOLDER.getType());
+        vanityUrlFields.put(URI_FIELD_VAR, GraphQLString);
+        vanityUrlFields.put(FORWARD_TO_FIELD_VAR, GraphQLString);
+        vanityUrlFields.put(ACTION_FIELD_VAR, GraphQLString);
+        vanityUrlFields.put(ORDER_FIELD_VAR, GraphQLInt);
+
+        interfaceTypes.put("VANITY_URL", createInterfaceType("Vanity_url", vanityUrlFields, new ContentResolver()));
+
+        final Map<String, GraphQLOutputType> keyValueFields = new HashMap<>(contentFields);
+        keyValueFields.put(KEY_VALUE_KEY_FIELD_VAR, GraphQLString);
+        keyValueFields.put(KEY_VALUE_VALUE_FIELD_VAR, GraphQLString);
+
+        interfaceTypes.put("KEY_VALUE", createInterfaceType("Key_value", keyValueFields, new ContentResolver()));
+
+        final Map<String, GraphQLOutputType> formFields = new HashMap<>(contentFields);
+        formFields.put(FORM_TITLE_FIELD_VAR, GraphQLString);
+        formFields.put(FORM_EMAIL_FIELD_VAR, GraphQLString);
+        formFields.put(FORM_RETURN_PAGE_FIELD_VAR, GraphQLString);
+        formFields.put(FORM_HOST_FIELD_VAR, CustomFieldType.SITE_OR_FOLDER.getType());
+
+        interfaceTypes.put("FORM", createInterfaceType("Form", formFields, new ContentResolver()));
     }
 
     public GraphQLInterfaceType getType() {
