cde1015e6c53d2c885b13e09417e953d2a79a313
Humberto Morera <humberto.morera@dotcms.com>
2019-01-23 16:51:12 -0600

    feedback

--- projects/dot-rules/src/lib/push-publish/add-to-bundle-dialog-component.ts
+++ projects/dot-rules/src/lib/push-publish/add-to-bundle-dialog-component.ts
@@ -1,11 +1,18 @@
-import {Component, ChangeDetectionStrategy, Input, Output, EventEmitter, OnChanges} from '@angular/core';
-import {IBundle} from '../services/bundle-service';
+import {
+    Component,
+    ChangeDetectionStrategy,
+    Input,
+    Output,
+    EventEmitter,
+    OnChanges
+} from '@angular/core';
+import { IBundle } from '../services/bundle-service';
 
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  selector: 'cw-add-to-bundle-dialog-component',
-  template: `
-    <p-dialog width="700" header="Add to Bundle" [visible]="!hidden" [modal]="true" [dismissableMask]="true" [closable]="false" appendTo="body" [draggable]="false" >
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'cw-add-to-bundle-dialog-component',
+    template: `
+    <p-dialog width="700" header="Add to Bundle" [visible]="!hidden" [modal]="true" [dismissableMask]="true" [closable]="false" appendTo="body" [focusOnShow]="false" [draggable]="false" >
       <p-message  *ngIf="errorMessage" style="margin-bottom: 16px; display: block;" severity="error" [text]="errorMessage"></p-message>
       <cw-input-dropdown
         flex
@@ -20,37 +27,40 @@ import {IBundle} from '../services/bundle-service';
         ></cw-input-option>
       </cw-input-dropdown>
       <p-footer>
-        <button type="button" pButton (click)="cancel.emit()" label="Cancel" class="ui-button-secondary"></button>
-        <button type="button" pButton (click)="addToBundle.emit(selectedBundle)" label="Add" [disabled]="!selectedBundle"></button>
+          <button type="button" pButton secondary (click)="cancel.emit()" label="Cancel" class="ui-button-secondary" ></button>
+          <button type="button" pButton (click)="addToBundle.emit(selectedBundle)" label="Add" focus [disabled]="!selectedBundle"></button>
       </p-footer>
     </p-dialog>`
 })
 export class AddToBundleDialogComponent implements OnChanges {
-  @Input() hidden = false;
-  @Input() bundleStores: IBundle[];
-  @Input() errorMessage: string = null;
+    @Input() hidden = false;
+    @Input() bundleStores: IBundle[];
+    @Input() errorMessage: string = null;
 
-  @Output() close: EventEmitter<{isCanceled: boolean}> = new EventEmitter(false);
-  @Output() cancel: EventEmitter<boolean> = new EventEmitter(false);
-  @Output() addToBundle: EventEmitter<IBundle> = new EventEmitter(false);
+    @Output() close: EventEmitter<{ isCanceled: boolean }> = new EventEmitter(false);
+    @Output() cancel: EventEmitter<boolean> = new EventEmitter(false);
+    @Output() addToBundle: EventEmitter<IBundle> = new EventEmitter(false);
 
-  public selectedBundle: IBundle = null;
+    public selectedBundle: IBundle = null;
 
-  ngOnChanges(change): void {
-    if (change.bundleStores && change.bundleStores.currentValue) {
-        this.selectedBundle = change.bundleStores.currentValue[0];
+    ngOnChanges(change): void {
+        debugger;
+        if (change.bundleStores && change.bundleStores.currentValue) {
+            this.selectedBundle = change.bundleStores.currentValue[0];
+        }
     }
-  }
 
-  setSelectedBundle(bundleId: string): void {
-    this.selectedBundle = {
-      id: bundleId,
-      name: bundleId
-    };
-    this.bundleStores.forEach((bundle) => {
-      if (bundle.id === bundleId) {
-        this.selectedBundle = bundle;
-      }
-    });
-  }
+    setSelectedBundle(bundleId: string): void {
+        this.selectedBundle = bundleId
+            ? {
+                  id: bundleId,
+                  name: bundleId
+              }
+            : null;
+        this.bundleStores.forEach(bundle => {
+            if (bundle.id === bundleId) {
+                this.selectedBundle = bundle;
+            }
+        });
+    }
 }
