4e3049f56fb67c06d7985d802c029c3c00ea3602
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2019-01-28 15:30:28 -0600

    #15800  Upgrade task should modify all subtypes of File so fileName field  wont be read-only

--- dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task05050FileAssetContentTypeReadOnlyFileName.java
+++ dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task05050FileAssetContentTypeReadOnlyFileName.java
@@ -7,6 +7,7 @@ import com.dotmarketing.exception.DotRuntimeException;
 import com.dotmarketing.startup.StartupTask;
 import com.dotmarketing.util.Logger;
 import java.sql.Connection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -17,14 +18,14 @@ public class Task05050FileAssetContentTypeReadOnlyFileName implements StartupTas
             + " f.inode as field_inode "
             + " from inode i, field f, structure s "
             + " where i.inode = f.inode "
-            + " and s.name = 'File Asset' "
+            + " and s.structuretype = 4 " //all subtypes of File, that includes (Video, FileAsset, Document)
             + " and f.structure_inode = s.inode "
             + " and f.velocity_var_name = 'fileName' ";
 
-    private static final String MySQL_UPDATE_FIELD = "update field set read_only = 0 where inode = '%s' ";
-    private static final String MS_SQL_UPDATE_FIELD = "update field set read_only = 0 where inode = '%s' ";
-    private static final String POSTGRE_SQL_UPDATE_FIELD = "update field set read_only = false where inode = '%s' ";
-    private static final String ORACLE_SQL_UPDATE_FIELD = "update field set read_only = 0 where inode = '%s' ";
+    private static final String MySQL_UPDATE_FIELD = "update field set read_only = 0 where inode in (%s) ";
+    private static final String MS_SQL_UPDATE_FIELD = "update field set read_only = 0 where inode in (%s) ";
+    private static final String POSTGRE_SQL_UPDATE_FIELD = "update field set read_only = false where inode in (%s) ";
+    private static final String ORACLE_SQL_UPDATE_FIELD = "update field set read_only = 0 where inode in (%s) ";
 
     @Override
     public boolean forceRun() {
@@ -43,10 +44,11 @@ public class Task05050FileAssetContentTypeReadOnlyFileName implements StartupTas
                 dotConnect.setSQL(SQL_SELECT_FIELD_INODE_FROM_STRUCTURE);
                 final List<Map<String,Object>> structureData = dotConnect.loadObjectResults();
                 if(structureData.isEmpty()){
-                   throw new IllegalStateException("Failed to locate the field 'fileName' on structure 'File Asset' ");
+                    throw new IllegalStateException("Failed to locate the field 'fileName' on structure 'File Asset' ");
                 }
-                final String fieldInode = String.class.cast(structureData.get(0).get("field_inode"));
-                Logger.debug(getClass(),()->" field  Inode: "+fieldInode );
+
+                final String fieldInodes = inStatement(structureData);
+                Logger.debug(getClass(),()->" field  Inode: "+fieldInodes );
 
                 String updateStatement = null;
 
@@ -70,7 +72,7 @@ public class Task05050FileAssetContentTypeReadOnlyFileName implements StartupTas
                    throw new IllegalStateException("Unable to determine db type ");
                 }
 
-                dotConnect.setSQL(String.format(updateStatement, fieldInode));
+                dotConnect.setSQL(String.format(updateStatement, fieldInodes));
                 dotConnect.loadResult();
 
             }finally{
@@ -82,4 +84,19 @@ public class Task05050FileAssetContentTypeReadOnlyFileName implements StartupTas
             throw new DotDataException(e.getMessage(), e);
         }
     }
+
+
+    private String inStatement(List<Map<String,Object>> inodes){
+        final StringBuilder stringBuilder =  new StringBuilder();
+        Iterator<Map<String,Object>> iterator =inodes.iterator();
+        while (iterator.hasNext()){
+            final Map<String,Object> entry = iterator.next();
+            stringBuilder.append("'").append(entry.get("field_inode").toString()).append("'");
+            if(iterator.hasNext()){
+               stringBuilder.append(",");
+            }
+        }
+        return stringBuilder.toString();
+    }
+
 }
