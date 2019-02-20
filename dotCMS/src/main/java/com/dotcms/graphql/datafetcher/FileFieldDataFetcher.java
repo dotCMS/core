da39f9c780fad48fa380f8a72ebbdf45a7acf936
Daniel Silva <daniel.silva@dotcms.com>
2019-02-20 08:40:51 -0600

    #16071 Validate non null identifier

--- dotCMS/src/main/java/com/dotcms/graphql/datafetcher/FileFieldDataFetcher.java
+++ dotCMS/src/main/java/com/dotcms/graphql/datafetcher/FileFieldDataFetcher.java
@@ -3,6 +3,7 @@ package com.dotcms.graphql.datafetcher;
 import com.dotcms.graphql.DotGraphQLContext;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
+import com.dotmarketing.util.UtilMethods;
 import com.liferay.portal.model.User;
 
 import graphql.schema.DataFetcher;
@@ -15,6 +16,11 @@ public class FileFieldDataFetcher implements DataFetcher<Contentlet> {
         final Contentlet contentlet = environment.getSource();
         final String var = environment.getField().getName();
         final String fileAssetIdentifier = (String) contentlet.get(var);
+
+        if(!UtilMethods.isSet(fileAssetIdentifier)) {
+            return null;
+        }
+
         final Contentlet fileAsContent = APILocator.getContentletAPI()
             .findContentletByIdentifier(fileAssetIdentifier, contentlet.isLive(), contentlet.getLanguageId(),
                 user, true);
