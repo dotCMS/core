9dd840016be2cc91604b76929075368b32cb7114
Daniel Silva <daniel.silva@dotcms.com>
2019-02-25 12:44:23 -0600

    #16098 unused import

--- dotCMS/src/main/java/com/dotcms/graphql/datafetcher/UserDataFetcher.java
+++ dotCMS/src/main/java/com/dotcms/graphql/datafetcher/UserDataFetcher.java
@@ -11,7 +11,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import graphql.ExceptionWhileDataFetching;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 
