007cdfaee5186d965275f40717ab0afe8447b8da
Jonathan <jonathan.sanchez@dotcms.com>
2019-03-06 12:16:38 -0600

    Issue 15853 publish unpublish issues fscontainer (#16175)
    * #15988 fixes when the spanish content type on container are archived
    
    * #15988 when the container.vtl is not on the default lang, avoid the clean up
    
    * #15988 now when deleting a content type but it is not the last one live, the content is not removing, only if it is the last content type of the fs container
    
    * #15853 fix an issue with the publish unpublish on container.vtl

--- dotCMS/src/main/java/com/dotcms/rendering/velocity/servlet/VelocityServlet.java
+++ dotCMS/src/main/java/com/dotcms/rendering/velocity/servlet/VelocityServlet.java
@@ -1,38 +1,23 @@
 package com.dotcms.rendering.velocity.servlet;
 
-import static com.dotmarketing.filters.TimeMachineFilter.TM_DATE_VAR;
-
 import com.dotcms.business.CloseDB;
 import com.dotcms.enterprise.LicenseUtil;
 import com.dotcms.enterprise.license.LicenseLevel;
 import com.dotcms.rendering.velocity.viewtools.VelocityRequestWrapper;
-
-import com.dotcms.repackage.com.fasterxml.jackson.databind.ObjectMapper;
-import com.dotcms.repackage.com.fasterxml.jackson.databind.ObjectWriter;
-import com.dotcms.rest.api.v1.page.PageResource;
-import com.dotcms.rest.api.v1.page.PageResourceHelper;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.db.DbConnectionFactory;
 import com.dotmarketing.filters.Constants;
 import com.dotmarketing.util.Logger;
 import com.dotmarketing.util.PageMode;
+import org.apache.velocity.exception.ResourceNotFoundException;
 
-import java.io.IOException;
-import java.net.URLDecoder;
-
-import java.util.HashMap;
-import java.util.Map;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.dotmarketing.util.WebKeys;
-import com.liferay.portal.model.User;
-import org.apache.velocity.exception.MethodInvocationException;
-import org.apache.velocity.exception.ParseErrorException;
-import org.apache.velocity.exception.ResourceNotFoundException;
+import java.io.IOException;
+import java.net.URLDecoder;
 
 public class VelocityServlet extends HttpServlet {
 
