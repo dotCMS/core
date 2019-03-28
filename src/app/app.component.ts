54c3fe43f492df16655a43c49624d4585e089e22
Alfredo Li <alfredo@dotcms.com>
2019-03-28 11:03:39 -0600

    clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { NotLicensedService } from '@services/not-licensed-service';
 import { DotcmsConfig } from 'dotcms-js';
 import { DotUiColors, DotUiColorsService } from '@services/dot-ui-colors/dot-ui-colors.service';
 import { take, pluck } from 'rxjs/operators';
-import { initDotCMS } from 'dotcms';
 
 @Component({
     selector: 'dot-root',
@@ -20,13 +19,6 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit() {
-
-        const dotcms = initDotCMS({
-            token: 'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlYWJmNDc1MS1iZDQ3LTRhYWEtODJhYy1kZmU5MjAzOGQ4NGUiLCJ4bW9kIjoxMjA0ODI0OTYxMDAwLCJzdWIiOiJkb3RjbXMub3JnLjEiLCJpYXQiOjE1NTM3ODY5NTEsImlzcyI6ImE3MTVlYzAwLTRkZmUtNDUwNC1hZDgwLTRkNDczMTMwZDNlNiIsImV4cCI6MTU1Mzg3MzM1MX0.NYW9AD1-2oBL6zmuBypF5DAOOFbUI8vgkMGDP8RV-Io'
-        });
-
-        dotcms.contentType.getFields('7fcedef5-1048-433d-9f67-bbec281d9c81').then(data => {console.log('---data', data)})
-
         this.dotCmsConfig
             .getConfig()
             .pipe(
