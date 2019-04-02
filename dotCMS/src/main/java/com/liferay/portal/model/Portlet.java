fb0aa8c7b1a62075fbc3bd42b37467413607241b
Will Ezell <will@dotcms.com>
2019-04-01 21:04:19 -0400

    #16161 unmapping portlets

--- dotCMS/src/main/java/com/liferay/portal/model/Portlet.java
+++ dotCMS/src/main/java/com/liferay/portal/model/Portlet.java
@@ -1,22 +1,3 @@
-/**
- * Copyright (c) 2000-2005 Liferay, LLC. All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- * associated documentation files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge, publish, distribute,
- * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
- * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 package com.liferay.portal.model;
 
 import java.util.HashMap;
@@ -31,15 +12,11 @@ import com.dotmarketing.util.Logger;
 import com.liferay.portal.ejb.PortletPK;
 import com.liferay.portlet.CachePortlet;
 
-/**
- * <a href="Portlet.java.html"><b><i>View Source</i></b></a>
- *
- * @author Brian Wing Shun Chan
- * @version $Revision: 1.49 $
- *
- */
+
 public class Portlet extends PortletModel {
 
+
+  private static final long serialVersionUID = 1L;
   private final String portletId;
   private final Map<String, String> initParams;
   private final String portletClass;
