93c1ca208efd388fcfc302be5234a991461f3f03
Nollymar Longa <nollymar.longa@dotcms.com>
2019-04-15 13:26:29 -0500

    #16277 Fixing failing test RulesUnderPageAssetsFTest.testFireRuleUnderLivePage

--- dotCMS/src/main/java/com/dotmarketing/portlets/htmlpageasset/business/HTMLPageAssetAPIImpl.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/htmlpageasset/business/HTMLPageAssetAPIImpl.java
@@ -865,7 +865,7 @@ public class HTMLPageAssetAPIImpl implements HTMLPageAssetAPI {
                     user, respectFrontEndPermissions);
             htmlPage = APILocator.getHTMLPageAssetAPI().fromContentlet(contentlet);
 
-        } catch (DotContentletStateException dse) {
+        } catch (DotStateException dse) {
             htmlPage = findPageInDefaultLanguageDifferentThanProvided(identifier, tryLang, live, user,
                     respectFrontEndPermissions, dse);
 
@@ -876,7 +876,7 @@ public class HTMLPageAssetAPIImpl implements HTMLPageAssetAPI {
 
     private IHTMLPage findPageInDefaultLanguageDifferentThanProvided(String identifier, long providedLang, boolean live,
                                                                      User user, boolean respectFrontEndPermissions,
-                                                                     DotContentletStateException dse)
+            DotStateException dse)
             throws DotDataException, DotSecurityException {
         Contentlet contentlet;
         IHTMLPage htmlPage;
@@ -887,7 +887,7 @@ public class HTMLPageAssetAPIImpl implements HTMLPageAssetAPI {
                 contentlet = APILocator.getContentletAPI().findContentletByIdentifier(identifier, live,
                     languageAPI.getDefaultLanguage().getId(), user, respectFrontEndPermissions);
                 htmlPage = APILocator.getHTMLPageAssetAPI().fromContentlet(contentlet);
-            } catch(DotContentletStateException e) {
+            } catch(DotStateException e) {
                 throw new ResourceNotFoundException(
                         "Can't find content. Identifier: " + identifier + ", Live: " + live + ", Lang: "
                                 + languageAPI.getDefaultLanguage().getId(), e);
