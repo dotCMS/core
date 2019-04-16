8914ccfe7babdee1261c0aff8118527d646f84b9
Freddy Rodriguez <freddy0309@gmail.com>
2019-04-16 07:10:34 -0600

    dotCMS/core#16161 Custom Content Portlets (#983)
    * Revert "Revert "dotCMS/core#16161 Custom Content Portlets (#982)""
    
    This reverts commit 4e364f24717ce3001953d0086d310219f2239c5a.
    
    * fix error with edit content in edit-page

--- src/app/view/components/dot-contentlet-editor/services/dot-contentlet-editor.service.spec.ts
+++ src/app/view/components/dot-contentlet-editor/services/dot-contentlet-editor.service.spec.ts
@@ -2,21 +2,26 @@ import { of as observableOf } from 'rxjs';
 import { DOTTestBed } from '../../../../test/dot-test-bed';
 import { DotContentletEditorService } from './dot-contentlet-editor.service';
 import { DotMenuService } from '@services/dot-menu.service';
+import { DotRouterService } from '@services/dot-router/dot-router.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('DotContentletEditorService', () => {
     const load = () => {};
     const keyDown = () => {};
     let service: DotContentletEditorService;
     let dotMenuService: DotMenuService;
+    let dotRouterService: DotRouterService;
     let injector;
 
     beforeEach(() => {
         injector = DOTTestBed.configureTestingModule({
-            providers: [DotContentletEditorService, DotMenuService]
+            providers: [DotContentletEditorService, DotMenuService, DotRouterService],
+            imports: [RouterTestingModule]
         });
 
         service = injector.get(DotContentletEditorService);
         dotMenuService = injector.get(DotMenuService);
+        dotRouterService = injector.get(DotRouterService);
         spyOn(dotMenuService, 'getDotMenuId').and.returnValue(observableOf('456'));
     });
 
@@ -54,6 +59,42 @@ describe('DotContentletEditorService', () => {
     });
 
     it('should set data to edit', () => {
+        spyOnProperty(dotRouterService, 'currentPortlet').and.returnValue({
+            url: '/c/c-Test/123',
+            id: '123'
+        });
+        service.editUrl$.subscribe((url: string) => {
+            expect(url).toEqual(
+                [
+                    `/c/portal/layout`,
+                    `?p_l_id=456`,
+                    `&p_p_id=c-Test`,
+                    `&p_p_action=1`,
+                    `&p_p_state=maximized`,
+                    `&p_p_mode=view`,
+                    `&_c-Test_struts_action=%2Fext%2Fcontentlet%2Fedit_contentlet`,
+                    `&_c-Test_cmd=edit&inode=999`
+                ].join('')
+            );
+        });
+
+        service.header$.subscribe((header: string) => {
+            expect(header).toEqual('This is a header for edit');
+        });
+
+        service.edit({
+            header: 'This is a header for edit',
+            data: {
+                inode: '999'
+            }
+        });
+    });
+
+    it('should set data to edit when current portlet is edit-page', () => {
+        spyOnProperty(dotRouterService, 'currentPortlet').and.returnValue({
+            url: '/#/edit-page/content?url=%2Fabout-us%2Findex&language_id=1',
+            id: 'edit-page'
+        });
         service.editUrl$.subscribe((url: string) => {
             expect(url).toEqual(
                 [
