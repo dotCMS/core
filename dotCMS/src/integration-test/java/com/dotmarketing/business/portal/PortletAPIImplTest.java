bb60763cef1dd06bbd29369af017914e6b824bf3
Nollymar Longa <nollymar.longa@dotcms.com>
2019-04-30 13:09:13 -0500

    Cleaning up whitespaces on base types and content types

--- dotCMS/src/integration-test/java/com/dotmarketing/business/portal/PortletAPIImplTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/business/portal/PortletAPIImplTest.java
@@ -1,5 +1,6 @@
 package com.dotmarketing.business.portal;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 import com.dotcms.util.IntegrationTestInitService;
@@ -7,7 +8,6 @@ import com.dotmarketing.business.APILocator;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.util.Config;
 import com.liferay.portal.SystemException;
-import com.liferay.portal.ejb.PortletManagerFactory;
 import com.liferay.portal.language.LanguageException;
 import com.liferay.portal.model.Portlet;
 import com.liferay.portal.model.User;
@@ -17,8 +17,10 @@ import com.liferay.portlet.StrutsPortlet;
 import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.apache.struts.Globals;
 import org.junit.Assert;
@@ -64,8 +66,21 @@ public class PortletAPIImplTest {
             throws LanguageException {
         Portlet portlet = null;
         try {
-            portlet = createCustomPortlet(testCase.portletName, testCase.porltedId, testCase.baseTypes, testCase.contentTypes);
-            Assert.assertTrue(testCase.createdSuccessfully);
+            portlet = createCustomPortlet(testCase.portletName, testCase.portletId, testCase.baseTypes, testCase.contentTypes);
+            assertTrue(testCase.createdSuccessfully);
+            if (testCase.baseTypes != null) {
+                final List<String> returnedBaseTypes = Arrays
+                        .asList(portlet.getInitParams().get("baseTypes").toLowerCase().split(","));
+                assertTrue(Arrays.asList(testCase.baseTypes.toLowerCase().split(",")).stream().allMatch(
+                        baseType -> returnedBaseTypes.contains(baseType.trim())));
+            }
+
+            if (testCase.contentTypes != null){
+                final List<String> returnedContentTypes = Arrays
+                        .asList(portlet.getInitParams().get("contentTypes").toLowerCase().split(","));
+                assertTrue(Arrays.asList(testCase.contentTypes.toLowerCase().split(",")).stream().allMatch(
+                        contentType -> returnedContentTypes.contains(contentType.trim())));
+            }
         }catch (DotDataException | IllegalArgumentException e){
             Assert.assertFalse(testCase.createdSuccessfully);
             return;
@@ -79,11 +94,13 @@ public class PortletAPIImplTest {
 
 
     private static class testCaseCreateCustomPortlet{
-        String porltedId, portletName, baseTypes, contentTypes;
+        String portletId, portletName, baseTypes, contentTypes;
         boolean createdSuccessfully;
 
-        testCaseCreateCustomPortlet(final String porltedId, final String portletName, final String baseTypes, final String contentTypes, final boolean createdSuccessfully){
-            this.porltedId = porltedId;
+        testCaseCreateCustomPortlet(final String portletId, final String portletName,
+                final String baseTypes, final String contentTypes,
+                final boolean createdSuccessfully) {
+            this.portletId = portletId;
             this.portletName = portletName;
             this.baseTypes = baseTypes;
             this.contentTypes = contentTypes;
@@ -92,15 +109,24 @@ public class PortletAPIImplTest {
     }
 
     @DataProvider
-    public static Object[] testCasesCreateCustomPortlet(){
-        return new Object[] {
-                new testCaseCreateCustomPortlet("testCustomPortlet","testCustomPortlet","Persona","",true),
-                new testCaseCreateCustomPortlet("testCustomPortlet","testCustomPortlet","","news",true),
-                new testCaseCreateCustomPortlet("","testCustomPortlet","Persona","",false),
-                new testCaseCreateCustomPortlet("testCustomPortlet","","Persona","",false),
-                new testCaseCreateCustomPortlet("testCustomPortlet","testCustomPortlet","","",false),
-                new testCaseCreateCustomPortlet("testCustomPortlet","testCustomPortlet","NoExist","",false),
-                new testCaseCreateCustomPortlet("testCustomPortlet","testCustomPortlet","","NoExist",false),
+    public static Object[] testCasesCreateCustomPortlet() {
+        return new Object[]{
+                new testCaseCreateCustomPortlet("testCustomPortlet", "testCustomPortlet", "Persona",
+                        "", true),
+                new testCaseCreateCustomPortlet("testCustomPortlet", "testCustomPortlet",
+                        "Content, Persona", "", true),
+                new testCaseCreateCustomPortlet("testCustomPortlet", "testCustomPortlet", "",
+                        "news", true),
+                new testCaseCreateCustomPortlet("testCustomPortlet", "testCustomPortlet", "",
+                        "news, youtube", true),
+                new testCaseCreateCustomPortlet("", "testCustomPortlet", "Persona", "", false),
+                new testCaseCreateCustomPortlet("testCustomPortlet", "", "Persona", "", false),
+                new testCaseCreateCustomPortlet("testCustomPortlet", "testCustomPortlet", "", "",
+                        false),
+                new testCaseCreateCustomPortlet("testCustomPortlet", "testCustomPortlet", "NoExist",
+                        "", false),
+                new testCaseCreateCustomPortlet("testCustomPortlet", "testCustomPortlet", "",
+                        "NoExist", false),
         };
     }
 
