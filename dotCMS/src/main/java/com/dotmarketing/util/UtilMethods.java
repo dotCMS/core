f6a8e1b48ffd85450cf2d6301b69c5fd286d7a94
Freddy Rodriguez <freddyrodriguez@Freddys-MacBook-Pro.local>
2019-05-08 13:04:21 -0600

    adding submit method

--- dotCMS/src/main/java/com/dotmarketing/util/UtilMethods.java
+++ dotCMS/src/main/java/com/dotmarketing/util/UtilMethods.java
@@ -2,6 +2,7 @@ package com.dotmarketing.util;
 
 import com.dotcms.contenttype.model.field.Field;
 import com.dotcms.contenttype.transform.field.JsonFieldTransformer;
+import com.dotcms.contenttype.transform.field.LegacyFieldTransformer;
 import com.dotcms.repackage.com.csvreader.CsvReader;
 import com.dotmarketing.beans.Host;
 import com.dotmarketing.beans.Identifier;
@@ -3526,7 +3527,9 @@ public class UtilMethods {
         return adminMode;
     }
 
-    public static JsonFieldTransformer getJsonFieldTransformer(final List<Field> fields) {
-        return new JsonFieldTransformer(fields);
+    public static JsonFieldTransformer getJsonFieldTransformer(
+            final List<com.dotmarketing.portlets.structure.model.Field> oldField) {
+
+        return new JsonFieldTransformer(new LegacyFieldTransformer(oldField).asList());
     }
 }
