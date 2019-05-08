fb440c23fdbe7d8b13bf069cd899934ad62bd776
Daniel Silva <daniel.silva@dotcms.com>
2019-05-08 15:15:15 -0500

    Fix default values for api_token_issued table on mysql (#16544)

--- dotCMS/src/main/java/com/dotcms/auth/providers/jwt/factories/ApiTokenSQLMySQL.java
+++ dotCMS/src/main/java/com/dotcms/auth/providers/jwt/factories/ApiTokenSQLMySQL.java
@@ -7,15 +7,15 @@ public class ApiTokenSQLMySQL extends ApiTokenSQL{
             return "create table api_token_issued("
                 + "token_id varchar(255) NOT NULL, "
                 + "token_userid varchar(255) NOT NULL, "
-                + "issue_date TIMESTAMP NOT NULL, "
-                + "expire_date TIMESTAMP NOT NULL, "
+                + "issue_date TIMESTAMP NOT NULL default CURRENT_TIMESTAMP, "
+                + "expire_date TIMESTAMP NOT NULL default CURRENT_TIMESTAMP, "
                 + "requested_by_userid  varchar(255) NOT NULL, "
                 + "requested_by_ip  varchar(255) NOT NULL, "
                 + "revoke_date TIMESTAMP NULL DEFAULT NULL, "
                 + "allowed_from  varchar(255) , "
                 + "issuer  varchar(255) , "
                 + "claims  text , "
-                + "mod_date  TIMESTAMP NOT NULL, "
+                + "mod_date  TIMESTAMP NOT NULL default CURRENT_TIMESTAMP, "
                 + "PRIMARY KEY (token_id));"
                 + "create index idx_api_token_issued_user ON api_token_issued (token_userid)";
         
