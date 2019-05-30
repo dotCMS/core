a1367ae71366a73c2abfcb7cb30f465f8ff4e135
Will Ezell <will@dotcms.com>
2019-05-30 11:56:43 -0400

    #16584 starting to add test

--- dotCMS/src/integration-test/java/com/dotmarketing/business/LanguageAPITest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/business/LanguageAPITest.java
@@ -2,19 +2,36 @@ package com.dotmarketing.business;
 
 import static com.dotcms.integrationtestutil.content.ContentUtils.createTestKeyValueContent;
 import static com.dotcms.integrationtestutil.content.ContentUtils.deleteContentlets;
+import static org.junit.Assert.assertEquals;
 
 import com.dotcms.contenttype.model.type.ContentType;
+import com.dotcms.datagen.ContainerDataGen;
+import com.dotcms.datagen.ContentletDataGen;
+import com.dotcms.datagen.StructureDataGen;
 import com.dotcms.languagevariable.business.LanguageVariableAPI;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.portlets.containers.model.Container;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
+import com.liferay.portal.language.LanguageUtil;
 import com.liferay.portal.model.User;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
 import org.junit.Assert;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.dotcms.util.IntegrationTestInitService;
+import com.dotmarketing.portlets.languagesmanager.business.LanguageAPI;
 import com.dotmarketing.portlets.languagesmanager.model.Language;
+import com.dotmarketing.portlets.structure.model.Structure;
+import com.dotmarketing.util.Logger;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 
 public class LanguageAPITest {
 	private static User systemUser;
@@ -141,8 +158,41 @@ public class LanguageAPITest {
 	
 	
 	
-	
-	
+  /**
+   * This test is for a language key that does not exists neither as a content or in the properties
+   * file, so the key will be returned.
+   */
+  @Test
+  public void getStringsAsMap_returnMap() throws Exception {
+    final String BAD_KEY = "KEY-DOES-NOT-EXIST";
+    final String CONTENTLET_KEY = "key-exists-as-contentlet";
+    final String PROPERTYFILE_KEY = "key-exists-in-properties";
+    final String SYSTEM_PROPERTYFILE_KEY = "contenttypes.action.cancel";
+    final LanguageAPI lapi = APILocator.getLanguageAPI();
+    ContentType langKeyType = APILocator.getContentTypeAPI(APILocator.systemUser()).find("Languagevariable");
+
+    Contentlet con = new ContentletDataGen(langKeyType.id()).setProperty("key", CONTENTLET_KEY).setProperty("value", CONTENTLET_KEY + "works")
+        .nextPersisted();
+
+    // Add Languague Variable to local properties
+    Map<String, String> propertyKeys = ImmutableMap.of(PROPERTYFILE_KEY, PROPERTYFILE_KEY + "works");
+
+    for (Language lang : APILocator.getLanguageAPI().getLanguages()) {
+      lapi.saveLanguageKeys(lang, propertyKeys, ImmutableMap.of(), ImmutableSet.of());
+    }
+
+    
+    List<String> keys = ImmutableList.of(BAD_KEY, CONTENTLET_KEY,PROPERTYFILE_KEY,SYSTEM_PROPERTYFILE_KEY );
+    
+    Locale locale = new Locale("en");
+    
+    Map<String, String> translatedMap = lapi.getStringsAsMap(locale, keys);
+    assertEquals(translatedMap.get(BAD_KEY), BAD_KEY);
+    assertEquals(translatedMap.get(CONTENTLET_KEY), CONTENTLET_KEY+"works");
+    assertEquals(translatedMap.get(PROPERTYFILE_KEY), PROPERTYFILE_KEY+"works");
+    assertEquals(translatedMap.get(SYSTEM_PROPERTYFILE_KEY), LanguageUtil.get( locale, SYSTEM_PROPERTYFILE_KEY ));
+  }
+  
 	
 	
 	
