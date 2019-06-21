9543dc8aa89d7b733c4d5769b936022d1e1e9186
jdotcms <jonathan.sanchez@dotcms.com>
2019-06-21 16:48:22 -0600

    #16697 first draft for getting the previous version when the persona tag has changed and see if update or not the multi tree based on it

--- dotCMS/src/main/java/com/dotmarketing/portlets/personas/business/MultiTreePersonaListener.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/personas/business/MultiTreePersonaListener.java
@@ -2,9 +2,15 @@ package com.dotmarketing.portlets.personas.business;
 
 import com.dotcms.content.elasticsearch.business.event.ContentletDeletedEvent;
 import com.dotcms.content.elasticsearch.business.event.ContentletPublishEvent;
+import com.dotmarketing.business.APILocator;
+import com.dotmarketing.business.Versionable;
+import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.portlets.contentlet.model.ContentletListener;
 import com.dotmarketing.portlets.personas.model.Persona;
 import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.UtilMethods;
+
+import java.util.Optional;
 
 /**
  * This listener is to track the changes on personas in order to keep up to date changes on the
@@ -18,8 +24,32 @@ public class MultiTreePersonaListener implements ContentletListener<Persona> {
     @Override
     public void onModified(final ContentletPublishEvent<Persona> contentletPublishEvent) {
 
-        // Here want to check if the persona key tag has changed.
-        DeleteMultiTreeUsedPersonaTagJob.triggerDeleteMultiTreeUsedPersonaTagJob(contentletPublishEvent.getUser(), false);
+        try {
+
+            final Optional<Versionable> versionable = APILocator.getVersionableAPI()
+                    .findPreviousVersion(contentletPublishEvent.getContentlet().getIdentifier());
+
+            if (versionable.isPresent()) {
+
+                final Contentlet previousPersonaContent = APILocator.getContentletAPI().find
+                        (versionable.get().getInode(), APILocator.systemUser(), false);
+
+                if (null != previousPersonaContent) {
+
+                    final String currentKeyTag  = contentletPublishEvent.getContentlet().getKeyTag();
+                    final String previousKeyTag = previousPersonaContent.getStringProperty(PersonaAPI.KEY_TAG_FIELD);
+
+                    if (UtilMethods.isSet(currentKeyTag) && UtilMethods.isSet(previousKeyTag) &&
+                            !currentKeyTag.equals(previousKeyTag)) { //  if the tag has changed.
+
+                        // todo: update the previous multi tree personalization with persona tag with the new one
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            Logger.error(this, e.getMessage(), e);
+        }
     }
 
     @Override
