11031afa984d0632674f87559fb6750d7340f338
Will Ezell <will@dotcms.com>
2019-07-10 17:13:17 -0400

    #16827-transaction-interuption

--- dotCMS/src/main/java/com/dotmarketing/listeners/ClickstreamListener.java
+++ dotCMS/src/main/java/com/dotmarketing/listeners/ClickstreamListener.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
+import com.dotcms.business.WrapInTransaction;
 import com.dotmarketing.beans.Clickstream;
 import com.dotmarketing.db.HibernateUtil;
 import com.dotmarketing.exception.DotHibernateException;
@@ -18,65 +19,58 @@ import com.dotmarketing.util.Config;
 import com.dotmarketing.util.Logger;
 
 /**
- * The listener that keeps track of all clickstreams in the container as well as
- * the creating new Clickstream objects and initiating logging when the
- * clickstream dies (session has been invalidated).
+ * The listener that keeps track of all clickstreams in the container as well as the creating new
+ * Clickstream objects and initiating logging when the clickstream dies (session has been
+ * invalidated).
  * 
  * @author <a href="plightbo@hotmail.com">Patrick Lightbody </a>
  */
 public class ClickstreamListener implements ServletContextListener, HttpSessionListener {
 
-    public static final String CLICKSTREAMS_ATTRIBUTE_KEY = "clickstreams";
-    private static Map<String, Clickstream> clickstreams = Collections.synchronizedMap(new HashMap<String, Clickstream>());
+  public static final String CLICKSTREAMS_ATTRIBUTE_KEY = "clickstreams";
+  private static Map<String, Clickstream> clickstreams = Collections.synchronizedMap(new HashMap<String, Clickstream>());
 
-    public ClickstreamListener() {
-        Logger.debug(this, "ClickstreamLogger constructed");
-    }
+  public ClickstreamListener() {
+    Logger.debug(this, "ClickstreamLogger constructed");
+  }
 
-    public void contextInitialized(ServletContextEvent sce) {
-    	if(Config.getBooleanProperty("ENABLE_CLICKSTREAM_TRACKING", false)){
-    		sce.getServletContext().setAttribute(CLICKSTREAMS_ATTRIBUTE_KEY, clickstreams);
-    	}
+  public void contextInitialized(ServletContextEvent sce) {
+    if (Config.getBooleanProperty("ENABLE_CLICKSTREAM_TRACKING", false)) {
+      sce.getServletContext().setAttribute(CLICKSTREAMS_ATTRIBUTE_KEY, clickstreams);
     }
+  }
 
-    public void contextDestroyed(ServletContextEvent sce) {
-    }
+  public void contextDestroyed(ServletContextEvent sce) {}
 
-    public void sessionCreated(HttpSessionEvent hse) {
-    	if(Config.getBooleanProperty("ENABLE_CLICKSTREAM_TRACKING", false)){
-	        HttpSession session = hse.getSession();
-	        Logger.debug(this, "Session " + session.getId() + " was created, adding a new clickstream.");
-	        Clickstream clickstream = new Clickstream();
-	        session.setAttribute("clickstream", clickstream);
-	        clickstreams.put(session.getId(), clickstream);
-    	}
+  public void sessionCreated(HttpSessionEvent hse) {
+    if (Config.getBooleanProperty("ENABLE_CLICKSTREAM_TRACKING", false)) {
+      HttpSession session = hse.getSession();
+      Logger.debug(this, "Session " + session.getId() + " was created, adding a new clickstream.");
+      Clickstream clickstream = new Clickstream();
+      session.setAttribute("clickstream", clickstream);
+      clickstreams.put(session.getId(), clickstream);
     }
+  }
 
-    public void sessionDestroyed(HttpSessionEvent hse) {
-    	if(Config.getBooleanProperty("ENABLE_CLICKSTREAM_TRACKING", false)){
-	        HttpSession session = hse.getSession();
-	        Logger.debug(this, "Session " + session.getId() + " was destroyed, logging the clickstream and removing it.");
-	        try {
-	        	
-	            Clickstream clickstream = (Clickstream) clickstreams.get(session.getId());
-	            ClickstreamFactory.flushClickStream(clickstream);
-				clickstreams.remove(session.getId());
-	            
-	        } catch (Exception e) {
-	            Logger.error(this, "An error as ocurred when saving the clickstream");
-	        } finally {
-	        	try {
-					HibernateUtil.closeSession();
-				} catch (DotHibernateException e) {
-					Logger.error(this, e.getMessage(),e);
-				}
-	        }
-    	}
+  
+  public void sessionDestroyed(HttpSessionEvent hse) {
+    if (Config.getBooleanProperty("ENABLE_CLICKSTREAM_TRACKING", false)) {
+      HttpSession session = hse.getSession();
+      Logger.debug(this, "Session " + session.getId() + " was destroyed, logging the clickstream and removing it.");
+      try {
+
+        Clickstream clickstream = (Clickstream) clickstreams.get(session.getId());
+        ClickstreamFactory.flushClickStream(clickstream);
+        clickstreams.remove(session.getId());
+
+      } catch (Exception e) {
+        Logger.error(this, "An error as ocurred when saving the clickstream");
+      }
     }
-    
-    public static Clickstream getClickstream(String sessionId){
-    	return clickstreams.get(sessionId);
-    }
-    
-    
+  }
+
+  public static Clickstream getClickstream(String sessionId) {
+    return clickstreams.get(sessionId);
+  }
+
 }
