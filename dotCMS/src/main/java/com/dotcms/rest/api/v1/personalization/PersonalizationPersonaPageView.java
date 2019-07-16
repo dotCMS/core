d18bd83aa3be277e28a8f91033c107fe293cc098
jdotcms <jonathan.sanchez@dotcms.com>
2019-07-16 10:59:09 -0600

    #16767 Formatting the page persona

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/personalization/PersonalizationPersonaPageView.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/personalization/PersonalizationPersonaPageView.java
@@ -1,6 +1,8 @@
 package com.dotcms.rest.api.v1.personalization;
 
+import com.dotmarketing.portlets.htmlpageasset.business.render.page.PageViewSerializer;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import java.io.Serializable;
 import java.util.Map;
@@ -9,6 +11,7 @@ import java.util.Map;
  * Encapsulates the personalization for personas per page, if the persona
  * @author jsanca
  */
+@JsonSerialize(using = PersonalizationPersonaPageViewSerializer.class)
 public class PersonalizationPersonaPageView implements Serializable {
 
     private final String pageId;
@@ -20,7 +23,6 @@ public class PersonalizationPersonaPageView implements Serializable {
         this.persona      = persona;
     }
 
-    @JsonIgnore
     public String getPageId() {
         return pageId;
     }
