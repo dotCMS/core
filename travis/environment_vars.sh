a07d8c3bfebd838179625e10eb7f6b66ace891a2
Jonathan Gamba <jonathan.gamba@dotcms.com>
2019-07-23 12:58:35 -0600

    Travis tests

--- travis/environment_vars.sh
+++ travis/environment_vars.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo $GOOGLE_CREDENTIALS_BASE64 | base64 -d - > credentials.json
+echo '{
+"project_id": "'$GOOGLE_PROJECT_ID'",
+"private_key_id": "'$GOOGLE_CREDENTIALS_BASE64'"
+}' > credentials.json
+
+#echo $GOOGLE_CREDENTIALS_BASE64 | base64 -d - > credentials.json
 
 if [ ! -s "credentials.json" ]
 then
