d31432d70de43350358550b66b325d4feeaa857d
Jonathan Gamba <jonathan.gamba@dotcms.com>
2019-07-23 11:47:33 -0600

    Travis tests

--- travis/environment_vars.sh
+++ travis/environment_vars.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo $GOOGLE_CREDENTIALS_BASE64 | base64 -d - > credentials.json
+
+if [ ! -s "credentials.json" ]
+then
+   echo ""
+   echo "================================================================"
+   echo " >>> Valid [credentials.json] NOT FOUND <<<"
+   echo "================================================================"
+   exit 1
+fi
