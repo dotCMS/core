0a7013b7b4bb14efcf0cca3df35db2ba4d451f15
Jonathan Gamba <jonathan.gamba@dotcms.com>
2019-07-24 08:40:42 -0600

    Travis tests

--- travis/prepare.sh
+++ travis/prepare.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-dockerFolder="docker/tests/integration"
+RELATIVE_MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$RELATIVE_MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+
+echo ""
+echo "***************************"
+echo "***************************"
+echo "${MY_PATH}"
+echo "${RELATIVE_MY_PATH}"
+echo "***************************"
+echo "***************************"
+echo ""
+
+dockerFolder="../docker/tests/integration"
 
 # postgres-docker-compose.yml
 sed -e '30s/.*/#&/' -i '' ${dockerFolder}/postgres-docker-compose.yml
