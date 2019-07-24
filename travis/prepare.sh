df320c8a996906a6c0745a2c4ba24fa84e41aed2
Jonathan Gamba <jonathan.gamba@dotcms.com>
2019-07-24 09:22:31 -0600

    Travis tests

--- travis/prepare.sh
+++ travis/prepare.sh
@@ -19,6 +19,30 @@ echo ""
 
 dockerFolder="/home/travis/build/dotCMS/core/docker/tests/integration"
 
+echo "Elements in /home/travis/build/dotCMS/core"
+for entry in "/home/travis/build/dotCMS/core/"/*
+do
+  echo "$entry"
+done
+
+echo "Elements in /home/travis/build/dotCMS/core/docker"
+for entry in "/home/travis/build/dotCMS/core/docker/"/*
+do
+  echo "$entry"
+done
+
+echo "Elements in /home/travis/build/dotCMS/core/docker/tests"
+for entry in "/home/travis/build/dotCMS/core/docker/tests/"/*
+do
+  echo "$entry"
+done
+
+echo "Elements in /home/travis/build/dotCMS/core/docker/tests/integration"
+for entry in "/home/travis/build/dotCMS/core/docker/tests/integration/"/*
+do
+  echo "$entry"
+done
+
 # postgres-docker-compose.yml
 sed -e '30s/.*/#&/' -i '' ${dockerFolder}/postgres-docker-compose.yml
 sed -e '31s/.*/#&/' -i '' ${dockerFolder}/postgres-docker-compose.yml
