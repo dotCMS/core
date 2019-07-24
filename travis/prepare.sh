1a66762054aab76e484270eb7efa0ea4cd0f96fb
Jonathan Gamba <jonathan.gamba@dotcms.com>
2019-07-24 11:23:48 -0600

    Travis tests

--- travis/prepare.sh
+++ travis/prepare.sh
@@ -4,6 +4,9 @@ dockerFolder="/home/travis/build/dotCMS/core/docker/tests/integration"
 postgresCompose="${dockerFolder}/postgres-docker-compose.yml"
 mysqlCompose="${dockerFolder}/mysql-docker-compose.yml"
 
+# We need to comment the volumen bind fragments as we won't mount any folder with google builds
+# The easiest way is do in it by line number as we don'tar want to remove also the db volumen binds
+
 # postgres-docker-compose.yml
 sed -e "30s/.*/#&/" ${postgresCompose} >tmpfile && mv tmpfile ${postgresCompose}
 sed -e "31s/.*/#&/" ${postgresCompose} >tmpfile && mv tmpfile ${postgresCompose}
