84210b2e986a9ea508b3fc20a2b06bbf57ffb298
Jonathan Gamba <jonathan.gamba@dotcms.com>
2019-07-24 08:24:47 -0600

    Travis tests

--- travis/build_branch.sh
+++
@@ -1,4 +0,0 @@
- #!/bin/bash
-echo $TRAVIS_BRANCH
-echo $TRAVIS_COMMIT
-gcloud builds submit --config=travis/cloudbuild.yaml --substitutions=BRANCH_NAME=$TRAVIS_BRANCH  --substitutions=COMMIT_SHA=$TRAVIS_COMMIT .
---
+++ travis/prepare.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dockerFolder="../docker/tests/integration/"
+
+# postgres-docker-compose.yml
+sed -e '30s/.*/#&/' -i '' ${dockerFolder}/postgres-docker-compose.yml
+sed -e '31s/.*/#&/' -i '' ${dockerFolder}/postgres-docker-compose.yml
+sed -e '32s/.*/#&/' -i '' ${dockerFolder}/postgres-docker-compose.yml
+sed -e '33s/.*/#&/' -i '' ${dockerFolder}/postgres-docker-compose.yml
+sed -e '34s/.*/#&/' -i '' ${dockerFolder}/postgres-docker-compose.yml
+sed -e '35s/.*/#&/' -i '' ${dockerFolder}/postgres-docker-compose.yml
+sed -e '36s/.*/#&/' -i '' ${dockerFolder}/postgres-docker-compose.yml
+
+# mysql-docker-compose.yml
+sed -e '30s/.*/#&/' -i '' ${dockerFolder}/mysql-docker-compose.yml
+sed -e '31s/.*/#&/' -i '' ${dockerFolder}/mysql-docker-compose.yml
+sed -e '32s/.*/#&/' -i '' ${dockerFolder}/mysql-docker-compose.yml
+sed -e '33s/.*/#&/' -i '' ${dockerFolder}/mysql-docker-compose.yml
+sed -e '34s/.*/#&/' -i '' ${dockerFolder}/mysql-docker-compose.yml
+sed -e '35s/.*/#&/' -i '' ${dockerFolder}/mysql-docker-compose.yml
+sed -e '36s/.*/#&/' -i '' ${dockerFolder}/mysql-docker-compose.yml
