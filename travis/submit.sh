b11a634d903e9e43034e475ce50027f28138e8ce
Jonathan Gamba <jonathan.gamba@dotcms.com>
2019-07-26 11:39:23 -0600

    Travis tests

--- travis/submit.sh
+++ travis/submit.sh
@@ -11,6 +11,5 @@ function bell() {
 bell &
 gcloud builds submit \
   --config=travis/cloudbuild.yaml \
-  --substitutions=_GIT_BRANCH_COMMIT==$TRAVIS_BRANCH,COMMIT_SHA=$TRAVIS_COMMIT,\
-  _LICENSE_KEY=$LICENSE,_CUSTOM_RUN_ID=$TRAVIS_BUILD_ID .
+  --substitutions=_GIT_BRANCH_COMMIT==$TRAVIS_BRANCH,COMMIT_SHA=$TRAVIS_COMMIT,_LICENSE_KEY=$LICENSE,_CUSTOM_RUN_ID=$TRAVIS_BUILD_ID .
 exit $?
