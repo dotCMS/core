115d801d1680fedee5d6a95d232a1c303dd5a2d4
Will Ezell <will@dotcms.com>
2019-07-26 10:19:43 -0400

    #16832 started work with layouts for edit content

--- dotCMS/src/integration-test/java/com/dotmarketing/business/LayoutAPITest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/business/LayoutAPITest.java
@@ -1,8 +1,16 @@
 package com.dotmarketing.business;
 
 import com.dotcms.IntegrationTestBase;
+import com.dotcms.mock.request.MockHeaderRequest;
+import com.dotcms.mock.request.MockHttpRequest;
+import com.dotcms.mock.request.MockParameterRequest;
 import com.dotcms.util.IntegrationTestInitService;
 import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.util.UUIDGenerator;
+import com.google.common.collect.ImmutableMap;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -25,7 +33,7 @@ public class LayoutAPITest extends IntegrationTestBase {
     public void test_SaveLayout_WhenCreateNewLayout_Success() throws DotDataException {
         Layout layout = null;
         try {
-            layout = createNewLayout("testNewLayout", "", 0);
+            layout = createNewLayout("testNewLayout"+ UUIDGenerator.generateUuid(), "", 0);
             Assert.assertNotNull(layout);
         }finally {
             if(layout != null){
@@ -38,7 +46,7 @@ public class LayoutAPITest extends IntegrationTestBase {
     public void test_SaveLayout_WhenCreateAndUpdateNewLayout_Success() throws DotDataException {
         Layout layout = null;
         try {
-            layout = createNewLayout("testNewLayout", "", 0);
+            layout = createNewLayout("testNewLayout"+ UUIDGenerator.generateUuid(), "", 0);
             Assert.assertNotNull(layout);
 
             layout.setName("testUpdateLayout");
@@ -67,5 +75,35 @@ public class LayoutAPITest extends IntegrationTestBase {
 
         return layoutAPI.findLayoutByName(layoutName);
     }
+    
+    
+    @Test
+    public void test_resolveLayout() throws DotDataException {
+        Layout layout1, layout2 = null;
+ 
+            layout1 = createNewLayout("testNewLayout" + UUIDGenerator.generateUuid(), "", 0);
+            layout2 = createNewLayout("testNewLayout2"+ UUIDGenerator.generateUuid(), "", 1);
+      
+
+            String uri ="/c/portal/layout";
+            String referer ="/c/portal/layout?p_l_id=" + layout2.getId() + "&p_p_id=content&p_p_action=0&&dm_rlout=1&r=1563999037622&in_frame=true&frame=detailFrame&container=true&angularCurrentPortlet=content";
+            
+            
+            HttpServletRequest request = new MockHttpRequest("localhost",uri).request();
+            HttpServletRequest headerRequest = new MockHeaderRequest(request, "referer", referer).request();
+            HttpServletRequest paramRequest = new MockParameterRequest(request,ImmutableMap.of("p_l_id", layout1.getId())).request();
+            
+            Assert.assertEquals(layout1, layoutAPI.resolveLayout(paramRequest).get());
+            Assert.assertEquals(layout2, layoutAPI.resolveLayout(headerRequest).get());
+            Assert.assertFalse(layoutAPI.resolveLayout(request).isPresent());
+    
+    }
+    
+    
+    
+    
+    
+    
+    
 
 }
