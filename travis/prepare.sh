10745e8e702dd7bf4e90085228a3be3cc6b5bc4c
Jonathan Gamba <jonathan.gamba@dotcms.com>
2019-07-30 19:44:49 -0600

    #16372

--- travis/prepare.sh
+++ travis/prepare.sh
@@ -8,19 +8,19 @@ mysqlCompose="${dockerFolder}/mysql-docker-compose.yml"
 # The easiest way is do in it by line number as we don'tar want to remove also the db volumen binds
 
 # postgres-docker-compose.yml
-sed -e "24s/.*/#&/" ${postgresCompose} >tmpfile && mv tmpfile ${postgresCompose}
 sed -e "25s/.*/#&/" ${postgresCompose} >tmpfile && mv tmpfile ${postgresCompose}
 sed -e "26s/.*/#&/" ${postgresCompose} >tmpfile && mv tmpfile ${postgresCompose}
 sed -e "27s/.*/#&/" ${postgresCompose} >tmpfile && mv tmpfile ${postgresCompose}
 sed -e "28s/.*/#&/" ${postgresCompose} >tmpfile && mv tmpfile ${postgresCompose}
 sed -e "29s/.*/#&/" ${postgresCompose} >tmpfile && mv tmpfile ${postgresCompose}
 sed -e "30s/.*/#&/" ${postgresCompose} >tmpfile && mv tmpfile ${postgresCompose}
+sed -e "31s/.*/#&/" ${postgresCompose} >tmpfile && mv tmpfile ${postgresCompose}
 
 # mysql-docker-compose.yml
-sed -e "24s/.*/#&/" ${mysqlCompose} >tmpfile && mv tmpfile ${mysqlCompose}
 sed -e "25s/.*/#&/" ${mysqlCompose} >tmpfile && mv tmpfile ${mysqlCompose}
 sed -e "26s/.*/#&/" ${mysqlCompose} >tmpfile && mv tmpfile ${mysqlCompose}
 sed -e "27s/.*/#&/" ${mysqlCompose} >tmpfile && mv tmpfile ${mysqlCompose}
 sed -e "28s/.*/#&/" ${mysqlCompose} >tmpfile && mv tmpfile ${mysqlCompose}
 sed -e "29s/.*/#&/" ${mysqlCompose} >tmpfile && mv tmpfile ${mysqlCompose}
 sed -e "30s/.*/#&/" ${mysqlCompose} >tmpfile && mv tmpfile ${mysqlCompose}
+sed -e "31s/.*/#&/" ${mysqlCompose} >tmpfile && mv tmpfile ${mysqlCompose}
