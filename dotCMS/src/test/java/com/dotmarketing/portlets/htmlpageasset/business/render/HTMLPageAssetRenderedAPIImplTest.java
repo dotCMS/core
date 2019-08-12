5bfc8dcdd22e0173b462be1ca6d38c7557c31fb2
Freddy Rodriguez <freddyrodriguez@Freddys-MacBook-Pro.local>
2019-08-12 06:41:01 -0600

    testing

--- dotCMS/src/test/java/com/dotmarketing/portlets/htmlpageasset/business/render/HTMLPageAssetRenderedAPIImplTest.java
+++ dotCMS/src/test/java/com/dotmarketing/portlets/htmlpageasset/business/render/HTMLPageAssetRenderedAPIImplTest.java
@@ -174,6 +174,9 @@ public class HTMLPageAssetRenderedAPIImplTest {
         when(this.permissionAPI.doesUserHavePermission(htmlPage, PermissionLevel.READ.getType(), user,
                 PageMode.PREVIEW_MODE.respectAnonPerms)).thenReturn(true);
 
+        when(this.permissionAPI.doesUserHavePermission(htmlPage, PermissionLevel.READ.getType(), systemUser,
+                PageMode.PREVIEW_MODE.respectAnonPerms)).thenReturn(true);
+
         final PageMode defaultEditPageMode =
                 hTMLPageAssetRenderedAPIImpl.getDefaultEditPageMode(user, request, pageUri);
 
@@ -222,10 +225,17 @@ public class HTMLPageAssetRenderedAPIImplTest {
 
         when(language.getId()).thenReturn(1l);
 
+
+        when(this.permissionAPI.doesUserHavePermission(htmlPage, PermissionLevel.READ.getType(), user,
+                PageMode.PREVIEW_MODE.respectAnonPerms)).thenReturn(true);
+
+        when(this.permissionAPI.doesUserHavePermission(htmlPage, PermissionLevel.READ.getType(), systemUser,
+                PageMode.PREVIEW_MODE.respectAnonPerms)).thenReturn(true);
+
         final UrlMapContext urlMapContext = UrlMapContextBuilder.builder()
                 .setMode(PageMode.PREVIEW_MODE)
                 .setUri(pageUri)
-                .setUser(user)
+                .setUser(systemUser)
                 .setHost(currentHost)
                 .setLanguageId(language.getId())
                 .build();
@@ -237,5 +247,37 @@ public class HTMLPageAssetRenderedAPIImplTest {
                 hTMLPageAssetRenderedAPIImpl.getDefaultEditPageMode(user, request, pageUri);
 
         assertEquals(PageMode.EDIT_MODE,defaultEditPageMode);
+    }
+
+    @Test
+    public void testShouldReturnAdminMODE_whenUserDontHavePermission()
+            throws DotSecurityException, DotDataException{
+
+        final User user = mock(User.class);
+        when(user.getUserId()).thenReturn("user");
+
+        final String pageUri = "/pageUri";
+        final HTMLPageAsset htmlPage = mock(HTMLPageAsset.class);
+
+        final ContentletVersionInfo info = mock(ContentletVersionInfo.class);
+        when(info.getLockedBy()).thenReturn("user2");
+
+        when(htmlPage.getIdentifier()).thenReturn("1");
+        when(htmlPage.getLanguageId()).thenReturn(2l);
+
+        when(htmlPageAssetAPI.getPageByPath(pageUri, currentHost, DEFAULT_LANGUAGE.getId(),
+                PageMode.PREVIEW_MODE.showLive)).thenReturn(htmlPage);
+        when(versionableAPI.getContentletVersionInfo(htmlPage.getIdentifier(), htmlPage.getLanguageId())).thenReturn(info);
+
+        when(this.permissionAPI.doesUserHavePermission(htmlPage, PermissionLevel.READ.getType(), user,
+                PageMode.PREVIEW_MODE.respectAnonPerms)).thenReturn(false);
+
+        when(this.permissionAPI.doesUserHavePermission(htmlPage, PermissionLevel.READ.getType(), systemUser,
+                PageMode.PREVIEW_MODE.respectAnonPerms)).thenReturn(true);
+
+        final PageMode defaultEditPageMode =
+                hTMLPageAssetRenderedAPIImpl.getDefaultEditPageMode(user, request, pageUri);
+
+        assertEquals(PageMode.ADMIN_MODE, defaultEditPageMode);
     }
 }
