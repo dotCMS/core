3b1632eec05785da712b8f5c3c88011c5121edc3
Jonathan Gamba <jonathan.gamba@dotcms.com>
2019-08-19 07:41:13 -0600

    #16890

--- dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task05165CreateContentTypeWorkflowActionMappingTable.java
+++ dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task05165CreateContentTypeWorkflowActionMappingTable.java
@@ -31,7 +31,7 @@ public class Task05165CreateContentTypeWorkflowActionMappingTable extends Abstra
                                         "   workflow_action varchar(255) not null,\n" +
                                         "   scheme_or_content_type  varchar(255) not null,\n" +
                                         "   primary key (id)\n" +
-                                        ");",
+                                        ")",
 
             DbType.POSTGRESQL,   "create table workflow_action_mappings (\n" +
                                         "\n" +
@@ -48,23 +48,29 @@ public class Task05165CreateContentTypeWorkflowActionMappingTable extends Abstra
                                         "   workflow_action varchar(255) not null,\n" +
                                         "   scheme_or_content_type  varchar(255) not null,\n" +
                                         "   primary key (id)\n" +
-                                        ");",
-            DbType.ORACLE,       "create table workflow_action_mappings (id varchar2(36) not null primary key, action varchar2(36) not null, workflow_action varchar2(255) not null, scheme_or_content_type  varchar2(255) not null )",
+                                        ")",
+            DbType.ORACLE,       "create table workflow_action_mappings (\n" +
+                                        "\n" +
+                                        "   id varchar2(36) not null primary key ,\n" +
+                                        "   action varchar2(36) not null,\n" +
+                                        "   workflow_action varchar2(255) not null,\n" +
+                                        "   scheme_or_content_type  varchar2(255) not null\n" +
+                                        ")",
             DbType.MSSQL,        "create table workflow_action_mappings (\n" +
                                         "\n" +
                                         "   id NVARCHAR(36) primary key,\n" +
                                         "   action NVARCHAR(36) not null,\n" +
                                         "   workflow_action NVARCHAR(255) not null,\n" +
                                         "   scheme_or_content_type  NVARCHAR(255) not null\n" +
-                                        ");"
+                                        ")"
     );
 
     private static final Map<DbType, String> createContentTypeWorkflowActionMappingTableUniqueIndexSQLMap = map(
-            DbType.H2,           "CREATE UNIQUE INDEX idx_workflow_action_mappings ON workflow_action_mappings (action, workflow_action, scheme_or_content_type);",
+            DbType.H2,           "CREATE UNIQUE INDEX idx_workflow_action_mappings ON workflow_action_mappings (action, workflow_action, scheme_or_content_type)",
             DbType.POSTGRESQL,   "CREATE UNIQUE INDEX idx_workflow_action_mappings ON workflow_action_mappings (action, workflow_action, scheme_or_content_type)",
-            DbType.MYSQL,        "CREATE UNIQUE INDEX idx_workflow_action_mappings ON workflow_action_mappings (action, workflow_action, scheme_or_content_type);",
+            DbType.MYSQL,        "CREATE UNIQUE INDEX idx_workflow_action_mappings ON workflow_action_mappings (action, workflow_action, scheme_or_content_type)",
             DbType.ORACLE,       "CREATE UNIQUE INDEX idx_workflow_action_mappings ON workflow_action_mappings (action, workflow_action, scheme_or_content_type)",
-            DbType.MSSQL,        "CREATE UNIQUE INDEX idx_workflow_action_mappings ON workflow_action_mappings (action, workflow_action, scheme_or_content_type);"
+            DbType.MSSQL,        "CREATE UNIQUE INDEX idx_workflow_action_mappings ON workflow_action_mappings (action, workflow_action, scheme_or_content_type)"
     );
 
     @Override
