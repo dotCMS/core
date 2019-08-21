772ce15e36d7f1775b19a40d400e8c1d69a7f44e
Will Ezell <will@dotcms.com>
2019-08-21 13:00:21 -0400

    #17048 added comment

--- dotCMS/src/main/java/com/dotmarketing/util/StringUtils.java
+++ dotCMS/src/main/java/com/dotmarketing/util/StringUtils.java
@@ -306,7 +306,12 @@ public class StringUtils {
     }
     
     
-    
+    /**
+     * this is just a reference to the FileUtil methos, 
+     * added for findability
+     * @param fileName
+     * @return
+     */
     public static String sanitizeFileName(final String fileName) {
       return FileUtil.sanitizeFileName(fileName);
       
