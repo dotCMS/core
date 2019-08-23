b95dc170d9c6bfe3ff9c78f669041e9363c48e6d
Will Ezell <will@dotcms.com>
2019-08-22 21:48:38 -0400

    #17015 fix test

--- dotCMS/src/integration-test/java/com/dotcms/rest/WebResourceIntegrationTest.java
+++ dotCMS/src/integration-test/java/com/dotcms/rest/WebResourceIntegrationTest.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-import org.junit.Before
+import org.junit.Before;
 import org.junit.Test;
 
 import com.dotcms.datagen.UserDataGen;
