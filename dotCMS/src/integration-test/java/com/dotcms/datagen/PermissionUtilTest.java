590c512c81364aeff73cf55dbb94dfe6012ca7d3
Freddy Rodriguez <freddyrodriguez@Freddys-MacBook-Pro.local>
2019-08-26 11:08:05 -0600

    miss class

---
+++ dotCMS/src/integration-test/java/com/dotcms/datagen/PermissionUtilTest.java
@@ -0,0 +1,25 @@
+package com.dotcms.datagen;
+
+import com.dotmarketing.beans.Permission;
+import com.dotmarketing.business.APILocator;
+import com.dotmarketing.business.PermissionAPI;
+import com.dotmarketing.business.Permissionable;
+import com.dotmarketing.business.Role;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotSecurityException;
+import com.liferay.portal.model.User;
+
+public class PermissionUtilTest {
+
+    private PermissionUtilTest(){}
+
+    public static void addAnonymousUser(final Permissionable permissionable) throws DotDataException, DotSecurityException {
+         final Role anonymousRole = APILocator.getRoleAPI().loadCMSAnonymousRole();
+
+        final Permission permission = new Permission(
+                permissionable.getPermissionId(),
+                anonymousRole.getId(),
+                PermissionAPI.PERMISSION_READ);
+        APILocator.getPermissionAPI().save(permission, permissionable, APILocator.systemUser(), false);
+    }
+}
