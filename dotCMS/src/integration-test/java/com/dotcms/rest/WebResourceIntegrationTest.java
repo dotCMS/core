67e8b1a09814773ba798c3b8e63083e90abcde97
Will Ezell <will@dotcms.com>
2019-10-05 01:59:42 -0400

    Fixing webresource test (#17437)
    * [travis postgres]
    
    * [travis postgres]
    
    * [travis postgres]
    
    * [travis postgres]

--- dotCMS/src/integration-test/java/com/dotcms/rest/WebResourceIntegrationTest.java
+++ dotCMS/src/integration-test/java/com/dotcms/rest/WebResourceIntegrationTest.java
@@ -2,7 +2,11 @@ package com.dotcms.rest;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.dotcms.business.CloseDB;
 import com.dotcms.datagen.UserDataGen;
 import com.dotcms.mock.request.MockAttributeRequest;
 import com.dotcms.mock.request.MockHttpRequest;
@@ -12,10 +16,6 @@ import com.dotmarketing.business.APILocator;
 import com.dotmarketing.util.Config;
 import com.liferay.portal.model.User;
 import com.liferay.portal.util.WebKeys;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.junit.Before;
-import org.junit.Test;
 
 public class WebResourceIntegrationTest {
 
@@ -23,14 +23,15 @@ public class WebResourceIntegrationTest {
   private static final String READ = "READ";
   private static final String NONE = "None";
 
-  private HttpServletResponse response;
-  private User frontEndUser = null;
-  private User backEndUser = null;
-  private User cmsAnon = null;
-  private User apiUser = null;
+  private static HttpServletResponse response;
+  private static User frontEndUser = null;
+  private static User backEndUser = null;
+  private static User cmsAnon = null;
+  private static User apiUser = null;
 
-  @Before
-  public void init() throws Exception {
+  @CloseDB
+  @BeforeClass
+  public static void init() throws Exception {
 
     // Setting web app environment
     IntegrationTestInitService.getInstance().init();
@@ -60,12 +61,14 @@ public class WebResourceIntegrationTest {
   
   private HttpServletRequest frontEndRequest() {
     final HttpServletRequest request = anonymousRequest();
+    assertTrue("frontEndUser has frontEnd role", frontEndUser.isFrontendUser());
     request.setAttribute(WebKeys.USER, frontEndUser);
     return request;
   }
   
   private HttpServletRequest backEndRequest() {
     final HttpServletRequest request = anonymousRequest();
+    assertTrue("backEndUser has backend role", backEndUser.isBackendUser());
     request.setAttribute(WebKeys.USER, backEndUser);
     return request;
   }
