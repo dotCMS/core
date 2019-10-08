a71fefec4d13b45b56e849ce8bf91f4bdb284db5
jdotcms <jonathan.sanchez@dotcms.com>
2019-10-08 12:00:39 -0600

    #17391 removing unnecessary logs

--- dotCMS/src/main/java/com/dotmarketing/portlets/workflows/actionlet/UnpublishContentActionlet.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/workflows/actionlet/UnpublishContentActionlet.java
@@ -10,6 +10,9 @@ import com.dotmarketing.util.Logger;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Unpublish a content if this one has a live version
+ */
 @Actionlet(unpublish = true)
 public class UnpublishContentActionlet extends WorkFlowActionlet {
 
@@ -34,20 +37,14 @@ public class UnpublishContentActionlet extends WorkFlowActionlet {
 			final boolean hasLiveVersion = APILocator.getVersionableAPI()
 					.hasLiveVersion(processor.getContentlet());
 
-			Logger.info(this, "Unpublishing: " + processor.getContentlet().getIdentifier());
-			Logger.info(this, "Unpublishing: " + processor.getContentlet().getInode());
 			if (hasLiveVersion) {
 
-				Logger.info(this, "Unpublishing: " + processor.getContentlet().getIdentifier());
 				APILocator.getContentletAPI().unpublish(processor.getContentlet(), processor.getUser(), false);
-				Logger.info(this, "Unpublished: " + processor.getContentlet().getIdentifier());
-				Logger.info(this, "Unpublished: " + processor.getContentlet().isLive());
+				Logger.debug(this, ()->"Unpublished: " + processor.getContentlet().getIdentifier());
 			} else {
 
-				Logger.info(this, "Unpublishing, already unpublish does not need: " + processor.getContentlet().getIdentifier());
-				Logger.info(this, "Unpublished: " + processor.getContentlet().isLive());
+				Logger.debug(this, ()->"Unpublishing, already unpublish does not need: " + processor.getContentlet().getIdentifier());
 			}
-
 		} catch (Exception e) {
 			Logger.error(this.getClass(),e.getMessage(),e);
 			throw new  WorkflowActionFailureException(e.getMessage(),e);
