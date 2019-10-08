eae56145915cb1f50cdc6b379a244ae862b02525
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2019-10-08 13:10:38 -0600

    #17088 improvement on test case

--- dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task05200WorkflowTaskUniqueKeyTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task05200WorkflowTaskUniqueKeyTest.java
@@ -1,7 +1,12 @@
 package com.dotmarketing.startup.runonce;
 
+import static org.junit.Assert.assertTrue;
+
 import com.dotcms.util.IntegrationTestInitService;
+import com.dotmarketing.common.db.DotConnect;
+import com.dotmarketing.db.DbConnectionFactory;
 import com.dotmarketing.exception.DotDataException;
+import java.sql.SQLException;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -13,13 +18,35 @@ public class Task05200WorkflowTaskUniqueKeyTest {
         IntegrationTestInitService.getInstance().init();
     }
 
-    @Test
-    public void Test_Upgrade_Task()
-            throws DotDataException {
-        final Task05200WorkflowTaskUniqueKey task =  new Task05200WorkflowTaskUniqueKey();
-        if(task.forceRun()){
-           task.executeUpgrade();
+    private void removeConstraintIfAny() throws DotDataException {
+        try {
+            DbConnectionFactory.getConnection().setAutoCommit(true);
+        } catch (SQLException e) {
+            throw new DotDataException(e.getMessage(), e);
         }
+
+        //Certain db engines store unique constraints as indices
+        final DotConnect dotConnect = new DotConnect();
+        try {
+           dotConnect.setSQL("alter table workflow_task drop index unique_workflow_task");
+           dotConnect.loadResult();
+       }catch (DotDataException e){
+           //Nah.
+       }
+        try {
+            dotConnect.setSQL("alter table workflow_task drop constraint unique_workflow_task");
+            dotConnect.loadResult();
+        }catch (DotDataException e){
+            //Nah.
+        }
+    }
+
+    @Test
+    public void Test_Upgrade_Task() throws DotDataException {
+        removeConstraintIfAny();
+        final Task05200WorkflowTaskUniqueKey task =  new Task05200WorkflowTaskUniqueKey();
+        assertTrue(task.forceRun());
+        task.executeUpgrade();
     }
 
 }
