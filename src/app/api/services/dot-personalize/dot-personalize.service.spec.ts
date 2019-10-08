0447ff0133739f2970bc00750920fb363745bdf6
alfredo-dotcms <37185433+alfredo-dotcms@users.noreply.github.com>
2019-10-08 10:18:49 -0600

    Fix DotPersonalizeService tests

--- src/app/api/services/dot-personalize/dot-personalize.service.spec.ts
+++ src/app/api/services/dot-personalize/dot-personalize.service.spec.ts
@@ -1,12 +1,46 @@
-import { TestBed } from '@angular/core/testing';
-
 import { DotPersonalizeService } from './dot-personalize.service';
+import { DOTTestBed } from '../../../test/dot-test-bed';
+import { ConnectionBackend, ResponseOptions, Response, RequestMethod } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
 describe('DotPersonalizeService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    beforeEach(() => {
+        this.injector = DOTTestBed.resolveAndCreate([DotPersonalizeService]);
+        this.dotPersonalizeService = this.injector.get(DotPersonalizeService);
+        this.backend = this.injector.get(ConnectionBackend) as MockBackend;
+        this.backend.connections.subscribe((connection: any) => (this.lastConnection = connection));
+    });
 
-  it('should be created', () => {
-    const service: DotPersonalizeService = TestBed.get(DotPersonalizeService);
-    expect(service).toBeTruthy();
-  });
+    it('should set Personalized', () => {
+        this.dotPersonalizeService.personalized('a', 'b').subscribe();
+
+        this.lastConnection.mockRespond(
+            new Response(
+                new ResponseOptions({
+                    body: []
+                })
+            )
+        );
+        expect(this.lastConnection.request.method).toBe(RequestMethod.Post);
+        expect(this.lastConnection.request.url).toContain('/api/v1/personalization/pagepersonas');
+        expect(this.lastConnection.request._body).toEqual({ pageId: 'a', personaTag: 'b' });
+    });
+
+    it('should despersonalized', () => {
+        const pageId = 'a';
+        const personaTag = 'b';
+        this.dotPersonalizeService.despersonalized(pageId, personaTag).subscribe();
+
+        this.lastConnection.mockRespond(
+            new Response(
+                new ResponseOptions({
+                    body: []
+                })
+            )
+        );
+        expect(this.lastConnection.request.method).toBe(RequestMethod.Delete);
+        expect(this.lastConnection.request.url).toContain(
+            `/api/v1/personalization/pagepersonas/page/${pageId}/personalization/${personaTag}`
+        );
+    });
 });
