0deb6e88de33f38896a9a22fc39fa15129967750
hmoreras <31667212+hmoreras@users.noreply.github.com>
2019-10-14 13:38:58 -0500

    Fix DotPersonaSelectedItemComponent tests

--- src/app/view/components/dot-persona-selected-item/dot-persona-selected-item.component.spec.ts
+++ src/app/view/components/dot-persona-selected-item/dot-persona-selected-item.component.spec.ts
@@ -7,9 +7,20 @@ import { DotIconModule } from '@components/_common/dot-icon/dot-icon.module';
 import { DotAvatarModule } from '@components/_common/dot-avatar/dot-avatar.module';
 import { By } from '@angular/platform-browser';
 import { mockDotPersona } from '@tests/dot-persona.mock';
+import { TooltipModule } from 'primeng/primeng';
+import { LoginService } from 'dotcms-js';
+import { LoginServiceMock } from '@tests/login-service.mock';
+import { MockDotMessageService } from '@tests/dot-message-service.mock';
+import { DotMessageService } from '@services/dot-messages-service';
+
+const messageServiceMock = new MockDotMessageService({
+    'modes.persona.selector.title.preview': 'Previewing As',
+    'modes.persona.selector.title.edit': 'Personalize As',
+    'modes.persona.no.persona': 'Default Visitor',
+    'editpage.personalization.content.add.message': 'Add content...'
+});
 
 describe('DotPersonaSelectedItemComponent', () => {
-
     let component: DotPersonaSelectedItemComponent;
     let fixture: ComponentFixture<DotPersonaSelectedItemComponent>;
     let de: DebugElement;
@@ -17,7 +28,17 @@ describe('DotPersonaSelectedItemComponent', () => {
     beforeEach(() => {
         DOTTestBed.configureTestingModule({
             declarations: [DotPersonaSelectedItemComponent],
-            imports: [BrowserAnimationsModule, DotIconModule, DotAvatarModule],
+            providers: [
+                {
+                    provide: LoginService,
+                    useClass: LoginServiceMock
+                },
+                {
+                    provide: DotMessageService,
+                    useValue: messageServiceMock
+                }
+            ],
+            imports: [BrowserAnimationsModule, DotIconModule, DotAvatarModule, TooltipModule]
         });
 
         fixture = DOTTestBed.createComponent(DotPersonaSelectedItemComponent);
@@ -38,7 +59,25 @@ describe('DotPersonaSelectedItemComponent', () => {
     it('should render persona name and label', () => {
         const label = de.query(By.css('.dot-persona-selector__label')).nativeElement;
         const name = de.query(By.css('.dot-persona-selector__name')).nativeElement;
-        expect(label.innerText).toBe('Titulo');
+        expect(label.innerText).toBe('Previewing As');
         expect(name.innerText).toBe('Global Investor');
+    });
+
+    describe('tooltip properties', () => {
+        let container: HTMLDivElement;
+
+        it('should set properties to null when enable', () => {
+            container = de.query(By.css('.dot-persona-selector__container')).nativeElement;
+            expect(container.getAttribute('ng-reflect-tooltip-position')).toEqual(null);
+            expect(container.getAttribute('ng-reflect-text')).toEqual(null);
+        });
+
+        it('should set properties correctly when disable', () => {
+            component.disabled = true;
+            fixture.detectChanges();
+            container = de.query(By.css('.dot-persona-selector__container')).nativeElement;
+            expect(container.getAttribute('ng-reflect-tooltip-position')).toEqual('bottom');
+            expect(container.getAttribute('ng-reflect-text')).toEqual('Add content...');
+        });
     });
 });
