dff89a81865dd32883e12adc226ee6519a88c491
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2019-10-22 12:47:26 -0600

    #17486 makeing update transactional + adding a test

--- dotCMS/src/integration-test/java/com/dotcms/datagen/CompanyDataGen.java
+++ dotCMS/src/integration-test/java/com/dotcms/datagen/CompanyDataGen.java
@@ -1,5 +1,156 @@
 package com.dotcms.datagen;
 
-public class CompanyDataGen {
+import com.dotmarketing.cms.factories.PublicCompanyFactory;
+import com.liferay.portal.SystemException;
+import com.liferay.portal.model.Company;
 
+public class CompanyDataGen extends AbstractDataGen<Company> {
+
+    private String key;
+    private String portalURL;
+    private String homeURL;
+    private String mx;
+    private String name;
+    private String shortName;
+    private String type;
+    private String size;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String phone;
+    private String fax;
+    private String emailAddress;
+    private String authType;
+    private boolean autoLogin;
+    private boolean strangers;
+
+    public CompanyDataGen key(String key) {
+        this.key = key;
+        return this;
+    }
+
+    public CompanyDataGen portalURL(String portalURL) {
+        this.portalURL = portalURL;
+        return this;
+    }
+
+    public CompanyDataGen homeURL(String homeURL) {
+        this.homeURL = homeURL;
+        return this;
+    }
+
+    public CompanyDataGen mx(String mx) {
+        this.mx = mx;
+        return this;
+    }
+
+    public CompanyDataGen name(String name) {
+        this.name = (name + System.currentTimeMillis());
+        return this;
+    }
+
+    public CompanyDataGen shortName(String shortName) {
+        this.shortName = shortName;
+        return this;
+    }
+
+    public CompanyDataGen type(String type) {
+        this.type = type;
+        return this;
+    }
+
+    public CompanyDataGen size(String size) {
+        this.size = size;
+        return this;
+    }
+
+    public CompanyDataGen street(String street) {
+        this.street = street;
+        return this;
+    }
+
+    public CompanyDataGen city(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public CompanyDataGen state(String state) {
+        this.state = state;
+        return this;
+    }
+
+    public CompanyDataGen zip(String zip) {
+        this.zip = zip;
+        return this;
+    }
+
+    public CompanyDataGen phone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    public CompanyDataGen fax(String fax) {
+        this.fax = fax;
+        return this;
+    }
+
+    public CompanyDataGen emailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+        return this;
+    }
+
+    public CompanyDataGen authType(String authType) {
+        this.authType = authType;
+        return this;
+    }
+
+    public CompanyDataGen autoLogin(boolean autoLogin) {
+        this.autoLogin = autoLogin;
+        return this;
+    }
+
+    public CompanyDataGen strangers(boolean strangers) {
+        this.strangers = strangers;
+        return this;
+    }
+
+    @Override
+    public Company next() {
+        final Company company = PublicCompanyFactory.create(name);
+        company.setHomeURL(homeURL);
+        company.setPortalURL(portalURL);
+        company.setMx(mx);
+        company.setName(name);
+        company.setShortName(shortName);
+        company.setKey(key);
+        company.setAuthType(authType);
+        company.setAutoLogin(autoLogin);
+        company.setCity(city);
+        company.setEmailAddress(emailAddress);
+        company.setFax(fax);
+        company.setPhone(phone);
+        company.setSize(size);
+        company.setState(state);
+        company.setStrangers(strangers);
+        company.setStreet(street);
+        company.setType(type);
+        company.setZip(zip);
+        return company;
+    }
+
+    @Override
+    public Company persist(Company object) {
+        try {
+            return PublicCompanyFactory.update(object);
+        } catch (SystemException e) {
+            throw new RuntimeException("Unable to persist company.", e);
+        }
+    }
+
+    @Override
+    public Company nextPersisted() {
+        final Company next = next();
+        return persist(next);
+    }
 }
--- dotCMS/src/integration-test/java/com/liferay/portal/ejb/CompanyUtilTest.java
+++ dotCMS/src/integration-test/java/com/liferay/portal/ejb/CompanyUtilTest.java
@@ -1,5 +1,119 @@
 package com.liferay.portal.ejb;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import com.dotcms.datagen.CompanyDataGen;
+import com.dotcms.util.IntegrationTestInitService;
+import com.dotmarketing.util.Logger;
+import com.liferay.portal.NoSuchCompanyException;
+import com.liferay.portal.SystemException;
+import com.liferay.portal.model.Company;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class CompanyUtilTest {
+
+    @BeforeClass
+    public static void prepare() throws Exception {
+
+        //Setting the test user
+        IntegrationTestInitService.getInstance().init();
+    }
+
+
+    @Test
+    public void Test_New_Company_Then_Update_Then_Verify_Changes_Are_Saved() {
+
+        final Company company = new CompanyDataGen()
+                .name("TestCompany")
+                .shortName("TC")
+                .authType("email")
+                .autoLogin(true)
+                .emailAddress("lol@dotCMS.com")
+                .homeURL("localhost")
+                .city("NYC")
+                .mx("MX")
+                .type("test")
+                .phone("5552368")
+                .portalURL("/portalURL")
+                .nextPersisted();
+        assertNotNull(company.getCompanyId());
+        try {
+            final Company retrievedCompany =  CompanyUtil.findByPrimaryKey(company.getCompanyId());
+            assertEquals(company.getCompanyId(), retrievedCompany.getCompanyId());
+
+            final String updatedName = "Test-Company.";
+            final String updatedShortName = "TC.";
+            final String updatedAuthType = "userID";
+            final boolean updatedAutoLogin = false;
+            final String updatedEmail = "lal@dotCMS.com";
+            final String updatedHomeURL = "127.0.0.1";
+            final String updatedCity = "SJ";
+            final String updatedMx = "mx";
+            final String updatedType = "RealWorld";
+            final String updatedPhone = "555-2368";
+            final String updatedPortalURL = "/portal-URL";
+
+            retrievedCompany.setName(updatedName);
+            retrievedCompany.setShortName(updatedShortName);
+            retrievedCompany.setAuthType(updatedAuthType);
+            retrievedCompany.setAutoLogin(updatedAutoLogin);
+            retrievedCompany.setEmailAddress(updatedEmail);
+            retrievedCompany.setHomeURL(updatedHomeURL);
+            retrievedCompany.setCity(updatedCity);
+            retrievedCompany.setMx(updatedMx);
+            retrievedCompany.setType(updatedType);
+            retrievedCompany.setPhone(updatedPhone);
+            retrievedCompany.setPortalURL(updatedPortalURL);
+
+            CompanyUtil.update(retrievedCompany);
+
+            final Company updatedCompany = CompanyUtil.findByPrimaryKey(company.getCompanyId());
+            assertEquals(updatedCompany.getCompanyId(), retrievedCompany.getCompanyId());
+
+            assertEquals(updatedName,updatedCompany.getName());
+            assertEquals(updatedShortName,updatedCompany.getShortName());
+            assertEquals(updatedAuthType,updatedCompany.getAuthType());
+            assertEquals(updatedAutoLogin,updatedCompany.getAutoLogin());
+            assertEquals(updatedEmail,updatedCompany.getEmailAddress());
+            assertEquals(updatedHomeURL,updatedCompany.getHomeURL());
+            assertEquals(updatedCity,updatedCompany.getCity());
+            assertEquals(updatedMx,updatedCompany.getMx());
+            assertEquals(updatedType,updatedCompany.getType());
+            assertEquals(updatedPhone,updatedCompany.getPhone());
+            assertEquals(updatedPortalURL,updatedCompany.getPortalURL());
+
+        } catch (SystemException | NoSuchCompanyException e) {
+            Logger.error(CompanyUtilTest.class, e);
+            fail("Fail updating company");
+        }
+    }
+
+
+    @Test(expected = NoSuchCompanyException.class)
+    public void Test_New_Company_Then_Remove_Then_Find_Expect_Exception()
+            throws SystemException, NoSuchCompanyException {
+
+        final Company company = new CompanyDataGen()
+                .name("TestCompany2")
+                .shortName("TC-2")
+                .authType("email")
+                .autoLogin(true)
+                .emailAddress("lol@dotCMS.com")
+                .homeURL("localhost")
+                .city("NYC")
+                .mx("MX")
+                .type("test")
+                .phone("5552368")
+                .portalURL("/portalURL")
+                .nextPersisted();
+        assertNotNull(company.getCompanyId());
+
+        CompanyUtil.remove(company.getCompanyId());
+        assertNotNull(CompanyUtil.findByPrimaryKey(company.getCompanyId()));
+    }
+
 
 }
