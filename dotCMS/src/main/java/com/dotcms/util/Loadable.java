851410f6011fa4d2b77192374b2463ce647324b8
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2019-10-31 10:49:55 -0600

    #adding doc

--- dotCMS/src/main/java/com/dotcms/util/Loadable.java
+++ dotCMS/src/main/java/com/dotcms/util/Loadable.java
@@ -1,9 +1,22 @@
 package com.dotcms.util;
 
+/**
+ * This interface is intended to model a pre-loadable state behavior
+ * The Loadable State behavior is intended to save in terms of having to do a recurrent effort required to change the state.
+ */
 public interface Loadable {
 
+    /**
+     * implement to ensure the state has been acquired after having called the `load` Method
+     * @return
+     */
     boolean isLoaded();
 
+    /**
+     * This method changes the state of the model and makes it hold values that require some effort.
+     * Once this method has been called any additional call performed on this method should do nothing.
+     * The state is loaded just once.
+     */
     void load();
 
 }
