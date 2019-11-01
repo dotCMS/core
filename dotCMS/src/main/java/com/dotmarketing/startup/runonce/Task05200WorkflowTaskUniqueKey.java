77e0ae5f5aad26d7253c8ddbd49faee7046cb0e9
Will Ezell <will@dotcms.com>
2019-11-01 17:20:00 -0400

    #17088 keeps the newest task

--- dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task05200WorkflowTaskUniqueKey.java
+++ dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task05200WorkflowTaskUniqueKey.java
@@ -7,7 +7,12 @@ import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotRuntimeException;
 import com.dotmarketing.startup.StartupTask;
 import com.dotmarketing.util.Logger;
+import io.vavr.control.Try;
 import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Task05200WorkflowTaskUniqueKey implements StartupTask {
 
@@ -36,6 +41,37 @@ public class Task05200WorkflowTaskUniqueKey implements StartupTask {
             throw new DotDataException(e.getMessage(), e);
         }
         final DotConnect dotConnect = new DotConnect();
+        
+        List<Map<String,Object>> results = dotConnect.setSQL("select webasset,language_id from workflow_task group by webasset,language_id having count(*)>1").loadObjectResults();
+        
+        for(final Map<String,Object> map : results) {
+            final String webAsset = (String) map.get("webasset");
+            final long lang = Try.of(()->(Long) map.get("language_id")).getOrElse(0l);
+            final List<String> deleteMes = dotConnect
+            .setSQL("select id from workflow_task where webasset=? and language_id=? order by mod_date desc")
+            .addParam(webAsset)
+            .addParam(lang)
+            .setStartRow(1)
+            .loadObjectResults()
+            .stream()
+            .map(r->(String)r.get("id")).collect(Collectors.toList());
+            
+            String subquery = "'" + String.join("','" , deleteMes) + "'";
+            
+            Logger.warn(this, "Found multiple workflow tasks for contentlet id:" + webAsset + " language: " + lang + ". Keeping the most recent workflow task");
+            
+            new DotConnect().setSQL("delete from workflow_comment where workflowtask_id in (" + subquery + ")" ).loadResult();
+
+            new DotConnect().setSQL("delete from workflow_history where workflowtask_id  in (" + subquery + ")" ).loadResult();
+        
+            new DotConnect().setSQL("delete from workflowtask_files where workflowtask_id in (" + subquery + ")" ).loadResult();
+        
+            new DotConnect().setSQL("delete from workflow_task where id in (" + subquery + ")" ).loadResult();
+
+
+        }
+        
+        
         try {
             dotConnect.executeStatement(ADD_CONSTRAINT_SQL);
         } catch (SQLException e) {
