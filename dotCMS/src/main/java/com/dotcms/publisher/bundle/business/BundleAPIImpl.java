f6a0b3d56a023d2f8922314e9a2cdf0ab1cd3997
Erick Gonzalez <erick.gonzalez@dotcms.com>
2019-11-26 12:54:21 -0600

    #17611 validation if user is admin or owner of bundle to delete

--- dotCMS/src/main/java/com/dotcms/publisher/bundle/business/BundleAPIImpl.java
+++ dotCMS/src/main/java/com/dotcms/publisher/bundle/business/BundleAPIImpl.java
@@ -154,21 +154,20 @@ public class BundleAPIImpl implements BundleAPI {
 
 			this.internalDeleteBundleAndDependencies(bundleId, user);
 		} else {
-
+			Logger.error(this,"The bundle id: " + bundleId + " does not exists");
 			throw new NotFoundInDbException("The bundle id: " + bundleId + " does not exists");
 		}
 	}
 
 	private void validateBundleDeletePermission(final User user, final String bundleId) throws DotDataException {
 
-		/**
-		if (!APILocator.getPermissionAPI().doesUserHavePermission(bundle,
-				PermissionAPI.PERMISSION_EDIT, user, false)) {
-
-			throw new DotSecurityException("User : " + user.getUserId() +
-					", is not allowed to delete the bundle: " + bundleId)
-		}*/
-		// 	todo: bundle is not a permissionable yet, so can not validate yet.
+		//This is meanwhile we make bundles permissionable, if the user is not an admin only the owner of the bundle can delete it
+		if(!userAPI.isCMSAdmin(user)){
+			if(!getBundleById(bundleId).getOwner().equals(user.getUserId())){
+				Logger.error(this,"User: " + user.getUserId() + " is not an admin or is not the owner of the bundle");
+				throw new DotDataException("User: " + user.getUserId() + " is not an admin or is not the owner of the bundle");
+			}
+		}
 	}
 
 	// no mark as a wrap in transaction, it is one trax per bundle to delete.
@@ -176,6 +175,7 @@ public class BundleAPIImpl implements BundleAPI {
 	public Set<String> deleteBundleAndDependenciesOlderThan(final Date olderThan, final User user) throws DotDataException {
 
 		if(olderThan.after(new Date())){
+			Logger.error(this,"To avoid deleting bundles that publish in the future, the date can not be after the current date");
 			throw new IllegalArgumentException("To avoid deleting bundles that publish in the future, the date can not be after the current date");
 		}
 
