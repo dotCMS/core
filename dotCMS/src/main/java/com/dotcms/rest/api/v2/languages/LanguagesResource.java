7e560fe7d31d5715a0354da940bcae9d321de7fb
Daniel Silva <daniel.silva@dotcms.com>
2019-11-27 17:38:31 -0600

    #17671 endpoint for creating lang. e2e test for success case

--- dotCMS/src/main/java/com/dotcms/rest/api/v2/languages/LanguagesResource.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v2/languages/LanguagesResource.java
@@ -1,11 +1,17 @@
 package com.dotcms.rest.api.v2.languages;
 
 import com.dotcms.repackage.com.google.common.annotations.VisibleForTesting;
+import com.dotcms.rest.api.v1.authentication.ResponseUtil;
+import com.dotcms.util.DotPreconditions;
+import com.dotmarketing.exception.DoesNotExistException;
+import com.dotmarketing.util.PortletID;
+import com.dotmarketing.util.StringUtils;
 import com.google.common.collect.ImmutableList;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.apache.commons.beanutils.BeanUtils;
 import org.glassfish.jersey.server.JSONP;
 import com.dotcms.rest.InitDataObject;
 import com.dotcms.rest.ResponseEntityView;
@@ -73,6 +79,34 @@ public class LanguagesResource {
         return Response.ok(new ResponseEntityView(ImmutableList.copyOf(languages))).build();
     }
 
+    /**
+     * Persists a new {@link Language}
+     *
+     * @param request HttpServletRequest
+     * @param languageForm LanguageForm
+     * @return Response
+     */
+    @POST
+    @JSONP
+    @NoCache
+    @Produces({MediaType.APPLICATION_JSON, "application/javascript"})
+    public final Response saveLanguage(@Context final HttpServletRequest request,
+            @Context final HttpServletResponse response,
+            final LanguageForm languageForm) {
+        this.webResource.init(null, request, response,
+                true, PortletID.LANGUAGES.toString());
+        try {
+            DotPreconditions.notNull(languageForm,"Expected Request body was empty.");
+            final Language language = saveOrUpdateLanguage(null, languageForm);
+            return Response.ok(new ResponseEntityView(language)).build(); // 200
+        } catch (Exception e) {
+            final String langCode = languageForm == null ? "" : languageForm.getLanguageCode();
+            Logger.error(this.getClass(), "Exception on save, Language code: " + langCode
+                    + ", exception message: " + e.getMessage(), e);
+            return ResponseUtil.mapExceptionResponse(e);
+        }
+    }
+
     @POST
     @JSONP
     @NoCache
@@ -82,5 +116,27 @@ public class LanguagesResource {
     public Response getMessages(@Context HttpServletRequest request,
                                 final I18NForm i18NForm) {
         return oldLanguagesResource.getMessages(request, i18NForm);
+    }
+
+    private Language saveOrUpdateLanguage(final String languageId, final LanguageForm form) {
+        final Language newLanguage = new Language();
+
+        if (StringUtils.isSet(languageId)) {
+            try {
+                final Language origLanguage = this.languageAPI.getLanguage(languageId);
+                BeanUtils.copyProperties(newLanguage, origLanguage);
+            } catch (Exception e) {
+                Logger.debug(this.getClass(), () -> "Unable to find language with id:" + languageId);
+                throw new DoesNotExistException(e.getMessage(), e);
+            }
+        }
+
+        newLanguage.setLanguageCode(form.getLanguageCode());
+        newLanguage.setLanguage(form.getLanguage());
+        newLanguage.setCountryCode(form.getCountryCode());
+        newLanguage.setCountry(form.getCountry());
+
+        this.languageAPI.saveLanguage(newLanguage);
+        return newLanguage;
     }
 }
