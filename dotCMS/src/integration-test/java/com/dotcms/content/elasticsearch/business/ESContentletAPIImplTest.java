27dc1bb49f603380b01d5c310b1392ff3f2bde4f
Nollymar Longa <nollymar.longa@dotcms.com>
2019-12-02 10:02:52 -0500

    #17601 A new validation was added to the check in process to verify if the ES cluster is in read only mode (#17608)
    * #17601 A new validation was added to the check in process to verify if the ES cluster is in read only mode
    
    * #17601 Implementing new ITs
    
    * #17601 New IT that covers the validation of the cluster state when a checkin is executed using legacy relationships
    
    * #17601 Javadoc for isClusterInReadOnlyMode()
    
    * #17601 Adding javadoc
    
    * #17601 Applying code review suggestions

--- dotCMS/src/integration-test/java/com/dotcms/content/elasticsearch/business/ESContentletAPIImplTest.java
+++ dotCMS/src/integration-test/java/com/dotcms/content/elasticsearch/business/ESContentletAPIImplTest.java
@@ -6,32 +6,50 @@ import static com.dotcms.datagen.TestDataUtils.relateContentTypes;
 import static com.dotcms.util.CollectionsUtils.list;
 import static com.dotcms.util.CollectionsUtils.map;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import com.dotcms.IntegrationTestBase;
 import com.dotcms.contenttype.business.ContentTypeAPI;
+import com.dotcms.contenttype.business.FieldAPI;
+import com.dotcms.contenttype.model.field.Field;
+import com.dotcms.contenttype.model.field.FieldBuilder;
+import com.dotcms.contenttype.model.field.RelationshipField;
 import com.dotcms.contenttype.model.type.ContentType;
+import com.dotcms.contenttype.model.type.ContentTypeBuilder;
+import com.dotcms.contenttype.model.type.SimpleContentType;
+import com.dotcms.contenttype.transform.contenttype.StructureTransformer;
 import com.dotcms.datagen.ContentletDataGen;
 import com.dotcms.datagen.TestDataUtils;
+import com.dotcms.util.CollectionsUtils;
 import com.dotcms.util.IntegrationTestInitService;
+import com.dotmarketing.beans.Host;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.business.CacheLocator;
 import com.dotmarketing.business.RelationshipAPI;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.portlets.contentlet.business.ContentletAPI;
+import com.dotmarketing.portlets.contentlet.business.DotContentletStateException;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.portlets.contentlet.model.IndexPolicy;
+import com.dotmarketing.portlets.folders.business.FolderAPI;
 import com.dotmarketing.portlets.languagesmanager.business.LanguageAPI;
 import com.dotmarketing.portlets.languagesmanager.model.Language;
+import com.dotmarketing.portlets.structure.model.ContentletRelationships;
+import com.dotmarketing.portlets.structure.model.ContentletRelationships.ContentletRelationshipRecords;
 import com.dotmarketing.portlets.structure.model.Relationship;
+import com.dotmarketing.portlets.structure.model.Structure;
 import com.dotmarketing.util.UtilMethods;
+import com.dotmarketing.util.WebKeys.Relationship.RELATIONSHIP_CARDINALITY;
 import com.liferay.portal.model.User;
+import com.liferay.util.StringPool;
 import java.util.Date;
 import java.util.List;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 /**
  * @author nollymar
@@ -42,6 +60,7 @@ public class ESContentletAPIImplTest extends IntegrationTestBase {
     private static LanguageAPI languageAPI;
     private static RelationshipAPI relationshipAPI;
     private static ContentletAPI contentletAPI;
+    private static FieldAPI fieldAPI;
     private static User user;
 
     @BeforeClass
@@ -57,6 +76,7 @@ public class ESContentletAPIImplTest extends IntegrationTestBase {
         languageAPI = APILocator.getLanguageAPI();
         relationshipAPI = APILocator.getRelationshipAPI();
         contentletAPI = APILocator.getContentletAPI();
+        fieldAPI = APILocator.getContentTypeFieldAPI();
     }
 
     @Test
@@ -159,5 +179,161 @@ public class ESContentletAPIImplTest extends IntegrationTestBase {
         }
     }
 
+    @Test(expected= DotContentletStateException.class)
+    public void testCheckInWithLegacyRelationshipsAndReadOnlyClusterShouldThrowAnException()
+            throws DotDataException, DotSecurityException {
+        final long time = System.currentTimeMillis();
+        ContentType contentType = null;
+        final ESContentletAPIImpl contentletAPIImpl = new ESContentletAPIImpl();
+
+        try {
+            contentType = createContentType("test" + time);
+
+            final Structure structure = new StructureTransformer(contentType).asStructure();
+
+            final Contentlet contentlet = new ContentletDataGen(contentType.id()).next();
+            final ContentletRelationships contentletRelationship = new ContentletRelationships(
+                    contentlet);
+
+            final Relationship relationship = new Relationship(structure, structure,
+                    "parent" + contentType.variable(), "child" + contentType.variable(),
+                    RELATIONSHIP_CARDINALITY.ONE_TO_MANY.ordinal(), false, false);
+
+            final ContentletRelationshipRecords relationshipsRecord = contentletRelationship.new ContentletRelationshipRecords(
+                    relationship,
+                    false);
+
+            contentletRelationship
+                    .setRelationshipsRecords(CollectionsUtils.list(relationshipsRecord));
+
+            final ESIndexAPI esIndexAPI = Mockito.mock(ESIndexAPI.class);
+
+            contentletAPIImpl.setEsIndexAPI(esIndexAPI);
+
+            Mockito.when(esIndexAPI.isClusterInReadOnlyMode()).thenReturn(true);
+
+            contentletAPIImpl.checkin(contentlet, contentletRelationship, null, null, user, false);
+
+        }finally{
+            if (contentType != null && contentType.id() != null){
+                contentTypeAPI.delete(contentType);
+            }
+        }
+    }
+
+    /**
+     * Test for isCheckInSafe method with legacy relationships. It should return false when the cluster
+     * is in read only mode
+     * @throws DotDataException
+     * @throws DotSecurityException
+     */
+    @Test
+    public void testIsCheckInSafeWithLegacyRelationshipsShouldReturnFalse()
+            throws DotDataException, DotSecurityException {
+        final long time = System.currentTimeMillis();
+        ContentType contentType = null;
+
+        try {
+            contentType = createContentType("test" + time);
+
+            final Structure structure = new StructureTransformer(contentType).asStructure();
+
+            final Contentlet contentlet = new ContentletDataGen(contentType.id()).next();
+            final ContentletRelationships contentletRelationship = new ContentletRelationships(
+                    contentlet);
+
+            final Relationship relationship = new Relationship(structure, structure,
+                    "parent" + contentType.variable(), "child" + contentType.variable(),
+                    RELATIONSHIP_CARDINALITY.ONE_TO_MANY.ordinal(), false, false);
+
+            final ContentletRelationshipRecords relationshipsRecord = contentletRelationship.new ContentletRelationshipRecords(
+                    relationship,
+                    false);
+
+            contentletRelationship
+                    .setRelationshipsRecords(CollectionsUtils.list(relationshipsRecord));
+
+            final ESIndexAPI esIndexAPI = Mockito.mock(ESIndexAPI.class);
+
+            Mockito.when(esIndexAPI.isClusterInReadOnlyMode()).thenReturn(true);
+
+            assertFalse(
+                    new ESContentletAPIImpl().isCheckInSafe(contentletRelationship, esIndexAPI));
+
+        }finally{
+            if (contentType != null && contentType.id() != null){
+                contentTypeAPI.delete(contentType);
+            }
+        }
+    }
+
+    /**
+     * Test for isCheckInSafe method with relationship fields. It should return true when the cluster
+     * is in read only mode
+     * @throws DotDataException
+     * @throws DotSecurityException
+     */
+    @Test
+    public void testIsCheckInSafeWithRelationshipsFieldsShouldReturnTrue()
+            throws DotDataException, DotSecurityException {
+        final long time = System.currentTimeMillis();
+        ContentType contentType = null;
+
+        try {
+            contentType = createContentType("test" + time);
+
+            final Contentlet contentlet = new ContentletDataGen(contentType.id()).next();
+            final ContentletRelationships contentletRelationship = new ContentletRelationships(
+                    contentlet);
+
+            Field field = FieldBuilder.builder(RelationshipField.class).name("newRel")
+                    .contentTypeId(contentType.id()).values(String.valueOf(RELATIONSHIP_CARDINALITY.ONE_TO_MANY.ordinal()))
+                    .relationType(contentType.variable()).build();
+
+            field = fieldAPI.save(field, user);
+
+            final String fullFieldVar = contentType.variable() + StringPool.PERIOD + field.variable();
+
+            final Relationship relationship = relationshipAPI.byTypeValue(fullFieldVar);
+
+            final ContentletRelationshipRecords relationshipsRecord = contentletRelationship.new ContentletRelationshipRecords(
+                    relationship,false);
+
+            contentletRelationship
+                    .setRelationshipsRecords(CollectionsUtils.list(relationshipsRecord));
+
+            final ESIndexAPI esIndexAPI = Mockito.mock(ESIndexAPI.class);
+
+            Mockito.when(esIndexAPI.isClusterInReadOnlyMode()).thenReturn(true);
+
+            assertTrue(
+                    new ESContentletAPIImpl().isCheckInSafe(contentletRelationship, esIndexAPI));
+
+        }finally{
+            if (contentType != null && contentType.id() != null){
+                contentTypeAPI.delete(contentType);
+            }
+        }
+    }
+
+    /**
+     * Test for isCheckInSafe method without relationships. It should return true no matter the cluster status
+     */
+    @Test
+    public void testIsCheckInSafeWithoutRelationshipsShouldReturnTrue() {
+        final ESIndexAPI esIndexAPI = Mockito.mock(ESIndexAPI.class);
+
+        Mockito.when(esIndexAPI.isClusterInReadOnlyMode()).thenReturn(true);
+
+        assertTrue(
+                new ESContentletAPIImpl().isCheckInSafe(null, esIndexAPI));
+    }
+
+
+    private ContentType createContentType(final String name) throws DotSecurityException, DotDataException {
+        return contentTypeAPI.save(ContentTypeBuilder.builder(SimpleContentType.class).folder(
+                FolderAPI.SYSTEM_FOLDER).host(Host.SYSTEM_HOST).name(name)
+                .owner(user.getUserId()).build());
+    }
 
 }
