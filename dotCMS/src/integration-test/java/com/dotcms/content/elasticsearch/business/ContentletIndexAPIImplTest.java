1d3f44a0727b499d7c9609a7af9d6babbb3e98fb
Nollymar Longa <>
2020-01-06 14:44:11 -0500

    #11552 Fixing tests

--- dotCMS/src/integration-test/java/com/dotcms/content/elasticsearch/business/ContentletIndexAPIImplTest.java
+++ dotCMS/src/integration-test/java/com/dotcms/content/elasticsearch/business/ContentletIndexAPIImplTest.java
@@ -1231,7 +1231,7 @@ public class ContentletIndexAPIImplTest extends IntegrationTestBase {
 
             //Build the index name
             String timestamp = String.valueOf(new Date().getTime());
-            workingIndex = IndexType.WORKING.getPrefix() + "_" + timestamp;
+            workingIndex = new ESIndexAPI().getIndexNameWithClusterIDPrefix(IndexType.WORKING.getPrefix() + "_" + timestamp);
 
             //Create a working index
             boolean result = indexAPI.createContentIndex(workingIndex);
