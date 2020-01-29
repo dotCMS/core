a96c01ba2f8bd0d503ea65b851ceb9885113a4da
Jonathan Gamba <jonathan.gamba@dotcms.com>
2020-01-29 08:18:37 -0600

    Adding Jackson code in order to allow importing of rules

--- dotCMS/src/main/java/com/dotmarketing/portlets/rules/parameter/ParameterDefinition.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/rules/parameter/ParameterDefinition.java
@@ -6,7 +6,13 @@ import com.dotmarketing.portlets.rules.exception.RuleEngineException;
 import com.dotmarketing.portlets.rules.model.ParameterModel;
 import com.dotmarketing.portlets.rules.parameter.display.Input;
 import com.dotmarketing.portlets.rules.parameter.type.DataType;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import org.apache.commons.lang.StringUtils;
+import org.elasticsearch.common.Nullable;
 
 public class ParameterDefinition<T extends DataType> {
 
@@ -58,5 +64,67 @@ public class ParameterDefinition<T extends DataType> {
     public void checkValid(ParameterModel model) throws InvalidRuleParameterException, RuleEngineException {
     	this.inputType.checkValid(model.getValue());
     }
-}
 
+    /**
+     * Utility type used to correctly read immutable object from JSON representation.
+     *
+     * @deprecated Do not use this type directly, it exists only for the <em>Jackson</em>-binding
+     * infrastructure
+     */
+    @Deprecated
+    @JsonDeserialize
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
+    @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE)
+    static final class Json {
+
+        @javax.annotation.Nullable
+        String key;
+        @javax.annotation.Nullable
+        String i18nBaseKey;
+        @javax.annotation.Nullable
+        String defaultValue;
+        @javax.annotation.Nullable
+        Input inputType;
+        @javax.annotation.Nullable
+        int priority;
+
+        @JsonProperty("key")
+        public void setKey(@Nullable String key) {
+            this.key = key;
+        }
+
+        @JsonProperty("i18nBaseKey")
+        public void setI18nBaseKey(@Nullable String i18nBaseKey) {
+            this.i18nBaseKey = i18nBaseKey;
+        }
+
+        @JsonProperty("defaultValue")
+        public void setDefaultValue(@Nullable String defaultValue) {
+            this.defaultValue = defaultValue;
+        }
+
+        @JsonProperty("inputType")
+        public void setInputType(@Nullable Input inputType) {
+            this.inputType = inputType;
+        }
+
+        @JsonProperty("priority")
+        public void setPriority(@Nullable int priority) {
+            this.priority = priority;
+        }
+
+    }
+
+    /**
+     * @param json A JSON-bindable data structure
+     * @deprecated Do not use this method directly, it exists only for the <em>Jackson</em>-binding
+     * infrastructure
+     */
+    @Deprecated
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    static ParameterDefinition fromJson(ParameterDefinition.Json json) {
+        return new ParameterDefinition(json.priority, json.key, json.i18nBaseKey, json.inputType,
+                json.defaultValue);
+    }
+
+}
--- dotCMS/src/main/java/com/dotmarketing/portlets/rules/parameter/display/Input.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/rules/parameter/display/Input.java
@@ -2,6 +2,12 @@ package com.dotmarketing.portlets.rules.parameter.display;
 
 import com.dotcms.rest.exception.InvalidRuleParameterException;
 import com.dotmarketing.portlets.rules.parameter.type.DataType;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.elasticsearch.common.Nullable;
 
 /**
  * @author Geoff M. Granum
@@ -32,5 +38,45 @@ public class Input<T extends DataType> {
     public void checkValid(String value)  throws InvalidRuleParameterException{
     	return;
     }
-}
 
+    /**
+     * Utility type used to correctly read immutable object from JSON representation.
+     *
+     * @deprecated Do not use this type directly, it exists only for the <em>Jackson</em>-binding
+     * infrastructure
+     */
+    @Deprecated
+    @JsonDeserialize
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
+    @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE)
+    static final class Json {
+
+        @javax.annotation.Nullable
+        String id;
+        @javax.annotation.Nullable
+        DataType dataType;
+
+        @JsonProperty("id")
+        public void setKey(@Nullable String id) {
+            this.id = id;
+        }
+
+        @JsonProperty("dataType")
+        public void setDataType(@Nullable DataType dataType) {
+            this.dataType = dataType;
+        }
+
+    }
+
+    /**
+     * @param json A JSON-bindable data structure
+     * @deprecated Do not use this method directly, it exists only for the <em>Jackson</em>-binding
+     * infrastructure
+     */
+    @Deprecated
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    static Input fromJson(Input.Json json) {
+        return new Input(json.id, json.dataType);
+    }
+
+}
