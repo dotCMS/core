2c47dc0caebb9964b92a7993eeb1ae3221b09630
Jose Castro <jose.castro@dotcms.com>
2020-02-11 13:29:37 -0600

    #17975 : Improving the error messages that come from the Push Now actionlet, and some minor code formatting and Javadoc.

--- dotCMS/src/main/java/com/dotmarketing/portlets/workflows/actionlet/PushNowActionlet.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/workflows/actionlet/PushNowActionlet.java
@@ -1,5 +1,10 @@
 package com.dotmarketing.portlets.workflows.actionlet;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 import com.dotcms.publisher.bundle.bean.Bundle;
 import com.dotcms.publisher.business.DotPublisherException;
 import com.dotcms.publisher.business.PublisherAPI;
@@ -15,23 +20,34 @@ import com.dotmarketing.portlets.workflows.model.WorkflowProcessor;
 import com.dotmarketing.util.Logger;
 import com.dotmarketing.util.UtilMethods;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
+/**
+ * This Workflow Actionlet allows users to Push Publish a piece of Content when executing a Workflow Action. It takes
+ * two parameters:
+ * <ol>
+ *     <li><b>Name of the Environment (String):</b> The name of the Push Publishing environment that will receive the
+ *     Contentlet.</li>
+ *     <li><b>Force the Push? true or false (String):</b> Type in true if the push will be forced. Otherwise, type in
+ *     false. Defaults to false.</li>
+ * </ol>
+ *
+ * @author Oscar Arrieta
+ * @version 3.2
+ * @since Mar 4, 2015
+ */
 public class PushNowActionlet extends WorkFlowActionlet {
 
     private static final long serialVersionUID = 1L;
+    private static final String ENVIRONMENT_DELIMITER = ",";
+    private static final String PARAM_ENVIRONMENT = "environment";
+    private static final String PARAM_FORCE_PUSH = "force";
+
     private PublisherAPI publisherAPI = PublisherAPI.getInstance();
 
     @Override
     public List<WorkflowActionletParameter> getParameters() {
-        List<WorkflowActionletParameter> params = new ArrayList<WorkflowActionletParameter>();
-
-        params.add(new WorkflowActionletParameter("environment", "Name of the Enviroment", "", true));
-        params.add(new WorkflowActionletParameter("force", "Force the Push? true or false", "false", true));
-
+        final List<WorkflowActionletParameter> params = new ArrayList<>();
+        params.add(new WorkflowActionletParameter(PARAM_ENVIRONMENT, "Name of the Environment", "", true));
+        params.add(new WorkflowActionletParameter(PARAM_FORCE_PUSH, "Force the Push? true or false", "false", true));
         return params;
     }
 
@@ -42,94 +58,78 @@ public class PushNowActionlet extends WorkFlowActionlet {
 
     @Override
     public String getHowTo() {
-        return "This actionlet will automatically publish the the content object to the specified enviroment(s). Multiple Environments can be separated by a comma";
+        return "This actionlet will automatically publish the the content object to the specified environment(s). Multiple Environments can be separated by a comma";
     }
 
-    public void executeAction(WorkflowProcessor processor, Map<String, WorkflowActionClassParameter> params)
+    @Override
+    public void executeAction(final WorkflowProcessor processor, final Map<String, WorkflowActionClassParameter> params)
             throws WorkflowActionFailureException {
+        final List<String> identifiers = new ArrayList<>();
+        final String environments = params.get(PARAM_ENVIRONMENT).getValue();
         try {
-            // Gets available languages
-            // List<Language> languages = languagesAPI.getLanguages();
-
-            Contentlet ref = processor.getContentlet();
-
-            boolean _contentPushNeverExpire = true;
-
-            boolean forcePush = ("true".equals(params.get("force").getValue())) ? true : false;
-            String envrions = params.get("environment").getValue();
-            if (envrions == null) {
-                Logger.error(this.getClass(), "There are no environments set to push to");
+            final Contentlet contentlet = processor.getContentlet();
+            final boolean forcePush = ("true".equals(params.get(PARAM_FORCE_PUSH).getValue())) ? Boolean.TRUE : Boolean.FALSE;
+            if (!UtilMethods.isSet(environments)) {
+                Logger.error(this, "There are no Push Publishing environments set to send the bundle.");
             }
-
-            String[] whereToSend = envrions.split(",");
-
-            List<Environment> envsToSendTo = new ArrayList<Environment>();
-            List<Environment> permissionedEnv = new ArrayList<Environment>();
-            List<Environment> finalEnvs = new ArrayList<Environment>();
-
-
+            final String[] whereToSend = environments.split(ENVIRONMENT_DELIMITER);
+            final List<Environment> envsToSendTo = new ArrayList<>();
+            final List<Environment> permissionedEnv = new ArrayList<>();
+            final List<Environment> finalEnvs = new ArrayList<>();
             // Lists of Environments to push to
-            for (String name : whereToSend) {
-                if (UtilMethods.isSet(name)) {
-                    name = name.trim();
-                    final Environment e = APILocator.getEnvironmentAPI().findEnvironmentByName(name);
-                    if (e != null) {
-
-                        envsToSendTo.add(e);
-                    }else{
-                        Logger.error(PushNowActionlet.class, "The Environment " + name + " does not exists");
+            for (String environmentName : whereToSend) {
+                if (UtilMethods.isSet(environmentName)) {
+                    environmentName = environmentName.trim();
+                    final Environment environment = APILocator.getEnvironmentAPI().findEnvironmentByName(environmentName);
+                    if (null != environment && UtilMethods.isSet(environment.getId())) {
+                        envsToSendTo.add(environment);
+                    } else {
+                        Logger.error(this, "The Environment '" + environmentName + "' does not exist.");
                     }
                 }
-
             }
-
-            if(envsToSendTo.isEmpty()){
-                throw new DotPublisherException("There are no enviroments to send the bundle");
+            if (envsToSendTo.isEmpty()) {
+                throw new WorkflowActionFailureException("There are no environments to send the bundle.");
             }
-
-
             // make sure the user has permissions to push
-            boolean isAdmin = APILocator.getUserAPI().isCMSAdmin(processor.getUser());
-            List<Role> roles = APILocator.getRoleAPI().loadRolesForUser(processor.getUser().getUserId(),true);
-            if(isAdmin){
-                List<Environment> app = APILocator.getEnvironmentAPI().findEnvironmentsWithServers();
-                for(Environment e:app)
+            final boolean isAdmin = APILocator.getUserAPI().isCMSAdmin(processor.getUser());
+            final List<Role> roleList = APILocator.getRoleAPI().loadRolesForUser(processor.getUser().getUserId(),true);
+            if (isAdmin) {
+                List<Environment> environmentList = APILocator.getEnvironmentAPI().findEnvironmentsWithServers();
+                for (final Environment e : environmentList) {
                     permissionedEnv.add(e);
-            }
-            else{
-                for(Role r: roles){
+                }
+            } else {
+                for (final Role role : roleList){
                     try {
-                        permissionedEnv.addAll(APILocator.getEnvironmentAPI().findEnvironmentsByRole(r.getId()));
-                    } catch (Exception e) {
-                        Logger.error(PushNowActionlet.class, e.getMessage());
+                        permissionedEnv.addAll(APILocator.getEnvironmentAPI().findEnvironmentsByRole(role.getId()));
+                    } catch (final Exception e) {
+                        Logger.error(this, String.format("An error occurred when verifying Role '%s' [%s]: %s", role
+                                .getName(), role.getId(), e.getMessage()));
                     }
                 }
             }
-            for(Environment e : envsToSendTo){
-                if(permissionedEnv.contains(e)){
-                    finalEnvs.add(e);
+            for (final Environment environment : envsToSendTo) {
+                if (permissionedEnv.contains(environment)) {
+                    finalEnvs.add(environment);
                 }
             }
-
-            // publish now
-            Date publishDate = new Date();
-
-            List<String> identifiers = new ArrayList<String>();
-            identifiers.add(ref.getIdentifier());
-
-            Bundle bundle = new Bundle(null, publishDate, null, processor.getUser().getUserId(), forcePush);
+            // Publish now
+            final Date publishDate = new Date();
+            identifiers.add(contentlet.getIdentifier());
+            final Bundle bundle = new Bundle(null, publishDate, null, processor.getUser().getUserId(), forcePush);
             APILocator.getBundleAPI().saveBundle(bundle, finalEnvs);
-
             publisherAPI.addContentsToPublish(identifiers, bundle.getId(), publishDate, processor.getUser());
-
-        } catch (DotPublisherException e) {
-            Logger.debug(PushNowActionlet.class, e.getMessage());
-            throw new WorkflowActionFailureException(e.getMessage(),e);
-        } catch (DotDataException e) {
-            Logger.debug(PushNowActionlet.class, e.getMessage());
-            throw new WorkflowActionFailureException(e.getMessage(),e);
+        } catch (final DotPublisherException e) {
+            final String errorMsg = String.format("An error occurred when adding the Identifiers %s to the bundle " +
+                    "for Environments [%s]: %s", identifiers, environments, e.getMessage());
+            Logger.debug(this, errorMsg);
+            throw new WorkflowActionFailureException(errorMsg, e);
+        } catch (final DotDataException e) {
+            final String errorMsg = String.format("An error occurred when saving the bundle for Environments [%s]: %s", environments, e.getMessage());
+            Logger.debug(this, errorMsg);
+            throw new WorkflowActionFailureException(errorMsg, e);
         }
-
     }
 
 }
