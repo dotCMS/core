5995a4e5a899198783e87635eb822ab8a6ce697a
Will Ezell <will@dotcms.com>
2020-03-06 17:12:16 -0500

    dotCMS/core#18104 Show on List for Binarys does not allow for sorting

--- dotCMS/src/main/java/com/dotcms/contenttype/model/type/DotAssetContentType.java
+++ dotCMS/src/main/java/com/dotcms/contenttype/model/type/DotAssetContentType.java
@@ -56,7 +56,7 @@ public abstract class DotAssetContentType extends ContentType implements UrlMapa
 
 		fields.add(
 				ImmutableBinaryField.builder()
-				.name("Binary Asset")
+				.name("Asset")
 				.variable(ASSET_FIELD_VAR)
 				.sortOrder(fields.size())
 				.fixed(true)
