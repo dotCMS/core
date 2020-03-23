7c40e02b5d169422fdcd42c60de094005cc457e3
Nollymar Longa <>
2020-03-23 14:45:09 -0500

    #18068 Applying code review suggestions

--- dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task05205UpdateIndexNameLengthTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task05205UpdateIndexNameLengthTest.java
@@ -6,6 +6,7 @@ import com.dotcms.util.IntegrationTestInitService;
 import com.dotmarketing.common.db.DotConnect;
 import com.dotmarketing.db.DbConnectionFactory;
 import com.dotmarketing.exception.DotDataException;
+import java.sql.SQLException;
 import java.util.Map;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -18,7 +19,7 @@ public class Task05205UpdateIndexNameLengthTest {
         IntegrationTestInitService.getInstance().init();
     }
 
-    private Map<String, String> getColumnPropertiesAfterUpgrade() throws DotDataException {
+    private Map<String, String> getColumnProperties() throws DotDataException {
         String query;
 
         if (!DbConnectionFactory.getDBType().equals("Oracle")){
@@ -36,13 +37,58 @@ public class Task05205UpdateIndexNameLengthTest {
 
     }
 
+    private void setStateBeforeUpgrade() throws SQLException {
+        String query = null;
+
+        switch(DbConnectionFactory.getDBType()){
+            case "PostgreSQL":
+                query = "alter table indicies alter column index_name type varchar(30);";
+                break;
+
+            case "MySQL":
+                query = "alter table indicies modify index_name varchar(30);";
+                break;
+
+            case "Oracle":
+                query = "alter table indicies modify index_name varchar2(30);";
+                break;
+
+            case "Microsoft SQL Server":
+                query = "DECLARE @SQL VARCHAR(4000)\n"
+                        + "SET @SQL = 'ALTER TABLE dbo.Indicies DROP CONSTRAINT |ConstraintName| '\n"
+                        + "\n"
+                        + "SET @SQL = REPLACE(@SQL, '|ConstraintName|', ( SELECT   name\n"
+                        + "                                               FROM     sysobjects\n"
+                        + "                                               WHERE    xtype = 'PK'\n"
+                        + "                                                        AND parent_obj =        OBJECT_ID('Indicies')))\n"
+                        + "\n"
+                        + "EXEC (@SQL)\n"
+                        + "alter table indicies alter column index_name nvarchar(100) not null;\n"
+                        + "alter table Indicies add primary key (index_name);";
+                break;
+        }
+
+        final DotConnect dotConnect = new DotConnect();
+        dotConnect.executeStatement(query);
+    }
+
     @Test
-    public void testUpgradeTaskShouldPass() throws DotDataException {
+    public void testUpgradeTaskShouldPass() throws DotDataException, SQLException {
+
+        setStateBeforeUpgrade();
+
+        //Check values before upgrade task
+        Map<String, String> result = getColumnProperties();
+        assertEquals("30", result.get("field_length"));
+        assertEquals(DbConnectionFactory.getDBType().equals("Oracle") ? "N" : "NO",
+                result.get("nullable_value"));
+
         final Task05205UpdateIndexNameLength task = new Task05205UpdateIndexNameLength();
 
         task.executeUpgrade();
 
-        final Map<String, String> result = getColumnPropertiesAfterUpgrade();
+        //check values after upgrade task
+        result = getColumnProperties();
         assertEquals("100", result.get("field_length"));
         assertEquals(DbConnectionFactory.getDBType().equals("Oracle") ? "N" : "NO",
                 result.get("nullable_value"));
