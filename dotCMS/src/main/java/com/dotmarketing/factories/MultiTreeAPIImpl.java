c0ca630cf6a714211227648b9eef946ef7ae99ef
Jonathan <jonathan.sanchez@dotcms.com>
2020-03-23 16:41:54 -0600

    dotCMS/core#18158 Page API does not return contentlet json for "Advanced Templates"

--- dotCMS/src/main/java/com/dotmarketing/factories/MultiTreeAPIImpl.java
+++ dotCMS/src/main/java/com/dotmarketing/factories/MultiTreeAPIImpl.java
@@ -5,6 +5,7 @@ import com.dotcms.api.web.HttpServletRequestThreadLocal;
 import com.dotcms.business.CloseDBIfOpened;
 import com.dotcms.business.WrapInTransaction;
 import com.dotcms.contenttype.exception.NotFoundInDbException;
+import com.dotcms.rendering.velocity.directive.ParseContainer;
 import com.dotcms.rendering.velocity.services.PageLoader;
 import com.dotcms.rendering.velocity.viewtools.DotTemplateTool;
 import com.dotcms.util.transform.TransformerLocator;
@@ -898,12 +899,14 @@ public class MultiTreeAPIImpl implements MultiTreeAPI {
      * @param container container
      * @return true in case of the containerUUId is contains in pageContents
      */
-    private boolean doesPageContentsHaveContainer(
+    protected boolean doesPageContentsHaveContainer(
             final Table<String, String, Set<PersonalizedContentlet>> pageContents,
             final ContainerUUID containerUUID,
             final Container container) {
 
         if(pageContents.contains(container.getIdentifier(), containerUUID.getUUID())){
+            return true;
+        } else if(pageContents.contains(container.getIdentifier(), ParseContainer.PARSE_CONTAINER_UUID_PREFIX + containerUUID.getUUID())) {
             return true;
         } else if (ContainerUUID.UUID_LEGACY_VALUE.equals(containerUUID.getUUID())) {
             boolean pageContenstContains = pageContents.contains(containerUUID.getIdentifier(), ContainerUUID.UUID_START_VALUE);
