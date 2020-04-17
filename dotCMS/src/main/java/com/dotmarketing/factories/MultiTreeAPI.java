ac2da777b61b12b08d5444f6709870e0cc754078
Jonathan <jonathan.sanchez@dotcms.com>
2020-04-17 14:16:04 -0600

    dotCMS/core#18021 [Content] : Container not behaving correctly when reaching max contents
    * #18021 added a logic to handle DEFAULT_CONTENT_TO_DEFAULT_LANGUAGE on the container content
    
    * #18021 fixing unit test
    
    * #18021 fixing the language when requesting a page
    
    * #now the contentlets when is edit or preview mode, are be taken from a diff variable
    
    * #18021 changes to avoid deleting contentlets on diff languages when another a content to the multi tree in another lang
    
    * #adding the max content logic to the page render util instead of the multitree
    
    * #18021 fix for personalization per language
    
    * #18021 fixing unit test
    
    * #18021 unit test fixes
    
    * #18021 unit test fixes

--- dotCMS/src/main/java/com/dotmarketing/factories/MultiTreeAPI.java
+++ dotCMS/src/main/java/com/dotmarketing/factories/MultiTreeAPI.java
@@ -9,6 +9,7 @@ import com.dotmarketing.portlets.htmlpageasset.model.IHTMLPage;
 import com.google.common.collect.Table;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
 
@@ -360,6 +361,22 @@ public interface MultiTreeAPI {
      * @throws DotDataException
      */
     void overridesMultitreesByPersonalization(String pageId, String personalization, List<MultiTree> multiTrees)  throws DotDataException ;
+
+    /**
+     * Save a collection of {@link MultiTree} and link them with a page, Also delete all the
+     * {@link MultiTree} linked previously with the page.
+     *
+     * @param pageId {@link String} Page's identifier
+     * @param personalization {@link String} personalization token
+     * @param multiTrees {@link List} of {@link MultiTree} to safe
+     * @param languageIdOpt {@link Optional} {@link Long}  optional language, if present will deletes only the contentlets that have a version on this language.
+     *                                      Since it is by identifier, when deleting for instance in spanish, will remove the english and any other lang version too.
+     * @throws DotDataException
+     */
+    void overridesMultitreesByPersonalization(final String pageId,
+                                                     final String personalization,
+                                                     final List<MultiTree> multiTrees,
+                                                     final Optional<Long> languageIdOpt) throws DotDataException;
 
     /**
      * Updates the current personalization to a new personalization
