6cfb47f448e0b1826d16090f106e38af15579e4a
Freddy Rodriguez <freddy0309@gmail.com>
2020-04-20 17:03:19 -0600

    #18309 Owner user should have permission to edit contentlet (#18313)
    * #18309 Owner user should have permission to edit contentlet
    
    * #18309 When a contentlet is not lock you not have to lock it
    
    * refactoring
    
    * #18309 refactoring
    
    * #18292 Javadoc
    
    * #18292 Javadoc
    
    Co-authored-by: Freddy Rodriguez <freddyrodriguez@Freddys-MacBook-Pro.local>

---
+++ dotCMS/src/integration-test/java/com/dotmarketing/portlets/workflows/actionlet/CheckinContentActionletTest.java
@@ -0,0 +1,150 @@
+package com.dotmarketing.portlets.workflows.actionlet;
+
+import com.dotcms.contenttype.model.field.*;
+import com.dotcms.contenttype.model.type.ContentType;
+import com.dotcms.datagen.*;
+import com.dotcms.exception.ExceptionUtil;
+import com.dotmarketing.beans.Permission;
+import com.dotmarketing.business.APILocator;
+import com.dotmarketing.business.PermissionLevel;
+import com.dotmarketing.business.Permissionable;
+import com.dotmarketing.business.Role;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotSecurityException;
+import com.dotmarketing.portlets.contentlet.business.DotLockException;
+import com.dotmarketing.portlets.contentlet.model.Contentlet;
+import com.dotmarketing.portlets.contentlet.model.ContentletDependencies;
+import com.dotmarketing.portlets.contentlet.model.ContentletVersionInfo;
+import com.dotmarketing.portlets.workflows.model.WorkflowActionClassParameter;
+import com.dotmarketing.portlets.workflows.model.WorkflowActionFailureException;
+import com.dotmarketing.portlets.workflows.model.WorkflowProcessor;
+import com.google.common.collect.ImmutableList;
+import com.liferay.portal.model.User;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Map;
+
+import static com.dotcms.util.CollectionsUtils.map;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(DataProviderRunner.class)
+public class CheckinContentActionletTest {
+
+    private static class TestCase {
+        Contentlet contentlet;
+        User user;
+
+        boolean hasWritePermission;
+
+        public TestCase(Contentlet contentlet, User user, boolean hasWritePermission) {
+            this.contentlet = contentlet;
+            this.user = user;
+            this.hasWritePermission = hasWritePermission;
+        }
+    }
+
+    @DataProvider
+    public static Object[] dataProviderSaveLanguage() throws DotSecurityException, DotDataException {
+        final ContentType contentType = new ContentTypeDataGen()
+                .fields(ImmutableList.of(ImmutableTextField.builder().name("Name").variable("name").build()))
+                .nextPersisted();
+
+        final Contentlet notLockContentlet = new ContentletDataGen(contentType.id())
+                .setProperty("name", "testName")
+                .nextPersisted();
+        final User limitedUser = new UserDataGen().next();
+
+        final Contentlet lockContentlet = new ContentletDataGen(contentType.id())
+                .setProperty("name", "testName")
+                .nextPersisted();
+
+        final User userWithPermission = createUserWithPermission(lockContentlet);
+        APILocator.getContentletAPI().lock(lockContentlet, userWithPermission, false);
+
+        return new TestCase[]{
+                new TestCase(notLockContentlet, limitedUser, false),
+                new TestCase(lockContentlet, limitedUser, false),
+                new TestCase(notLockContentlet, userWithPermission, true)
+        };
+    }
+
+    private static User createUserWithPermission(final Contentlet lockContentlet) throws DotDataException {
+        final Role backEndUserRole = APILocator.getRoleAPI().loadBackEndUserRole();
+        final Role role = new RoleDataGen().nextPersisted();
+        final User userWithPermission = new UserDataGen().roles(role, backEndUserRole).nextPersisted();
+        addWritePermissionToContentlet(role, lockContentlet);
+        return userWithPermission;
+    }
+
+    private static void addWritePermissionToContentlet(final Role role, final Contentlet contentlet) throws DotDataException {
+        final Permission permission = getPermission(role, contentlet, PermissionLevel.EDIT.getType());
+
+        try {
+            APILocator.getPermissionAPI().save(permission, contentlet, APILocator.systemUser(), false);
+
+        } catch (DotDataException | DotSecurityException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Permission getPermission(
+            final Role role,
+            final Permissionable permissionable,
+            final int permissionLevel) {
+
+        final Permission permission = new Permission();
+        permission.setInode(permissionable.getPermissionId());
+        permission.setRoleId(role.getId());
+        permission.setPermission(permissionLevel);
+        return permission;
+    }
+
+    /**
+     * Method to test: {@link CheckinContentActionlet#executeAction(WorkflowProcessor, Map)}
+     * Given Scenario: User without WRITE permission over the {@link Contentlet} try to unlock a not lock contentlet
+     * Expected Result: should do anything
+     *
+     * @throws DotSecurityException
+     * @throws DotDataException
+     */
+    @Test
+    @UseDataProvider("dataProviderSaveLanguage")
+    public void tryToUnlockContentlet (final TestCase testCase) throws DotSecurityException, DotDataException {
+
+        final WorkflowProcessor workflowProcessor = mock(WorkflowProcessor.class);
+        final Map<String, WorkflowActionClassParameter> params = map();
+
+        final ContentletDependencies contentletDependencies = mock(ContentletDependencies.class);
+
+        when(workflowProcessor.getContentlet()).thenReturn(testCase.contentlet);
+        when(workflowProcessor.getUser()).thenReturn(testCase.user);
+        when(workflowProcessor.getContentletDependencies()).thenReturn(contentletDependencies);
+        when(contentletDependencies.isRespectAnonymousPermissions()).thenReturn(false);
+
+        final CheckinContentActionlet checkinContentActionlet = new CheckinContentActionlet();
+
+        try {
+            final boolean locked = testCase.contentlet.isLocked();
+            checkinContentActionlet.executeAction(workflowProcessor, params);
+            assertTrue(testCase.hasWritePermission || !locked);
+
+            final ContentletVersionInfo info = APILocator.getVersionableAPI().
+                    getContentletVersionInfo(testCase.contentlet.getIdentifier(), testCase.contentlet.getLanguageId());
+
+            assertNull(info.getLockedBy());
+        } catch (WorkflowActionFailureException e) {
+            if (ExceptionUtil.causedBy(e, DotLockException.class)) {
+                assertTrue(!testCase.hasWritePermission && testCase.contentlet.isLocked());
+            } else {
+                throw e;
+            }
+        }
+    }
+
+}
