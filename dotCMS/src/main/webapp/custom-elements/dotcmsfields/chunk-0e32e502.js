89defd59eb5cdf4494612bd62e87102189f55f5e
Freddy Montes <freddy@dotcms.com>
2020-04-21 13:27:26 -0600

    dotCMS/core#18288 Replace dotcms-field-elements with dotcms-webcomponents

--- dotCMS/src/main/webapp/custom-elements/dotcmsfields/chunk-0e32e502.js
+++
@@ -1,1 +0,0 @@
-var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{};function o(e,o){return e(o={exports:{}},o.exports),o.exports}export{o as a,e as b};
