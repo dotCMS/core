ee59687d4c72848248572ea1d8abb08ef51683b9
Will Ezell <will@dotcms.com>
2020-04-23 16:05:26 -0400

    #18325 more tests

--- dotCMS/src/main/java/com/dotmarketing/filters/VanityURLFilter.java
+++ dotCMS/src/main/java/com/dotmarketing/filters/VanityURLFilter.java
@@ -21,6 +21,7 @@ import com.dotmarketing.business.web.WebAPILocator;
 
 /**
  * This Filter handles the vanity url logic
+ * 
  * @author Jonathan Gamba 7/27/17
  */
 // todo: change this to an interceptor
@@ -30,65 +31,63 @@ public class VanityURLFilter implements Filter {
     private final CMSUrlUtil urlUtil;
     private final HostWebAPI hostWebAPI;
     private final LanguageWebAPI languageWebAPI;
-    private final UserWebAPI  userWebAPI;
+    private final UserWebAPI userWebAPI;
 
     public VanityURLFilter() {
 
-        this (VanityUrlHandlerResolver.getInstance(), CMSUrlUtil.getInstance(),
-                WebAPILocator.getHostWebAPI(), WebAPILocator.getLanguageWebAPI(),
-                WebAPILocator.getUserWebAPI());
+        this(VanityUrlHandlerResolver.getInstance(), CMSUrlUtil.getInstance(), WebAPILocator.getHostWebAPI(),
+                        WebAPILocator.getLanguageWebAPI(), WebAPILocator.getUserWebAPI());
     }
 
     @VisibleForTesting
-    protected VanityURLFilter(final VanityUrlHandlerResolver vanityUrlHandlerResolver,
-                           final CMSUrlUtil urlUtil,
-                           final HostWebAPI hostWebAPI,
-                           final LanguageWebAPI languageWebAPI,
-                           final UserWebAPI  userWebAPI) {
+    protected VanityURLFilter(final VanityUrlHandlerResolver vanityUrlHandlerResolver, final CMSUrlUtil urlUtil,
+                    final HostWebAPI hostWebAPI, final LanguageWebAPI languageWebAPI, final UserWebAPI userWebAPI) {
 
         this.vanityUrlHandlerResolver = vanityUrlHandlerResolver;
-        this.urlUtil                  = urlUtil;
-        this.hostWebAPI               = hostWebAPI;
-        this.languageWebAPI           = languageWebAPI;
-        this.userWebAPI               = userWebAPI;
+        this.urlUtil = urlUtil;
+        this.hostWebAPI = hostWebAPI;
+        this.languageWebAPI = languageWebAPI;
+        this.userWebAPI = userWebAPI;
     }
 
-    public void init(FilterConfig filterConfig) throws ServletException {
-    }
+    public void init(FilterConfig filterConfig) throws ServletException {}
 
-    public void doFilter(final ServletRequest req, final ServletResponse res,
-            final FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(final ServletRequest req, final ServletResponse res, final FilterChain filterChain)
+                    throws IOException, ServletException {
 
-        final HttpServletRequest  request  = (HttpServletRequest)  req;
+        final HttpServletRequest request = (HttpServletRequest) req;
         final HttpServletResponse response = (HttpServletResponse) res;
 
-        //Get the URI from the request and check for possible XSS hacks
-        final String uri         = this.urlUtil.getURIFromRequest(request);
-        final boolean isFiltered = this.urlUtil.isVanityUrlFiltered (uri);
-        //Getting the site form the request
-        final Host site          = WebAPILocator.getHostWebAPI().getCurrentHostNoThrow(request);
+        // Get the URI from the request and check for possible XSS hacks
+        final String uri = this.urlUtil.getURIFromRequest(request);
+        
+        final boolean isFiltered = this.urlUtil.isVanityUrlFiltered(uri);
+        
+        // Getting the site form the request
+        final Host site = hostWebAPI.getCurrentHostNoThrow(request);
 
-        if (!isFiltered) {
-
-            //Get the user language
+        // do not run again if the filter has been run
+        if (!isFiltered && request.getAttribute(Constants.VANITY_URL_HAS_RUN) == null) {
+            request.setAttribute(Constants.VANITY_URL_HAS_RUN, true);
+            // Get the user language
             final long languageId = this.languageWebAPI.getLanguage(request).getId();
 
-            //Verify if the given URI is a VanityURL
+            // Verify if the given URI is a VanityURL
             if (this.urlUtil.isVanityUrl(uri, site, languageId)) {
 
-                //Find the Vanity URL handler and handle this given URI
+                // Find the Vanity URL handler and handle this given URI
                 final VanityUrlHandler vanityUrlHandler = this.vanityUrlHandlerResolver.getVanityUrlHandler();
-                final VanityUrlResult vanityUrlResult = vanityUrlHandler
-                        .handle(uri, response, site, languageId, this.userWebAPI.getUser(request));
+                final VanityUrlResult vanityUrlResult =
+                                vanityUrlHandler.handle(uri, response, site, languageId, this.userWebAPI.getUser(request));
 
-                //If the handler already resolved the requested URI we stop the processing here
+                // If the handler already resolved the requested URI we stop the processing here
                 if (vanityUrlResult.isResolved()) {
                     return;
                 }
 
-                filterChain.doFilter(new  VanityUrlRequestWrapper(request, vanityUrlResult) , response);
+                filterChain.doFilter(new VanityUrlRequestWrapper(request, vanityUrlResult), response);
                 return;
-           }
+            }
 
         }
 
@@ -96,13 +95,8 @@ public class VanityURLFilter implements Filter {
     } // doFilter.
 
 
-    public void destroy() {
-    }
-    
-    
-    
+    public void destroy() {}
 
-    
-    
+
 
 } // E:O:F:VanityURLFilter.
