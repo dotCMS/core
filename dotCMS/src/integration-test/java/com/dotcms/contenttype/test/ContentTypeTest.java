7bf746182fee4f7307f87812036ca17600c23c24
Will Ezell <will@dotcms.com>
2020-04-30 10:12:19 -0400

    #18397 better tests

--- dotCMS/src/integration-test/java/com/dotcms/contenttype/test/ContentTypeTest.java
+++ dotCMS/src/integration-test/java/com/dotcms/contenttype/test/ContentTypeTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import com.dotcms.contenttype.model.field.ConstantField;
 import com.dotcms.contenttype.model.field.Field;
 import com.dotcms.contenttype.model.field.ImmutableConstantField;
+import com.dotcms.contenttype.model.field.ImmutableTextField;
+import com.dotcms.contenttype.model.field.TextField;
 import com.dotcms.contenttype.model.type.ContentType;
 import com.dotcms.datagen.TestDataUtils;
 import io.vavr.control.Try;
@@ -14,73 +16,107 @@ import io.vavr.control.Try;
 public class ContentTypeTest extends ContentTypeBaseTest {
 
 
-    
-    
-	@Test
-	public void test_that_fields_of_type_are_returned() throws Exception {
-	    ContentType type = TestDataUtils.getFormLikeContentType();
-	    
-	    assert(!type.fields().isEmpty());
-	    assert(type.fields().size()==type.fieldMap().size());
-	    assert(!type.fields(ConstantField.class).isEmpty());
-	    assert(type.fields(ConstantField.class).size()==2);
 
-	}
+    /**
+     * This tests that calling type.fields() returns valid fields and that calling type.fields(Class
+     * clazz) returns a subset of those fields
+     * 
+     * @throws Exception
+     */
 
-	
+    @Test
+    public void test_that_fields_of_type_are_returned() throws Exception {
+        ContentType type = TestDataUtils.getFormLikeContentType();
+
+        assert (!type.fields().isEmpty());
+        assert (type.fields().size() == type.fieldMap().size());
+        assert (!type.fields(ConstantField.class).isEmpty());
+        assert (type.fields(ConstantField.class).size() == 2);
+
+    }
+
+    /**
+     * This tests that calling type.fields(TextField.class) returns the same subset of fields as does
+     * calling type.fields(ImmutableTextField.class)
+     * 
+     * @throws Exception
+     */
+
     @Test
     public void test_that_immutable_fields_work_the_same_as_normal_fields_returned() throws Exception {
-        
+
         ContentType type = TestDataUtils.getFormLikeContentType();
-        
-        assert(!type.fields().isEmpty());
-        assert(type.fields().size()==type.fieldMap().size());
-        
+
+        assert (!type.fields().isEmpty());
+        assert (type.fields().size() == type.fieldMap().size());
+
         List<Field> constants = type.fields(ConstantField.class);
         List<Field> immutableConstants = type.fields(ImmutableConstantField.class);
-        assert(!constants.isEmpty());
-        assert(!immutableConstants.isEmpty());
+        assert (!constants.isEmpty());
+        assert (!immutableConstants.isEmpty());
 
-        
-        
+
+
         // make sure that getting the field by Type or by its immutable type works the same
-        assert(constants.size() == immutableConstants.size());
-        assertEquals(constants,immutableConstants);
-        
+        assert (constants.size() == immutableConstants.size());
+        assertEquals(constants, immutableConstants);
+
     }
-	
-    
+
+
+
+    /**
+     * This tests that the changing of the code in the ContentType to return fields works the same as
+     * the old way we used to do it
+     * 
+     * @throws Exception
+     */
+
     @Test
     public void test_that_it_works_like_the_old_way() throws Exception {
-        ContentType type = TestDataUtils.getFormLikeContentType();
-        
-        assert(!type.fields().isEmpty());
-        assert(type.fields().size()==type.fieldMap().size());
-        assert(!type.fields(ConstantField.class).isEmpty());
-        
-        
-        List<Field> constantsNewWay = type.fields(ConstantField.class);
-        
-        List<Field> constantsOldWay = oldWayOfGettingFields(type, ConstantField.class);
-        
-        
-        // make sure that getting the field by Type or by its immutable type works the same
-        assert(constantsNewWay.size() >0);
-        assert(constantsOldWay.size() >0);
-        assertEquals(constantsNewWay,constantsOldWay);
-        
-        
-        
-    }
-	
-	
-	
-	
+        ContentType type = TestDataUtils.getWidgetLikeContentType();
 
-	  private List<Field> oldWayOfGettingFields(ContentType type, final Class<? extends Field> clazz) {
-	    return type.fields()
-	    .stream()
-	    .filter(field -> Try.of(()->field.getClass().asSubclass(clazz)!=null).getOrElse(false))
-	    .collect(Collectors.toList());
-	  }
+        assert (!type.fields().isEmpty());
+        assert (type.fields().size() == type.fieldMap().size());
+        assert (!type.fields(ConstantField.class).isEmpty());
+
+
+        List<Field> newWayfields = type.fields(ConstantField.class);
+
+        List<Field> oldWayFields = oldWayOfGettingFields(type, ConstantField.class);
+
+
+        // make sure that getting the field by Type or by its immutable type works the same
+        assert (oldWayFields.size() > 0);
+        assert (newWayfields.size() > 0);
+        assertEquals(oldWayFields, newWayfields);
+
+
+        newWayfields = type.fields(TextField.class);
+
+        oldWayFields = oldWayOfGettingFields(type, TextField.class);
+
+
+        // make sure that getting the field by Type or by its immutable type works the same
+        assert (oldWayFields.size() > 0);
+        assert (newWayfields.size() > 0);
+        assertEquals(newWayfields, oldWayFields);
+        
+        
+        // test that Immutables work the same way
+        oldWayFields = oldWayOfGettingFields(type, ImmutableTextField.class);
+        assertEquals(newWayfields, oldWayFields);
+        
+        
+
+    }
+
+
+
+    private List<Field> oldWayOfGettingFields(ContentType type, final Class<? extends Field> clazz) {
+        return type.fields()
+                .stream()
+                .filter(field -> Try.of(() -> field.getClass().asSubclass(clazz) != null).getOrElse(false))
+                .collect(Collectors.toList());
+    }
 }
