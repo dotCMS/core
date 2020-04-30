ccc681dfdd70ae371afcbc612b7f10e7d3d50af4
jdotcms <jonathan.sanchez@dotcms.com>
2020-04-30 11:06:57 -0600

    #18392 adding progress

---
+++ dotCMS/src/main/java/com/dotmarketing/portlets/contentlet/transform/SingleFolderToMapViewTransformer.java
@@ -0,0 +1,14 @@
+package com.dotmarketing.portlets.contentlet.transform;
+
+import java.util.Map;
+
+/**
+ *
+ * @author jsanca
+ */
+public class SingleFolderToMapViewTransformer implements FieldsToMapTransformer {
+    @Override
+    public Map<String, Object> asMap() {
+        return null;
+    }
+}
