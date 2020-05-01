7a17afe240bfedd223b24e040112837a9a4bd23a
jdotcms <jonathan.sanchez@dotcms.com>
2020-04-30 21:45:12 -0600

    #18407 this fix allows you to save not only file asset on the edit contentlet but also, dotAsset and any contentlet with binary field

--- dotCMS/src/main/java/com/dotmarketing/portlets/fileassets/ajax/FileAssetAjax.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/fileassets/ajax/FileAssetAjax.java
@@ -1,5 +1,6 @@
 package com.dotmarketing.portlets.fileassets.ajax;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -8,8 +9,10 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.dotcms.contenttype.model.type.DotAssetContentType;
 import com.dotcms.repackage.org.directwebremoting.WebContext;
 import com.dotcms.repackage.org.directwebremoting.WebContextFactory;
+import com.dotcms.rest.api.v1.temp.DotTempFile;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.business.web.UserWebAPI;
 import com.dotmarketing.business.web.WebAPILocator;
@@ -17,11 +20,14 @@ import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.portlets.fileassets.business.FileAsset;
+import com.dotmarketing.portlets.fileassets.business.FileAssetAPI;
 import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.UtilMethods;
 import com.dotmarketing.util.WebKeys;
 import com.liferay.portal.PortalException;
 import com.liferay.portal.SystemException;
 import com.liferay.portal.model.User;
+import org.apache.commons.io.IOUtils;
 
 public class FileAssetAjax {
 
@@ -56,41 +62,50 @@ public class FileAssetAjax {
 	}
 
 
-	public void saveFileText(String contentletInode, String newText, String binField) throws PortalException, SystemException,
-	DotDataException, DotSecurityException, IOException {
-		WebContext ctx = WebContextFactory.get();
-		HttpServletRequest req = ctx.getHttpServletRequest();
-		User user = userAPI.getLoggedInUser(req);
-		boolean respectFrontendRoles = userAPI.isLoggedToFrontend(req);
-		Contentlet cont  = APILocator.getContentletAPI().find(contentletInode, user, respectFrontendRoles);
-		FileAsset fa = APILocator.getFileAssetAPI().fromContentlet(cont);
+	public void saveFileText(final String contentletInode, final String newText, final String binField)
+			throws PortalException, SystemException, DotDataException, DotSecurityException, IOException {
 
-		java.io.File tempDir =  new java.io.File(APILocator.getFileAssetAPI().getRealAssetPathTmpBinary() + java.io.File.separator + contentletInode.charAt(0)
-					+ java.io.File.separator + contentletInode.charAt(1) + java.io.File.separator + contentletInode
-					+ java.io.File.separator + APILocator.getFileAssetAPI().BINARY_FIELD);
+		final WebContext webContext        = WebContextFactory.get();
+		final HttpServletRequest request   = webContext.getHttpServletRequest();
+		final User user 			       = userAPI.getLoggedInUser(request);
+		final boolean respectFrontendRoles = userAPI.isLoggedToFrontend(request);
+		final Contentlet contentlet        = APILocator.getContentletAPI().find(contentletInode, user, respectFrontendRoles);
+		String incomingFileName            = null;
+		String binaryFieldName             = binField;
 
-		if(!tempDir.exists())
+		if (contentlet.isFileAsset()) {
+
+			final FileAsset fileAsset = APILocator.getFileAssetAPI().fromContentlet(contentlet);
+			incomingFileName = fileAsset.getFileAsset().getName();
+			binaryFieldName  = FileAssetAPI.BINARY_FIELD;
+		} else if (contentlet.isDotAsset()) {
+
+			incomingFileName = contentlet.getBinary(DotAssetContentType.ASSET_FIELD_VAR).getName();
+			binaryFieldName  = DotAssetContentType.ASSET_FIELD_VAR;
+		} else {
+
+			incomingFileName = contentlet.getBinary(binField).getName();
+		}
+
+		final File tempDir =  new File(APILocator.getFileAssetAPI().getRealAssetPathTmpBinary() + File.separator + contentletInode.charAt(0)
+				+ File.separator + contentletInode.charAt(1) + File.separator + contentletInode
+				+ File.separator + binaryFieldName);
+
+		if(!tempDir.exists()) {
 			tempDir.mkdirs();
+		}
 
-		java.io.File fileData = new java.io.File(tempDir.getAbsoluteFile() + java.io.File.separator + WebKeys.TEMP_FILE_PREFIX + fa.getFileAsset().getName());
+		final File fileData = new File(tempDir.getAbsoluteFile() + File.separator + WebKeys.TEMP_FILE_PREFIX + incomingFileName);
 
 		fileData.deleteOnExit();
-		OutputStream os = null;
-		try {
-			os = Files.newOutputStream(fileData.toPath());
+
+		try (OutputStream os = Files.newOutputStream(fileData.toPath())) {
+
 			os.write(newText.getBytes());
 		} catch(Exception e) {
-			Logger.error(getClass(), "Error writing to file", e);
-		}finally {
-			if (os != null){
-				os.close();
-			}
 
+			Logger.error(getClass(), "Error writing to file", e);
 		}
 	}
-
-
-
-
 
 }
