8ddaafe1298e078ad4e719cf8443c8f92e069e92
freddyucv <freddy0309@gmail.com>
2020-05-07 14:03:30 -0600

    #18185 testing

--- dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task05210CreateDefaultDotAssetTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task05210CreateDefaultDotAssetTest.java
@@ -1,6 +1,7 @@
 package com.dotmarketing.startup.runonce;
 
 import com.dotcms.util.IntegrationTestInitService;
+import com.dotmarketing.business.FactoryLocator;
 import com.dotmarketing.common.db.DotConnect;
 import com.dotmarketing.db.DbConnectionFactory;
 import com.dotmarketing.exception.DotDataException;
@@ -8,6 +9,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertTrue;
 
@@ -46,11 +50,24 @@ public class Task05210CreateDefaultDotAssetTest {
             }
         }
 
+        final List<Map<String, Object>> workflowSchemeXStructure = new DotConnect().setSQL("select * from workflow_scheme_x_structure where structure_id = ?")
+                .addParam(Task05210CreateDefaultDotAsset.DOTASSET_VARIABLE_INODE)
+                .loadObjectResults();
+
+        new DotConnect().setSQL("delete from workflow_scheme_x_structure where structure_id = ?")
+                .addParam(Task05210CreateDefaultDotAsset.DOTASSET_VARIABLE_INODE).loadResult();
+
         new DotConnect().setSQL("delete from structure where inode = ?")
                 .addParam(Task05210CreateDefaultDotAsset.DOTASSET_VARIABLE_INODE).loadResult();
 
         new DotConnect().setSQL("delete from inode where inode = ?")
                 .addParam(Task05210CreateDefaultDotAsset.DOTASSET_VARIABLE_INODE).loadResult();
+
+        FactoryLocator.getWorkFlowFactory().saveSchemeIdsForContentType(
+                Task05210CreateDefaultDotAsset.DOTASSET_VARIABLE_INODE,
+                workflowSchemeXStructure.stream().map(register -> (String) register.get("scheme_id")).collect(Collectors.toSet()),
+                null
+        );
     }
 
     @Test
