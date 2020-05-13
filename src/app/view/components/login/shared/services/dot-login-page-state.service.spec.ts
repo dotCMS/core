3ae0ecb4ef166fd31f27f9c854e18e3db3ef57d9
hmoreras <31667212+hmoreras@users.noreply.github.com>
2020-05-13 10:49:28 -0600

    Fix Tests in Master (#1322)
    * check
    
    * fix error happening when running all tests

--- src/app/view/components/login/shared/services/dot-login-page-state.service.spec.ts
+++ src/app/view/components/login/shared/services/dot-login-page-state.service.spec.ts
@@ -1,4 +1,3 @@
-import { async } from '@angular/core/testing';
 import { DotLoginPageStateService, LOGIN_LABELS } from './dot-login-page-state.service';
 import { DOTTestBed } from '@tests/dot-test-bed';
 import { LoginService } from 'dotcms-js';
@@ -10,23 +9,21 @@ describe('DotLoginPageStateServiceService', () => {
     let dotloginPageStateService: DotLoginPageStateService;
     let loginService: LoginService;
 
-    beforeEach(
-        async(() => {
-            const testbed = DOTTestBed.configureTestingModule({
-                providers: [
-                    DotLoginPageStateService,
-                    { provide: LoginService, useClass: LoginServiceMock }
-                ],
-                imports: []
-            });
+    beforeEach(() => {
+        const testbed = DOTTestBed.configureTestingModule({
+            providers: [
+                DotLoginPageStateService,
+                { provide: LoginService, useClass: LoginServiceMock }
+            ],
+            imports: []
+        });
 
-            dotloginPageStateService = testbed.get(DotLoginPageStateService);
-            loginService = testbed.get(LoginService);
-            spyOn(loginService, 'getLoginFormInfo').and.returnValue(
-                of({ bodyJsonObject: mockLoginFormResponse })
-            );
-        })
-    );
+        dotloginPageStateService = testbed.get(DotLoginPageStateService);
+        loginService = testbed.get(LoginService);
+        spyOn(loginService, 'getLoginFormInfo').and.returnValue(
+            of({ bodyJsonObject: mockLoginFormResponse })
+        );
+    });
 
     it('should set new value to dotLoginInformation$ and call service correctly', () => {
         dotloginPageStateService.set('es_ES').subscribe();
