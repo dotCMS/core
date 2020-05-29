600374e0cd22e1af9f51459f481fc659f785a383
Will Ezell <will@dotcms.com>
2020-05-29 13:48:57 -0400

    #18567 support cors in graphql

--- dotCMS/src/main/java/com/dotcms/graphql/DotGraphQLHttpServlet.java
+++ dotCMS/src/main/java/com/dotcms/graphql/DotGraphQLHttpServlet.java
@@ -1,14 +1,26 @@
 package com.dotcms.graphql;
 
+import java.io.IOException;
 import java.util.Collections;
-
+import java.util.HashMap;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.dotcms.rest.api.CorsFilter;
+import com.dotmarketing.util.Config;
+import com.dotmarketing.util.UtilMethods;
 import graphql.servlet.GraphQLConfiguration;
 import graphql.servlet.GraphQLInvocationInputFactory;
 import graphql.servlet.GraphQLObjectMapper;
 import graphql.servlet.GraphQLQueryInvoker;
+import io.vavr.Function0;
 
 public class DotGraphQLHttpServlet extends AbstractGraphQLHttpServlet {
-
+    
+    
+    final private static String CORS_DEFAULT=CorsFilter.CORS_PREFIX + "." + CorsFilter.CORS_DEFAULT;
+    
+    final private static String CORS_GRAPHQL = CorsFilter.CORS_PREFIX + ".graphql";
     @Override
     protected GraphQLQueryInvoker getQueryInvoker() {
         throw new UnsupportedOperationException();
@@ -38,4 +50,57 @@ public class DotGraphQLHttpServlet extends AbstractGraphQLHttpServlet {
             .build();
     }
 
+    @Override
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        corsHeaders.apply().entrySet().stream().forEach(e -> response.setHeader(e.getKey(), e.getValue()));
+        super.doGet(request, response);
+    }
+
+    @Override
+    protected void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        corsHeaders.apply().entrySet().stream().forEach(e -> response.setHeader(e.getKey(), e.getValue()));
+        super.doPost(request, response);
+    }
+    
+    
+    /**
+     * header list is computed but once. It first reads all values set as default, e.g.
+     * api.cors.default.access-control-allow-headers that start with api.cors.default. It then overrides
+     * those with the specific ones for graphql, api.cors.graphql.access-control-allow-headers
+     * 
+     */
+    private final Function0<HashMap<String, String>> corsHeaders = Function0.of(() -> {
+
+        final HashMap<String, String> headers = new HashMap<>();
+        // load defaults
+        Config.subset(CORS_DEFAULT).forEachRemaining(k -> {
+            final String prop = Config.getStringProperty(CORS_DEFAULT + "." + k, null);
+            if (UtilMethods.isSet(prop)) {
+                headers.put(k.toLowerCase(), prop);
+            } 
+
+        });
+        // then override with graph
+        Config.subset(CORS_GRAPHQL).forEachRemaining(k -> {
+            final String prop = Config.getStringProperty(CORS_GRAPHQL + "." + k, null);
+            if (UtilMethods.isSet(prop)) {
+                headers.put(k.toLowerCase(), prop);
+            } else {
+                headers.remove(k.toLowerCase());
+            }
+
+        });
+
+
+
+        return headers;
+
+
+    }).memoized();
+    
+    
+    
+    
+    
+    
 }
