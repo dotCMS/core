3ad4bfdc04438d5bb9beb64081be991b12ddb062
jdotcms <jonathan.sanchez@dotcms.com>
2020-06-03 11:11:48 -0600

    #18299 adding new changes and feedback for the metadata

--- dotCMS/src/main/java/com/dotcms/storage/StorageProvider.java
+++ dotCMS/src/main/java/com/dotcms/storage/StorageProvider.java
@@ -5,16 +5,16 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class StorageProvider {
 
-    private final Map<String, Storage> fileSystemStorageMap = new ConcurrentHashMap<>();
+    private final Map<String, Storage> storageMap = new ConcurrentHashMap<>();
 
     public FileSystemStorage getFileSystemStorage() {
 
-        return (FileSystemStorage) this.fileSystemStorageMap.get(StorageType.FILE_SYSTEM.name());
+        return (FileSystemStorage) this.storageMap.get(StorageType.FILE_SYSTEM.name());
     }
 
     public DataBaseStorage getDbStorage() {
 
-        return (DataBaseStorage) this.fileSystemStorageMap.get(StorageType.DB.name());
+        return (DataBaseStorage) this.storageMap.get(StorageType.DB.name());
     }
 
     public Storage getStorage (final StorageType storageType) {
@@ -24,12 +24,12 @@ public class StorageProvider {
 
     public Storage getStorage (final String storageType) {
 
-        return this.fileSystemStorageMap.get(storageType);
+        return this.storageMap.get(storageType);
     }
 
     public void addStorage (final String storageType, final Storage storage) {
 
-        this.fileSystemStorageMap.put(storageType, storage);
+        this.storageMap.put(storageType, storage);
     }
 
     public void addStorage (final StorageType storageType, final Storage storage) {
