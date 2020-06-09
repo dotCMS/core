6d49a6eaeabc39cab963ad2529c01d793cdc29e6
Victor Alfaro <victor.alfaro@dotcms.com>
2020-06-09 16:23:30 -0600

    test

--- .cicd/seed.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export DOT_CICD_TARGET=core
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/dotCMS/dot-cicd/master/seed/install-dot-cicd.sh)"
--- travis/discover_cicd.sh
+++ travis/discover_cicd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export DOT_CICD_TARGET=core
+source .cicd/seed.source
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/dotCMS/dot-cicd/master/seed/install-dot-cicd.sh)"
+sh -c "`curl -fsSL https://raw.githubusercontent.com/dotCMS/dot-cicd/master/seed/install-dot-cicd.sh`"
