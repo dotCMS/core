afd6956c6caf1edd37e0224a0aa3874a12d2f7a8
Victor Alfaro <victor.alfaro@dotcms.com>
2020-06-10 16:25:18 -0600

    test

--- .cicd/discover.sh
+++ .cicd/discover.sh
@@ -3,4 +3,5 @@
 source .cicd/seed.source
 
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/dotCMS/dot-cicd/master/seed/install-dot-cicd.sh)"
+#TODO: restore to previous
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/dotCMS/dot-cicd/issue-18504-ci-github-actions/seed/install-dot-cicd.sh)"
