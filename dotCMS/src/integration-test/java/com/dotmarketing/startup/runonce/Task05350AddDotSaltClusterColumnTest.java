050a780b7961ca1c9f9274e2caf01ece12bb8e92
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2020-06-16 10:21:50 -0600

    #18381  small improvement on the test case

--- dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task05350AddDotSaltClusterColumnTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task05350AddDotSaltClusterColumnTest.java
@@ -1,5 +1,6 @@
 package com.dotmarketing.startup.runonce;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -8,6 +9,9 @@ import com.dotmarketing.common.db.DotConnect;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.util.Logger;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -19,10 +23,9 @@ public class Task05350AddDotSaltClusterColumnTest {
         IntegrationTestInitService.getInstance().init();
     }
 
-    private void dropColumn() throws SQLException {
+    private void dropColumn(final DotConnect dotConnect) throws SQLException {
         try {
             final String dropColumnSQL = "ALTER TABLE dot_cluster DROP COLUMN cluster_salt";
-            final DotConnect dotConnect = new DotConnect();
             dotConnect.executeStatement(dropColumnSQL);
         } catch (Exception e) {
             Logger.info(Task05350AddDotSaltClusterColumnTest.class, () -> "Failed removing cluster_salt column. Maybe it didn't exist?");
@@ -31,11 +34,19 @@ public class Task05350AddDotSaltClusterColumnTest {
 
     @Test
     public void test_upgradeTask_success() throws SQLException, DotDataException {
-        dropColumn();
+        final DotConnect dotConnect = new DotConnect();
+        dropColumn(dotConnect);
         final Task05350AddDotSaltClusterColumn task = new Task05350AddDotSaltClusterColumn();
         assertTrue(task.forceRun());//True because the column does not exists
         task.executeUpgrade();
         assertFalse(task.forceRun());//False because the column exists
+
+        dotConnect.setSQL("SELECT cluster_id, cluster_salt FROM dot_cluster ");
+        final List<Map<String, Object>> maps = dotConnect.loadObjectResults();
+        assertEquals("There can only be 1.", 1, maps.size());
+        final Map<String, Object> map = maps.get(0);
+        Assert.assertNotNull(map.get("cluster_id"));
+        Assert.assertNotNull(map.get("cluster_salt"));
     }
 
 }
