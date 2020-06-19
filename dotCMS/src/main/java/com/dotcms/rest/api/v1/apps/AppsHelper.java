e8d85f03b92f93573c0fd3bc35660b7d5ebb07ba
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2020-06-19 08:33:31 -0600

    #18587  optmized imports

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/apps/AppsHelper.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/apps/AppsHelper.java
@@ -33,7 +33,6 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
