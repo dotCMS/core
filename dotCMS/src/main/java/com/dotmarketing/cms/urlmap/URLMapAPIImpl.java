2f5ce7a444491e0ecc07287db51f27e68ba9417f
Will Ezell <will@dotcms.com>
2020-06-22 11:42:31 -0400

    #18744 initial commit

--- dotCMS/src/main/java/com/dotmarketing/cms/urlmap/URLMapAPIImpl.java
+++ dotCMS/src/main/java/com/dotmarketing/cms/urlmap/URLMapAPIImpl.java
@@ -1,6 +1,14 @@
 package com.dotmarketing.cms.urlmap;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import org.jetbrains.annotations.NotNull;
 import com.dotcms.content.elasticsearch.util.ESUtils;
+import com.dotcms.contenttype.business.ContentTypeAPI;
+import com.dotcms.contenttype.model.type.ContentType;
 import com.dotcms.rendering.velocity.viewtools.content.util.ContentUtils;
 import com.dotmarketing.beans.Host;
 import com.dotmarketing.beans.Identifier;
@@ -13,29 +21,18 @@ import com.dotmarketing.business.PermissionLevel;
 import com.dotmarketing.business.web.HostWebAPI;
 import com.dotmarketing.business.web.UserWebAPI;
 import com.dotmarketing.business.web.WebAPILocator;
-import com.dotmarketing.cache.FieldsCache;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotRuntimeException;
 import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.filters.CMSUrlUtil;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.portlets.structure.StructureUtil;
-import com.dotmarketing.portlets.structure.factories.StructureFactory;
-import com.dotmarketing.portlets.structure.model.Field;
 import com.dotmarketing.portlets.structure.model.SimpleStructureURLMap;
-import com.dotmarketing.portlets.structure.model.Structure;
 import com.dotmarketing.util.Logger;
-import com.dotmarketing.util.PageMode;
 import com.dotmarketing.util.RegEX;
 import com.dotmarketing.util.RegExMatch;
 import com.dotmarketing.util.UtilMethods;
 import com.liferay.util.StringPool;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * {@link URLMapAPI} implementation
@@ -46,10 +43,9 @@ public class URLMapAPIImpl implements URLMapAPI {
 
     private volatile Collection<ContentTypeURLPattern> patternsCache;
     private final UserWebAPI wuserAPI = WebAPILocator.getUserWebAPI();
-    private final HostWebAPI whostAPI = WebAPILocator.getHostWebAPI();
     private final PermissionAPI permissionAPI = APILocator.getPermissionAPI();
     private final IdentifierAPI identifierAPI = APILocator.getIdentifierAPI();
-
+    private final ContentTypeAPI typeAPI = APILocator.getContentTypeAPI(APILocator.systemUser());
     public boolean isUrlPattern(final UrlMapContext urlMapContext)
             throws DotDataException, DotSecurityException {
         return matchingUrlPattern(urlMapContext.getUri()) && getContentlet(urlMapContext) != null;
@@ -63,11 +59,13 @@ public class URLMapAPIImpl implements URLMapAPI {
             return Optional.empty();
         }
 
-        final Structure structure = CacheLocator.getContentTypeCache()
-                .getStructureByInode(contentlet.getStructureInode());
-        final Identifier pageUriIdentifier = this.getDetailtPageUri(structure);
+        final ContentType contentType = contentlet.getContentType();
+        final Optional<Identifier> optDetailIdentifier = this.getDetailtPageUri(contentType, context.getHost());
 
-        return Optional.of(new URLMapInfo(contentlet, pageUriIdentifier, context.getUri()));
+        if(!optDetailIdentifier.isPresent()) {
+            return Optional.empty();
+        }
+        return Optional.of(new URLMapInfo(contentlet, optDetailIdentifier.get(), context.getUri()));
     }
 
     /**
@@ -89,13 +87,10 @@ public class URLMapAPIImpl implements URLMapAPI {
             if (!matchesFound.isEmpty()) {
 
                 for (final Matches matches : matchesFound) {
-                    final Structure structure = CacheLocator.getContentTypeCache()
-                            .getStructureByInode(matches.getPatternChange().getStructureInode());
-
-                    final Field hostField = this.findHostField(structure);
+                    final ContentType contentType = typeAPI.find(matches.getPatternChange().getStructureInode());
 
                     matchingContentlet = this
-                            .getContentlet(matches, structure, hostField, urlMapContext);
+                            .getContentlet(matches, contentType, urlMapContext);
                     if (null != matchingContentlet) {
                         break;
                     }
@@ -111,27 +106,42 @@ public class URLMapAPIImpl implements URLMapAPI {
         return matchingContentlet;
     }
 
-    private Identifier getDetailtPageUri(final Structure structure) {
-        if (structure != null && UtilMethods.isSet(structure.getDetailPage())) {
-
-            try {
-
-                final Identifier identifier = this.identifierAPI.find(structure.getDetailPage());
-                if (identifier == null || !UtilMethods.isSet(identifier.getInode())) {
-                    throw new DotRuntimeException(
-                            "No valid detail page for structure '" + structure.getName()
-                                    + "'. Looking for detail page id=" + structure
-                                    .getDetailPage());
-                }
-
-                return identifier;
-            } catch (Exception e) {
-                throw new DotRuntimeException(e);
-            }
-        } else {
-            return null;
+    private Optional<Identifier> getDetailtPageUri(final ContentType contentType, Host currentHost) {
+        if (contentType == null || UtilMethods.isEmpty(contentType.detailPage())) {
+            return Optional.empty();
         }
-    }
+        
+        try {
+            final Identifier identifier = this.identifierAPI.find(contentType.detailPage());
+            if (identifier == null || !UtilMethods.isSet(identifier.getId())) {
+                Logger.info(this.getClass(),
+                        "No valid detail page for structure '" + contentType.name()
+                                + "'. Looking for detail page id=" + contentType
+                                .detailPage());
+                return Optional.empty();
+            }
+            
+            //if the detail page is on this host, send it!
+            if(identifier.getHostId().equals(currentHost.getIdentifier())) {
+                return Optional.of(identifier);
+            }
+            
+            // look for it on the current host
+            final Identifier myHostIdentifier = this.identifierAPI.find(currentHost, identifier.getPath());
+            if (myHostIdentifier == null || !UtilMethods.isSet(myHostIdentifier.getId())) {
+                Logger.info(this.getClass(),
+                        "No valid detail page for structure '" + contentType.name()
+                                + "'. Looking for a detail page=" + identifier.getPath() + " on host " + currentHost.getHostname());
+                return Optional.empty();
+            }
+
+            return Optional.of(myHostIdentifier);
+        } catch (Exception e) {
+            Logger.warnAndDebug(this.getClass(), e);
+            return Optional.empty();
+        }
+    } 
+    
 
     /**
      * Return all the matches related to a given URI, multiple content types could use the URLMap
@@ -187,26 +197,8 @@ public class URLMapAPIImpl implements URLMapAPI {
         return !foundMatches.isEmpty();
     }
 
-    private Field findHostField(final Structure structure) {
-        return FieldsCache.getFieldsByStructureInode(structure.getInode()).stream()
-                .filter(field -> field.getFieldType()
-                        .equals(Field.FieldType.HOST_OR_FOLDER.toString()))
-                .findFirst()
-                .orElse(null);
-    }
 
-    private String getHostFilter(final Host host) {
-        try {
-            final Host systemHost = this.whostAPI.findSystemHost(this.wuserAPI.getSystemUser(), true);
-            return String.format("+(conhost: %s conhost: %s)", host.getIdentifier(), systemHost.getIdentifier());
-        } catch (DotDataException | DotSecurityException e) {
-            Logger.error(URLMapAPIImpl.class, e.getMessage()
-                    + " : Unable to build host in query : ", e);
-            return "";
-        }
-    }
-
-    private String buildFields(final Structure structure,
+    private String buildFields(final ContentType structure,
             final Matches matches) {
 
         final StringBuilder query = new StringBuilder();
@@ -220,7 +212,7 @@ public class URLMapAPIImpl implements URLMapAPI {
             if (value.endsWith("/")) {
                 value = value.substring(0, value.length() - 1);
             }
-            query.append('+').append(structure.getVelocityVarName()).append('.')
+            query.append('+').append(structure.variable()).append('.')
                     .append(fieldMatches.get(counter)).append("_dotRaw").append(':')
                     .append(ESUtils.escapeExcludingSlashIncludingSpace(value)).append(' ');
             counter++;
@@ -231,18 +223,15 @@ public class URLMapAPIImpl implements URLMapAPI {
 
     private Contentlet getContentlet(
             final Matches matches,
-            final Structure structure,
-            final Field hostField,
+            final ContentType contentType,
             final UrlMapContext context)
              throws DotDataException, DotSecurityException {
 
         Contentlet contentlet = null;
 
-        final String query = this.buildContentQuery(matches, structure, hostField, context);
+        final String query = this.buildContentQuery(matches, contentType, context);
         final List<Contentlet> contentletSearches =
-                ContentUtils.pull(query, 0, 2,
-                        (hostField!=null && hostField.isRequired()) ? "conhost, modDate" : "modDate",
-                        this.wuserAPI.getSystemUser(), true);
+                ContentUtils.pull(query, 0, 2, "score",this.wuserAPI.getSystemUser(), true);
 
         if (!contentletSearches.isEmpty()) {
             int idx = 0;
@@ -268,34 +257,40 @@ public class URLMapAPIImpl implements URLMapAPI {
                 contentlet, PermissionLevel.READ.getType(), context.getUser(), context.getMode().respectAnonPerms);
 
         if (!havePermission) {
-            throw new DotSecurityException(String.format("User dont have permission in content: %s", contentlet.getName()));
+            throw new DotSecurityException(String.format("User does not have permission in content: %s", contentlet.getName()));
         }
     }
 
     private String buildContentQuery(
             final Matches matches,
-            final Structure structure,
-            final Field hostField,
+            final ContentType structure,
             final UrlMapContext context) {
 
         final StringBuilder query = new StringBuilder();
 
-        query.append("+structureName:").append(structure.getVelocityVarName())
-             .append(" +deleted:false ");
+        query.append("+contentType:")
+            .append(structure.variable())
+            .append(" +deleted:false ")
+            .append(" +(conhost:")
+                .append(context.getHost().getIdentifier())
+                .append(" OR conhost:")
+                .append(Host.SYSTEM_HOST)
+            .append(")");
+        
 
-        if (context.getMode() == PageMode.PREVIEW_MODE || context.getMode() == PageMode.EDIT_MODE) {
-            query.append("+working:true ");
+        if (context.getMode().showLive) {
+            query.append(" +live:true ");
         } else {
-            query.append("+live:true ");
+            query.append(" +working:true ");
         }
 
-        if (null != hostField && context.getHost() != null) {
-            query.append(this.getHostFilter(context.getHost()));
-        }
+
 
         query.append(" ");
-        query.append(this.buildFields(structure, matches))
-             .append(" +languageId:").append(context.getLanguageId());
+        query.append(this.buildFields(structure, matches));
+        
+        // score the current language higher
+        query.append(" languageId:").append(context.getLanguageId());
 
         return query.toString();
     }
@@ -325,7 +320,7 @@ public class URLMapAPIImpl implements URLMapAPI {
     private synchronized void loadPatterns() throws DotDataException {
         patternsCache = new ArrayList<>();
 
-        final List<SimpleStructureURLMap> urlMaps = StructureFactory.findStructureURLMapPatterns();
+        final List<SimpleStructureURLMap> urlMaps = typeAPI.findStructureURLMapPatterns();
 
         if (urlMaps != null && !urlMaps.isEmpty()) {
             final StringBuilder masterRegEx = new StringBuilder("^(");
