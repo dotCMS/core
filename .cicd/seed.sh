5d95268d3276451a74f633025926f18fd72c47ef
Victor Alfaro <victor.alfaro@dotcms.com>
2020-06-22 09:40:07 -0600

    test

--- .cicd/seed.sh
+++ .cicd/seed.sh
@@ -1,65 +1,80 @@
 #!/bin/bash
 
-export CICD_REPO="https://github.com/dotCMS/dot-cicd.git"
-export CICD_BRANCH=
-export CICD_FOLDER=dotcicd
-export CICD_DEST=${CICD_FOLDER}/library
-export CICD_VERSION=
-export CICD_TARGET=core
-export CICD_TOOL=travis
+: ${DOT_CICD_PATH:="./dotcicd"} && export DOT_CICD_PATH
+: ${DOT_CICD_REPO:="https://github.com/dotCMS/dot-cicd.git"} && export DOT_CICD_REPO
+: ${DOT_CICD_BRANCH:=""} && export DOT_CICD_BRANCH
+: ${DOT_CICD_LIB:="${DOT_CICD_PATH}/library"} && export DOT_CICD_LIB
+
+echo "#############"
+echo "dot-cicd vars"
+echo "#############"
+echo "DOT_CICD_PATH: ${DOT_CICD_PATH}"
+echo "DOT_CICD_REPO: ${DOT_CICD_REPO}"
+echo "DOT_CICD_BRANCH: ${DOT_CICD_BRANCH}"
+echo "DOT_CICD_LIB: ${DOT_CICD_LIB}"
+echo
 
 # Prepares folders for CI/CD
-function prepareCICD() {
-  mkdir ${CICD_FOLDER}
+prepareCICD () {
+  if [ ! -d ${DOT_CICD_PATH} ]; then
+    mkdir ${DOT_CICD_PATH}
+  elif [ -d ${DOT_CICD_LIB} ]; then
+    rm -rf ${DOT_CICD_LIB}
+  fi
 }
 
 # Clones and checkout a provided repo url with branch (optional)
-function gitCloneAndCheckout() {
-  local CICD_REPO=$1
-  local CICD_BRANCH=$2
+gitCloneAndCheckout () {
+  local DOT_CICD_REPO=$1
+  local DOT_CICD_BRANCH=$2
 
-  if [[ -z "${CICD_REPO}" ]]; then
+  if [ -z "${DOT_CICD_REPO}" ]; then
     echo "Repo not provided, cannot continue"
     exit 1
   fi
 
-  cloneOk=false
-  if [[ ! -z "${CICD_BRANCH}" ]]; then
-    echo "Cloning CI/CD repo from ${CICD_REPO} with branch ${CICD_BRANCH} to ${CICD_DEST}"
-    git clone ${CICD_REPO} -b ${CICD_BRANCH} ${CICD_DEST}
-    if [[ $? != 0 ]]; then
-      echo "Error checking out branch '${CICD_BRANCH}', continuing with master"
-    else
-      cloneOk=true
-    fi
+  echo "Cloning CI/CD repo from ${DOT_CICD_REPO} to ${DOT_CICD_LIB}"
+  git clone ${DOT_CICD_REPO} ${DOT_CICD_LIB}
+
+  if [ $? -ne 0 ]; then
+    echo "Error cloning repo '${DOT_CICD_REPO}'"
+    exit 1
   fi
 
-  if [[ $cloneOk == false ]]; then
-    echo "Cloning CI/CD repo from ${CICD_REPO} to ${CICD_DEST}"
-    git clone ${CICD_REPO} ${CICD_DEST}
-
-    if [[ $? != 0 ]]; then
-      echo "Error cloning repo '${CICD_REPO}'"
-      exit 1
+  if [ ! -z "${DOT_CICD_BRANCH}" ]; then
+    cd ${DOT_CICD_LIB}
+    echo "Checking out branch ${DOT_CICD_BRANCH}"
+    git checkout -b ${DOT_CICD_BRANCH}
+    if [ $? -ne 0 ]; then
+      echo "Error checking out branch '${DOT_CICD_BRANCH}', continuing with master"
+    else
+      git branch
+      git pull origin ${DOT_CICD_BRANCH}
     fi
+
+    cd ../../
   fi
 }
 
 # Make bash scripts to be executable
-function prepareScripts() {
-  pushd ${CICD_DEST}
-
-  for script in $(find . -type f -name "*.sh"); do
+prepareScripts () {
+  for script in $(find ${DOT_CICD_LIB} -type f -name "*.sh"); do
     echo "Making ${script} executable"
     chmod +x ${script}
   done
-
-  popd
 }
 
 # Fetch CI/CD github repo to include and use its library
-function fetchCICD() {
+fetchCICD () {
   prepareCICD
-  gitCloneAndCheckout ${CICD_REPO} ${CICD_BRANCH}
+  gitCloneAndCheckout ${DOT_CICD_REPO} ${DOT_CICD_BRANCH}
   prepareScripts
+  exit 0
 }
+
+fetchCICD
+
+if [ $? -ne 0 ]; then
+  echo "Aborting pipeline"
+  exit 1
+fi
