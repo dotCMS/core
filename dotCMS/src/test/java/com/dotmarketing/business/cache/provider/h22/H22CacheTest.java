526ad54bdefe9b330d92c8114f488cbd2031e9e1
Will Ezell <will@dotcms.com>
2020-07-02 16:25:44 -0400

    #18826 async puts

--- dotCMS/src/test/java/com/dotmarketing/business/cache/provider/h22/H22CacheTest.java
+++ dotCMS/src/test/java/com/dotmarketing/business/cache/provider/h22/H22CacheTest.java
@@ -2,21 +2,22 @@ package com.dotmarketing.business.cache.provider.h22;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
-
-import com.dotmarketing.util.Logger;
-import com.liferay.util.FileUtil;
 import java.io.File;
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import org.apache.commons.lang.RandomStringUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.UUIDGenerator;
+import com.liferay.util.FileUtil;
+import io.vavr.control.Try;
 
 public class H22CacheTest {
 
@@ -36,27 +37,37 @@ public class H22CacheTest {
 	final int numberOfThreads = 40;
 	final int numberOfGroups = 100;
 	final int maxCharOfObjects = 100;
+    final private ExecutorService executor  = Executors.newSingleThreadExecutor();
+    
+    final public H22Cache cache = getCache();
+    
+    final H22Cache getCache() {
+        final File dir = new File("/tmp/h2cachetest");
+        dir.delete();
+        dir.mkdirs();
+        return new H22Cache(dir.getAbsolutePath());
+        
+    }
+    
+    
+    @Before
+    public void testInit() throws Exception {
 
+        cache.init();
+    
+    }
+    
+    
 	@Test
-	public void testInit() throws Exception {
+	public void test_h22_basic_cache_functions() throws Exception {
 
-		// File dir = Files.createTempDir();
-		File dir = new File("/tmp/h2cachetest");
-		dir.delete();
-		dir.mkdirs();
-
-		H22Cache cache = new H22Cache(dir.getCanonicalPath());
-		cache.init();
-
-		
-		
 		assertThat("Are we the H2 Cache Loader?", "H22Cache".equals(cache.getKey()));
 
 		for (String group : GROUPNAMES) {
 			// put content
 			cache.put(group, KEYNAME, CONTENT);
 			// get content from cache
-			assertThat("Did we cache something", CONTENT.equals(cache.get(group, KEYNAME)));
+			assertThat("Did we cache something", CONTENT.equals(getFromCache(group, KEYNAME).get()));
 			// flush the group and check that we are null
 			cache.remove(group);
 			assertThat("we should be null", cache.get(group, KEYNAME) == null);
@@ -65,33 +76,44 @@ public class H22CacheTest {
 			cache.put(group, KEYNAME, CONTENT);
 
 			// get content from cache
-			assertThat("Did we cache something", CONTENT.equals(cache.get(group, KEYNAME)));
-
+			assertThat("Did we cache something", CONTENT.equals(getFromCache(group, KEYNAME).get()));
 			// remove the keyname
 			cache.remove(group, KEYNAME);
 
 			// get content from cache and check that we are null after remove
-			assertThat("we should be null after remove", cache.get(group, KEYNAME) == null);
+			assertThat("we should be null after remove", getFromCache(group, KEYNAME).get() == null);
 
 		}
+	}
+    @Test
+    public void test_long_key() throws Exception {
 
 		// try to cache a log key
 		cache.put(LONG_GROUPNAME, LONG_KEYNAME, CONTENT);
-		assertThat("We should cache with a long key", CONTENT.equals(cache.get(LONG_GROUPNAME, LONG_KEYNAME)));
+		assertThat("We should cache with a long key", CONTENT.equals(getFromCache(LONG_GROUPNAME, LONG_KEYNAME).get()));
 
 		// try to remove the long key
 		cache.remove(LONG_GROUPNAME);
 		assertThat("we should be null after remove", cache.get(LONG_GROUPNAME, KEYNAME) == null);
+    }
+    
+    @Test
+    public void test_cannot_cache_object() throws Exception {
+        // try to cache something that can't be cached
+        cache.put(LONG_GROUPNAME, CANT_CACHE_KEYNAME, new CantCacheMeObject());
+        assertThat("we should be null because of the CANT_CACHE_ME ", getFromCache(LONG_GROUPNAME, CANT_CACHE_KEYNAME).get() == null);
 
-		// try to cache something that can't be cached
-		cache.put(LONG_GROUPNAME, CANT_CACHE_KEYNAME, new CantCacheMeObject());
-		assertThat("we should be null because of the CANT_CACHE_ME ", cache.get(LONG_GROUPNAME, CANT_CACHE_KEYNAME) == null);
-
-		// try to cache a log key
-		cache.put(LONG_GROUPNAME, LONG_KEYNAME, CONTENT);
-		assertThat("We should cache with a long key", CONTENT.equals(cache.get(LONG_GROUPNAME, LONG_KEYNAME)));
-
-		Fqn fqn = new Fqn(LONG_GROUPNAME, LONG_KEYNAME);
+        
+        
+    }
+    @Test
+    public void test_get_keys_and_remove_all() throws Exception {
+        String keyName = UUIDGenerator.generateUuid();
+        cache.put(LONG_GROUPNAME, keyName, CONTENT);
+        
+        getFromCache(LONG_GROUPNAME, keyName).get();
+        
+		Fqn fqn = new Fqn(LONG_GROUPNAME, keyName);
 		Set<String> keys = cache.getKeys(LONG_GROUPNAME);
 		assertThat("Keys should include long key", keys.contains(fqn.id));
 
@@ -100,24 +122,75 @@ public class H22CacheTest {
 		cache.removeAll();
 		assertThat("Cache flushed, we have no groups", cache.getGroups().size() == 0);
 
-
+    }
 		
+    @Test
+    public void test_multithreaded_access() throws Exception {
+
+        // test dumping and rebuilding cache
+        //testMultithreaded(cache, numberOfThreads,true, false, true);
+        
+        // test causing an error and recovering
+        testMultithreaded(false, false);
+
+    }
+    
+    
+    
+    
+    
+    
+    @Test
+    public void test_breaking_cache_access() throws Exception {
+
 		// test dumping and rebuilding cache
 		//testMultithreaded(cache, numberOfThreads,true, false, true);
 		
 		// test causing an error and recovering
-		testMultithreaded(cache, numberOfThreads,false, true, false);
-		
-		//
-		cache.shutdown();
-
-		// cleanup
-		FileUtil.deltree(dir);
+		testMultithreaded(false, true);
 
 	}
+    
+    @Test
+    public void test_dumpCacheInMiddle() throws Exception {
 
-	void testMultithreaded(H22Cache cache, int numberOfThreads,boolean dumpCacheInMiddle, boolean breakCache, final boolean dieOnError) throws InterruptedException, SQLException{
+        // test dumping and rebuilding cache
+        //testMultithreaded(cache, numberOfThreads,true, false, true);
+        
+        // test causing an error and recovering
+        testMultithreaded(true, false);
 
+    }
+    
+    
+    
+    @After
+    public void test_shutdown() throws Exception {
+
+        cache.shutdown();
+
+    }
+	
+    public Future<String> getFromCache(final String group, final String key) {        
+        return executor.submit(() -> {
+            for(int i=0;i<10;i++) {
+                String value = (String) cache.get(group, key);
+                if(value!=null) {
+                    return value;
+                }
+                Thread.sleep(100);
+            }
+            
+            return null;
+        });
+    }
+	
+	
+	
+	
+
+	void testMultithreaded(boolean dumpCacheInMiddle, boolean breakCache) throws InterruptedException, SQLException{
+	    
 		ExecutorService pool = Executors.newFixedThreadPool(numberOfThreads);
 
 
@@ -139,10 +212,7 @@ public class H22CacheTest {
 		pool.shutdown();
 
 		while(!pool.isTerminated()){
-			if(errors.size()>0 && dieOnError){
-				throw new AssertionError(errors.get(0)) ;
-			}
-			Thread.sleep(500);
+			Thread.sleep(50);
 		}
 		
 
@@ -175,12 +245,12 @@ public class H22CacheTest {
 	}
 	
 	void breakCache(H22Cache cache) throws SQLException{
-		Optional<Connection> conn = cache.createConnection(true, 0);
-		if(conn.isPresent()){
-			Statement stmt = conn.get().createStatement();
-			stmt.execute("drop table " + H22Cache.TABLE_PREFIX + "0");
-			conn.get().close();
-		}
+	    
+	    File dbFolder = new File(cache.dbRoot);
+	    File newFolder = new File(dbFolder.getParent(), UUIDGenerator.shorty());
+	    dbFolder.renameTo(newFolder);
+	    FileUtil.deltree(newFolder, true);
+
 	}
 	
 	
@@ -198,8 +268,8 @@ public class H22CacheTest {
 		@Override
 		public void run() {
 
-			String group = "group_" + iter % numberOfGroups;
-			String key = RandomStringUtils.randomAlphanumeric(20);
+			final String group = "group_" + iter % numberOfGroups;
+			final String key = RandomStringUtils.randomAlphanumeric(20);
 			int len = (int) (Math.random() * maxCharOfObjects);
 			String val = RandomStringUtils.randomAlphanumeric(len+1);
 			cache.put(group, key, val);
@@ -211,14 +281,12 @@ public class H22CacheTest {
 			// and initiing, it is possible to get a null value, so we try again
 			//
 			while(newVal==null && --testTimes > 0){
-				try {
-					Thread.sleep(500);
-				} catch (InterruptedException e) {
 
-				}
+				Try.run(()->Thread.sleep(50));
+
 				
 				cache.put(group, key, val);
-				newVal = (String) cache.get(group, key);
+				newVal = Try.of(()->getFromCache(group, key).get()).getOrNull();
 
 			}
 			try{
