de4603169a1241fa8698a2dc200bd71efdb03d2b
freddyucv <freddy0309@gmail.com>
2020-07-10 14:01:17 -0600

    #18795 refactoring

--- dotCMS/src/main/java/com/dotcms/util/TimeMachineUtil.java
+++ dotCMS/src/main/java/com/dotcms/util/TimeMachineUtil.java
@@ -5,16 +5,24 @@ import com.dotcms.api.web.HttpServletRequestThreadLocal;
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
-public class TimeMachineUtil {
+public final class TimeMachineUtil {
 
     private TimeMachineUtil(){}
 
+    /**
+     * If Time Machine is running return the timestamp of the Time Machine date
+     * @return
+     */
     public static Optional<String> getTimeMachineDate() {
-        final HttpSession session = HttpServletRequestThreadLocal.INSTANCE.getRequest().getSession();
+        final HttpSession session = HttpServletRequestThreadLocal.INSTANCE.getRequest().getSession(false);
         final Object timeMachineObject = session != null ? session.getAttribute("tm_date") : null;
         return Optional.ofNullable(timeMachineObject != null ? timeMachineObject.toString() : null);
     }
 
+    /**
+     * Return true if Time Machine is running, otherwise return false
+     * @return
+     */
     public static boolean isRunning(){
         final Optional<String> timeMachine = getTimeMachineDate();
         return timeMachine.isPresent();
