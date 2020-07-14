81f12ac29ac9697f69b4a27f6338c2de53d3842a
Will Ezell <will@dotcms.com>
2020-07-14 17:03:48 -0400

    updating client

--- dotCMS/src/main/java/com/dotcms/cache/lettuce/MasterReplicaLettuceClient.java
+++ dotCMS/src/main/java/com/dotcms/cache/lettuce/MasterReplicaLettuceClient.java
@@ -15,8 +15,8 @@ import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.codec.CompressionCodec;
 import io.lettuce.core.masterreplica.MasterReplica;
 import io.lettuce.core.masterreplica.StatefulRedisMasterReplicaConnection;
-import io.lettuce.core.resource.ClientResources;
 import io.lettuce.core.resource.DefaultClientResources;
+import io.lettuce.core.resource.DirContextDnsResolver;
 import io.lettuce.core.support.ConnectionPoolSupport;
 import io.vavr.control.Try;
 
@@ -27,11 +27,11 @@ public enum MasterReplicaLettuceClient implements LettuceClient{
     INSTANCE;
 
     private final List<RedisURI> redisUris =
-                    Arrays.asList(Config.getStringArrayProperty("redis.lettuceclient.uris", new String[] {"redis://localhost"}))
+                    Arrays.asList(Config.getStringArrayProperty("REDIS_LETTUCECLIENT_URLS", new String[] {"redis://password@oboxturbo"}))
                                     .stream().map(u -> RedisURI.create(u)).collect(Collectors.toList());
 
-    private final int timeout        = Config.getIntProperty("redis.lettuceclient.timeout.ms", 5000);
-    private final int maxConnections = Config.getIntProperty("redis.lettuceclient.max.connections", 50);
+    private final int timeout        = Config.getIntProperty("REDIS_LETTUCECLIENT_TIMEOUT_MS", 5000);
+    private final int maxConnections = Config.getIntProperty("REDIS_LETTUCECLIENT_MAX_CONNECTIONS", 50);
     private final GenericObjectPool<StatefulRedisConnection<String, Object>> pool;
 
     
@@ -47,7 +47,7 @@ public enum MasterReplicaLettuceClient implements LettuceClient{
 
     }
     
-    private final ClientResources sharedResources = DefaultClientResources.create();
+
     
     private GenericObjectPool<StatefulRedisConnection<String, Object>> buildPool() {
 
@@ -55,23 +55,27 @@ public enum MasterReplicaLettuceClient implements LettuceClient{
         config.setMinIdle(2);
         config.setMaxIdle(5);
         config.setMaxTotal(maxConnections);
-
-
+        
+        final DefaultClientResources clientResources = DefaultClientResources.builder()
+                        .dnsResolver(new DirContextDnsResolver()) // Does not cache DNS lookups
+                        .build();
+        
+        final RedisClient lettuceClient = RedisClient.create(clientResources);
+        
         return ConnectionPoolSupport.createGenericObjectPool(() -> {
-
-            RedisClient lettuceClient = RedisClient.create(sharedResources);
 
             try {
                 StatefulRedisMasterReplicaConnection<String, Object> connection = MasterReplica.connect(lettuceClient,
                                 CompressionCodec.valueCompressor(new DotObjectCodec(), CompressionCodec.CompressionType.GZIP),
                                 redisUris);
-                connection.setReadFrom(ReadFrom.MASTER_PREFERRED);
+                connection.setReadFrom(ReadFrom.REPLICA_PREFERRED);
                 if (timeout > 0) {
                     connection.setTimeout(Duration.ofMillis(timeout));
                 }
                 return connection;
             }
             catch(Exception e) {
+                Logger.warnAndDebug(this.getClass(), e);
                 return null;
             }
         }, config, true);
