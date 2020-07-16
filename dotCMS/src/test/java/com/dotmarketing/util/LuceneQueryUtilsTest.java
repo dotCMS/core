68ab689cc602345556a59eeceb5ed7598bbf0ae0
Jose Castro <jose.castro@dotcms.com>
2020-07-16 10:44:57 -0600

    #18697 : Several improvements have been made to the ES implementation… (#18701)
    * #18697 : Several improvements have been made to the ES implementation in dotCMS. As part of that, the WhitespaceAnalyzer is not being used anymore. However, there's code that is still using this code and applying other outdated verifications to Lucene queries. For some specific ones, this is causing an error that is keeping the AVAILABLE WORKFLOW ACTIONS button from working as expected.
    
    * Update of unit tests to evaluate new suffix at bulk action queries
    
    Co-authored-by: Victor Alfaro <victor.alfaro@dotcms.com>

--- dotCMS/src/test/java/com/dotmarketing/util/LuceneQueryUtilsTest.java
+++ dotCMS/src/test/java/com/dotmarketing/util/LuceneQueryUtilsTest.java
@@ -9,7 +9,7 @@ public class LuceneQueryUtilsTest extends UnitTestBase {
     @Test
     public void testRetrieveAllActions() throws Exception {
         final String retrieveAllActionsQuery = "+contentType:FileAsset +(conhost:48190c8c-42c4-46af-8d1a-0cd5db894797 conhost:SYSTEM_HOST) +languageId:1 +deleted:false +working:true";
-        final String preparedRetrieveAllActionsQuery = "+contentType:FileAsset +(conhost:48190c8c-42c4-46af-8d1a-0cd5db894797 conhost:SYSTEM_HOST) +languageId:1 +deleted:false +working:true -contentType:Host";
+        final String preparedRetrieveAllActionsQuery = "+contentType:FileAsset +(conhost:48190c8c-42c4-46af-8d1a-0cd5db894797 conhost:SYSTEM_HOST) +languageId:1 +deleted:false +working:true -contentType:host";
         final String sanitizedQuery = LuceneQueryUtils.sanitizeBulkActionsQuery(retrieveAllActionsQuery);
         Assert.assertEquals(preparedRetrieveAllActionsQuery , sanitizedQuery);
     }
@@ -17,7 +17,7 @@ public class LuceneQueryUtilsTest extends UnitTestBase {
     @Test
     public void testRetrieveAllActionsQueryWithPrefix() throws Exception {
         final String retrieveAllActionsQueryWithPrefix = "query_-contentType:Host -baseType:3 +(conhost:48190c8c-42c4-46af-8d1a-0cd5db894797 conhost:SYSTEM_HOST) +languageId:1 +deleted:false +working:true";
-        final String preparedRetrieveAllActionsQueryWithPrefix = "-contentType:Host -baseType:3 +(conhost:48190c8c-42c4-46af-8d1a-0cd5db894797 conhost:SYSTEM_HOST) +languageId:1 +deleted:false +working:true";
+        final String preparedRetrieveAllActionsQueryWithPrefix = "-contentType:Host -baseType:3 +(conhost:48190c8c-42c4-46af-8d1a-0cd5db894797 conhost:SYSTEM_HOST) +languageId:1 +deleted:false +working:true -contentType:host";
         final String sanitizedQuery = LuceneQueryUtils.sanitizeBulkActionsQuery(retrieveAllActionsQueryWithPrefix);
         Assert.assertEquals(preparedRetrieveAllActionsQueryWithPrefix , sanitizedQuery);
     }
@@ -25,7 +25,7 @@ public class LuceneQueryUtilsTest extends UnitTestBase {
     @Test
     public void testRetrieveAllActionsQueryWithPrefixAndExcludeInodes() throws Exception {
         final String retrieveAllActionsQueryWithPrefixAndExcludeInodes = "query_-contentType:Host -baseType:3 +(conhost:48190c8c-42c4-46af-8d1a-0cd5db894797 conhost:SYSTEM_HOST) +languageId:1 +deleted:false +working:true -(inode:4eb3c99f-e999-44aa-acdc-75371deb8776 inode:2cd36499-beac-4c5a-9f8e-65c28ce62b8f)";
-        final String preparedRetrieveAllActionsQueryWithPrefixAndExcludeInodes = "-contentType:Host -baseType:3 +(conhost:48190c8c-42c4-46af-8d1a-0cd5db894797 conhost:SYSTEM_HOST) +languageId:1 +deleted:false +working:true -(inode:4eb3c99f-e999-44aa-acdc-75371deb8776 inode:2cd36499-beac-4c5a-9f8e-65c28ce62b8f)";
+        final String preparedRetrieveAllActionsQueryWithPrefixAndExcludeInodes = "-contentType:Host -baseType:3 +(conhost:48190c8c-42c4-46af-8d1a-0cd5db894797 conhost:SYSTEM_HOST) +languageId:1 +deleted:false +working:true -(inode:4eb3c99f-e999-44aa-acdc-75371deb8776 inode:2cd36499-beac-4c5a-9f8e-65c28ce62b8f) -contentType:host";
         final String sanitizedQuery = LuceneQueryUtils.sanitizeBulkActionsQuery(retrieveAllActionsQueryWithPrefixAndExcludeInodes);
         Assert.assertEquals(preparedRetrieveAllActionsQueryWithPrefixAndExcludeInodes , sanitizedQuery);
     }
