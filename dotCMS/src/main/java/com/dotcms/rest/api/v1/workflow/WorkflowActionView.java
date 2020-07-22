fdfe6476bd035b914e75b9169bb96d8a3025837c
jdotcms <jonathan.sanchez@dotcms.com>
2020-07-22 16:19:50 -0600

    #186780 adding just a few doc

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/workflow/WorkflowActionView.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/workflow/WorkflowActionView.java
@@ -4,6 +4,10 @@ import com.dotmarketing.portlets.workflows.model.WorkflowAction;
 
 import java.util.List;
 
+/**
+ * Self contains Workflow Action, this one is a {@link WorkflowAction} + the action inputs.
+ * @author jsanca
+ */
 public class WorkflowActionView extends WorkflowAction {
 
     private List<ActionInputView> actionInputs;
