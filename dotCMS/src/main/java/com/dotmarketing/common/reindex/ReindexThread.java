7cf99c13ef6883ed72fcaf84281bbf5451b67b4f
Nollymar Longa <>
2020-07-29 17:31:32 -0500

    Decreasing the bulk actions default settings

--- dotCMS/src/main/java/com/dotmarketing/common/reindex/ReindexThread.java
+++ dotCMS/src/main/java/com/dotmarketing/common/reindex/ReindexThread.java
@@ -88,10 +88,10 @@ public class ReindexThread {
     private long contentletsIndexed = 0;
     // bulk up to this many requests
     public static final int ELASTICSEARCH_BULK_ACTIONS = Config
-            .getIntProperty("REINDEX_THREAD_ELASTICSEARCH_BULK_ACTIONS", 1000);
+            .getIntProperty("REINDEX_THREAD_ELASTICSEARCH_BULK_ACTIONS", 250);
     //how many threads will be used per shard
     public static final int ELASTICSEARCH_CONCURRENT_REQUESTS = Config
-            .getIntProperty("REINDEX_THREAD_CONCURRENT_REQUESTS", 3);
+            .getIntProperty("REINDEX_THREAD_CONCURRENT_REQUESTS", 1);
     //Bulk size in MB. -1 means disabled
     public static final int ELASTICSEARCH_BULK_SIZE = Config
             .getIntProperty("REINDEX_THREAD_ELASTICSEARCH_BULK_SIZE", 10);
