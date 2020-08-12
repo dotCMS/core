727ae03699a84e06007c55645cdf682e1851894f
Freddy Rodriguez <freddy0309@gmail.com>
2020-08-12 17:15:06 -0600

    Revert "Add dotcms webcomponents (#1407)" (#1408)
    This reverts commit d7c7bacd6fbf0dedff77164f29d5e72294fc7e67.

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { defineCustomElements } from "dotcms-webcomponents/loader";
-
 if (environment.production) {
     enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
-
-defineCustomElements();
