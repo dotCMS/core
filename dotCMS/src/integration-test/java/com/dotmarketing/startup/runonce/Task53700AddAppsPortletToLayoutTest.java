36afd7c867f1d45b1920a810280c48a2b086af9b
Erick Gonzalez <erick.gonzalez@dotcms.com>
2020-08-17 09:13:48 -0600

    #19077 rename file v2

--- dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task53700AddAppsPortletToLayoutTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task53700AddAppsPortletToLayoutTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertTrue;
  * Test adding the APPS portlet to the layout
  * @author jsanca
  */
-public class Task537000AddAppsPortletToLayoutTest {
+public class Task53700AddAppsPortletToLayoutTest {
 
     @BeforeClass
     public static void prepare() throws Exception {
@@ -27,7 +27,7 @@ public class Task537000AddAppsPortletToLayoutTest {
             final String dropAppPortletSQL = "delete from cms_layouts_portlets where portlet_id = 'apps'";
             dotConnect.executeStatement(dropAppPortletSQL);
         } catch (Exception e) {
-            Logger.info(Task537000AddAppsPortletToLayoutTest.class, () -> "Failed deleting the portlet_id apps");
+            Logger.info(Task53700AddAppsPortletToLayoutTest.class, () -> "Failed deleting the portlet_id apps");
         }
     }
 
@@ -43,7 +43,7 @@ public class Task537000AddAppsPortletToLayoutTest {
     public void test_upgradeTask_success() throws DotDataException {
         final DotConnect dotConnect = new DotConnect();
         dropAppPortlet(dotConnect);
-        final Task537000AddAppsPortletToLayout task = new Task537000AddAppsPortletToLayout();
+        final Task53700AddAppsPortletToLayout task = new Task53700AddAppsPortletToLayout();
         assertTrue(task.forceRun());//True because the apps does not exists
         task.executeUpgrade();
         assertFalse(task.forceRun());//False because the apps exists
