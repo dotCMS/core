dff804debb3b34a7f42514bd13774932c0da3cf6
Daniel Silva <daniel.silva@dotcms.com>
2020-08-20 14:48:22 -0500

    #19108 javadoc

--- dotCMS/src/main/java/com/dotcms/device/DeviceAPI.java
+++ dotCMS/src/main/java/com/dotcms/device/DeviceAPI.java
@@ -5,7 +5,15 @@ import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.liferay.portal.model.User;
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * API for Device-related operations
+ */
 public interface DeviceAPI {
+
+    /**
+     * Returns a {@link com.dotmarketing.portlets.contentlet.model.Contentlet} representing the Device
+     * given a request and a user
+     */
     Contentlet getCurrentDevice(final HttpServletRequest request, final User user) throws
             DotDataException;
 }
