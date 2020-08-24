54b4713cee6c89a2a0622c8924df1eb177ab7131
Daniel Silva <daniel.silva@dotcms.com>
2020-08-24 12:12:40 -0500

    #19108 viewAs field for the GraphQL Page API (#19119)
    * #19108 first draft
    
    * #19108 rest of viewAs fields
    
    * #19108 more fields for viewAs and postman tests
    
    * #19108 add geolocation and finish postman tests
    
    * #19108 codacy
    
    * #19108 javadoc
    
    * #19108 restore normal value
    
    * Update DeviceAPI.java
    
    * #19108 fix postman test
    
    * #19108 return optional

--- dotCMS/src/main/java/com/dotcms/graphql/business/PageAPIGraphQLTypesProvider.java
+++ dotCMS/src/main/java/com/dotcms/graphql/business/PageAPIGraphQLTypesProvider.java
@@ -3,20 +3,30 @@ package com.dotcms.graphql.business;
 import static graphql.Scalars.GraphQLBoolean;
 import static graphql.Scalars.GraphQLLong;
 import static graphql.Scalars.GraphQLString;
+import static graphql.schema.GraphQLList.list;
 
 import com.dotcms.graphql.ContentFields;
-import com.dotcms.graphql.InterfaceType;
+import com.dotcms.graphql.datafetcher.LanguageDataFetcher;
 import com.dotcms.graphql.datafetcher.MapFieldPropertiesDataFetcher;
 import com.dotcms.graphql.datafetcher.UserDataFetcher;
 import com.dotcms.graphql.datafetcher.page.TemplateDataFetcher;
+import com.dotcms.graphql.datafetcher.page.ViewAsDataFetcher;
 import com.dotcms.graphql.util.TypeUtil;
 import com.dotcms.graphql.util.TypeUtil.TypeFetcher;
+import com.dotcms.visitor.domain.Geolocation;
+import com.dotcms.visitor.domain.Visitor;
+import com.dotcms.visitor.domain.Visitor.AccruedTag;
+import com.dotmarketing.portlets.htmlpageasset.business.render.page.ViewAsPageStatus;
+import eu.bitwalker.useragentutils.Browser;
 import graphql.schema.GraphQLOutputType;
 import graphql.schema.GraphQLType;
 import graphql.schema.GraphQLTypeReference;
+import graphql.schema.PropertyDataFetcher;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
 
 /**
  * Singleton class that provides all the {@link GraphQLType}s needed for the Page API
@@ -75,6 +85,8 @@ public enum PageAPIGraphQLTypesProvider implements GraphQLTypesProvider {
         pageFields.put("wfNeverExpire", new TypeFetcher(GraphQLString));
         pageFields.put("wfPublishDate", new TypeFetcher(GraphQLString));
         pageFields.put("wfPublishTime", new TypeFetcher(GraphQLString));
+        pageFields.put("viewAs", new TypeFetcher(GraphQLTypeReference.typeRef("ViewAs")
+                , new ViewAsDataFetcher()));
 
         typeMap.put("Page", TypeUtil.createObjectType("Page", pageFields));
 
@@ -114,7 +126,111 @@ public enum PageAPIGraphQLTypesProvider implements GraphQLTypesProvider {
 
         typeMap.put("Template", TypeUtil.createObjectType("Template", templateFields));
 
+        final Map<String, TypeFetcher> viewAsFields = new HashMap<>();
+        viewAsFields.put("visitor", new TypeFetcher(GraphQLTypeReference.typeRef("Visitor"),
+                new PropertyDataFetcher<ViewAsPageStatus>("visitor")));
+        viewAsFields.put("language", new TypeFetcher(GraphQLTypeReference.typeRef("Language"),
+                new LanguageDataFetcher()));
+        viewAsFields.put("mode", new TypeFetcher(GraphQLString,
+                PropertyDataFetcher.fetching((Function<ViewAsPageStatus, String>)
+                        (viewAs)->viewAs.getPageMode().name())));
+        viewAsFields.put("persona", new TypeFetcher(GraphQLTypeReference.typeRef("PersonaBaseType"),
+                new PropertyDataFetcher<ViewAsPageStatus>("persona")));
+
+        typeMap.put("ViewAs", TypeUtil.createObjectType("ViewAs", viewAsFields));
+
+        final Map<String, TypeFetcher> visitorFields = new HashMap<>();
+        visitorFields.put("tags", new TypeFetcher(list(GraphQLTypeReference.typeRef("Tag")),
+                new PropertyDataFetcher<Visitor>("tags")));
+        visitorFields.put("device", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<String>("device")));
+        visitorFields.put("isNew", new TypeFetcher(GraphQLBoolean,
+                new PropertyDataFetcher<Boolean>("newVisitor")));
+        visitorFields.put("userAgent", new TypeFetcher(GraphQLTypeReference.typeRef("UserAgent"),
+                new PropertyDataFetcher<String>("userAgent")));
+        visitorFields.put("personas",
+                new TypeFetcher(list(GraphQLTypeReference.typeRef("WeightedPersona")),
+                PropertyDataFetcher.fetching((Function<Visitor, Set>)
+                        (visitor)->visitor.getWeightedPersonas().entrySet())));
+        visitorFields.put("persona", new TypeFetcher(GraphQLTypeReference.typeRef("PersonaBaseType"),
+                new PropertyDataFetcher<Visitor>("persona")));
+        visitorFields.put("geo", new TypeFetcher(GraphQLTypeReference.typeRef("Geolocation"),
+                new PropertyDataFetcher<Visitor>("geo")));
+
+        typeMap.put("Visitor", TypeUtil.createObjectType("Visitor", visitorFields));
+
+        final Map<String, TypeFetcher> weightedPersonaFields = new HashMap<>();
+        weightedPersonaFields.put("persona", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Map<String, Float>>("key")));
+        weightedPersonaFields.put("count", new TypeFetcher(GraphQLLong,
+                new PropertyDataFetcher<Map<String, Float>>("value")));
+
+        typeMap.put("WeightedPersona", TypeUtil.createObjectType("WeightedPersona",
+                weightedPersonaFields));
+
+        final Map<String, TypeFetcher> tagFields = new HashMap<>();
+        tagFields.put("tag", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<AccruedTag>("tag")));
+        tagFields.put("count", new TypeFetcher(GraphQLLong,
+                new PropertyDataFetcher<AccruedTag>("count")));
+
+        typeMap.put("Tag", TypeUtil.createObjectType("Tag", tagFields));
+
+        final Map<String, TypeFetcher> userAgentFields = new HashMap<>();
+        userAgentFields.put("operatingSystem", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<AccruedTag>("operatingSystem")));
+        userAgentFields.put("browser", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<AccruedTag>("browser")));
+        userAgentFields.put("id", new TypeFetcher(GraphQLLong,
+                new PropertyDataFetcher<AccruedTag>("id")));
+        userAgentFields.put("browserVersion", new TypeFetcher(GraphQLTypeReference.typeRef("BrowserVersion"),
+                new PropertyDataFetcher<AccruedTag>("browserVersion")));
+
+        typeMap.put("UserAgent", TypeUtil.createObjectType("UserAgent", userAgentFields));
+
+        final Map<String, TypeFetcher> browserVersionFields = new HashMap<>();
+        browserVersionFields.put("version", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Browser>("version")));
+        browserVersionFields.put("majorVersion", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Browser>("majorVersion")));
+        browserVersionFields.put("minorVersion", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Browser>("minorVersion")));
+
+        typeMap.put("BrowserVersion", TypeUtil.createObjectType("BrowserVersion", browserVersionFields));
+
+        final Map<String, TypeFetcher> geoFields = new HashMap<>();
+        geoFields.put("latitude", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Geolocation>("latitude")));
+        geoFields.put("longitude", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Geolocation>("longitude")));
+        geoFields.put("country", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Geolocation>("country")));
+        geoFields.put("countryCode", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Geolocation>("countryCode")));
+        geoFields.put("city", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Geolocation>("city")));
+        geoFields.put("continent", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Geolocation>("continent")));
+        geoFields.put("continentCode", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Geolocation>("continentCode")));
+        geoFields.put("company", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Geolocation>("company")));
+        geoFields.put("timezone", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Geolocation>("timezone")));
+        geoFields.put("subdivision", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Geolocation>("subdivision")));
+        geoFields.put("subdivisionCode", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Geolocation>("subdivisionCode")));
+        geoFields.put("ipAddress", new TypeFetcher(GraphQLString,
+                new PropertyDataFetcher<Geolocation>("ipAddress")));
+
+        typeMap.put("Geolocation", TypeUtil.createObjectType("Geolocation",
+                geoFields));
+
+
+
         return typeMap.values();
+
     }
 
     Map<String, GraphQLOutputType> getTypesMap() {
