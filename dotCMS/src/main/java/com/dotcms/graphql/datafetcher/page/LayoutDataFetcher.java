d0a0a08b24af7857368bbe6a755eee38ad513a0a
Daniel Silva <daniel.silva@dotcms.com>
2020-08-27 17:59:39 -0500

    #19136 codacy feedback

--- dotCMS/src/main/java/com/dotcms/graphql/datafetcher/page/LayoutDataFetcher.java
+++ dotCMS/src/main/java/com/dotcms/graphql/datafetcher/page/LayoutDataFetcher.java
@@ -3,26 +3,16 @@ package com.dotcms.graphql.datafetcher.page;
 import com.dotcms.graphql.DotGraphQLContext;
 import com.dotcms.rendering.velocity.viewtools.DotTemplateTool;
 import com.dotmarketing.business.APILocator;
-import com.dotmarketing.business.PermissionLevel;
-import com.dotmarketing.exception.DotDataException;
-import com.dotmarketing.exception.DotRuntimeException;
-import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
-import com.dotmarketing.portlets.htmlpageasset.business.HTMLPageAssetAPI;
-import com.dotmarketing.portlets.htmlpageasset.business.render.page.JsonMapper;
 import com.dotmarketing.portlets.htmlpageasset.model.HTMLPageAsset;
 import com.dotmarketing.portlets.templates.design.bean.TemplateLayout;
 import com.dotmarketing.portlets.templates.model.Template;
 import com.dotmarketing.util.Logger;
 import com.dotmarketing.util.PageMode;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.liferay.portal.model.User;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
-import java.io.CharArrayReader;
-import java.io.IOException;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * This DataFetcher returns a {@link Map} representing a {@link Template} associated to the originally
