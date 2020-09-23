243cc436a0fb844236849859729a70d838915fcd
jdotcms <jonathan.sanchez@dotcms.com>
2020-09-23 10:39:07 -0600

    #1437 handling the runtime exception on jersey

---
+++ dotCMS/src/main/java/com/dotcms/rest/exception/mapper/RuntimeExceptionMapper.java
@@ -0,0 +1,24 @@
+package com.dotcms.rest.exception.mapper;
+
+import com.dotcms.rest.api.v1.authentication.ResponseUtil;
+import com.dotmarketing.util.Logger;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Generic runtime exception handler to avoid cyclical issues with the stack trace mapping
+ * @author jsanca
+ */
+@Provider
+public class RuntimeExceptionMapper implements javax.ws.rs.ext.ExceptionMapper<RuntimeException> {
+
+    @Override
+    public Response toResponse(RuntimeException exception) {
+
+        //Log into our logs first.
+        Logger.warn(this.getClass(), exception.getMessage(), exception);
+
+        return ResponseUtil.mapExceptionResponse(exception);
+    }
+}
