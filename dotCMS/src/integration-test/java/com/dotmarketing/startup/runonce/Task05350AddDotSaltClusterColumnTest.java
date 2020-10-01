f3d7ef8b0cebe425ad58ce9beac55717bbb33e88
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2020-10-01 14:44:58 -0600

    fix broken test on mysql

--- dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task05350AddDotSaltClusterColumnTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task05350AddDotSaltClusterColumnTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 
 import com.dotcms.util.IntegrationTestInitService;
 import com.dotmarketing.common.db.DotConnect;
+import com.dotmarketing.db.DbConnectionFactory;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.util.Logger;
 import java.sql.SQLException;
@@ -28,13 +29,19 @@ public class Task05350AddDotSaltClusterColumnTest {
             final String dropColumnSQL = "ALTER TABLE dot_cluster DROP COLUMN cluster_salt";
             dotConnect.executeStatement(dropColumnSQL);
         } catch (Exception e) {
-            Logger.info(Task05350AddDotSaltClusterColumnTest.class, () -> "Failed removing cluster_salt column. Maybe it didn't exist?");
+            Logger.error(Task05350AddDotSaltClusterColumnTest.class, "Failed removing cluster_salt column. Maybe it didn't exist?");
         }
     }
 
     @Test
     public void test_upgradeTask_success() throws DotDataException {
         final DotConnect dotConnect = new DotConnect();
+        Logger.debug(this, "Prepping for testing `add` column.");
+        try {
+            DbConnectionFactory.getConnection().setAutoCommit(true);
+        } catch (SQLException e) {
+            throw new DotDataException(e.getMessage(), e);
+        }
         dropColumn(dotConnect);
         final Task05350AddDotSaltClusterColumn task = new Task05350AddDotSaltClusterColumn();
         assertTrue(task.forceRun());//True because the column does not exists
