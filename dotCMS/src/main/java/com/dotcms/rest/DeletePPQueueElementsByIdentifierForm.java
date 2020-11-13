6eab8d52ddd16932af2bf29140fc53080f98bcfe
Daniel Silva <daniel.silva@dotcms.com>
2020-11-12 22:00:49 -0600

    #19378 new resource to remove elements from pp queue

--- dotCMS/src/main/java/com/dotcms/rest/DeletePPQueueElementsByIdentifierForm.java
+++ dotCMS/src/main/java/com/dotcms/rest/DeletePPQueueElementsByIdentifierForm.java
@@ -2,15 +2,14 @@ package com.dotcms.rest;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
 import java.util.List;
 
-@JsonDeserialize(builder = DeleteBundlesByIdentifierForm.Builder.class)
-public class DeleteBundlesByIdentifierForm {
+@JsonDeserialize(builder = DeletePPQueueElementsByIdentifierForm.Builder.class)
+public class DeletePPQueueElementsByIdentifierForm {
 
     private final List<String> identifiers;
 
-    private DeleteBundlesByIdentifierForm(final Builder builder) {
+    private DeletePPQueueElementsByIdentifierForm(final Builder builder) {
 
         this.identifiers = builder.identifiers;
     }
@@ -23,13 +22,13 @@ public class DeleteBundlesByIdentifierForm {
     public static final class Builder {
         @JsonProperty(required = true) private List<String> identifiers;
 
-        public DeleteBundlesByIdentifierForm.Builder identifiers(final List<String> identifiers) {
+        public DeletePPQueueElementsByIdentifierForm.Builder identifiers(final List<String> identifiers) {
             this.identifiers = identifiers;
             return this;
         }
 
-        public DeleteBundlesByIdentifierForm build() {
-            return new DeleteBundlesByIdentifierForm(this);
+        public DeletePPQueueElementsByIdentifierForm build() {
+            return new DeletePPQueueElementsByIdentifierForm(this);
         }
     }
 }
