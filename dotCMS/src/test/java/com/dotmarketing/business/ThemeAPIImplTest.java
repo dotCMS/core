645df4f2b8da910d629d8b2367cc039e04207847
jdotcms <jonathan.sanchez@dotcms.com>
2020-12-15 00:31:43 -0600

    #19415 new the ThemeAPI is being used instead of the Folder API for themes, also adding the system theme in some missing parts

--- dotCMS/src/test/java/com/dotmarketing/business/ThemeAPIImplTest.java
+++ dotCMS/src/test/java/com/dotmarketing/business/ThemeAPIImplTest.java
@@ -4,6 +4,7 @@ import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.portlets.contentlet.business.ContentletAPI;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
+import com.dotmarketing.portlets.folders.business.FolderAPI;
 import com.dotmarketing.portlets.folders.model.Folder;
 import com.liferay.portal.model.User;
 import org.junit.Test;
@@ -26,6 +27,7 @@ public class ThemeAPIImplTest {
     @Test
     public void when_FolderAndUserAreNotNull_And_ExceptionIsNotThrown() throws DotDataException, DotSecurityException {
         final ContentletAPI contentletAPI = mock(ContentletAPI.class);
+        final FolderAPI folderAPI         = mock(FolderAPI.class);
         final User user = mock(User.class);
         final Folder folder = mock(Folder.class);
         final String folderInode = "1";
@@ -38,7 +40,7 @@ public class ThemeAPIImplTest {
         when(contentletAPI.search(LUCENE_QUERY, -1,
                 0, null, user, false)).thenReturn(contentlets);
 
-        final ThemeAPIImpl themeAPI = new ThemeAPIImpl(contentletAPI);
+        final ThemeAPIImpl themeAPI = new ThemeAPIImpl(contentletAPI, folderAPI);
         final String themeThumbnail = themeAPI.getThemeThumbnail(folder, user);
 
         assertEquals(themeThumbnailExpected, themeThumbnail);
@@ -47,6 +49,7 @@ public class ThemeAPIImplTest {
     @Test(expected = DotDataException.class)
     public void when_FolderAndUserAreNotNull_And_DotDataExceptionThrown() throws DotDataException, DotSecurityException {
         final ContentletAPI contentletAPI = mock(ContentletAPI.class);
+        final FolderAPI folderAPI         = mock(FolderAPI.class);
         final User user = mock(User.class);
         final Folder folder = mock(Folder.class);
         final String folderInode = "1";
@@ -57,13 +60,14 @@ public class ThemeAPIImplTest {
                 0, null, user, false)).thenThrow(dotDataException);
 
 
-        final ThemeAPIImpl themeAPI = new ThemeAPIImpl(contentletAPI);
+        final ThemeAPIImpl themeAPI = new ThemeAPIImpl(contentletAPI, folderAPI);
         themeAPI.getThemeThumbnail(folder, user);
     }
 
     @Test(expected = DotSecurityException.class)
     public void when_FolderAndUserAreNotNull_And_DotSecurityExceptionThrown() throws DotDataException, DotSecurityException {
         final ContentletAPI contentletAPI = mock(ContentletAPI.class);
+        final FolderAPI folderAPI         = mock(FolderAPI.class);
         final User user = mock(User.class);
         final Folder folder = mock(Folder.class);
         final String folderInode = "1";
@@ -74,13 +78,14 @@ public class ThemeAPIImplTest {
                 0, null, user, false)).thenThrow(dotSecurityException);
 
 
-        final ThemeAPIImpl themeAPI = new ThemeAPIImpl(contentletAPI);
+        final ThemeAPIImpl themeAPI = new ThemeAPIImpl(contentletAPI, folderAPI);
         themeAPI.getThemeThumbnail(folder, user);
     }
 
     @Test
     public void when_FolderIsNull_And_DotDataExceptionThrown() throws DotDataException, DotSecurityException {
         final ContentletAPI contentletAPI = mock(ContentletAPI.class);
+        final FolderAPI folderAPI         = mock(FolderAPI.class);
         final User user = mock(User.class);
         final Folder folder = null;
         final DotDataException dotDataException = mock(DotDataException.class);
@@ -89,7 +94,7 @@ public class ThemeAPIImplTest {
                 0, null, user, false)).thenThrow(dotDataException);
 
 
-        final ThemeAPIImpl themeAPI = new ThemeAPIImpl(contentletAPI);
+        final ThemeAPIImpl themeAPI = new ThemeAPIImpl(contentletAPI, folderAPI);
         final String themeThumbnail = themeAPI.getThemeThumbnail(folder, user);
 
         assertNull(themeThumbnail);
@@ -98,6 +103,7 @@ public class ThemeAPIImplTest {
     @Test
     public void when_UserIsNull_And_DotDataExceptionThrown() throws DotDataException, DotSecurityException {
         final ContentletAPI contentletAPI = mock(ContentletAPI.class);
+        final FolderAPI folderAPI         = mock(FolderAPI.class);
         final User user = null;
         final Folder folder = mock(Folder.class);
         final DotDataException dotDataException = mock(DotDataException.class);
@@ -106,7 +112,7 @@ public class ThemeAPIImplTest {
                 0, null, user, false)).thenThrow(dotDataException);
 
 
-        final ThemeAPIImpl themeAPI = new ThemeAPIImpl(contentletAPI);
+        final ThemeAPIImpl themeAPI = new ThemeAPIImpl(contentletAPI, folderAPI);
         final String themeThumbnail = themeAPI.getThemeThumbnail(folder, user);
 
         assertNull(themeThumbnail);
