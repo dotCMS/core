6b5b1446ff29bbaded1a86b2f36e241d8f3d2605
freddyucv <freddy0309@gmail.com>
2021-01-13 11:57:32 -0600

    Adding hard link support

--- dotCMS/src/main/java/com/dotcms/publishing/output/BundleOutput.java
+++ dotCMS/src/main/java/com/dotcms/publishing/output/BundleOutput.java
@@ -2,8 +2,11 @@ package com.dotcms.publishing.output;
 
 import com.dotcms.publishing.PublisherConfig;
 import com.dotcms.util.DotPreconditions;
+import com.dotmarketing.util.Config;
+
 import com.dotmarketing.util.Logger;
 import com.google.common.collect.ImmutableList;
+import com.liferay.util.FileUtil;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -20,6 +23,23 @@ public abstract class BundleOutput implements Closeable {
     }
 
     public abstract OutputStream addFile(String filePath) throws IOException;
+
+    public void copyFile(File source, String destinationPath) throws IOException {
+        if (Config.getBooleanProperty("CONTENT_VERSION_HARD_LINK", true) && this.useHardLink()) {
+            FileUtil.copyFile(source, new File(destinationPath));
+        } else {
+            try(final OutputStream outputStream = addFile(destinationPath)) {
+                FileUtil.copyFile(source, outputStream);
+            } catch(IOException e) {
+            Logger.error(FileUtil.class, e);
+            throw e;
+        }
+        }
+    }
+
+    public boolean useHardLink() {
+        return false;
+    }
 
     public abstract File getFile();
 
