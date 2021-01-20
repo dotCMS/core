7fb53debfe1fc8ffd950c0931101f271b649ddb4
Jose Castro <jose.castro@dotcms.com>
2021-01-20 11:02:45 -0600

    #19831 : There are two main changes in this PR, beyond the usual Javadoc/refactor changes:
    1. The method that validates the URL in a page is retrieving the right Site now when pages are created at the root of the Site. This was not occurring as expected, and revealed a situation in which the `SYSTEM_FOLDER` was referencing a Site that is not the `SYSTEM_HOST`. This is an extremely random situation, and haven't been able to reproduce locally.
    
    2. In the Folder Cache, I'm explicitly setting the `SYSTEM_FOLDER` to always point to the `SYSTEM_HOST`.

--- dotCMS/src/main/java/com/dotmarketing/portlets/contentlet/business/web/ContentletWebAPIImpl.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/contentlet/business/web/ContentletWebAPIImpl.java
@@ -4,7 +4,9 @@ import com.dotcms.api.system.event.ContentletSystemEventUtil;
 import com.dotcms.business.WrapInTransaction;
 import com.dotcms.contenttype.business.ContentTypeAPI;
 import com.dotcms.contenttype.exception.NotFoundInDbException;
+import com.dotcms.contenttype.model.type.BaseContentType;
 import com.dotcms.contenttype.model.type.ContentType;
+import com.dotcms.contenttype.model.type.PageContentType;
 import com.dotcms.contenttype.transform.contenttype.StructureTransformer;
 import com.dotcms.publisher.environment.bean.Environment;
 import com.dotcms.repackage.javax.portlet.WindowState;
@@ -12,13 +14,22 @@ import com.dotcms.repackage.org.directwebremoting.WebContextFactory;
 import com.dotmarketing.beans.Host;
 import com.dotmarketing.beans.Identifier;
 import com.dotmarketing.beans.MultiTree;
-import com.dotmarketing.business.*;
+import com.dotmarketing.business.APILocator;
+import com.dotmarketing.business.CacheLocator;
+import com.dotmarketing.business.FactoryLocator;
+import com.dotmarketing.business.IdentifierAPI;
+import com.dotmarketing.business.PermissionAPI;
+import com.dotmarketing.business.UserAPI;
+import com.dotmarketing.business.VersionableAPI;
 import com.dotmarketing.business.web.HostWebAPI;
 import com.dotmarketing.business.web.WebAPILocator;
 import com.dotmarketing.cache.FieldsCache;
 import com.dotmarketing.db.HibernateUtil;
-import com.dotmarketing.exception.*;
-import com.dotmarketing.portlets.calendar.business.EventAPI;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotHibernateException;
+import com.dotmarketing.exception.DotLanguageException;
+import com.dotmarketing.exception.DotRuntimeException;
+import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.portlets.calendar.model.Event;
 import com.dotmarketing.portlets.categories.business.CategoryAPI;
 import com.dotmarketing.portlets.categories.model.Category;
@@ -40,7 +51,15 @@ import com.dotmarketing.portlets.structure.model.ContentletRelationships.Content
 import com.dotmarketing.portlets.structure.model.Field;
 import com.dotmarketing.portlets.structure.model.Relationship;
 import com.dotmarketing.portlets.structure.model.Structure;
-import com.dotmarketing.util.*;
+import com.dotmarketing.util.Config;
+import com.dotmarketing.util.Constants;
+import com.dotmarketing.util.InodeUtils;
+import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.PageMode;
+import com.dotmarketing.util.PortletURLUtil;
+import com.dotmarketing.util.UtilHTML;
+import com.dotmarketing.util.UtilMethods;
+import com.dotmarketing.util.WebKeys;
 import com.liferay.portal.PortalException;
 import com.liferay.portal.SystemException;
 import com.liferay.portal.language.LanguageUtil;
@@ -48,21 +67,28 @@ import com.liferay.portal.model.User;
 import com.liferay.util.StringPool;
 import com.liferay.util.servlet.SessionMessages;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.text.DateFormat;
 import java.text.MessageFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
 import static com.dotmarketing.portlets.contentlet.util.ActionletUtil.hasPushPublishActionlet;
 import static com.dotmarketing.portlets.folders.business.FolderAPI.SYSTEM_FOLDER;
 import static com.dotmarketing.portlets.workflows.actionlet.PushPublishActionlet.getEnvironmentsToSendTo;
 
-/*
- *     //http://jira.dotmarketing.net/browse/DOTCMS-2273
- *     To save content via ajax.
+/**
+ * Implementation class for the {@link ContentletWebAPI} class.
  */
 public class ContentletWebAPIImpl implements ContentletWebAPI {
 
@@ -75,10 +101,11 @@ public class ContentletWebAPIImpl implements ContentletWebAPI {
 	private UserAPI userAPI;
 	private FolderAPI folderAPI;
 	private IdentifierAPI identAPI;
-	private EventAPI eventAPI;
 
 	private static DateFormat eventRecurrenceStartDateF = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 	private static DateFormat eventRecurrenceEndDateF = new SimpleDateFormat("yyyy-MM-dd");
+	private static final boolean RESPECT_FRONT_END_ROLES = Boolean.TRUE;
+    private static final boolean DONT_RESPECT_FRONT_END_ROLES = Boolean.FALSE;
 
 	private final ContentletSystemEventUtil contentletSystemEventUtil;
 
@@ -91,7 +118,6 @@ public class ContentletWebAPIImpl implements ContentletWebAPI {
 		this.userAPI = APILocator.getUserAPI();
 		this.folderAPI = APILocator.getFolderAPI();
 		this.identAPI = APILocator.getIdentifierAPI();
-        this.eventAPI = APILocator.getEventAPI();
 		contentletSystemEventUtil = ContentletSystemEventUtil.getInstance();
 	}
 
@@ -596,54 +622,45 @@ public class ContentletWebAPIImpl implements ContentletWebAPI {
 		return UtilMethods.isSet((String) contentletFormData.get("wfActionId"));
 	}
 
-	/**
-     * {@inheritDoc}
-	 */
-
     @Override
-	public String validateNewContentPage(Contentlet contentPage) {
-		String parentFolderId = contentPage.getFolder();
-		String pageUrl = contentPage.getMap().get("url") == null ? "" : contentPage.getMap().get("url").toString();
-		String status = null;
+	public String validateNewContentPage(final Contentlet contentPage) {
+		final String parentFolderId = contentPage.getFolder();
+        final String pageUrl = !UtilMethods.isSet(contentPage.getMap().get(PageContentType.PAGE_URL_FIELD_VAR)) ?
+                StringUtils.EMPTY : contentPage.getMap().get(PageContentType.PAGE_URL_FIELD_VAR).toString();
+        String status = null;
 		try {
-			User systemUser = userAPI.getSystemUser();
-			Folder parentFolder = folderAPI.find(parentFolderId, systemUser,
-					false);
-			if (parentFolder != null
-					&& InodeUtils.isSet(parentFolder.getInode())) {
-
-				Host host = hostAPI.find(parentFolder.getHostId(), systemUser,
-						true);
-
+            final Folder parentFolder = this.folderAPI.find(parentFolderId, this.userAPI.getSystemUser(), DONT_RESPECT_FRONT_END_ROLES);
+            if (parentFolder != null && InodeUtils.isSet(parentFolder.getInode())) {
+				final String siteId = SYSTEM_FOLDER.equals(parentFolder.getInode()) ? contentPage.getHost() :
+						parentFolder.getHostId();
+				final Host site = this.hostAPI.find(siteId, this.userAPI.getSystemUser(), RESPECT_FRONT_END_ROLES);
 				String parentFolderPath = parentFolder.getPath();
 				if (UtilMethods.isSet(parentFolderPath)) {
-					if (!parentFolderPath.startsWith("/")) {
-						parentFolderPath = "/" + parentFolderPath;
+					if (!parentFolderPath.startsWith(StringPool.SLASH)) {
+						parentFolderPath = StringPool.SLASH + parentFolderPath;
 					}
-					if (!parentFolderPath.endsWith("/")) {
-						parentFolderPath = parentFolderPath + "/";
+					if (!parentFolderPath.endsWith(StringPool.SLASH)) {
+						parentFolderPath = parentFolderPath + StringPool.SLASH;
 					}
-					String fullPageUrl = parentFolderPath + pageUrl;
+					final String fullPageUrl = parentFolderPath + pageUrl;
 					if (!pageUrl.endsWith(".html")) {
-						List<Identifier> folders = identAPI
-								.findByURIPattern("folder", fullPageUrl,true, host);
-						if (folders.size() > 0) {
+						final List<Identifier> folders = this.identAPI
+								.findByURIPattern("folder", fullPageUrl,true, site);
+						if (!folders.isEmpty()) {
 							// Found a folder with same path
 							status = "message.htmlpage.error.htmlpage.exists.folder";
 						}
 					}
 					if (!UtilMethods.isSet(status)) {
-						Identifier i = identAPI.find(host, fullPageUrl);
-						if (i != null && InodeUtils.isSet(i.getId())) {
+						final Identifier identifier = this.identAPI.find(site, fullPageUrl);
+						if (identifier != null && InodeUtils.isSet(identifier.getId())) {
 							try {
-								Contentlet existingContent = conAPI
-										.findContentletByIdentifier(i.getId(),
-												true,
-												contentPage.getLanguageId(),
-												systemUser, false);
+                                final Contentlet existingContent = this.conAPI.findContentletByIdentifier(identifier
+                                        .getId(), true, contentPage.getLanguageId(), this.userAPI.getSystemUser(),
+                                        DONT_RESPECT_FRONT_END_ROLES);
 								if (null != existingContent) {
-									if (existingContent.getStructure()
-											.getStructureType() == Structure.STRUCTURE_TYPE_FILEASSET && !existingContent.getIdentifier().equals(contentPage.getIdentifier())) {
+                                    if (existingContent.getContentType().baseType().equals(BaseContentType.FILEASSET)
+                                            && !existingContent.getIdentifier().equals(contentPage.getIdentifier())) {
 										// Found a file asset with same path
 										status = "message.htmlpage.error.htmlpage.exists.file";
 									} else if (!existingContent.getIdentifier().equals(contentPage.getIdentifier())) {
@@ -651,33 +668,27 @@ public class ContentletWebAPIImpl implements ContentletWebAPI {
 										status = "message.htmlpage.error.htmlpage.exists";
 									}
 								}
-							} catch (DotContentletStateException e) {
+							} catch (final DotContentletStateException e) {
 								// If it's a brand new page...
-								if (!UtilMethods.isSet(contentPage
-										.getIdentifier())) {
+								if (!UtilMethods.isSet(contentPage.getIdentifier())) {
 									// Found page with same path
 									status = "message.htmlpage.error.htmlpage.exists";
-								} else {
-									Logger.info(getClass(),
-											"Page with same URI and same language does not exist, so we are OK");
 								}
 							}
 						}
 					}
 				}
 			}
-		} catch (DotDataException e) {
-			Logger.debug(this,
-					"Error trying to retreive information from page '"
-							+ contentPage.getIdentifier() + "'");
-			throw new DotRuntimeException("Page information is not valid", e);
-		} catch (DotSecurityException e) {
-			Logger.debug(this,
-					"Current user has no permission to perform the selected action on page '"
-							+ contentPage.getIdentifier() + "'");
-			throw new DotRuntimeException(
-					"Current user has no permission to perform the selected action",
-					e);
+        } catch (final DotDataException e) {
+            final String errorMsg = String.format("An error occurred when validating page '%s' [%s]: %s", pageUrl,
+                    contentPage.getIdentifier(), e.getMessage());
+            Logger.error(this, errorMsg, e);
+            throw new DotRuntimeException(errorMsg, e);
+        } catch (final DotSecurityException e) {
+            final String errorMsg = String.format("The current user has no permission to perform the selected action " +
+                    "on page '%s' [%s]: %s", pageUrl, contentPage.getIdentifier(), e.getMessage());
+            Logger.error(this, errorMsg, e);
+            throw new DotRuntimeException(errorMsg, e);
 		}
 		return status;
 	}
