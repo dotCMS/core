31f5c3f4dc542cc7591bb843a5fa2d9b37c7964c
Will Ezell <will@dotcms.com>
2021-01-22 10:19:14 -0500

    #19453 deleting userproxy and the struts classes that had to do with … (#19455)
    * #19453 deleting userproxy and the struts classes that had to do with user management.
    
    * #19453 Fixing ITs
    
    Co-authored-by: Nollymar Longa <>

--- dotCMS/src/main/java/com/dotmarketing/cms/login/factories/LoginFactory.java
+++ dotCMS/src/main/java/com/dotmarketing/cms/login/factories/LoginFactory.java
@@ -9,7 +9,6 @@ import com.dotcms.enterprise.de.qaware.heimdall.PasswordException;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.business.DuplicateUserException;
 import com.dotmarketing.cms.factories.PublicEncryptionFactory;
-import com.dotmarketing.cms.login.struts.LoginForm;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.portal.struts.DotCustomLoginPostAction;
@@ -39,9 +38,7 @@ public class LoginFactory {
 	public static boolean useCASLoginFilter = new Boolean (Config.getBooleanProperty("FRONTEND_CAS_FILTER_ON",false));
 	/*End of Custom Code*/
 
-    public static boolean doLogin(LoginForm form, HttpServletRequest request, HttpServletResponse response) throws NoSuchUserException {
-        return doLogin(form.getUserName(), form.getPassword(), form.isRememberMe(), request, response);
-    }
+
 
     public static boolean doCookieLogin(String encryptedId, HttpServletRequest request, HttpServletResponse response) {
 
