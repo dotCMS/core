2bbbf543b50b166a0a44d4542b65a6aff4d602f7
hmoreras <humberto.morera@dotcms.com>
2021-02-04 15:42:43 -0600

    Revert "final only with gh-pages"
    This reverts commit 053e715715fbc99ebd63bc3e3bdfd3198c6192b3.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,4 @@
 module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-    refs: {
-        dotcmsWebComponents: {
-            title: 'Dotcms Web Componentes',
-            url: 'https://dotcms.github.io/dotcms-webcomponents/'
-        }
-    }
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials']
 };
-
-
