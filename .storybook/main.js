053e715715fbc99ebd63bc3e3bdfd3198c6192b3
hmoreras <humberto.morera@dotcms.com>
2021-02-04 15:36:33 -0600

    final only with gh-pages

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,12 @@
 module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials']
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+    refs: {
+        dotcmsWebComponents: {
+            title: 'Dotcms Web Componentes',
+            url: 'https://dotcms.github.io/dotcms-webcomponents/'
+        }
+    }
 };
+
+
