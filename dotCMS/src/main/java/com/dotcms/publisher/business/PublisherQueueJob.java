1013e70740fe8b709cc001bb6b85bfd0e3982e9b
freddyucv <freddy0309@gmail.com>
2021-02-05 08:33:36 -0600

    Revert "setting 30 minutes success message life time"
    This reverts commit 99e16a753d1a8c1456b15556a748484f94cc286d.

--- dotCMS/src/main/java/com/dotcms/publisher/business/PublisherQueueJob.java
+++ dotCMS/src/main/java/com/dotcms/publisher/business/PublisherQueueJob.java
@@ -44,7 +44,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
 import org.apache.logging.log4j.ThreadContext;
@@ -341,7 +340,7 @@ public class PublisherQueueJob implements StatefulJob {
 			message.setMessage(LanguageUtil.get(
 					notificationMessage,
 					notificationMessageArgument));
-			message.setLife(TimeUnit.MINUTES.toMillis(30));
+			message.setLife(86400000);
 			message.setSeverity(MessageSeverity.ERROR);
 		} else if (groupPushStats.getCountGroupFailed() > 0 && groupPushStats.getCountGroupFailed() == endpointTrackingMap.size()) {
 			// If bundle cannot be installed in all groups
