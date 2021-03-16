4d185fbc3b01cb650da401f918d9d0bfe79fb9fb
Humberto Morera <31667212+hmoreras@users.noreply.github.com>
2021-03-16 17:06:04 -0600

    dotCMS/core#20043 Bring dotcms-field-elements stenciljs project (#1625)
    * skeleton
    
    * dotcms-field-elements source
    
    * breaking changes
    
    * build running
    
    * build running
    
    * make test pass
    
    * improve e2e test
    
    * remove warning fix more tests
    
    * build modifications
    
    * build modifications
    
    * reference update

---
+++ libs/dotcms-field-elements/src/components/dot-error-message/dot-error-message.tsx
@@ -0,0 +1,11 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+    tag: 'dot-error-message',
+    styleUrl: 'dot-error-message.scss'
+})
+export class DotErrorMessageComponent {
+    render() {
+        return <slot />;
+    }
+}
---
+++ libs/dotcms-field-elements/src/utils/index.ts
@@ -0,0 +1,3 @@
+export * from './utils';
+export * from '../test/utils';
+export * from './checkProp';
