1ae3f9a377cc8f4bfaf62a0032c5f6b7e2278989
Ivor <ivorpad@users.noreply.github.com>
2021-03-17 01:10:36 +0100

    Separated storybook links (#1627)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,4 @@
 module.exports = {
     stories: [],
-    addons: ['@storybook/addon-knobs/register', '@storybook/addon-links', '@storybook/addon-essentials'],
-    refs: {
-        dotcmsWebComponents: {
-            title: 'DotCMS Web Components',
-            url: 'https://dotcms.github.io/core-web/dotcms-webcomponents'
-        }
-    }
+    addons: ['@storybook/addon-knobs/register', '@storybook/addon-links', '@storybook/addon-essentials']
 };
