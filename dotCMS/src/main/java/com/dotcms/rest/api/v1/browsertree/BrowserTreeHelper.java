73bce0c9250f2dddce1c0e3af38a72d03c577bfb
Jonathan <jonathan.sanchez@dotcms.com>
2021-04-16 10:01:28 -0600

    dotCMS/core#20190 Create an endpoint to set site browser selected folder
    * #20190 adding the logic to select a folder on the site browser
    
    * #20190 added some refactor and test
    
    * #20190 fix when the browser ajax has not been set
    
    * #20190 adding curl test
    
    * #20190 adding doc
    
    * #20190 adding feedback
    
    * #20190 adding fixes and more curl tests
    
    * #20190 fixes and refactor

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/browsertree/BrowserTreeHelper.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/browsertree/BrowserTreeHelper.java
@@ -1,19 +1,30 @@
 package com.dotcms.rest.api.v1.browsertree;
 
+import com.dotcms.rest.ResponseEntityView;
 import com.dotmarketing.beans.Host;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.business.Treeable;
 import com.dotmarketing.business.UserAPI;
 import com.dotcms.util.TreeableNameComparator;
+import com.dotmarketing.business.web.WebAPILocator;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotSecurityException;
+import com.dotmarketing.portlets.browser.ajax.BrowserAjax;
 import com.dotmarketing.portlets.contentlet.business.HostAPI;
 import com.dotmarketing.portlets.folders.business.FolderAPI;
 import com.dotmarketing.portlets.folders.model.Folder;
+import com.dotmarketing.util.HostUtil;
+import com.dotmarketing.util.UtilMethods;
+import com.dotmarketing.util.WebKeys;
 import com.liferay.portal.model.User;
+import com.liferay.util.StringPool;
+import io.vavr.control.Try;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -73,4 +84,90 @@ public class BrowserTreeHelper {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Tries to find the select folder
+     * @return Optional String, present if the folder select exists
+     * @throws DotDataException
+     * @throws DotSecurityException
+     */
+    public Optional<String> findSelectedFolder (final HttpServletRequest request) throws DotDataException, DotSecurityException {
+
+        final BrowserAjax browserAjax = (BrowserAjax)request.getSession().getAttribute("BrowserAjax");
+        String siteBrowserActiveFolderInode = (String)request.getSession().getAttribute("siteBrowserActiveFolderInode");
+        return Optional.ofNullable(null == siteBrowserActiveFolderInode && null != browserAjax?
+                browserAjax.getActiveFolderInode(): siteBrowserActiveFolderInode);
+    }
+
+    private Optional<Host> findHostFromPath(final String folderPath, final User user) {
+
+        final int hostIndicatorIndex = folderPath.indexOf(HostUtil.HOST_INDICATOR);
+        if (-1 != hostIndicatorIndex) {
+
+            final int nextPathSlashIndex = folderPath.indexOf(StringPool.FORWARD_SLASH, hostIndicatorIndex+2);
+            if (-1 != nextPathSlashIndex) {
+
+                final String hostname = folderPath.substring(hostIndicatorIndex+2, nextPathSlashIndex);
+                final Host   host     = Try.of(()->this.hostAPI.findByName(hostname, user, false)).getOrNull();
+                return Optional.ofNullable(host);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private String getFolderPath(final String folderPath) {
+
+        final int hostIndicatorIndex = folderPath.indexOf(HostUtil.HOST_INDICATOR);
+        if (-1 != hostIndicatorIndex) {
+
+            final int nextPathSlashIndex = folderPath.indexOf(StringPool.FORWARD_SLASH, hostIndicatorIndex+2);
+            if (-1 != nextPathSlashIndex) {
+
+                final String path = folderPath.substring(nextPathSlashIndex);
+                return path;
+            }
+        }
+
+        return folderPath;
+    }
+
+    /**
+     * Set the session configuration to select on the site browser a particular folder
+     * @param request {@link HttpServletRequest}
+     * @param fullFolderPath
+     * @param user
+     * @param respectFrontendRoles
+     * @throws DotDataException
+     * @throws DotSecurityException
+     */
+    public void selectFolder (final HttpServletRequest request, final String fullFolderPath,
+                              final User user, final boolean respectFrontendRoles) throws DotDataException, DotSecurityException {
+
+        final Optional<Host> hostOpt = findHostFromPath(fullFolderPath, user);
+        final Host host              = hostOpt.isPresent()? hostOpt.get(): APILocator.getHostAPI().findDefaultHost(user, respectFrontendRoles);
+        final Host currentHost       = WebAPILocator.getHostWebAPI().getCurrentHostNoThrow(request);
+        final String folderPath      = getFolderPath(fullFolderPath);
+        final Folder folder          = APILocator.getFolderAPI().findFolderByPath(folderPath, host, user, respectFrontendRoles);
+
+        if (null != folder && UtilMethods.isSet(folder.getIdentifier()) && null != host) {
+
+            if (null == currentHost || !host.getIdentifier().equals(currentHost.getIdentifier())) {
+
+                HostUtil.switchSite(request, host);
+            }
+
+            BrowserAjax browserAjax = (BrowserAjax)request.getSession().getAttribute("BrowserAjax");
+            if (null == browserAjax) {
+
+                browserAjax = new BrowserAjax();
+                request.getSession().setAttribute("BrowserAjax", browserAjax);
+            }
+
+            browserAjax.setCurrentOpenFolder(folder.getInode(), host.getIdentifier(), user);
+            request.getSession().setAttribute("siteBrowserActiveFolderInode", folder.getInode());
+        } else {
+
+            throw new IllegalArgumentException("The path seems to be not valid: " + fullFolderPath);
+        }
+    }
 }
