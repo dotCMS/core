39afc8c956274a5a3ec66e5d5eb4aa184b75e619
alfredo-dotcms <37185433+alfredo-dotcms@users.noreply.github.com>
2021-04-22 11:28:47 -0600

    dotCMS/core#19842 issue-19842-reload-all-portlet-when-site-switched (#1652)
    * fix Reload all portlet when user change the site in the top-right selector
    
    * feedback

--- libs/dotcms-js/src/lib/core/site.service.ts
+++ libs/dotcms-js/src/lib/core/site.service.ts
@@ -156,7 +156,10 @@ export class SiteService {
                 method: 'PUT',
                 url: `${this.urls.switchSiteUrl}/${site.identifier}`
             })
-            .subscribe();
+            .pipe(take(1))
+            .subscribe(() => {
+                this.setCurrentSite(site);
+            });
     }
 
     /**
@@ -180,8 +183,10 @@ export class SiteService {
     }
 
     private setCurrentSite(site: Site): void {
-        this.selectedSite = site;
-        this._switchSite$.next({ ...site });
+        if (this.selectedSite?.identifier !== site.identifier) {
+            this.selectedSite = site;
+            this._switchSite$.next({ ...site });
+        }
     }
 
     private loadCurrentSite(): void {
