a561d55d7dd26c411bcc57fc3d9f09b12889f6df
Scott Wicken <78741732+swicken-dotcms@users.noreply.github.com>
2021-04-23 13:37:51 -0400

    #20136: Adding code changes to dotCMS 5.3.8.5 LTS release.

--- dotCMS/src/main/java/com/dotcms/publisher/ajax/RemotePublishAjaxAction.java
+++ dotCMS/src/main/java/com/dotcms/publisher/ajax/RemotePublishAjaxAction.java
@@ -227,6 +227,7 @@ public class RemotePublishAjaxAction extends AjaxAction {
             try {
                  publishDate = dateFormat.parse(_contentPushPublishDate + "-" + _contentPushPublishTime);
             } catch (ParseException ex) {
+                //this is to catch an issue with the format of a date being sent in the front end. This is an LTS fix only and will not impact master.
                  publishDate = dateFormat.parse(new Date().toString());
             }
 
