3b97698d741661b949c53442b55934b0d50a04d3
Erick Gonzalez <erick.gonzalez@dotcms.com>
2021-05-03 12:53:11 -0600

    #19778 tests

--- dotCMS/src/integration-test/java/com/dotcms/rest/api/v1/versionable/VersionableResourceTest.java
+++ dotCMS/src/integration-test/java/com/dotcms/rest/api/v1/versionable/VersionableResourceTest.java
@@ -1,6 +1,8 @@
 package com.dotcms.rest.api.v1.versionable;
 
 import com.dotcms.datagen.ContainerDataGen;
+import com.dotcms.datagen.FolderDataGen;
+import com.dotcms.datagen.LinkDataGen;
 import com.dotcms.datagen.SiteDataGen;
 import com.dotcms.datagen.TemplateDataGen;
 import com.dotcms.datagen.TestDataUtils;
@@ -24,12 +26,14 @@ import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.portlets.containers.model.Container;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
+import com.dotmarketing.portlets.links.model.Link;
 import com.dotmarketing.portlets.templates.model.Template;
 import com.dotmarketing.util.UUIDGenerator;
 import com.ettrema.cache.Cache;
 import com.liferay.portal.model.User;
 import com.liferay.util.Base64;
 import java.util.Collections;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Response;
@@ -225,7 +229,7 @@ public class VersionableResourceTest {
         container.setInode(UUIDGenerator.generateUuid());
         container = APILocator.getContainerAPI().save(container, Collections.emptyList(),newHost,adminUser,false);
         //Call Resource
-        final Response responseResource = resource.findVersion(getHttpRequest(adminUser.getEmailAddress(),"admin"),response,
+        final Response responseResource = resource.findVersionable(getHttpRequest(adminUser.getEmailAddress(),"admin"),response,
                 oldContainerInode);
         //Check that the response is 200
         Assert.assertEquals(Status.OK.getStatusCode(),responseResource.getStatus());
@@ -234,6 +238,33 @@ public class VersionableResourceTest {
         Assert.assertEquals(oldContainerInode,versionableView.getInode());
         Assert.assertEquals(container.getVersionType(),versionableView.getVersionType());
         Assert.assertEquals(container.getTitle(),versionableView.getTitle());
+    }
+
+    /**
+     * Method to test: findVersion in the VersionableResource
+     * Given Scenario: Create and edit a link, this will create an old version of,
+     *                  using the endpoint find the version (the old one).
+     * ExpectedResult: The endpoint should return 200. Check the versionableView against the link.
+     *
+     */
+    @Test
+    public void test_findVersion_link_success() throws DotSecurityException, DotDataException {
+        final Host newHost = new SiteDataGen().nextPersisted();
+        //Create container and a new version
+        Link link = new LinkDataGen(new FolderDataGen().site(newHost).nextPersisted()).nextPersisted();
+        final String oldLinkInode = link.getInode();
+        link.setInode(UUIDGenerator.generateUuid());
+        APILocator.getMenuLinkAPI().save(link,adminUser,false);
+        //Call Resource
+        final Response responseResource = resource.findVersionable(getHttpRequest(adminUser.getEmailAddress(),"admin"),response,
+                oldLinkInode);
+        //Check that the response is 200
+        Assert.assertEquals(Status.OK.getStatusCode(),responseResource.getStatus());
+        final ResponseEntityView entityView = ResponseEntityView.class.cast(responseResource.getEntity());
+        final VersionableView versionableView = VersionableView.class.cast(entityView.getEntity());
+        Assert.assertEquals(oldLinkInode,versionableView.getInode());
+        Assert.assertEquals(link.getVersionType(),versionableView.getVersionType());
+        Assert.assertEquals(link.getTitle(),versionableView.getTitle());
     }
 
     /**
@@ -252,7 +283,7 @@ public class VersionableResourceTest {
         contentlet.setInode(UUIDGenerator.generateUuid());
         contentlet = APILocator.getContentletAPI().checkin(contentlet,adminUser,false);
         //Call Resource
-        final Response responseResource = resource.findVersion(getHttpRequest(adminUser.getEmailAddress(),"admin"),response,
+        final Response responseResource = resource.findVersionable(getHttpRequest(adminUser.getEmailAddress(),"admin"),response,
                 oldContentletInode);
         //Check that the response is 200
         Assert.assertEquals(Status.OK.getStatusCode(),responseResource.getStatus());
@@ -279,7 +310,7 @@ public class VersionableResourceTest {
         template.setInode(UUIDGenerator.generateUuid());
         template = APILocator.getTemplateAPI().saveTemplate(template,newHost,adminUser,false);
         //Call Resource
-        final Response responseResource = resource.findVersion(getHttpRequest(adminUser.getEmailAddress(),"admin"),response,
+        final Response responseResource = resource.findVersionable(getHttpRequest(adminUser.getEmailAddress(),"admin"),response,
                 oldTemplateInode);
         //Check that the response is 200
         Assert.assertEquals(Status.OK.getStatusCode(),responseResource.getStatus());
@@ -308,7 +339,7 @@ public class VersionableResourceTest {
         limitedUser.setPassword(password);
         APILocator.getUserAPI().save(limitedUser,APILocator.systemUser(),false);
         //Call Resource
-        resource.findVersion(getHttpRequest(limitedUser.getEmailAddress(),"admin"),response,
+        resource.findVersionable(getHttpRequest(limitedUser.getEmailAddress(),"admin"),response,
                 container.getInode());
     }
 
@@ -331,7 +362,7 @@ public class VersionableResourceTest {
         limitedUser.setPassword(password);
         APILocator.getUserAPI().save(limitedUser,APILocator.systemUser(),false);
         //Call Resource
-        resource.findVersion(getHttpRequest(limitedUser.getEmailAddress(),"admin"),response,
+        resource.findVersionable(getHttpRequest(limitedUser.getEmailAddress(),"admin"),response,
                 contentlet.getInode());
     }
 
@@ -354,7 +385,7 @@ public class VersionableResourceTest {
         limitedUser.setPassword(password);
         APILocator.getUserAPI().save(limitedUser,APILocator.systemUser(),false);
         //Call Resource
-        resource.findVersion(getHttpRequest(limitedUser.getEmailAddress(),"admin"),response,
+        resource.findVersionable(getHttpRequest(limitedUser.getEmailAddress(),"admin"),response,
                 template.getInode());
     }
 
@@ -368,7 +399,171 @@ public class VersionableResourceTest {
     public void test_findVersion_inodeDoesNotExist_return404()
             throws DotSecurityException, DotDataException {
         //Call Resource
-        resource.findVersion(getHttpRequest(adminUser.getEmailAddress(),"admin"),response,
+        resource.findVersionable(getHttpRequest(adminUser.getEmailAddress(),"admin"),response,
+                UUIDGenerator.generateUuid());
+    }
+
+    /**
+     * Method to test: findVersion in the VersionableResource
+     * Given Scenario: Create and edit a container, this will create an old version of,
+     *                  using the endpoint find all the versions.
+     * ExpectedResult: The endpoint should return 200.
+     *
+     */
+    @Test
+    public void test_findAllVersions_container_success() throws DotSecurityException, DotDataException {
+        final Host newHost = new SiteDataGen().nextPersisted();
+        //Create container and a new version
+        Container container = new ContainerDataGen().site(newHost).nextPersisted();
+        container.setInode(UUIDGenerator.generateUuid());
+        container = APILocator.getContainerAPI().save(container, Collections.emptyList(),newHost,adminUser,false);
+        //Call Resource
+        final Response responseResource = resource.findVersionable(getHttpRequest(adminUser.getEmailAddress(),"admin"),response,
+                container.getIdentifier());
+        //Check that the response is 200
+        Assert.assertEquals(Status.OK.getStatusCode(),responseResource.getStatus());
+        final ResponseEntityView entityView = ResponseEntityView.class.cast(responseResource.getEntity());
+        final List<VersionableView> allVersions = List.class.cast(entityView.getEntity());
+        Assert.assertEquals(2,allVersions.size());
+        Assert.assertEquals(container.getTitle(),allVersions.get(0).getTitle());
+        Assert.assertEquals(container.getVersionType(),allVersions.get(1).getVersionType());
+    }
+
+    /**
+     * Method to test: findVersion in the VersionableResource
+     * Given Scenario: Create and edit a link, this will create an old version of,
+     *                  using the endpoint find all the versions.
+     * ExpectedResult: The endpoint should return 200.
+     *
+     */
+    @Test
+    public void test_findAllVersions_link_success() throws DotSecurityException, DotDataException {
+        final Host newHost = new SiteDataGen().nextPersisted();
+        //Create container and a new version
+        Link link = new LinkDataGen(new FolderDataGen().site(newHost).nextPersisted()).nextPersisted();
+        link.setInode(UUIDGenerator.generateUuid());
+        APILocator.getMenuLinkAPI().save(link,adminUser,false);
+        //Call Resource
+        final Response responseResource = resource.findVersionable(getHttpRequest(adminUser.getEmailAddress(),"admin"),response,
+                link.getIdentifier());
+        //Check that the response is 200
+        Assert.assertEquals(Status.OK.getStatusCode(),responseResource.getStatus());
+        final ResponseEntityView entityView = ResponseEntityView.class.cast(responseResource.getEntity());
+        final List<VersionableView> allVersions = List.class.cast(entityView.getEntity());
+        Assert.assertEquals(2,allVersions.size());
+        Assert.assertEquals(link.getTitle(),allVersions.get(0).getTitle());
+        Assert.assertEquals(link.getVersionType(),allVersions.get(1).getVersionType());
+    }
+
+    /**
+     * Method to test: findVersion in the VersionableResource
+     * Given Scenario: Create and edit a contentlet, this will create an old version of,
+     *                  using the endpoint find all the versions.
+     * ExpectedResult: The endpoint should return 200.
+     *
+     */
+    @Test
+    public void test_findAllVersions_contentlet_success() throws DotSecurityException, DotDataException {
+        final Host newHost = new SiteDataGen().nextPersisted();
+        //Create contentlet and a new version
+        Contentlet contentlet = TestDataUtils.getGenericContentContent(true,1,newHost);
+        contentlet.setInode(UUIDGenerator.generateUuid());
+        contentlet = APILocator.getContentletAPI().checkin(contentlet,adminUser,false);
+        //Call Resource
+        final Response responseResource = resource.findVersionable(getHttpRequest(adminUser.getEmailAddress(),"admin"),response,
+                contentlet.getIdentifier());
+        //Check that the response is 200
+        Assert.assertEquals(Status.OK.getStatusCode(),responseResource.getStatus());
+        final ResponseEntityView entityView = ResponseEntityView.class.cast(responseResource.getEntity());
+        final List<VersionableView> allVersions = List.class.cast(entityView.getEntity());
+        Assert.assertEquals(2,allVersions.size());
+        Assert.assertEquals(contentlet.getTitle(),allVersions.get(0).getTitle());
+        Assert.assertEquals(contentlet.getVersionType(),allVersions.get(1).getVersionType());
+    }
+
+    /**
+     * Method to test: findVersion in the VersionableResource
+     * Given Scenario: Create and edit a template, this will create an old version of,
+     *                  using the endpoint find all the versions.
+     * ExpectedResult: The endpoint should return 200.
+     *
+     */
+    @Test
+    public void test_findAllVersions_template_success() throws DotSecurityException, DotDataException {
+        final Host newHost = new SiteDataGen().nextPersisted();
+        //Create template and a new version
+        Template template = new TemplateDataGen().site(newHost).nextPersisted();
+        template.setInode(UUIDGenerator.generateUuid());
+        template = APILocator.getTemplateAPI().saveTemplate(template,newHost,adminUser,false);
+        //Call Resource
+        final Response responseResource = resource.findVersionable(getHttpRequest(adminUser.getEmailAddress(),"admin"),response,
+                template.getIdentifier());
+        //Check that the response is 200
+        Assert.assertEquals(Status.OK.getStatusCode(),responseResource.getStatus());
+        final ResponseEntityView entityView = ResponseEntityView.class.cast(responseResource.getEntity());
+        final List<VersionableView> allVersions = List.class.cast(entityView.getEntity());
+        Assert.assertEquals(2,allVersions.size());
+        Assert.assertEquals(template.getTitle(),allVersions.get(0).getTitle());
+        Assert.assertEquals(template.getVersionType(),allVersions.get(1).getVersionType());
+    }
+
+    /**
+     * Method to test: findVersion in the VersionableResource
+     * Given Scenario: Create and edit a contentlet, this will create an old version of,
+     *                   using the endpoint find all the versions but as a limited user.
+     * ExpectedResult: The endpoint should return 403, because the user needs View/Read Permission to
+     *                   find all versions.
+     *
+     */
+    @Test (expected = DotSecurityException.class)
+    public void test_findAllVersions_limitedUserWithoutViewPermission_contentlet_return403() throws DotSecurityException, DotDataException {
+        final Host newHost = new SiteDataGen().nextPersisted();
+        //Create contentlet and a new version
+        final Contentlet contentlet = TestDataUtils.getGenericContentContent(true,1,newHost);
+        //Create the limited user
+        final User limitedUser = new UserDataGen().roles(TestUserUtils.getFrontendRole(), TestUserUtils.getBackendRole()).nextPersisted();
+        final String password = "admin";
+        limitedUser.setPassword(password);
+        APILocator.getUserAPI().save(limitedUser,APILocator.systemUser(),false);
+        //Call Resource
+        resource.findVersionable(getHttpRequest(limitedUser.getEmailAddress(),"admin"),response,
+                contentlet.getIdentifier());
+    }
+
+    /**
+     * Method to test: findVersion in the VersionableResource
+     * Given Scenario: Create and edit a template, this will create an old version of,
+     *                   using the endpoint find all the versions but as a limited user.
+     * ExpectedResult: The endpoint should return 403, because the user needs View/Read Permission to
+     *                   find all versions.
+     *
+     */
+    @Test (expected = DotSecurityException.class)
+    public void test_findAllVersions_limitedUserWithoutViewPermission_template_return403() throws DotSecurityException, DotDataException {
+        final Host newHost = new SiteDataGen().nextPersisted();
+        //Create template and a new version
+        final Template template = new TemplateDataGen().site(newHost).nextPersisted();
+        //Create the limited user
+        final User limitedUser = new UserDataGen().roles(TestUserUtils.getFrontendRole(), TestUserUtils.getBackendRole()).nextPersisted();
+        final String password = "admin";
+        limitedUser.setPassword(password);
+        APILocator.getUserAPI().save(limitedUser,APILocator.systemUser(),false);
+        //Call Resource
+        resource.findVersionable(getHttpRequest(limitedUser.getEmailAddress(),"admin"),response,
+                template.getIdentifier());
+    }
+
+    /**
+     * Method to test: findVersion in the VersionableResource
+     * Given Scenario: find a version of a non-existent uuid
+     * ExpectedResult: The endpoint should return 404, because the uuid does not belong to any element.
+     *
+     */
+    @Test(expected = DoesNotExistException.class)
+    public void test_findAllVersion_identifierDoesNotExist_return404()
+            throws DotSecurityException, DotDataException {
+        //Call Resource
+        resource.findVersionable(getHttpRequest(adminUser.getEmailAddress(),"admin"),response,
                 UUIDGenerator.generateUuid());
     }
 }
