2dc56b06e98090fbee4b67cdb06c4ecde5b23b20
freddyucv <freddy0309@gmail.com>
2021-05-25 11:34:06 -0600

    Refactoring

--- dotCMS/src/main/java/com/dotcms/publisher/util/dependencies/DependencyAssetProcessor.java
+++ dotCMS/src/main/java/com/dotcms/publisher/util/dependencies/DependencyAssetProcessor.java
@@ -1,5 +1,5 @@
 package com.dotcms.publisher.util.dependencies;
 
 public interface DependencyAssetProcessor<T> {
-    void process(T asset) throws DependencyProcessorException;
+    void process(T asset);
 }
