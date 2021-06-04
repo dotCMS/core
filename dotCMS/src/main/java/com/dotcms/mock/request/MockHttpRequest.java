2cf67d851c96323bae9e18f10ceb00060f926209
freddyucv <freddy0309@gmail.com>
2021-06-04 10:46:00 -0600

    Not using mockito to render page

--- dotCMS/src/main/java/com/dotcms/mock/request/MockHttpRequest.java
+++ dotCMS/src/main/java/com/dotcms/mock/request/MockHttpRequest.java
@@ -1,10 +1,9 @@
 package com.dotcms.mock.request;
 
+import com.dotcms.repackage.org.directwebremoting.util.FakeHttpServletRequest;
+import java.util.Enumeration;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
-import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 import com.dotmarketing.util.UtilMethods;
 import com.liferay.util.StringPool;
 import java.nio.charset.Charset;
@@ -13,6 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
+
 
 
 /**
@@ -33,33 +33,26 @@ public class MockHttpRequest implements MockRequest {
         
         final String uri = UtilMethods.isSet(incomingUri) ? incomingUri : StringPool.FORWARD_SLASH;
         final String hostname = UtilMethods.isSet(incomingHostname) ? incomingHostname : "localhost";
-        HttpServletRequest mockReq = new BaseRequest().request();
-        Mockito.when(mockReq.getRequestURI()).thenReturn(uri);
-        Mockito.when(mockReq.getRequestURL()).thenReturn(new StringBuffer("http://" + hostname + uri));
-        Mockito.when(mockReq.getServerName()).thenReturn(hostname);
-        Mockito.when(mockReq.getRemoteAddr()).thenReturn("127.0.0.1");
-        Mockito.when(mockReq.getRemoteHost()).thenReturn("127.0.0.1");
+        DotCMSMockRequest mockReq = new DotCMSMockRequest();
+        mockReq.setRequestURI(uri);
+        mockReq.setRequestURL(new StringBuffer("http://" + hostname + uri));
+        mockReq.setServerName(hostname);
+        mockReq.setRemoteAddr("127.0.0.1");
+        mockReq.setRemoteHost("127.0.0.1");
+
         paramMap = new HashMap<>();
         if(uri.contains("?")) {
             final String queryString = uri.substring(uri.indexOf("?") + 1, uri.length());
-            Mockito.when(mockReq.getQueryString()).thenReturn(queryString);
+
             List<NameValuePair> additional = URLEncodedUtils.parse(queryString, Charset.forName("UTF-8"));
             for(NameValuePair nvp : additional) {
                 paramMap.compute(nvp.getName(), (k, v) -> (v == null) ? new String[] {nvp.getValue()} : new String[]{nvp.getValue(),v[0]});
             }
-            Mockito.when(mockReq.getQueryString()).thenReturn(queryString);
-            Mockito.when(mockReq.getParameterMap()).thenReturn(paramMap);
-            Mockito.when(mockReq.getParameterNames()).thenReturn(Collections.enumeration(paramMap.keySet()));
-            Mockito.when(mockReq.getParameter(Mockito.anyString())).thenAnswer(new Answer<String>() {
-                public String answer(InvocationOnMock invocation) {
-                    Object[] args = invocation.getArguments();
-                    Object mock = invocation.getMock();
-                    String[] answer = paramMap.get((String)args[0]);
-                    return answer!=null && answer.length>0 ? answer[0] : null;
-                    }
-                });
+
+            mockReq.setQueryString(queryString);
+            mockReq.setParameterMap(paramMap);
         }
-        Mockito.when(mockReq.getHeader("Host")).thenReturn(hostname);
+        mockReq.addHeader("Host", hostname);
         
         request = new MockHeaderRequest(new MockSessionRequest(new MockAttributeRequest(mockReq)));
     }
@@ -70,6 +63,99 @@ public class MockHttpRequest implements MockRequest {
         return request;
     }
 
+    private class DotCMSMockRequest extends FakeHttpServletRequest {
 
+        private String uri;
+        private StringBuffer requestURL;
+        private String serverName;
+        private String remoteAddr;
+        private String remoteHost;
+        private String queryString;
+        private Map<String, String[]> paramMap;
+        private Map<String, String> headers = new HashMap<>();
+
+        @Override
+        public String getRequestURI() {
+            return uri;
+        }
+
+        @Override
+        public StringBuffer getRequestURL() {
+            return requestURL;
+        }
+
+        @Override
+        public String getServerName() {
+            return serverName;
+        }
+
+        @Override
+        public String getRemoteAddr() {
+            return remoteAddr;
+        }
+
+        @Override
+        public String getRemoteHost() {
+            return remoteHost;
+        }
+
+        @Override
+        public String getQueryString() {
+            return queryString;
+        }
+
+        @Override
+        public Map<String, String[]> getParameterMap() {
+            return paramMap;
+        }
+
+        @Override
+        public Enumeration<String> getParameterNames() {
+            return Collections.enumeration(paramMap.keySet());
+        }
+
+        @Override
+        public String getParameter(final String name) {
+            final String[] answer = paramMap.get(name);
+            return answer!=null && answer.length > 0 ? answer[0] : null;
+        }
+
+        @Override
+        public String getHeader(final String name) {
+            return headers.get(name);
+        }
+
+        public void addHeader(final String name, final String value) {
+            this.headers.put(name, value);
+        }
+
+        public void setRequestURI(final String uri) {
+            this.uri = uri;
+        }
+
+        public void setRequestURL(final StringBuffer requestURL) {
+            this.requestURL = requestURL;
+        }
+
+        public void setServerName(final String serverName) {
+            this.serverName = serverName;
+        }
+
+        public void setRemoteAddr(final String remoteAddr) {
+            this.remoteAddr = remoteAddr;
+        }
+
+        public void setRemoteHost(final String remoteHost) {
+            this.remoteHost = remoteHost;
+        }
+
+        public void setQueryString(final String queryString) {
+            this.queryString = queryString;
+        }
+
+        public void setParameterMap(final Map<String, String[]> paramMap) {
+            this.paramMap = paramMap;
+        }
+    }
 
 }
