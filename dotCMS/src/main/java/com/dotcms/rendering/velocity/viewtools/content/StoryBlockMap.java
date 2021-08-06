6459b5d4da58064ef3ec7cac837033b5a743cd1d
Erick Gonzalez <erick.gonzalez@dotcms.com>
2021-08-06 13:02:54 -0600

    #20678 map to get storyblock properties

---
+++ dotCMS/src/main/java/com/dotcms/rendering/velocity/viewtools/content/StoryBlockMap.java
@@ -0,0 +1,44 @@
+package com.dotcms.rendering.velocity.viewtools.content;
+
+
+import com.dotcms.contenttype.transform.field.LegacyFieldTransformer;
+import com.dotcms.repackage.org.codehaus.jettison.json.JSONException;
+import com.dotcms.repackage.org.codehaus.jettison.json.JSONObject;
+import com.dotmarketing.business.APILocator;
+import com.dotmarketing.portlets.contentlet.model.Contentlet;
+import com.dotmarketing.portlets.structure.model.Field;
+import com.liferay.util.StringPool;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+public class StoryBlockMap {
+
+    private String type = StringPool.BLANK;
+    private String render = StringPool.BLANK;
+    private String content = StringPool.BLANK;
+
+    public StoryBlockMap(final Field field,final Contentlet contentlet) throws JSONException {
+        final com.dotcms.contenttype.model.field.Field fieldTransformed = new LegacyFieldTransformer(field).from();
+        final Object contFieldValue = APILocator.getContentletAPI().getFieldValue(contentlet,fieldTransformed);
+        final JSONObject jsonContFieldValue = new JSONObject(contFieldValue.toString());
+        type = jsonContFieldValue.get("type").toString();
+        render = jsonContFieldValue.get("render").toString();
+        content = jsonContFieldValue.get("content").toString();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getRender() {
+        return render;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+}
