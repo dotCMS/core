2299a9b5cbfce814bf07d45df9f3e0df5c5d24ec
Fabrizzio Araya <37148755+fabrizzio-dotCMS@users.noreply.github.com>
2021-08-30 08:44:13 -0600

    #20777 workflow email improvements (#20831)
    * #20777 workflow email improvements
    
    * #20777  imports clean up
    
    * #20777 applying feedback plus changes discussed on PO meeting.
    
    * #20777 refactoring to reuse code better
    
    * #20777  no need to extend from validated. Since validation has been moved into a method
    
    * #20777 params were backwards
    
    * #20777 cleanup jsp
    
    * #20777 updating adding Test into MainSuite

--- dotCMS/src/integration-test/java/com/dotcms/MainSuite.java
+++ dotCMS/src/integration-test/java/com/dotcms/MainSuite.java
@@ -53,6 +53,7 @@ import com.dotcms.rest.api.v1.apps.view.AppsInterpolationTest;
 import com.dotcms.rest.api.v1.authentication.ResetPasswordTokenUtilTest;
 import com.dotcms.rest.api.v1.folder.FolderResourceTest;
 import com.dotcms.rest.api.v1.pushpublish.PushPublishFilterResourceTest;
+import com.dotcms.rest.api.v1.system.ConfigurationHelperTest;
 import com.dotcms.rest.api.v1.user.UserResourceIntegrationTest;
 import com.dotcms.saml.IdentityProviderConfigurationFactoryTest;
 import com.dotcms.saml.SamlConfigurationServiceTest;
@@ -87,6 +88,7 @@ import com.dotmarketing.portlets.templates.business.TemplateFactoryImplTest;
 import com.dotmarketing.portlets.workflows.actionlet.MoveContentActionletTest;
 import com.dotmarketing.portlets.workflows.actionlet.PushNowActionletTest;
 import com.dotmarketing.portlets.workflows.model.TestWorkflowAction;
+import com.dotmarketing.portlets.workflows.util.WorkflowEmailUtilTest;
 import com.dotmarketing.quartz.DotStatefulJobTest;
 import com.dotmarketing.quartz.job.CleanUpFieldReferencesJobTest;
 import com.dotmarketing.quartz.job.IntegrityDataGenerationJobTest;
@@ -454,8 +456,11 @@ import org.junit.runners.Suite.SuiteClasses;
         BundlePublisherTest.class,
         CategoryFactoryTest.class,
         Task210805DropUserProxyTableTest.class,
+        Task210816DeInodeRelationshipTest.class,
         BundlerUtilTest.class,
-        Task210816DeInodeRelationshipTest.class
+        Task210816DeInodeRelationshipTest.class,
+        WorkflowEmailUtilTest.class,
+        ConfigurationHelperTest.class
 })
 public class MainSuite {
 
