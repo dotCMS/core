0c9580dec69e5c7440f52bf5bb583aae6db1fd87
jdotcms <jonathan.sanchez@dotcms.com>
2021-09-01 14:49:31 -0600

    #20891 adding the config ger vars config

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/system/ConfigurationResource.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/system/ConfigurationResource.java
@@ -4,12 +4,20 @@ import static com.dotcms.rest.ResponseEntityView.OK;
 
 import com.dotcms.rest.InitDataObject;
 import com.dotcms.rest.WebResource.InitBuilder;
+import com.dotmarketing.util.StringUtils;
+import com.google.common.collect.ImmutableSet;
 import io.vavr.Tuple2;
+
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Locale;
 import java.util.Map;
 
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
 
 import javax.servlet.http.HttpServletResponse;
@@ -21,6 +29,7 @@ import javax.ws.rs.core.Response;
 import com.dotcms.rest.WebResource;
 import com.dotmarketing.business.Role;
 import com.dotmarketing.util.Config;
+import io.vavr.control.Try;
 import org.glassfish.jersey.server.JSONP;
 import com.dotcms.rest.ResponseEntityView;
 import com.dotcms.rest.annotation.NoCache;
@@ -48,11 +57,66 @@ public class ConfigurationResource implements Serializable {
 
 	private final ConfigurationHelper helper;
 
+	private static final Pattern BLACK_LIST = Try.of(()->Pattern.compile(
+			Config.getStringProperty("OBFUSCATE_SYSTEM_ENVIRONMENTAL_VARIABLES", "passw|pass|passwd|secret|key|token"),
+			Pattern.CASE_INSENSITIVE)).getOrNull();
+
+	private static final Set<String> WHITE_LIST = ImmutableSet.copyOf(
+			Config.getStringArrayProperty("CONFIGURATION_WHITE_LIST",
+					new String[] {"EMAIL_SYSTEM_ADDRESS", "CHARSET","DEFAULT_LANGUAGE_COUNTRY","DEFAULT_LANGUAGE",
+					"DEFAULT_LANGUAGE_CODE","DEFAULT_LANGUAGE_STR", "DEFAULT_LANGUAGE_COUNTRY_CODE", "CMS_STRUTS_PATH",
+					"PATH_TO_REDIRECT", "PATH_TO_IMAGES", "REPORT_PATH", "ASSET_PATH", "CONTENT_AUTOSAVE_INTERVAL",
+					"DEFAULT_HEIGHT","DEFAULT_WIDTH", "DEFAULT_BG_R_COLOR", "DEFAULT_BG_G_COLOR", "DEFAULT_BG_B_COLOR",
+					"ACCRUE_TAGS_IN_URLMAPS","ACCRUE_TAGS_IN_PAGES", "ACCRUE_TAGS_IN_CONTENTS_ON_PAGE", "PULLPERSONALIZED_PERSONA_WEIGHT",
+					"ASSETS_SEARCH_AND_REPLACE_ALLOWED_FILE_TYPES","CONTENT_VERSION_HARD_LINK", "CONTENT_ALLOW_ZERO_LENGTH_FILES", "DEFAULT_PAGE_CACHE_SECONDS",
+					"ENABLE_NAV_PERMISSION_CHECK","CONTENT_ESCAPE_HTML_TEXT", "CMS_INDEX_PAGE", "DEFAULT_REST_PAGE_COUNT",	"HEADLESS_USER_CONTENT_DELIVERY",
+					"DEFAULT_VANITY_URL_TO_DEFAULT_LANGUAGE","WHITELISTED_HEADERS", "WHITELISTED_PARAMS", "WHITELISTED_COOKIES"}));
+
+
+
+	private boolean isOnBlackList(final String key) {
+
+		return null != BLACK_LIST? BLACK_LIST.matcher(key).find() : false;
+	}
 	/**
 	 * Default constructor.
 	 */
 	public ConfigurationResource() {
 		this.helper = ConfigurationHelper.INSTANCE;
+	}
+
+	@Path("/config")
+	@GET
+	@JSONP
+	@NoCache
+	@Produces({MediaType.APPLICATION_JSON, "application/javascript"})
+	public final Response getConfigVariables(@Context final HttpServletRequest request,
+											 @Context final HttpServletResponse response,
+											 @QueryParam("keys") final String keysQuery)
+			throws IOException {
+
+		// todo: not sure about this
+		final InitDataObject initData = new WebResource.InitBuilder(request, response)
+				.requiredBackendUser(true)
+				.requestAndResponse(request, response)
+				.rejectWhenNoUser(true)
+				.requiredPortlet("maintenance")
+				.init();
+
+		final String [] keys = StringUtils.splitByCommas(keysQuery);
+		final Map<String,Object> resultMap = new LinkedHashMap<>();
+		if (null != keys) {
+
+			for (final String key : keys) {
+
+				if (this.WHITE_LIST.contains(key) && !this.isOnBlackList(key)) {
+
+					resultMap.put(key, Config.getStringProperty(key, "NOT_FOUND"));
+				}
+			}
+		}
+
+		return Response.ok(resultMap).build();
 	}
 
 	/**
