a1b7c4f124b6b27b03bf950a6b6b79d79c172598
Nollymar Longa <>
2021-09-09 13:40:10 -0500

    #20899 Doc

--- dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task210901UpdateDateTimezonesTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task210901UpdateDateTimezonesTest.java
@@ -13,12 +13,18 @@ public class Task210901UpdateDateTimezonesTest {
         IntegrationTestInitService.getInstance().init();
     }
 
+    /**
+     * <b>Method to Test:</b> {@link Task210901UpdateDateTimezones#executeUpgrade()} <p>
+     * <b>Given Scenario:</b> When postgres is used, the upgrade task should be executed <p>
+     * <b>Expected Result:</b> When using postgres, dates should be declared as timestamps with timezone
+     * @throws Exception
+     */
     @Test
     public void test() throws Exception {
         Task210901UpdateDateTimezones tztask = new Task210901UpdateDateTimezones();
         if(tztask.forceRun()) {
             tztask.executeUpgrade();
-        };
+        }
         assertTrue("Timezones have been updated",true);
     }
 
