ef7b114fbd09301100aeca92a8b0c57d8996d3c8
Daniel Silva <daniel.silva@dotcms.com>
2021-09-13 14:13:44 -0500

    #20948 undo changes

--- dotCMS/src/main/java/com/dotcms/content/elasticsearch/business/ESContentFactoryImpl.java
+++ dotCMS/src/main/java/com/dotcms/content/elasticsearch/business/ESContentFactoryImpl.java
@@ -1919,9 +1919,9 @@ public class ESContentFactoryImpl extends ContentletFactory {
 
         final String inode = getInode(existingInode, contentlet);
         upsertContentlet(contentlet, inode);
-        final Contentlet toReturn = findInDb(inode).orElseThrow(()->
-                new DotStateException(String.format("Contentlet with inode '%s' not found in DB", inode)));
         contentlet.setInode(inode);
+//        final Contentlet toReturn = findInDb(inode).orElseThrow(()->
+//                new DotStateException(String.format("Contentlet with inode '%s' not found in DB", inode)));
 
         REMOVABLE_KEY_SET.forEach(key -> contentlet.getMap().remove(key));
 
