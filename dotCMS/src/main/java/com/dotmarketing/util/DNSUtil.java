13e95dda91e2d57059c9bf4c024cd6d68de8ff28
spbolton <steve.bolton@dotcms.com>
2021-09-20 18:18:15 +0000

    Revert "#20940 unrepackaged dnsutils (#20953)"
    This reverts commit 92ebbc85acb50da254cc5e75ee736c1b922d383b.

--- dotCMS/src/main/java/com/dotmarketing/util/DNSUtil.java
+++ dotCMS/src/main/java/com/dotmarketing/util/DNSUtil.java
@@ -2,19 +2,19 @@ package com.dotmarketing.util;
 
 import java.io.IOException;
 
-import org.xbill.DNS.DClass;
-import org.xbill.DNS.ExtendedResolver;
-import org.xbill.DNS.Message;
-import org.xbill.DNS.Name;
-import org.xbill.DNS.Record;
-import org.xbill.DNS.Resolver;
-import org.xbill.DNS.ReverseMap;
-import org.xbill.DNS.Section;
-import org.xbill.DNS.Type;
+import com.dotcms.repackage.org.xbill.DNS.DClass;
+import com.dotcms.repackage.org.xbill.DNS.ExtendedResolver;
+import com.dotcms.repackage.org.xbill.DNS.Message;
+import com.dotcms.repackage.org.xbill.DNS.Name;
+import com.dotcms.repackage.org.xbill.DNS.Record;
+import com.dotcms.repackage.org.xbill.DNS.Resolver;
+import com.dotcms.repackage.org.xbill.DNS.ReverseMap;
+import com.dotcms.repackage.org.xbill.DNS.Section;
+import com.dotcms.repackage.org.xbill.DNS.Type;
 
 public class DNSUtil {
 	public static String reverseDns(String hostIp) throws IOException {
-
+		Record opt = null;
 		Resolver res = new ExtendedResolver();
 		res.setTimeout(10);
 		Name name = ReverseMap.fromAddress(hostIp);
