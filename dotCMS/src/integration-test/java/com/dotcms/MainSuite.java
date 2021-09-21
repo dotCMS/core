0cb207ef0c6915ef92a6b6e77b4500f0361841c4
freddyucv <freddy0309@gmail.com>
2021-09-21 11:42:24 -0600

    FIxing MainSuite

--- dotCMS/src/integration-test/java/com/dotcms/MainSuite.java
+++ dotCMS/src/integration-test/java/com/dotcms/MainSuite.java
@@ -44,6 +44,7 @@ import com.dotcms.publishing.PushPublishFiltersInitializerTest;
 import com.dotcms.publishing.job.SiteSearchJobImplTest;
 import com.dotcms.publishing.manifest.CSVManifestBuilderTest;
 import com.dotcms.publishing.manifest.CSVManifestReaderTest;
+import com.dotcms.publishing.manifest.ManifestReaderFactoryTest;
 import com.dotcms.rendering.velocity.directive.DotParseTest;
 import com.dotcms.rendering.velocity.servlet.VelocityServletIntegrationTest;
 import com.dotcms.rendering.velocity.viewtools.DotTemplateToolTest;
