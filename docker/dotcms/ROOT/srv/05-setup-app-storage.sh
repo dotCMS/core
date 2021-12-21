9f0b1bf79f1ea78ac515c73329be716db8a38e96
Will Ezell <will@dotcms.com>
2021-12-21 10:20:11 -0500

    Issue 21440 docker uid (#21444)
    * #21440 updating uid/gid to 65001
    
    * #21440 updating uid/gid to 65001

--- docker/dotcms/ROOT/srv/05-setup-app-storage.sh
+++ docker/dotcms/ROOT/srv/05-setup-app-storage.sh
@@ -10,24 +10,4 @@ source /srv/utils/config-defaults.sh
 [[ ! -d /data/shared/felix/load ]] && mkdir -p /data/shared/felix/load
 [[ ! -d /data/shared/felix/undeployed ]] && mkdir -p /data/shared/felix/undeployed
 
-ASSET_USER_ID=$( stat -c %u /data/shared/assets )
-FELIX_USER_ID=$( stat -c %u /data/shared/felix/load )
 
-
-
-echo "found /data/shared/assets directory owner: $ASSET_USER_ID"
-if [[ ! "$CMS_RUNAS_UID" -eq $ASSET_USER_ID ]]; then
-    echo "Updating asset directory owner to $CMS_RUNAS_UID:$CMS_RUNAS_GID"
-    chown $CMS_RUNAS_UID:$CMS_RUNAS_GID /data/shared/assets &
-fi
-
-
-if [[ ! "$CMS_RUNAS_UID" -eq $FELIX_USER_ID ]]; then
-    echo "Updating felix directory owner to $CMS_RUNAS_UID:$CMS_RUNAS_GID"
-    chown $CMS_RUNAS_UID:$CMS_RUNAS_GID /data/shared/felix/load &
-    chown $CMS_RUNAS_UID:$CMS_RUNAS_GID /data/shared/felix/undeployed &
-fi
-
-
-
-mkdir -p /srv/home
