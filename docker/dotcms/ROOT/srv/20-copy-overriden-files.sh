4c0a5e235346f6357ee0e127265c7b01ea928b10
Will Ezell <will@dotcms.com>
2021-12-22 12:10:38 -0500

    #21454 cp log4j

--- docker/dotcms/ROOT/srv/20-copy-overriden-files.sh
+++ docker/dotcms/ROOT/srv/20-copy-overriden-files.sh
@@ -2,4 +2,5 @@
 
 set -e
 
-cp /srv/OVERRIDE/tomcat/conf/* $TOMCAT_HOME/conf/
+cp /srv/OVERRIDE/tomcat/conf/*      $TOMCAT_HOME/conf/
+cp /srv/OVERRIDE/WEB-INF/log4j/*    $TOMCAT_HOME/webapps/ROOT/WEB-INF/log4j/
