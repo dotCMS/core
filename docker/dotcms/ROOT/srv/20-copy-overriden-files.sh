e3fbedaa8937ef61dfbcda218ec9bc5a576cbf68
nollymar <nollymarlonga@Nollymars-MacBook-Pro-2.local>
2022-01-24 10:51:33 -0500

    #21454 Copying server.xml to the right path

--- docker/dotcms/ROOT/srv/20-copy-overriden-files.sh
+++ docker/dotcms/ROOT/srv/20-copy-overriden-files.sh
@@ -2,5 +2,9 @@
 
 set -e
 
+[[ -f /srv/TOMCAT_VERSION ]] && TOMCAT_VERSION=$( cat /srv/TOMCAT_VERSION )
+
 cp /srv/OVERRIDE/tomcat/conf/*      $TOMCAT_HOME/conf/
+mv $TOMCAT_HOME/conf/server-${TOMCAT_VERSION}.xml $TOMCAT_HOME/conf/server.xml && \
+rm -f $TOMCAT_HOME/conf/server-*.xml
 cp /srv/OVERRIDE/WEB-INF/log4j/*    $TOMCAT_HOME/webapps/ROOT/WEB-INF/log4j/
