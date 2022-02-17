236932049a91a3e7be1b1c198f4dc21b2dcc9d39
Will Ezell <will@dotcms.com>
2022-02-17 07:44:05 -0500

    update defaults

--- docker/dotcms/ROOT/srv/20-dotcms-environment.sh
+++ docker/dotcms/ROOT/srv/20-dotcms-environment.sh
@@ -6,9 +6,9 @@ source /srv/utils/config-defaults.sh
 
 echo "dotCMS environment ...."
 # Default opts
-JAVA_OPTS="-Djava.awt.headless=true -Xverify:none -Dfile.encoding=UTF8 -server -XX:+DisableExplicitGC"
+JAVA_OPTS="-Djava.awt.headless=true -Xverify:none -Dfile.encoding=UTF8 -server -XX:+DisableExplicitGC -Dsun.reflect.inflationThreshold=2147483647 -XX:+UseShenandoahGC -XX:+UnlockExperimentalVMOptions -XX:ShenandoahUncommitDelay=5000 -XX:ShenandoahGuaranteedGCInterval=10000"
 # Memory opts
-JAVA_OPTS="$JAVA_OPTS -Xms${CMS_HEAP_SIZE} -Xmx${CMS_HEAP_SIZE}"
+JAVA_OPTS="$JAVA_OPTS -Xmx${CMS_HEAP_SIZE}"
 
 # Agent opts
 JAVA_OPTS="$JAVA_OPTS -javaagent:${TOMCAT_HOME}/webapps/ROOT/WEB-INF/lib/byte-buddy-agent-1.9.0.jar"
