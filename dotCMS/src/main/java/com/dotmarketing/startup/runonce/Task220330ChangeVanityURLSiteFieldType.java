144097099e63897bd869b684a5593c694355966a
freddyucv <freddy0309@gmail.com>
2022-03-30 11:57:55 -0600

    #21889 Adding the Task Upgrade

---
+++ dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task220330ChangeVanityURLSiteFieldType.java
@@ -0,0 +1,42 @@
+package com.dotmarketing.startup.runonce;
+
+import com.dotmarketing.startup.AbstractJDBCStartupTask;
+import java.util.List;
+
+public class Task220330ChangeVanityURLSiteFieldType extends AbstractJDBCStartupTask  {
+
+    private String getScript(){
+        return "UPDATE field SET field_type = 'com.dotcms.contenttype.model.field.HostFolderField' "
+                + "WHERE field_name = 'Site' and structure_inode in (select inode from structure where structuretype = 7)";
+    }
+
+    @Override
+    public String getPostgresScript() {
+        return getScript();
+    }
+
+    @Override
+    public String getMySQLScript() {
+        return getScript();
+    }
+
+    @Override
+    public String getOracleScript() {
+        return getScript();
+    }
+
+    @Override
+    public String getMSSQLScript() {
+        return getScript();
+    }
+
+    @Override
+    protected List<String> getTablesToDropConstraints() {
+        return null;
+    }
+
+    @Override
+    public boolean forceRun() {
+        return true;
+    }
+}
