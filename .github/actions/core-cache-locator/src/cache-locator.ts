068dc9182981180d92cfbbc1d7634289dcd0f197
Victor Alfaro <victor.alfaro@dotcms.com>
2022-04-05 14:50:21 -0600

    (#21754): Adding restore and relocation of dependencies and build output support and unit tests execution. (#21942)

--- .github/actions/core-cache-locator/src/cache-locator.ts
+++ .github/actions/core-cache-locator/src/cache-locator.ts
@@ -1,10 +1,12 @@
 import * as core from '@actions/core'
+import * as fs from 'fs'
 import * as path from 'path'
 
 interface CacheLocations {
   dependencies: string[]
   buildOutput: string[]
 }
+
 interface CacheConfiguration {
   gradle: CacheLocations
   maven: CacheLocations
@@ -13,11 +15,7 @@ interface CacheConfiguration {
 const CACHE_CONFIGURATION: CacheConfiguration = {
   gradle: {
     dependencies: ['~/.gradle/caches', '~/.gradle/wrapper'],
-    buildOutput: [
-      'dotCMS/.gradle',
-      'dotCMS/build/classes',
-      'dotCMS/build/resources'
-    ]
+    buildOutput: ['dotCMS/.gradle', 'dotCMS/build/classes', 'dotCMS/build/resources']
   },
   maven: {
     dependencies: ['~/.m2/repository'],
@@ -31,10 +29,7 @@ const EMPTY_CACHE_LOCATIONS: CacheLocations = {
 }
 
 const BUILD_OUTPUT = 'buildOutput'
-const CACHE_FOLDER = path.join(
-  path.dirname(core.getInput('core-root')),
-  'cache'
-)
+const CACHE_FOLDER = path.join(path.dirname(core.getInput('core-root')), 'cache')
 
 /**
  * Resolves locations to be cached after building core.
@@ -42,27 +37,19 @@ const CACHE_FOLDER = path.join(
  * @returns a {@link CacheLocation} instance with the information of what is going to be cached
  */
 export const getCacheLocations = (): CacheLocations => {
-  // Resolves build tool environment
-  const buildToolEnv = core.getInput('build-tool-env')
-  core.info(`Resolving cache locations with buid tool ${buildToolEnv}`)
+  // Resolves build environment
+  const buildEnv = core.getInput('build-env')
+  core.info(`Resolving cache locations with buid env ${buildEnv}`)
 
-  const cacheLocations =
-    CACHE_CONFIGURATION[buildToolEnv as keyof CacheConfiguration]
+  const cacheLocations = CACHE_CONFIGURATION[buildEnv as keyof CacheConfiguration]
   if (!cacheLocations) {
-    core.warning(
-      `Cannot find cache configuration for build tool ${buildToolEnv}`
-    )
+    core.warning(`Cannot find cache configuration for build env ${buildEnv}`)
     return EMPTY_CACHE_LOCATIONS
   }
 
   // For each cache location resolves the location to be cached
   Object.keys(cacheLocations).forEach(key =>
-    resolveLocations(
-      buildToolEnv,
-      cacheLocations,
-      key,
-      key === BUILD_OUTPUT ? decorateBuildOutput : undefined
-    )
+    resolveLocations(buildEnv, cacheLocations, key, key === BUILD_OUTPUT ? decorateBuildOutput : undefined)
   )
 
   return cacheLocations
@@ -71,22 +58,19 @@ export const getCacheLocations = (): CacheLocations => {
 /**
  * Adds to a {@link CacheLocation[]} the locations for each lcoation type (key).
  * 
- * @param buildToolEnv build tool environtment (gradle or maven)
- * @param cacheLocations cache locations based on the build tool env
+ * @param buildEnv build environtment (gradle or maven)
+ * @param cacheLocations cache locations based on the build env
  * @param key location type (depedencies or buildOutput)
  * @param decorateFn function to do some extra decoration to locations path
 
  */
 const resolveLocations = (
-  buildToolEnv: string,
+  buildEnv: string,
   cacheLocations: CacheLocations,
   key: string,
   decorateFn?: (location: string) => string
 ) => {
-  const cacheEnableKey = key.replace(
-    /[A-Z]/g,
-    letter => `-${letter.toLowerCase()}`
-  )
+  const cacheEnableKey = key.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`)
   const cacheEnable = core.getBooleanInput(`cache-${cacheEnableKey}`)
   if (!cacheEnable) {
     core.warning(`Core cache is disabled for ${key}`)
@@ -97,16 +81,23 @@ const resolveLocations = (
   const locationKey = key as keyof CacheLocations
   const locations = cacheLocations[locationKey]
   if (!locations) {
-    core.warning(
-      `Cannot resolve any ${key} locations for build tool ${buildToolEnv}`
-    )
+    core.warning(`Cannot resolve any ${key} locations for build env ${buildEnv}`)
     return
   }
 
   core.info(`Found ${key} locations: ${locations}`)
-  const resolvedLocations = locations.map(location =>
-    decorateFn ? decorateFn(location) : location
-  )
+  const resolvedLocations = locations.map(location => {
+    const newLocation = decorateFn ? decorateFn(location) : location
+
+    if (location !== newLocation) {
+      core.info(`Relocating cache from ${location} to ${newLocation}`)
+      fs.mkdirSync(path.dirname(newLocation), {recursive: true})
+      fs.renameSync(location, newLocation)
+    }
+
+    return newLocation
+  })
+
   core.info(`Resolved ${key} locations: ${resolvedLocations}`)
   cacheLocations[locationKey] = resolvedLocations
 }
@@ -117,5 +108,4 @@ const resolveLocations = (
  * @param location location
  * @returns decorated string
  */
-const decorateBuildOutput = (location: string): string =>
-  path.join(CACHE_FOLDER, path.basename(location))
+const decorateBuildOutput = (location: string): string => path.join(CACHE_FOLDER, location)
