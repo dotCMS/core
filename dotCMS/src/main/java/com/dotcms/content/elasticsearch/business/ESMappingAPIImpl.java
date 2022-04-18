91f19b6c880fa6012c494e1018ef43ef1992bc11
freddyucv <freddy0309@gmail.com>
2022-04-18 13:38:28 -0600

    Adding Time zone to ES date format

--- dotCMS/src/main/java/com/dotcms/content/elasticsearch/business/ESMappingAPIImpl.java
+++ dotCMS/src/main/java/com/dotcms/content/elasticsearch/business/ESMappingAPIImpl.java
@@ -748,7 +748,7 @@ public class ESMappingAPIImpl implements ContentMappingAPI {
 	public static final FastDateFormat dateFormat = FastDateFormat.getInstance("yyyyMMdd");
 	public static final FastDateFormat datetimeFormat = FastDateFormat.getInstance("yyyyMMddHHmmss");
 
-	public static final String elasticSearchDateTimeFormatPattern="yyyy-MM-dd'T'HH:mm:ss'Z";
+	public static final String elasticSearchDateTimeFormatPattern="yyyyMMdd'T'HHmmssZ";
 	public static final FastDateFormat elasticSearchDateTimeFormat = FastDateFormat.getInstance(elasticSearchDateTimeFormatPattern);
 
 	public static final FastDateFormat timeFormat = FastDateFormat.getInstance("HH:mm:ss");
