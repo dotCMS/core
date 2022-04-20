67561ca1ff8cb093f095daace0f879fceb32f941
Victor Alfaro <victor.alfaro@dotcms.com>
2022-04-20 16:13:55 -0600

    (#22034): Creating a separate Github Action to send tests results status to Github (#22052)

--- .github/actions/run-unit-tests/dist/index.js
+++ .github/actions/run-unit-tests/dist/index.js
@@ -53,15 +53,17 @@ const run = () => {
             skipResultsReport: false
         };
         core.info(`Unit test results:\n${JSON.stringify(results)}`);
-        core.setOutput('tests-run-exit-code', exitCode);
-        core.setOutput('test-results-location', cmd.outputDir);
+        core.setOutput('tests-run-exit-code', exitCode === 0 ? 'PASSED' : 'FAILED');
+        core.setOutput('tests-results-status', cmd.outputDir);
+        core.setOutput('tests-results-location', cmd.outputDir);
         core.setOutput('skip-results-report', false);
     })
         .catch(reason => {
         const messg = `Running unit tests failed due to ${reason}`;
         const skipResults = !fs.existsSync(cmd.outputDir);
         core.setOutput('tests-run-exit-code', 1);
-        core.setOutput('test-results-location', cmd.outputDir);
+        core.setOutput('tests-run-exit-code', 'FAILED');
+        core.setOutput('tests-results-location', cmd.outputDir);
         core.setOutput('skip-results-report', skipResults);
         core.setFailed(messg);
     });
