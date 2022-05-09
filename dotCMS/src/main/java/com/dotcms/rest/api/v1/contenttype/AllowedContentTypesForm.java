659bf41a33041bedbc14bced17a551e51fad5457
Jose Castro <jose.castro@dotcms.com>
2022-05-09 13:54:53 -0600

    #22039 : Implementing Code Review feedback.

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/contenttype/AllowedContentTypesForm.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/contenttype/AllowedContentTypesForm.java
@@ -1,6 +1,7 @@
 package com.dotcms.rest.api.v1.contenttype;
 
 import com.dotcms.rest.api.Validated;
+import com.dotmarketing.util.UtilMethods;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
@@ -15,6 +16,13 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 public class AllowedContentTypesForm extends Validated {
 
     private final String types;
+    private final int page;
+    private final int perPage;
+    private final String orderBy;
+    private final String direction;
+
+    private static final String DEFAULT_ORDER_BY = "UPPER(name)";
+    private static final String DEFAULT_DIRECTION = "ASC";
 
     /**
      * Internal class constructor meant to be used by the Builder class.
@@ -23,6 +31,10 @@ public class AllowedContentTypesForm extends Validated {
      */
     private AllowedContentTypesForm(final Builder builder) {
         this.types = builder.types;
+        this.page = builder.page;
+        this.perPage = builder.perPage;
+        this.orderBy = builder.orderBy;
+        this.direction = builder.direction;
     }
 
     /**
@@ -36,12 +48,60 @@ public class AllowedContentTypesForm extends Validated {
     }
 
     /**
+     * The result page that will be returned as part of the response, for pagination purposes.
+     *
+     * @return The result page.
+     */
+    public int getPage() {
+        return this.page;
+    }
+
+    /**
+     * The total amount of items per result page that will be returned as part of the response, for pagination
+     * purposes.
+     *
+     * @return The total items per result page.
+     */
+    public int getPerPage() {
+        return this.perPage;
+    }
+
+    /**
+     * The criterion in which results will be ordered in the response.
+     *
+     * @return The ordering criterion.
+     */
+    public String getOrderBy() {
+        return UtilMethods.isSet(this.orderBy) ? this.orderBy : DEFAULT_ORDER_BY;
+    }
+
+    /**
+     * The ordering direction for the results in the response: {@code ASC} or {@code DESC}.
+     *
+     * @return The ordering direction.
+     */
+    public String getDirection() {
+        return UtilMethods.isSet(this.direction) ? this.direction : DEFAULT_DIRECTION;
+    }
+
+    /**
      * Builder class used to create an instance of the {@link AllowedContentTypesForm} class.
      */
     public static final class Builder {
 
         @JsonProperty(required = true)
         private String types;
+        @JsonProperty
+        private int page;
+        @JsonProperty
+        private int perPage;
+        @JsonProperty
+        private String orderBy;
+        @JsonProperty
+        private String direction;
+
+        private Builder() {
+        }
 
         /**
          * The comma-separated list of Velocity Variable Names for each Content Type that can be referenced in a Story
@@ -53,6 +113,55 @@ public class AllowedContentTypesForm extends Validated {
          */
         public AllowedContentTypesForm.Builder types(final String types) {
             this.types = types;
+            return this;
+        }
+
+        /**
+         * The result page that will be returned as part of the response, for pagination purposes.
+         *
+         * @param page The result page.
+         *
+         * @return An instance of the {@link Builder} class.
+         */
+        public AllowedContentTypesForm.Builder page(final int page) {
+            this.page = page;
+            return this;
+        }
+
+        /**
+         * The total amount of items per result page that will be returned as part of the response, for pagination
+         * purposes.
+         *
+         * @param perPage The total items per result page.
+         *
+         * @return An instance of the {@link Builder} class.
+         */
+        public AllowedContentTypesForm.Builder perPage(final int perPage) {
+            this.perPage = perPage;
+            return this;
+        }
+
+        /**
+         * The criterion in which results will be ordered in the response.
+         *
+         * @param orderBy The ordering criterion.
+         *
+         * @return An instance of the {@link Builder} class.
+         */
+        public AllowedContentTypesForm.Builder orderBy(final String orderBy) {
+            this.orderBy = orderBy;
+            return this;
+        }
+
+        /**
+         * The ordering direction for the results in the response: {@code ASC} or {@code DESC}.
+         *
+         * @param direction The ordering direction.
+         *
+         * @return An instance of the {@link Builder} class.
+         */
+        public AllowedContentTypesForm.Builder direction(final String direction) {
+            this.direction = direction;
             return this;
         }
 
