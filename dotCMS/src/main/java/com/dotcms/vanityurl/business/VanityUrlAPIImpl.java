0284fe9b7fc184e14fac02a0d2059fb8fbd634e2
Scott Wicken <78741732+swicken-dotcms@users.noreply.github.com>
2022-05-18 15:03:30 -0400

    Vanity URL changes

--- dotCMS/src/main/java/com/dotcms/vanityurl/business/VanityUrlAPIImpl.java
+++ dotCMS/src/main/java/com/dotcms/vanityurl/business/VanityUrlAPIImpl.java
@@ -267,7 +267,6 @@ public class VanityUrlAPIImpl implements VanityUrlAPI {
     checkMissingField(contentlet, language, VanityUrlContentType.ACTION_FIELD_VAR);
     checkMissingField(contentlet, language, VanityUrlContentType.URI_FIELD_VAR);
     checkMissingField(contentlet, language, VanityUrlContentType.FORWARD_TO_FIELD_VAR);
-    checkMissingField(contentlet, language, VanityUrlContentType.SITE_FIELD_VAR);
     checkMissingField(contentlet, language, VanityUrlContentType.TITLE_FIELD_VAR);
     checkMissingField(contentlet, language, VanityUrlContentType.ORDER_FIELD_VAR);
 
