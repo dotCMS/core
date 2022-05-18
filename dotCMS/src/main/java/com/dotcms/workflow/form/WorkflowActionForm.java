7e6dd9493edfd3e3fd28203f227dbb485045b436
Daniel Silva <daniel.silva@dotcms.com>
2022-05-18 14:15:58 -0500

    Unused import

--- dotCMS/src/main/java/com/dotcms/workflow/form/WorkflowActionForm.java
+++ dotCMS/src/main/java/com/dotcms/workflow/form/WorkflowActionForm.java
@@ -1,15 +1,13 @@
 package com.dotcms.workflow.form;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.dotcms.repackage.javax.validation.constraints.NotNull;
 import com.dotcms.rest.api.Validated;
 import com.dotmarketing.portlets.workflows.model.WorkflowState;
-
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-import javax.ws.rs.FormParam;
 
 @JsonDeserialize(builder = WorkflowActionForm.Builder.class)
 public class WorkflowActionForm extends Validated {
