cf713223c013654cbfc8326d6e23230b36d49aa9
Victor Alfaro <victor.alfaro@dotcms.com>
2022-05-23 09:09:04 -0600

    Core-web merging test

--- core-web/.storybook/main.js
+++ core-web/.storybook/main.js
@@ -1,14 +1,17 @@
 module.exports = {
     stories: [],
-    addons: ['@storybook/addon-essentials', {
-        name: '@storybook/addon-docs',
-        options: {
-            configureJSX: true,
-            babelOptions: {},
-            sourceLoaderOptions: null,
-            transcludeMarkdown: true,
-        },
-    }],
+    addons: [
+        '@storybook/addon-essentials',
+        {
+            name: '@storybook/addon-docs',
+            options: {
+                configureJSX: true,
+                babelOptions: {},
+                sourceLoaderOptions: null,
+                transcludeMarkdown: true
+            }
+        }
+    ]
     // uncomment the property below if you want to apply some webpack config globally
     // webpackFinal: async (config, { configType }) => {
     //   // Make whatever fine-grained changes you need that should apply to all storybook configs
