3bbd75164c8a6e7006426218cfd73094bf985fce
Will Ezell <will@dotcms.com>
2022-06-09 14:43:04 -0400

    #22381 updating dates before adding tz

--- dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task210901UpdateDateTimezones.java
+++ dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task210901UpdateDateTimezones.java
@@ -6,13 +6,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.TimeZone;
+import com.dotmarketing.common.db.DotConnect;
 import com.dotmarketing.common.db.DotDatabaseMetaData;
 import com.dotmarketing.db.DbConnectionFactory;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotRuntimeException;
 import com.dotmarketing.startup.AbstractJDBCStartupTask;
 import com.dotmarketing.util.Logger;
+import com.google.common.annotations.VisibleForTesting;
 
 /**
  * This class updates all postgres "timestamp without timezone" fields
@@ -29,6 +31,14 @@ public class Task210901UpdateDateTimezones extends AbstractJDBCStartupTask {
         return DbConnectionFactory.isPostgres();
     }
 
+    boolean hasTimeZones() {
+        final String type = new DotConnect().setSQL("select udt_name from information_schema.columns where table_name='contentlet' and column_name='date25'").getString("udt_name");
+        // if the date fields are no longer with us, this task will still run
+        if(type==null) {
+            return true;
+        }
+        return "timestamptz".equals(type );
+    }
 
     List<String> findAllTables() throws SQLException {
         final String[] types = {
@@ -76,6 +86,15 @@ public class Task210901UpdateDateTimezones extends AbstractJDBCStartupTask {
 
     @Override
     public void executeUpgrade() throws DotDataException, DotRuntimeException {
+        
+        
+        // add the current offset to the content date fields
+        if(!hasTimeZones()) {
+            updateContentDateFieldsToUTC();
+        }
+        
+        
+        
         tablesCount = 0;
         try {
             for (final String table : findAllTables()) {
@@ -90,38 +109,39 @@ public class Task210901UpdateDateTimezones extends AbstractJDBCStartupTask {
 
 
     }
+    
+    @VisibleForTesting
+    String selectTimeZone() {
+        return new DotConnect().setSQL("select timezoneid from user_ where userid='dotcms.org.default'").getString("timezoneid");
 
-
-
-    @Override
-    protected List<String> getTablesToDropConstraints() {
-        // TODO Auto-generated method stub
-        return null;
     }
-
-
-
-    @Override
-    public String getPostgresScript() {
-        // TODO Auto-generated method stub
-        return null;
+    
+    
+    
+    @VisibleForTesting
+    int calculateOffsetSeconds() {
+        TimeZone timezone = TimeZone.getTimeZone(selectTimeZone());
+        return timezone.getRawOffset()/1000;
     }
+    
+    
+    @VisibleForTesting
+    void updateContentDateFieldsToUTC() throws DotDataException {
+    
+        int offset = calculateOffsetSeconds();
 
-    @Override
-    public String getMySQLScript() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+        for(int i=1;i<26;i++) {
+            final String sql = "update contentlet set {1} = {1} + interval '{2}' SECOND where {1} is not null"
+                            .replace("{1}", "date" + i)
+                            .replace("{2}", String.valueOf(offset));
 
-    @Override
-    public String getOracleScript() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+            new DotConnect().setSQL(sql).loadResult();
 
-    @Override
-    public String getMSSQLScript() {
-        // TODO Auto-generated method stub
-        return null;
+        }
+
     }
+    
+    
+
+
 }
