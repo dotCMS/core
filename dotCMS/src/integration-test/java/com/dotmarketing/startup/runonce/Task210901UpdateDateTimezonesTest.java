22ed3fef666a4012cea17570b02cba819afe5f58
Jose Castro <jose.castro@dotcms.com>
2022-06-17 15:38:43 -0600

    #22381 : Update dates before timezone change, for release-22.06

--- dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task210901UpdateDateTimezonesTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task210901UpdateDateTimezonesTest.java
@@ -1,16 +1,28 @@
 package com.dotmarketing.startup.runonce;
 
 import static org.junit.Assert.assertTrue;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import com.dotcms.util.IntegrationTestInitService;
+import com.dotmarketing.common.db.DotConnect;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.util.UtilMethods;
 
 public class Task210901UpdateDateTimezonesTest {
+
+   static String selectedTimezone;
 
     @BeforeClass
     public static void prepare() throws Exception {
         // Setting web app environment
         IntegrationTestInitService.getInstance().init();
+        selectedTimezone = new Task210901UpdateDateTimezones().selectTimeZone();
+    }
+
+    @AfterClass
+    public static void cleanup() throws Exception {
+        updateTimeZone(selectedTimezone);
     }
 
     /**
@@ -25,7 +37,29 @@ public class Task210901UpdateDateTimezonesTest {
         if(tztask.forceRun()) {
             tztask.executeUpgrade();
         }
-        assertTrue("Timezones have been updated",true);
+        assertTrue("Timezones have been updated",tztask.hasTimeZones());
+    }
+
+    @Test
+    public void test_timezones_have_been_added() throws Exception{
+        final Task210901UpdateDateTimezones tztask = new Task210901UpdateDateTimezones();
+        assertTrue("Timezones are already added",tztask.hasTimeZones());
+    }
+
+    @Test
+    public void test_timezone_offset() throws Exception{
+        final Task210901UpdateDateTimezones tztask = new Task210901UpdateDateTimezones();
+        updateTimeZone("UTC");
+        assertTrue(tztask.calculateOffsetSeconds() == 0);
+        updateTimeZone("US/Eastern");
+        assertTrue(tztask.calculateOffsetSeconds() == -18000);
+    }
+
+    private static void updateTimeZone(final String newTimezone) throws DotDataException {
+        if(UtilMethods.isEmpty(newTimezone)) {
+            return;
+        }
+        new DotConnect().setSQL("update user_ set timezoneid = ? where userid='dotcms.org.default'").addParam(newTimezone).loadResult();
     }
 
 }
