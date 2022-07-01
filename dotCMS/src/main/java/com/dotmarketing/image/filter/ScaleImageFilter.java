0dfb8319172ef6e86d5b340983eb3a1010b6b481
Daniel Silva <daniel.silva@dotcms.com>
2022-07-01 17:14:00 -0500

    #22463 Support old scale redirecting to resize

---
+++ dotCMS/src/main/java/com/dotmarketing/image/filter/ScaleImageFilter.java
@@ -0,0 +1,9 @@
+package com.dotmarketing.image.filter;
+
+/**
+ * Empty class for backwards compatibility of the scale_w/800/scale_h/500 filter
+ *
+ */
+public class ScaleImageFilter extends ResizeImageFilter {
+
+}
