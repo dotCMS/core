16cde0dd9ade0ea210dbeca37dbf7adeff5e4209
Arcadio Quintero <oidacra@gmail.com>
2022-07-07 12:01:07 -0500

    Nx update working, except test

--- core-web/libs/dotcms-webcomponents/.storybook/main.js
+++ core-web/libs/dotcms-webcomponents/.storybook/main.js
@@ -7,8 +7,8 @@ module.exports = {
   
   stories: [
     ...rootMain.stories,
-    '../src/lib/**/*.stories.mdx',
-    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
   addons: [...rootMain.addons 
     
