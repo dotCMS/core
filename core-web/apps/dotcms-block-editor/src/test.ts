d89d312f696185c3ab9b3483eaaed6268b7b9413
Arcadio Quintero <oidacra@gmail.com>
2022-07-07 12:01:07 -0500

    Nx update working, except test

--- core-web/apps/dotcms-block-editor/src/test.ts
+++ core-web/apps/dotcms-block-editor/src/test.ts
@@ -7,6 +7,7 @@ import {
     platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const require: any;
 
 // First, initialize the Angular testing environment.
