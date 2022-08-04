64aef63204f9a80af5003d8c51b76630724c6e40
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2022-08-04 11:32:58 -0600

    #Reverting changes on site command class

--- tools/dotcms-cli/cli/src/main/java/com/dotcms/cli/command/SiteCommand.java
+++ tools/dotcms-cli/cli/src/main/java/com/dotcms/cli/command/SiteCommand.java
@@ -2,33 +2,27 @@ package com.dotcms.cli.command;
 
 import com.dotcms.api.SiteAPI;
 import com.dotcms.api.client.RestClientFactory;
-import com.dotcms.api.client.ServiceManager;
-import com.dotcms.cli.common.OutputOptionMixin;
 import com.dotcms.model.ResponseEntityView;
 import com.dotcms.model.site.Site;
 import java.util.List;
-import java.util.concurrent.Callable;
 import javax.enterprise.context.control.ActivateRequestContext;
 import javax.inject.Inject;
+import org.jboss.logging.Logger;
 import picocli.CommandLine;
-import picocli.CommandLine.ArgGroup;
-import picocli.CommandLine.ExitCode;
 
 @ActivateRequestContext
 @CommandLine.Command(name = "site", description = "@|bold,green Retrieves Sites info.|@ Option params @|bold,cyan -n|@ to filter by name. @|bold,cyan -a|@ Shows archived sites. @|bold,cyan -l|@ Shows live Sites. @|bold,cyan -p|@ (Page) @|bold,cyan -ps|@ (PageSize) Can be used combined for pagination.")
-public class SiteCommand implements Callable<Integer> {
+public class SiteCommand implements Runnable {
 
+    private static final Logger logger = Logger.getLogger(SiteCommand.class);
 
-    static final String NAME = "site";
-
-    /*
-    @CommandLine.Option(names = {"-n", "--name"}, description = "Filter by site name.")
+    @CommandLine.Option(names = {"-n", "--name"}, description = "Filter by site name.", interactive = true)
     String name;
 
     @CommandLine.Option(names = {"-a", "--archived"}, description = "Show archived sites.", defaultValue = "false")
     Boolean archived;
 
-    @CommandLine.Option(names = {"-li", "--live"}, description = "Show live sites.", defaultValue = "true")
+    @CommandLine.Option(names = {"-l", "--live"}, description = "Show live sites.", defaultValue = "true")
     Boolean live;
 
     @CommandLine.Option(names = {"-p", "--page"}, description = "Page Number.", defaultValue = "1")
@@ -36,65 +30,23 @@ public class SiteCommand implements Callable<Integer> {
 
     @CommandLine.Option(names = {"-ps", "--pageSize"}, description = "Items per page.", defaultValue = "25")
     Integer pageSize;
-*/
-    static class ListAllOptionGroup {
-
-        @CommandLine.Option(names = {"-l", "--list"}, description = "List all sites.", required = true)
-        Boolean list;
-    }
-
-    static class SearchOptionGroup {
-
-        @CommandLine.Option(names = {"-f", "--find"}, description = "Find site by name", required = true)
-        Boolean find;
-
-        @CommandLine.Option(names = {"-n", "--name"}, description = "Filter by site name.", required = true)
-        String name;
-    }
-
-    @ArgGroup(exclusive = true, multiplicity = "0..1")
-    ListAllOptionGroup listOption;
-
-
-    @ArgGroup(exclusive = true, multiplicity = "0..1")
-    SearchOptionGroup searchOption;
-
-    @CommandLine.Mixin(name = "output")
-    protected OutputOptionMixin output;
-
-    @Inject
-    ServiceManager serviceManager;
 
     @Inject
     RestClientFactory clientFactory;
 
-
-
     @Override
-    public Integer call() {
+    public void run() {
 
-        System.out.println("@|bold,green Testing.|@");
-
-        if(null != listOption) {
-            System.out.println(listOption.list);
-        }
-
-        if(null != searchOption) {
-            System.out.println(searchOption.name);
-        }
-        /*
         final SiteAPI siteAPI = clientFactory.getClient(SiteAPI.class);
-        final ResponseEntityView<List<Site>> response = siteAPI.getSites(name, archived, live, false, page, pageSize);
+        final ResponseEntityView<List<Site>> response = siteAPI.getSites(name, archived, live, true, page, pageSize);
         final List<Site> sites = response.entity();
         if (sites.isEmpty()) {
-
+            logger.info("I couldn't find any sites with this search criteria.");
         } else {
             for (final Site site : sites) {
-
+                logger.info(site);
             }
-        }*/
-
-        return ExitCode.OK;
+        }
 
     }
 
