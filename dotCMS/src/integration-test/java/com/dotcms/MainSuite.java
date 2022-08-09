7eac42457c1d1a60f8c1f24fabd18e20e054af59
freddyucv <freddy0309@gmail.com>
2022-08-09 13:43:04 -0600

    Moving ContentletWebAPIImplIntegrationTest to the top

--- dotCMS/src/integration-test/java/com/dotcms/MainSuite.java
+++ dotCMS/src/integration-test/java/com/dotcms/MainSuite.java
@@ -179,6 +179,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(MainBaseSuite.class)
 @SuiteClasses({
+        ContentletWebAPIImplIntegrationTest.class,
         DependencyBundlerTest.class,
         FolderCacheImplIntegrationTest.class,
         StaticPublisherIntegrationTest.class,
@@ -442,7 +443,6 @@ import org.junit.runners.Suite.SuiteClasses;
         GoogleTranslationServiceIntegrationTest.class,
         Task05380ChangeContainerPathToAbsoluteTest.class,
         DotTemplateToolTest.class,
-        ContentletWebAPIImplIntegrationTest.class,
         Task05370AddAppsPortletToLayoutTest.class,
         FolderFactoryImplTest.class,
         DotSamlResourceTest.class,
