c369ff3a64b8878b4cd7959fedc50aba3b3f0073
Freddy Montes <freddymontes@gmail.com>
2022-08-17 16:45:54 -0600

    Ue scss

--- core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-create/container-create.component.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-create/container-create.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'dot-container-create',
     templateUrl: './container-create.component.html',
-    styleUrls: ['./container-create.component.css']
+    styleUrls: ['./container-create.component.scss']
 })
 export class ContainerCreateComponent {}
--- core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-list/container-list.component.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-list/container-list.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'dot-container-list',
     templateUrl: './container-list.component.html',
-    styleUrls: ['./container-list.component.css']
+    styleUrls: ['./container-list.component.scss']
 })
 export class ContainerListComponent {}
