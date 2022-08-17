0ee368e67a59e79e0be6baa0724c033bd4ce647f
Freddy Montes <freddymontes@gmail.com>
2022-08-17 16:26:33 -0600

    Add portlet base

--- core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-create/container-create.module.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-create/container-create.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContainerCreateRoutingModule } from './container-create-routing.module';
+import { ContainerCreateComponent } from './container-create.component';
+import { DotPortletBaseModule } from '@components/dot-portlet-base/dot-portlet-base.module';
 
 @NgModule({
-    declarations: [],
-    imports: [CommonModule, ContainerCreateRoutingModule]
+    declarations: [ContainerCreateComponent],
+    imports: [CommonModule, ContainerCreateRoutingModule, DotPortletBaseModule]
 })
 export class ContainerCreateModule {}
