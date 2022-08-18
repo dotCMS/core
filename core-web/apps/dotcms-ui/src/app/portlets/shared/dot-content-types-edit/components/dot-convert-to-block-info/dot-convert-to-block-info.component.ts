fc5fa0ee0cf5d0be20efb4d4d8ca126556d2c2bf
Freddy Montes <freddymontes@gmail.com>
2022-08-18 12:18:42 -0600

    Use dm pipe

--- core-web/apps/dotcms-ui/src/app/portlets/shared/dot-content-types-edit/components/dot-convert-to-block-info/dot-convert-to-block-info.component.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/shared/dot-content-types-edit/components/dot-convert-to-block-info/dot-convert-to-block-info.component.ts
@@ -1,27 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { DotMessageService } from '@dotcms/app/api/services/dot-message/dot-messages.service';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
     selector: 'dot-convert-to-block-info',
     templateUrl: './dot-convert-to-block-info.component.html',
     styleUrls: ['./dot-convert-to-block-info.component.scss']
 })
-export class DotConvertToBlockInfoComponent implements OnInit {
+export class DotConvertToBlockInfoComponent {
     @Input() currentFieldType;
     @Output() action = new EventEmitter<MouseEvent>();
     @Input() currentField;
-
-    messages: Record<string, string>;
-
-    constructor(private dotMessageService: DotMessageService) {}
-
-    ngOnInit(): void {
-        this.messages = {
-            content: this.dotMessageService.get(
-                'contenttypes.field.properties.wysiwyg.info.content'
-            ),
-            button: this.dotMessageService.get('contenttypes.field.properties.wysiwyg.info.button'),
-            learnMore: this.dotMessageService.get('learn-more')
-        };
-    }
 }
