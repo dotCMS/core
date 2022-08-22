3e012043339b6a989e5e8e4f546a1632d6fd7cd2
Daniel Silva <daniel.silva@dotcms.com>
2022-08-22 18:07:15 -0500

    #22829 Create new JSON Field Type

---
+++ dotCMS/src/main/java/com/dotcms/contenttype/model/field/JSONField.java
@@ -0,0 +1,62 @@
+package com.dotcms.contenttype.model.field;
+
+import static com.dotcms.util.CollectionsUtils.list;
+
+import com.dotcms.content.model.FieldValueBuilder;
+import com.dotcms.content.model.type.TextAreaType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.google.common.collect.ImmutableList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import org.immutables.value.Value;
+
+@JsonSerialize(as = ImmutableJSONField.class)
+@JsonDeserialize(as = ImmutableJSONField.class)
+@Value.Immutable
+public abstract class JSONField extends Field {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public  Class type() {
+		return  JSONField.class;
+	}
+
+	@JsonIgnore
+	@Value.Derived
+	@Override
+	public List<DataTypes> acceptedDataTypes(){
+		return ImmutableList.of(DataTypes.LONG_TEXT);
+	}
+	@Value.Default
+	@Override
+	public DataTypes dataType(){
+		return DataTypes.LONG_TEXT;
+	};
+	public abstract static class Builder implements FieldBuilder {}
+
+	@JsonIgnore
+	public Collection<ContentTypeFieldProperties> getFieldContentTypeProperties(){
+		return list(ContentTypeFieldProperties.REQUIRED, ContentTypeFieldProperties.NAME,
+				ContentTypeFieldProperties.REGEX_CHECK, ContentTypeFieldProperties.DEFAULT_VALUE, ContentTypeFieldProperties.HINT,
+				ContentTypeFieldProperties.SEARCHABLE, ContentTypeFieldProperties.INDEXED);
+	}
+
+	/**
+	 * {@inheritDoc}
+     * @return
+     */
+	@Override
+	public Optional<FieldValueBuilder> fieldValue(Object value) {
+		if (value instanceof String) {
+			return Optional.of(TextAreaType.builder().value((String) value));
+		}
+		return Optional.empty();
+	}
+
+
+}
--- dotCMS/src/main/java/com/dotcms/util/JsonUtil.java
+++ dotCMS/src/main/java/com/dotcms/util/JsonUtil.java
@@ -2,6 +2,7 @@ package com.dotcms.util;
 
 import com.dotcms.content.model.Contentlet;
 import com.dotmarketing.util.Logger;
+import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
@@ -25,5 +26,14 @@ public class JsonUtil {
 
         final URL url = classLoader.getResource(path);
         return new String(com.liferay.util.FileUtil.getBytes(new File(url.getPath())));
+    }
+
+    public static boolean isValidJSON(final String fieldValue) {
+        try {
+            JSON_MAPPER.readTree(fieldValue);
+        } catch (JacksonException e) {
+            return false;
+        }
+        return true;
     }
 }
