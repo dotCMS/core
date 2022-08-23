91e8116f8e15cbefa46527514ba0d0490fc487ec
jdotcms <jonathan.sanchez@dotcms.com>
2022-08-23 12:54:19 -0500

    #22779 the file can holds a binary buffer or a file

---
+++ dotCMS/src/integration-test/java/com/dotcms/storage/BinaryBlockFilJoinerImplTest.java
@@ -0,0 +1,78 @@
+package com.dotcms.storage;
+
+import com.dotcms.util.IntegrationTestInitService;
+import com.dotmarketing.util.FileUtil;
+import com.google.common.io.Files;
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class BinaryBlockFilJoinerImplTest {
+
+
+    @BeforeClass
+    public static void prepare() throws Exception {
+        IntegrationTestInitService.getInstance().init();
+    }
+
+    /**
+     * Method to test: {@link BinaryBlockFileJoinerImpl#join(byte[], int, int)}
+     * Given Scenario: Adds less than 1000 bytes
+     * ExpectedResult: should return the binary and not a file
+     *
+     */
+    @Test
+    public void Test_Easy_Path() throws IOException {
+
+        final File file = FileUtil.createTemporaryFile("dot-db-storage-recovery", ".tmp", true);
+        final BinaryBlockFileJoinerImpl binaryBlockFileJoiner = new BinaryBlockFileJoinerImpl(file, 1000);
+
+        final String s1 = "one,two,three,four,five";
+        binaryBlockFileJoiner.join(s1.getBytes(StandardCharsets.UTF_8), 0, s1.length());
+
+        final String s2 = "six,seven,eight,nine,ten";
+        binaryBlockFileJoiner.join(s2.getBytes(StandardCharsets.UTF_8), 0, s2.length());
+
+        final byte [] bytes = binaryBlockFileJoiner.getBytes();
+        Assert.assertNotNull(bytes);
+        Assert.assertEquals(new String(bytes,StandardCharsets.UTF_8).trim(),s1+s2);
+    }
+
+    /**
+     * Method to test: {@link BinaryBlockFileJoinerImpl#join(byte[], int, int)}
+     * Given Scenario: Adds less than 1000 bytes
+     * ExpectedResult: should return the binary and not a file
+     *
+     */
+    @Test
+    public void Test_Flush() throws IOException {
+
+        final File file = FileUtil.createTemporaryFile("dot-db-storage-recovery", ".tmp", true);
+        final BinaryBlockFileJoinerImpl binaryBlockFileJoiner = new BinaryBlockFileJoinerImpl(file, 1000);
+        final StringBuilder builder = new StringBuilder();
+        for (int i=0;i<100;++i) {
+            final String s1 = "one,two,three,four,five,six,seven,eight,nine,ten,one,two,three,four,five,six,seven,eight,nine,ten,one,two,three,four," +
+                    "five,six,seven,eight,nine,ten,one,two,three,four,five,six,seven,eight,nine,ten" + "five,six,seven,eight,nine,ten,one,two,three,four,five,six,seven,eight,nine,ten" +
+                    "five,six,seven,eight,nine,ten,one,two,three,four,five,six,seven,eight,nine,ten";
+            builder.append(s1);
+            binaryBlockFileJoiner.join(s1.getBytes(StandardCharsets.UTF_8), 0, s1.length());
+        }
+
+        final byte [] bytes = binaryBlockFileJoiner.getBytes(); //
+        Assert.assertNull(bytes);
+
+        binaryBlockFileJoiner.flush();
+        binaryBlockFileJoiner.close();
+
+        Assert.assertTrue(file.exists());
+        final String fileContent = Files.asCharSource(file, StandardCharsets.UTF_8).read();
+
+        Assert.assertEquals(fileContent, builder.toString());
+    }
+
+}
--- dotCMS/src/main/java/com/dotcms/storage/BinaryBlockFileJoinerImpl.java
+++ dotCMS/src/main/java/com/dotcms/storage/BinaryBlockFileJoinerImpl.java
@@ -2,41 +2,46 @@ package com.dotcms.storage;
 
 import com.dotcms.util.FileJoiner;
 import com.dotcms.util.FileJoinerImpl;
+import com.dotmarketing.util.Logger;
+import io.vavr.Lazy;
+import org.apache.commons.lang3.mutable.MutableBoolean;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.function.Function;
 
 /**
  * Instead of joining into a file will join into a bytes stream
  * The joining in memory will be limited by a given size
  * @author jsanca
  */
-public class BinaryBlockFileJoinerImpl extends FileJoinerImpl implements FileJoiner {
+public class BinaryBlockFileJoinerImpl implements FileJoiner {
 
-    private byte [] buffer;
+    private final byte [] buffer;
+    private final File file;
+    private final Lazy<FileJoinerImpl> innerFileJoiner = Lazy.of(()->this.createFileJoinerImpl());
+    private final MutableBoolean flushed = new MutableBoolean(false);
     private int bufferIndex = 0;
 
+    private FileJoinerImpl createFileJoinerImpl() {
+
+        try {
+            return new FileJoinerImpl(this.file);
+        } catch (IOException e) {
+            Logger.error(this, e.getMessage(), e);
+            return null;
+        }
+    }
+
     public BinaryBlockFileJoinerImpl(final File file, final int maxSize) throws IOException {
-        super(file);
+        this.file = file;
         this.buffer  = new byte[maxSize];
     }
 
-    public BinaryBlockFileJoinerImpl(OutputStream outputStream, final int maxSize) {
-        super(outputStream);
-        this.buffer  = new byte[maxSize];
-    }
 
-    public BinaryBlockFileJoinerImpl(OutputStream outputStream, int bufferSize, final int maxSize) {
-        super(outputStream, bufferSize);
-        this.buffer  = new byte[maxSize];
-    }
+    @Override
+    public void join(File file) {
 
-    public BinaryBlockFileJoinerImpl(OutputStream outputStream, Function<File, InputStream> inputStreamCreatorFunction, int bufferSize, final int maxSize) {
-        super(outputStream, inputStreamCreatorFunction, bufferSize);
-        this.buffer  = new byte[maxSize];
+        this.innerFileJoiner.get().join(this.buffer, 0, this.buffer.length);
     }
 
     @Override
@@ -44,14 +49,15 @@ public class BinaryBlockFileJoinerImpl extends FileJoinerImpl implements FileJoi
 
         int nextSize = this.bufferIndex + length;
 
-        if (nextSize >= bytes.length) {
-            if (null != this.buffer) {
+        if (nextSize >= this.buffer.length || this.bufferIndex == Integer.MAX_VALUE) {
+            if (this.flushed.isFalse()) { // not flushed yet
 
-                super.join(this.buffer, 0, this.buffer.length);
-                this.buffer = null;
+                this.innerFileJoiner.get().join(this.buffer, 0, this.bufferIndex); // flush
+                this.flushed.setValue(true);
+                this.bufferIndex = Integer.MAX_VALUE;
             }
 
-            super.join(bytes, offset, length);
+            this.innerFileJoiner.get().join(bytes, offset, length);
         } else {
 
             for (int i = offset; i < length; ++i) {
@@ -62,6 +68,18 @@ public class BinaryBlockFileJoinerImpl extends FileJoinerImpl implements FileJoi
     }
 
    public byte [] getBytes() {
-        return this.buffer;
+        return this.flushed.isFalse()?this.buffer:null;
    }
+
+    @Override
+    public void flush() throws IOException {
+
+        this.innerFileJoiner.get().flush();
+    }
+
+    @Override
+    public void close() throws IOException {
+
+        this.innerFileJoiner.get().close();
+    }
 }
