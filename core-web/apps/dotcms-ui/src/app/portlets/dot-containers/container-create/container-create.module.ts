c9f4b49b3793bb026a3dd1f3d1459b664426a573
Motasim Hussain <motasim.hussain@venturedive.com>
2022-09-06 21:56:41 +0500

    Added Tabs to Container Create Page and updated translation strings

--- core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-create/container-create.module.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-create/container-create.module.ts
@@ -4,9 +4,17 @@ import { CommonModule } from '@angular/common';
 import { ContainerCreateRoutingModule } from './container-create-routing.module';
 import { ContainerCreateComponent } from './container-create.component';
 import { DotPortletBaseModule } from '@components/dot-portlet-base/dot-portlet-base.module';
+import { TabViewModule } from 'primeng/tabview';
+import { DotMessagePipeModule } from '@pipes/dot-message/dot-message-pipe.module';
 
 @NgModule({
     declarations: [ContainerCreateComponent],
-    imports: [CommonModule, ContainerCreateRoutingModule, DotPortletBaseModule]
+    imports: [
+        CommonModule,
+        ContainerCreateRoutingModule,
+        DotPortletBaseModule,
+        TabViewModule,
+        DotMessagePipeModule
+    ]
 })
 export class ContainerCreateModule {}
