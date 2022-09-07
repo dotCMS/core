56643e7a33ece465e807baa60534319e1cf26812
freddyucv <freddy0309@gmail.com>
2022-09-06 20:43:49 -0600

    Add Test into MainSuite

--- dotCMS/src/integration-test/java/com/dotcms/MainSuite.java
+++ dotCMS/src/integration-test/java/com/dotcms/MainSuite.java
@@ -94,6 +94,7 @@ import com.dotcms.storage.StoragePersistenceAPITest;
 import com.dotcms.translate.GoogleTranslationServiceIntegrationTest;
 import com.dotcms.uuid.shorty.LegacyShortyIdApiTest;
 import com.dotcms.variant.VariantAPITest;
+import com.dotcms.variant.business.VariantCacheTest;
 import com.dotmarketing.beans.HostTest;
 import com.dotmarketing.business.DeterministicIdentifierAPITest;
 import com.dotmarketing.business.PermissionBitFactoryImplTest;
@@ -575,7 +576,8 @@ import org.junit.runners.Suite.SuiteClasses;
         Task220824CreateDefaultVariantTest.class,
         Task220822CreateVariantTableTest.class,
         Task220829CreateExperimentsTableTest.class,
-        StoryBlockTest.class
+        StoryBlockTest.class,
+        VariantCacheTest.class
 })
 public class MainSuite {
 
