ce71526666f76fe7e38890c8f5cce909d45213ee
Motasim Hussain <motasim.hussain@venturedive.com>
2022-09-15 21:19:49 +0500

    Refactored ContainerList functionality to ContainerListStore

--- core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-list/container-list.component.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-list/container-list.component.ts
@@ -1,20 +1,8 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { DotListingDataTableComponent } from '@components/dot-listing-data-table/dot-listing-data-table.component';
-import { MenuItem } from 'primeng/api';
-import { pluck, takeUntil } from 'rxjs/operators';
-import { Observable, Subject } from 'rxjs';
-import { DotMessageService } from '@services/dot-message/dot-messages.service';
-import { DotAlertConfirmService } from '@services/dot-alert-confirm';
-import { DotPushPublishDialogService } from '@dotcms/dotcms-js';
-import { DotRouterService } from '@services/dot-router/dot-router.service';
-import { ActionHeaderOptions } from '@models/action-header';
-import { DataTableColumn } from '@models/data-table';
 import { DotContainer } from '@models/container/dot-container.model';
 import { DotContentState } from '@dotcms/dotcms-models';
-import {
-    DotContainerListState,
-    DotContainerListStore
-} from '@portlets/dot-containers/container-list/store/dot-container-list.store';
+import { DotContainerListStore } from '@portlets/dot-containers/container-list/store/dot-container-list.store';
 
 @Component({
     selector: 'dot-container-list',
@@ -22,128 +10,19 @@ import {
     styleUrls: ['./container-list.component.scss'],
     providers: [DotContainerListStore]
 })
-export class ContainerListComponent implements OnInit, OnDestroy {
-    @ViewChild('listing', { static: false })
+export class ContainerListComponent {
     vm$ = this.store.vm$;
 
+    @ViewChild('listing', { static: false })
     listing: DotListingDataTableComponent;
-    selectedContainers: DotContainer[] = [];
 
-    containerBulkActions$: Observable<MenuItem[]>;
-    tableColumns$: Observable<DataTableColumn[]>;
-    actionHeaderOptions$: Observable<ActionHeaderOptions>;
-    addToBundleIdentifier$: Observable<string>;
-
-    updateBundleIdentifier = this.store.updateBundleIdentifier;
-
-    private isEnterPrise: boolean;
-    private hasEnvironments: boolean;
-    private destroy$: Subject<boolean> = new Subject<boolean>();
-
-    constructor(
-        private store: DotContainerListStore,
-        private dotMessageService: DotMessageService,
-        private dotAlertConfirmService: DotAlertConfirmService,
-        private dotPushPublishDialogService: DotPushPublishDialogService,
-        private dotRouterService: DotRouterService
-    ) {}
-
-    ngOnInit(): void {
-        this.store.updateTableColumns(this.setContainerColumns());
-        this.store.updateContainerBulkActions(this.setContainerBulkActions());
-
-        this.containerBulkActions$ = this.vm$.pipe(
-            takeUntil(this.destroy$),
-            pluck('containerBulkActions')
-        );
-
-        this.tableColumns$ = this.vm$.pipe(takeUntil(this.destroy$), pluck('tableColumns'));
-
-        this.actionHeaderOptions$ = this.vm$.pipe(
-            takeUntil(this.destroy$),
-            pluck('actionHeaderOptions')
-        );
-
-        this.addToBundleIdentifier$ = this.vm$.pipe(
-            takeUntil(this.destroy$),
-            pluck('addToBundleIdentifier')
-        );
-
-        this.vm$
-            .pipe(takeUntil(this.destroy$))
-            .subscribe(({ selectedContainers }: DotContainerListState) => {
-                this.selectedContainers = selectedContainers;
-            });
-
-        this.vm$
-            .pipe(takeUntil(this.destroy$))
-            .subscribe(({ isEnterPrise, hasEnvironments }: DotContainerListState) => {
-                this.isEnterPrise = isEnterPrise;
-                this.hasEnvironments = hasEnvironments;
-            });
-
-        this.setAddOptions();
-    }
-
-    ngOnDestroy(): void {
-        this.destroy$.next(true);
-        this.destroy$.complete();
-    }
-
-    private setContainerColumns(): DataTableColumn[] {
-        return [
-            {
-                fieldName: 'title',
-                header: this.dotMessageService.get('message.containers.fieldName.name'),
-                sortable: true
-            },
-            {
-                fieldName: 'status',
-                header: this.dotMessageService.get('message.containers.fieldName.status'),
-                width: '8%'
-            },
-            {
-                fieldName: 'friendlyName',
-                header: this.dotMessageService.get('message.containers.fieldName.description')
-            },
-            {
-                fieldName: 'modDate',
-                format: 'date',
-                header: this.dotMessageService.get('message.containers.fieldName.lastEdit'),
-                sortable: true
-            }
-        ];
-    }
-
-    private setAddOptions(): void {
-        this.store.updateActionHeaderOptions({
-            primary: {
-                command: () => {
-                    this.dotRouterService.gotoPortlet(`/container-new/new`);
-                }
-            }
-        });
-    }
-
-    /**
-     * set the labels of dot-state-icon.
-     * @returns { [key: string]: string }
-     * @memberof DotContainerListComponent
-     */
-    setStateLabels(): { [key: string]: string } {
-        return {
-            archived: this.dotMessageService.get('Archived'),
-            published: this.dotMessageService.get('Published'),
-            revision: this.dotMessageService.get('Revision'),
-            draft: this.dotMessageService.get('Draft')
-        };
-    }
+    constructor(private store: DotContainerListStore) {}
 
     /**
      * get the attributes that define the state of a container.
      * @param {DotContainer} { live, working, deleted, hasLiveVersion}
      * @returns DotContentState
-     * @memberof DotContainerListComponent
+     * @memberof ContainerListComponent
      */
     getContainerState({ live, working, deleted }: DotContainer): DotContentState {
         return { live, working, deleted, hasLiveVersion: live };
@@ -153,7 +32,7 @@ export class ContainerListComponent implements OnInit, OnDestroy {
      * Handle filter for hide / show archive containers
      * @param {boolean} checked
      *
-     * @memberof DotContainerListComponent
+     * @memberof ContainerListComponent
      */
     handleArchivedFilter(checked: boolean): void {
         checked
@@ -166,60 +45,18 @@ export class ContainerListComponent implements OnInit, OnDestroy {
      * Keep updated the selected containers in the grid
      * @param {DotContainer[]} containers
      *
-     * @memberof DotContainerListComponent
+     * @memberof ContainerListComponent
      */
     updateSelectedContainers(containers: DotContainer[]): void {
         this.store.updateSelectedContainers(containers);
     }
 
-    private setContainerBulkActions(): MenuItem[] {
-        return [
-            {
-                label: this.dotMessageService.get('Publish')
-            },
-            ...this.setLicenseAndRemotePublishContainerBulkOptions(),
-            {
-                label: this.dotMessageService.get('Unpublish')
-            },
-            {
-                label: this.dotMessageService.get('Archive')
-            },
-            {
-                label: this.dotMessageService.get('Unarchive')
-            },
-            {
-                label: this.dotMessageService.get('Delete')
-            }
-        ];
-    }
-
-    private setLicenseAndRemotePublishContainerBulkOptions(): MenuItem[] {
-        const bulkOptions: MenuItem[] = [];
-        if (this.hasEnvironments) {
-            bulkOptions.push({
-                label: this.dotMessageService.get('Remote-Publish'),
-                command: () => {
-                    this.dotPushPublishDialogService.open({
-                        assetIdentifier: this.selectedContainers
-                            .map((container) => container.identifier)
-                            .toString(),
-                        title: this.dotMessageService.get('contenttypes.content.push_publish')
-                    });
-                }
-            });
-        }
-
-        if (this.isEnterPrise) {
-            bulkOptions.push({
-                label: this.dotMessageService.get('Add-To-Bundle'),
-                command: () => {
-                    this.store.updateBundleIdentifier(
-                        this.selectedContainers.map((container) => container.identifier).toString()
-                    );
-                }
-            });
-        }
-
-        return bulkOptions;
+    /**
+     * Reset bundle state to null
+     *
+     * @memberof ContainerListComponent
+     */
+    resetBundleIdentifier(): void {
+        this.store.updateBundleIdentifier(null);
     }
 }
--- core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-list/store/dot-container-list.store.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-list/store/dot-container-list.store.ts
@@ -6,6 +6,9 @@ import { ActionHeaderOptions } from '@models/action-header';
 import { DataTableColumn } from '@models/data-table';
 import { ActivatedRoute } from '@angular/router';
 import { pluck, take } from 'rxjs/operators';
+import { DotMessageService } from '@services/dot-message/dot-messages.service';
+import { DotRouterService } from '@services/dot-router/dot-router.service';
+import { DotPushPublishDialogService } from '@dotcms/dotcms-js';
 
 export interface DotContainerListState {
     containerBulkActions: MenuItem[];
@@ -13,83 +16,55 @@ export interface DotContainerListState {
     addToBundleIdentifier: string;
     actionHeaderOptions: ActionHeaderOptions;
     tableColumns: DataTableColumn[];
-    isEnterPrise: boolean;
+    isEnterprise: boolean;
     hasEnvironments: boolean;
+    stateLabels: { [key: string]: string };
 }
-
-const defaultState: DotContainerListState = {
-    containerBulkActions: [],
-    selectedContainers: [],
-    addToBundleIdentifier: '',
-    actionHeaderOptions: {},
-    tableColumns: [],
-    isEnterPrise: false,
-    hasEnvironments: false
-};
 
 @Injectable()
 export class DotContainerListStore extends ComponentStore<DotContainerListState> {
-    constructor(private route: ActivatedRoute) {
-        super(defaultState);
+    constructor(
+        private route: ActivatedRoute,
+        private dotMessageService: DotMessageService,
+        private dotRouterService: DotRouterService,
+        private dotPushPublishDialogService: DotPushPublishDialogService
+    ) {
+        super(null);
+
+        this.init();
 
         this.route.data
             .pipe(pluck('dotContainerListResolverData'), take(1))
-            .subscribe(([isEnterPrise, hasEnvironments]: [boolean, boolean]) => {
-                this.updateIsEnterprise(isEnterPrise);
-                this.updateHasEnvironments(hasEnvironments);
+            .subscribe(([isEnterprise, hasEnvironments]: [boolean, boolean]) => {
+                this.updateResolvedProperties([isEnterprise, hasEnvironments]);
+                this.setContainerBulkActions();
             });
     }
 
     readonly vm$ = this.select(
         ({
             containerBulkActions,
-            selectedContainers,
             addToBundleIdentifier,
             actionHeaderOptions,
-            tableColumns
+            tableColumns,
+            stateLabels
         }: DotContainerListState) => {
             return {
                 containerBulkActions,
-                selectedContainers,
                 addToBundleIdentifier,
                 actionHeaderOptions,
-                tableColumns
+                tableColumns,
+                stateLabels
             };
         }
     );
 
-    readonly updateIsEnterprise = this.updater<boolean>(
-        (state: DotContainerListState, isEnterPrise: boolean) => {
+    readonly updateResolvedProperties = this.updater<[boolean, boolean]>(
+        (state: DotContainerListState, [isEnterprise, hasEnvironments]) => {
             return {
                 ...state,
-                isEnterPrise
-            };
-        }
-    );
-
-    readonly updateHasEnvironments = this.updater<boolean>(
-        (state: DotContainerListState, hasEnvironments: boolean) => {
-            return {
-                ...state,
+                isEnterprise,
                 hasEnvironments
-            };
-        }
-    );
-
-    readonly updateContainerBulkActions = this.updater<MenuItem[]>(
-        (state: DotContainerListState, bulkActions: MenuItem[]) => {
-            return {
-                ...state,
-                containerBulkActions: bulkActions
-            };
-        }
-    );
-
-    readonly updateTableColumns = this.updater<DataTableColumn[]>(
-        (state: DotContainerListState, tableColumns: DataTableColumn[]) => {
-            return {
-                ...state,
-                tableColumns
             };
         }
     );
@@ -112,12 +87,126 @@ export class DotContainerListStore extends ComponentStore<DotContainerListState>
         }
     );
 
-    readonly updateActionHeaderOptions = this.updater<ActionHeaderOptions>(
-        (state: DotContainerListState, actionHeaderOptions: ActionHeaderOptions) => {
+    private init() {
+        this.setState({
+            containerBulkActions: [],
+            tableColumns: this.setContainerColumns(),
+            stateLabels: this.setStateLabels(),
+            isEnterprise: false,
+            hasEnvironments: false,
+            addToBundleIdentifier: '',
+            selectedContainers: [],
+            actionHeaderOptions: this.setActionHeaderOptions()
+        });
+    }
+
+    private setContainerBulkActions() {
+        this.setState((state: DotContainerListState): DotContainerListState => {
             return {
                 ...state,
-                actionHeaderOptions
+                containerBulkActions: [
+                    {
+                        label: this.dotMessageService.get('Publish')
+                    },
+                    ...this.setLicenseAndRemotePublishContainerBulkOptions(state),
+                    {
+                        label: this.dotMessageService.get('Unpublish')
+                    },
+                    {
+                        label: this.dotMessageService.get('Archive')
+                    },
+                    {
+                        label: this.dotMessageService.get('Unarchive')
+                    },
+                    {
+                        label: this.dotMessageService.get('Delete')
+                    }
+                ]
             };
+        });
+    }
+
+    private setContainerColumns(): DataTableColumn[] {
+        return [
+            {
+                fieldName: 'title',
+                header: this.dotMessageService.get('message.containers.fieldName.name'),
+                sortable: true
+            },
+            {
+                fieldName: 'status',
+                header: this.dotMessageService.get('message.containers.fieldName.status'),
+                width: '8%'
+            },
+            {
+                fieldName: 'friendlyName',
+                header: this.dotMessageService.get('message.containers.fieldName.description')
+            },
+            {
+                fieldName: 'modDate',
+                format: 'date',
+                header: this.dotMessageService.get('message.containers.fieldName.lastEdit'),
+                sortable: true
+            }
+        ];
+    }
+
+    private setActionHeaderOptions(): ActionHeaderOptions {
+        return {
+            primary: {
+                command: () => {
+                    this.dotRouterService.gotoPortlet(`/container-new/create`);
+                }
+            }
+        };
+    }
+
+    /**
+     * set the labels of dot-state-icon.
+     * @returns { [key: string]: string }
+     * @memberof DotContainerListStore
+     */
+    private setStateLabels = () => {
+        return {
+            archived: this.dotMessageService.get('Archived'),
+            published: this.dotMessageService.get('Published'),
+            revision: this.dotMessageService.get('Revision'),
+            draft: this.dotMessageService.get('Draft')
+        };
+    };
+
+    private setLicenseAndRemotePublishContainerBulkOptions({
+        hasEnvironments,
+        isEnterprise
+    }: DotContainerListState): MenuItem[] {
+        const bulkOptions: MenuItem[] = [];
+        if (hasEnvironments) {
+            bulkOptions.push({
+                label: this.dotMessageService.get('Remote-Publish'),
+                command: () => {
+                    const state = this.get();
+                    this.dotPushPublishDialogService.open({
+                        assetIdentifier: state.selectedContainers
+                            .map((container) => container.identifier)
+                            .toString(),
+                        title: this.dotMessageService.get('contenttypes.content.push_publish')
+                    });
+                }
+            });
         }
-    );
+
+        if (isEnterprise) {
+            bulkOptions.push({
+                label: this.dotMessageService.get('Add-To-Bundle'),
+                command: () => {
+                    const state = this.get();
+                    this.updateBundleIdentifier(
+                        state.selectedContainers.map((container) => container.identifier).toString()
+                    );
+                }
+            });
+        }
+
+        return bulkOptions;
+    }
 }
