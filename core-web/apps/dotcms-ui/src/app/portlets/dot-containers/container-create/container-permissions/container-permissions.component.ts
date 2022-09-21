8479072b8a148e282b974579bcb718cb92950e6e
Zulqarnain Huda <zulqarnain.huda@venturedive.com>
2022-09-21 10:46:00 +0500

    refactoring

--- core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-create/container-permissions/container-permissions.component.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-create/container-permissions/container-permissions.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'dot-container-permissions',
     templateUrl: './container-permissions.component.html',
     styleUrls: ['./container-permissions.component.scss']
 })
-export class ContainerPermissionsComponent implements OnInit, OnChanges {
+export class ContainerPermissionsComponent implements OnInit {
     permissionsUrl = '';
 
     ngOnInit() {
         this.permissionsUrl = `/html/templates/permissions.jsp?templateId=7acdb856-4bbc-41c5-8695-a39c2e4a913f&popup=true&in_frame=true&frame=detailFrame&container=true&angularCurrentPortlet=templates`;
     }
-
-    ngOnChanges(): void {}
 }
