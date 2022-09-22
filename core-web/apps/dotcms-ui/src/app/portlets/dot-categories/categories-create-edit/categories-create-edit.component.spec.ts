77b209690d8cf72f5e827873d988c1e6bec14786
Zulqarnain Huda <zulqarnain.huda@venturedive.com>
2022-09-22 16:48:13 +0500

    fix unit test of category component

--- core-web/apps/dotcms-ui/src/app/portlets/dot-categories/categories-create-edit/categories-create-edit.component.spec.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-categories/categories-create-edit/categories-create-edit.component.spec.ts
@@ -1,14 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CategoriesCreateEditComponent } from './categories-create-edit.component';
 
+import { DotPortletBaseModule } from '@components/dot-portlet-base/dot-portlet-base.module';
+import { CategoriesListModule } from '../categories-list/categories-list.module';
+import { TabViewModule } from 'primeng/tabview';
+
+@Pipe({ name: 'dm' })
+class MockPipe implements PipeTransform {
+    transform(value: string): string {
+        return value;
+    }
+}
 describe('CategoriesCreateEditComponent', () => {
     let component: CategoriesCreateEditComponent;
     let fixture: ComponentFixture<CategoriesCreateEditComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [CategoriesCreateEditComponent]
+            declarations: [CategoriesCreateEditComponent, MockPipe],
+            imports: [DotPortletBaseModule, CategoriesListModule, TabViewModule]
         }).compileComponents();
 
         fixture = TestBed.createComponent(CategoriesCreateEditComponent);
