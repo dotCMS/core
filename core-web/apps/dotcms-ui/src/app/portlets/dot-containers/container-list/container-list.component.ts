ddc699486f29c5d50dd386438222a9803f1e8720
motasimvd <62100713+motasimvd@users.noreply.github.com>
2022-09-26 17:29:06 +0500

    Added Container listing table and Filters (#22920)
    * [WIP] Added Container listing and filters to Container list page
    
    * [WIP] Refactor Container list to use already provided Container Interface
    
    * [WIP] Container status is now displayed correctly
    
    * [WIP] Updated unit tests and minor refactor
    
    * Fixed unit test and listing data table
    
    * Added template outlet before listing table search, Container list style and ui refactor
    
    * Added strings to Language file and updated string keys in Container List component
    
    * Added Component Store to Container Component
    
    * Modified container list component to better use selectors
    
    * moved activeRoue logic to store
    
    * Refactored ContainerList functionality to ContainerListStore
    
    * Method name refactor and State update minimisation
    
    * Fixed state initialization

--- core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-list/container-list.component.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-list/container-list.component.ts
@@ -1,8 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { DotListingDataTableComponent } from '@components/dot-listing-data-table/dot-listing-data-table.component';
+import { DotContainer } from '@models/container/dot-container.model';
+import { DotContentState } from '@dotcms/dotcms-models';
+import { DotContainerListStore } from '@portlets/dot-containers/container-list/store/dot-container-list.store';
 
 @Component({
     selector: 'dot-container-list',
     templateUrl: './container-list.component.html',
-    styleUrls: ['./container-list.component.scss']
+    styleUrls: ['./container-list.component.scss'],
+    providers: [DotContainerListStore]
 })
-export class ContainerListComponent {}
+export class ContainerListComponent {
+    vm$ = this.store.vm$;
+
+    @ViewChild('listing', { static: false })
+    listing: DotListingDataTableComponent;
+
+    constructor(private store: DotContainerListStore) {}
+
+    /**
+     * get the attributes that define the state of a container.
+     * @param {DotContainer} { live, working, deleted, hasLiveVersion}
+     * @returns DotContentState
+     * @memberof ContainerListComponent
+     */
+    getContainerState({ live, working, deleted }: DotContainer): DotContentState {
+        return { live, working, deleted, hasLiveVersion: live };
+    }
+
+    /**
+     * Handle filter for hide / show archive containers
+     * @param {boolean} checked
+     *
+     * @memberof ContainerListComponent
+     */
+    handleArchivedFilter(checked: boolean): void {
+        checked
+            ? this.listing.paginatorService.setExtraParams('archive', checked)
+            : this.listing.paginatorService.deleteExtraParams('archive');
+        this.listing.loadFirstPage();
+    }
+
+    /**
+     * Keep updated the selected containers in the grid
+     * @param {DotContainer[]} containers
+     *
+     * @memberof ContainerListComponent
+     */
+    updateSelectedContainers(containers: DotContainer[]): void {
+        this.store.updateSelectedContainers(containers);
+    }
+
+    /**
+     * Reset bundle state to null
+     *
+     * @memberof ContainerListComponent
+     */
+    resetBundleIdentifier(): void {
+        this.store.updateBundleIdentifier(null);
+    }
+}
--- core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-list/container-list.module.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-list/container-list.module.ts
@@ -4,9 +4,31 @@ import { CommonModule } from '@angular/common';
 import { ContainerListRoutingModule } from './container-list-routing.module';
 import { DotPortletBaseModule } from '@components/dot-portlet-base/dot-portlet-base.module';
 import { ContainerListComponent } from './container-list.component';
+import { DotListingDataTableModule } from '@components/dot-listing-data-table';
+import { DotBaseTypeSelectorModule } from '@components/dot-base-type-selector';
+import { ButtonModule } from 'primeng/button';
+import { DotMessagePipeModule } from '@pipes/dot-message/dot-message-pipe.module';
+import { CheckboxModule } from 'primeng/checkbox';
+import { MenuModule } from 'primeng/menu';
+import { DotEmptyStateModule } from '@components/_common/dot-empty-state/dot-empty-state.module';
+import { DotContainerListResolver } from '@portlets/dot-containers/container-list/dot-container-list-resolver.service';
+import { DotAddToBundleModule } from '@components/_common/dot-add-to-bundle';
 
 @NgModule({
     declarations: [ContainerListComponent],
-    imports: [CommonModule, ContainerListRoutingModule, DotPortletBaseModule]
+    imports: [
+        CommonModule,
+        ContainerListRoutingModule,
+        DotPortletBaseModule,
+        DotListingDataTableModule,
+        DotBaseTypeSelectorModule,
+        DotMessagePipeModule,
+        ButtonModule,
+        CheckboxModule,
+        MenuModule,
+        DotEmptyStateModule,
+        DotAddToBundleModule
+    ],
+    providers: [DotContainerListResolver]
 })
 export class ContainerListModule {}
