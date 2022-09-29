adf59d6702a89399763b4ed1d6a5dbe077192b10
Motasim Hussain <motasim.hussain@venturedive.com>
2022-09-29 14:51:35 +0500

    Added MonacoEditor type model for reusability

--- core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-create/dot-container-properties/dot-container-properties.component.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-containers/container-create/dot-container-properties/dot-container-properties.component.ts
@@ -1,17 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { DotContainerPropertiesStore } from '@portlets/dot-containers/container-create/dot-container-properties/store/dot-container-properties.store';
-
-interface MonacoEditorOperation {
-    range: number;
-    text: string;
-    forceMoveMarkers: boolean;
-}
-
-interface MonacoEditor {
-    getSelection: () => number;
-    executeEdits: (action: string, data: MonacoEditorOperation[]) => void;
-}
+import { MonacoEditor } from '@models/monaco-editor';
 
 @Component({
     selector: 'dot-container-properties',
