413988ebe7d208f631197e3659831b915e7aa259
Arcadio Quintero A <arcadio.quintero@dotcms.com>
2022-10-11 14:24:36 -0400

    #22845 list and filter experiments (#23094)
    * List and filter experiments

--- core-web/apps/dotcms-ui/src/app/portlets/dot-experiments/dot-experiments-shell/dot-experiments-shell.module.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-experiments/dot-experiments-shell/dot-experiments-shell.module.ts
@@ -1,35 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { RouterModule, Routes } from '@angular/router';
-import { DotExperimentsListModule } from '../dot-experiments-list/dot-experiments-list.module';
+import { RouterModule } from '@angular/router';
 import { DotExperimentsShellComponent } from './dot-experiments-shell.component';
-import { DotExperimentsStore } from '../shared/stores/dot-experiments-store.service';
 import { DotLoadingIndicatorModule } from '@components/_common/iframe/dot-loading-indicator/dot-loading-indicator.module';
-
-const routes: Routes = [
-    {
-        path: '',
-        component: DotExperimentsShellComponent,
-        children: [
-            {
-                path: '',
-                loadChildren: async () =>
-                    (await import('../dot-experiments-list/dot-experiments-list.module'))
-                        .DotExperimentsListModule
-            }
-        ]
-    }
-];
+import { DotExperimentsUiHeaderComponent } from '../shared/ui/experiments-header/dot-experiments-ui-header.component';
+import { DotExperimentsShellRoutingModule } from '@portlets/dot-experiments/dot-experiments-shell/dot-experiments-shell-routing.module';
+import { DotExperimentsListModule } from '@portlets/dot-experiments/dot-experiments-list/dot-experiments-list.module';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { DotExperimentsShellStore } from './store/dot-experiments-shell-store.service';
 
 @NgModule({
     declarations: [DotExperimentsShellComponent],
     imports: [
         CommonModule,
-        RouterModule.forChild(routes),
+        RouterModule,
+
+        DotExperimentsShellRoutingModule,
         DotLoadingIndicatorModule,
-        DotExperimentsListModule
+        DotExperimentsListModule,
+        DotExperimentsUiHeaderComponent,
+
+        // PrimeNg
+        ToastModule
     ],
-    providers: [DotExperimentsStore]
+    providers: [DotExperimentsShellStore, MessageService]
 })
 export class DotExperimentsShellModule {}
---
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-experiments/shared/models/dot-experiments.model.ts
@@ -0,0 +1,36 @@
+import {
+    DotExperimentStatusList,
+    TrafficProportionTypes
+} from '@portlets/dot-experiments/shared/models/dot-experiments-constants';
+import { DotPage } from '@models/dot-page/dot-page.model';
+
+export interface DotExperiment {
+    id: string;
+    pageId: string;
+    name: string;
+    description: string;
+    status: DotExperimentStatusList;
+    readyToStart: boolean;
+    archived: boolean;
+    trafficProportion: TrafficProportion;
+    trafficAllocation: number;
+    scheduling: RangeOfDateAndTime | null;
+    creationDate: Date;
+    modDate: Date;
+}
+
+export type RenderedPageExperiments = Pick<DotPage, 'title' | 'identifier'>;
+
+export type GroupedExperimentByStatus = Partial<Record<DotExperimentStatusList, DotExperiment[]>>;
+
+interface TrafficProportion {
+    percentages: {
+        [index: string]: number;
+    };
+    type: TrafficProportionTypes;
+}
+
+interface RangeOfDateAndTime {
+    startDate: Date;
+    endDate: Date;
+}
