b66bf912e237ad35557073956dedbde574bd6b7f
erickgonzalez <erick.gonzalez@dotcms.com>
2022-10-24 15:57:54 -0600

    #21619 include in 22.03.3

--- HOTFIX_TRACKING.md
+++ HOTFIX_TRACKING.md
@@ -41,3 +41,4 @@ This maintenance release includes the following code fixes:
 30. https://github.com/dotCMS/core/issues/22916 : Need a way to run UT in LTS Releases #22916
 31. https://github.com/dotCMS/core/issues/21482 : [REST] : Updating one Binary Field is removing the value from the other Binary Field #21482
 32. https://github.com/dotCMS/core/issues/21885 : Template show archive not working #21885
+33. https://github.com/dotCMS/core/issues/21619 : ContentTypeFactoryImpl Getting Config values cause unneeded load #21619
--- dotCMS/src/integration-test/java/com/dotmarketing/util/ConfigTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/util/ConfigTest.java
@@ -1,10 +1,5 @@
 package com.dotmarketing.util;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
@@ -15,6 +10,8 @@ import java.util.NoSuchElementException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class ConfigTest {
 
@@ -259,6 +256,31 @@ public class ConfigTest {
 
         String notArray  =Config.getStringProperty("testing_string_with_comma");
         assertEquals(notArray, "VALUE1,VALUE2");
+    }
+
+    /**
+     * Method to test {@link Config#getBooleanProperty(String, boolean)}
+     * Here we test that the "same" property seen as an environmental var overrides the other regular property already set in the map
+     */
+    @Test
+    public void Test_Env_Prop_Overrides_Regular_Prop(){
+
+        final String DOT_MY_BOOLEAN_PROPERTY = "DOT_MY_BOOLEAN_PROPERTY";
+        final String MY_BOOLEAN_PROPERTY = "my.boolean.property";
+
+        //Now lets suppose the two properties exits in the Config
+        Config.setProperty(DOT_MY_BOOLEAN_PROPERTY, false);
+        Config.setProperty(MY_BOOLEAN_PROPERTY, true);
+        //But one must take precedence over the other and that's the one that starts with dot.
+
+        //if I request the dot prop one should easily expect the value it was initialized with
+        assertFalse(Config.getBooleanProperty(DOT_MY_BOOLEAN_PROPERTY,true));
+        //if I request the regular non-dot prop we should still get the value assigned to the dot prop because it overrides it
+        assertFalse(Config.getBooleanProperty(MY_BOOLEAN_PROPERTY,true));
+
+        //The second I get rid of the DOT property now I should get the original regular prop
+        Config.setProperty(DOT_MY_BOOLEAN_PROPERTY, null);
+        assertTrue(Config.getBooleanProperty(MY_BOOLEAN_PROPERTY,false));
     }
 
 
