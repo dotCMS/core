d5e864f35ff1eb208af7c41115621a2b28ab92fc
Rafael Velazco <rjvelazco21@gmail.com>
2022-10-25 15:15:49 -0400

    dotCMS/core#22904 Block Editor: Add char count information (#23198)
    * dev: add char count information v1
    
    * dev: set doc attributes v1
    
    * dev: set doc attributes v2
    
    * dev: hidden class v1
    
    * clean up
    
    * feedback
    
    * test

--- core-web/libs/block-editor/src/lib/shared/utils/prosemirror.utils.ts
+++ core-web/libs/block-editor/src/lib/shared/utils/prosemirror.utils.ts
@@ -2,6 +2,7 @@ import { ResolvedPos } from 'prosemirror-model';
 import { EditorView } from 'prosemirror-view';
 import { SelectionRange } from 'prosemirror-state';
 import { Editor } from '@tiptap/core';
+import { Step, StepResult } from 'prosemirror-transform';
 
 import { NodeTypes, CustomNodeTypes } from '@dotcms/block-editor';
 
@@ -177,3 +178,65 @@ export const replaceInlineContent = (content) => {
         ? inlineContent.replace(imgTagRex, '') + images.join('')
         : images.join('');
 };
+
+// Adapted from https://discuss.prosemirror.net/t/changing-doc-attrs/784
+export class SetDocAttrStep extends Step {
+    private key: string;
+    private value: string;
+    private prevValue: string;
+    private STEP_TYPE = 'setDocAttr';
+
+    constructor(key, value) {
+        super();
+        this.key = key;
+        this.value = value;
+    }
+
+    get stepType() {
+        return this.STEP_TYPE;
+    }
+
+    static fromJSON(_schema, json) {
+        return new SetDocAttrStep(json.key, json.value);
+    }
+
+    static register() {
+        try {
+            Step.jsonID(this.prototype.STEP_TYPE, SetDocAttrStep);
+        } catch (err) {
+            if (err.message !== `Duplicate use of step JSON ID ${this.prototype.STEP_TYPE}`)
+                throw err;
+        }
+
+        return true;
+    }
+
+    apply(doc) {
+        this.prevValue = doc.attrs[this.key];
+        // avoid clobbering doc.type.defaultAttrs
+        if (doc.attrs === doc.type.defaultAttrs) {
+            doc.attrs = Object.assign({}, doc.attrs);
+        }
+
+        doc.attrs[this.key] = this.value;
+
+        return StepResult.ok(doc);
+    }
+
+    invert() {
+        return new SetDocAttrStep(this.key, this.prevValue);
+    }
+
+    // position never changes so map should always return same step
+    map() {
+        return this;
+    }
+
+    toJSON() {
+        return {
+            stepType: this.stepType,
+            key: this.key,
+            value: this.value
+        };
+    }
+}
