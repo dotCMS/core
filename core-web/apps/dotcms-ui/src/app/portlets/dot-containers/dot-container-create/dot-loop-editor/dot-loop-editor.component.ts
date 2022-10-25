8fadc27982c2d0eb0be080425bbda49cfb8b31e5
Motasim Hussain <motasim.hussain@venturedive.com>
2022-10-25 19:29:43 +0500

    Implemented Control Value Accessor for DotLoopEditor Component

--- core-web/apps/dotcms-ui/src/app/portlets/dot-containers/dot-container-create/dot-loop-editor/dot-loop-editor.component.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-containers/dot-container-create/dot-loop-editor/dot-loop-editor.component.ts
@@ -1,18 +1,58 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
     selector: 'dot-loop-editor',
     templateUrl: './dot-loop-editor.component.html',
-    styleUrls: ['./dot-loop-editor.component.scss']
+    styleUrls: ['./dot-loop-editor.component.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => DotLoopEditorComponent),
+            multi: true
+        }
+    ]
 })
-export class DotLoopEditorComponent {
+export class DotLoopEditorComponent implements ControlValueAccessor, OnInit {
     @Input() isEditorVisible = false;
-    @Input() control: FormControl;
     @Output() buttonClick = new EventEmitter();
+
+    public readonly loopControl = new FormControl('');
 
     constructor() {
         //
+    }
+
+    public ngOnInit(): void {
+        this.loopControl.valueChanges.subscribe((fieldVal) => {
+            this._onChange(fieldVal);
+            this.onTouched();
+        });
+    }
+
+    public writeValue(value: string | null): void {
+        value = value ?? '';
+        this.loopControl.setValue(value);
+    }
+
+    public setDisabledState(isDisabled: boolean): void {
+        if (isDisabled) {
+            this.loopControl.disable();
+        } else {
+            this.loopControl.enable();
+        }
+    }
+
+    private _onChange = (_value: string | null) => undefined;
+
+    public registerOnChange(fn: (value: string | null) => void): void {
+        this._onChange = fn;
+    }
+
+    public onTouched = () => undefined;
+
+    public registerOnTouched(fn: () => void): void {
+        this.onTouched = fn;
     }
 
     /**
