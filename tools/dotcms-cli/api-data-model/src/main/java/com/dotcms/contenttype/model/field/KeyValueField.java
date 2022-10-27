938f7e42ca15e5118fd4f4d1d10ebd762b783ce9
Fabrizzio Araya <37148755+fabrizzio-dotCMS@users.noreply.github.com>
2022-10-27 15:44:29 -0600

    #22365  initial commit (#23171)
    * #22365  initial commit
    
    * #22365 adding all new classes need to fix broken polymorphism
    
    * #22365 adding the proper BasicPolymorphicTypeValidator required by Rest-Easy to validate safe json polymorphism
    
    * #22365 addintional json model work
    
    * #22365 Content-type command
    
    * exclude false values from the json
    
    * #22365 adding readmes
    
    * #22365 adding readmes
    
    * #22365 removed unwanted properties from the model

---
+++ tools/dotcms-cli/api-data-model/src/main/java/com/dotcms/contenttype/model/field/KeyValueField.java
@@ -0,0 +1,12 @@
+package com.dotcms.contenttype.model.field;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableKeyValueField.class)
+@JsonDeserialize(as = ImmutableKeyValueField.class)
+public abstract class KeyValueField extends Field {
+
+}
---
+++ tools/dotcms-cli/api-data-model/src/main/java/com/dotcms/contenttype/model/field/TabDividerField.java
@@ -0,0 +1,12 @@
+package com.dotcms.contenttype.model.field;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableTabDividerField.class)
+@JsonDeserialize(as = ImmutableTabDividerField.class)
+public abstract class TabDividerField extends Field {
+
+}
