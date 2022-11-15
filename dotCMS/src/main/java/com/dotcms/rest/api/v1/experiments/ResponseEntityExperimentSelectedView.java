516f20f9655686e60ea7e01cfc5490a142aa539f
freddyucv <freddy0309@gmail.com>
2022-11-15 08:34:11 -0600

    Doc

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/experiments/ResponseEntityExperimentSelectedView.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/experiments/ResponseEntityExperimentSelectedView.java
@@ -1,8 +1,12 @@
 package com.dotcms.rest.api.v1.experiments;
 
 import com.dotcms.experiments.business.web.SelectedExperiment;
+import com.dotcms.experiments.model.Experiment;
 import com.dotcms.rest.ResponseEntityView;
 
+/**
+ * {@link ResponseEntityView} for {@link Experiment}
+ */
 public class ResponseEntityExperimentSelectedView extends
         ResponseEntityView<SelectedExperiment> {
 
