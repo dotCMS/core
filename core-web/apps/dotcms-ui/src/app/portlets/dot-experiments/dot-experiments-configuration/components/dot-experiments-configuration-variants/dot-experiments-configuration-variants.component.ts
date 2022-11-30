b0237fea7a3ed1b4fdefb93ccbbbbe068f1bbcf8
Arcadio Quintero A <oidacra@gmail.com>
2022-11-30 11:50:40 -0500

    Issue 22984 add new variants (#23400)
    * #22984 - add, delete and go to Edit Page to see the variants

--- core-web/apps/dotcms-ui/src/app/portlets/dot-experiments/dot-experiments-configuration/components/dot-experiments-configuration-variants/dot-experiments-configuration-variants.component.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-experiments/dot-experiments-configuration/components/dot-experiments-configuration-variants/dot-experiments-configuration-variants.component.ts
@@ -1,12 +1,26 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
-import { TrafficProportion } from '@portlets/dot-experiments/shared/models/dot-experiments.model';
 import { DotMessagePipeModule } from '@pipes/dot-message/dot-message-pipe.module';
 import { ButtonModule } from 'primeng/button';
-import { MAX_VARIANTS_ALLOWED } from '@portlets/dot-experiments/shared/models/dot-experiments-constants';
+import {
+    DEFAULT_VARIANT_ID,
+    MAX_VARIANTS_ALLOWED,
+    SidebarStatus
+} from '@portlets/dot-experiments/shared/models/dot-experiments-constants';
 import { DotExperimentsConfigurationItemsCountComponent } from '@portlets/dot-experiments/dot-experiments-configuration/components/dot-experiments-configuration-items-count/dot-experiments-configuration-items-count.component';
 import { DotIconModule } from '@dotcms/ui';
+import { DotExperimentsConfigurationVariantsAddComponent } from '@portlets/dot-experiments/dot-experiments-configuration/components/dot-experiments-configuration-variants-add/dot-experiments-configuration-variants-add.component';
+import {
+    DotExperiment,
+    EditPageTabs,
+    ExperimentSteps,
+    StepStatus,
+    Variant
+} from '@portlets/dot-experiments/shared/models/dot-experiments.model';
+import { UiDotIconButtonModule } from '@components/_common/dot-icon-button/dot-icon-button.module';
+import { UiDotIconButtonTooltipModule } from '@components/_common/dot-icon-button-tooltip/dot-icon-button-tooltip.module';
+import { Status } from '@portlets/shared/models/shared-models';
 
 @Component({
     selector: 'dot-experiments-configuration-variants',
@@ -16,28 +30,29 @@ import { DotIconModule } from '@dotcms/ui';
         DotExperimentsConfigurationItemsCountComponent,
         DotMessagePipeModule,
         DotIconModule,
+        UiDotIconButtonModule,
         //PrimeNg
         CardModule,
-        ButtonModule
+        ButtonModule,
+        UiDotIconButtonTooltipModule,
+        DotExperimentsConfigurationVariantsAddComponent
     ],
     templateUrl: './dot-experiments-configuration-variants.component.html',
     styleUrls: ['./dot-experiments-configuration-variants.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DotExperimentsConfigurationVariantsComponent {
-    maxLength = MAX_VARIANTS_ALLOWED;
-    @Input()
-    trafficProportion: TrafficProportion;
+    statusList = Status;
+    sidebarStatusList = SidebarStatus;
+    maxVariantsAllowed = MAX_VARIANTS_ALLOWED;
+    defaultVariantId = DEFAULT_VARIANT_ID;
+    experimentStepName = ExperimentSteps.VARIANTS;
 
-    changeTrafficProportionType() {
-        // to implemented
-    }
+    @Input() stepStatus: StepStatus;
+    @Input() variants: Variant[];
 
-    viewVariant() {
-        // to be implemented
-    }
-
-    addNewVariant() {
-        // to be implemented
-    }
+    @Output() sidebarStatusChanged = new EventEmitter<SidebarStatus>();
+    @Output() delete = new EventEmitter<Variant>();
+    @Output() save = new EventEmitter<Pick<DotExperiment, 'name'>>();
+    @Output() goToEditPage = new EventEmitter<{ variant: Variant; mode: EditPageTabs }>();
 }
