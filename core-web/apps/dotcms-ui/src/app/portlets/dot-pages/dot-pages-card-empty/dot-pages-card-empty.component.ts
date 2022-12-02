88ebbe8c2b934d0dbb841155cd37c9c1dc9bdc53
alfredo-dotcms <37185433+alfredo-dotcms@users.noreply.github.com>
2022-12-02 11:33:55 -0600

    dotCMS/core#23058 Favorite Page portlet - listing component (#23437)
    * dotCMS/core#23058 Favorite Page portlet - listing component
    
    * dotCMS/core#23058 Favorite Page portlet - listing component - tests
    
    * Update on auto-generated documentation for dotcms-webcomponents
    
    * fix on dot-template-selector.component.spec.ts test failing
    
    * dotCMS/core#23058 Favorite Page portlet - listing component - Added missing doc
    
    * dotCMS/core#23058 Favorite Page portlet - listing component - Added Empty State
    
    * dotCMS/core#23058 Favorite Page portlet - listing component - Feedback
    
    * dotCMS/core#23058 Favorite Page portlet - listing component - Feedback 2
    
    * dotCMS/core#23058 Favorite Page portlet - listing component - Feedback 3

---
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-pages/dot-pages-card-empty/dot-pages-card-empty.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'dot-pages-card-empty',
+    templateUrl: './dot-pages-card-empty.component.html',
+    styleUrls: ['./dot-pages-card-empty.component.scss']
+})
+export class DotPagesCardEmptyComponent {}
