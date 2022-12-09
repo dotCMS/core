b5c11f68ce55b1e9808863126f86c030cf0a4adc
Will Ezell <will@dotcms.com>
2022-12-09 11:36:34 -0500

    #23508 5.2.8 working with externalized ES

--- docker/images/elasticsearch-6.1.3/ROOT/srv/entrypoint.sh
+++ docker/images/elasticsearch-6.1.3/ROOT/srv/entrypoint.sh
@@ -7,7 +7,4 @@ if [[ ! -z "${ES_NETWORK_PUBLISH_HOST}" ]]; then
   echo "network.publish_host: $ES_NETWORK_PUBLISH_HOST" >> /srv/config/elasticsearch.yml
 fi
 
-PUBLISH_HOST="${PUBLISH_HOST:-default_value}"
-
-
 bin/elasticsearch
