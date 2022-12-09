4c4b3641f1da23302ea4cf157d019b8744f99d24
Will Ezell <will@dotcms.com>
2022-12-09 17:56:40 -0500

    #23508 dbconnection factory

--- docker/images/elasticsearch-6.1.3/ROOT/srv/entrypoint.sh
+++ docker/images/elasticsearch-6.1.3/ROOT/srv/entrypoint.sh
@@ -3,16 +3,17 @@
 set -e
 
 if [[ ! -z "${ES_NETWORK_PUBLISH_HOST}" ]]; then
-
-    if  grep -q "Adding Network Publish Host" "/srv/config/elasticsearch.yml"; then
+    if  grep -q "# Network Publish Host" "/srv/config/elasticsearch.yml"; then
         echo "ES_NETWORK_PUBLISH_HOST already found"
-
     else
-        echo "# Adding Network Publish Host" >> /srv/config/elasticsearch.yml
+        echo "Add ES_NETWORK_PUBLISH_HOST"
+        echo "" >> /srv/config/elasticsearch.yml
+        echo "# Network Publish Host" >> /srv/config/elasticsearch.yml
         echo "" >> /srv/config/elasticsearch.yml
         echo "network.publish_host: $ES_NETWORK_PUBLISH_HOST" >> /srv/config/elasticsearch.yml
+        echo "" >> /srv/config/elasticsearch.yml
     fi
-
 fi
 
 bin/elasticsearch
+
