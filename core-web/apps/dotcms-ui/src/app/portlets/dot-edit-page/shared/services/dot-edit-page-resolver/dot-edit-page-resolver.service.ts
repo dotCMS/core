8113e6ec08a6fcfec267e9e342f1a61b91d057d9
Arcadio Quintero A <oidacra@gmail.com>
2022-12-16 09:54:56 -0500

    #23319 Edit Page change variant (#23580)

--- core-web/apps/dotcms-ui/src/app/portlets/dot-edit-page/shared/services/dot-edit-page-resolver/dot-edit-page-resolver.service.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-edit-page/shared/services/dot-edit-page-resolver/dot-edit-page-resolver.service.ts
@@ -1,10 +1,10 @@
-import { HttpResponse, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { throwError, Observable, of } from 'rxjs';
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 
-import { HttpCode, DotCMSResponse } from '@dotcms/dotcms-js';
-import { tap, switchMap, filter, catchError, map } from 'rxjs/operators';
+import { DotCMSResponse, HttpCode } from '@dotcms/dotcms-js';
+import { catchError, filter, map, switchMap, tap } from 'rxjs/operators';
 
 import { DotRouterService } from '@dotcms/app/api/services/dot-router/dot-router.service';
 import { DotHttpErrorManagerService } from '@dotcms/app/api/services/dot-http-error-manager/dot-http-error-manager.service';
