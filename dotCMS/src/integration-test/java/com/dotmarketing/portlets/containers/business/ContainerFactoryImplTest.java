3a193805d7d18a2c7bd2c4306560e147d29460da
Hassan Mustafa Baig <hassan.mustafa@venturedive.com>
2022-12-20 04:14:28 +0500

    Added integration test containers

--- dotCMS/src/integration-test/java/com/dotmarketing/portlets/containers/business/ContainerFactoryImplTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/portlets/containers/business/ContainerFactoryImplTest.java
@@ -23,42 +23,32 @@ import com.liferay.portal.model.User;
 
 public class ContainerFactoryImplTest {
 
-
     private static HostAPI hostAPI;
-    private static TemplateAPI templateAPI;
     private static User user;
     private static UserAPI userAPI;
     private static Host host;
 
     @BeforeClass
     public static void prepare() throws Exception {
-
         // Setting web app environment
         IntegrationTestInitService.getInstance().init();
 
-
         hostAPI = APILocator.getHostAPI();
-        templateAPI = APILocator.getTemplateAPI();
         userAPI = APILocator.getUserAPI();
         user = userAPI.getSystemUser();
-
         host = hostAPI.findDefaultHost(user, false);
     }
-    
-    
-    
+
     /**
      * this tests whether we properly escaping the orderby clause in the 
      * find containers method
      * @throws DotDataException
      * @throws DotSecurityException
      */
-    
     @Test
     public void test_container_find_by_query_no_SQL_injection_in_orderby() throws DotDataException, DotSecurityException {
 
       final Host daHost = new SiteDataGen().nextPersisted();
-
       
       Container container =  new ContainerDataGen()
                       .site(daHost)
@@ -69,22 +59,39 @@ public class ContainerFactoryImplTest {
       assertNotNull(container.getInode());
       Container containerFromDB = APILocator.getContainerAPI().find(container.getInode(), APILocator.systemUser(), false);
 
-      
       // get normally
       List<Container> containers = FactoryLocator.getContainerFactory().findContainers(user, false, ImmutableMap.of("title",container.getTitle() ), daHost.getIdentifier(), null, null, null, 0, 100, "mod_date");
       
       assert ! containers.isEmpty() && containers.contains(containerFromDB);
-       
-      
+
       // get with a malicious SQL order by
       containers = FactoryLocator.getContainerFactory().findContainers(user, false, ImmutableMap.of("title",container.getTitle() ), daHost.getIdentifier(), null, null, null, 0, 100, SQLUtilTest.MALICIOUS_SQL_ORDER_BY);
       
       assert ! containers.isEmpty() && containers.contains(containerFromDB);
-       
-      
-      
     }
 
+    @Test
+    public void test_find_inodeInputParameter_shouldReturnContainerByInode() throws DotDataException, DotSecurityException {
 
+        final Host daHost = new SiteDataGen().nextPersisted();
+
+        Container newContainer =  new ContainerDataGen()
+                .site(daHost)
+                .title(UUIDGenerator.generateUuid())
+                .nextPersisted();
+
+        assertNotNull(newContainer);
+        assertNotNull(newContainer.getInode());
+
+        Container containerFromDB = APILocator.getContainerAPI().find(newContainer.getInode(), APILocator.systemUser(), false);
+
+        final Container container = FactoryLocator.getContainerFactory().find(newContainer.getInode());
+
+        assertNotNull(container);
+        assertNotNull(container.getInode());
+
+        APILocator.getContainerAPI().delete(container, APILocator.systemUser(), false);
+        assertNull(FactoryLocator.getContainerFactory().find(newContainer.getInode()));
+    }
 
 }
