0eca6acf0dbafdc196a929dbf945f8a4e43e7e9b
Hassan Mustafa Baig <hassan.mustafa@venturedive.com>
2022-12-23 06:26:04 +0500

    reverted last commit

--- dotCMS/src/integration-test/java/com/dotmarketing/portlets/containers/business/ContainerFactoryImplTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/portlets/containers/business/ContainerFactoryImplTest.java
@@ -72,4 +72,121 @@ public class ContainerFactoryImplTest {
       
       assert ! containers.isEmpty() && containers.contains(containerFromDB);
     }
+
+    @Test
+    public void test_find_inodeInputParameter_shouldReturnContainerByInode() throws DotDataException, DotSecurityException {
+
+        final Host daHost = new SiteDataGen().nextPersisted();
+
+        Container newContainer =  new ContainerDataGen()
+                .site(daHost)
+                .title(UUIDGenerator.generateUuid())
+                .nextPersisted();
+
+        assertNotNull(newContainer);
+        assertNotNull(newContainer.getInode());
+
+        Container containerFromDB = APILocator.getContainerAPI().find(newContainer.getInode(), APILocator.systemUser(), false);
+
+        final Container container = FactoryLocator.getContainerFactory().find(newContainer.getInode());
+
+        assertNotNull(container);
+        assertNotNull(container.getInode());
+
+        APILocator.getContainerAPI().delete(container, APILocator.systemUser(), false);
+        assertNull(FactoryLocator.getContainerFactory().find(newContainer.getInode()));
+    }
+    @Test
+    public void test_save_containerWithoutIdentifier_shouldThrowException() throws DotDataException, DotSecurityException {
+
+        Container container = new Container();
+
+        container.setMaxContentlets(0);
+        container.setNotes("some notes");
+        container.setPreLoop("preLoop xxxx");
+        container.setPostLoop("postLoop xxxx");
+        container.setFriendlyName("Test container");
+        container.setModDate(new Date());
+        container.setModUser(user.getUserId());
+        container.setOwner(user.getUserId());
+        container.setTitle("Test container");
+        container.setInode(UUIDGenerator.generateUuid());
+
+        assertThrows(DotStateException.class,
+                ()->{
+                    FactoryLocator.getContainerFactory().save(container);
+                });
+    }
+    @Test
+    public void test_save_inputNewContainerData_shouldInsertNewContainer() throws DotDataException, DotSecurityException {
+
+        final String newContainerInode = UUIDGenerator.generateUuid();
+        final String newContainerIdentifier = UUIDGenerator.generateUuid();
+
+        Container container = new Container();
+
+        container.setMaxContentlets(0);
+        container.setNotes("some notes");
+        container.setPreLoop("preLoop xxxx");
+        container.setPostLoop("postLoop xxxx");
+        container.setFriendlyName("Test container");
+        container.setModDate(new Date());
+        container.setModUser(user.getUserId());
+        container.setOwner(user.getUserId());
+        container.setTitle("Test container");
+        container.setInode(newContainerInode);
+        container.setIdentifier(newContainerIdentifier);
+
+        final Container nonExistingContainer = FactoryLocator.getContainerFactory().find(newContainerInode);
+        assertNull(nonExistingContainer);
+
+        FactoryLocator.getContainerFactory().save(container);
+
+        final Container existingContainer = FactoryLocator.getContainerFactory().find(newContainerInode);
+        assertNotNull(existingContainer);
+
+        APILocator.getContainerAPI().delete(existingContainer, APILocator.systemUser(), false);
+        assertNull(FactoryLocator.getContainerFactory().find(existingContainer.getInode()));
+    }
+    @Test
+    public void test_save_inputExistingContainerData_shouldUpdateExistingContainer() throws DotDataException, DotSecurityException {
+
+        final String newContainerInode = UUIDGenerator.generateUuid();
+        final String newContainerIdentifier = UUIDGenerator.generateUuid();
+
+        Container container = new Container();
+
+        container.setMaxContentlets(0);
+        container.setNotes("some notes");
+        container.setPreLoop("preLoop xxxx");
+        container.setPostLoop("postLoop xxxx");
+        container.setFriendlyName("Test container");
+        container.setModDate(new Date());
+        container.setModUser(user.getUserId());
+        container.setOwner(user.getUserId());
+        container.setTitle("Test container");
+        container.setInode(newContainerInode);
+        container.setIdentifier(newContainerIdentifier);
+
+        final Container nonExistingContainer = FactoryLocator.getContainerFactory().find(newContainerInode);
+        assertNull(nonExistingContainer);
+
+        FactoryLocator.getContainerFactory().save(container);
+
+        Container existingContainer = FactoryLocator.getContainerFactory().find(newContainerInode);
+        assertNotNull(existingContainer);
+
+        existingContainer.setTitle("Updated title");
+        existingContainer.setFriendlyName("Updated friendly name");
+
+        FactoryLocator.getContainerFactory().save(existingContainer);
+
+        existingContainer = FactoryLocator.getContainerFactory().find(newContainerInode);
+
+        assertEquals("Updated title",existingContainer.getTitle());
+        assertEquals("Updated friendly name",existingContainer.getFriendlyName());
+
+        APILocator.getContainerAPI().delete(existingContainer, APILocator.systemUser(), false);
+        assertNull(FactoryLocator.getContainerFactory().find(existingContainer.getInode()));
+    }
 }
