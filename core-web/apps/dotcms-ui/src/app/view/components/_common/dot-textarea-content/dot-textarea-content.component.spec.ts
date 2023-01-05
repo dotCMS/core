b1de3fce0e531369bfef357d818c5716a533dd8f
Humberto Morera <humberto.morera@dotcms.com>
2023-01-05 16:05:55 -0600

    remove async

--- core-web/apps/dotcms-ui/src/app/view/components/_common/dot-textarea-content/dot-textarea-content.component.spec.ts
+++ core-web/apps/dotcms-ui/src/app/view/components/_common/dot-textarea-content/dot-textarea-content.component.spec.ts
@@ -189,7 +189,7 @@ describe('DotTextareaContentComponent', () => {
         expect(spy).toHaveBeenCalledTimes(2);
     });
 
-    it('should init editor with the correct value', async () => {
+    fit('should init editor with the correct value', () => {
         const mockEditor = { test: 'editor' };
         component.editorName = 'testName';
         spyOn(component.monacoInit, 'emit');
