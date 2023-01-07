067e229a97f5e3941f645125c6b576399c0f83b2
Victor Alfaro <victor.alfaro@dotcms.com>
2023-01-06 19:59:12 -0600

    #issue-23330: adding postman test matrix (#23630)
    Postman collection matrix

--- .github/actions/buildActions.sh
+++ .github/actions/buildActions.sh
@@ -10,7 +10,6 @@ for d in $(ls) ; do
   pushd ${d}
   if [[ "${mode}" == 'clean' ]]; then
     [[ -d ./node_modules ]] && rm -rf ./node_modules
-    [[ -f package-lock.json ]] && rm package-lock.json
     npm install
   fi
   npm run all
