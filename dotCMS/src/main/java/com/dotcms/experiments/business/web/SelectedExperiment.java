e8829652f7d234005fd4f7aebc0e2862ac1d069d
Freddy Rodriguez <freddy0309@gmail.com>
2023-01-17 16:25:38 -0600

    Add Extrada data to Jitsu events / connect all with jitsu (#23698)
    * Add Extrada data to Jitsu events / connect all with jitsu
    
    * Fixing error when send a Event to jitsu
    
    * Fixing test
    
    * Removing unneed change
    
    * Fixing test
    
    * Avoid Exception when the Varinat not have any url
    
    * Fixing test
    
    * Avoid Exception if Optional is empty
    
    * Doc
    
    * Stopping Experiment/Compare the right message
    
    * Fixing test
    
    * Fixing test
    
    * Rename ShortExperiment to LiteExperiment

--- dotCMS/src/main/java/com/dotcms/experiments/business/web/SelectedExperiment.java
+++ dotCMS/src/main/java/com/dotcms/experiments/business/web/SelectedExperiment.java
@@ -41,12 +41,19 @@ public class SelectedExperiment implements Serializable {
     @JsonProperty("variant")
     private final SelectedVariant variant;
 
-    public SelectedExperiment(final String id, final String name, final String pageUrl,
-            final SelectedVariant variant) {
+    /**
+     * Experiment's lookBackWindows'
+     */
+    @JsonProperty("lookBackWindow")
+    private final String lookBackWindow;
+
+    private SelectedExperiment(final String id, final String name, final String pageUrl,
+            final SelectedVariant variant, final String lookBackWindow) {
         this.id = id;
         this.name = name;
         this.pageUrl = pageUrl;
         this.variant = variant;
+        this.lookBackWindow = lookBackWindow;
     }
 
     public String id() {
@@ -63,5 +70,46 @@ public class SelectedExperiment implements Serializable {
 
     public SelectedVariant variant() {
         return variant;
+    }
+
+    public String getLookBackWindow() {
+        return lookBackWindow;
+    }
+
+    public static class Builder {
+        private String name;
+        private String id;
+        private String pageUrl;
+        private SelectedVariant variant;
+        private String lookBackWindow;
+
+        public Builder id(final String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(final String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder pageUrl(final String pageUrl) {
+            this.pageUrl = pageUrl;
+            return this;
+        }
+
+        public Builder variant(final SelectedVariant selectedVariant) {
+            this.variant = selectedVariant;
+            return this;
+        }
+
+        public Builder lookBackWindow(final String lookBackWindow) {
+            this.lookBackWindow = lookBackWindow;
+            return this;
+        }
+
+        public SelectedExperiment build(){
+            return new SelectedExperiment(id, name, pageUrl, variant, lookBackWindow);
+        }
     }
 }
