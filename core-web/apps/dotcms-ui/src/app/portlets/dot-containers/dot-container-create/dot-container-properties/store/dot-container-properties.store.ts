ee96c0badf4a3549a9e3bd3abbe664c03a63f6d3
Zulqarnain Huda <zulqarnain.huda@venturedive.com>
2023-01-17 16:44:18 +0500

    dotCMS/core#23591 fix container layout changeissue on load

--- core-web/apps/dotcms-ui/src/app/portlets/dot-containers/dot-container-create/dot-container-properties/store/dot-container-properties.store.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-containers/dot-container-create/dot-container-properties/store/dot-container-properties.store.ts
@@ -63,9 +63,9 @@ export class DotContainerPropertiesStore extends ComponentStore<DotContainerProp
             )
             .subscribe((containerEntity: DotContainerEntity) => {
                 const { container, contentTypes } = containerEntity;
-                if (container && (container.preLoop || container.postLoop)) {
+                if (container && contentTypes?.length > 0) {
                     this.updatePrePostLoopAndContentTypeVisibility({
-                        showPrePostLoopInput: true,
+                        showPrePostLoopInput: !!container.preLoop || !!container.postLoop,
                         isContentTypeVisible: true,
                         container: container,
                         containerStructures: contentTypes ?? []
