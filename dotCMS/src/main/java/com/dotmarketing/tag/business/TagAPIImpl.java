59942cc864cd6e3dec6095e79609fb15bf4d41f3
Daniel Silva <daniel.silva@dotcms.com>
2023-01-18 17:14:28 -0600

    #22872 Avoid creation of duplicate persona tags

--- dotCMS/src/main/java/com/dotmarketing/tag/business/TagAPIImpl.java
+++ dotCMS/src/main/java/com/dotmarketing/tag/business/TagAPIImpl.java
@@ -75,7 +75,7 @@ public class TagAPIImpl implements TagAPI {
     }
 
     @Override
-    public Tag getTagAndCreate(final String name, final String userId, final String siteId, final boolean persona, final boolean searchInSystemHost) throws DotDataException, DotSecurityException {
+    public Tag getTagAndCreate(final String tagName, final String userId, final String siteId, final boolean persona, final boolean searchInSystemHost) throws DotDataException, DotSecurityException {
 
         boolean localTransaction = false;
 
@@ -88,6 +88,12 @@ public class TagAPIImpl implements TagAPI {
 
             //Search for tags with this given name
             Tag existingTag = null;
+
+            final String tagNameNoPersona = tagName.endsWith(":persona")
+                    ? tagName.substring(0, tagName.indexOf(":persona"))
+                    : tagName;
+
+            final String tagNameNoPersona = tagName;
 
             //Before to decide if exist or not lets give it a try to the System host
             if ( searchInSystemHost ) {
@@ -102,7 +108,7 @@ public class TagAPIImpl implements TagAPI {
                 Tag hostTag = null;
                 Tag globalTag = null;
 
-                final List<Tag> existingTags = tagFactory.getTagsByName(name);
+                final List<Tag> existingTags = tagFactory.getTagsByName(tagNameNoPersona);
                 if ( existingTags != null ) {
                     for (final Tag foundTag : existingTags) {
 
@@ -134,13 +140,13 @@ public class TagAPIImpl implements TagAPI {
 
                 }
             } else {
-                existingTag = tagFactory.getTagByNameAndHost(name, siteId);
+                existingTag = tagFactory.getTagByNameAndHost(tagNameNoPersona, siteId);
             }
 
             // if doesn't exists then the tag is created
             if ( existingTag == null || !UtilMethods.isSet(existingTag.getTagId()) ) {
                 // creating tag
-                return saveTag(name, userId, siteId, persona);
+                return saveTag(tagNameNoPersona, userId, siteId, persona);
             } else {
 
                 String existHostId;
@@ -170,7 +176,7 @@ public class TagAPIImpl implements TagAPI {
                 if ( !globalTagExists ) {
                     //if global doesn't exist, then save the tag and after it checks if it was stored as a global tag
                     if ( !tagExists ) {
-                        newTag = saveTag(name, userId, siteId, persona);
+                        newTag = saveTag(tagNameNoPersona, userId, siteId, persona);
                     }
 
                     if ( newTag.getHostId().equals(Host.SYSTEM_HOST) ) {
@@ -193,7 +199,7 @@ public class TagAPIImpl implements TagAPI {
 
         } catch (final Exception e) {
             Logger.error(this, String.format("An error occurred when getting and creating Tag with name '%s' under " +
-                    "Site ID '%s': %s", name, siteId, e.getMessage()));
+                    "Site ID '%s': %s", tagName, siteId, e.getMessage()));
             if ( localTransaction ) {
                 HibernateUtil.rollbackTransaction();
             }
