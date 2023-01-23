2ccecda1f5c952adc23d6a12eace4dae4751f86c
Daniel Silva <daniel.silva@dotcms.com>
2023-01-23 17:03:21 -0500

    #22872 Avoid creation of duplicate persona tags (#23848)
    * #22872 Avoid creation of duplicate persona tags
    
    * #22872 remove dup line
    
    * #22872 Upgrade task to get rid of rels to dup tags, and dup tags per se
    
    * #22872 compile errors fix
    
    * #22872 IT for the UT
    
    * #22872 new method in TestDataUtil
    
    * #22872 delete with join for MSSQL. Add task to tasklocator. Add wrapIntransacton annotation

---
+++ dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task230119MigrateContentToProperPersonaTagAndRemoveDupTagsTest.java
@@ -0,0 +1,73 @@
+package com.dotmarketing.startup.runonce;
+
+import com.dotcms.datagen.TagDataGen;
+import com.dotcms.datagen.TestDataUtils;
+import com.dotcms.util.IntegrationTestInitService;
+import com.dotmarketing.business.APILocator;
+import com.dotmarketing.common.db.DotConnect;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.portlets.contentlet.model.Contentlet;
+import com.dotmarketing.tag.model.Tag;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Task230119MigrateContentToProperPersonaTagAndRemoveDupTagsTest {
+
+    @BeforeClass
+    public static void prepare() throws Exception {
+        IntegrationTestInitService.getInstance().init();
+    }
+
+    /**
+     * Method to test: {@link Task230119MigrateContentToProperPersonaTagAndRemoveDupTags#executeUpgrade()}
+     * Given scenario: duplicated persona tags, "hipster" and "hipster:persona" and related content to the latter
+     * Expected behavior: UT should
+     *  <ul>
+     *     <li>Insert new relationships between the correct tag and the content related to the wrong duplicate tag
+     *     <li>Delete bad relationships between content and the wrong duplicate tag
+     *     <li>Delete the duplicate tags
+     * </ul>
+     */
+    @Test
+    public void testUT() throws DotDataException {
+        final String realHipsterName = "hipster";
+        final String fakeHipsterName = "hipster:persona";
+        // Create dup tag and relate content to it
+        final Tag realHipster = new TagDataGen().name(realHipsterName).nextPersisted();
+        final Tag fakeHipster = new TagDataGen().name(fakeHipsterName).nextPersisted();
+        final Contentlet bannerWithTag = TestDataUtils.getBannerLikeContent(true, 1);
+        APILocator.getTagAPI().addContentletTagInode(fakeHipster,
+                bannerWithTag.getInode(), "tags");
+
+        List<Map<String, Object>> resultsBeforeUT = new DotConnect().setSQL("SELECT tagname\n"
+                + "FROM tag_inode JOIN tag t ON tag_inode.tag_id = t.tag_id\n"
+                + "WHERE inode = '" + bannerWithTag.getInode() + "'").loadObjectResults();
+
+        // get(1) since get(0) is a test tag created by the TestDataUtils
+        Assert.assertEquals(fakeHipsterName, resultsBeforeUT.get(1).get("tagname"));
+
+        new Task230119MigrateContentToProperPersonaTagAndRemoveDupTags().executeUpgrade();
+
+        List<Map<String, Object>> resultsAfterUT = new DotConnect().setSQL("SELECT tagname\n"
+                + "FROM tag_inode JOIN tag t ON tag_inode.tag_id = t.tag_id\n"
+                + "WHERE inode = '" + bannerWithTag.getInode() + "'").loadObjectResults();
+
+        Assert.assertEquals(realHipsterName, resultsAfterUT.get(1).get("tagname"));
+
+        // no related content with the wrong tag should be present
+        Assert.assertTrue(new DotConnect().setSQL("SELECT tagname\n"
+                + "FROM tag_inode JOIN tag t ON tag_inode.tag_id = t.tag_id\n"
+                + "WHERE tagname = '"+fakeHipsterName+"'").loadObjectResults().isEmpty());
+
+        // wrong tag should not be present anymore
+        Assert.assertTrue(new DotConnect()
+                .setSQL("SELECT * FROM tag WHERE tagname = '"+fakeHipsterName+"'")
+                .loadObjectResults()
+                .isEmpty());
+
+    }
+}
