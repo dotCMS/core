d59f67c44da298a27c8cf002791e47231b78e2bb
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-01-24 13:44:04 -0600

    Fix types in actions-menu.extension.ts

--- core-web/libs/block-editor/src/lib/extensions/action-button/actions-menu.extension.ts
+++ core-web/libs/block-editor/src/lib/extensions/action-button/actions-menu.extension.ts
@@ -38,7 +38,13 @@ declare module '@tiptap/core' {
                 range: Range;
                 type: { name: string; level?: number };
             }) => ReturnType;
-            addContentletBlock: ({ range: Range, payload: unknown }) => ReturnType;
+            addContentletBlock: ({
+                range,
+                payload
+            }: {
+                range: Range;
+                payload: unknown;
+            }) => ReturnType;
             addNextLine: () => ReturnType;
         };
     }
