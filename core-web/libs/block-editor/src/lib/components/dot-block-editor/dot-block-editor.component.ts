0deac8d09fad94482f078e91379b747a9e79b430
Rafael <rjvelazco21@gmail.com>
2023-01-26 17:20:53 -0400

    fix: configure image node

--- core-web/libs/block-editor/src/lib/components/dot-block-editor/dot-block-editor.component.ts
+++ core-web/libs/block-editor/src/lib/components/dot-block-editor/dot-block-editor.component.ts
@@ -210,7 +210,7 @@ export class DotBlockEditorComponent implements OnInit, OnDestroy {
             ...(this._allowedBlocks.includes('contentlets')
                 ? [customExtensions.get('contentlets')]
                 : []),
-            ...(this._allowedBlocks.includes('dotImage') ? [customExtensions.get('dotImage')] : [])
+            ...(this._allowedBlocks.includes('image') ? [customExtensions.get('image')] : [])
         ];
     }
 }
