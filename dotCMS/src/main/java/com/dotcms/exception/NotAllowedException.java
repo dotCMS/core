dcb61290d2525c2e9d57a81d36c602601bb2d2c6
Daniel Silva <daniel.silva@dotcms.com>
2023-01-27 16:37:57 -0600

    #23573 missing class

--- dotCMS/src/main/java/com/dotcms/exception/NotAllowedException.java
+++ dotCMS/src/main/java/com/dotcms/exception/NotAllowedException.java
@@ -1,2 +1,18 @@
-package com.dotcms.exception;public class NotAllowedException {
+package com.dotcms.exception;
+
+import com.dotmarketing.exception.DotRuntimeException;
+
+public class NotAllowedException extends DotRuntimeException {
+
+    public NotAllowedException(String message) {
+        super(message);
+    }
+
+    public NotAllowedException(Throwable cause) {
+        super(cause);
+    }
+
+    public NotAllowedException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
