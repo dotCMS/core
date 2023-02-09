ed25c5f8d4b13f98f9c1e6c207bfd50924e1e147
Will Ezell <will@dotcms.com>
2023-02-09 18:33:30 -0500

    #24075 removes caching the context

--- dotCMS/src/main/java/com/dotcms/rendering/velocity/directive/DotCacheDirective.java
+++ dotCMS/src/main/java/com/dotcms/rendering/velocity/directive/DotCacheDirective.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.velocity.context.InternalContextAdapter;
@@ -14,12 +13,9 @@ import org.apache.velocity.exception.ResourceNotFoundException;
 import org.apache.velocity.runtime.directive.Directive;
 import org.apache.velocity.runtime.parser.node.Node;
 import com.dotcms.enterprise.license.LicenseManager;
-import com.dotcms.enterprise.velocity.DotCachedInternalContextAdapter;
 import com.dotmarketing.business.BlockDirectiveCache;
 import com.dotmarketing.business.CacheLocator;
-import com.dotmarketing.util.Config;
 import com.dotmarketing.util.PageMode;
-import io.vavr.Lazy;
 import io.vavr.control.Try;
 
 public class DotCacheDirective extends Directive {
@@ -34,35 +30,17 @@ public class DotCacheDirective extends Directive {
         return BLOCK;
     }
 
+    private static final String REFRESH="refresh";
 
     public boolean render(InternalContextAdapter context, Writer writer, Node node)
                     throws IOException, ResourceNotFoundException, ParseErrorException, MethodInvocationException {
 
 
-        boolean noCache = false;
-        boolean refreshCache = false;
-
-
 
         HttpServletRequest request = (HttpServletRequest) context.get("request");
-
-        if (request != null && request.getParameter(getName()) != null) {
-            noCache = (request.getParameter(getName()).equals("no"));
-            refreshCache = (request.getParameter(getName()).equals("refresh"));
-        }
-
-
-        if (!allowExecution()) {
-            noCache = true;
-        }
-        
-        PageMode mode = PageMode.get(request);
-        // only force cache refresh for edit mode requests
-        if (mode.isEditMode() || mode == PageMode.PREVIEW_MODE) {
-            noCache = true;
-        }
-
-        if (noCache) {
+        boolean shouldCache = shouldCache(request);
+        boolean refreshCache = refreshCache(request);
+        if (!shouldCache) {
             node.jjtGetChild(2).render(context, writer);
             return true;
         }
@@ -76,38 +54,73 @@ public class DotCacheDirective extends Directive {
 
         Map<String, Serializable> savedMap = CacheLocator.getBlockDirectiveCache().get(key);
 
-        if (savedMap.isEmpty() || !savedMap.containsKey(BlockDirectiveCache.PAGE_CONTENT_KEY)) {
-            savedMap = new HashMap<>(savedMap);
-            // read and parse content in
-            final StringWriter content = new StringWriter();
-
-            DotCachedInternalContextAdapter localContext = new DotCachedInternalContextAdapter(context, savedMap);
-
-            node.jjtGetChild(2).render(localContext, content);
-            if(!DOTCACHE_VELOCITY_CONTEXT.get()) {
-                savedMap.clear();
-            }
-            savedMap.put(BlockDirectiveCache.PAGE_CONTENT_KEY, content.toString());
-            
-            CacheLocator.getBlockDirectiveCache().add(key, savedMap, ttl);
-
-        } else if(DOTCACHE_VELOCITY_CONTEXT.get()) {
-            savedMap.forEach(context::put);
+        if (savedMap.containsKey(BlockDirectiveCache.PAGE_CONTENT_KEY)) {
+            writer.write((String) savedMap.get(BlockDirectiveCache.PAGE_CONTENT_KEY));
+            return true;
         }
-        writer.write((String) savedMap.get(BlockDirectiveCache.PAGE_CONTENT_KEY));
 
-
+        final StringWriter blockContent = STRINGWRITER.get();
+        blockContent.getBuffer().setLength(0);
+        node.jjtGetChild(2).render(context, blockContent);
+        writer.write(blockContent.toString());
+        CacheLocator.getBlockDirectiveCache().add(key, Map.of(BlockDirectiveCache.PAGE_CONTENT_KEY, blockContent.toString()),
+                        ttl);
         return true;
+
+
     }
 
     protected boolean allowExecution() {
         return LicenseManager.getInstance().isEnterprise();
     }
 
-    private static final ThreadLocal<StringWriter> STRINGWRITER = ThreadLocal.withInitial(StringWriter::new);
+    private static final ThreadLocal<StringWriter> STRINGWRITER = ThreadLocal.withInitial(StringWriter::new); //NOSONAR
 
 
-    private static final Lazy<Boolean> DOTCACHE_VELOCITY_CONTEXT = Lazy.of(()->Config.getBooleanProperty("DOTCACHE_VELOCITY_CONTEXT", false));
+    boolean shouldCache(HttpServletRequest request) {
+        if (request == null) {
+            return false;
+        }
+        if ("no".equals(request.getParameter(getName())) || "no".equals(request.getAttribute(getName()))) {
+            return false;
+        }
+        if ("false".equals(request.getParameter(getName())) || "false".equals(request.getAttribute(getName()))) {
+            return false;
+        }
+        if ("true".equals(request.getParameter(getName())) || "true".equals(request.getAttribute(getName()))) {
+            return true;
+        }
+        if (refreshCache(request)) {
+            return false;
+        }
+        
+        if (!allowExecution()) {
+            return false;
+        }
+
+        PageMode mode = PageMode.get(request);
+        String url = Try.of(request::getRequestURI).getOrElse("");
+
+        // force cache refresh for edit mode requests (but not velocity endpoint)
+        if (mode.isAdmin && !url.contains("/api/vtl/dynamic")) {
+            return false;
+        }
+
+        return true;
+
+
+    }
+
+    boolean refreshCache(HttpServletRequest request) {
+        if (REFRESH.equals(request.getParameter(getName())) 
+            || REFRESH.equals(request.getAttribute(getName()))) {
+            return true;
+        }
+        return false;
+
+    }
     
     
+    
+
 }
