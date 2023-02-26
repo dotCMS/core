d8536adc31b379634ef2d0c8f43c4b84a7b6f5d5
Will Ezell <will@dotcms.com>
2023-02-26 12:18:32 -0500

    #24086 improves delete performance

--- dotCMS/src/main/java/com/dotmarketing/portlets/workflows/business/WorkflowFactoryImpl.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/workflows/business/WorkflowFactoryImpl.java
@@ -532,17 +532,26 @@ public class WorkflowFactoryImpl implements WorkFlowFactory {
 		final List<WorkflowTask> tasksToClearFromCache = this
 				.convertListToObjects(db.loadObjectResults(), WorkflowTask.class);
 
-		new DotConnect().setSQL("delete from workflow_comment where workflowtask_id   in (select id from workflow_task where webasset = ?)")
-				.addParam(webAsset).loadResult();
+		if(tasksToClearFromCache.isEmpty()) {
+		    return;
+		}
 
-		new DotConnect().setSQL("delete from workflow_history where workflowtask_id   in (select id from workflow_task where webasset = ?)")
-				.addParam(webAsset).loadResult();
 
-		new DotConnect().setSQL("delete from workflowtask_files where workflowtask_id in (select id from workflow_task where webasset = ?)")
-				.addParam(webAsset).loadResult();
+		final String parameters = "(" + String.join(",", tasksToClearFromCache.stream().map(q->"?").collect(Collectors.toList())) +")";
+		
+		db.setSQL("delete from workflow_comment where workflowtask_id in " + parameters);
+		tasksToClearFromCache.forEach(t->db.addParam(t.getId()));
+		db.loadResult();
 
-		new DotConnect().setSQL("delete from workflow_task where webasset = ?")
-				.addParam(webAsset).loadResult();
+		db.setSQL("delete from workflow_history where workflowtask_id   in " + parameters);
+        tasksToClearFromCache.forEach(t->db.addParam(t.getId()));
+        db.loadResult();
+
+        db.setSQL("delete from workflowtask_files where workflowtask_id in " + parameters   );
+        tasksToClearFromCache.forEach(t->db.addParam(t.getId()));
+        db.loadResult();
+        
+		db.setSQL("delete from workflow_task where webasset = ?").addParam(webAsset).loadResult();
 
 		tasksToClearFromCache.forEach(cache::remove);
 
