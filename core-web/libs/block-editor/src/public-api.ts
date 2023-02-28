61b4a9cf07a5e03fd780c7d6efa2e8ab10ef12cd
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-02-27 21:25:47 -0600

    Test

--- core-web/libs/block-editor/src/public-api.ts
+++ core-web/libs/block-editor/src/public-api.ts
@@ -2,3 +2,7 @@ export { getEditorBlockOptions } from './lib/shared/utils/suggestion.utils';
 
 export * from './lib/block-editor.module';
 export * from './lib/components/dot-block-editor/dot-block-editor.component';
+
+export function test() {
+    return 'test';
+}
