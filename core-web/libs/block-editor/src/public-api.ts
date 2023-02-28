a94714b72e57d7e9751b543147a7a3bd1a088442
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-02-27 23:26:59 -0600

    Revert "Test"
    This reverts commit 61b4a9cf07a5e03fd780c7d6efa2e8ab10ef12cd.

--- core-web/libs/block-editor/src/public-api.ts
+++ core-web/libs/block-editor/src/public-api.ts
@@ -2,7 +2,3 @@ export { getEditorBlockOptions } from './lib/shared/utils/suggestion.utils';
 
 export * from './lib/block-editor.module';
 export * from './lib/components/dot-block-editor/dot-block-editor.component';
-
-export function test() {
-    return 'test';
-}
