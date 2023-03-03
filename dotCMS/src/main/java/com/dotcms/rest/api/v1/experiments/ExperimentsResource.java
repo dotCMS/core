7411524697d7ca7f93c692e26538fb4667170aa9
freddyucv <freddy0309@gmail.com>
2023-03-03 14:15:27 -0600

    #24070 CHange Endpoint Return Object

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/experiments/ExperimentsResource.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/experiments/ExperimentsResource.java
@@ -433,7 +433,7 @@ public class ExperimentsResource {
     @NoCache
     @Path("/{id}/result")
     @Produces({MediaType.APPLICATION_JSON, "application/javascript"})
-    public ResponseEntityView<ExperimentResults> getResult(@Context final HttpServletRequest request,
+    public ResponseEntityExperimentResults getResult(@Context final HttpServletRequest request,
             @Context final HttpServletResponse response, @PathParam("id") String id
     ) throws DotDataException, DotSecurityException {
         final InitDataObject initData = getInitData(request, response);
@@ -445,7 +445,7 @@ public class ExperimentsResource {
 
         final ExperimentResults experimentResults = APILocator.getExperimentsAPI().getResults(experiment);
 
-        return new ResponseEntityView<>(experimentResults);
+        return new ResponseEntityExperimentResults(experimentResults);
     }
 
     private Experiment patchExperiment(final Experiment experimentToUpdate,
