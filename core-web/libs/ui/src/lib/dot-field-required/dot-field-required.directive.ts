3b698aba22cb077c3252dd2c1025d52a2a6dd019
Zulqarnain <zulqarnain.huda@venturedive.com>
2023-03-06 14:30:57 +0500

    dotCMS/core#23635 Add Docs in DotRequired Field Directive

--- core-web/libs/ui/src/lib/dot-field-required/dot-field-required.directive.ts
+++ core-web/libs/ui/src/lib/dot-field-required/dot-field-required.directive.ts
@@ -1,6 +1,11 @@
 import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 import { FormGroupDirective, Validators } from '@angular/forms';
 
+/**
+ * The purpose of this directive is to add or remove a class p-label-input-required to a HTML element based on whether the input control is required or not.
+ * The directive has a selector [dotFieldRequired] which can be used to apply this directive to any HTML element. It also has an input property checkIsRequiredControl which takes a string parameter controlName which is the name of the control to check if it is required or not.
+ */
+
 @Directive({
     selector: '[dotFieldRequired]',
     standalone: true
@@ -14,6 +19,11 @@ export class DotFieldRequiredDirective {
         renderer.addClass(this.el.nativeElement, 'p-label-input-required');
     }
 
+    /**
+     * Remove Required Class if it is not required
+     * @memberof DotFieldRequiredDirective
+     * @param {string} controlName
+     */
     @Input()
     set checkIsRequiredControl(controlName: string) {
         if (!this.isRequiredControl(controlName)) {
@@ -21,6 +31,13 @@ export class DotFieldRequiredDirective {
         }
     }
 
+    /**
+     * Helper function for check control is required or not
+     * @private
+     * @param {string} controlName
+     * @return {*}  {boolean}
+     * @memberof DotFieldRequiredDirective
+     */
     private isRequiredControl(controlName: string): boolean {
         const formControl = this.formGroupDirective.control?.get(controlName);
 
