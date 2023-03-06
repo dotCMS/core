5769e3b680b1d7de2bb96df1af0db22da63b1338
spbolton <steve.bolton@dotcms.com>
2023-03-06 15:44:24 -0500

    Use a queue for single thread reindex command processing

--- dotCMS/src/main/java/com/dotmarketing/common/reindex/ReindexQueueAPIImpl.java
+++ dotCMS/src/main/java/com/dotmarketing/common/reindex/ReindexQueueAPIImpl.java
@@ -3,29 +3,33 @@
  */
 package com.dotmarketing.common.reindex;
 
-import com.dotcms.contenttype.model.type.ContentType;
-import java.sql.Connection;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-
-import com.dotcms.business.CloseDBIfOpened;
-import com.dotcms.business.WrapInTransaction;
 import com.dotcms.content.elasticsearch.business.ElasticReadOnlyCommand;
+import com.dotcms.contenttype.model.type.ContentType;
 import com.dotcms.repackage.com.google.common.annotations.VisibleForTesting;
+import com.dotcms.util.ReturnableDelegate;
+import com.dotcms.util.VoidDelegate;
 import com.dotmarketing.beans.Host;
 import com.dotmarketing.beans.Identifier;
 import com.dotmarketing.business.FactoryLocator;
 import com.dotmarketing.common.reindex.ReindexQueueFactory.Priority;
 import com.dotmarketing.db.DbConnectionFactory;
+import com.dotmarketing.db.LocalTransaction;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.portlets.folders.model.Folder;
-import com.dotmarketing.util.Config;
 import com.dotmarketing.util.Logger;
 import com.dotmarketing.util.UtilMethods;
-import com.google.common.collect.ImmutableList;
-import io.vavr.control.Try;
+import java.sql.Connection;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author Jason Tesser
@@ -36,6 +40,137 @@ public class ReindexQueueAPIImpl implements ReindexQueueAPI {
 
     private final ReindexQueueFactory reindexQueueFactory;
     private final ElasticReadOnlyCommand esReadOnlyMonitor;
+
+    private final AtomicBoolean started = new AtomicBoolean(false);
+
+    private Thread reindexThread;
+
+    private final ThreadLocal<Boolean> isExecuting = ThreadLocal.withInitial(() -> false);
+
+
+    private final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
+
+
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+
+    private <T> CompletableFuture<T> executeAsync(ReturnableDelegate<T> task) {
+        try {
+            start();
+            CompletableFuture<T> future = new CompletableFuture<>();
+            if (isExecuting.get()) {
+                // execute the task directly if the current thread is from the executor
+                try {
+                    T response = task.execute();
+                    future.complete(response);
+                } catch (Throwable ex) {
+                    future.completeExceptionally(ex);
+                }
+            } else {
+                queue.put(() -> {
+                    try {
+                        isExecuting.set(true);
+                        T response = task.execute();
+                        future.complete(response);
+                    } catch (Throwable ex) {
+                        future.completeExceptionally(ex);
+                    } finally {
+                        isExecuting.set(false);
+                    }
+                });
+            }
+
+            return future;
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Failed to execute task", ex);
+        }
+    }
+
+    public <T>  T execute(ReturnableDelegate<T> task) throws DotDataException {
+           return executeAsync(task).join();
+    }
+
+
+    public void execute(VoidDelegate task) throws DotDataException {
+        try {
+            executeAsync(task).join();
+        } catch (Exception e) {
+            throw new DotDataException(e);
+        }
+    }
+
+    public void executeInTransaction(VoidDelegate task) throws DotDataException {
+        try {
+            executeAsync(() -> LocalTransaction.wrap(task)).join();
+        } catch (Exception e) {
+            throw new DotDataException(e);
+        }
+    }
+
+    public <T>  T executeInTransaction(ReturnableDelegate<T> task) throws DotDataException {
+        return executeAsync(() -> LocalTransaction.wrapReturn(task)).join();
+    }
+
+    private CompletableFuture<Void> executeAsync(VoidDelegate task) {
+        try {
+            start();
+            CompletableFuture<Void> future = new CompletableFuture<>();
+            if (isExecuting.get()) {
+                // execute the task directly if the current thread is from the executor
+                try {
+                    task.execute();
+                    future.complete(null);
+                } catch (Throwable ex) {
+                    future.completeExceptionally(ex);
+                }
+            } else {
+                queue.put(() -> {
+                    try {
+                        isExecuting.set(true);
+                        task.execute();
+                        future.complete(null);
+                    } catch (Throwable ex) {
+                        future.completeExceptionally(ex);
+                    } finally {
+                        isExecuting.set(false);
+                    }
+                });
+            }
+
+            return future;
+        }  catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Failed to execute task", ex);
+        }
+
+    }
+
+
+    private void processQueue() {
+        try {
+            do {
+                Runnable task = queue.take();
+                task.run();
+            } while (started.get() && !Thread.currentThread().isInterrupted());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Failed to process queue", e);
+        }
+    }
+
+    public void start() {
+        if (started.compareAndSet(false, true)) {
+            Logger.info(this, "Reindex Task Queue Starting");
+            executor.submit(this::processQueue);
+        }
+    }
+
+    public void stop() throws DotDataException {
+        if (started.compareAndSet(true, false)) {
+                execute(ExitTask::new);
+        }
+    }
 
     public ReindexQueueAPIImpl() {
         this(FactoryLocator.getReindexQueueFactory(), ElasticReadOnlyCommand.getInstance());
@@ -48,204 +183,186 @@ public class ReindexQueueAPIImpl implements ReindexQueueAPI {
     }
 
     @Override
-    @WrapInTransaction
     public void addStructureReindexEntries(final ContentType contentType) throws DotDataException {
-        reindexQueueFactory.addStructureReindexEntries(contentType);
+        executeInTransaction(() -> reindexQueueFactory.addStructureReindexEntries(contentType));
     }
 
     @Override
-    @WrapInTransaction
     public synchronized void addAllToReindexQueue() throws DotDataException {
-        reindexQueueFactory.addAllToReindexQueue();
+           execute(reindexQueueFactory::addAllToReindexQueue);
     }
 
     @Override
-    @CloseDBIfOpened
     public Map<String, ReindexEntry> findContentToReindex() throws DotDataException {
-        return this.findContentToReindex(this.reindexQueueFactory.REINDEX_RECORDS_TO_FETCH);
+        return this.findContentToReindex(
+                            ReindexQueueFactory.REINDEX_RECORDS_TO_FETCH);
     }
 
     @Override
-    @CloseDBIfOpened
     public Map<String, ReindexEntry> findContentToReindex(final int recordsToReturn) throws DotDataException {
-        return reindexQueueFactory.findContentToReindex(recordsToReturn);
+        return executeInTransaction(() -> reindexQueueFactory.findContentToReindex(recordsToReturn));
     }
 
     @Override
-    @WrapInTransaction
     public void deleteReindexEntry(ReindexEntry iJournal) throws DotDataException {
-        reindexQueueFactory.deleteReindexEntry(iJournal);
+        executeInTransaction(()-> reindexQueueFactory.deleteReindexEntry(iJournal));
     }
 
     @Override
-    @CloseDBIfOpened
     public boolean areRecordsLeftToIndex() throws DotDataException {
-        return reindexQueueFactory.areRecordsLeftToIndex();
+        return executeInTransaction(reindexQueueFactory::areRecordsLeftToIndex);
     }
 
     @Override
-    @CloseDBIfOpened
     public long recordsInQueue() throws DotDataException {
         return recordsInQueue(DbConnectionFactory.getConnection());
     }
     
     @Override
-    @CloseDBIfOpened
     public long failedRecordCount() throws DotDataException {
-        return reindexQueueFactory.failedRecordCount();
+        return executeInTransaction(reindexQueueFactory::failedRecordCount);
     }
-    @CloseDBIfOpened
     @Override
     public boolean hasReindexRecords() throws DotDataException {
-        return reindexQueueFactory.hasReindexRecords();
+        return executeInTransaction(reindexQueueFactory::hasReindexRecords);
     }
-    @CloseDBIfOpened
+
     @Override
     public long recordsInQueue(Connection conn) throws DotDataException {
-        return reindexQueueFactory.recordsInQueue(conn);
+        return executeInTransaction(()->reindexQueueFactory.recordsInQueue(conn));
     }
 
     @Override
-    @WrapInTransaction
     public void deleteReindexAndFailedRecords() throws DotDataException {
-        reindexQueueFactory.deleteReindexAndFailedRecords();
+        executeInTransaction(reindexQueueFactory::deleteReindexAndFailedRecords);
     }
 
     @Override
-    @WrapInTransaction
     public void deleteReindexRecords() throws DotDataException {
-        reindexQueueFactory.deleteReindexRecords();
+        executeInTransaction(reindexQueueFactory::deleteReindexRecords);
     }
     
     
     @Override
-    @WrapInTransaction
     public void deleteFailedRecords() throws DotDataException {
         reindexQueueFactory.deleteFailedRecords();
     }
 
     @Override
-    @WrapInTransaction
+
     public void refreshContentUnderHost(Host host) throws DotDataException {
-        reindexQueueFactory.refreshContentUnderHost(host);
+        executeInTransaction(()->reindexQueueFactory.refreshContentUnderHost(host));
     }
 
     @Override
-    @WrapInTransaction
     public void refreshContentUnderFolder(Folder folder) throws DotDataException {
-        reindexQueueFactory.refreshContentUnderFolder(folder);
+        executeInTransaction(()->reindexQueueFactory.refreshContentUnderFolder(folder));
     }
 
     @Override
-    @WrapInTransaction
     public void refreshContentUnderFolderPath(String hostId, String folderPath) throws DotDataException {
-        reindexQueueFactory.refreshContentUnderFolderPath(hostId, folderPath);
+        executeInTransaction(() ->reindexQueueFactory.refreshContentUnderFolderPath(hostId, folderPath));
     }
 
     @Override
-    @CloseDBIfOpened
     public List<ReindexEntry> getFailedReindexRecords() throws DotDataException {
-        return reindexQueueFactory.getFailedReindexRecords();
+        return executeInTransaction(reindexQueueFactory::getFailedReindexRecords);
     }
 
-    @WrapInTransaction
     @Override
     public void addIdentifierReindex(final String id) throws DotDataException {
-
-        Logger.info(this, "addIdentifierReindex: " + id);
-
-        this.reindexQueueFactory.addIdentifierReindex(id);
+        executeInTransaction(()->this.reindexQueueFactory.addIdentifierReindex(id));
     }
 
-    @WrapInTransaction
     @Override
     public void addIdentifierReindex(final String identifier, int priority) throws DotDataException {
-
-        this.reindexQueueFactory.addIdentifierReindex(identifier, priority);
+        executeInTransaction(()->this.reindexQueueFactory.addIdentifierReindex(identifier, priority));
     }
 
-    @WrapInTransaction
+
     @Override
     public void addReindexHighPriority(final String identifier) throws DotDataException {
 
-        this.reindexQueueFactory.addReindexHighPriority(identifier);
+        executeInTransaction(()->this.reindexQueueFactory.addReindexHighPriority(identifier));
     }
 
-    @WrapInTransaction
+
     @Override
     public int addIdentifierReindex(final Collection<String> ids) throws DotDataException {
 
-        return this.reindexQueueFactory.addIdentifierReindex(ids);
+        return executeInTransaction(() -> this.reindexQueueFactory.addIdentifierReindex(ids));
     }
-    
-    @WrapInTransaction
+
     @Override
     public int addIdentifierDelete(final Collection<String> ids) throws DotDataException {
 
-
         
-        return this.reindexQueueFactory.addIdentifierDelete(ids,Priority.NORMAL.dbValue());
+        return executeInTransaction(()->this.reindexQueueFactory.addIdentifierDelete(ids,Priority.NORMAL.dbValue()));
         
     }
 
-    @CloseDBIfOpened /*This is a highly concurrent method, in order to avoid table locks we won't use here WrapInTransaction*/
     @Override
     public int addIdentifierDelete(final String id) throws DotDataException {
-
-        return this.addIdentifierDelete(ImmutableList.of(id));
-        
+        return addIdentifierDelete(List.of(id));
     }
-    @WrapInTransaction
     @Override
     public int addReindexHighPriority(final Collection<String> ids) throws DotDataException {
 
-        return this.reindexQueueFactory.addReindexHighPriority(ids);
+        return executeInTransaction(()->this.reindexQueueFactory.addReindexHighPriority(ids));
     }
 
-    @WrapInTransaction
     @Override
     public void addContentletReindex(final Contentlet contentlet) throws DotDataException {
 
-        this.reindexQueueFactory.addIdentifierReindex(contentlet.getIdentifier());
+        executeInTransaction(()->this.reindexQueueFactory.addIdentifierReindex(contentlet.getIdentifier()));
     }
 
-    @WrapInTransaction
+
     @Override
     public void addContentletsReindex(final Collection<Contentlet> contentlet) throws DotDataException {
-        contentlet.forEach(con -> {
+        executeInTransaction(() -> contentlet.forEach(con -> {
             try {
-                addIdentifierReindex(con.getIdentifier());
+                this.reindexQueueFactory.addIdentifierReindex(con.getIdentifier());
             } catch (DotDataException e) {
                 Logger.warnAndDebug(this.getClass(), e);
             }
 
-        });
-
+        }));
     }
 
-    @WrapInTransaction
     @Override
     public void addIdentifierReindex(final Identifier identifier) throws DotDataException {
 
-        this.reindexQueueFactory.addIdentifierReindex(identifier.getId());
+        executeInTransaction(() ->this.reindexQueueFactory.addIdentifierReindex(identifier.getId()));
     }
 
-    @CloseDBIfOpened /*This is a highly concurrent method, in order to avoid table locks we won't use here WrapInTransaction*/
     @Override
     public void deleteReindexEntry(List<ReindexEntry> recordsToDelete) throws DotDataException {
-        reindexQueueFactory.deleteReindexEntry(recordsToDelete);
+        executeInTransaction(() ->reindexQueueFactory.deleteReindexEntry(recordsToDelete));
     }
 
     @Override
-    @WrapInTransaction
     public void deleteReindexEntry(String identiferToDelete) throws DotDataException {
-        reindexQueueFactory.deleteReindexEntry(identiferToDelete);
+        executeInTransaction(() ->reindexQueueFactory.deleteReindexEntry(identiferToDelete));
     }
 
     @Override
-    @WrapInTransaction
     public void markAsFailed(final ReindexEntry idx, final String cause) throws DotDataException {
-        reindexQueueFactory.markAsFailed(idx, UtilMethods.shortenString(cause, 300));
+        executeInTransaction(() -> reindexQueueFactory.markAsFailed(idx, UtilMethods.shortenString(cause, 300)));
     }
+
+    private class ExitTask implements Runnable {
+        @Override
+        public void run() {
+            executor.shutdown();
+            try {
+                executor.awaitTermination(10, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
 
 }
