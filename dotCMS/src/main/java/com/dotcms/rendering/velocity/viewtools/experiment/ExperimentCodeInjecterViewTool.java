da1c5b010d1546a7073506ee7da67c8917f852ae
freddyucv <freddy0309@gmail.com>
2023-03-08 13:22:10 -0600

    missing code

---
+++ dotCMS/src/main/java/com/dotcms/rendering/velocity/viewtools/experiment/ExperimentCodeInjecterViewTool.java
@@ -0,0 +1,37 @@
+package com.dotcms.rendering.velocity.viewtools.experiment;
+
+import com.dotcms.api.web.HttpServletRequestThreadLocal;
+import com.dotcms.experiments.business.ExperimentCodeGenerator;
+import com.dotmarketing.beans.Host;
+import com.dotmarketing.business.APILocator;
+import com.dotmarketing.business.web.WebAPILocator;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotRuntimeException;
+import com.dotmarketing.exception.DotSecurityException;
+import com.liferay.portal.model.User;
+import com.liferay.util.StringPool;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.velocity.tools.view.tools.ViewTool;
+
+public class ExperimentCodeInjecterViewTool implements ViewTool {
+
+    @Override
+    public void init(Object initData) {
+
+    }
+
+    @Override
+    public User getUser(HttpServletRequest request) {
+        return ViewTool.super.getUser(request);
+    }
+
+    public String code(){
+        try {
+            final Host currentHost = WebAPILocator.getHostWebAPI().getCurrentHost();
+            final HttpServletRequest request = HttpServletRequestThreadLocal.INSTANCE.getRequest();
+            return ExperimentCodeGenerator.INSTANCE.getCode(currentHost, request).orElse(StringPool.BLANK);
+        } catch (DotDataException | DotSecurityException e) {
+            throw new DotRuntimeException(e);
+        }
+    }
+}
