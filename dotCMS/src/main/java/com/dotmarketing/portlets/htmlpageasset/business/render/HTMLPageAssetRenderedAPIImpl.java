8ffa7b66e1b6bedc2efa3875ea8bfb1c8e49187d
freddyucv <freddy0309@gmail.com>
2023-03-08 12:58:37 -0600

    #23943 Create a ViewTool to inject the Experiment JS/HTML code

--- dotCMS/src/main/java/com/dotmarketing/portlets/htmlpageasset/business/render/HTMLPageAssetRenderedAPIImpl.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/htmlpageasset/business/render/HTMLPageAssetRenderedAPIImpl.java
@@ -1,10 +1,10 @@
 package com.dotmarketing.portlets.htmlpageasset.business.render;
 
-import static com.dotmarketing.util.FileUtil.getFileContentFromResourceContext;
-
 import com.dotcms.analytics.app.AnalyticsApp;
 import com.dotcms.analytics.helper.AnalyticsHelper;
 import com.dotcms.api.web.HttpServletRequestThreadLocal;
+import com.dotcms.experiments.business.ConfigExperimentUtil;
+import com.dotcms.experiments.business.ExperimentCodeGenerator;
 import com.dotcms.rendering.velocity.services.PageLoader;
 import com.dotcms.repackage.com.google.common.annotations.VisibleForTesting;
 import com.dotcms.visitor.domain.Visitor;
@@ -41,32 +41,17 @@ import com.dotmarketing.portlets.rules.model.Rule.FireOn;
 import com.dotmarketing.util.Logger;
 import com.dotmarketing.util.PageMode;
 import com.dotmarketing.util.UUIDUtil;
-import com.dotmarketing.util.UtilMethods;
 import com.dotmarketing.util.WebKeys;
 import com.liferay.portal.model.User;
 import com.liferay.util.StringPool;
-import io.vavr.Lazy;
 import io.vavr.control.Try;
 
-import java.io.IOException;
-
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.core.Context;
-import org.apache.commons.lang3.StringUtils;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
+
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -300,28 +285,20 @@ public class HTMLPageAssetRenderedAPIImpl implements HTMLPageAssetRenderedAPI {
                 .setLive(htmlPageUrl.hasLive())
                 .getPageHTML(context.getPageMode());
 
-        if (context.getPageMode() == PageMode.LIVE) {
-            return APILocator.getExperimentsAPI().isAnyExperimentRunning() ?
-                    injectExperimentJSCode(pageHTML, host, request) : injectNoExperimentCode(pageHTML);
+        if (context.getPageMode() == PageMode.LIVE && ConfigExperimentUtil.INSTANCE.isExperimentAutoJsInjection()) {
+            return ExperimentCodeGenerator.INSTANCE.getCode(host, request)
+                    .map(jsCodeToBeInjected -> injectJSCode(pageHTML, jsCodeToBeInjected))
+                    .orElse(pageHTML);
         } else {
             return pageHTML;
         }
     }
 
-    private String injectExperimentJSCode(final String pageHTML, final Host host, final HttpServletRequest request) {
-        return injectJSCode(pageHTML, getExperimentJSCode(host, request));
-    }
-
-    private String injectNoExperimentCode(final String pageHTML) {
-        return injectJSCode(pageHTML, getNotExperimentJSCode());
-    }
-
-
     private String injectJSCode(final String pageHTML, final String JsCode) {
 
         if (StringUtils.containsIgnoreCase(pageHTML, "<head>")) {
-            return StringUtils.replaceIgnoreCase(pageHTML, "<head>",
-                    "<head>" + JsCode);
+            final int indexOf = pageHTML.toLowerCase().indexOf("<head>".toLowerCase());
+            return pageHTML.substring(0, indexOf + 6) + JsCode + pageHTML.substring(indexOf + 6);
         } else {
             return JsCode + "\n" + pageHTML;
         }
@@ -608,31 +585,6 @@ public class HTMLPageAssetRenderedAPIImpl implements HTMLPageAssetRenderedAPI {
                 .setLive(false).build(true, PageMode.PREVIEW_MODE)).getHtml();
 
         return new PageLivePreviewVersionBean(renderLive, renderWorking);
-    }
-
-    private String getNotExperimentJSCode() {
-        return "<SCRIPT>localStorage.removeItem('experiment_data');</SCRIPT>";
-    }
-
-    private String getExperimentJSCode(final Host host, final HttpServletRequest request) {
-
-        try {
-            final String analyticsKey = getAnalyticsKey(host);
-            final String jsJitsuCode =  getFileContentFromResourceContext("experiment/html/experiment_head.html")
-                    .replaceAll("\\$\\{jitsu_key}", analyticsKey)
-                    .replaceAll("\\$\\{site}", getLocalServerName(request));
-
-            final String runningExperimentsId = APILocator.getExperimentsAPI().getRunningExperiments().stream()
-                    .map(experiment -> "'" + experiment.id().get() + "'")
-                    .collect(Collectors.joining(","));
-
-            final String shouldBeInExperimentCalled =  getFileContentFromResourceContext("experiment/js/init_script.js")
-                    .replaceAll("\\$\\{running_experiments_list}", runningExperimentsId);
-
-            return jsJitsuCode + "\n<SCRIPT>" + shouldBeInExperimentCalled + "</SCRIPT>";
-        } catch (IOException | DotDataException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @Nullable
