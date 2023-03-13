84b9accd353914c9a66b74d061b22d5b1706b43c
Jalinson Diaz <zjaaaldev@gmail.com>
2023-03-13 17:19:23 -0300

    add needed modules and methods to dot-api-link

--- core-web/apps/dotcms-ui/src/app/view/components/dot-api-link/dot-api-link.component.ts
+++ core-web/apps/dotcms-ui/src/app/view/components/dot-api-link/dot-api-link.component.ts
@@ -16,4 +16,8 @@ export class DotApiLinkComponent {
     private getFixedLink(link: string): string {
         return link.startsWith('/') ? link : `/${link}`;
     }
+
+    openLink() {
+        window.open(this.link, '_blank');
+    }
 }
