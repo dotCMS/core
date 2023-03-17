ac8f4b16d94bfca74f0a611def085cdb38cc68bf
Jalinson Diaz <zjaaaldev@gmail.com>
2023-03-17 13:46:22 -0300

    compile and use dot-counter as webcomponent

--- core-web/apps/angular-elements/src/app/app.component.ts
+++ core-web/apps/angular-elements/src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { DotCounterComponent } from './components/dot-counter/dot-counter.component';
+// import { DotCounterComponent } from './components/dot-counter/dot-counter.component';
 
 @Component({
     standalone: true,
-    imports: [DotCounterComponent],
+    imports: [],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
     selector: 'dotcms-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
     title = 'angular-elements';
+
+    alertFlag(flag: Event) {
+        alert(`Current flag: ${flag}`);
+    }
 }
--- core-web/apps/angular-elements/src/main.ts
+++ core-web/apps/angular-elements/src/main.ts
@@ -1,5 +1,15 @@
+import { createCustomElement } from '@angular/elements';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { DotCounterComponent } from './app/components/dot-counter/dot-counter.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent)
+    .then((app) => {
+        const myCounterButton = createCustomElement(DotCounterComponent, {
+            injector: app.injector
+        });
+
+        customElements.define('wc-counter', myCounterButton);
+    })
+    .catch((err) => console.error(err));
