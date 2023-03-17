b450ac0b7463696ade8924e26d75b4e2fc3572c9
Jalinson Diaz <zjaaaldev@gmail.com>
2023-03-17 18:50:00 -0300

    testing

--- core-web/apps/angular-elements/src/app/app.component.ts
+++ core-web/apps/angular-elements/src/app/app.component.ts
@@ -1,19 +1,13 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component } from '@angular/core';
 
 // import { DotCounterComponent } from './components/dot-counter/dot-counter.component';
 
 @Component({
     standalone: true,
     imports: [],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
     selector: 'dotcms-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    title = 'angular-elements';
-
-    alertFlag(flag: Event) {
-        alert(`Current flag: ${(flag as CustomEvent).detail}`);
-    }
-}
+export class AppComponent {}
