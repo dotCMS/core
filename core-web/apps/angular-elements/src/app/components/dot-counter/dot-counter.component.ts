6aba85399257ce8e0cf4a77ee0bba01320348981
Jalinson Diaz <zjaaaldev@gmail.com>
2023-03-17 13:45:54 -0300

    add simple functionality and styles to dot-counter

--- core-web/apps/angular-elements/src/app/components/dot-counter/dot-counter.component.ts
+++ core-web/apps/angular-elements/src/app/components/dot-counter/dot-counter.component.ts
@@ -1,12 +1,49 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    Input,
+    OnInit,
+    Output,
+    EventEmitter,
+    DoCheck
+} from '@angular/core';
 
 @Component({
     selector: 'dotcms-dot-counter',
     standalone: true,
-    imports: [CommonModule],
+    imports: [],
     templateUrl: './dot-counter.component.html',
     styleUrls: ['./dot-counter.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DotCounterComponent {}
+export class DotCounterComponent implements OnInit, DoCheck {
+    @Input() initialValue?: number;
+
+    @Input() step?: number;
+
+    @Input() flag?: number;
+
+    @Output() flagReached: EventEmitter<number> = new EventEmitter();
+
+    count = 0;
+
+    ngOnInit() {
+        this.count = this.initialValue ?? 0;
+    }
+
+    ngDoCheck() {
+        if (this.flag !== undefined && this.count == this.flag) this.flagReached.emit(this.flag);
+    }
+
+    increment() {
+        this.count += this.step ?? 1;
+    }
+
+    decrement() {
+        this.count -= this.step ?? 1;
+    }
+
+    reset() {
+        this.count = this.initialValue ?? 0;
+    }
+}
