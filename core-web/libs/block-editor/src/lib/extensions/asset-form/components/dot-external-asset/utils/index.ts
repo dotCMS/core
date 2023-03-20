0da5d6439fcca735191377628a83173cec4182a6
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-03-20 10:19:26 -0600

    Update core-web/libs/block-editor/src/lib/extensions/asset-form/components/dot-external-asset/utils/index.ts

--- core-web/libs/block-editor/src/lib/extensions/asset-form/components/dot-external-asset/utils/index.ts
+++ core-web/libs/block-editor/src/lib/extensions/asset-form/components/dot-external-asset/utils/index.ts
@@ -13,7 +13,7 @@ export const handleLoadVideoError = (e): string => {
             return 'A network error caused the video download to fail part-way.';
 
         case e.target.error.MEDIA_ERR_DECODE:
-            return 'The video playback was aborted due to a corruption problem or because the video used features your browser did not support. For more info, visit:  <a href="https://developer.mozilla.org/en-US/docs/Web/Media/Formats#media_file_types_and_codecs">MDN Video Support</a>';
+            return Video playback aborted due to browser compatibility issues. Try a different browser or visit <a href="https://developer.mozilla.org/en-US/docs/Web/Media/Formats#media_file_types_and_codecs">MDN Video Support</a> for more information.';
 
         case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
             return 'Invalid URL. Please provide a URL to a .mp4, .webm, or .ogv video file. For more info, visit:  <a href="https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Containers#common_container_formats" target="_blank">MDN Video Format Support</a>';
