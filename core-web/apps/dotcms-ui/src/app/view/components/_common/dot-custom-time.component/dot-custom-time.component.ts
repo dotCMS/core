7a023be628716fb74fc252038bb2d6120e462f83
Jalinson Diaz <zjaaaldev@gmail.com>
2023-03-20 11:26:14 -0300

    set changeDetection to on push

--- core-web/apps/dotcms-ui/src/app/view/components/_common/dot-custom-time.component/dot-custom-time.component.ts
+++ core-web/apps/dotcms-ui/src/app/view/components/_common/dot-custom-time.component/dot-custom-time.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
     encapsulation: ViewEncapsulation.Emulated,
     selector: 'dot-custom-time',
     styleUrls: ['./dot-custom-time.component.scss'],
-    templateUrl: 'dot-custom-time.component.html'
+    templateUrl: 'dot-custom-time.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CustomTimeComponent {
     @Input() time: string;
