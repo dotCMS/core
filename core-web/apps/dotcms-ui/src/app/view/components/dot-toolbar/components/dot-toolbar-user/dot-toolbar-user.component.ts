09a09396d11619d904b3fcd4f6092d481679fc54
Jalinson Diaz <zjaaaldev@gmail.com>
2023-03-21 09:45:50 -0300

    add header of menu to p-menu component

--- core-web/apps/dotcms-ui/src/app/view/components/dot-toolbar/components/dot-toolbar-user/dot-toolbar-user.component.ts
+++ core-web/apps/dotcms-ui/src/app/view/components/dot-toolbar/components/dot-toolbar-user/dot-toolbar-user.component.ts
@@ -1,13 +1,15 @@
+import * as md5 from 'md5';
 import { Observable } from 'rxjs';
 
 import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 
 import { MenuItem } from 'primeng/api';
 
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 
 import { DotDropdownComponent } from '@components/_common/dot-dropdown-component/dot-dropdown.component';
 import { DotNavigationService } from '@components/dot-navigation/services/dot-navigation.service';
+import { DotGravatarService } from '@dotcms/app/api/services/dot-gravatar-service';
 import { DotRouterService } from '@dotcms/app/api/services/dot-router/dot-router.service';
 import { LOCATION_TOKEN } from '@dotcms/app/providers';
 import { DotMessageService } from '@dotcms/data-access';
@@ -39,7 +41,8 @@ export class DotToolbarUserComponent implements OnInit {
         public iframeOverlayService: IframeOverlayService,
         private dotNavigationService: DotNavigationService,
         private dotRouterService: DotRouterService,
-        private dotMessageService: DotMessageService
+        private dotMessageService: DotMessageService,
+        private dotGravatarService: DotGravatarService
     ) {}
 
     ngOnInit(): void {
@@ -47,40 +50,52 @@ export class DotToolbarUserComponent implements OnInit {
             this.auth = auth;
         });
 
-        this.items$ = this.loginService.getCurrentUser().pipe(
-            map(
-                ({ loginAs }: CurrentUser) =>
-                    [
-                        {
-                            id: 'dot-toolbar-user-link-my-account',
-                            label: this.dotMessageService.get('my-account'),
-                            icon: 'pi pi-user-edit',
-                            visible: !this.auth.loginAsUser,
-                            command: () => this.toggleMyAccount()
-                        },
-                        {
-                            id: 'dot-toolbar-user-link-login-as',
-                            label: this.dotMessageService.get('login-as'),
-                            icon: 'pi pi-sort-alt',
-                            visible: !this.auth.loginAsUser && loginAs,
-                            command: () => this.tooggleLoginAs()
-                        },
-                        { separator: true },
-                        {
-                            id: 'dot-toolbar-user-link-logout',
-                            label: this.dotMessageService.get('Logout'),
-                            icon: 'pi pi-sign-out',
-                            visible: !this.auth.loginAsUser,
-                            url: this.logoutUrl
-                        },
-                        {
-                            id: 'dot-toolbar-user-link-logout-as',
-                            label: this.dotMessageService.get('logout-as'),
-                            icon: 'pi pi-sign-out',
-                            visible: !!this.auth.loginAsUser,
-                            command: (event) => this.logoutAs(event.originalEvent)
-                        }
-                    ] as MenuItem[]
+        this.items$ = this.dotGravatarService.getPhoto(md5(this.getEmailFromAuthState())).pipe(
+            switchMap((photo) =>
+                this.loginService.getCurrentUser().pipe(
+                    map(
+                        ({ loginAs }: CurrentUser) =>
+                            [
+                                {
+                                    id: 'toolbar-header',
+                                    label: `<div class="toolbar-user__header">
+                                    ${this.getTemplateFromPhoto(photo)}
+                                    </div>`,
+                                    escape: false
+                                },
+                                { separator: true },
+                                {
+                                    id: 'dot-toolbar-user-link-my-account',
+                                    label: this.dotMessageService.get('my-account'),
+                                    icon: 'pi pi-user-edit',
+                                    visible: !this.auth.loginAsUser,
+                                    command: () => this.toggleMyAccount()
+                                },
+                                {
+                                    id: 'dot-toolbar-user-link-login-as',
+                                    label: this.dotMessageService.get('login-as'),
+                                    icon: 'pi pi-sort-alt',
+                                    visible: !this.auth.loginAsUser && loginAs,
+                                    command: () => this.tooggleLoginAs()
+                                },
+                                { separator: true, visible: !this.auth.loginAsUser },
+                                {
+                                    id: 'dot-toolbar-user-link-logout',
+                                    label: this.dotMessageService.get('Logout'),
+                                    icon: 'pi pi-sign-out',
+                                    visible: !this.auth.loginAsUser,
+                                    url: this.logoutUrl
+                                },
+                                {
+                                    id: 'dot-toolbar-user-link-logout-as',
+                                    label: this.dotMessageService.get('logout-as'),
+                                    icon: 'pi pi-sign-out',
+                                    visible: !!this.auth.loginAsUser,
+                                    command: (event) => this.logoutAs(event.originalEvent)
+                                }
+                            ] as MenuItem[]
+                    )
+                )
             )
         );
     }
@@ -142,5 +157,33 @@ export class DotToolbarUserComponent implements OnInit {
         this.showMyAccount = !this.showMyAccount;
 
         return false;
+    }
+
+    private getEmailFromAuthState() {
+        return this.auth.loginAsUser === null
+            ? this.auth.user.emailAddress
+            : this.auth.loginAsUser.emailAddress;
+    }
+
+    private getNameFromAuthState() {
+        return this.auth.loginAsUser
+            ? this.auth.loginAsUser.name || this.auth.loginAsUser.fullName
+            : this.auth.user.name || this.auth.user.fullName;
+    }
+
+    private getTemplateFromPhoto(photo: string | null) {
+        const name = this.getNameFromAuthState();
+
+        const photoTemplate = photo
+            ? `<img src=${photo} class="gravatar" />`
+            : `<div class="gravatar-placeholder">${name.charAt(0)}</div>`;
+
+        const nameTemplate = ` <div>
+        <p class="toolbar-user__user-name" id="dot-toolbar-user-name">
+            ${name}
+        </p>
+    </div>`;
+
+        return photoTemplate + nameTemplate;
     }
 }
