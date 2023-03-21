2b0bbe85ca68d995ba2adb08ade065f8c6d0be39
Jalinson Diaz <zjaaaldev@gmail.com>
2023-03-21 17:59:09 -0300

    delete commented import

--- core-web/apps/dotcms-ui/src/app/view/components/dot-toolbar/components/dot-toolbar-user/dot-toolbar-user.component.spec.ts
+++ core-web/apps/dotcms-ui/src/app/view/components/dot-toolbar/components/dot-toolbar-user/dot-toolbar-user.component.spec.ts
@@ -45,7 +45,6 @@ import { DotPipesModule } from '@pipes/dot-pipes.module';
 
 import { DotToolbarUserComponent } from './dot-toolbar-user.component';
 
-// import { DotGravatarComponent } from '../dot-gravatar/dot-gravatar.component';
 import { DotGravatarModule } from '../dot-gravatar/dot-gravatar.module';
 import { DotLoginAsComponent } from '../dot-login-as/dot-login-as.component';
 import { DotMyAccountComponent } from '../dot-my-account/dot-my-account.component';
