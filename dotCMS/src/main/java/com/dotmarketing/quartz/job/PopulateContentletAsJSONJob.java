0ef9da2089e45d1bc851839fdf6dbc60c0d949a8
Jonathan Gamba <jonathan.gamba@dotcms.com>
2023-04-10 12:26:16 -0600

    #24093 Adding logic to schedule the job to allow multiple executions in case of errors, if everything goes well, the job is removed.

--- dotCMS/src/main/java/com/dotmarketing/quartz/job/PopulateContentletAsJSONJob.java
+++ dotCMS/src/main/java/com/dotmarketing/quartz/job/PopulateContentletAsJSONJob.java
@@ -1,20 +1,18 @@
 package com.dotmarketing.quartz.job;
 
-import com.dotcms.enterprise.HostAssetsJobProxy;
 import com.dotcms.util.content.json.PopulateContentletAsJSONUtil;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotRuntimeException;
 import com.dotmarketing.quartz.DotStatefulJob;
+import com.dotmarketing.quartz.QuartzUtils;
+import com.dotmarketing.util.Config;
 import com.dotmarketing.util.Logger;
-import com.google.common.collect.ImmutableMap;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.quartz.Trigger;
+import io.vavr.Lazy;
+import io.vavr.control.Try;
+import org.quartz.*;
 
 import java.io.IOException;
-import java.io.Serializable;
 import java.sql.SQLException;
-import java.util.Map;
 
 /**
  * Job created to populate in the Contentlet table missing contentlet_as_json columns.
@@ -22,24 +20,34 @@ import java.util.Map;
 public class PopulateContentletAsJSONJob extends DotStatefulJob {
 
     public static final String EXCLUDING_ASSET_SUB_TYPE = "excludingAssetSubType";
+    public static final String CONFIG_PROPERTY_HOURS_INTERVAL = "populateContentletAsJSONJob.hours.interval";
+    private static final Lazy<Integer> HOURS_INTERVAL = Lazy.of(() -> Config.getIntProperty(
+            CONFIG_PROPERTY_HOURS_INTERVAL, 4));
 
     @Override
     public void run(JobExecutionContext jobContext) throws JobExecutionException {
 
-        final Trigger trigger = jobContext.getTrigger();
-        final Map<String, Serializable> executionData = getExecutionData(trigger, PopulateContentletAsJSONJob.class);
+        final var jobDataMap = jobContext.getJobDetail().getJobDataMap();
 
         final String excludingAssetSubType;
-        if (executionData.containsKey(EXCLUDING_ASSET_SUB_TYPE)) {
-            excludingAssetSubType = (String) executionData.get(EXCLUDING_ASSET_SUB_TYPE);
+        if (jobDataMap.containsKey(EXCLUDING_ASSET_SUB_TYPE)) {
+            excludingAssetSubType = (String) jobDataMap.get(EXCLUDING_ASSET_SUB_TYPE);
         } else {
             excludingAssetSubType = null;
         }
 
         try {
+            // Executing the populate contentlet as JSON logic
             new PopulateContentletAsJSONUtil().populateExcludingAssetSubType(excludingAssetSubType);
+
+            // Removing the job if everything went well
+            QuartzUtils.removeJob(getJobName(), getJobGroupName());
         } catch (SQLException | DotDataException | IOException e) {
             Logger.error(this, "Error executing Contentlet as JSON population job", e);
+            throw new DotRuntimeException(e);
+        } catch (SchedulerException e) {
+            Logger.error(this, String.format("Unable to remove [%s] job",
+                    PopulateContentletAsJSONJob.class.getName()), e);
             throw new DotRuntimeException(e);
         }
     }
@@ -49,14 +57,59 @@ public class PopulateContentletAsJSONJob extends DotStatefulJob {
      */
     public static void fireJob(final String excludingAssetSubType) {
 
-        final ImmutableMap<String, Serializable> nextExecutionData = ImmutableMap
-                .of(EXCLUDING_ASSET_SUB_TYPE, excludingAssetSubType);
+        final var jobName = getJobName();
+        final var groupName = getJobGroupName();
+
+        final var scheduler = QuartzUtils.getScheduler();
+
+        // Checking if the job already exists
+        var jobDetail = Try.of(() -> scheduler.getJobDetail(jobName, groupName))
+                .onFailure(e -> {
+                    Logger.error(PopulateContentletAsJSONJob.class,
+                            String.format("Error retrieving job detail [%s, %s]", jobName, groupName), e);
+                }).getOrNull();
+
+        if (jobDetail != null) {
+            Logger.info(PopulateContentletAsJSONJob.class,
+                    String.format("Job [%s, %s] already exists, skipping creation", jobName, groupName));
+            return;
+        }
+
+        // Creating the job
+        final var jobDataMap = new JobDataMap();
+        jobDataMap.put(EXCLUDING_ASSET_SUB_TYPE, excludingAssetSubType);
+
+        jobDetail = new JobDetail(
+                jobName, groupName, PopulateContentletAsJSONJob.class
+        );
+
+        jobDetail.setJobDataMap(jobDataMap);
+        jobDetail.setDurability(false);
+        jobDetail.setVolatility(false);
+        jobDetail.setRequestsRecovery(true);
+
+        // This trigger will fire the job every 4 hours
+        final var trigger = TriggerUtils.makeHourlyTrigger(HOURS_INTERVAL.get());
+        trigger.setName(jobName);
+        trigger.setGroup(groupName);
+        trigger.setJobName(jobName);
+        trigger.setJobGroup(groupName);
+        trigger.setMisfireInstruction(SimpleTrigger.MISFIRE_INSTRUCTION_FIRE_NOW);
+
         try {
-            DotStatefulJob.enqueueTrigger(nextExecutionData, PopulateContentletAsJSONJob.class);
+            scheduler.scheduleJob(jobDetail, trigger);
         } catch (Exception e) {
-            Logger.error(HostAssetsJobProxy.class, "Error scheduling populate content as JSON job", e);
+            Logger.error(PopulateContentletAsJSONJob.class, "Error scheduling populate content as JSON job", e);
             throw new DotRuntimeException("Error scheduling populate content as JSON job", e);
         }
+    }
+
+    private static String getJobName() {
+        return PopulateContentletAsJSONJob.class.getSimpleName();
+    }
+
+    private static String getJobGroupName() {
+        return getJobName() + "_Group";
     }
 
 }
