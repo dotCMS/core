a37b3db02abc1a244c38e664b9b6e2adc24fcb12
Manuel Rojas <manuel.rojas.21@gmail.com>
2023-04-19 16:00:09 -0600

    Adding compare functionality improvements

--- core-web/apps/dotcms-ui/src/app/view/components/dot-content-compare/components/dot-content-compare-editor/dot-content-compare-editor.component.ts
+++ core-web/apps/dotcms-ui/src/app/view/components/dot-content-compare/components/dot-content-compare-editor/dot-content-compare-editor.component.ts
@@ -1,6 +1,7 @@
-import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 import { DotContentCompareTableData } from '@components/dot-content-compare/store/dot-content-compare.store';
+import { DotDiffPipe } from '@dotcms/app/view/pipes';
 import { DotBlockEditorComponent } from '@dotcms/block-editor';
 
 @Component({
@@ -9,25 +10,39 @@ import { DotBlockEditorComponent } from '@dotcms/block-editor';
 })
 export class DotContentCompareEditorComponent implements AfterViewInit {
     @ViewChild('blockEditor') blockEditor: DotBlockEditorComponent;
+    @ViewChild('blockEditorCompare') blockEditorCompare: DotBlockEditorComponent;
+    @ViewChild('HtmlCompare') htmlCompare: ElementRef;
+    @ViewChild('HtmlWorking') htmlWorking: ElementRef;
 
     @Input() data: DotContentCompareTableData;
     @Input() showDiff: boolean;
     @Input() field: string;
     @Input() label: boolean;
 
-    HTMLWorking: string;
-    HTMLCompare: string;
-    ngAfterViewInit(): void {
-        document.addEventListener(
-            'DOMContentLoaded',
-            () => {
-                this.blockEditor?.editor?.commands.setContent(this.data.working[this.field]);
-                this.HTMLWorking = this.blockEditor?.editor?.getHTML();
+    private dotDiffPipe = new DotDiffPipe();
 
-                this.blockEditor?.editor?.commands.setContent(this.data.compare[this.field]);
-                this.HTMLCompare = this.blockEditor?.editor.getHTML();
-            },
-            { once: true }
-        );
+    htmlCompareValue: string;
+    htmlWorkingValue: string;
+
+    ngAfterViewInit(): void {
+        this.blockEditor.value = this.data.working[this.field];
+        this.blockEditor?.valueChange.subscribe(() => {
+            this.htmlWorkingValue = this.blockEditor.editor.getHTML();
+            if (this.htmlWorking) {
+                this.htmlWorking.nativeElement.innerHTML = this.htmlWorkingValue;
+            }
+        });
+
+        this.blockEditorCompare.value = this.data.compare[this.field];
+        this.blockEditorCompare?.valueChange.subscribe(() => {
+            this.htmlWorkingValue = this.blockEditorCompare.editor.getHTML();
+            if (this.htmlCompare) {
+                this.htmlCompare.nativeElement.innerHTML = this.dotDiffPipe.transform(
+                    this.htmlWorkingValue,
+                    this.htmlCompareValue,
+                    true
+                );
+            }
+        });
     }
 }
