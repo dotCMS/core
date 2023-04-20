e0eb11935bfa32f4da57a132d53eefc706705e54
alfredo-dotcms <37185433+alfredo-dotcms@users.noreply.github.com>
2023-04-19 18:07:14 -0600

    Fix #24442 dot favorite page context menu of favorite needs to address source portlet (#24534)
    * #24442 adding first draft of the find available actions for a page
    
    * #24442 adding curl test
    
    * dotCMS/core#24442 DotFavoritePage-context-menu-of-favorite-needs-to-address-source-portlet
    
    * #24442 adding now the page to the actions
    
    * #24442 adding a fix to wrap the entity into the response entity view
    
    * dotCMS/core#24442 DotFavoritePage-context-menu-of-favorite-needs-to-address-source-portlet - test
    
    * dotCMS/core#24442 DotFavoritePage-context-menu-of-favorite-needs-to-address-source-portlet -feedback
    
    * dotCMS/core#24442 DotFavoritePage-context-menu-of-favorite-needs-to-address-source-portlet - Feedback
    
    * #24442 fixing the curl test
    
    * #24442 fixing curl
    
    * dotCMS/core#24442 DotFavoritePage-context-menu-of-favorite-needs-to-address-source-portlet - Feedback
    
    * #24442 fixing curl test
    
    * #24442 fixing curl test
    
    ---------
    
    Co-authored-by: jdotcms <jonathan.sanchez@dotcms.com>

--- core-web/apps/dotcms-ui/src/app/portlets/dot-pages/dot-pages-store/dot-pages.store.spec.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-pages/dot-pages-store/dot-pages.store.spec.ts
@@ -24,6 +24,7 @@ import {
     DotLanguagesService,
     DotLicenseService,
     DotPageTypesService,
+    DotPageWorkflowsActionsService,
     DotRenderMode,
     DotWorkflowActionsFireService,
     DotWorkflowsActionsService,
@@ -48,6 +49,7 @@ import {
 } from '@dotcms/dotcms-models';
 import {
     DotcmsConfigServiceMock,
+    dotcmsContentletMock,
     dotcmsContentTypeBasicMock,
     DotcmsEventsServiceMock,
     DotLanguagesServiceMock,
@@ -97,6 +99,7 @@ describe('DotPageStore', () => {
     let dotESContentService: DotESContentService;
     let dotPageTypesService: DotPageTypesService;
     let dotWorkflowsActionsService: DotWorkflowsActionsService;
+    let dotPageWorkflowsActionsService: DotPageWorkflowsActionsService;
     let dotHttpErrorManagerService: DotHttpErrorManagerService;
 
     beforeEach(() => {
@@ -111,6 +114,7 @@ describe('DotPageStore', () => {
                 DotWizardService,
                 DotWorkflowActionsFireService,
                 DotWorkflowsActionsService,
+                DotPageWorkflowsActionsService,
                 DotWorkflowEventHandlerService,
                 LoggerService,
                 StringUtils,
@@ -136,6 +140,7 @@ describe('DotPageStore', () => {
         dotPageTypesService = TestBed.inject(DotPageTypesService);
         dotHttpErrorManagerService = TestBed.inject(DotHttpErrorManagerService);
         dotWorkflowsActionsService = TestBed.inject(DotWorkflowsActionsService);
+        dotPageWorkflowsActionsService = TestBed.inject(DotPageWorkflowsActionsService);
 
         spyOn(dialogService, 'open').and.callThrough();
         spyOn(dotHttpErrorManagerService, 'handle');
@@ -473,5 +478,21 @@ describe('DotPageStore', () => {
             favoritePagesInitialTestData[0].inode,
             DotRenderMode.LISTING
         );
+    });
+
+    it('should get all Workflow actions and static actions from a favorite page', () => {
+        spyOn(dotPageWorkflowsActionsService, 'getByUrl').and.returnValue(
+            of({ actions: mockWorkflowsActions, page: dotcmsContentletMock })
+        );
+        dotPageStore.showActionsMenu({
+            item: { ...favoritePagesInitialTestData[0], contentType: 'dotFavoritePage' },
+            actionMenuDomId: 'test1'
+        });
+
+        expect(dotPageWorkflowsActionsService.getByUrl).toHaveBeenCalledWith({
+            host_id: 'A',
+            language_id: '1',
+            url: '/index1'
+        });
     });
 });
