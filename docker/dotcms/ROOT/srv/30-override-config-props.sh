4499c6bba5bb69f2f47b7c9077074097f008b838
Jose Castro <jose.castro@dotcms.com>
2023-04-20 12:59:55 -0600

    Updating `sed` command.

--- docker/dotcms/ROOT/srv/30-override-config-props.sh
+++ docker/dotcms/ROOT/srv/30-override-config-props.sh
@@ -3,4 +3,4 @@
 set -e
 
 # If enabled, add the required config for Tomcat Session Management with Redis
-sed -i "s,\${REDIS_SESSION_VALVE},${REDIS_SESSION_VALVE},g $TOMCAT_HOME/conf/context.xml"
+sed -i "s,\${REDIS_SESSION_VALVE},${REDIS_SESSION_VALVE},g" $TOMCAT_HOME/conf/context.xml
