9c0d86986d14870d437a971e0053cbe02a53231f
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2023-04-25 11:03:38 -0600

    #24168 improve interactive help  message

--- tools/dotcms-cli/cli/src/main/java/com/dotcms/cli/common/InteractiveOptionMixin.java
+++ tools/dotcms-cli/cli/src/main/java/com/dotcms/cli/common/InteractiveOptionMixin.java
@@ -4,7 +4,13 @@ import picocli.CommandLine;
 
 public class InteractiveOptionMixin {
 
-    @CommandLine.Option(names = { "-i", "--interactive" }, description = {"When specified User confirmation is enforced."}, defaultValue = "true")
+    @CommandLine.Option(names = { "-i", "--interactive" },
+            paramLabel = "Interactive",
+            description = {
+                "When specified User confirmation is enforced.",
+                "By default this is true.",
+                "If set to false, the command will not ask for confirmation."
+            }, defaultValue = "true")
     boolean interactive = true;
 
     public boolean isInteractive() {
