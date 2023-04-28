f1d30091068fcb99c4153340debab03d59305e63
Rafael <rjvelazco21@gmail.com>
2023-04-28 11:51:31 -0400

    fix: tests

--- core-web/apps/dotcms-ui/src/app/portlets/dot-edit-page/content/services/dot-copy-content-modal/dot-copy-content-modal.service.spec.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-edit-page/content/services/dot-copy-content-modal/dot-copy-content-modal.service.spec.ts
@@ -1,16 +1,84 @@
+import { of } from 'rxjs';
+
 import { TestBed } from '@angular/core/testing';
 
-import { DotCopyContentModalService } from './dot-copy-content-modal.service';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+
+import { DotMessageService } from '@dotcms/data-access';
+import { MockDotMessageService } from '@dotcms/utils-testing';
+
+import { DotCopyContentModalService, OPTIONS } from './dot-copy-content-modal.service';
+
+const messageServiceMock = new MockDotMessageService({
+    'Edit-Content': 'Edit Content'
+});
 
 describe('DotCopyContentModalService', () => {
     let service: DotCopyContentModalService;
+    let dialogService: DialogService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [
+                DotCopyContentModalService,
+                DialogService,
+                {
+                    provide: DotMessageService,
+                    useValue: messageServiceMock
+                }
+            ]
+        });
         service = TestBed.inject(DotCopyContentModalService);
+        dialogService = TestBed.inject(DialogService);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should not return anything if the user close the modal without select an option', (done) => {
+        service.open().subscribe(
+            (res) => fail('This should not be called. Response: ' + res),
+            (err) => fail('This should not be called. Error: ' + err),
+            () => {
+                expect(true).toBe(true);
+                done();
+            }
+        );
+
+        service.dialogRef.close();
+    });
+
+    it('should return true if the user select the first option', (done) => {
+        service.open().subscribe((res) => {
+            expect(res.shouldCopy).toBe(true);
+            done();
+        });
+
+        service.dialogRef.close(OPTIONS.option1.value);
+    });
+
+    it('should return false if the user select the second option', (done) => {
+        service.open().subscribe((res) => {
+            expect(res.shouldCopy).toBe(false);
+            done();
+        });
+
+        service.dialogRef.close(OPTIONS.option2.value);
+    });
+
+    it('shoud have be called one time with the correct data', () => {
+        spyOn(dialogService, 'open').and.returnValue({
+            onClose: of('')
+        } as DynamicDialogRef);
+        service.open().subscribe();
+        expect(dialogService.open).toHaveBeenCalledTimes(1);
+
+        expect(dialogService.open).toHaveBeenCalledWith(jasmine.any(Function), {
+            header: 'Edit Content',
+            width: '37rem',
+            data: { options: OPTIONS },
+            contentStyle: { padding: '0px' }
+        });
     });
 });
--- core-web/apps/dotcms-ui/src/app/portlets/dot-edit-page/content/services/dot-copy-content-modal/dot-copy-content-modal.service.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-edit-page/content/services/dot-copy-content-modal/dot-copy-content-modal.service.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs';
 
 import { Injectable } from '@angular/core';
 
-import { DialogService } from 'primeng/dynamicdialog';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 import { map, take, filter } from 'rxjs/operators';
 
@@ -11,35 +11,40 @@ import {
     BINARY_OPTION
 } from '@dotcms/app/portlets/shared/dot-binary-option-selector/dot-binary-option-selector.component';
 import { DotMessageService } from '@dotcms/data-access';
-import { DotLoadingIndicatorService } from '@dotcms/utils';
 
 export interface ModelCopyContentResponse {
     shouldCopy: boolean;
 }
 
+export const OPTIONS = {
+    option1: {
+        value: 'Copy',
+        message: 'editpage.content.edit.content.in.this.page.message',
+        icon: 'article',
+        label: 'editpage.content.edit.content.in.this.page',
+        buttonLabel: 'editpage.content.edit.content.in.this.page.button.label'
+    },
+    option2: {
+        value: 'NotCopy',
+        message: 'editpage.content.edit.content.in.all.pages.message',
+        icon: 'dynamic_feed',
+        label: 'editpage.content.edit.content.in.all.pages',
+        buttonLabel: 'editpage.content.edit.content.in.all.pages.button.label'
+    }
+};
+
 @Injectable()
 export class DotCopyContentModalService {
-    private readonly CONTENT_EDIT_OPTIONS: BINARY_OPTION = {
-        option1: {
-            value: 'Copy',
-            message: 'editpage.content.edit.content.in.this.page.message',
-            icon: 'article',
-            label: 'editpage.content.edit.content.in.this.page',
-            buttonLabel: 'editpage.content.edit.content.in.this.page.button.label'
-        },
-        option2: {
-            value: 'NotCopy',
-            message: 'editpage.content.edit.content.in.all.pages.message',
-            icon: 'dynamic_feed',
-            label: 'editpage.content.edit.content.in.all.pages',
-            buttonLabel: 'editpage.content.edit.content.in.all.pages.button.label'
-        }
-    };
+    private readonly CONTENT_EDIT_OPTIONS: BINARY_OPTION = OPTIONS;
+    private _dialogRef: DynamicDialogRef;
+
+    get dialogRef() {
+        return this._dialogRef;
+    }
 
     constructor(
         private dialogService: DialogService,
-        private dotMessageService: DotMessageService,
-        public dotLoadingIndicatorService: DotLoadingIndicatorService
+        private dotMessageService: DotMessageService
     ) {}
 
     /**
@@ -49,14 +54,14 @@ export class DotCopyContentModalService {
      * @memberof DotEditContentComponent
      */
     open(): Observable<ModelCopyContentResponse> {
-        const ref = this.dialogService.open(DotBinaryOptionSelectorComponent, {
+        this._dialogRef = this.dialogService.open(DotBinaryOptionSelectorComponent, {
             header: this.dotMessageService.get('Edit-Content'),
             width: '37rem',
             data: { options: this.CONTENT_EDIT_OPTIONS },
             contentStyle: { padding: '0px' }
         });
 
-        return ref.onClose.pipe(
+        return this._dialogRef.onClose.pipe(
             take(1),
             // If the user close the modal without select an option, we return false
             // This will complete the observable
