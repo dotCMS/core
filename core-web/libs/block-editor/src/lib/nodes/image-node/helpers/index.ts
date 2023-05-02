9086266058472968219460eda5fdaebf6e0ed545
Rafael Velazco <rjvelazco21@gmail.com>
2023-05-02 10:12:07 -0400

    Fix #24789  Block editor: Import image is not updating the reference (#24789)
    * #24760: removing enterprise building, jar generation/upload
    
    * dev: update image reference after importing it #24629
    
    * dev: use fileAsset as a source
    
    * feedback: remove loading on error
    
    * update dotcms-block-editor.js file
    
    ---------
    
    Co-authored-by: Victor Alfaro <victor.alfaro@dotcms.com>

--- core-web/libs/block-editor/src/lib/nodes/image-node/helpers/index.ts
+++ core-web/libs/block-editor/src/lib/nodes/image-node/helpers/index.ts
@@ -1,17 +1,20 @@
+import { DOMOutputSpec } from 'prosemirror-model';
+
 import { mergeAttributes } from '@tiptap/core';
 
 import { DotCMSContentlet } from '@dotcms/dotcms-models';
 
 const LANGUAGE_ID = 'language_id';
 
-export const imageLinkElement = (attrs, newAttrs) => {
+export const imageLinkElement = (attrs, newAttrs): DOMOutputSpec => {
+
     const { href = null } = newAttrs;
 
     return ['a', { href }, imageElement(attrs, newAttrs)];
 };
 
-export const imageElement = (attrs, newAttrs) => {
-    return ['div', { style: newAttrs.style }, ['img', mergeAttributes(attrs, newAttrs)]];
+export const imageElement = (attrs, newAttrs): DOMOutputSpec => {
+    return ['img', mergeAttributes(attrs, newAttrs)];
 };
 
 export const addImageLanguageId = (src: string, languageId: number) =>
