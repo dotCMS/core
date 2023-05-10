f541f7f023d3871d18c21e97f3aac4f2925070af
Rafael Velazco <rjvelazco21@gmail.com>
2023-05-10 12:50:41 -0400

    Fix #24829 dev: add feature flag to enable/disable the new template builder using directive approach #24819 (#24897)

--- core-web/apps/dotcms-ui/src/app/portlets/dot-edit-page/layout/dot-edit-layout.module.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-edit-page/layout/dot-edit-layout.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DotEditLayoutDesignerModule } from '@components/dot-edit-layout-designer/dot-edit-layout-designer.module';
+import { DotShowHideFeatureDirective } from '@dotcms/app/shared/directives/dot-show-hide-feature/dot-show-hide-feature.directive';
 
 import { DotEditLayoutComponent } from './dot-edit-layout/dot-edit-layout.component';
 
@@ -15,7 +16,12 @@ const routes: Routes = [
 
 @NgModule({
     declarations: [DotEditLayoutComponent],
-    imports: [CommonModule, RouterModule.forChild(routes), DotEditLayoutDesignerModule],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        DotEditLayoutDesignerModule,
+        DotShowHideFeatureDirective
+    ],
     exports: [DotEditLayoutComponent]
 })
 export class DotEditLayoutModule {}
