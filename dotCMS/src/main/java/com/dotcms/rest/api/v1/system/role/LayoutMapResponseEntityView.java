6face1231356d950f32a31c8278908be3b2e2bd7
jdotcms <jonathan.sanchez@dotcms.com>
2023-05-15 11:35:59 -0600

    #24801 adding just a comment

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/system/role/LayoutMapResponseEntityView.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/system/role/LayoutMapResponseEntityView.java
@@ -5,6 +5,10 @@ import com.dotcms.rest.ResponseEntityView;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * LayoutMapResponseEntityView
+ * @author jsanca
+ */
 public class LayoutMapResponseEntityView extends ResponseEntityView<List<Map<String, Object>>> {
 
     public LayoutMapResponseEntityView(final List<Map<String, Object>> entity) {
