4caaea553f23586ff8c1ce96b7ba917a85ea8c49
Jalinson Diaz <zjaaaldev@gmail.com>
2023-05-17 13:32:11 -0300

    Fix #24820: Add POC to Template Builder Nx Library (#24957)
    * install GridStack
    
    * add POC of Template Builder
    
    * cleaning
    
    * add gridstack styles
    
    * Import template builder module correctly
    
    * Remove the feature flag variable from the code
    
    ---------
    
    Co-authored-by: Freddy Montes <751424+fmontes@users.noreply.github.com>

--- core-web/apps/dotcms-ui/src/app/portlets/dot-templates/dot-templates.module.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-templates/dot-templates.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { DotTemplatesService } from '@dotcms/app/api/services/dot-templates/dot-templates.service';
 import { LayoutEditorCanDeactivateGuardService } from '@dotcms/app/api/services/guards/layout-editor-can-deactivate-guard.service';
-import { TemplateBuilderModule } from '@dotcms/template-builder';
 import { DotTemplateListModule } from '@portlets/dot-templates/dot-template-list/dot-template-list.module';
 
 import { DotTemplateCreateEditResolver } from './dot-template-create-edit/resolvers/dot-template-create-edit.resolver';
@@ -11,12 +10,7 @@ import { DotTemplatesRoutingModule } from './dot-templates-routing.module';
 
 @NgModule({
     declarations: [],
-    imports: [
-        CommonModule,
-        DotTemplatesRoutingModule,
-        DotTemplateListModule,
-        TemplateBuilderModule
-    ],
+    imports: [CommonModule, DotTemplatesRoutingModule, DotTemplateListModule],
     providers: [
         DotTemplatesService,
         DotTemplateCreateEditResolver,
