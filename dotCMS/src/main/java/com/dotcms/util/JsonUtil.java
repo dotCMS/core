7d5c8bd509e982041376062513fcb8ed5a742466
Jose Castro <jose.castro@dotcms.com>
2023-05-17 15:36:30 -0600

    Missing Javadoc.

--- dotCMS/src/main/java/com/dotcms/util/JsonUtil.java
+++ dotCMS/src/main/java/com/dotcms/util/JsonUtil.java
@@ -30,12 +30,15 @@ public class JsonUtil {
     }
 
     /**
+     * Takes a JSON value in the form of a String, and maps its attributes into the specified Java Class.
      *
-     * @param json
-     * @param clazz
-     * @return
-     * @param <T>
-     * @throws IOException
+     * @param json  The original JSON as String.
+     * @param clazz The Class that the JSON will be transformed into.
+     * @param <T>   The type of the resulting object
+     *
+     * @return The resulting object with the JSON data
+     *
+     * @throws IOException An error occurred when transforming the JSON String.
      */
     public static <T> T getObjectFromJson(final String json, Class<T> clazz) throws IOException {
         return JSON_MAPPER.readValue(json, clazz);
