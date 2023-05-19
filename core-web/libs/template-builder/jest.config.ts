a43df972594e44e088b1dccaa5af7026f445e187
Jalinson Diaz <zjaaaldev@gmail.com>
2023-05-19 14:57:28 -0300

    added library with jest runner

---
+++ core-web/libs/template-builder/jest.config.ts
@@ -0,0 +1,22 @@
+/* eslint-disable */
+export default {
+    displayName: 'template-builder',
+    preset: '../../jest.preset.js',
+    setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.spec.json',
+            stringifyContentPathRegex: '\\.(html|svg)$'
+        }
+    },
+    coverageDirectory: '../../coverage/libs/template-builder',
+    transform: {
+        '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular'
+    },
+    transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+    snapshotSerializers: [
+        'jest-preset-angular/build/serializers/no-ng-attributes',
+        'jest-preset-angular/build/serializers/ng-snapshot',
+        'jest-preset-angular/build/serializers/html-comment'
+    ]
+};
