d483241cef415631419a7b37fad83dda97a3dae2
Fabrizzio Araya <37148755+fabrizzio-dotCMS@users.noreply.github.com>
2023-05-24 09:28:36 -0600

    Issue 24730 assets rest (#24808)
    * #24730 initial commit
    
    * #24730
    
    * #24730 wrapping response within n Entity View
    
    * #24730
    
    * #24730 fix bug resolvign folder asset path
    
    * #24730 reverting changes in favor of BrowserAPI
    
    * #24730 adding test and doc
    
    * #24730 imporving test
    
    * #24730
    
    * #24730 fix create folder script

--- dotCMS/src/integration-test/java/com/dotcms/MainSuite.java
+++ dotCMS/src/integration-test/java/com/dotcms/MainSuite.java
@@ -84,6 +84,7 @@ import com.dotcms.rest.BundleResourceTest;
 import com.dotcms.rest.IntegrityResourceIntegrationTest;
 import com.dotcms.rest.api.v1.apps.AppsResourceTest;
 import com.dotcms.rest.api.v1.apps.view.AppsInterpolationTest;
+import com.dotcms.rest.api.v1.assets.AssetPathResolverImplTest;
 import com.dotcms.rest.api.v1.authentication.ResetPasswordTokenUtilTest;
 import com.dotcms.rest.api.v1.folder.FolderResourceTest;
 import com.dotcms.rest.api.v1.menu.MenuResourceTest;
@@ -634,9 +635,10 @@ import org.junit.runners.Suite.SuiteClasses;
         ContentTypeDestroyAPIImplTest.class,
         Task230328AddMarkedForDeletionColumnTest.class,
         StartupTasksExecutorDataTest.class,
-        Task230426AlterVarcharLengthOfLockedByColTest.class
+        Task230426AlterVarcharLengthOfLockedByColTest.class,
 //        AnalyticsAPIImplTest.class,
 //        AccessTokenRenewJobTest.class,
+        AssetPathResolverImplTest.class
 })
 
 public class MainSuite {
