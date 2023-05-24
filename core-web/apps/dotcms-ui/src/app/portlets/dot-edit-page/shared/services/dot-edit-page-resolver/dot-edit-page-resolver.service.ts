54ec6135ead19ccba0a14ed5f56d9f36f8c4117d
Rafael Velazco <rjvelazco21@gmail.com>
2023-05-24 12:07:13 -0400

    Favorite Pages: Allow User Cross-Site Page Editing (#25005)
    * dev: allow user cross site page editing #24992
    
    * dev: set site in edit page route resolver
    
    * dev: handler error
    
    * fix: test
    
    * feedback v1
    
    * feedback v2
    
    * fix: docs
    
    * fix: docs v2

--- core-web/apps/dotcms-ui/src/app/portlets/dot-edit-page/shared/services/dot-edit-page-resolver/dot-edit-page-resolver.service.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-edit-page/shared/services/dot-edit-page-resolver/dot-edit-page-resolver.service.ts
@@ -1,14 +1,14 @@
-import { Observable, of, throwError } from 'rxjs';
+import { Observable, forkJoin, of, throwError } from 'rxjs';
 
 import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 
-import { catchError, filter, map, switchMap, tap } from 'rxjs/operators';
+import { catchError, filter, map, switchMap } from 'rxjs/operators';
 
 import { DotHttpErrorManagerService } from '@dotcms/app/api/services/dot-http-error-manager/dot-http-error-manager.service';
 import { DotRouterService } from '@dotcms/app/api/services/dot-router/dot-router.service';
-import { DotCMSResponse, HttpCode } from '@dotcms/dotcms-js';
+import { DotCMSResponse, HttpCode, Site, SiteService } from '@dotcms/dotcms-js';
 import { DotPageRenderOptions, DotPageRenderState } from '@dotcms/dotcms-models';
 
 import { DotPageStateService } from '../../../content/services/dot-page-state/dot-page-state.service';
@@ -25,37 +25,21 @@ export class DotEditPageResolver implements Resolve<DotPageRenderState> {
     constructor(
         private dotPageStateService: DotPageStateService,
         private dotRouterService: DotRouterService,
-        private dotHttpErrorManagerService: DotHttpErrorManagerService
+        private dotHttpErrorManagerService: DotHttpErrorManagerService,
+        private siteService: SiteService
     ) {}
 
     resolve(route: ActivatedRouteSnapshot): Observable<DotPageRenderState> {
         const data = this.dotPageStateService.getInternalNavigationState();
+        const renderOptions = this.getDotPageRenderOptions(route);
+        const currentSection = route.children[0].url[0].path;
+        const isLayout = currentSection === 'layout';
+        const hostId = route.queryParams?.host_id;
 
-        if (data) {
-            return of(data);
-        } else {
-            return this.dotPageStateService.requestPage(this.getDotPageRenderOptions(route)).pipe(
-                tap((state: DotPageRenderState) => {
-                    if (!state) {
-                        this.dotRouterService.goToSiteBrowser();
-                    }
-                }),
-                filter((state: DotPageRenderState) => !!state),
-                switchMap((dotRenderedPageState: DotPageRenderState) => {
-                    const currentSection = route.children[0].url[0].path;
-                    const isLayout = currentSection === 'layout';
+        // If we have data, we don't need to request the page again
+        const data$ = data ? of(data) : this.getPageRenderState(renderOptions, isLayout);
 
-                    return isLayout
-                        ? this.checkUserCanGoToLayout(dotRenderedPageState)
-                        : of(dotRenderedPageState);
-                }),
-                catchError((err: HttpErrorResponse) => {
-                    this.dotRouterService.goToSiteBrowser();
-
-                    return this.dotHttpErrorManagerService.handle(err).pipe(map(() => null));
-                })
-            );
-        }
+        return forkJoin([this.setSite(hostId), data$]).pipe(map(([_, pageRender]) => pageRender));
     }
 
     private checkUserCanGoToLayout(
@@ -103,5 +87,42 @@ export class DotEditPageResolver implements Resolve<DotPageRenderState> {
         }
 
         return renderOptions;
+    }
+
+    private setSite(id: string): Observable<Site> {
+        const currentSiteId = this.siteService.currentSite?.identifier;
+        const shouldSwitchSite = id && id !== currentSiteId;
+
+        // If we have a site id and is different from the current one, we switch
+        return shouldSwitchSite
+            ? this.siteService.switchSiteById(id).pipe(
+                  catchError((err: HttpErrorResponse) => {
+                      return this.dotHttpErrorManagerService.handle(err).pipe(map(() => null));
+                  })
+              )
+            : of(null);
+    }
+
+    private getPageRenderState(
+        renderOptions: DotPageRenderOptions,
+        isLayout: boolean
+    ): Observable<DotPageRenderState> {
+        return this.dotPageStateService.requestPage(renderOptions).pipe(
+            filter((state: DotPageRenderState) => {
+                if (!state) this.dotRouterService.goToSiteBrowser();
+
+                return !!state;
+            }),
+            switchMap((dotRenderedPageState: DotPageRenderState) => {
+                return isLayout
+                    ? this.checkUserCanGoToLayout(dotRenderedPageState)
+                    : of(dotRenderedPageState);
+            }),
+            catchError((err: HttpErrorResponse) => {
+                this.dotRouterService.goToSiteBrowser();
+
+                return this.dotHttpErrorManagerService.handle(err).pipe(map(() => null));
+            })
+        );
     }
 }
