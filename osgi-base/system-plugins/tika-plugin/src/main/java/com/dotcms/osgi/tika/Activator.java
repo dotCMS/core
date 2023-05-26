9043dbd983c6692714dbe2a38884d24b14e859cb
spbolton <steve.bolton@dotcms.com>
2023-05-26 14:26:52 -0400

    update bundles and make tike plugin not dependent on core

--- osgi-base/system-plugins/tika-plugin/src/main/java/com/dotcms/osgi/tika/Activator.java
+++ osgi-base/system-plugins/tika-plugin/src/main/java/com/dotcms/osgi/tika/Activator.java
@@ -1,14 +1,14 @@
 package com.dotcms.osgi.tika;
 
 import com.dotcms.tika.TikaServiceBuilder;
-import com.dotmarketing.osgi.GenericBundleActivator;
 import java.util.Hashtable;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Activator extends GenericBundleActivator {
+public class Activator implements BundleActivator {
     private static Logger LOG = LoggerFactory.getLogger(Activator.class);
 
     private ServiceRegistration dotTikaService;
