ec63e8123a226c82d23743a2120e04f8038e3587
Jalinson Diaz <zjaaaldev@gmail.com>
2023-05-30 14:38:40 -0300

    delete duplicated tests

--- core-web/apps/dotcms-ui/src/app/portlets/dot-pages/dot-pages-listing-panel/dot-pages-listing-panel.component.spec.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-pages/dot-pages-listing-panel/dot-pages-listing-panel.component.spec.ts
@@ -270,23 +270,5 @@ describe('DotPagesListingPanelComponent', () => {
             switchSiteSubject.next(mockSites[1]); // switching the site
             expect(store.getPages).toHaveBeenCalledWith({ offset: 0 });
         });
-
-        it('should reload portlet only when the site change', () => {
-            switchSiteSubject.next(mockSites[0]); // setting the site
-            switchSiteSubject.next(mockSites[1]); // switching the site
-            expect(store.getPages).toHaveBeenCalledWith({ offset: 0 });
-        });
-
-        it('should reload portlet only when the site change', () => {
-            switchSiteSubject.next(mockSites[0]); // setting the site
-            switchSiteSubject.next(mockSites[1]); // switching the site
-            expect(store.getPages).toHaveBeenCalledWith({ offset: 0 });
-        });
-
-        it('should reload portlet only when the site change', () => {
-            switchSiteSubject.next(mockSites[0]); // setting the site
-            switchSiteSubject.next(mockSites[1]); // switching the site
-            expect(store.getPages).toHaveBeenCalledWith({ offset: 0 });
-        });
     });
 });
