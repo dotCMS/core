18b383d55f5de7a041e6a2f5ce1d555f4e02a476
Will Ezell <will@dotcms.com>
2023-05-31 12:11:17 -0400

    fixing unit tests #24977

--- dotCMS/src/main/java/com/dotcms/test/TestUtil.java
+++ dotCMS/src/main/java/com/dotcms/test/TestUtil.java
@@ -2,28 +2,28 @@ package com.dotcms.test;
 
 public class TestUtil {
 
-    
-    
-    public static final String DOTCMS_INTEGRATION_TEST= "DOTCMS_INTEGRATION_TEST";
+
+
+    public static final String DOTCMS_INTEGRATION_TEST = "DOTCMS_INTEGRATION_TEST";
 
     public static boolean isUnitTest() {
-        if(isIntegrationTest()) {
+        if (isIntegrationTest()) {
             return false;
         }
-        
+
 
         for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
-          if (element.getClassName().startsWith("org.junit.")) {
-            return true;
-          }           
+            if (element.getClassName().startsWith("org.junit.")) {
+                return true;
+            }
         }
         return false;
-          
+
     }
 
     public static boolean isIntegrationTest() {
-        return (System.getProperty("DOTCMS_INTEGRATION_TEST")!=null);
+        return System.getProperty("DOTCMS_INTEGRATION_TEST") != null;
     }
-    
-    
+
+
 }
