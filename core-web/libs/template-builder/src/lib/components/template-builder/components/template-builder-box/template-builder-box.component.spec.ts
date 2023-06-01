e166feadc21841081fe1ffc0a9caaf6eeaff7467
Jalinson Diaz <zjaaaldev@gmail.com>
2023-06-01 14:02:59 -0300

    add tests for events

--- core-web/libs/template-builder/src/lib/components/template-builder/components/template-builder-box/template-builder-box.component.spec.ts
+++ core-web/libs/template-builder/src/lib/components/template-builder/components/template-builder-box/template-builder-box.component.spec.ts
@@ -1,6 +1,7 @@
 import { byTestId, createHostFactory, SpectatorHost } from '@ngneat/spectator/jest';
 
 import { NgClass, NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 import { ButtonModule } from 'primeng/button';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
@@ -10,20 +11,51 @@ import {
     TemplateBuilderBoxSize
 } from './template-builder-box.component';
 
+import { DotContainer } from '../../models/models';
+import { CONTAINERS_DATA_MOCK } from '../../utils/mocks';
+
+@Component({
+    // eslint-disable-next-line @angular-eslint/component-selector
+    selector: 'test-host-component'
+})
+class HostComponent {
+    @Input()
+    size: TemplateBuilderBoxSize;
+    @Input()
+    items: DotContainer[];
+
+    editStyle(): void {
+        /* */
+    }
+
+    addContainer(): void {
+        /* */
+    }
+
+    deleteContainer(): void {
+        /* */
+    }
+
+    deleteColumn(): void {
+        /* */
+    }
+}
 describe('TemplateBuilderBoxComponent', () => {
-    let spectator: SpectatorHost<TemplateBuilderBoxComponent>;
+    let spectator: SpectatorHost<TemplateBuilderBoxComponent, HostComponent>;
 
     const createHost = createHostFactory({
         component: TemplateBuilderBoxComponent,
-        imports: [NgClass, NgIf, ButtonModule, ScrollPanelModule]
+        imports: [NgClass, NgIf, ButtonModule, ScrollPanelModule],
+        host: HostComponent
     });
 
     beforeEach(() => {
         spectator = createHost(
-            `<dotcms-template-builder-box [size]="size"> </dotcms-template-builder-box>`,
+            `<dotcms-template-builder-box [size]="size" [items]="items" (editStyle)="editStyle()" (addContainer)="addContainer()" (deleteContainer)="deleteContainer()" (deleteColumn)="deleteColumn()"> </dotcms-template-builder-box>`,
             {
                 hostProps: {
-                    size: 'large'
+                    size: TemplateBuilderBoxSize.large,
+                    items: CONTAINERS_DATA_MOCK
                 }
             }
         );
@@ -74,5 +106,37 @@ describe('TemplateBuilderBoxComponent', () => {
         expect(addButton).toBeTruthy();
         expect(paletteButton).toBeTruthy();
         expect(deleteButton).toBeTruthy();
+    });
+
+    it('should trigger addContainer when click on plus button', () => {
+        jest.spyOn(spectator.hostComponent, 'addContainer');
+        const addButton = spectator.query(byTestId('btn-plus'));
+
+        addButton.dispatchEvent(new Event('onClick'));
+        expect(spectator.hostComponent.addContainer).toHaveBeenCalled();
+    });
+
+    it('should trigger editStyle when click on palette button', () => {
+        jest.spyOn(spectator.hostComponent, 'editStyle');
+        const paletteButton = spectator.query(byTestId('btn-palette'));
+
+        paletteButton.dispatchEvent(new Event('onClick'));
+        expect(spectator.hostComponent.editStyle).toHaveBeenCalled();
+    });
+
+    it('should trigger deleteContainer when click on container trash button', () => {
+        jest.spyOn(spectator.hostComponent, 'deleteContainer');
+        const containerTrashButton = spectator.query(byTestId('btn-trash-container'));
+
+        containerTrashButton.dispatchEvent(new Event('onClick'));
+        expect(spectator.hostComponent.deleteContainer).toHaveBeenCalled();
+    });
+
+    it('should trigger deleteColumn when click on column trash button', () => {
+        jest.spyOn(spectator.hostComponent, 'deleteColumn');
+        const columnTrashButton = spectator.query(byTestId('btn-trash-column'));
+
+        columnTrashButton.dispatchEvent(new Event('onClick'));
+        expect(spectator.hostComponent.deleteColumn).toHaveBeenCalled();
     });
 });
