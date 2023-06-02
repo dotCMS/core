9844f4d961fa4ada999035e08f3904ee50faa659
Jalinson Diaz <zjaaaldev@gmail.com>
2023-06-02 16:20:29 -0300

    add basic test cases for template builder

--- core-web/libs/template-builder/src/lib/components/template-builder/template-builder.component.spec.ts
+++ core-web/libs/template-builder/src/lib/components/template-builder/template-builder.component.spec.ts
@@ -1,4 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SpectatorHost, byTestId, createHostFactory } from '@ngneat/spectator';
+
+import { AsyncPipe, NgFor } from '@angular/common';
 
 import { AddWidgetComponent } from './components/add-widget/add-widget.component';
 import { TemplateBuilderBoxComponent } from './components/template-builder-box/template-builder-box.component';
@@ -8,32 +10,58 @@ import { TemplateBuilderComponent } from './template-builder.component';
 import { FULL_DATA_MOCK } from './utils/mocks';
 
 describe('TemplateBuilderComponent', () => {
-    let component: TemplateBuilderComponent;
-    let fixture: ComponentFixture<TemplateBuilderComponent>;
+    let spectator: SpectatorHost<TemplateBuilderComponent>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [TemplateBuilderComponent],
-            providers: [DotTemplateBuilderStore],
-            imports: [AddWidgetComponent, TemplateBuilderRowComponent, TemplateBuilderBoxComponent]
-        }).compileComponents();
-
-        fixture = TestBed.createComponent(TemplateBuilderComponent);
-        component = fixture.componentInstance;
-
-        component.templateLayout = {
-            body: FULL_DATA_MOCK,
-            footer: false,
-            header: false,
-            sidebar: {},
-            title: '',
-            width: ''
-        };
-
-        fixture.detectChanges();
+    const createHost = createHostFactory({
+        component: TemplateBuilderComponent,
+        imports: [
+            NgFor,
+            AsyncPipe,
+            TemplateBuilderRowComponent,
+            AddWidgetComponent,
+            TemplateBuilderBoxComponent
+        ],
+        providers: [DotTemplateBuilderStore]
+    });
+    beforeEach(() => {
+        spectator = createHost(
+            `<dotcms-template-builder [templateLayout]="templateLayout"></dotcms-template-builder>`,
+            {
+                hostProps: {
+                    templateLayout: { body: FULL_DATA_MOCK }
+                }
+            }
+        );
     });
 
     it('should create', () => {
-        expect(component).toBeTruthy();
+        expect(spectator).toBeTruthy();
+    });
+
+    it('should have a Add Row Button', () => {
+        expect(spectator.query(byTestId('add-row'))).toBeTruthy();
+    });
+    it('should have a Add Box Button', () => {
+        expect(spectator.query(byTestId('add-box'))).toBeTruthy();
+    });
+    it('should have the same quantity of rows as mocked data', () => {
+        expect(spectator.queryAll(byTestId('row')).length).toBe(FULL_DATA_MOCK.rows.length);
+    });
+    it('should have the same quantity of boxes as mocked data', () => {
+        const totalBoxes = FULL_DATA_MOCK.rows.reduce((acc, row) => {
+            return acc + row.columns.length;
+        }, 0);
+
+        expect(spectator.queryAll(byTestId('box')).length).toBe(totalBoxes);
+    });
+
+    it('should trigger removeColumn when clicking on delete column', () => {
+        const deleteColumnButton = spectator.query(byTestId('btn-trash-column'));
+
+        const mockRemoveColumn = jest.spyOn(spectator.component, 'removeColumn');
+
+        spectator.dispatchFakeEvent(deleteColumnButton, 'onClick');
+
+        expect(mockRemoveColumn).toHaveBeenCalled();
     });
 });
