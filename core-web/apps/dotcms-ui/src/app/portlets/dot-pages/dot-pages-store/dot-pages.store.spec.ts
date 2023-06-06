5f3e0ba7c64bac62347e6d10e6b648ccf672a324
Jalinson Diaz <zjaaaldev@gmail.com>
2023-06-06 15:07:51 -0300

    Fix #25143: Favorite Pages fix broken LocalStorage collapsed state  (#25158)
    * fix localstorage functionality
    
    * Update dot-pages.store.ts
    
    * add test
    
    * add fallback in case localStorage doesn't exists
    
    * delete focus
    
    * small cleaning
    
    * add test to know that it retrieves collapse from locasStorage
    
    * fix broken test
    
    * delete unnecessary min-height

--- core-web/apps/dotcms-ui/src/app/portlets/dot-pages/dot-pages-store/dot-pages.store.spec.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-pages/dot-pages-store/dot-pages.store.spec.ts
@@ -331,6 +331,19 @@ describe('DotPageStore', () => {
         );
     });
 
+    it('should have favorites collapsed state setted when requesting favorite pages', () => {
+        dotPageStore.getFavoritePages(5); // Here it sets the favorite state again
+
+        // Should retrieve the value from local storage when setting the state
+        expect(dotLocalstorageService.getItem).toHaveBeenCalledWith(
+            LOCAL_STORAGE_FAVORITES_PANEL_KEY
+        );
+
+        dotPageStore.state$.subscribe((data) => {
+            expect(data.favoritePages.collapsed).toEqual(true);
+        });
+    });
+
     it('should update Pages Status', () => {
         dotPageStore.setPagesStatus(ComponentStatus.LOADING);
         dotPageStore.state$.subscribe((data) => {
@@ -391,7 +404,7 @@ describe('DotPageStore', () => {
             expect(data.favoritePages.items).toEqual(expectedInputArray);
             expect(data.favoritePages.showLoadMoreButton).toEqual(true);
             expect(data.favoritePages.total).toEqual(expectedInputArray.length);
-            expect(data.favoritePages.collapsed).toEqual(undefined);
+            expect(data.favoritePages.collapsed).toEqual(true);
         });
         expect(dotFavoritePageService.get).toHaveBeenCalledTimes(1);
     });
