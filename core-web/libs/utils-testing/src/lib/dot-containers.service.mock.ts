16abfd6dfef5aec435952abaa2a37c397f3868b3
danielmdob <danmdob@gmail.com>
2023-06-09 16:41:21 -0600

    default options improvement

--- core-web/libs/utils-testing/src/lib/dot-containers.service.mock.ts
+++ core-web/libs/utils-testing/src/lib/dot-containers.service.mock.ts
@@ -7,22 +7,6 @@ const MOCK_CONTAINERS: DotContainerEntity[] = Array.from(Array(10).keys()).map((
     contentTypes: []
 }));
 
-/*[
-    {
-        container: { friendlyName: 'Container 1', parentPermissionable: { hostname: '' } },
-        contentTypes: []
-    },
-    {
-        container: { friendlyName: 'Container 2', parentPermissionable: { hostname: '' } },
-        contentTypes: []
-    },
-    {
-        container: { friendlyName: 'Container 3', parentPermissionable: { hostname: '' } },
-        contentTypes: []
-    }
-];
-*/
-
 export class DotContainersServiceMock {
     getFiltered(_: string): Observable<DotContainerEntity[] | null> {
         return of(MOCK_CONTAINERS);
