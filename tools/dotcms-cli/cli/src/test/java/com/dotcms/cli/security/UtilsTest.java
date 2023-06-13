1431dd27967cb960609763d5fdcf70678eaa72ff
Jonathan Gamba <jonathan.gamba@dotcms.com>
2023-06-13 16:07:16 -0600

    #24737 Working on tests

--- tools/dotcms-cli/cli/src/test/java/com/dotcms/cli/security/UtilsTest.java
+++ tools/dotcms-cli/cli/src/test/java/com/dotcms/cli/security/UtilsTest.java
@@ -7,14 +7,17 @@ import org.junit.jupiter.api.Test;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class UtilsTest {
 
     @Test
     public void testSha256toUnixHash_withValidFile_returnsCorrectHash() throws Exception {
 
+        Path path = null;
+
         try {
-            Path path = Path.of("test.txt");
+            path = Files.createTempFile("test", ".txt");
             Files.write(path, "Hello, world!".getBytes());
 
             String expectedHash = "315f5bdb76d078c43b8ac0064e4a0164612b1fce77c869345bfc94c75894edd3";
@@ -22,30 +25,39 @@ public class UtilsTest {
 
             Assertions.assertEquals(expectedHash, actualHash);
         } finally {
-            Files.deleteIfExists(Path.of("test.txt"));
+            if (path != null) {
+                Files.deleteIfExists(path);
+            }
         }
     }
 
     @Test
     public void testSha256toUnixHash_withValidFile_afterRename_returnsSameHash() throws Exception {
 
+        Path originalPath = null;
+        Path renamePath = null;
+
         try {
-            Path originalPath = Path.of("test.txt");
+            originalPath = Files.createTempFile("test", ".txt");
             Files.write(originalPath, "Hello, world!".getBytes());
 
             var originalHash = Utils.Sha256toUnixHash(originalPath);
             Assertions.assertNotNull(originalHash);
 
-            Path renamePath = Path.of("new-test.txt");
-            Files.move(originalPath, renamePath);
+            renamePath = Files.createTempFile("new-test", ".txt");
+            Files.move(originalPath, renamePath, StandardCopyOption.REPLACE_EXISTING);
 
             var newHash = Utils.Sha256toUnixHash(renamePath);
             Assertions.assertNotNull(newHash);
 
             Assertions.assertEquals(originalHash, newHash);
         } finally {
-            Files.deleteIfExists(Path.of("test.txt"));
-            Files.deleteIfExists(Path.of("new-test.txt"));
+            if (originalPath != null) {
+                Files.deleteIfExists(originalPath);
+            }
+            if (renamePath != null) {
+                Files.deleteIfExists(renamePath);
+            }
         }
     }
 
