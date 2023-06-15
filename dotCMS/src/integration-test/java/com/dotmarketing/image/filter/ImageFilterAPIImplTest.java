ebd259d8f6b1ae27bb42f99d8dc8ea894819dd92
Will Ezell <will@dotcms.com>
2023-06-15 14:32:41 -0400

    #25247 fixes svg width,height

--- dotCMS/src/integration-test/java/com/dotmarketing/image/filter/ImageFilterAPIImplTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/image/filter/ImageFilterAPIImplTest.java
@@ -76,7 +76,22 @@ public class ImageFilterAPIImplTest {
         assert (dim.getWidth() == 1024d);
 
     }
-    
+
+
+
+    @Test
+    public void test_svg_dimensions() {
+
+        URL url = getClass().getResource("/images/test.svg");
+        File incomingFile = new File(url.getFile());
+
+        Dimension dim = imageApi.getWidthHeight(incomingFile);
+        assert (dim.getHeight() == 103);
+        assert (dim.getWidth() == 203);
+
+    }
+
+
 
     
     @Test
