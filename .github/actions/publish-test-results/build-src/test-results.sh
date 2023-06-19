628eb44db85eee2fd6e6b966f6c17b50fcf8426f
Victor Alfaro <victor.alfaro@dotcms.com>
2023-06-19 12:08:52 -0600

    Resolving conflicts after pulling results

--- .github/actions/publish-test-results/build-src/test-results.sh
+++ .github/actions/publish-test-results/build-src/test-results.sh
@@ -102,6 +102,30 @@ function addResults {
   executeCmd "cp -R ${OUTPUT_FOLDER}/* ${target_folder}"
 }
 
+# Resolves possible conflicts by checking out ours
+function resolveConflicts {
+  local pull_output=$("git pull origin ${BUILD}")
+  [[ ${cmd_result} != 0 ]] && echo "Error pulling from git branch ${BUILD_ID}, error code: ${cmd_result}"
+
+  local ifs_bak=${IFS}
+  while IFS= read -r line; do
+    if [[ ${line} =~ ^CONFLICT*$ ]]; then
+      local arr=(${line})
+      local size=${#arr[@]}
+      local last=$(expr $size - 1)
+      local conflict_path=${arr[${last}]}
+      echo "Conflict detected: at ${conflict_path}, resolving with ours"
+      executeCmd "git checkout --ours ${conflict_path}"
+      [[ ${cmd_result} != 0 ]] \
+        && echo "Error checking out ours for ${conflict_path}, error code: ${cmd_result}" \
+        && exit 1
+    else
+      echo ${line}
+    fi
+  done <<< "${pull_output}"
+  IFS=$ifs_bak
+}
+
 # Persists results in 'test-results' repo in the provided INPUT_BUILD_ID branch.
 function persistResults {
   cd ${INPUT_PROJECT_ROOT}
@@ -170,10 +194,7 @@ function persistResults {
     # Do not pull unless branch is remote
     if [[ ${remote_branch} == 1 ]]; then
       # Perform a pull just in case
-      executeCmd "git pull --strategy-option=ours origin ${BUILD_ID}"
-      if [[ ${cmd_result} != 0 ]]; then
-        echo "Error pulling from git branch ${BUILD_ID}, error code: ${cmd_result}"
-      fi
+      resolveConflicts
     else
       echo "Not pulling ${BUILD_ID} since it is not remote yet"
     fi
@@ -189,7 +210,8 @@ function persistResults {
   fi
 }
 
-function gatherResults {
+# Build necessary results files
+function buildResults {
   local results_base_path=$1
   executeCmd "cp -R ${results_base_path}/postman/reports/xml/* ${INPUT_TESTS_RESULTS_LOCATION}/"
   executeCmd "cat ./postman-results-header.html > ./index.html"
@@ -235,35 +257,18 @@ function closeResults {
   executeCmd "mkdir -p ${INPUT_TESTS_RESULTS_LOCATION}"
   setOutput tests_results_location ${INPUT_TESTS_RESULTS_LOCATION}
 
-  gatherResults ${results_base_path}
+  buildResults ${results_base_path}
   [[ "${MULTI_COMMIT}" == 'true' ]] \
     && results_base_path=${base_path}/current \
     && cd ${results_base_path}/postman/reports/html \
-    && gatherResults ${results_base_path}
+    && buildResults ${results_base_path}
 
   cd ${base_path}
   executeCmd "git status"
   executeCmd "git add ."
   executeCmd "git commit -m \"Closing results for branch ${BUILD_ID}\""
 
-  local pull_output=$("git pull origin ${BUILD}")
-  local ifs_bak=${IFS}
-  while IFS= read -r line; do
-    if [[ ${line} =~ ^CONFLICT*$ ]]; then
-      local arr=(${line})
-      local size=${#arr[@]}
-      local last=$(expr $size - 1)
-      local conflict_path=${arr[${last}]}
-      echo "Conflict detected: at ${conflict_path}"
-      executeCmd "git checkout --ours ${conflict_path}"
-      [[ ${cmd_result} != 0 ]] \
-        && echo "Error checking out ours for ${conflict_path}, error code: ${cmd_result}" \
-        && exit 1
-    else
-      echo ${line}
-    fi
-  done <<< "${pull_output}"
-  IFS=$ifs_bak
+  resolveConflicts
 
   executeCmd "git push ${test_results_repo_url}"
   [[ ${cmd_result} != 0 ]] \
