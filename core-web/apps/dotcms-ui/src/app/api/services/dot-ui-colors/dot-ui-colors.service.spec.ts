b43fa9b4694a92a6776897fe9b8d9c83c888bbe3
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-06-20 08:34:58 -0600

    Fix test 1

--- core-web/apps/dotcms-ui/src/app/api/services/dot-ui-colors/dot-ui-colors.service.spec.ts
+++ core-web/apps/dotcms-ui/src/app/api/services/dot-ui-colors/dot-ui-colors.service.spec.ts
@@ -43,16 +43,108 @@ describe('DotUiColorsService', () => {
 
         const html = <HTMLElement>document.querySelector('');
 
-        expect(html.style.setProperty).toHaveBeenCalledWith('--color-main', '#78E4FF');
-        expect(html.style.setProperty).toHaveBeenCalledWith('--color-main_mod', '#B5F0FF');
-        expect(html.style.setProperty).toHaveBeenCalledWith('--color-main_rgb', '120, 228, 255');
-        expect(html.style.setProperty).toHaveBeenCalledWith('--color-sec', '#98FF78');
-        expect(html.style.setProperty).toHaveBeenCalledWith('--color-sec_rgb', '152, 255, 120');
-        expect(html.style.setProperty).toHaveBeenCalledWith('--color-background', '#CB8978');
-        expect(html.style.setProperty).toHaveBeenCalledTimes(6);
+        [
+            { key: '--color-primary-h', value: '192deg' },
+            { key: '--color-primary-s', value: '100%' },
+            { key: '--color-palette-primary-100', value: 'hsl(194deg, 100%, 97%)' },
+            { key: '--color-palette-primary-200', value: 'hsl(192deg, 100%, 92%)' },
+            { key: '--color-palette-primary-300', value: 'hsl(192deg, 100%, 87%)' },
+            { key: '--color-palette-primary-400', value: 'hsl(192deg, 100%, 82%)' },
+            { key: '--color-palette-primary-500', value: 'hsl(192deg, 100%, 74%)' },
+            { key: '--color-palette-primary-600', value: 'hsl(192deg, 51%, 59%)' },
+            { key: '--color-palette-primary-700', value: 'hsl(192deg, 36%, 44%)' },
+            { key: '--color-palette-primary-800', value: 'hsl(193deg, 36%, 22%)' },
+            { key: '--color-palette-primary-900', value: 'hsl(193deg, 37%, 7%)' },
+            {
+                key: '--color-palette-primary-op-10',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.1)'
+            },
+            {
+                key: '--color-palette-primary-op-20',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.2)'
+            },
+            {
+                key: '--color-palette-primary-op-30',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.3)'
+            },
+            {
+                key: '--color-palette-primary-op-40',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.4)'
+            },
+            {
+                key: '--color-palette-primary-op-50',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.5)'
+            },
+            {
+                key: '--color-palette-primary-op-60',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.6)'
+            },
+            {
+                key: '--color-palette-primary-op-70',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.7)'
+            },
+            {
+                key: '--color-palette-primary-op-80',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.8)'
+            },
+            {
+                key: '--color-palette-primary-op-90',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.9)'
+            },
+            { key: '--color-secondary-h', value: '106deg' },
+            { key: '--color-secondary-s', value: '100%' },
+            { key: '--color-palette-secondary-100', value: 'hsl(106deg, 100%, 97%)' },
+            { key: '--color-palette-secondary-200', value: 'hsl(107deg, 100%, 92%)' },
+            { key: '--color-palette-secondary-300', value: 'hsl(106deg, 100%, 87%)' },
+            { key: '--color-palette-secondary-400', value: 'hsl(106deg, 100%, 82%)' },
+            { key: '--color-palette-secondary-500', value: 'hsl(106deg, 100%, 74%)' },
+            { key: '--color-palette-secondary-600', value: 'hsl(106deg, 51%, 59%)' },
+            { key: '--color-palette-secondary-700', value: 'hsl(106deg, 36%, 44%)' },
+            { key: '--color-palette-secondary-800', value: 'hsl(105deg, 36%, 22%)' },
+            { key: '--color-palette-secondary-900', value: 'hsl(107deg, 37%, 7%)' },
+            {
+                key: '--color-palette-secondary-op-10',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.1)'
+            },
+            {
+                key: '--color-palette-secondary-op-20',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.2)'
+            },
+            {
+                key: '--color-palette-secondary-op-30',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.3)'
+            },
+            {
+                key: '--color-palette-secondary-op-40',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.4)'
+            },
+            {
+                key: '--color-palette-secondary-op-50',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.5)'
+            },
+            {
+                key: '--color-palette-secondary-op-60',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.6)'
+            },
+            {
+                key: '--color-palette-secondary-op-70',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.7)'
+            },
+            {
+                key: '--color-palette-secondary-op-80',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.8)'
+            },
+            {
+                key: '--color-palette-secondary-op-90',
+                value: 'hsla(var(--color-primary-h), var(--color-primary-s), 100%, 0.9)'
+            },
+            { key: '--color-background', value: '#CB8978' }
+        ].forEach(({ key, value }) => {
+            expect(html.style.setProperty).toHaveBeenCalledWith(key, value);
+        });
     });
 
-    it('should not set invalid colors', () => {
+    fit('should not set invalid colors', () => {
         service.setColors(document.querySelector('html'), {
             primary: 'sdfadfg',
             secondary: 'dfgsdfg',
