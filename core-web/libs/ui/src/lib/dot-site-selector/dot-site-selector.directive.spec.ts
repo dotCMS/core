fdd25a87623030c45ebddc6023c28fd1ff135f6b
Manuel Rojas <manuel.rojas.21@gmail.com>
2023-06-21 08:04:37 -0600

    Fixing tests

--- core-web/libs/ui/src/lib/dot-site-selector/dot-site-selector.directive.spec.ts
+++ core-web/libs/ui/src/lib/dot-site-selector/dot-site-selector.directive.spec.ts
@@ -1,8 +1,38 @@
+import { createDirectiveFactory, SpectatorDirective } from '@ngneat/spectator';
+
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { DropdownModule } from 'primeng/dropdown';
+
+import { DotEventsService, PaginatorService } from '@dotcms/data-access';
+import { CoreWebService, SiteService } from '@dotcms/dotcms-js';
+import { CoreWebServiceMock, SiteServiceMock } from '@dotcms/utils-testing';
+
 import { DotSiteSelectorDirective } from './dot-site-selector.directive';
 
 describe('DotSiteSelectorDirective', () => {
-    it('should create an instance', () => {
-        const directive = new DotSiteSelectorDirective();
-        expect(directive).toBeTruthy();
+    let spectator: SpectatorDirective<DotSiteSelectorDirective>;
+    const createDirective = createDirectiveFactory({
+        directive: DotSiteSelectorDirective,
+        imports: [HttpClientTestingModule, DropdownModule],
+        providers: [
+            { provide: SiteService, useValue: new SiteServiceMock() },
+            { provide: CoreWebService, useClass: CoreWebServiceMock },
+            {
+                provide: DotEventsService
+            },
+            {
+                provide: PaginatorService
+            }
+        ]
+    });
+
+    beforeEach(() => {
+        spectator = createDirective('<p-dropdown dotSiteSelector><p-dropdown>');
+    });
+
+    it('should create', () => {
+        spectator.detectChanges();
+        expect(spectator.directive).toBeTruthy();
     });
 });
