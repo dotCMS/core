0ecb36b3f9d08c4a6e18381b61b11bb069be9612
Manuel Rojas <manuel.rojas.21@gmail.com>
2023-06-23 12:29:12 -0600

    Adding feature-flag SEO

--- core-web/apps/dotcms-ui/src/app/portlets/dot-edit-page/components/dot-edit-page-info/dot-edit-page-info.component.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-edit-page/components/dot-edit-page-info/dot-edit-page-info.component.ts
@@ -1,5 +1,10 @@
 import { DOCUMENT } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Inject, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, Input, OnInit } from '@angular/core';
+
+import { take } from 'rxjs/operators';
+
+import { DotPropertiesService } from '@dotcms/data-access';
+import { FeaturedFlags } from '@dotcms/dotcms-models';
 
 /**
  * Basic page information for edit mode
@@ -11,19 +16,32 @@ import { ChangeDetectionStrategy, Component, Inject, Input } from '@angular/core
     selector: 'dot-edit-page-info',
     templateUrl: './dot-edit-page-info.component.html',
     styleUrls: ['./dot-edit-page-info.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.Default
 })
-export class DotEditPageInfoComponent {
+export class DotEditPageInfoComponent implements OnInit {
     @Input() title: string;
     @Input() url: string;
     innerApiLink: string;
     previewUrl: string;
     baseUrl: string;
+    seoImprovements: boolean;
 
-    constructor(@Inject(DOCUMENT) private document: Document) {
+    constructor(
+        @Inject(DOCUMENT) private document: Document,
+        private dotPropertiesService: DotPropertiesService
+    ) {
         this.baseUrl = document.defaultView.location.href.includes('edit-page')
             ? document.defaultView.location.origin
             : '';
+    }
+
+    ngOnInit() {
+        this.dotPropertiesService
+            .getKey(FeaturedFlags.FEATURE_FLAG_SEO_IMPROVEMENTS)
+            .pipe(take(1))
+            .subscribe((value) => {
+                this.seoImprovements = value && value === 'true';
+            });
     }
 
     @Input()
