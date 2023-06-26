a5b0e8789cae38ddcc8281ba237f67795941ca76
freddyucv <freddy0309@gmail.com>
2023-06-26 15:20:20 -0600

    merge

--- dotCMS/src/integration-test/java/com/dotcms/experiments/business/IndexRegexUrlPatterStrategyIntegrationTest.java
+++ dotCMS/src/integration-test/java/com/dotcms/experiments/business/IndexRegexUrlPatterStrategyIntegrationTest.java
@@ -123,11 +123,7 @@ public class IndexRegexUrlPatterStrategyIntegrationTest {
     /**
      * Method to test: {@link IndexRegexUrlPatterStrategy#isMatch(HTMLPageAsset)} and
      * {@link IndexRegexUrlPatterStrategy#getRegexPattern(HTMLPageAsset)}
-<<<<<<< HEAD
-     * When: The page is the an index page and the protocol is https
-=======
      * When: The page is an index page and the protocol is https
->>>>>>> origin/master
      * Should: the {@link IndexRegexUrlPatterStrategy#isMatch(HTMLPageAsset)} return true
      * and the {@link IndexRegexUrlPatterStrategy#getRegexPattern(HTMLPageAsset)} return the correct regex pattern
      * also the regex pattern should match the page url
@@ -223,11 +219,7 @@ public class IndexRegexUrlPatterStrategyIntegrationTest {
     /**
      * Method to test: {@link IndexRegexUrlPatterStrategy#isMatch(HTMLPageAsset)} and
      * {@link IndexRegexUrlPatterStrategy#getRegexPattern(HTMLPageAsset)}
-<<<<<<< HEAD
-     * When: The page an index page but not the root Index Page and the Domain is not localhost
-=======
      * When: The page is an index page but not the root Index Page and the Domain is not localhost
->>>>>>> origin/master
      * Should: the {@link IndexRegexUrlPatterStrategy#isMatch(HTMLPageAsset)} return true
      * and the {@link IndexRegexUrlPatterStrategy#getRegexPattern(HTMLPageAsset)} return the correct regex pattern
      * also the regex pattern should match the page url
