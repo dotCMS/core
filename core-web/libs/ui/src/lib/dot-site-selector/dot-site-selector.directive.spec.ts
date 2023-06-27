8f2cf03f4c0047a7eaa5e89c2cea1d8a6f1e5638
Rafael <rjvelazco21@gmail.com>
2023-06-27 11:22:18 -0400

    test: dot-site-selector.directive.ts

--- core-web/libs/ui/src/lib/dot-site-selector/dot-site-selector.directive.spec.ts
+++ core-web/libs/ui/src/lib/dot-site-selector/dot-site-selector.directive.spec.ts
@@ -1,38 +1,83 @@
 import { createDirectiveFactory, SpectatorDirective } from '@ngneat/spectator';
+import { of } from 'rxjs';
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { fakeAsync } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { DropdownModule } from 'primeng/dropdown';
+import { Dropdown, DropdownModule } from 'primeng/dropdown';
 
-import { DotEventsService, PaginatorService } from '@dotcms/data-access';
-import { CoreWebService, SiteService } from '@dotcms/dotcms-js';
+import { DotEventsService, DotSiteService } from '@dotcms/data-access';
+import { CoreWebService, mockSites, SiteService } from '@dotcms/dotcms-js';
 import { CoreWebServiceMock, SiteServiceMock } from '@dotcms/utils-testing';
 
 import { DotSiteSelectorDirective } from './dot-site-selector.directive';
 
 describe('DotSiteSelectorDirective', () => {
     let spectator: SpectatorDirective<DotSiteSelectorDirective>;
+    let dropdown: Dropdown;
+    let dotEventsService: DotEventsService;
+    let dotSiteService: DotSiteService;
+
     const createDirective = createDirectiveFactory({
         directive: DotSiteSelectorDirective,
-        imports: [HttpClientTestingModule, DropdownModule],
+        imports: [HttpClientTestingModule, DropdownModule, BrowserAnimationsModule],
         providers: [
-            { provide: SiteService, useValue: new SiteServiceMock() },
+            { provide: SiteService, useClass: SiteServiceMock },
             { provide: CoreWebService, useClass: CoreWebServiceMock },
-            {
-                provide: DotEventsService
-            },
-            {
-                provide: PaginatorService
-            }
+            DotEventsService,
+            DotSiteService
         ]
     });
 
     beforeEach(() => {
-        spectator = createDirective('<p-dropdown dotSiteSelector><p-dropdown>');
+        spectator = createDirective(
+            '<p-dropdown [dotSiteSelector] [archive]="true" [live]="true" [system]="true" [pageSize]="10"></p-dropdown>'
+        );
+        dotSiteService = spectator.inject(DotSiteService);
+        dotEventsService = spectator.inject(DotEventsService);
+        dropdown = spectator.directive['control'] as Dropdown;
     });
 
     it('should create', () => {
         spectator.detectChanges();
         expect(spectator.directive).toBeTruthy();
+    });
+
+    describe('Get Sites', () => {
+        let getSitesSpy;
+
+        beforeEach(() => {
+            getSitesSpy = spyOn(dotSiteService, 'getSites').and.returnValue(of(mockSites));
+        });
+
+        it('should get sites list', () => {
+            spectator.detectChanges();
+
+            spectator.directive.ngOnInit();
+
+            expect(getSitesSpy).toHaveBeenCalled();
+        });
+
+        it('should get sites list with filter', fakeAsync(() => {
+            const filter = 'demo';
+
+            dropdown.onFilter.emit({ filter });
+
+            spectator.tick(500);
+            expect(getSitesSpy).toHaveBeenCalledWith(filter, 10);
+        }));
+    });
+
+    describe('Listen login-as/logout-as events', () => {
+        it('should send notification when login-as/logout-as', fakeAsync(() => {
+            const getSitesSpy = spyOn(dotSiteService, 'getSites').and.returnValue(of(mockSites));
+            spectator.detectChanges();
+            dotEventsService.notify('login-as');
+            spectator.tick(0);
+            dotEventsService.notify('logout-as');
+            spectator.tick(0);
+            expect(getSitesSpy).toHaveBeenCalledTimes(2);
+        }));
     });
 });
--- core-web/libs/ui/src/lib/dot-site-selector/dot-site-selector.directive.ts
+++ core-web/libs/ui/src/lib/dot-site-selector/dot-site-selector.directive.ts
@@ -3,21 +3,19 @@ import { Subject } from 'rxjs';
 import {
     ChangeDetectorRef,
     Directive,
-    EventEmitter,
     Input,
     OnDestroy,
     OnInit,
     Optional,
-    Output,
     Self
 } from '@angular/core';
 
 import { Dropdown } from 'primeng/dropdown';
 
-import { debounceTime, delay, retryWhen, take, takeUntil, tap } from 'rxjs/operators';
+import { debounceTime, take, takeUntil } from 'rxjs/operators';
 
 import { DotEventsService, PaginatorService, DotSiteService } from '@dotcms/data-access';
-import { Site, SiteService } from '@dotcms/dotcms-js';
+import { Site } from '@dotcms/dotcms-js';
 
 @Directive({
     selector: '[dotSiteSelector]',
@@ -30,9 +28,6 @@ export class DotSiteSelectorDirective implements OnInit, OnDestroy {
     @Input() system = true;
     @Input() pageSize = 10;
 
-    @Output() switchSite: EventEmitter<Site> = new EventEmitter();
-
-    protected currentSite: Site;
     private readonly destroy$: Subject<boolean> = new Subject<boolean>();
     private readonly dotEvents = ['login-as', 'logout-as'];
     private readonly control: Dropdown;
@@ -41,13 +36,12 @@ export class DotSiteSelectorDirective implements OnInit, OnDestroy {
         @Optional() @Self() private readonly primeDropdown: Dropdown,
         private readonly dotEventsService: DotEventsService,
         private readonly dotSiteService: DotSiteService,
-        private readonly cd: ChangeDetectorRef,
-        private readonly siteService: SiteService
+        private readonly cd: ChangeDetectorRef
     ) {
         this.control = this.primeDropdown;
 
         if (this.control) {
-            this.control.onFilter.pipe(debounceTime(225)).subscribe((event) => {
+            this.control.onFilter.pipe(debounceTime(300)).subscribe((event) => {
                 this.getSitesList(event.filter);
             });
         } else {
@@ -58,45 +52,12 @@ export class DotSiteSelectorDirective implements OnInit, OnDestroy {
     ngOnInit() {
         this.getSitesList();
 
-        this.siteService.refreshSites$
-            .pipe(takeUntil(this.destroy$))
-            .subscribe((site: Site) => this.handleSitesRefresh(site));
-
         this.dotEvents.forEach((event: string) => {
             this.dotEventsService
                 .listen(event)
                 .pipe(takeUntil(this.destroy$))
                 .subscribe(() => this.getSitesList());
         });
-
-        this.siteService.switchSite$.pipe(takeUntil(this.destroy$)).subscribe(() => {
-            this.updateCurrentSite(this.siteService.currentSite);
-        });
-    }
-
-    /**
-     * Manage the sites refresh when a event happen
-     * @memberof SiteSelectorComponent
-     */
-    handleSitesRefresh(site: Site): void {
-        this.dotSiteService
-            .getSites()
-            .pipe(
-                take(1),
-                tap((items: Site[]) => {
-                    const siteIndex = items.findIndex(
-                        (item: Site) => site.identifier === item.identifier
-                    );
-
-                    const shouldRetry = site.archived ? siteIndex >= 0 : siteIndex === -1;
-
-                    if (shouldRetry) {
-                        throw new Error('Indexing... site still present');
-                    }
-                }),
-                retryWhen((error) => error.pipe(delay(1000), take(10)))
-            )
-            .subscribe((items: Site[]) => this.updateOptions(items));
     }
 
     ngOnDestroy(): void {
@@ -111,30 +72,6 @@ export class DotSiteSelectorDirective implements OnInit, OnDestroy {
     private setOptions(options: Array<Site>): void {
         this.primeDropdown.options = [...options];
         this.cd.detectChanges();
-    }
-
-    /**
-     * Updates the current site
-     *
-     * @param {Site} site
-     * @memberof DotSiteSelectorComponent
-     */
-    private updateCurrentSite(site: Site): void {
-        this.currentSite = site;
-        this.switchSite.emit(site);
-        this.cd.detectChanges();
-    }
-
-    /**
-     * Updates the options and the current site
-     *
-     * @private
-     * @param {Site[]} items
-     * @memberof DotSiteSelectorDirective
-     */
-    private updateOptions(items: Site[]): void {
-        this.setOptions(items);
-        this.updateCurrentSite(this.siteService.currentSite);
     }
 
     /**
