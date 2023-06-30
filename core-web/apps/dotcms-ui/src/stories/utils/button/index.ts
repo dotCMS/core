acbd7ee5ae60fcf812cfca99848d87b2aa6d7fdb
Jalinson Diaz <zjaaaldev@gmail.com>
2023-06-30 18:10:20 -0300

    code cleaning

--- core-web/apps/dotcms-ui/src/stories/utils/button/index.ts
+++ core-web/apps/dotcms-ui/src/stories/utils/button/index.ts
@@ -1,11 +1,13 @@
 const SEVERITY_PLACEHOLDER_REGEX = /SEVERITY_TEMPLATE/g;
 const BUTTONS_PLACEHOLDER_REGEX = /BUTTONS_TEMPLATE/g;
+
 const SEVERITY_VALUE_REGEX = /SEVERITY_VALUE/g;
 const SIZE_VALUE_REGEX = /SIZE_VALUE/g;
 const BUTTON_TYPE_REGEX = /BUTTON_TYPE/g;
 
 const MAIN_DIV_TEMPLATE = `<div style="display: flex; gap: 24px; flex-direction: column; align-items: center;">SEVERITY_TEMPLATE</div>`;
-const SEVERITY_SEPARATOR_TEMPLATE = `<div style="display: flex; gap: 8px; justify-content: center; width: fit-content;">BUTTONS_TEMPLATE</div>`;
+const SIZE_SEPARATOR_TEMPLATE = `<div style="display: flex; gap: 8px; justify-content: center; width: fit-content;">BUTTONS_TEMPLATE</div>`;
+
 export const MAIN_BUTTONS_TEMPLATE = `<button class="SIZE_VALUE SEVERITY_VALUE BUTTON_TYPE" pButton label="Button"></button>
 <button class="SIZE_VALUE SEVERITY_VALUE BUTTON_TYPE" pButton label="Button" icon="pi pi-plus"></button>
 <button class="SIZE_VALUE SEVERITY_VALUE BUTTON_TYPE" pButton label="Button" disabled="true"></button>`;
@@ -14,6 +16,7 @@ export const ICON_ONLY_BUTTONS_TEMPLATE = `<button class="SIZE_VALUE SEVERITY_VA
 <button class="SIZE_VALUE SEVERITY_VALUE BUTTON_TYPE p-button-rounded" pButton icon="pi pi-ellipsis-v"></button>
 <button class="SIZE_VALUE SEVERITY_VALUE BUTTON_TYPE p-button-rounded" pButton icon="pi pi-ellipsis-v" disabled="true"></button>`;
 
+// Empty string represents basic buttons (no severity, no size)
 const severities = ['', 'p-button-secondary', 'p-button-danger'];
 const sizes = ['p-button-lg', '', 'p-button-sm'];
 
@@ -27,24 +30,25 @@ const sizes = ['p-button-lg', '', 'p-button-sm'];
 export function createButtonTemplate(
     buttonType: string = '',
     buttonTemplate: string = MAIN_BUTTONS_TEMPLATE
-) {
-    const mainDiv = MAIN_DIV_TEMPLATE;
-
+): string {
+    // All buttons separated by severity
     const buttonsBySeverity = severities
         .map((severity) => {
-            let sizeDiv = '';
-            sizes.forEach((size) => {
-                const button = buttonTemplate
-                    .replace(SEVERITY_VALUE_REGEX, severity)
-                    .replace(SIZE_VALUE_REGEX, size)
-                    .replace(BUTTON_TYPE_REGEX, buttonType);
+            // All buttons separated by size but with the same severity
+            return sizes
+                .map((size) => {
+                    const button = buttonTemplate
+                        .replace(SEVERITY_VALUE_REGEX, severity)
+                        .replace(SIZE_VALUE_REGEX, size)
+                        .replace(BUTTON_TYPE_REGEX, buttonType);
 
-                sizeDiv += SEVERITY_SEPARATOR_TEMPLATE.replace(BUTTONS_PLACEHOLDER_REGEX, button);
-            });
-
-            return sizeDiv;
+                    // We insert the button group in the size template
+                    return SIZE_SEPARATOR_TEMPLATE.replace(BUTTONS_PLACEHOLDER_REGEX, button);
+                })
+                .join('');
         })
         .join('');
 
-    return mainDiv.replace(SEVERITY_PLACEHOLDER_REGEX, buttonsBySeverity);
+    // All buttons inside the main div
+    return MAIN_DIV_TEMPLATE.replace(SEVERITY_PLACEHOLDER_REGEX, buttonsBySeverity);
 }
