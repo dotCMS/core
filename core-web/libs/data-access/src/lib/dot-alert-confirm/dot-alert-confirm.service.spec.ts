593d45264e0e5a74359d82ed88e1c5233390e9f6
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-07-03 16:16:01 -0600

    Fix more tests

--- core-web/libs/data-access/src/lib/dot-alert-confirm/dot-alert-confirm.service.spec.ts
+++ core-web/libs/data-access/src/lib/dot-alert-confirm/dot-alert-confirm.service.spec.ts
@@ -55,10 +55,11 @@ describe('DotAlertConfirmService', () => {
         confirmationService = testbed.get(ConfirmationService);
     });
 
-    describe('confirmation', () => {
+    describe('confirmation', (done) => {
         it('should set model and call confirm method in primeng service', fakeAsync(() => {
             jest.spyOn(confirmationService, 'confirm');
             service.confirmDialogOpened$.pipe(take(1)).subscribe((response: boolean) => {
+                done();
                 expect(response).toBe(true);
             });
             service.confirm(mockData);
