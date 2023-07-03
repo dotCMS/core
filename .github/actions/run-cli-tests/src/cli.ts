549824d5f689ea7caeef9f41db9756a94ae4aa45
daniel.colina <daniel.colina@dotcms.com>
2023-07-03 17:24:16 +0200

    #22706 Testing docker-compose deployment.

--- .github/actions/run-cli-tests/src/cli.ts
+++ .github/actions/run-cli-tests/src/cli.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-// import * as exec from '@actions/exec'
+import * as exec from '@actions/exec'
 import * as fs from 'fs'
 import * as path from 'path'
 // import * as shelljs from 'shelljs'
@@ -25,17 +25,24 @@ const volumes = [licenseFolder, path.join(dockerFolder, 'cms-shared'), path.join
 const PASSED = 'PASSED'
 const FAILED = 'FAILED'
 
-// const DEPS_ENV: {[key: string]: string} = {
-//   DOTCMS_IMAGE: builtImageName,
-//   TEST_TYPE: 'cli',
-//   DB_TYPE: dbType,
-//   CUSTOM_STARTER_FOLDER: customStarterUrl,
-//   WAIT_FOR_DEPS: waitForDeps,
-//   POSTGRES_USER: 'postgres',
-//   POSTGRES_PASSWORD: 'postgres',
-//   POSTGRES_DB: 'dotcms',
-//   JVM_ENDPOINT_TEST_PASS: 'obfuscate_me'
-// }
+const DEPS_ENV: {[key: string]: string} = {
+  DOTCMS_IMAGE: builtImageName,
+  TEST_TYPE: 'cli',
+  DB_TYPE: dbType,
+  CUSTOM_STARTER_FOLDER: customStarterUrl,
+  WAIT_FOR_DEPS: waitForDeps,
+  POSTGRES_USER: 'postgres',
+  POSTGRES_PASSWORD: 'postgres',
+  POSTGRES_DB: 'dotcms',
+  JVM_ENDPOINT_TEST_PASS: 'obfuscate_me'
+}
+
+export interface Command {
+  cmd: string
+  args?: string[]
+  workingDir?: string
+  env?: {[key: string]: string}
+}
 
 export interface TestsResult {
   testsRunExitCode: number
@@ -61,6 +68,8 @@ export const runTests = async (): Promise<TestsResult> => {
   try {
     printInputs()
     await setup()
+    await startDependencies()
+    return await runCliTestsInDocker()
   } catch (err) {
     core.setFailed(`Error setting up environment: ${err}`)
     return {
@@ -68,12 +77,8 @@ export const runTests = async (): Promise<TestsResult> => {
       testsResultsStatus: FAILED,
       skipResultsReport: true
     }
-  }
-
-  return {
-    testsRunExitCode: 0,
-    testsResultsStatus: PASSED,
-    skipResultsReport: false
+  } finally {
+    await stopDependencies()
   }
 }
 
@@ -104,30 +109,150 @@ const prepareLicense = async () => {
   fs.writeFileSync(licenseFile, licenseKey, {encoding: 'utf8', flag: 'a+', mode: 0o777})
 }
 
-// /**
-//  * Delays the resolve part of a promise to simulate a sleep
-//  *
-//  * @param seconds number of seconds
-//  * @returns void promise
-//  */
-// const delay = (seconds: number) => new Promise(resolve => setTimeout(resolve, seconds * 1000))
+/**
+ * Start test depencies: db, ES and DotCMS instance.
+ */
+const startDependencies = async () => {
+  // Starting dependencies
+  core.info(`
+      =======================================
+      Starting tests dependencies
+      =======================================`)
 
-// /**
-//  * Waits for specific time with corresponding messages.
-//  *
-//  * @param wait time to wait
-//  * @param startLabel start label
-//  * @param endLabel end label
-//  */
-// const waitFor = async (wait: number, startLabel: string, endLabel?: string) => {
-//     core.info(`Waiting ${wait} seconds for ${startLabel}`)
-//     await delay(wait)
-//     const finalLabel = endLabel || startLabel
-//     core.info(`Waiting on ${finalLabel} loading has ended`)
-//   }
+  await execCmd(toCommand('docker', ['pull', 'ghcr.io/dotcms/elasticsearch:7.9.1'], dockerFolder))
+  await execCmd(toCommand('docker', ['pull', 'ghcr.io/dotcms/postgres:13-alpine'], dockerFolder))
+  await execCmd(toCommand('docker', ['pull', builtImageName], dockerFolder))
 
-//   startAppEnvironment = async () => {
-//     core.info('Starting dotCMS environment...')
-//     await exec.exec('docker-compose', ['up', '-d'], {cwd: dockerFolder})
-//     await waitForAppEnvironment()
-//   }
+  execCmdAsync(
+    toCommand(
+      'docker-compose',
+      ['-f', 'open-distro-compose.yml', '-f', `${dbType}-compose.yml`, '-f', 'dotcms-compose.yml', 'up'],
+      dockerFolder,
+      DEPS_ENV
+    )
+  )
+}
+
+/**
+ * Stop postman depencies: db, ES and DotCMS isntance.
+ */
+const stopDependencies = async () => {
+  // Stopping dependencies
+  core.info(`
+      ===================================
+      Stopping tests dependencies
+      ===================================`)
+
+  try {
+    await execCmd(
+      toCommand(
+        'docker-compose',
+        ['-f', 'open-distro-compose.yml', '-f', `${dbType}-compose.yml`, '-f', 'dotcms-compose.yml', 'down', '-v'],
+        dockerFolder,
+        DEPS_ENV
+      )
+    )
+  } catch (err) {
+    console.error(`Error stopping dependencies: ${err}`)
+  }
+}
+
+/**
+ * Executes command contained in Command object.
+ *
+ * @param cmd Command object
+ * @returns Promise with process number
+ */
+const execCmd = async (cmd: Command): Promise<number> => {
+  printCmd(cmd)
+  return await exec.exec(cmd.cmd, cmd.args || [], {cwd: cmd.workingDir, env: cmd.env})
+}
+
+/**
+ * Async executes command contained in Command object.
+ *
+ * @param cmd Command object
+ */
+const execCmdAsync = (cmd: Command) => {
+  printCmd(cmd)
+  exec.exec(cmd.cmd, cmd.args || [], {cwd: cmd.workingDir, env: cmd.env})
+}
+
+/**
+ * Gather values and build a Command instance.
+ *
+ * @param cmd command
+ * @param args arguments
+ * @param workingDir working dir
+ * @param env environment variables
+ * @returns Command object
+ */
+const toCommand = (cmd: string, args?: string[], workingDir?: string, env?: {[key: string]: string}): Command => {
+  return {
+    cmd,
+    args,
+    workingDir,
+    env
+  }
+}
+
+/**
+ * Prints string Command representation.
+ *
+ * @param cmd Command object
+ */
+const printCmd = (cmd: Command) => {
+  let message = `Executing cmd: ${cmd.cmd} ${cmd.args?.join(' ') || ''}`
+  if (cmd.workingDir) {
+    message += `\ncwd: ${cmd.workingDir}`
+  }
+  if (cmd.env) {
+    message += `\nenv: ${JSON.stringify(cmd.env, null, 2)}`
+  }
+  core.info(message)
+}
+
+/**
+ * Run postman tests.
+ *
+ * @returns an overall ivew of the tests results
+ */
+const runCliTestsInDocker = async (): Promise<TestsResult> => {
+  await waitFor(120, `DotCMS instance`)
+
+  // Run tests
+  core.info(`
+      ===========================================
+      Running CLI tests against ${dbType}
+      ===========================================`)
+
+  // runCliTests()
+
+  return {
+    testsRunExitCode: 0,
+    testsResultsStatus: PASSED,
+    skipResultsReport: false
+  }
+}
+
+/**
+ * Delays the resolve part of a promise to simulate a sleep
+ *
+ * @param seconds number of seconds
+ * @returns void promise
+ */
+const delay = (seconds: number) => new Promise(resolve => setTimeout(resolve, seconds * 1000))
+
+/**
+ * Waits for specific time with corresponding messages.
+ *
+ * @param wait time to wait
+ * @param startLabel start label
+ * @param endLabel end label
+ */
+const waitFor = async (wait: number, startLabel: string, endLabel?: string) => {
+  core.info(`Waiting ${wait} seconds for ${startLabel}`)
+  await delay(wait)
+  const finalLabel = endLabel || startLabel
+  core.info(`Waiting on ${finalLabel} loading has ended`)
+}
