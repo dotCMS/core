1571370c5d2e8d3b615821e44625d4b392dbf0f9
Jalinson Diaz <zjaaaldev@gmail.com>
2023-07-10 14:14:00 -0300

    fix merge problems

--- core-web/apps/dotcms-ui/src/app/view/components/dot-iframe-dialog/dot-iframe-dialog.module.ts
+++ core-web/apps/dotcms-ui/src/app/view/components/dot-iframe-dialog/dot-iframe-dialog.module.ts
@@ -7,7 +7,7 @@ import { DotIframeDialogComponent } from './dot-iframe-dialog.component';
 import { IFrameModule } from '../_common/iframe';
 
 @NgModule({
-    imports: [CommonModule, DotDialogModule, IFrameModule],
+    imports: [DotDialogModule, IFrameModule],
     declarations: [DotIframeDialogComponent],
     exports: [DotIframeDialogComponent]
 })
