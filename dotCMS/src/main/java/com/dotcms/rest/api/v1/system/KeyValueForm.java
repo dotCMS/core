eccb588f1d9504976742cb550162a02516905f02
jdotcms <jonathan.sanchez@dotcms.com>
2023-07-10 23:18:51 -0600

    #25184 adding the system table resource

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/system/KeyValueForm.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/system/KeyValueForm.java
@@ -2,9 +2,12 @@ package com.dotcms.rest.api.v1.system;
 
 import com.dotcms.repackage.javax.validation.constraints.NotNull;
 import com.dotcms.rest.api.Validated;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Encapsulates a key value pair
+ * @author jsanca 
  */
 public class KeyValueForm extends Validated {
 
@@ -14,7 +17,9 @@ public class KeyValueForm extends Validated {
     @NotNull
     private final String value;
 
-    public KeyValueForm(final String key, final String value) {
+    @JsonCreator
+    public KeyValueForm(@JsonProperty("key") final String key,
+                        @JsonProperty("value") final String value) {
         this.key = key;
         this.value = value;
         this.checkValid();
