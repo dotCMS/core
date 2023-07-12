90bca81918dff5b7f94c3b5fe1a63c7ba46c6128
daniel.colina <daniel.colina@dotcms.com>
2023-07-12 16:09:51 +0200

    #22706 Testing docker-compose-maven-plugin approach.

--- tools/dotcms-cli/api-data-model/src/test/java/com/dotcms/ContainerTest.java
+++ tools/dotcms-cli/api-data-model/src/test/java/com/dotcms/ContainerTest.java
@@ -1,32 +1,32 @@
-package com.dotcms;
-
-import io.quarkus.test.common.QuarkusTestResource;
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.given;
-
-@QuarkusTest
-@QuarkusTestResource(ContainerResource.class)
-public class ContainerTest {
-
-    @Test
-    void test_dotcms_container_up() {
-        System.out.println("test_dotcms_container_up");
-
-        given()
-          .when().get("http://localhost:8080/dotAdmin")
-          .then()
-             .statusCode(200);
-    }
-
-    @Test
-    void test_elasticsearch_container_up() {
-        System.out.println("test_elasticsearch_container_up");
-
-        given()
-                .when().get("http://localhost:9200/")
-                .then()
-                .statusCode(200);
-    }
-}
+//package com.dotcms;
+//
+//import io.quarkus.test.common.QuarkusTestResource;
+//import io.quarkus.test.junit.QuarkusTest;
+//import org.junit.jupiter.api.Test;
+//
+//import static io.restassured.RestAssured.given;
+//
+//@QuarkusTest
+//@QuarkusTestResource(ContainerResource.class)
+//public class ContainerTest {
+//
+//    @Test
+//    void test_dotcms_container_up() {
+//        System.out.println("test_dotcms_container_up");
+//
+//        given()
+//          .when().get("http://localhost:8080/dotAdmin")
+//          .then()
+//             .statusCode(200);
+//    }
+//
+//    @Test
+//    void test_elasticsearch_container_up() {
+//        System.out.println("test_elasticsearch_container_up");
+//
+//        given()
+//                .when().get("http://localhost:9200/")
+//                .then()
+//                .statusCode(200);
+//    }
+//}
