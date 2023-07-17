4431dd5119c0a788d3fc109849666a967d57e8a6
Humberto Morera <humberto.morera@dotcms.com>
2023-07-17 10:42:31 -0500

    icons + test

--- core-web/libs/dotcms-models/src/lib/dot-experiments-constants.ts
+++ core-web/libs/dotcms-models/src/lib/dot-experiments-constants.ts
@@ -156,12 +156,12 @@ export const GOALS_METADATA_MAP: Record<
     [GOAL_TYPES.URL_PARAMETER]: {
         label: 'experiments.goal.url_parameter.name',
         description: 'experiments.goal.url_parameter.description',
-        icon: 'pi-check-square'
+        icon: 'pi-paperclip'
     },
     [GOAL_TYPES.EXIT_RATE]: {
         label: 'experiments.goal.exit_rate.name',
         description: 'experiments.goal.exit_rate.description',
-        icon: 'pi-check-square'
+        icon: 'pi-sign-out'
     }
 };
 
