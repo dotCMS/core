b33c13113b3d923b9abc9de9321cfad53765c1cd
Jonathan Gamba <jonathan.gamba@dotcms.com>
2023-07-18 09:57:20 -0600

    #24740

--- tools/dotcms-cli/cli/src/main/java/com/dotcms/api/client/files/traversal/task/PushTreeNodeTask.java
+++ tools/dotcms-cli/cli/src/main/java/com/dotcms/api/client/files/traversal/task/PushTreeNodeTask.java
@@ -116,12 +116,13 @@ public class PushTreeNodeTask extends RecursiveAction {
                     // And we need to create the non-existing site
                     pusher.pushSite(folder.host(), this.localPathStructure.status());
                     logger.debug(String.format("Site [%s] created", folder.host()));
-                } else {
+                } /*else { // If the folder does not exist is created by the resource when the asset is pushed,
+                           // we don't need to create it here.
 
                     // Creating the non-existing folder
                     pusher.createFolder(folder.host(), folder.path());
                     logger.debug(String.format("Folder [%s] created", folder.path()));
-                }
+                }*/
 
                 // Folder processed, updating the progress bar
                 this.progressBar.incrementStep();
