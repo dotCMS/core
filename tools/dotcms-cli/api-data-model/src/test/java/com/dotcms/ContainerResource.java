8444cbd382fb59bf8f8a06662396fdd0a57d959b
daniel.colina <daniel.colina@dotcms.com>
2023-07-20 20:21:37 +0200

    #22706 Testing cli test workflow.+4

--- tools/dotcms-cli/api-data-model/src/test/java/com/dotcms/ContainerResource.java
+++ tools/dotcms-cli/api-data-model/src/test/java/com/dotcms/ContainerResource.java
@@ -27,9 +27,10 @@ public class ContainerResource implements QuarkusTestResourceLifecycleManager {
         DockerComposeContainer dockerComposeContainer = new DockerComposeContainer("dotcms-env", new File("src/test/resources/docker-compose.yaml"));
         dockerComposeContainer.withExposedService("postgres", POSTGRES_SERVICE_PORT, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(STARTUP_TIMEOUT)));
         dockerComposeContainer.withExposedService("elasticsearch", ELASTICSEARCH_SERVICE_PORT, Wait.forHttp("/").forPort(ELASTICSEARCH_SERVICE_PORT).forStatusCode(200));
+//        dockerComposeContainer.withExposedService("dotcms", DOTCMS_SERVICE_PORT, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(STARTUP_TIMEOUT)));
         dockerComposeContainer.withExposedService("dotcms", DOTCMS_SERVICE_PORT, Wait.forHttp("/dotAdmin").forPort(DOTCMS_SERVICE_PORT).forStatusCode(200));
         dockerComposeContainer.withLogConsumer("dotcms", new Slf4jLogConsumer(LOGGER));
-        dockerComposeContainer.withLocalCompose(LOCAL_COMPOSE);//                    .withExposedService("dotcms", DOTCMS_SERVICE_PORT, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(STARTUP_TIMEOUT)))
+        dockerComposeContainer.withLocalCompose(LOCAL_COMPOSE);//
         COMPOSE_CONTAINER = dockerComposeContainer;
     }
 
