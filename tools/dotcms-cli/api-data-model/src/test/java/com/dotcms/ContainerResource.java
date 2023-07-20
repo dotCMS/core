7c55aab264eed6dd98c73e2c14c6e93106d50dd0
daniel.colina <daniel.colina@dotcms.com>
2023-07-20 16:56:41 +0200

    #22706 Testing cli test workflow.+1

--- tools/dotcms-cli/api-data-model/src/test/java/com/dotcms/ContainerResource.java
+++ tools/dotcms-cli/api-data-model/src/test/java/com/dotcms/ContainerResource.java
@@ -1,7 +1,10 @@
 package com.dotcms;
 
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
 import java.io.File;
@@ -16,13 +19,20 @@ public class ContainerResource implements QuarkusTestResourceLifecycleManager {
     private static final int DOTCMS_SERVICE_PORT = 8080;
     private static final int STARTUP_TIMEOUT = 120;
     private static final boolean LOCAL_COMPOSE = false;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ContainerResource.class);
 
-    static DockerComposeContainer<?> COMPOSE_CONTAINER =
-            new DockerComposeContainer("dotcms-env", new File("src/test/resources/docker-compose.yaml"))
-                    .withExposedService("postgres", POSTGRES_SERVICE_PORT, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(STARTUP_TIMEOUT)))
-                    .withExposedService("elasticsearch", ELASTICSEARCH_SERVICE_PORT, Wait.forHttp("/").forPort(ELASTICSEARCH_SERVICE_PORT).forStatusCode(200))
-                    .withExposedService("dotcms", DOTCMS_SERVICE_PORT, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(STARTUP_TIMEOUT)))
-                    .withLocalCompose(LOCAL_COMPOSE); // need to be false to run on macOS
+    static DockerComposeContainer<?> COMPOSE_CONTAINER; // need to be false to run on macOS
+
+    static {
+        DockerComposeContainer dockerComposeContainer = new DockerComposeContainer("dotcms-env", new File("src/test/resources/docker-compose.yaml"));
+        dockerComposeContainer.withExposedService("postgres", POSTGRES_SERVICE_PORT, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(STARTUP_TIMEOUT)));
+        dockerComposeContainer.withExposedService("elasticsearch", ELASTICSEARCH_SERVICE_PORT, Wait.forHttp("/").forPort(ELASTICSEARCH_SERVICE_PORT).forStatusCode(200));
+        dockerComposeContainer.withExposedService("dotcms", DOTCMS_SERVICE_PORT, Wait.forHttp("/dotAdmin").forPort(DOTCMS_SERVICE_PORT).forStatusCode(200));
+        dockerComposeContainer.withLogConsumer("dotcms", new Slf4jLogConsumer(LOGGER));
+        dockerComposeContainer.withLocalCompose(LOCAL_COMPOSE);//                    .withExposedService("dotcms", DOTCMS_SERVICE_PORT, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(STARTUP_TIMEOUT)))
+        COMPOSE_CONTAINER = dockerComposeContainer;
+    }
+
     @Override
     public Map<String, String> start() {
         COMPOSE_CONTAINER.start();
