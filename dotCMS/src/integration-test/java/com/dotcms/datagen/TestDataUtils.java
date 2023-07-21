82c3ee5a48d5d250688bf3c2565742dd8e8bfb5e
Jamie Mauro <102264829+jdcmsd@users.noreply.github.com>
2023-07-21 12:57:09 -0400

    Update TestDataUtils.java

--- dotCMS/src/integration-test/java/com/dotcms/datagen/TestDataUtils.java
+++ dotCMS/src/integration-test/java/com/dotcms/datagen/TestDataUtils.java
@@ -1009,7 +1009,7 @@ public class TestDataUtils {
                     .setProperty("contentHost", site)
                     .setProperty("title", "genericContent")
                     .setProperty("author", "systemUser")
-                    .setProperty("body", "{\"type\":\"doc\",\"attrs\":{\"chartCount\":20,\"wordCount\":3,\"readingTime\":1},\"content\":[{\"type\":\"paragraph\",\"attrs\":{\"textAlign\":\"left\"},\"content\":[{\"type\":\"text\",\"text\":\"Generic Content Body\"}]}]}");
+                    .setProperty("body", "{\"type\":\"doc\",\"attrs\":{\"charCount\":20,\"wordCount\":3,\"readingTime\":1},\"content\":[{\"type\":\"paragraph\",\"attrs\":{\"textAlign\":\"left\"},\"content\":[{\"type\":\"text\",\"text\":\"Generic Content Body\"}]}]}");
 
             if (persist) {
                 return contentletDataGen.nextPersisted();
