36883110ecf61d3900ea4ce54c29325639614460
danielmdob <danmdob@gmail.com>
2023-07-24 12:26:26 -0600

    add autosave on templates portlet

--- core-web/apps/dotcms-ui/src/app/portlets/dot-templates/dot-template-create-edit/dot-template-builder/dot-template-builder.component.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-templates/dot-template-create-edit/dot-template-builder/dot-template-builder.component.ts
@@ -1,6 +1,16 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Subject } from 'rxjs';
 
-import { map } from 'rxjs/operators';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnDestroy,
+    OnInit,
+    Output,
+    ViewChild
+} from '@angular/core';
+
+import { debounceTime, map, takeUntil } from 'rxjs/operators';
 
 import { IframeComponent } from '@components/_common/iframe/iframe-component';
 import { DotPropertiesService } from '@dotcms/data-access';
@@ -8,12 +18,14 @@ import { FeaturedFlags } from '@dotcms/dotcms-models';
 
 import { DotTemplateItem } from '../store/dot-template.store';
 
+export const DEBOUNCE_TIME = 5000;
+
 @Component({
     selector: 'dot-template-builder',
     templateUrl: './dot-template-builder.component.html',
     styleUrls: ['./dot-template-builder.component.scss']
 })
-export class DotTemplateBuilderComponent implements OnInit {
+export class DotTemplateBuilderComponent implements OnInit, OnDestroy {
     @Input() item: DotTemplateItem;
     @Input() didTemplateChanged: boolean;
     @Output() saveAndPublish = new EventEmitter<DotTemplateItem>();
@@ -28,12 +40,20 @@ export class DotTemplateBuilderComponent implements OnInit {
     featureFlagIsOn$ = this.propertiesService
         .getKey(this.featureFlag)
         .pipe(map((result) => result && result === 'true'));
+    templateUpdate$ = new Subject<DotTemplateItem>();
+    destroy$: Subject<boolean> = new Subject<boolean>();
 
     constructor(private propertiesService: DotPropertiesService) {}
 
     ngOnInit() {
         this.permissionsUrl = `/html/templates/permissions.jsp?templateId=${this.item.identifier}&popup=true`;
         this.historyUrl = `/html/templates/push_history.jsp?templateId=${this.item.identifier}&popup=true`;
+        this.saveTemplateDebounce();
+    }
+
+    ngOnDestroy() {
+        this.destroy$.next(true);
+        this.destroy$.complete();
     }
 
     /**
@@ -47,5 +67,16 @@ export class DotTemplateBuilderComponent implements OnInit {
         if (this.historyIframe) {
             this.historyIframe.iframeElement.nativeElement.contentWindow.location.reload();
         }
+
+        this.templateUpdate$.next(item);
+    }
+
+    saveTemplateDebounce() {
+        // Approach based on DotEditLayoutComponent, see that component for more info
+        this.templateUpdate$
+            .pipe(debounceTime(DEBOUNCE_TIME), takeUntil(this.destroy$))
+            .subscribe((templateItem) => {
+                this.saveAndPublish.emit(templateItem);
+            });
     }
 }
