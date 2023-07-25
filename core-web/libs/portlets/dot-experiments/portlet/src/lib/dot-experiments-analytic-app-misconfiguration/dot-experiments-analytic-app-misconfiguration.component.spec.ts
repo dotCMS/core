5b1e68ce8488f2509c9a5e69b317f0144612f931
Arcadio Quintero <oidacra@gmail.com>
2023-07-25 09:43:35 -0400

    (#25425) - missing test

--- core-web/libs/portlets/dot-experiments/portlet/src/lib/dot-experiments-analytic-app-misconfiguration/dot-experiments-analytic-app-misconfiguration.component.spec.ts
+++ core-web/libs/portlets/dot-experiments/portlet/src/lib/dot-experiments-analytic-app-misconfiguration/dot-experiments-analytic-app-misconfiguration.component.spec.ts
@@ -1,22 +1,41 @@
-import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
+import { createComponentFactory, mockProvider, Spectator, SpyObject } from '@ngneat/spectator/jest';
 
 import { DotMessageService } from '@dotcms/data-access';
 import { DotEmptyContainerComponent } from '@dotcms/ui';
 
 import { DotExperimentsAnalyticAppMisconfigurationComponent } from './dot-experiments-analytic-app-misconfiguration.component';
+import { DotExperimentsUiHeaderComponent } from '../shared/ui/dot-experiments-header/dot-experiments-ui-header.component';
+import { Router } from '@angular/router';
 
 describe('DotExperimentsAnalyticAppMisconfigurationComponent', () => {
     let spectator: Spectator<DotExperimentsAnalyticAppMisconfigurationComponent>;
+    let router: SpyObject<Router>;
     const createComponent = createComponentFactory({
         component: DotExperimentsAnalyticAppMisconfigurationComponent,
-        providers: [mockProvider(DotMessageService)]
+        providers: [mockProvider(DotMessageService), mockProvider(Router)]
     });
 
     beforeEach(() => {
         spectator = createComponent();
+
+        router = spectator.inject(Router);
     });
 
     it('should DotEmptyContainerComponent exist in the component', () => {
         expect(spectator.query(DotEmptyContainerComponent)).toExist();
+
+        spectator.query(DotEmptyContainerComponent).buttonAction.emit();
+
+        expect(router.navigate).toHaveBeenCalledWith(['/apps/dotAnalytics-config']);
+    });
+
+    it('should DotExperimentsUiHeaderComponent exist in the component', () => {
+        expect(spectator.query(DotExperimentsUiHeaderComponent)).toExist();
+
+        spectator.query(DotExperimentsUiHeaderComponent).goBack.emit();
+
+        expect(router.navigate).toHaveBeenCalledWith(['edit-page/content'], {
+            queryParamsHandling: 'merge'
+        });
     });
 });
