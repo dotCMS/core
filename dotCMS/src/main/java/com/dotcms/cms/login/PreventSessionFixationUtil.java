6b582607a8b39dffee499aaeaa9360e61df192e4
Jose Castro <jose.castro@dotcms.com>
2023-07-31 12:00:09 -0600

    More code changes

--- dotCMS/src/main/java/com/dotcms/cms/login/PreventSessionFixationUtil.java
+++ dotCMS/src/main/java/com/dotcms/cms/login/PreventSessionFixationUtil.java
@@ -1,5 +1,7 @@
 package com.dotcms.cms.login;
 
+import com.dotcms.concurrent.DotConcurrentFactory;
+import com.dotcms.concurrent.DotSubmitter;
 import com.dotmarketing.util.Config;
 import com.dotmarketing.util.Logger;
 import io.vavr.control.Try;
@@ -11,6 +13,11 @@ import java.io.Serializable;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.Future;
 
 /**
  * Prevent the session fixation if the config property "PREVENT_SESSION_FIXATION_ON_LOGIN" is not in false.
@@ -65,16 +72,22 @@ public class PreventSessionFixationUtil {
                     Logger.info(this, "- Attr '" + key + "' = " + value);
                 }
 
-                final Map<String, Object> newSessionMap = Map.copyOf(sessionMap);
+                final DotSubmitter dotSubmitter = DotConcurrentFactory.getInstance()
+                        .getSubmitter("map-copy");
+                final CompletionService<Map<String, Object>> completionService = new ExecutorCompletionService<>(dotSubmitter);
+                completionService.submit(() -> Map.copyOf(sessionMap));
+                final Map<String, Object> newSessionMap = Try.of(() -> completionService.take().get()).getOrElse(new HashMap<>());
+
+                //final Map<String, Object> newSessionMap = Map.copyOf(sessionMap);
 
                 Logger.info(this, "-> New Session params size = " + newSessionMap.size());
 
-                try {
+                /*try {
                     Logger.info(this, "... Sleeping for 3 seconds ...");
                     Thread.sleep(3000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
-                }
+                }*/
 
                 oldSession.invalidate();
 
