286c1831f623f00d3b59c648c544106afe505eb9
Rafael <rjvelazco21@gmail.com>
2023-08-02 14:06:41 -0400

    dev: integrate library in storybook

--- core-web/apps/dotcms-ui/.storybook/main.js
+++ core-web/apps/dotcms-ui/.storybook/main.js
@@ -11,7 +11,8 @@ module.exports = {
         '../src/**/*.stories.mdx',
         '../src/**/*.stories.@(js|jsx|ts|tsx)',
         '../../../libs/template-builder/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-        '../../../libs/block-editor/**/*.stories.@(js|jsx|ts|tsx|mdx)'
+        '../../../libs/block-editor/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+        '../../../libs/dotcms-fields/**/*.stories.@(js|jsx|ts|tsx|mdx)'
     ],
     addons: ['storybook-design-token', '@storybook/addon-essentials', ...rootMain.addons],
     features: {
