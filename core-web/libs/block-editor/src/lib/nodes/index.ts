8116d587f4a5c9c24de260b51181431648c1cbcb
Nikola <nikola.trajkovic@symphony.is>
2023-08-04 16:49:40 +0200

    Remove import path for ai-content-node

--- core-web/libs/block-editor/src/lib/nodes/index.ts
+++ core-web/libs/block-editor/src/lib/nodes/index.ts
@@ -1,5 +1,4 @@
 export * from './contentlet-block/contentlet-block.component';
 export * from './contentlet-block/contentlet-block.node';
-export * from './ai-content/ai-content.node';
 export * from './image-node/image.node';
 export * from './video/video.node';
