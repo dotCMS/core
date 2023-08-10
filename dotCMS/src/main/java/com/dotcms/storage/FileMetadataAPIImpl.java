8f5f978163112fe1fd3e4189e50e59c94e5798b6
nollymar <nollymarlonga@Nollymars-MacBook-Pro-2.local>
2023-08-09 19:07:08 -0500

    #25618 Fixing tests

--- dotCMS/src/main/java/com/dotcms/storage/FileMetadataAPIImpl.java
+++ dotCMS/src/main/java/com/dotcms/storage/FileMetadataAPIImpl.java
@@ -442,13 +442,12 @@ public class FileMetadataAPIImpl implements FileMetadataAPI {
      * @return
      */
     private Map<String, Serializable> filterNonBasicMetadataFields(final Map<String, Serializable> originalMap) {
-        final Set<String> basicMetadataConfig = Arrays.stream(
-                        Config.getStringProperty(BASIC_METADATA_OVERRIDE_KEYS, "").split(",")).map(String::trim)
-                .collect(Collectors.toSet());
+        final Set<String> basicMetadataKeySet = Arrays.stream(
+                        Config.getStringProperty(BASIC_METADATA_OVERRIDE_KEYS,
+                                String.join(",", BasicMetadataFields.keyMap().keySet())).split(","))
+                .map(String::trim).collect(Collectors.toSet());
 
-        final Set<String> basicMetadataFieldsSet = !basicMetadataConfig.isEmpty()? basicMetadataConfig: BasicMetadataFields.keyMap().keySet();
-
-        return originalMap.entrySet().stream().filter(entry -> basicMetadataFieldsSet
+        return originalMap.entrySet().stream().filter(entry -> basicMetadataKeySet
                 .contains(entry.getKey()) || entry.getKey().startsWith(Metadata.CUSTOM_PROP_PREFIX) ).collect(
                 Collectors.toMap(Entry::getKey, Entry::getValue));
     }
