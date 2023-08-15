4a2fc10dcd26a5f8530f57d83ad2903bcf550fbc
Rafael <rjvelazco21@gmail.com>
2023-08-15 14:24:49 -0400

    dev: add command to generate contenttype-fields.js

--- core-web/apps/contenttype-fields-builder/src/app/app.component.ts
+++ core-web/apps/contenttype-fields-builder/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'dotcms-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
     title = 'contenttype-fields-builder';
