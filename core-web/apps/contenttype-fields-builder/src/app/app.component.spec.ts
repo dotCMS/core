184900defe6cc87734439571583f761956de343e
Rafael <rjvelazco21@gmail.com>
2023-08-15 17:26:52 -0400

    feedback: fix tests

--- core-web/apps/contenttype-fields-builder/src/app/app.component.spec.ts
+++ core-web/apps/contenttype-fields-builder/src/app/app.component.spec.ts
@@ -20,13 +20,4 @@ describe('AppComponent', () => {
         const app = fixture.componentInstance;
         expect(app.title).toEqual('contenttype-fields-builder');
     });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('h1')?.textContent).toContain(
-            'Welcome contenttype-fields-builder'
-        );
-    });
 });
