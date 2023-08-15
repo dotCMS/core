aa4d0a15666bd0438c3dd7385fa2e423896e9752
Rafael <rjvelazco21@gmail.com>
2023-08-15 15:14:50 -0400

    feedback: fix lint

--- core-web/apps/contenttype-fields-builder/src/app/app.component.spec.ts
+++ core-web/apps/contenttype-fields-builder/src/app/app.component.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 describe('AppComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [AppComponent, NxWelcomeComponent]
+            declarations: [AppComponent]
         }).compileComponents();
     });
 
