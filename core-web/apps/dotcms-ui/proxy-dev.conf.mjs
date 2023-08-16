f9f60b5c0520000c210fe286ca7db2132bf573da
Arcadio Quintero <oidacra@gmail.com>
2023-08-16 11:18:09 -0500

    #25693 modify proxy files to run locally angular with localhost

---
+++ core-web/apps/dotcms-ui/proxy-dev.conf.mjs
@@ -0,0 +1,19 @@
+export default [
+    {
+        context: [
+            '/api',
+            '/c/portal',
+            '/html',
+            '/dwr',
+            '/dA',
+            '/dotcms-webcomponents',
+            '/DotAjaxDirector',
+            '/contentAsset',
+            '/application',
+            '/assets/seo/page-tools.json'
+        ],
+        target: 'http://localhost:8080',
+        secure: false,
+        logLevel: 'debug'
+    }
+];
