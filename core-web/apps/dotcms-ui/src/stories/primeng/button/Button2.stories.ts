c954ec60b7a8de96abd3848a2e4d08a5f74894b0
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-08-23 14:45:39 -0600

    Add controls to primeng button

---
+++ core-web/apps/dotcms-ui/src/stories/primeng/button/Button2.stories.ts
@@ -0,0 +1,35 @@
+import { moduleMetadata } from '@storybook/angular';
+import { Meta, Story } from '@storybook/angular/types-6-0';
+
+import { ButtonModule } from 'primeng/button';
+
+import { MainTemplate, IconOnlyTemplate } from './templates';
+
+export default {
+    title: 'PrimeNG/Button/Button2',
+    decorators: [
+        moduleMetadata({
+            imports: [ButtonModule]
+        })
+    ],
+    parameters: {
+        layout: 'centered',
+        docs: {
+            description: {
+                component: 'All the buttons, more information: https://primeng.org/button'
+            }
+        }
+    }
+} as Meta;
+
+export const Main: Story = () => {
+    return {
+        template: MainTemplate
+    };
+};
+
+export const IconOnly: Story = () => {
+    return {
+        template: IconOnlyTemplate
+    };
+};
