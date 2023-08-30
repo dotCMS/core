3248f9a3aba39e8ceb7e90ded6387f6787496d21
AndreyDotcms <127987858+AndreyDotcms@users.noreply.github.com>
2023-08-30 10:54:04 -0600

    dotCMS/core#25127 adding test to main suites

--- dotCMS/src/integration-test/java/com/dotcms/MainSuite.java
+++ dotCMS/src/integration-test/java/com/dotcms/MainSuite.java
@@ -60,6 +60,7 @@ import com.dotcms.junit.MainBaseSuite;
 import com.dotcms.mail.MailAPIImplTest;
 import com.dotcms.mock.request.CachedParameterDecoratorTest;
 import com.dotcms.publisher.bundle.business.BundleAPITest;
+import com.dotcms.publisher.bundle.business.BundleFactoryImplTest;
 import com.dotcms.publisher.bundle.business.BundleFactoryTest;
 import com.dotcms.publisher.business.PublishAuditAPITest;
 import com.dotcms.publisher.business.PublishQueueElementTransformerTest;
@@ -653,7 +654,8 @@ import org.junit.runners.Suite.SuiteClasses;
         AssetPathResolverImplIntegrationTest.class,
         WebAssetHelperIntegrationTest.class,
         Task230701AddHashIndicesToWorkflowTablesTest.class,
-        Task230713IncreaseDisabledWysiwygColumnSizeTest.class
+        Task230713IncreaseDisabledWysiwygColumnSizeTest.class,
+        BundleFactoryImplTest.class,
 })
 
 public class MainSuite {
