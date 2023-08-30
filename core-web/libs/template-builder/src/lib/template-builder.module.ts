86dd5d19b9c7f401f08095e7fd346505123bd592
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-08-30 16:48:04 -0600

    Fix #25926 Reimplement the autocomplete component (#25935)
    * Reimplement the autocomplete component
    
    * dev (add styles autocomplete): enhance functionality
    
    * Fix filtering and styling.
    
    * Fix the filtering and language messages
    
    * Add tests for JsonClassesService
    
    * Test are not running
    
    * fix (add styles dialog): now tests are running
    
    * Fix tests
    
    * Update mocks
    
    ---------
    
    Co-authored-by: Jalinson Diaz <zjaaaldev@gmail.com>

--- core-web/libs/template-builder/src/lib/template-builder.module.ts
+++ core-web/libs/template-builder/src/lib/template-builder.module.ts
@@ -8,7 +8,6 @@ import { ToolbarModule } from 'primeng/toolbar';
 import { DotContainersService } from '@dotcms/data-access';
 import { DotMessagePipe } from '@dotcms/ui';
 
-import { DotAddStyleClassesDialogStore } from './components/template-builder/components/add-style-classes-dialog/store/add-style-classes-dialog.store';
 import { DotLayoutPropertiesComponent } from './components/template-builder/components/dot-layout-properties/dot-layout-properties.component';
 import { TemplateBuilderComponentsModule } from './components/template-builder/components/template-builder-components.module';
 import { TemplateBuilderComponent } from './components/template-builder/template-builder.component';
@@ -27,12 +26,7 @@ import { TemplateBuilderComponent } from './components/template-builder/template
         TemplateBuilderComponentsModule
     ],
     declarations: [TemplateBuilderComponent],
-    providers: [
-        DialogService,
-        DynamicDialogRef,
-        DotAddStyleClassesDialogStore,
-        DotContainersService
-    ],
+    providers: [DialogService, DynamicDialogRef, DotContainersService],
     exports: [TemplateBuilderComponent, DotLayoutPropertiesComponent]
 })
 export class TemplateBuilderModule {}
