ba29c070ca39d059cea38b9b53b372565b7486cb
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-08-30 12:20:46 -0600

    Add tests for JsonClassesService

--- core-web/libs/template-builder/src/lib/components/template-builder/components/add-style-classes-dialog/services/json-classes.service.spec.ts
+++ core-web/libs/template-builder/src/lib/components/template-builder/components/add-style-classes-dialog/services/json-classes.service.spec.ts
@@ -1,16 +1,38 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 
-import { JsonClassesService } from './json-classes.service';
+import { JsonClassesService, STYLE_CLASSES_FILE_URL } from './json-classes.service';
 
 describe('JsonClassesService', () => {
+    let injector: TestBed;
     let service: JsonClassesService;
+    let httpMock: HttpTestingController;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(JsonClassesService);
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [JsonClassesService]
+        });
+
+        injector = getTestBed();
+        service = injector.inject(JsonClassesService);
+        httpMock = injector.inject(HttpTestingController);
     });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
+    afterEach(() => {
+        httpMock.verify();
+    });
+
+    it('should return an Observable with classes', () => {
+        const expectedClasses = { classes: ['class1', 'class2', 'class3'] };
+        // httpClientSpy.get.and.returnValue(of(expectedClasses));
+
+        service.getClasses().subscribe((classes) => {
+            expect(classes).toEqual(expectedClasses);
+        });
+
+        const req = httpMock.expectOne(STYLE_CLASSES_FILE_URL);
+        expect(req.request.method).toBe('GET');
+        req.flush(expectedClasses);
     });
 });
