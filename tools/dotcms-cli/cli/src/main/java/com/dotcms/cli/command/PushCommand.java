91b6f2fda8ea22e130a3db02d7638ace09f41837
Jonathan Gamba <jonathan.gamba@dotcms.com>
2023-09-07 13:15:51 -0600

    #25822 Refactor CLI push command for modularity and readability
    Enhanced push command implementation by extracting push logic into specific subcommands and a shared mixin, improving modularity and readability of the code. Introduced PushMixin for common options and DotPush interface for specific push commands, and added a new FilesPushMixin for file push operations. This refactor aims to streamline the push process and make it easier to modify or extend in the future.

---
+++ tools/dotcms-cli/cli/src/main/java/com/dotcms/cli/command/PushCommand.java
@@ -0,0 +1,77 @@
+package com.dotcms.cli.command;
+
+import com.dotcms.cli.common.HelpOptionMixin;
+import com.dotcms.cli.common.OutputOptionMixin;
+import com.dotcms.cli.common.PushMixin;
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import javax.enterprise.context.control.ActivateRequestContext;
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.spi.CDI;
+import picocli.CommandLine;
+
+@ActivateRequestContext
+@CommandLine.Command(
+        name = PushCommand.NAME,
+        header = "@|bold,blue dotCMS global push|@",
+        description = {
+                " This command push Sites, Content Types, Languages and Files to the server.",
+                "" // empty string here so we can have a new line
+        }
+)
+public class PushCommand implements Callable<Integer>, DotCommand {
+
+    static final String NAME = "push";
+
+    @CommandLine.Mixin(name = "output")
+    OutputOptionMixin output;
+
+    @CommandLine.Mixin
+    HelpOptionMixin helpOption;
+
+    @CommandLine.Mixin
+    PushMixin pushMixin;
+
+    @CommandLine.Spec
+    CommandLine.Model.CommandSpec spec;
+
+    // Find the instances of all push subcommands
+    Instance<DotPush> pushCommands = CDI.current().select(DotPush.class);
+
+    @Override
+    public Integer call() throws Exception {
+
+        // Checking for unmatched arguments
+        output.throwIfUnmatchedArguments(spec.commandLine());
+
+        // Preparing the list of arguments to be passed to the subcommands
+        var args = new ArrayList<>(spec.commandLine().getParseResult().expandedArgs());
+        args.add("--noValidateUnmatchedArguments");
+
+        // Process each subcommand
+        for (var subCommand : pushCommands) {
+
+            var cmdLine = new CommandLine(subCommand);
+            CustomConfigurationUtil.getInstance().customize(cmdLine);
+
+            // Use execute to parse the parameters with the subcommand
+            int exitCode = cmdLine.execute(args.toArray(new String[0]));
+            if (exitCode != CommandLine.ExitCode.OK) {
+                return exitCode;
+            }
+        }
+
+        return CommandLine.ExitCode.OK;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public OutputOptionMixin getOutput() {
+        return output;
+    }
+
+}
