1d69d01bb784865395ab42f80ba958e757fca438
erickgonzalez <erick.gonzalez@dotcms.com>
2023-09-12 12:03:39 -0600

    fix MSSQL upgrade issues

--- dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task230426AlterVarcharLengthOfLockedByCol.java
+++ dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task230426AlterVarcharLengthOfLockedByCol.java
@@ -16,10 +16,14 @@ public class Task230426AlterVarcharLengthOfLockedByCol implements StartupTask {
     private void alterTables() throws SQLException {
         final DotConnect dc = new DotConnect();
         if (DbConnectionFactory.isMsSql()) {
-            dc.executeStatement("alter table contentlet_version_info alter column locked_by varchar (100)");
-            dc.executeStatement("alter table container_version_info alter column locked_by varchar (100)");
-            dc.executeStatement("alter table template_version_info alter column locked_by varchar (100)");
-            dc.executeStatement("alter table link_version_info alter column locked_by varchar (100)");
+            dc.executeStatement("drop index cvi_locked_by_index on contentlet_version_info");
+            dc.executeStatement("alter table contentlet_version_info drop CONSTRAINT FK_con_ver_lockedby");
+            dc.executeStatement("alter table contentlet_version_info alter column locked_by nvarchar (100)");
+            dc.executeStatement("create index cvi_locked_by_index on contentlet_version_info (locked_by)");
+            dc.executeStatement("alter table contentlet_version_info add constraint FK_con_ver_lockedby foreign key (locked_by) references user_(userid)");
+            dc.executeStatement("alter table container_version_info alter column locked_by nvarchar (100)");
+            dc.executeStatement("alter table template_version_info alter column locked_by nvarchar (100)");
+            dc.executeStatement("alter table link_version_info alter column locked_by nvarchar (100)");
         }else {
             dc.executeStatement("alter table contentlet_version_info alter column locked_by type varchar (100)");
             dc.executeStatement("alter table container_version_info alter column locked_by type varchar (100)");
@@ -34,6 +38,11 @@ public class Task230426AlterVarcharLengthOfLockedByCol implements StartupTask {
 
     @Override
     public void executeUpgrade() throws DotDataException, DotRuntimeException {
+        try {
+            DbConnectionFactory.getConnection().setAutoCommit(true);
+        } catch (SQLException e) {
+            throw new DotDataException(e.getMessage(), e);
+        }
         try {
             alterTables();
         } catch (SQLException e) {
