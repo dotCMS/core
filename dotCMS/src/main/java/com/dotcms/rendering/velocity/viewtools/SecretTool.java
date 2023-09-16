8e4f8c69b989fa162d02aaaad0ed29a9abcc0afe
Victor Alfaro <victor.alfaro@dotcms.com>
2023-09-15 20:34:10 -0600

    Revert "feat(core) Fix #25587 Add a role extra layer constraint into the secrets view tool"
    This reverts commit e19679cfdbe4f7b6afc3b91eccfc0e49977e6c2f.

--- dotCMS/src/main/java/com/dotcms/rendering/velocity/viewtools/SecretTool.java
+++ dotCMS/src/main/java/com/dotcms/rendering/velocity/viewtools/SecretTool.java
@@ -3,21 +3,9 @@ package com.dotcms.rendering.velocity.viewtools;
 import com.dotcms.api.web.HttpServletRequestThreadLocal;
 import com.dotcms.rendering.velocity.viewtools.secrets.DotVelocitySecretAppConfig;
 import com.dotmarketing.business.APILocator;
-import com.dotmarketing.business.Role;
-import com.dotmarketing.business.UserAPI;
-import com.dotmarketing.business.web.WebAPILocator;
-import com.dotmarketing.portlets.contentlet.model.Contentlet;
-import com.dotmarketing.util.Config;
-import com.dotmarketing.util.Logger;
-import com.dotmarketing.util.UtilMethods;
-import com.liferay.portal.model.User;
-import org.apache.velocity.context.Context;
-import org.apache.velocity.context.InternalContextAdapterImpl;
-import org.apache.velocity.tools.view.context.ViewContext;
 import org.apache.velocity.tools.view.tools.ViewTool;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
 import java.util.Optional;
 
 /**
@@ -27,16 +15,8 @@ import java.util.Optional;
  */
 public class SecretTool implements ViewTool {
 
-    private InternalContextAdapterImpl internalContextAdapter;
-    private Context context;
-	private HttpServletRequest request;
-
 	@Override
-	public void init(final Object initData) {
-
-        final ViewContext context = (ViewContext) initData;
-		this.request = context.getRequest();
-        this.context = context.getVelocityContext();
+	public void init(Object initData) {
 	}
 
 	/**
@@ -45,8 +25,6 @@ public class SecretTool implements ViewTool {
 	 * @return Object
 	 */
 	public Object get(final String key) {
-
-		canUserEvaluate();
 
 		final HttpServletRequest request = HttpServletRequestThreadLocal.INSTANCE.getRequest();
 		final Optional<DotVelocitySecretAppConfig> config = DotVelocitySecretAppConfig.config(request);
@@ -73,14 +51,12 @@ public class SecretTool implements ViewTool {
 	public Object getSystemSecret (final String key,
 								   final Object defaultValue) {
 
-		canUserEvaluate();
 		final Optional<DotVelocitySecretAppConfig> config = DotVelocitySecretAppConfig.config(APILocator.systemHost());
 		return config.isPresent()? config.get().getStringOrNull(key, null!= defaultValue? defaultValue.toString():null) : defaultValue;
 	}
 
 	public char[] getCharArray(final String key) {
 
-		canUserEvaluate();
 		final HttpServletRequest request = HttpServletRequestThreadLocal.INSTANCE.getRequest();
 		final Optional<DotVelocitySecretAppConfig> config = DotVelocitySecretAppConfig.config(request);
 		return config.isPresent()? config.get().getCharArrayOrNull(key) : null;
@@ -94,58 +70,7 @@ public class SecretTool implements ViewTool {
 	public char[] getCharArraySystemSecret (final String key,
 								   final char[] defaultValue) {
 
-		canUserEvaluate();
 		final Optional<DotVelocitySecretAppConfig> config = DotVelocitySecretAppConfig.config(APILocator.systemHost());
 		return config.isPresent()? config.get().getCharArrayOrNull(key, defaultValue) : defaultValue;
 	}
-
-    private static final boolean ENABLE_SCRIPTING = Config.getBooleanProperty("ENABLE_SCRIPTING", false);
-
-    /**
-     * Test 2 things.
-	 * 1) see if the user has the scripting role
-	 * 2) otherwise check if the last modified user has the scripting role
-     * @return boolean
-     */
-    protected void canUserEvaluate() {
-
-        if(!ENABLE_SCRIPTING) {
-
-            Logger.warn(this.getClass(), "Scripting called and ENABLE_SCRIPTING set to false");
-			throw new SecurityException("External scripting is disabled in your dotcms instance.");
-        }
-
-        try {
-
-			boolean hasScriptingRole = false;
-			final Role scripting = APILocator.getRoleAPI().loadRoleByKey(Role.SCRIPTING_DEVELOPER);
-
-			this.internalContextAdapter    = new InternalContextAdapterImpl(context);
-			final String fieldResourceName = this.internalContextAdapter.getCurrentTemplateName();
-			if (UtilMethods.isSet(fieldResourceName)) {
-				final String contentletFileAssetInode = fieldResourceName.substring(fieldResourceName.indexOf("/") + 1, fieldResourceName.indexOf("_"));
-				final Contentlet contentlet = APILocator.getContentletAPI().find(contentletFileAssetInode, APILocator.systemUser(), true);
-				final User lastModifiedUser = APILocator.getUserAPI().loadUserById(contentlet.getModUser(), APILocator.systemUser(), true);
-				hasScriptingRole = APILocator.getRoleAPI().doesUserHaveRole(lastModifiedUser, scripting);
-			}
-
-			if (!hasScriptingRole) {
-				final User user = WebAPILocator.getUserWebAPI().getUser(this.request);
-				// try with the current user
-				if (null != user) {
-
-					hasScriptingRole = APILocator.getRoleAPI().doesUserHaveRole(user, scripting);
-				}
-			}
-
-			if (!hasScriptingRole) {
-
-				throw new SecurityException("External scripting is disabled in your dotcms instance.");
-			}
-        } catch(Exception e) {
-
-            Logger.warn(this.getClass(), "Scripting called with error" + e);
-			throw new SecurityException("External scripting is disabled in your dotcms instance.", e);
-        }
-    } // canUserEvaluate.
 }
