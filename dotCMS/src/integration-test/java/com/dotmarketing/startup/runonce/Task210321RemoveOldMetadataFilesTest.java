333456a3c0208d04c80f6130e0026b71884d2e5c
Jose Castro <jose.castro@dotcms.com>
2023-09-18 09:55:53 -0600

    Fixing Integration Test.

--- dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task210321RemoveOldMetadataFilesTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task210321RemoveOldMetadataFilesTest.java
@@ -1,33 +1,25 @@
 package com.dotmarketing.startup.runonce;
 
-import static com.dotcms.datagen.TestDataUtils.getFileAssetContent;
-import static com.dotcms.datagen.TestDataUtils.getMultipleBinariesContent;
-import static com.dotcms.storage.StoragePersistenceProvider.DEFAULT_STORAGE_TYPE;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import com.dotcms.contenttype.model.field.BinaryField;
 import com.dotcms.datagen.TestDataUtils.TestFile;
 import com.dotcms.storage.StoragePersistenceProvider;
 import com.dotcms.storage.StorageType;
-import com.dotcms.tika.TikaUtils;
 import com.dotcms.util.IntegrationTestInitService;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.exception.DotDataException;
-import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.util.Config;
 import io.vavr.Tuple2;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.concurrent.ExecutionException;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class Task210321RemoveOldMetadataFilesTest {
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
+import static com.dotcms.datagen.TestDataUtils.getFileAssetContent;
+import static com.dotcms.storage.StoragePersistenceProvider.DEFAULT_STORAGE_TYPE;
+import static org.junit.Assert.assertTrue;
+
+public class Task210321RemoveOldMetadataFilesTest {
 
     @BeforeClass
     public static void prepare() throws Exception {
@@ -36,26 +28,28 @@ public class Task210321RemoveOldMetadataFilesTest {
     }
 
     /**
-     * Given scenario: Recreate the asset + metadata structure under a temp ASSET_REAL_PATH
-     * Expected scenario: since we're passing a file asset we expect just one occurrence of the fileasset-metadata.json
-     * and one occurrence of /metaData/content
-     * @throws DotDataException
-     * @throws IOException
-     * @throws ExecutionException
-     * @throws InterruptedException
-     * @throws DotSecurityException
+     * <ul>
+     *     <li><b>Method to test: </b>{@link Task210321RemoveOldMetadataFiles#executeUpgrade()}</li>
+     *     <li><b>Given Scenario: </b>Recreate the asset + metadata structure under the {@code
+     *     ASSET_REAL_PATH} folder.</li>
+     *     <li><b>Expected Result: </b>Since we're passing a file asset, we expect at least one
+     *     occurrence of the {@code fileasset-metadata.json}.</li>
+     * </ul>
+     *
+     * @throws DotDataException     An error occurred when generating the file's metadata.
+     * @throws IOException          An error occurred when generating the file's metadata.
+     * @throws ExecutionException   An error occurred when retrieving the result of the Future
+     *                              object.
+     * @throws InterruptedException An error occurred when retrieving the result of the Future
+     *                              object.
      */
     @Test
     public void Test_Upgrade_Task()
-            throws DotDataException, IOException, ExecutionException, InterruptedException, DotSecurityException {
-        final String assetRealPath = Config.getStringProperty("ASSET_REAL_PATH", null);
+            throws DotDataException, IOException, ExecutionException, InterruptedException {
         final String stringProperty = Config.getStringProperty(DEFAULT_STORAGE_TYPE, null);
         StoragePersistenceProvider.INSTANCE.get().forceInitialize();
         try {
-            final Path temp = Files.createTempDirectory(null);
-            Config.setProperty("ASSET_REAL_PATH", temp.toString());
             Config.setProperty(DEFAULT_STORAGE_TYPE, StorageType.FILE_SYSTEM.name());
-
             final long langId = APILocator.getLanguageAPI().getDefaultLanguage().getId();
             //Recreate the metadata structure
             final Contentlet contentlet = getFileAssetContent(true, langId, TestFile.PDF);
@@ -66,20 +60,15 @@ public class Task210321RemoveOldMetadataFilesTest {
             assertTrue(task.forceRun());
             task.executeUpgrade();
             final Tuple2<Integer, Integer> tuple = task.getFuture().get();
-            //1 fileasset-metadata.json
+            // At least 1 fileasset-metadata.json
             assertTrue("The Task210321RemoveOldMetadataFiles should've deleted at least one file"
                     , tuple._1 >= 1);
-            //1 metaData/content
-            assertEquals(1, tuple._2.intValue());
-
+            // Zero metaData/content
+            assertTrue("There should NOT be any 'metaData/content' folders anymore", 0 == tuple._2);
         } finally {
             Config.setProperty(DEFAULT_STORAGE_TYPE, stringProperty);
-            Config.setProperty("ASSET_REAL_PATH", assetRealPath);
             StoragePersistenceProvider.INSTANCE.get().forceInitialize();
         }
-
-
     }
-
 
 }
