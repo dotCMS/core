f494b743672561f4fb3c6a6e8396383ad2c71db8
jdotcms <jonathan.sanchez@dotcms.com>
2023-09-22 00:51:58 -0600

    #26227 adding js engine + js script actionlet + js resource + js servlet. Still missing the graal real work, but most of the dot-boilerplate is kinda done

--- dotCMS/src/main/java/com/dotcms/rendering/js/JsScriptEngine.java
+++ dotCMS/src/main/java/com/dotcms/rendering/js/JsScriptEngine.java
@@ -2,20 +2,25 @@ package com.dotcms.rendering.js;
 
 import com.dotcms.api.vtl.model.DotJSON;
 import com.dotcms.rendering.engine.ScriptEngine;
-import com.dotcms.rendering.velocity.util.VelocityUtil;
-import com.dotcms.rendering.velocity.viewtools.exception.DotToolException;
+
 import com.dotcms.util.CollectionsUtils;
 import com.dotmarketing.exception.DotRuntimeException;
 import com.dotmarketing.util.Logger;
 import com.liferay.util.StringPool;
-import org.apache.velocity.context.Context;
-import org.apache.velocity.exception.MethodInvocationException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.Reader;
 import java.io.StringWriter;
 import java.util.Map;
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.Invocable;
+import java.io.IOException;
+import org.graalvm.polyglot.Source;
 
 /**
  * Js script engine implementation
@@ -28,25 +33,22 @@ public class JsScriptEngine implements ScriptEngine {
                        final Reader scriptReader,
                        final Map<String, Object> contextParams) {
         // todo: this should be implemented by graal js
-        final Context context = null == request || null == response?
-                com.dotmarketing.util.VelocityUtil.getBasicContext():
-                VelocityUtil.getInstance().getContext(request, response);
 
-        contextParams.forEach(context::put);
-        context.put("dotJSON", new DotJSON());
+        final DotJSON dotJSON = new DotJSON();
+        try (Context context = Context.create()) {
 
-        final StringWriter evalResult = new StringWriter();
-
-        try {
-
-            VelocityUtil.getEngine().evaluate(context, evalResult, StringPool.BLANK, scriptReader);
-        } catch(MethodInvocationException e) {
-            if(e.getCause() instanceof DotToolException) {
-                Logger.error(this,"Error evaluating velocity: " + (e.getCause()).getCause().getMessage());
-                throw new DotRuntimeException(e.getCause());
+            final Source source = Source.newBuilder("js", scriptReader,
+                            "sample.js") // todo: we do not have this name
+                    .build();
+            final Value bindings = context.getBindings("js");
+            // todo: put here the view tools and everything else
+            for (final Map.Entry<String, Object> entry : contextParams.entrySet()) {
+                bindings.putMember(entry.getKey(), entry.getValue());
             }
+            bindings.putMember("dotJSON", dotJSON);
+            final Value eval = context.eval(source).build();
+            final Value result = eval.execute();
+            return CollectionsUtils.map("output", result.asString(), "dotJSON", dotJSON);
         }
-
-        return CollectionsUtils.map("output", evalResult, "dotJSON", context.get("dotJSON"));
     }
 }
