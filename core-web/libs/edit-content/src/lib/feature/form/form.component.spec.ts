d0a956e9570d96612d6954be21c32bb7e8a569dd
Jalinson Diaz <zjaaaldev@gmail.com>
2023-09-22 18:14:09 -0300

    fix (form component test): missing import

--- core-web/libs/edit-content/src/lib/feature/form/form.component.spec.ts
+++ core-web/libs/edit-content/src/lib/feature/form/form.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { FormComponent } from './form.component';
 
@@ -8,7 +9,7 @@ describe('FormComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [FormComponent]
+            imports: [FormComponent, RouterTestingModule]
         }).compileComponents();
 
         fixture = TestBed.createComponent(FormComponent);
