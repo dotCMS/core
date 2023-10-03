4d81071b2ac35b01899b410ef04f256955b5472c
daniel.solis <daniel.solis@dotcms.com>
2023-10-03 15:37:18 -0600

    fix: add publish and unpublish date (#25233)

--- dotCMS/src/integration-test/java/com/dotmarketing/portlets/contentlet/business/ContentletAPITest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/portlets/contentlet/business/ContentletAPITest.java
@@ -3317,6 +3317,13 @@ public class ContentletAPITest extends ContentletBaseTest {
         //Validations
         assertNotNull(isLive);
         assertFalse(isLive);
+
+        final Optional<ContentletVersionInfo> versionInfo = APILocator.getVersionableAPI()
+                .getContentletVersionInfo(contentlet.getIdentifier(),
+                        contentlet.getLanguageId(), contentlet.getVariantId());
+        final Date unpublishDate = versionInfo.map(ContentletVersionInfo::getUnpublishDate).orElse(null);
+        assertNotNull(unpublishDate);
+
     }
 
     /**
@@ -8341,7 +8348,12 @@ public class ContentletAPITest extends ContentletBaseTest {
     /**
      * Method to test: {@link ESContentletAPIImpl#publish(Contentlet, User, boolean)}
      * When: You have live and not live contentlets
+<<<<<<< HEAD
      * Should: Update publish_date when contentlet is published
+=======
+     * Should: Update publish_date when contentlet is published,
+     * update unpublish_date when contentlet is unpublished
+>>>>>>> dca2283c43 (fix: add publish and unpublish date (#25233))
      */
     @Test
     public void getMostRecentPublishedContent() throws Exception {
