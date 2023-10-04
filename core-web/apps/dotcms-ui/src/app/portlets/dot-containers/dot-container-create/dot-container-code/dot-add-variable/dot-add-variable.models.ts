641d005ce6dfc0a381d2c93b4588af1c98c61cb7
Jalinson Diaz <zjaaaldev@gmail.com>
2023-10-04 17:40:33 -0300

    dev (dot add variable models): filter fieldTypes that does not appear on legacy portlet

--- core-web/apps/dotcms-ui/src/app/portlets/dot-containers/dot-container-create/dot-container-code/dot-add-variable/dot-add-variable.models.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-containers/dot-container-create/dot-container-code/dot-add-variable/dot-add-variable.models.ts
@@ -1,7 +1,14 @@
 // You can add here the field types you want to filter
 export enum FilteredFieldTypes {
     Column = 'Column',
-    Row = 'Row'
+    Row = 'Row',
+    Category = 'Category',
+    ConstantField = 'Constant-Field',
+    HiddenField = 'Hidden-Field',
+    JsonField = 'Json-Field',
+    KeyValue = 'Key-Value',
+    PermissionsField = 'Permissions-Field',
+    Relationship = 'Relationship'
 }
 
 // This is the model we use in the HTML to render the field
@@ -15,5 +22,8 @@ export interface DotFieldContent {
 // Here goes all the fieldTypes that have extra fields
 export enum FieldTypeWithExtraFields {
     IMAGE = 'Image',
-    DEFAULT = 'default'
+    HOST = 'Host-Folder',
+    FILE = 'File',
+    DEFAULT = 'default',
+    BLOCK_EDITOR = 'Story-Block'
 }
