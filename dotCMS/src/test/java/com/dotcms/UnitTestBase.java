ba983f8a5f01a692477a41054221ef9d8cc66f31
Jose Castro <jose.castro@dotcms.com>
2023-10-13 12:43:17 -0600

    Attempting to fix Unit Test run

--- dotCMS/src/test/java/com/dotcms/UnitTestBase.java
+++ dotCMS/src/test/java/com/dotcms/UnitTestBase.java
@@ -1,21 +1,18 @@
 package com.dotcms;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 import com.dotcms.company.CompanyAPI;
-import com.liferay.portal.model.Company;
-import org.junit.BeforeClass;
-
 import com.dotcms.contenttype.business.ContentTypeAPI;
 import com.dotmarketing.business.APILocator;
-import com.dotmarketing.exception.DotDataException;
-import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.util.BaseMessageResources;
 import com.dotmarketing.util.Config;
+import com.liferay.portal.model.Company;
 import com.liferay.portal.model.User;
+import org.junit.BeforeClass;
 
 import java.util.TimeZone;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public abstract class UnitTestBase extends BaseMessageResources {
 
@@ -41,16 +38,16 @@ public abstract class UnitTestBase extends BaseMessageResources {
 	}
 
 	@BeforeClass
-	public static void prepare () throws DotDataException, DotSecurityException, Exception {
-
+	public static void prepare () throws Exception {
 		Config.initializeConfig();
-	//	Config.setProperty("API_LOCATOR_IMPLEMENTATION", MyAPILocator.class.getName());
+		Config.setProperty("API_LOCATOR_IMPLEMENTATION", MyAPILocator.class.getName());
 		Config.setProperty("SYSTEM_EXIT_ON_STARTUP_FAILURE", false);
 
-		APILocator.destroyAndForceInit();
+		MyAPILocator.destroyAndForceInit();
 
 		final Company company = mock(Company.class);
 		when(company.getTimeZone()).thenReturn(TimeZone.getDefault());
 		when(companyAPI.getDefaultCompany()).thenReturn(company);
 	}
+
 }
