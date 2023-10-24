e08c3453a21b9976cbd93f059213f7e7ed2cb6c6
jdotcms <jonathan.sanchez@dotcms.com>
2023-10-24 13:39:43 -0600

    #24307 adding more test

--- dotCMS/src/integration-test/java/com/dotcms/storage/Chainable404StorageCacheTest.java
+++ dotCMS/src/integration-test/java/com/dotcms/storage/Chainable404StorageCacheTest.java
@@ -12,8 +12,22 @@ import static org.junit.Assert.assertTrue;
  * Test for {@link Chainable404StorageCache}
  * @author jsanca
  */
-public class Chainable4040StorageCacheTest extends IntegrationTestBase {
+public class Chainable404StorageCacheTest extends IntegrationTestBase {
 
+    /**
+     * Method to test: This test tries the {@link Chainable404StorageCache}
+     * Given Scenario: Check the basics just to be align with test coverage :)
+     * ExpectedResult: Do some implicit asserts
+     */
+    @Test
+    public void Test_Cache_Basics() {
+
+        final Chainable404StorageCache cache = new Chainable404StorageCache();
+
+        Assert.assertEquals(Chainable404StorageCache.NOT_FOUND_404, "CHAINABLE_404_STORAGE_NOT_FOUND");
+        Assert.assertEquals(cache.getPrimaryGroup(), "Chainable_404_Storage_group");
+        Assert.assertEquals(cache.getGroups(), new String [] {"Chainable_404_Storage_group"});
+    }
     /**
      * Method to test: This test tries the {@link Chainable404StorageCache}
      * Given Scenario: Will add a couple hits to the cache and then test them
@@ -36,11 +50,32 @@ public class Chainable4040StorageCacheTest extends IntegrationTestBase {
         assertTrue("The object on group mem-test and path2 should be 404", cache.is404(groupName, path2));
         assertFalse("The object on group mem-test and path3 should be NOT 404", cache.is404(groupName, path3));
 
+        cache.remove(groupName, path1);
+        assertFalse("The object on group mem-test and path1 should be NOT 404, after remove cache", cache.is404(groupName, path1));
+
         // clear the cache
         cache.clearCache();
         assertFalse("The object on group mem-test and path1 should be NOT 404, after clean cache", cache.is404(groupName, path1));
         assertFalse("The object on group mem-test and path2 should be NOT 404, after clean cache", cache.is404(groupName, path2));
         assertFalse("The object on group mem-test and path3 should be NOT 404, after clean cache", cache.is404(groupName, path3));
+    }
+
+    /**
+     * Method to test: This test tries the {@link Chainable404StorageCache#put404(String, String)}
+     * Given Scenario: Test null params put404
+     * ExpectedResult: The values added as a 404, should be there and the ones that are not should not
+     */
+    @Test
+    public void Test_Cache_null_put404() {
+
+        final Chainable404StorageCache cache = new Chainable404StorageCache();
+
+        // mark path1 and path2 as a 404, path 3 is not
+        try {
+            cache.put404(null, null);
+        } catch (Exception e) {
+            Assert.fail("Should not throw exception put404 null null");
+        }
     }
 
 }
