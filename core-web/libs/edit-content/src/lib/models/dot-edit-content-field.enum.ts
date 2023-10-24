b103c86aeac074a3c97c7caa82dbd7019096c476
KevinDavilaDotCMS <144152756+KevinDavilaDotCMS@users.noreply.github.com>
2023-10-24 15:55:56 -0500

    feat(edit-content): Edit Content: Render Radio and Select Field to the Form #26448
    * Added Select and Radio fields. Added basic tests on both components. Changed formData structure and architecture
    
    * Added validation to set, in case dont have value or defaultValue, the first option as value in select and radio field
    
    * Added enum to DataType
    
    * Added tests. Changed mocks.
    
    * Working on PR Suggestions. Commit before merge
    
    * Added merge with master. Added Typing to forms on Select and Radio button. Added more test cases
    
    * Pair programming select tests
    
    * Changed tests. Changed language of mocks. Moved folders to shared/
    
    * Replaced shared folder with models content
    
    * Changed and added test to RadioButton field
    
    * Changed name on util => function
    
    * Working on DotEditContentForm tests
    
    * Solved test on DotEditContentForm
    
    * Pair programming tests for dot-edit-content
    
    * Added more tests. Reduced html on radio field. Casting values on init form
    
    ---------
    
    Co-authored-by: Freddy Montes <751424+fmontes@users.noreply.github.com>

---
+++ core-web/libs/edit-content/src/lib/models/dot-edit-content-field.enum.ts
@@ -0,0 +1,8 @@
+/**
+ * Represents the selectable data types (Dropdown, Radio button) for a DotCMS content field.
+ */
+export enum DotEditContentFieldSingleSelectableDataType {
+    BOOL = 'BOOL',
+    INTEGER = 'INTEGER',
+    FLOAT = 'FLOAT'
+}
