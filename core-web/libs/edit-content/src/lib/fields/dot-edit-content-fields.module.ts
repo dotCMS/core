64f6bb65a7a633a6cdefa71b0224bb68cc124a96
Jalinson Diaz <zjaaaldev@gmail.com>
2023-10-25 19:18:46 -0300

    feat(edit-content): Edit Content add Calendar related fields #26512
    * dev (dot edit content calendar field): create and adapt to the lib
    
    * dev (edit content): integrate component with the lib and cleanups

--- core-web/libs/edit-content/src/lib/fields/dot-edit-content-fields.module.ts
+++ core-web/libs/edit-content/src/lib/fields/dot-edit-content-fields.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 
+import { DotEditContentCalendarFieldComponent } from './dot-edit-content-calendar-field/dot-edit-content-calendar-field.component';
+import { DotEditContentRadioFieldComponent } from './dot-edit-content-radio-field/dot-edit-content-radio-field.component';
+import { DotEditContentSelectFieldComponent } from './dot-edit-content-select-field/dot-edit-content-select-field.component';
 import { DotEditContentTextAreaComponent } from './dot-edit-content-text-area/dot-edit-content-text-area.component';
 import { DotEditContentTextFieldComponent } from './dot-edit-content-text-field/dot-edit-content-text-field.component';
 
 @NgModule({
     declarations: [],
-    imports: [DotEditContentTextAreaComponent, DotEditContentTextFieldComponent],
-    exports: [DotEditContentTextAreaComponent, DotEditContentTextFieldComponent]
+    imports: [
+        DotEditContentTextAreaComponent,
+        DotEditContentRadioFieldComponent,
+        DotEditContentSelectFieldComponent,
+        DotEditContentTextFieldComponent,
+        DotEditContentCalendarFieldComponent
+    ],
+    exports: [
+        DotEditContentTextAreaComponent,
+        DotEditContentRadioFieldComponent,
+        DotEditContentSelectFieldComponent,
+        DotEditContentTextFieldComponent,
+        DotEditContentCalendarFieldComponent
+    ]
 })
 export class DotEditContentFieldsModule {}
