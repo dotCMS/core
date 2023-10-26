31a880419fbe8b94ab80d26a16501a10de9fc1a1
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-10-26 17:22:19 -0600

    Clean up

--- core-web/libs/edit-content/src/lib/components/dot-edit-content-field/utils.ts
+++ core-web/libs/edit-content/src/lib/components/dot-edit-content-field/utils.ts
@@ -1,11 +1,3 @@
-import { Type } from '@angular/core';
-
-import { DotEditContentCheckboxFieldComponent } from '../../fields/dot-edit-content-checkbox-field/dot-edit-content-checkbox-field.component';
-import { DotEditContentRadioFieldComponent } from '../../fields/dot-edit-content-radio-field/dot-edit-content-radio-field.component';
-import { DotEditContentSelectFieldComponent } from '../../fields/dot-edit-content-select-field/dot-edit-content-select-field.component';
-import { DotEditContentTextAreaComponent } from '../../fields/dot-edit-content-text-area/dot-edit-content-text-area.component';
-import { DotEditContentTextFieldComponent } from '../../fields/dot-edit-content-text-field/dot-edit-content-text-field.component';
-
 // Map to match the field type to component selector
 export enum FIELD_TYPES {
     TEXT = 'Text',
@@ -14,13 +6,3 @@ export enum FIELD_TYPES {
     RADIO = 'Radio',
     CHECKBOX = 'Checkbox'
 }
-
-// This holds the mapping between the field type and the component that should be used to render it.
-export const FIELD_TYPES_COMPONENTS: Record<FIELD_TYPES, Type<unknown>> = {
-    // We had to use unknown because components have different types.
-    [FIELD_TYPES.TEXT]: DotEditContentTextFieldComponent,
-    [FIELD_TYPES.TEXTAREA]: DotEditContentTextAreaComponent,
-    [FIELD_TYPES.SELECT]: DotEditContentSelectFieldComponent,
-    [FIELD_TYPES.RADIO]: DotEditContentRadioFieldComponent,
-    [FIELD_TYPES.CHECKBOX]: DotEditContentCheckboxFieldComponent
-};
