1e4b9d7403a2800d95bbc9c415262f7073507bf4
Manuel Rojas <manuel.rojas.21@gmail.com>
2023-10-31 09:20:20 -0600

    Adding variable

--- core-web/apps/dotcms-ui/src/app/view/components/dot-toolbar/components/dot-toolbar-user/store/dot-toolbar-user.store.ts
+++ core-web/apps/dotcms-ui/src/app/view/components/dot-toolbar/components/dot-toolbar-user/store/dot-toolbar-user.store.ts
@@ -127,7 +127,7 @@ export class DotToolbarUserStore extends ComponentStore<DotToolbarUserState> {
             },
             {
                 id: 'dot-toolbar-user-link-login-as',
-                label: 'Sign In As',
+                label: this.dotMessageService.get('login-as'),
                 icon: 'pi pi-users',
                 visible: !auth.isLoginAs,
                 command: () => this.showLoginAs(true)
@@ -135,8 +135,7 @@ export class DotToolbarUserStore extends ComponentStore<DotToolbarUserState> {
             { separator: true, visible: !auth.isLoginAs },
             {
                 id: 'dot-toolbar-user-link-logout',
-                //label: this.dotMessageService.get('Logout'),
-                label: 'Sign Out',
+                label: this.dotMessageService.get('Logout'),
                 icon: 'pi pi-sign-out',
                 visible: !auth.isLoginAs,
                 url: this.FINAL_LOGOUT_URL,
