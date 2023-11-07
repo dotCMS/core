32ede7c4d4be68e80e198b3d05fed2ded74bb427
KevinDavilaDotCMS <144152756+KevinDavilaDotCMS@users.noreply.github.com>
2023-11-07 11:32:01 -0500

    Uncomment jest config

--- core-web/libs/edit-content/jest.config.ts
+++ core-web/libs/edit-content/jest.config.ts
@@ -1,22 +1,22 @@
 /* eslint-disable */
 export default {
-    // displayName: 'edit-content',
-    // preset: '../../jest.preset.js',
-    // setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-    // globals: {
-    //     'ts-jest': {
-    //         stringifyContentPathRegex: '\\.(html|svg)$',
-    //         tsconfig: '<rootDir>/tsconfig.spec.json'
-    //     }
-    // },
-    // coverageDirectory: '../../coverage/libs/edit-content',
-    // transform: {
-    //     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular'
-    // },
-    // transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
-    // snapshotSerializers: [
-    //     'jest-preset-angular/build/serializers/no-ng-attributes',
-    //     'jest-preset-angular/build/serializers/ng-snapshot',
-    //     'jest-preset-angular/build/serializers/html-comment'
-    // ]
+    displayName: 'edit-content',
+    preset: '../../jest.preset.js',
+    setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+    globals: {
+        'ts-jest': {
+            stringifyContentPathRegex: '\\.(html|svg)$',
+            tsconfig: '<rootDir>/tsconfig.spec.json'
+        }
+    },
+    coverageDirectory: '../../coverage/libs/edit-content',
+    transform: {
+        '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular'
+    },
+    transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+    snapshotSerializers: [
+        'jest-preset-angular/build/serializers/no-ng-attributes',
+        'jest-preset-angular/build/serializers/ng-snapshot',
+        'jest-preset-angular/build/serializers/html-comment'
+    ]
 };
