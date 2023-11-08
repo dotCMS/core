de6da6f28bece040bace7e81e798430b92cc3921
daniel.solis <daniel.solis@dotcms.com>
2023-11-08 12:40:03 -0600

    #25233: reindex content with new publish_date column values populated

--- dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task230922AddPublishDateToContentletVersionInfo.java
+++ dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task230922AddPublishDateToContentletVersionInfo.java
@@ -1,21 +1,33 @@
 package com.dotmarketing.startup.runonce;
 
+import com.dotmarketing.business.APILocator;
 import com.dotmarketing.common.db.DotConnect;
 import com.dotmarketing.common.db.DotDatabaseMetaData;
-import com.dotmarketing.db.DbConnectionFactory;
+import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotRuntimeException;
+import com.dotmarketing.exception.DotSecurityException;
 import com.dotmarketing.startup.StartupTask;
 import com.dotmarketing.util.Logger;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * This Upgrade Task adds the publish_date column to the contentlet_version_info table
  */
 public class Task230922AddPublishDateToContentletVersionInfo implements StartupTask {
 
-    public static final String ADD_PUBLISH_DATE_COLUMN =
+    private static final String ADD_PUBLISH_DATE_COLUMN =
             "ALTER TABLE contentlet_version_info ADD publish_date TIMESTAMPTZ NULL";
+
+    private static final String UPDATE_PUBLISH_DATE_COLUMN =
+            "UPDATE contentlet_version_info SET publish_date = version_ts WHERE live_inode IS NOT NULL";
+
+    private static final String QUERY_CONTENT_TYPES_WITH_LIVE_CONTENT =
+            "SELECT DISTINCT i.asset_subtype " +
+                    "FROM identifier i INNER JOIN contentlet_version_info cvi " +
+                    "ON i.id = cvi.identifier WHERE cvi.live_inode IS NOT NULL";
 
     @Override
     public boolean forceRun() {
@@ -30,8 +42,18 @@ public class Task230922AddPublishDateToContentletVersionInfo implements StartupT
                 Logger.info(this, "Adding the 'publish_date' column to the 'contentlet_version_info' table");
                 dotConnect.executeStatement(ADD_PUBLISH_DATE_COLUMN);
                 Logger.info(this, "Setting the 'publish_date' column to the current version timestamp for existing content");
-                dotConnect.executeStatement("UPDATE contentlet_version_info SET publish_date = version_ts WHERE live_inode IS NOT NULL");
-            } catch (SQLException e) {
+                dotConnect.executeStatement(UPDATE_PUBLISH_DATE_COLUMN);
+
+                // Reindex contentlets to update the publish_date field
+                dotConnect.setSQL(QUERY_CONTENT_TYPES_WITH_LIVE_CONTENT);
+                final List<String> contentTypeVariables = dotConnect.loadObjectResults()
+                        .stream().map(r -> r.get("asset_subtype").toString()).collect(Collectors.toList());
+                for (final String contentTypeVariable : contentTypeVariables) {
+                    APILocator.getContentletAPI().refresh(
+                            APILocator.getContentTypeAPI(APILocator.systemUser()).find(contentTypeVariable));
+                }
+
+            } catch (SQLException | DotDataException | DotSecurityException e) {
                 throw new DotRuntimeException(e);
             }
         }
