2bed85ee4e939bbf7a2c5b0a4e0c6d5dcc7d91cc
Victor Alfaro <victor.alfaro@dotcms.com>
2023-11-08 17:57:29 -0600

    test

--- dotcms-postman/runtests.sh
+++ dotcms-postman/runtests.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+wait=60
+echo "Waiting for ${wait} seconds to images be pulled"
+sleep $wait
+docker-compose logs -t -f dotcms-test &
+
 postman_collections="$1"
 dotcms_dir=../dotCMS
 postman_tests_dir=${dotcms_dir}/src/curl-test
-postman_tests_results_dir=./target/postman-reports
+postman_tests_results_dir=$(pwd)/target/postman-reports
 
 mkdir -p ${postman_tests_results_dir}
+echo "
+Current dir: $(pwd)
+Postman tests dir: ${postman_tests_dir}
+Postman tests results dir: ${postman_tests_results_dir}
+"
+
 old_ifs=$IFS
 IFS=,
 collections=( ${postman_collections} )
-wait=120
 
 echo "Waiting for ${wait} seconds to allow dotCMS to fully start running tests"
-echo "Time is up! Preparing to run collections: [${postman_collections//,/$'\n'}]"
-
-echo "Current dir: $(pwd)"
 sleep ${wait}
+echo "Time is up! Preparing to run collections: [
+${postman_collections//,/$'\n'}
+]"
 
 pushd ${postman_tests_dir}
 collection_error=false
@@ -27,9 +37,9 @@ do
   echo "Running newman command: newman run ${collection} -e postman_environment.json --reporters cli,junit --reporter-junit-export ${postman_tests_results_dir}/TEST-${collection_name}.xml"
   newman run ${collection} -e postman_environment.json --reporters cli,junit --reporter-junit-export ${postman_tests_results_dir}/TEST-${collection_name}.xml
 
-  pcec=$?
-  if [[ $pcec -ne 0 ]]; then
-    echo "Collection [${collection}] failed with exit code [${pcec}]"
+  collection_exit_code=$?
+  if [[ $collection_exit_code -ne 0 ]]; then
+    echo "Collection [${collection}] failed with exit code [${collection_exit_code}]"
     collection_error=true
   fi
 done
