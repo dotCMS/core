6bd8aa64f567b9eff66a9636cbaf98ba7cc08a7b
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2023-11-09 12:24:07 -0600

    #26642 removing NON_DEFAULT setting from mapper

--- tools/dotcms-cli/api-data-model/src/main/java/com/dotcms/contenttype/model/field/Relationships.java
+++ tools/dotcms-cli/api-data-model/src/main/java/com/dotcms/contenttype/model/field/Relationships.java
@@ -2,7 +2,6 @@ package com.dotcms.contenttype.model.field;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import javax.annotation.Nullable;
 import org.immutables.value.Value;
 
 @Value.Immutable
@@ -10,11 +9,10 @@ import org.immutables.value.Value;
 @JsonDeserialize(as = ImmutableRelationships.class)
 public interface Relationships {
 
-      @Nullable
       Boolean isParentField();
-      @Nullable
+
       String velocityVar();
-      @Nullable
-      Long cardinality();
+
+      Integer cardinality();
 
 }
