ea78684d1034685d8c2a3d7e34c69248861394d4
Arcadio Quintero <oidacra@gmail.com>
2023-11-13 12:52:56 -0500

    #26399 added mising test

--- core-web/libs/portlets/dot-experiments/portlet/src/lib/dot-experiments-list/components/dot-experiments-list-table/dot-experiments-list-table.component.spec.ts
+++ core-web/libs/portlets/dot-experiments/portlet/src/lib/dot-experiments-list/components/dot-experiments-list-table/dot-experiments-list-table.component.spec.ts
@@ -10,7 +10,11 @@ import { Table } from 'primeng/table';
 import { DotMessageService } from '@dotcms/data-access';
 import { LoginService } from '@dotcms/dotcms-js';
 import { DotExperimentStatus, GroupedExperimentByStatus } from '@dotcms/dotcms-models';
-import { DotEmptyContainerComponent, DotFormatDateService } from '@dotcms/ui';
+import {
+    DotEmptyContainerComponent,
+    DotFormatDateService,
+    DotTimestampToDatePipe
+} from '@dotcms/ui';
 import {
     DotFormatDateServiceMock,
     getExperimentMock,
@@ -78,6 +82,7 @@ describe('DotExperimentsListTableComponent', () => {
 
     const createComponent = createComponentFactory({
         component: DotExperimentsListTableComponent,
+        imports: [DotTimestampToDatePipe],
         componentMocks: [ConfirmPopup],
         declarations: [MockDatePipe],
         providers: [
@@ -139,9 +144,11 @@ describe('DotExperimentsListTableComponent', () => {
                 DRAFT_EXPERIMENT_MOCK.name
             );
             expect(spectator.query(byTestId('experiment-row__createdDate'))).toHaveText(
-                '1 hour ago'
+                '10/10/2020 10:10 PM'
             );
-            expect(spectator.query(byTestId('experiment-row__modDate'))).toHaveText('1 hour ago');
+            expect(spectator.query(byTestId('experiment-row__modDate'))).toHaveText(
+                '10/10/2020 10:10 PM'
+            );
         });
 
         it('should emit action when a row is clicked', () => {
