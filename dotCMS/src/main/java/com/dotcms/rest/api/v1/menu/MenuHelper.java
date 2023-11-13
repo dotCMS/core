492c35bd46d636ec490e539a2cfd93b8ab88e9af
Will Ezell <will@dotcms.com>
2023-11-13 11:35:35 -0500

    fix(menu) cleans up menu labels if the translation is not available.
    ref: #26660

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/menu/MenuHelper.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/menu/MenuHelper.java
@@ -4,6 +4,7 @@ import com.dotcms.rest.BaseRestPortlet;
 import com.dotcms.spring.portlet.PortletController;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.UtilMethods;
 import com.liferay.portal.language.LanguageException;
 import com.liferay.portal.language.LanguageUtil;
 import com.liferay.portal.model.Portlet;
@@ -22,6 +23,7 @@ import java.util.Locale;
 
 /**
  * Helper for the {@link MenuResource}
+ *
  * @author jsanca
  */
 public class MenuHelper implements Serializable {
@@ -30,11 +32,14 @@ public class MenuHelper implements Serializable {
             new MenuHelper();
 
     private static final String PORTLET_DOESNT_EXIST_ERROR_MSG = "Portlet ID '%s' does not exist";
+    private static final String PORTLET_KEY_PREFIX = "com.dotcms.repackage.javax.portlet.title.";
 
-    private MenuHelper() {}
+    private MenuHelper() {
+    }
 
     /**
      * Get the list of menu portlet items for the specified menucontext
+     *
      * @param menuContext
      * @return a list of menu item associated to this context
      * @throws LanguageException
@@ -47,20 +52,76 @@ public class MenuHelper implements Serializable {
         List<String> portletIds = menuContext.getLayout().getPortletIds();
 
         for (String portletId : portletIds) {
-            menuContext.setPortletId( portletId );
+            menuContext.setPortletId(portletId);
             String linkHREF = getUrl(menuContext);
-            Locale locale = Try.of(()-> new Locale(menuContext.getHttpServletRequest().getSession().getAttribute("com.dotcms.repackage.org.apache.struts.action.LOCALE").toString())).getOrElse(Locale.US);
-            String linkName = LanguageUtil.get(locale, "com.dotcms.repackage.javax.portlet.title." + portletId);
-            boolean isAngular = isAngular( portletId );
-            boolean isAjax = isAjax( portletId );
+            Locale locale = Try.of(() -> new Locale(menuContext.getHttpServletRequest().getSession().getAttribute("com.dotcms.repackage.org.apache.struts.action.LOCALE").toString())).getOrElse(Locale.US);
+            String linkName = normalizeLinkName(LanguageUtil.get(locale, PORTLET_KEY_PREFIX + portletId));
 
-            menuItems.add ( new MenuItem(portletId, linkHREF, linkName, isAngular, isAjax) );
+            boolean isAngular = isAngular(portletId);
+            boolean isAjax = isAjax(portletId);
+
+            menuItems.add(new MenuItem(portletId, linkHREF, linkName, isAngular, isAjax));
         }
         return menuItems;
     }
 
     /**
+     * Get the url of the menucontext
+     *
+     * @param menuContext
+     * @return the portlet url
+     * @throws ClassNotFoundException
+     */
+    public String getUrl(final MenuContext menuContext) throws ClassNotFoundException {
+        final Portlet portlet = APILocator.getPortletAPI().findPortlet(menuContext.getPortletId());
+
+        if (null != portlet) {
+            final String portletClass = portlet.getPortletClass();
+            final Class<?> classs = Class.forName(portletClass);
+
+            Logger.debug(MenuResource.class, "### getPortletId" + menuContext.getPortletId());
+            Logger.debug(MenuResource.class, "### portletClass" + portletClass);
+            final PortletURLImpl portletURLImpl = new PortletURLImpl(menuContext.getHttpServletRequest(),
+                    menuContext.getPortletId(), menuContext.getLayout().getId(), false);
+            if (StrutsPortlet.class.isAssignableFrom(classs)
+                    || JSPPortlet.class.isAssignableFrom(classs)
+                    || VelocityPortlet.class.isAssignableFrom(classs)) {
+                return portletURLImpl + "&dm_rlout=1&r=" + System.currentTimeMillis();
+            } else if (BaseRestPortlet.class.isAssignableFrom(classs)) {
+                return portletURLImpl + "&dm_rlout=1&r=" + System.currentTimeMillis() + "&" + WebKeys.AJAX_PORTLET + "=true";
+            } else if (PortletController.class.isAssignableFrom(classs)) {
+                return StringPool.FORWARD_SLASH + menuContext.getPortletId();
+            }
+        } else {
+            Logger.error(this, String.format(PORTLET_DOESNT_EXIST_ERROR_MSG,
+                    menuContext.getPortletId()));
+        }
+
+        return null;
+    }
+
+    private static String normalizeLinkName(String linkName) {
+        if(UtilMethods.isEmpty(linkName)){
+            return "ukn";
+        }
+        else if (!linkName.startsWith(PORTLET_KEY_PREFIX)) {
+            return linkName;
+        }
+        linkName = linkName.replace(PORTLET_KEY_PREFIX, "");
+
+        if (linkName.startsWith("c_")) {
+            linkName = linkName.substring(2);
+        }
+
+        linkName = linkName.replace("_", " ");
+
+        return UtilMethods.capitalize(linkName);
+
+    }
+
+    /**
      * Validate if the portlet is a PortletController
+     *
      * @param portletId Id of the portlet
      * @return true if the portlet is a PortletController portlet, false if not
      * @throws ClassNotFoundException
@@ -79,6 +140,7 @@ public class MenuHelper implements Serializable {
 
     /**
      * Validate if the portlet is a BaseRestPortlet
+     *
      * @param portletId Id of the portlet
      * @return true if the portlet is a BaseRestPortlet portlet, false if not
      * @throws ClassNotFoundException
@@ -94,40 +156,6 @@ public class MenuHelper implements Serializable {
         }
 
         return false;
-    }
-
-    /**
-     * Get the url of the menucontext
-     * @param menuContext
-     * @return the portlet url
-     * @throws ClassNotFoundException
-     */
-    public String getUrl(final MenuContext menuContext) throws ClassNotFoundException {
-        final Portlet portlet = APILocator.getPortletAPI().findPortlet( menuContext.getPortletId() );
-
-        if (null != portlet) {
-            final String portletClass = portlet.getPortletClass();
-            final Class<?> classs = Class.forName(portletClass);
-
-            Logger.debug(MenuResource.class, "### getPortletId" + menuContext.getPortletId());
-            Logger.debug(MenuResource.class, "### portletClass" + portletClass);
-            final PortletURLImpl portletURLImpl = new PortletURLImpl(menuContext.getHttpServletRequest(),
-                    menuContext.getPortletId(), menuContext.getLayout().getId(), false);
-            if ( StrutsPortlet.class.isAssignableFrom(classs)
-                    || JSPPortlet.class.isAssignableFrom(classs)
-                    || VelocityPortlet.class.isAssignableFrom(classs) ) {
-                return portletURLImpl + "&dm_rlout=1&r=" + System.currentTimeMillis();
-            } else if (BaseRestPortlet.class.isAssignableFrom(classs)) {
-                return portletURLImpl + "&dm_rlout=1&r=" + System.currentTimeMillis() + "&" + WebKeys.AJAX_PORTLET + "=true";
-            } else if (PortletController.class.isAssignableFrom(classs)) {
-                return StringPool.FORWARD_SLASH + menuContext.getPortletId();
-            }
-        } else {
-            Logger.error(this, String.format(PORTLET_DOESNT_EXIST_ERROR_MSG,
-                    menuContext.getPortletId()));
-        }
-
-        return null;
     }
 
 } // E:O:F:MenuHelper.
