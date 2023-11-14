840f2ac051ec187e0957655b39458e6f6bd1a049
Arcadio Quintero <oidacra@gmail.com>
2023-11-14 10:54:40 -0500

    dotCMS/core#26664 Add a flag to setup storybook use the middleware (#26672)
    * #26399 fix test, missing mock
    
    * #26399 fix lint
    
    * fix(storybook) add a flag to setup storybook use the middleware

--- core-web/apps/dotcms-ui/.storybook/middleware.js
+++ core-web/apps/dotcms-ui/.storybook/middleware.js
@@ -1,45 +1,49 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 /**
  * This is for add those api that are not mocked in the storybook
- * and need to be proxied to the real backend for testing purposes
+ * and need to be proxied to the real backend for development purposes
  *
  * @module module.exports
  */
 module.exports = function expressMiddleware(router) {
-    // Proxy to the backend to generate text using OpenAI
-    router.use(
-        '/api/ai/text/generate',
-        createProxyMiddleware({
-            target: 'http://localhost:8080',
-            changeOrigin: true,
-            pathRewrite: {
-                '^/api/ai/text/generate': '/api/ai/text/generate'
-            }
-        })
-    );
+    if (process.env.USE_MIDDLEWARE) {
+        // Proxy to the backend to generate text using OpenAI
+        router.use(
+            '/api/v1/ai/text/generate',
+            createProxyMiddleware({
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api/v1/ai/text/generate': '/api/v1/ai/text/generate'
+                }
+            })
+        );
 
-    // Proxy to the backend to generate an image using OpenAI
-    router.use(
-        '/api/ai/image/generate',
-        createProxyMiddleware({
-            target: 'http://localhost:8080',
-            changeOrigin: true,
-            pathRewrite: {
-                '^/api/ai/image/generate': '/api/ai/image/generate'
-            }
-        })
-    );
+        // Proxy to the backend to generate an image using OpenAI
+        router.use(
+            '/api/ai/image/generate',
+            createProxyMiddleware({
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api/v1/ai/image/generate': '/api/v1/ai/image/generate'
+                }
+            })
+        );
 
-    // Publish the image_temp generated
-    router.use(
-        '/api/v1/workflow/actions/default/fire/PUBLISH',
-        createProxyMiddleware({
-            target: 'http://localhost:8080',
-            changeOrigin: true,
-            pathRewrite: {
-                '^/api/v1/workflow/actions/default/fire/PUBLISH':
-                    '/api/v1/workflow/actions/default/fire/PUBLISH'
-            }
-        })
-    );
+        // Publish the image_temp generated
+        router.use(
+            '/api/v1/workflow/actions/default/fire/PUBLISH',
+            createProxyMiddleware({
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api/v1/workflow/actions/default/fire/PUBLISH':
+                        '/api/v1/workflow/actions/default/fire/PUBLISH'
+                }
+            })
+        );
+
+        console.info(`\x1b[32m[dotCMS]\x1b[0m`, 'Using middleware for storybook');
+    }
 };
