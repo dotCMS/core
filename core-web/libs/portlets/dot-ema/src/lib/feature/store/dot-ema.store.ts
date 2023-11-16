fcd5fcfcf8b8af9e09c7f67ea6cefa542171b0ab
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-11-15 23:18:09 -0600

    Clean up store

--- core-web/libs/portlets/dot-ema/src/lib/feature/store/dot-ema.store.ts
+++ core-web/libs/portlets/dot-ema/src/lib/feature/store/dot-ema.store.ts
@@ -9,7 +9,6 @@ import { DotPageApiService } from '../../services/dot-page-api.service';
 
 export interface EditEmaState {
     language_id: string;
-    iframeUrl: string;
     url: string;
     editor: {
         page: {
@@ -23,7 +22,6 @@ export class EditEmaStore extends ComponentStore<EditEmaState> {
     constructor(private dotPageApiService: DotPageApiService) {
         super({
             language_id: '',
-            iframeUrl: '',
             url: '',
             editor: {
                 page: {
@@ -33,27 +31,12 @@ export class EditEmaStore extends ComponentStore<EditEmaState> {
         });
     }
 
-    readonly iframeUrl$: Observable<string> = this.select((state) => state.iframeUrl);
+    readonly iframeUrl$: Observable<string> = this.select(
+        ({ url, language_id }) => `http://localhost:3000/${url}?language_id=${language_id}`
+    );
     readonly language_id$: Observable<string> = this.select((state) => state.language_id);
     readonly url$: Observable<string> = this.select((state) => state.url);
     readonly pageTitle$: Observable<string> = this.select((state) => state.editor.page.title);
-
-    readonly setContent = this.updater(
-        (
-            state,
-            {
-                editor,
-                language_id,
-                url
-            }: { editor: { page: { title: string } }; language_id: string; url: string }
-        ) => ({
-            ...state,
-            editor,
-            language_id,
-            url,
-            iframeUrl: `http://localhost:3000/${url}?language_id=${language_id}`
-        })
-    );
 
     readonly load = this.effect((params$: Observable<{ language_id: string; url: string }>) => {
         return params$.pipe(
@@ -61,7 +44,7 @@ export class EditEmaStore extends ComponentStore<EditEmaState> {
                 this.dotPageApiService.get({ language_id, url }).pipe(
                     tap({
                         next: (editor) => {
-                            this.setContent({
+                            this.patchState({
                                 editor,
                                 language_id,
                                 url
@@ -80,13 +63,11 @@ export class EditEmaStore extends ComponentStore<EditEmaState> {
 
     readonly setLanguage = this.updater((state, language_id: string) => ({
         ...state,
-        language_id,
-        iframeUrl: `http://localhost:3000/${state.url}?language_id=${language_id}`
+        language_id
     }));
 
     readonly setUrl = this.updater((state, url: string) => ({
         ...state,
-        url,
-        iframeUrl: `http://localhost:3000/${url}?language_id=${state.language_id}`
+        url
     }));
 }
