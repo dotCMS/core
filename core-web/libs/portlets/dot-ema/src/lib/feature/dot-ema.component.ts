5a8d85805f53b53d73959743512a8669c3d97f13
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-11-15 19:54:33 -0600

    feat(edit-ema): Sync the URL from Angular to the iframe

--- core-web/libs/portlets/dot-ema/src/lib/feature/dot-ema.component.ts
+++ core-web/libs/portlets/dot-ema/src/lib/feature/dot-ema.component.ts
@@ -1,11 +1,85 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'dot-ema',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule],
     templateUrl: './dot-ema.component.html',
     styleUrls: ['./dot-ema.component.scss']
 })
-export class DotEmaComponent {}
+export class DotEmaComponent {
+    language_id = '';
+    url = '';
+
+    languages = [
+        {
+            name: 'English',
+            value: '1'
+        },
+        {
+            name: 'Spanish',
+            value: '2'
+        }
+    ];
+
+    pages = [
+        {
+            name: 'Page One',
+            value: 'page-one'
+        },
+        {
+            name: 'Page Two',
+            value: 'page-two'
+        }
+    ];
+
+    route = inject(ActivatedRoute);
+    router = inject(Router);
+    sanitizer = inject(DomSanitizer);
+    path$ = this.route.queryParams.pipe(
+        map((params: Params) => {
+            let path;
+
+            const queryParams = Object.keys(params)
+                .map((key) => {
+                    if (key === 'url') {
+                        path = `${decodeURIComponent(params[key])}`;
+
+                        this.url = `${decodeURIComponent(params[key])}`;
+
+                        return null;
+                    }
+
+                    if (key === 'language_id') {
+                        this.language_id = `${decodeURIComponent(params[key])}`;
+                    }
+
+                    return `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`;
+                })
+                .filter(Boolean)
+                .join('&');
+
+            return this.sanitizer.bypassSecurityTrustResourceUrl(
+                `http://localhost:3000/${path}?${queryParams}`
+            );
+        })
+    );
+
+    onChange(e: Event) {
+        const name = (e.target as HTMLSelectElement).name;
+        const value = (e.target as HTMLSelectElement).value;
+
+        this.router.navigate([], {
+            queryParams: {
+                [name]: value
+            },
+            queryParamsHandling: 'merge'
+        });
+    }
+}
