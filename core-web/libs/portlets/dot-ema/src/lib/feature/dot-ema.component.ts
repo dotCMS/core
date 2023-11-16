c7c42cbcac79eaf68d3fd878fe5c8dcdab89d4b4
Jalinson Diaz <zjaaaldev@gmail.com>
2023-11-16 10:44:46 -0300

    dev (dot ema): add navigation for edit content

--- core-web/libs/portlets/dot-ema/src/lib/feature/dot-ema.component.ts
+++ core-web/libs/portlets/dot-ema/src/lib/feature/dot-ema.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule, DOCUMENT } from '@angular/common';
 import { AfterViewInit, Component, Inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'dot-ema',
@@ -9,12 +10,16 @@ import { AfterViewInit, Component, Inject } from '@angular/core';
     styleUrls: ['./dot-ema.component.scss']
 })
 export class DotEmaComponent implements AfterViewInit {
-    constructor(@Inject(DOCUMENT) private document: Document) {}
+    constructor(@Inject(DOCUMENT) private document: Document, private router: Router) {}
     ngAfterViewInit(): void {
         this.document.defaultView?.addEventListener('message', (event: MessageEvent) => {
             // This should be the host the user uses for nextjs, because this can trigger react dev tools messages
             if ((event as { origin: string }).origin !== 'http://localhost:3000') {
                 return;
+            }
+
+            if (event.data.action === 'edit-contentlet') {
+                this.router.navigate([`/c/content/${event.data.data.inode}`]);
             }
         });
     }
