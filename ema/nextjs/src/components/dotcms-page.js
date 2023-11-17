0f66693c551a34d6b3049c9de3885ff80f02644b
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-11-16 18:57:23 -0600

    Add ImageComponent to dotcms-page.js

--- ema/nextjs/src/components/dotcms-page.js
+++ ema/nextjs/src/components/dotcms-page.js
@@ -10,7 +10,8 @@ const contentComponents = {
     webPageContent: WebPageContent,
     Banner: Banner,
     Activity: Activity,
-    Product: Product
+    Product: Product,
+    Image: ImageComponent
 };
 
 function WebPageContent({ title, body }) {
@@ -19,6 +20,25 @@ function WebPageContent({ title, body }) {
             <h1 className="text-xl font-bold">{title}</h1>
             <div dangerouslySetInnerHTML={{ __html: body }} />
         </>
+    );
+}
+
+function ImageComponent({ fileAsset, title, description }) {
+    return (
+        <div className="relative overflow-hidden bg-white rounded shadow-lg group">
+            <div className="relative w-full bg-gray-200 h-96">
+                <Image
+                    src={`${process.env.NEXT_PUBLIC_DOTCMS_HOST}${fileAsset}`}
+                    fill={true}
+                    className="object-cover"
+                    alt={title}
+                />
+            </div>
+            <div className="absolute bottom-0 w-full px-6 py-8 text-white transition-transform duration-300 translate-y-full bg-orange-500 bg-opacity-80 w-100 group-hover:translate-y-0">
+                <div className="mb-2 text-2xl font-bold">{title}</div>
+                <p className="text-base">{description}</p>
+            </div>
+        </div>
     );
 }
 
