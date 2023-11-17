cba82e798dbb1d5f6877684348a0986aea337cef
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-11-16 19:30:39 -0600

    Add navigation, logo and language selector

--- ema/nextjs/src/app/[[...slug]]/page.js
+++ ema/nextjs/src/app/[[...slug]]/page.js
@@ -23,15 +23,39 @@ async function getPage({ url, language_id }) {
     return res.json();
 }
 
+async function getNav() {
+    // TODO: Check why /json is not getting the page correctly
+    const requestUrl = `${process.env.NEXT_PUBLIC_DOTCMS_HOST}/api/v1/nav/?depth=2`;
+
+    const res = await fetch(requestUrl, {
+        headers: {
+            Authorization: `Bearer ${process.env.DOTCMS_AUTH_TOKEN}`
+        }
+    });
+
+    if (!res.ok) {
+        const message = await res.text();
+        throw new Error(`Failed to fetch data ${res.status} ${message}`);
+    }
+
+    return res.json();
+}
+
 export default async function Home({ searchParams, params }) {
     const data = await getPage({
         url: params?.slug ? params.slug.join('/') : 'index',
         language_id: searchParams.language_id
     });
 
+    const nav = await getNav();
+
     return (
         // Provide the page data globally
-        <GlobalProvider entity={data.entity}>
+        <GlobalProvider
+            entity={{
+                ...data.entity,
+                nav: nav.entity.children
+            }}>
             <DotcmsPage />
         </GlobalProvider>
     );
--- ema/nextjs/src/components/dotcms-page.js
+++ ema/nextjs/src/components/dotcms-page.js
@@ -136,7 +136,24 @@ function NoContent({ contentType }) {
 
 // Header component
 const Header = () => {
-    return <header className="p-4 text-white bg-blue-500">Header</header>;
+    const { nav, page } = useContext(GlobalContext);
+    return (
+        <header className="flex items-center justify-between p-4 bg-blue-500">
+            <div className="flex items-center">
+                <h2 className="text-3xl font-bold text-white">
+                    <Link href="/">TravelLux</Link>
+                </h2>
+            </div>
+            <Navigation className="text-white" nav={nav} />
+            <div className="flex items-center">
+                <select className="px-2 py-1 border border-gray-300 rounded">
+                    <option value="en">English</option>
+                    <option value="es">Español</option>
+                    <option value="fr">Français</option>
+                </select>
+            </div>
+        </header>
+    );
 };
 
 // Footer component
@@ -261,10 +278,29 @@ const Row = ({ row }) => {
     );
 };
 
+function Navigation({ nav, className }) {
+    return (
+        <nav className={className}>
+            <ul className="flex space-x-4">
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                {nav.map((item) => (
+                    <li key={item.folder}>
+                        <Link href={item.href} target={item.target}>
+                            {item.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+}
+
 // Main layout component
 export const DotcmsPage = () => {
     // Get the page layout from the global context
-    const { layout, page } = useContext(GlobalContext);
+    const { layout } = useContext(GlobalContext);
 
     return (
         <div className="flex flex-col min-h-screen gap-6">
