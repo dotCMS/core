d25cd40e7d8b830d1f6b5c9773b23ed7c75a48d2
jdotcms <jonathan.sanchez@dotcms.com>
2023-11-20 17:13:27 -0600

    #26573 adding minor changes

--- dotCMS/src/main/java/com/dotcms/rest/api/v2/tags/ResponseEntityTagInodesMapView.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v2/tags/ResponseEntityTagInodesMapView.java
@@ -5,6 +5,10 @@ import com.dotmarketing.tag.model.TagInode;
 
 import java.util.List;
 
+/**
+ * Tags by Inode view
+ * @author jsanca
+ */
 public class ResponseEntityTagInodesMapView  extends ResponseEntityView<List<TagInode>>  {
     public ResponseEntityTagInodesMapView(final List<TagInode> entity) {
         super(entity);
