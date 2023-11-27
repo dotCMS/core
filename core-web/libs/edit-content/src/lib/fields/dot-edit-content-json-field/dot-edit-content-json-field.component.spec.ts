2ef353a8dae4996166e47c40db234816f337c029
Rafael Velazco <rjvelazco21@gmail.com>
2023-11-27 16:25:25 -0400

    dev: make editor resizable - vertical

--- core-web/libs/edit-content/src/lib/fields/dot-edit-content-json-field/dot-edit-content-json-field.component.spec.ts
+++ core-web/libs/edit-content/src/lib/fields/dot-edit-content-json-field/dot-edit-content-json-field.component.spec.ts
@@ -1,5 +1,5 @@
 import { MonacoEditorComponent, MonacoEditorModule } from '@materia-ui/ngx-monaco-editor';
-import { Spectator } from '@ngneat/spectator';
+import { Spectator, byTestId } from '@ngneat/spectator';
 import { createComponentFactory } from '@ngneat/spectator/jest';
 import { MockComponent } from 'ng-mocks';
 
@@ -40,20 +40,45 @@ describe('DotEditContentJsonFieldComponent', () => {
 
         beforeEach(() => {
             spectator = createComponent();
+            spectator.setInput('field', JSON_FIELD_MOCK);
+            spectator.detectComponentChanges();
         });
 
         it('should render the Monoaco Editor with Current Value', () => {
-            spectator.setInput('field', JSON_FIELD_MOCK);
-            spectator.detectComponentChanges();
             const monacoEditorComponent = spectator.query(MonacoEditorComponent);
             expect(monacoEditorComponent).not.toBeNull();
         });
 
         it('should have the form Variable as a FormControlName', () => {
-            spectator.setInput('field', JSON_FIELD_MOCK);
-            spectator.detectComponentChanges();
             const element = spectator.query('ngx-monaco-editor');
             expect(element.getAttribute('ng-reflect-name')).toBe(JSON_FIELD_MOCK.variable);
+        });
+
+        it('should have the right editor options', () => {
+            const monacoEditorComponent = spectator.query(MonacoEditorComponent);
+
+            expect(monacoEditorComponent.options).toEqual({
+                theme: 'vs',
+                minimap: {
+                    enabled: false
+                },
+                cursorBlinking: 'solid',
+                overviewRulerBorder: false,
+                mouseWheelZoom: false,
+                lineNumbers: 'on',
+                roundedSelection: false,
+                automaticLayout: true,
+                language: 'json'
+            });
+        });
+
+        describe('container', () => {
+            it('should be resizible and min-heigh 9.375rem', () => {
+                const element = spectator.query(byTestId('json-field-container'));
+                const style = getComputedStyle(element);
+                const resizeValue = style.getPropertyValue('resize');
+                expect(resizeValue).toBe('vertical');
+            });
         });
     });
 });
