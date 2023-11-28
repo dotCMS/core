04aed846625e918e21ccd82340a5e0e623f1b0e9
Arcadio Quintero <oidacra@gmail.com>
2023-11-28 16:49:16 -0500

    #26551 UI `AiImagePromptInputComponent` remove unnused code

--- core-web/libs/block-editor/src/lib/shared/services/dot-ai/dot-ai.service.ts
+++ core-web/libs/block-editor/src/lib/shared/services/dot-ai/dot-ai.service.ts
@@ -1,4 +1,4 @@
-import { Observable, of, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -15,51 +15,30 @@ interface OpenAIResponse {
 
 const API_ENDPOINT = '/api/v1/ai';
 const API_ENDPOINT_FOR_PUBLISH = '/api/v1/workflow/actions/default/fire/PUBLISH';
+const headers = new HttpHeaders({
+    'Content-Type': 'application/json'
+});
 
 @Injectable()
 export class DotAiService {
-    // TODO: Work on progress
-    private lastUsedPrompt: string | null = null;
-    private lastImagePrompt: string | null = null;
-    private lastContentResponse: string | null = null;
-
     constructor(private http: HttpClient) {}
 
-    // TODO: Work on progress
-    getLastUsedPrompt(): string | null {
-        return this.lastUsedPrompt;
-    }
-
-    // TODO: Work on progress
-    getLastContentResponse(): string | null {
-        return this.lastContentResponse;
-    }
-
     generateContent(prompt: string): Observable<string> {
-        const url = '/api/v1/ai/text/generate';
-        const body = JSON.stringify({
-            prompt
-        });
-
-        const headers = new HttpHeaders({
-            'Content-Type': 'application/json'
-        });
-
-        return this.http.post<OpenAIResponse>(url, body, { headers }).pipe(
-            catchError(() => {
-                return throwError('Error fetching AI content');
-            }),
-            map(({ response }) => {
-                return response;
+        return this.http
+            .post<OpenAIResponse>(`${API_ENDPOINT}/text/generate`, JSON.stringify({ prompt }), {
+                headers
             })
-        );
+            .pipe(
+                catchError(() => {
+                    return throwError('Error fetching AI content');
+                }),
+                map(({ response }) => {
+                    return response;
+                })
+            );
     }
 
     generateAndPublishImage(prompt: string): Observable<DotCMSContentlet[]> {
-        const headers = new HttpHeaders({
-            'Content-Type': 'application/json'
-        });
-
         return this.http
             .post<OpenAIResponse>(`${API_ENDPOINT}/image/generate`, JSON.stringify({ prompt }), {
                 headers
@@ -73,23 +52,6 @@ export class DotAiService {
             );
     }
 
-    getLatestContent() {
-        return this.lastContentResponse;
-    }
-
-    // TODO: Work on progress
-    getNewContent(contentType: string): Observable<string> {
-        if (contentType === 'aiContent') {
-            return this.generateContent(this.lastUsedPrompt);
-        }
-
-        if (contentType === 'dotImage') {
-            // return this.generateImage(this.lastImagePrompt);
-        }
-
-        return of('');
-    }
-
     createAndPublishContentlet(fileId: string): Observable<DotCMSContentlet[]> {
         const contentlets = [
             {
@@ -100,10 +62,6 @@ export class DotAiService {
                 indexPolicy: 'WAIT_FOR'
             }
         ];
-
-        const headers = new HttpHeaders({
-            'Content-Type': 'application/json'
-        });
 
         return this.http
             .post(`${API_ENDPOINT_FOR_PUBLISH}`, JSON.stringify({ contentlets }), {
