68393fe1dc0b72adfb00d20d3895e0ebcde418f1
Will Ezell <will@dotcms.com>
2023-12-07 19:44:38 -0500

    Revert "updating IdentifierCache to enforce 'Infos' keu"
    This reverts commit dd33d7dbf9e1eaaa6cfb857fdfffe65c5b6768ef.

--- dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task231207AddInodeAndIdentifierLeftIndexes.java
+++ dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task231207AddInodeAndIdentifierLeftIndexes.java
@@ -7,9 +7,19 @@ import com.dotmarketing.exception.DotRuntimeException;
 import com.dotmarketing.startup.StartupTask;
 
 /**
- * This upgrade task adds "left" indexes to on the inode.inode and the identifier.id columns.  This
- * GREATLY speeds up queries that use `like 'param%'` clauses in their queries, where we only wildcard
- * on the right hand side.
+ * This Upgrade Task increases the size of the {@code contentlet.disabled_wysiwyg} column to 1000.
+ * <p>This column stores the velocity variable names -- and their respective separation characters -- of the WYSIWYG and
+ * Text Area fields whose view mode has been changed. For example:
+ * <ul>
+ *     <li>View Modes for WYSIWYG: {@code WYSIWYG}, {@code CODE}, and {@code PLAIN}.</li>
+ *     <li>View Modes for Text Area: {@code Toggle Editor} and "normal" editor.</li>
+ * </ul>
+ * In case customers have a Content Type with several of those fields, dotCMS will hit a database problem due to the
+ * column not being able to hold a String longer than the original 255 limit. This upgrade Task fixes such a situation.
+ * </p>
+ *
+ * @author Jose Castro
+ * @since Jul 13th, 2023
  */
 public class Task231207AddInodeAndIdentifierLeftIndexes implements StartupTask {
 
@@ -21,7 +31,6 @@ public class Task231207AddInodeAndIdentifierLeftIndexes implements StartupTask {
     @Override
     @WrapInTransaction
     public void executeUpgrade() throws DotDataException, DotRuntimeException {
-
         new DotConnect().setSQL("CREATE INDEX if not exists inode_inode_leading_idx ON inode(inode text_pattern_ops);").loadResult();
 
         new DotConnect().setSQL("CREATE INDEX if not exists identifier_id_leading_idx ON identifier(id text_pattern_ops);").loadResult();
