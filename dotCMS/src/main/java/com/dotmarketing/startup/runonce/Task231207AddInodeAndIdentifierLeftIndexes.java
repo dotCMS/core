d7cff51fc480cd6e861f7b7b9db5e565128ceddb
Will Ezell <will@dotcms.com>
2023-12-07 21:32:46 -0500

    perf - changing index to collate 'c' as that is the more modern version of the left index #26926

--- dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task231207AddInodeAndIdentifierLeftIndexes.java
+++ dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task231207AddInodeAndIdentifierLeftIndexes.java
@@ -19,9 +19,9 @@ public class Task231207AddInodeAndIdentifierLeftIndexes implements StartupTask {
 
     @Override
     public void executeUpgrade() throws DotDataException, DotRuntimeException {
-        new DotConnect().setSQL("CREATE INDEX if not exists inode_inode_leading_idx ON inode(inode text_pattern_ops);").loadResult();
+        new DotConnect().setSQL("CREATE INDEX if not exists inode_inode_leading_idx ON inode(inode  COLLATE \"C\");").loadResult();
 
-        new DotConnect().setSQL("CREATE INDEX if not exists identifier_id_leading_idx ON identifier(id text_pattern_ops);").loadResult();
+        new DotConnect().setSQL("CREATE INDEX if not exists identifier_id_leading_idx ON identifier(id  COLLATE \"C\");").loadResult();
 
     }
 
