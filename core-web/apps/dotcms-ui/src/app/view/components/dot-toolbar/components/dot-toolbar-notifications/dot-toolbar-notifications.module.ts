79da634f8ceb4c9fd8e601e2983574372d3d3166
Manuel Rojas <manuel.rojas.21@gmail.com>
2023-12-11 15:31:12 -0600

    #25737 Build announcements menu

--- core-web/apps/dotcms-ui/src/app/view/components/dot-toolbar/components/dot-toolbar-notifications/dot-toolbar-notifications.module.ts
+++ core-web/apps/dotcms-ui/src/app/view/components/dot-toolbar/components/dot-toolbar-notifications/dot-toolbar-notifications.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { ButtonModule } from 'primeng/button';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 
 import { DotCustomTimeModule } from '@components/_common/dot-custom-time.component/dot-custom-time.module';
 import { DotDropdownModule } from '@components/_common/dot-dropdown-component/dot-dropdown.module';
@@ -13,6 +14,7 @@ import {
     DotNotificationsListComponent
 } from './components/dot-notifications/dot-notifications.component';
 import { DotToolbarNotificationsComponent } from './dot-toolbar-notifications.component';
+import { DividerModule } from 'primeng/divider';
 
 @NgModule({
     imports: [
@@ -21,7 +23,9 @@ import { DotToolbarNotificationsComponent } from './dot-toolbar-notifications.co
         ButtonModule,
         DotCustomTimeModule,
         DotPipesModule,
-        DotMessagePipe
+        DotMessagePipe,
+        OverlayPanelModule,
+        DividerModule
     ],
     exports: [DotToolbarNotificationsComponent],
     declarations: [
