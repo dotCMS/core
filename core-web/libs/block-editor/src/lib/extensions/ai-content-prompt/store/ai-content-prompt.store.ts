85eb41f151acaeae9008898a3226dae79c5cf05e
Humberto Morera <31667212+hmoreras@users.noreply.github.com>
2023-12-11 10:27:22 -0600

    dotCMS/core#26666 [UI] When the BlockEditor lose the focus, and get it again the generated content is duplicated  (#26888)
    * progress, disable editor while loading, remove trust in currentSelection when handling the aiNode to accept,replace,delete node
    
    * fix double subscription error, replace content directly in the ai content extension
    
    * progress - doc - clean
    
    * doc clean up
    
    * fix ts lint error in CI ?
    
    * fix
    
    * simplify to a status property
    
    * re builid editor
    
    ---------
    
    Co-authored-by: Arcadio Quintero <oidacra@gmail.com>

--- core-web/libs/block-editor/src/lib/extensions/ai-content-prompt/store/ai-content-prompt.store.ts
+++ core-web/libs/block-editor/src/lib/extensions/ai-content-prompt/store/ai-content-prompt.store.ts
@@ -3,16 +3,16 @@ import { Observable, of } from 'rxjs';
 
 import { Injectable } from '@angular/core';
 
-import { catchError, switchMap, tap } from 'rxjs/operators';
+import { catchError, switchMap, tap, withLatestFrom } from 'rxjs/operators';
 
 import { DotAiService } from '../../../shared';
 
 export interface AiContentPromptState {
     prompt: string;
-    loading: boolean;
     content: string;
-    open: boolean;
     acceptContent: boolean;
+    deleteContent: boolean;
+    status: 'loading' | 'loaded' | 'open' | 'exit' | 'close';
 }
 
 @Injectable({
@@ -20,33 +20,48 @@ export interface AiContentPromptState {
 })
 export class AiContentPromptStore extends ComponentStore<AiContentPromptState> {
     constructor(private dotAiService: DotAiService) {
-        super({ prompt: '', loading: false, content: '', open: false, acceptContent: false });
+        super({
+            prompt: '',
+            content: '',
+            acceptContent: false,
+            deleteContent: false,
+            status: 'close'
+        });
     }
 
     //Selectors
     readonly prompt$ = this.select((state) => state.prompt);
     readonly content$ = this.select((state) => state.content);
-    readonly open$ = this.select((state) => state.open);
+    readonly deleteContent$ = this.select((state) => state.deleteContent);
+    readonly status$ = this.select((state) => state.status);
     readonly vm$ = this.select((state) => state);
 
     //Updaters
-    readonly setOpen = this.updater((state, open: boolean) => ({ ...state, open }));
+    readonly setStatus = this.updater((state, status: AiContentPromptState['status']) => ({
+        ...state,
+        status
+    }));
+
     readonly setAcceptContent = this.updater((state, acceptContent: boolean) => ({
         ...state,
         acceptContent
+    }));
+    readonly setDeleteContent = this.updater((state, deleteContent: boolean) => ({
+        ...state,
+        deleteContent
     }));
 
     // Effects
     readonly generateContent = this.effect((prompt$: Observable<string>) => {
         return prompt$.pipe(
             switchMap((prompt) => {
-                this.patchState({ loading: true, prompt });
+                this.patchState({ status: 'loading', prompt });
 
                 return this.dotAiService.generateContent(prompt).pipe(
-                    tap((content) => this.patchState({ loading: false, content })),
+                    tap((content) => this.patchState({ status: 'loaded', content })),
                     catchError(() => {
                         //TODO: Notify to handle error in the UI.
-                        this.patchState({ loading: false, content: '' });
+                        this.patchState({ status: 'loaded', content: '' });
 
                         return of(null);
                     })
@@ -56,11 +71,17 @@ export class AiContentPromptStore extends ComponentStore<AiContentPromptState> {
     });
 
     /**
-     * Use the last prompt to generate content
-     * @returns void
+     * When this effect is triggered, it uses the latest prompt value from the store's state
+     * to generate content using the `generateContent` effect.
+     *
+     * @param trigger$ - An observable that triggers the effect when it emits a value (e.g., `this.reGenerateContent()`)
+     * @returns An observable representing the triggering of the effect.
      * @memberof AiContentPromptStore
      */
-    reGenerateContent() {
-        this.generateContent(this.prompt$);
-    }
+    readonly reGenerateContent = this.effect((trigger$: Observable<void>) => {
+        return trigger$.pipe(
+            withLatestFrom(this.state$),
+            tap(([_, { prompt }]) => this.generateContent(of(prompt)))
+        );
+    });
 }
--- core-web/libs/block-editor/src/lib/shared/utils/prosemirror.utils.ts
+++ core-web/libs/block-editor/src/lib/shared/utils/prosemirror.utils.ts
@@ -9,6 +9,12 @@ import { CustomNodeTypes, NodeTypes } from '../../extensions';
 const aTagRex = new RegExp(/<a(|\s+[^>]*)>(\s|\n|<img[^>]*src="[^"]*"[^>]*>)*?<\/a>/gm);
 const imgTagRex = new RegExp(/<img[^>]*src="[^"]*"[^>]*>/gm);
 
+export interface DotTiptapNodeInformation {
+    node: Node;
+    from: number;
+    to: number;
+}
+
 /**
  * Get the parent node of the ResolvedPos sent
  * @param selectionStart ResolvedPos
@@ -213,6 +219,58 @@ export const getCursorPosition = (view: EditorView): { from: number; to: number 
     const to = Math.max(...ranges.map((range) => range.$to.pos));
 
     return { from, to };
+};
+
+/**
+ * Replace a node in Tiptap editor with new content.
+ *
+ * @param {Editor} editor - The Tiptap editor instance.
+ * @param {DotTiptapNodeInformation} tiptapNodeInfo - Information about the Tiptap node to be replaced,
+ * including the node itself, and its start and end positions.
+ * @param {string} content - The new content to replace the existing node.
+ * @returns {void}
+ */
+export const replaceNodeWithContent = (
+    editor: Editor,
+    tiptapNodeInfo: DotTiptapNodeInformation,
+    content: string
+): void => {
+    const { node, from, to } = tiptapNodeInfo;
+
+    // If the node is found, replace it with the new content
+    if (node) {
+        editor.chain().deleteRange({ from, to }).insertContentAt(from, content).run();
+    }
+};
+
+/**
+ * Find the first occurrence of a node of a specific type in the TipTap editor and determine its position range.
+ *
+ * @param {Editor} editor - The TipTap editor instance.
+ * @param {NodeTypes} nodeType - The type of the node to search for (e.g., 'paragraph').
+ * @returns {DotTiptapNodeInformation | null} An object containing information about the found node:
+ *          - `node`: The found node or `null` if not found.
+ *          - `from`: The starting position of the found node in the document or `null` if not found.
+ *          - `to`: The ending position (exclusive) of the found node in the document or `null` if not found.
+ */
+export const findNodeByType = (
+    editor: Editor, // The TipTap editor instance
+    nodeType: NodeTypes // The type of the node to search for
+): DotTiptapNodeInformation | null => {
+    let node = null;
+    let from = null;
+    let to = null;
+
+    // Traverse the document's descendants
+    editor.state.doc.descendants((currentNode, currentPosition) => {
+        if (currentNode.type.name === nodeType) {
+            node = currentNode;
+            from = currentPosition;
+            to = from + node.nodeSize;
+        }
+    });
+
+    return node ? { node, from, to } : null;
 };
 
 /**
