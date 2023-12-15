fdc03bc23bd6ac152f647eaf91234aa703ff8dff
Jalinson Diaz <zjaaaldev@gmail.com>
2023-12-15 17:32:48 -0300

    fix broken tests

--- core-web/libs/portlets/dot-ema/src/lib/edit-ema-layout/edit-ema-layout.component.spec.ts
+++ core-web/libs/portlets/dot-ema/src/lib/edit-ema-layout/edit-ema-layout.component.spec.ts
@@ -1,6 +1,11 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditEmaLayoutComponent } from './edit-ema-layout.component';
+
+import { EditEmaStore } from '../dot-ema-shell/store/dot-ema.store';
+import { DotPageApiService } from '../services/dot-page-api.service';
 
 describe('EditEmaLayoutComponent', () => {
     let component: EditEmaLayoutComponent;
@@ -8,7 +13,8 @@ describe('EditEmaLayoutComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [EditEmaLayoutComponent]
+            imports: [EditEmaLayoutComponent, HttpClientTestingModule],
+            providers: [EditEmaStore, DotPageApiService, HttpClient]
         }).compileComponents();
 
         fixture = TestBed.createComponent(EditEmaLayoutComponent);
