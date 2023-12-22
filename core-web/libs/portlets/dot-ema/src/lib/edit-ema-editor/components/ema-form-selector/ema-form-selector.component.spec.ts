d89c5e971659d4035c42f57f77086cd42caa1241
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-12-21 21:29:24 -0600

    Fix EmaFormSelectorComponent

--- core-web/libs/portlets/dot-ema/src/lib/edit-ema-editor/components/ema-form-selector/ema-form-selector.component.spec.ts
+++ core-web/libs/portlets/dot-ema/src/lib/edit-ema-editor/components/ema-form-selector/ema-form-selector.component.spec.ts
@@ -1,22 +1,55 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { byText, createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { of } from 'rxjs';
+
+import { CommonModule } from '@angular/common';
+
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+
+import { DotContentTypeService } from '@dotcms/data-access';
 
 import { EmaFormSelectorComponent } from './ema-form-selector.component';
 
 describe('EmaFormSelectorComponent', () => {
-    let component: EmaFormSelectorComponent;
-    let fixture: ComponentFixture<EmaFormSelectorComponent>;
-
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [EmaFormSelectorComponent]
-        }).compileComponents();
-
-        fixture = TestBed.createComponent(EmaFormSelectorComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+    let spectator: Spectator<EmaFormSelectorComponent>;
+    let mockDotContentTypeService: Partial<DotContentTypeService>;
+    const createComponent = createComponentFactory({
+        component: EmaFormSelectorComponent,
+        imports: [CommonModule, TableModule, ButtonModule],
+        mocks: [DotContentTypeService]
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    beforeEach(() => {
+        // Mock the DotContentTypeService and its methods
+        mockDotContentTypeService = {
+            getByTypes: jest
+                .fn()
+                .mockReturnValue(of([{ id: '1', name: 'Test Type', description: 'Description' }]))
+        };
+
+        spectator = createComponent({
+            providers: [{ provide: DotContentTypeService, useValue: mockDotContentTypeService }]
+        });
+    });
+
+    it('should have table headers', () => {
+        const tableHeaders = spectator.queryAll('th');
+        expect(tableHeaders.length).toBeGreaterThan(0);
+        expect(byText('Name')).not.toBeNull();
+        expect(byText('Description')).not.toBeNull();
+    });
+
+    it('should display content types in the table', () => {
+        const tableRows = spectator.queryAll('tr');
+        expect(tableRows.length).toBeGreaterThan(1); // More than the header row
+        expect(byText('Test Type')).not.toBeNull();
+        expect(byText('Description')).not.toBeNull();
+    });
+
+    it('should emit selected event when button is clicked', () => {
+        jest.spyOn(spectator.component.selected, 'emit');
+        const selectButton = spectator.query('p-button');
+        spectator.click(selectButton);
+        expect(spectator.component.selected.emit).toHaveBeenCalledWith('1');
     });
 });
