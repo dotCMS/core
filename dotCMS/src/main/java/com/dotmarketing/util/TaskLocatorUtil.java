03ab9a50df4057b9e1e13679b739a062cdd7021e
erickgonzalez <erick.gonzalez@dotcms.com>
2024-01-02 16:42:26 -0600

    #26926 include in 22.03.13

--- dotCMS/src/main/java/com/dotmarketing/util/TaskLocatorUtil.java
+++ dotCMS/src/main/java/com/dotmarketing/util/TaskLocatorUtil.java
@@ -358,6 +358,7 @@ public class TaskLocatorUtil {
 		ret.add(Task230119MigrateContentToProperPersonaTagAndRemoveDupTags.class);
 		ret.add(Task230426AlterVarcharLengthOfLockedByCol.class);
 		ret.add(Task230713IncreaseDisabledWysiwygColumnSize.class);
+		ret.add(Task231207AddInodeAndIdentifierLeftIndexes.class);
 		return ret.stream().sorted(classNameComparator).collect(Collectors.toList());
 	}
 
