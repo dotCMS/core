56a62591a54b5748f58a51964ed9d8d75dce01e6
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-01-02 22:00:52 -0600

    Fix dupe sq warn

--- core-web/libs/sdk/nextjs/src/lib/components/DotcmsLayout/DotcmsLayout.spec.tsx
+++ core-web/libs/sdk/nextjs/src/lib/components/DotcmsLayout/DotcmsLayout.spec.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { ReactNode } from 'react';
 
-import { PageProviderContext } from '@dotcms/react';
+import { mockPageContext } from '@dotcms/react';
 
 import { DotcmsLayout } from './DotcmsLayout';
 
@@ -43,64 +43,15 @@ Object.defineProperty(window, 'parent', {
     value: { postMessage: postMessageMock }
 });
 
-const mockEntity: PageProviderContext = {
-    layout: {
-        header: true,
-        footer: true,
-        body: {
-            rows: [
-                {
-                    columns: [
-                        {
-                            width: 6,
-                            leftOffset: 3,
-                            containers: [
-                                {
-                                    identifier: 'container-1',
-                                    uuid: 'uuid-1'
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        }
-    },
-    containers: {
-        'container-1': {
-            container: {
-                path: 'path/to/container',
-                identifier: 'container-1'
-            },
-            containerStructures: [
-                {
-                    contentTypeVar: 'content-type-1'
-                }
-            ],
-            contentlets: {
-                'uuid-1': [
-                    {
-                        contentType: 'content-type-1',
-                        identifier: 'contentlet-1',
-                        title: 'Contentlet 1',
-                        inode: 'inode-1'
-                    }
-                ]
-            }
-        }
-    },
-    page: { identifier: 'page-1', title: 'Hello Page' },
-    viewAs: { language: { id: 'en' }, persona: { keyTag: 'persona-1' } },
-    components: {}
-};
-
 describe('DotcmsLayout', () => {
     afterEach(() => {
         jest.clearAllMocks();
     });
 
-    it('renders correctly with PageProvider and rows', () => {
-        render(<DotcmsLayout entity={mockEntity} />);
-        expect(screen.getAllByTestId('mockRow').length).toBe(mockEntity.layout.body.rows.length);
+    it('should render correctly with PageProvider and rows', () => {
+        render(<DotcmsLayout entity={mockPageContext} />);
+        expect(screen.getAllByTestId('mockRow').length).toBe(
+            mockPageContext.layout.body.rows.length
+        );
     });
 });
--- core-web/libs/sdk/react/src/lib/components/DotcmsLayout/DotcmsLayout.spec.tsx
+++ core-web/libs/sdk/react/src/lib/components/DotcmsLayout/DotcmsLayout.spec.tsx
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom';
 
 import { DotcmsLayout } from './DotcmsLayout';
 
-import { PageProviderContext } from '../PageProvider/PageProvider';
+import { mockPageContext } from '../../mocks/mockPageContext';
 
 // Mock the custom hook and components
 jest.mock('../../hooks/usePageEditor', () => ({
@@ -29,60 +29,11 @@ jest.mock('../PageProvider/PageProvider', () => {
     };
 });
 
-export const mockEntity: PageProviderContext = {
-    layout: {
-        header: true,
-        footer: true,
-        body: {
-            rows: [
-                {
-                    columns: [
-                        {
-                            width: 6,
-                            leftOffset: 3,
-                            containers: [
-                                {
-                                    identifier: 'container-1',
-                                    uuid: 'uuid-1'
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        }
-    },
-    containers: {
-        'container-1': {
-            container: {
-                path: 'path/to/container',
-                identifier: 'container-1'
-            },
-            containerStructures: [
-                {
-                    contentTypeVar: 'content-type-1'
-                }
-            ],
-            contentlets: {
-                'uuid-1': [
-                    {
-                        contentType: 'content-type-1',
-                        identifier: 'contentlet-1',
-                        title: 'Contentlet 1',
-                        inode: 'inode-1'
-                    }
-                ]
-            }
-        }
-    },
-    page: { identifier: 'page-1', title: 'Hello Page' },
-    viewAs: { language: { id: 'en' }, persona: { keyTag: 'persona-1' } },
-    components: {}
-};
-
 describe('DotcmsLayout', () => {
     it('renders correctly with PageProvider and rows', () => {
-        render(<DotcmsLayout entity={mockEntity} />);
-        expect(screen.getAllByTestId('mockRow').length).toBe(mockEntity.layout.body.rows.length);
+        render(<DotcmsLayout entity={mockPageContext} />);
+        expect(screen.getAllByTestId('mockRow').length).toBe(
+            mockPageContext.layout.body.rows.length
+        );
     });
 });
