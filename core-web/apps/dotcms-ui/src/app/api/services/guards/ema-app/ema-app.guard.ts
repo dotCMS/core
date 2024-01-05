564403497f7a69c67b32b212a4c1991eab780041
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-01-04 21:42:28 -0600

    Some clean up

--- core-web/apps/dotcms-ui/src/app/api/services/guards/ema-app/ema-app.guard.ts
+++ core-web/apps/dotcms-ui/src/app/api/services/guards/ema-app/ema-app.guard.ts
@@ -1,4 +1,4 @@
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
@@ -10,10 +10,10 @@ import {
     UrlTree
 } from '@angular/router';
 
-import { pluck, switchMap, map } from 'rxjs/operators';
+import { pluck, switchMap, map, catchError } from 'rxjs/operators';
 
 import { Site } from '@dotcms/dotcms-js';
-import { DotAppsSite } from '@dotcms/dotcms-models';
+import { DotApps, DotAppsSite } from '@dotcms/dotcms-models';
 
 import { DotAppsService } from '../../dot-apps/dot-apps.service';
 
@@ -33,40 +33,65 @@ export class EmaAppGuard implements CanActivate {
         route: ActivatedRouteSnapshot,
         _state: RouterStateSnapshot
     ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        const currentSite$ = this.http
-            .get<{ entity: Site }>('/api/v1/site/currentSite')
-            .pipe(pluck('entity', 'identifier'));
+        return this.getCurrentSiteIdentifier().pipe(
+            switchMap((currentSiteId) => this.processSiteConfiguration(route, currentSiteId)),
+            catchError((error) => {
+                console.error('Error with currentSite$ stream:', error);
 
-        return currentSite$.pipe(
-            switchMap((siteId) => {
-                return this.appService.getConfiguration('dotema-config-v2', siteId).pipe(
-                    map((appConfiguration) =>
-                        appConfiguration.sites.find((site) => site.id === siteId)
-                    ),
-                    map((site: DotAppsSite) => {
-                        try {
-                            const val = JSON.parse(site.secrets[0].value)[0];
-                            const newQueryParams = getUpdatedQueryParams(route);
-
-                            if (doesPathMatchPattern(val.pattern, route.queryParams.url)) {
-                                this.router.navigate(['edit-ema'], {
-                                    queryParams: newQueryParams,
-                                    state: {
-                                        remoteUrl: 'https://google.com'
-                                    }
-                                });
-
-                                return false;
-                            }
-
-                            return true;
-                        } catch (error) {
-                            return true;
-                        }
-                    })
-                );
+                return of(true);
             })
         );
+    }
+
+    private getCurrentSiteIdentifier(): Observable<string> {
+        return this.http
+            .get<{ entity: Site }>('/api/v1/site/currentSite')
+            .pipe(pluck('entity', 'identifier'));
+    }
+
+    private processSiteConfiguration(
+        route: ActivatedRouteSnapshot,
+        currentSiteId: string
+    ): Observable<boolean> {
+        return this.appService.getConfiguration('dotema-config-v2', currentSiteId).pipe(
+            map((appConfiguration) =>
+                this.getConfigurationForCurrentSite(appConfiguration, currentSiteId)
+            ),
+            switchMap((site: DotAppsSite) => this.processSite(route, site)),
+            catchError((error) => {
+                console.error('Error getting site configuration:', error);
+
+                return of(true);
+            })
+        );
+    }
+
+    private getConfigurationForCurrentSite(appConfiguration: DotApps, siteId: string): DotAppsSite {
+        return appConfiguration.sites.find((site) => site.id === siteId);
+    }
+
+    private processSite(route: ActivatedRouteSnapshot, site: DotAppsSite): Observable<boolean> {
+        try {
+            const val = JSON.parse(site.secrets[0].value)[0];
+            const newQueryParams = getUpdatedQueryParams(route);
+
+            if (doesPathMatchPattern(val.pattern, route.queryParams.url)) {
+                this.router.navigate(['edit-ema'], {
+                    queryParams: newQueryParams,
+                    state: {
+                        remoteUrl: 'https://google.com'
+                    }
+                });
+
+                return of(false);
+            }
+
+            return of(true);
+        } catch (error) {
+            console.error('Error processing site:', error);
+
+            return of(true);
+        }
     }
 }
 
