8ebb9bb807aed620f265d5db2596cbed14b165d8
AndreyDotcms <127987858+AndreyDotcms@users.noreply.github.com>
2024-01-05 11:33:04 -0600

    dotCMS/core#25729 adding desc order to list

--- dotCMS/src/main/java/com/dotmarketing/portlets/workflows/business/WorkflowAPIImpl.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/workflows/business/WorkflowAPIImpl.java
@@ -4026,7 +4026,9 @@ public class WorkflowAPIImpl implements WorkflowAPI, WorkflowAPIOsgiService {
 				CollectionsUtils.join(this.findWorkFlowComments(task),
 						this.findWorkflowHistory (task));
 
-	    return workflowTimelineItems.stream().collect(CollectionsUtils.toImmutableList());
+		return workflowTimelineItems.stream()
+				.sorted(Comparator.comparing(WorkflowTimelineItem::createdDate).reversed())
+				.collect(CollectionsUtils.toImmutableList());
 	}
 
 
