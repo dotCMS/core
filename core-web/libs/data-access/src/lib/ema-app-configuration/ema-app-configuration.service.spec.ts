a57249908bbe05d6d4a60a447a84b9cfce88ab41
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-01-08 17:23:49 -0600

    Make EmaAppConfigurationService run

--- core-web/libs/data-access/src/lib/ema-app-configuration/ema-app-configuration.service.spec.ts
+++ core-web/libs/data-access/src/lib/ema-app-configuration/ema-app-configuration.service.spec.ts
@@ -1,16 +1,52 @@
-import { TestBed } from '@angular/core/testing';
+import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
+import { of } from 'rxjs';
+
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { SiteService } from '@dotcms/dotcms-js';
 
 import { EmaAppConfigurationService } from './ema-app-configuration.service';
 
-describe('EmaAppConfigurationService', () => {
-    let service: EmaAppConfigurationService;
+import { DotLicenseService } from '../dot-license/dot-license.service';
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(EmaAppConfigurationService);
+describe('EmaAppConfigurationService', () => {
+    let spectator: SpectatorService<EmaAppConfigurationService>;
+    // let httpTestingController: HttpTestingController;
+
+    const createService = createServiceFactory({
+        service: EmaAppConfigurationService,
+        imports: [HttpClientTestingModule, RouterTestingModule],
+        providers: [
+            {
+                provide: DotLicenseService,
+                useValue: {
+                    isEnterprise() {
+                        return of(true);
+                    }
+                }
+            },
+            {
+                provide: SiteService,
+                useValue: {
+                    getCurrentSite() {
+                        return of({
+                            identifier: '123'
+                        });
+                    }
+                }
+            }
+        ]
     });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
+    beforeEach(() => {
+        spectator = createService();
+        // httpTestingController = spectator.inject(HttpTestingController);
+    });
+
+    describe('get', () => {
+        it('should create', () => {
+            expect(spectator.service).toBeTruthy();
+        });
     });
 });
