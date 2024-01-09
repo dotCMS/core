55eff902f1616b4e07114fc94e1865ccd13aff5e
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-01-09 07:18:51 -0600

    Clean up

--- core-web/libs/data-access/src/lib/ema-app-secrets/ema-app-secrets.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-    providedIn: 'root'
-})
-export class EmaAppSecretsService {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    constructor() {}
-}
