d4ab455a53572e9c48acd207912465a22861ac5d
Kevin Davila <144152756+KevinDavilaDotCMS@users.noreply.github.com>
2024-01-11 14:39:09 -0500

    feat(edit-ema): Move Preview Device Selected to New Bar #27308 (#27313)
    * Added preview message on banner in EMA editor
    
    * Update core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/edit-ema-editor.component.scss
    
    ---------
    
    Co-authored-by: Freddy Montes <751424+fmontes@users.noreply.github.com>
    Co-authored-by: Jalinson Diaz <zjaaaldev@gmail.com>

--- core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-ema-device-display/dot-ema-device-display.component.spec.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-ema-device-display/dot-ema-device-display.component.spec.ts
@@ -1,7 +1,6 @@
 import { Spectator, byTestId, createComponentFactory } from '@ngneat/spectator/jest';
 
 import { CommonModule } from '@angular/common';
-import { By } from '@angular/platform-browser';
 
 import { mockDotDevices } from '@dotcms/utils-testing';
 
@@ -24,21 +23,9 @@ describe('DotEmaDeviceDisplayComponent', () => {
     });
 
     it('should show name, sizes and icon of the selected device', () => {
-        expect(spectator.query(byTestId('device-name')).textContent).toBe('iphone 200 x 100');
+        expect(spectator.query(byTestId('device-name')).textContent.trim()).toBe(
+            'iphone 200 x 100'
+        );
         expect(spectator.query(byTestId('device-icon'))).not.toBeNull();
-    });
-
-    it('should show a x button to reset the device selection', () => {
-        expect(spectator.query(byTestId('reset-device'))).not.toBeNull();
-    });
-
-    it("should emit deviceReset event when click on 'x' button", () => {
-        const resetDevice = jest.spyOn(spectator.component.resetDevice, 'emit');
-
-        const resetButton = spectator.debugElement.query(By.css('[data-testId="reset-device"]'));
-
-        spectator.triggerEventHandler(resetButton, 'onClick', {});
-
-        expect(resetDevice).toHaveBeenCalled();
     });
 });
