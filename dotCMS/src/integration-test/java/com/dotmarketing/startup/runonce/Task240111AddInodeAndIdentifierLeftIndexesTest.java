754fbd9fba8996f080e24595e1764e930b7b5a7e
nollymarlonga <nollymar@gmail.com>
2024-01-11 21:33:54 -0500

    #26926 Fixing IT

--- dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task240111AddInodeAndIdentifierLeftIndexesTest.java
+++ dotCMS/src/integration-test/java/com/dotmarketing/startup/runonce/Task240111AddInodeAndIdentifierLeftIndexesTest.java
@@ -4,15 +4,17 @@ import static org.junit.Assert.assertTrue;
 
 import com.dotcms.util.IntegrationTestInitService;
 import com.dotmarketing.common.db.DotDatabaseMetaData;
+import com.dotmarketing.db.DbConnectionFactory;
 import com.dotmarketing.exception.DotDataException;
-import com.google.common.collect.ImmutableList;
-import io.vavr.control.Try;
-import java.util.stream.Collectors;
+import com.dotmarketing.util.Logger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
- * Verifies that the {@link Task240111AddInodeAndIdentifierLeftIndexes} Upgrade Task is working as expected.
+ * Verifies that the {@link Task240111AddInodeAndIdentifierLeftIndexes} Upgrade Task is working as
+ * expected.
  *
  * @author Nollymar Longa
  * @since Jan 11st, 2024
@@ -35,18 +37,29 @@ public class Task240111AddInodeAndIdentifierLeftIndexesTest {
     public void executeTaskUpgrade() throws DotDataException {
         final Task240111AddInodeAndIdentifierLeftIndexes task = new Task240111AddInodeAndIdentifierLeftIndexes();
         task.executeUpgrade();
-        final DotDatabaseMetaData dbMetadata = new DotDatabaseMetaData();
 
-        assertTrue("inode_inode_leading_idx index should exist in the inode table", (Try.of(
-                () -> dbMetadata.getIndices("inode"))
-                .getOrElse(ImmutableList.of())).stream().map(String::toLowerCase).collect(
-                Collectors.toList()).contains("inode_inode_leading_idx"));
-
-        assertTrue("identifier_id_leading_idx index should exist in the identifier table", (Try.of(
-                        () -> dbMetadata.getIndices("identifier"))
-                .getOrElse(ImmutableList.of())).stream().map(String::toLowerCase).collect(
-                Collectors.toList()).contains("identifier_id_leading_idx"));
-
+        assertTrue("inode_inode_leading_idx index should exist in the inode table",
+                validateIndex("inode", "inode_inode_leading_idx"));
+        assertTrue("identifier_id_leading_idx index should exist in the identifier table",
+                validateIndex("identifier", "identifier_id_leading_idx"));
     }
 
+    private boolean validateIndex(final String tableName, final String indexName) {
+        final DotDatabaseMetaData dbMetadata = new DotDatabaseMetaData();
+
+        try {
+            final ResultSet indicesInfo = dbMetadata.getIndices(DbConnectionFactory.getConnection(),
+                    null, tableName, false);
+            while (indicesInfo.next()) {
+                final String currentIndex = indicesInfo.getString("INDEX_NAME");
+
+                if (currentIndex.equals(indexName)) {
+                    return true;
+                }
+            }
+        } catch (final SQLException e) {
+            Logger.error(this, e);
+        }
+        return false;
+    }
 }
