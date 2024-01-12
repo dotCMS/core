cbf436047db0f036d3e7a5da9412d1922431c823
Kevin Davila <144152756+KevinDavilaDotCMS@users.noreply.github.com>
2024-01-12 11:12:09 -0500

    chore(edit-ema): Translate Side Navigation Labels #27283 (#27323)
    * Added translation on Edit EMA Navbar
    
    * Added lint

--- core-web/libs/portlets/edit-ema/portlet/src/lib/dot-ema-shell/components/edit-ema-navigation-bar/edit-ema-navigation-bar.component.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/dot-ema-shell/components/edit-ema-navigation-bar/edit-ema-navigation-bar.component.ts
@@ -2,15 +2,17 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { DotMessagePipe } from '@dotcms/ui';
+
 import { NavigationBarItem } from '../../../shared/models';
 
 @Component({
     selector: 'dot-edit-ema-navigation-bar',
     standalone: true,
-    imports: [CommonModule, RouterModule],
     templateUrl: './edit-ema-navigation-bar.component.html',
     styleUrls: ['./edit-ema-navigation-bar.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [CommonModule, RouterModule, DotMessagePipe]
 })
 export class EditEmaNavigationBarComponent {
     @Input() items: NavigationBarItem[];
