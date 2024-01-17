18544ae622c717ae8dc88f9ae82b958060d80b49
Jonathan Gamba <jonathan.gamba@dotcms.com>
2024-01-17 14:28:40 -0600

    #26643 Update documentation and add sorting to traversal results
    Extended the login documentation to provide more usage guidance, while also improving security advice on password usage. In the code, updated the activation shortcut in the instance.adoc file and enhanced File Push service in the system by adding a sorting feature to the traversal results. Removed redundant sorting from FilesPush class as it has been transferred to the PushService class.

--- tools/dotcms-cli/cli/src/main/java/com/dotcms/api/client/files/PushServiceImpl.java
+++ tools/dotcms-cli/cli/src/main/java/com/dotcms/api/client/files/PushServiceImpl.java
@@ -93,7 +93,29 @@ public class PushServiceImpl implements PushService {
         }
 
         normalize(traversalResult);
+        sort(traversalResult);
+
         return traversalResult;
+    }
+
+    /**
+     * Sorts the given list of TraverseResult objects based on site and status.
+     *
+     * @param traversalResult the list of TraverseResult objects to be sorted
+     */
+    private void sort(ArrayList<TraverseResult> traversalResult) {
+
+        // Apply some sorting to ensure a proper processing order
+        traversalResult.sort((o1, o2) -> {
+            var left = o1.localPaths();
+            var right = o2.localPaths();
+            return left.site().compareTo(right.site());
+        });
+        traversalResult.sort((o1, o2) -> {
+            var left = o1.localPaths();
+            var right = o2.localPaths();
+            return right.status().compareTo(left.status());// Making sure "working" comes first
+        });
     }
 
     /**
