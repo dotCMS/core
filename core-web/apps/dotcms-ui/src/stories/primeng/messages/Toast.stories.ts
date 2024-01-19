6c07db46a573d298afc2050bc65253d35713cb87
Jalinson Diaz <zjaaaldev@gmail.com>
2024-01-19 12:41:17 -0300

    feat(ui): Implement Toast new design #27369 (#27370)
    * implement design system to toast component
    
    * fix magnitudes
    
    * apply feedback
    
    * apply feedback and control for icon
    
    * toast alignment fixed

--- core-web/apps/dotcms-ui/src/stories/primeng/messages/Toast.stories.ts
+++ core-web/apps/dotcms-ui/src/stories/primeng/messages/Toast.stories.ts
@@ -12,11 +12,56 @@ import { ToastComponent } from './Toast.component';
 export default {
     title: 'PrimeNG/Messages/Toast',
     component: ToastComponent,
+    args: {
+        severity: 'success',
+        summary: 'Success Message',
+        detail: 'The action "Publish" was executed succesfully',
+        position: 'top-right',
+        life: 2000,
+        icon: 'pi-check-circle'
+    },
+    argTypes: {
+        severity: {
+            options: ['success', 'info', 'error', 'warn'],
+            control: { type: 'select' },
+            description: 'Severity level of the message'
+        },
+        summary: {
+            control: { type: 'text' },
+            description: 'Summary text of the message'
+        },
+        detail: {
+            control: { type: 'text' },
+            description: 'Detail text of the message'
+        },
+        position: {
+            options: [
+                'top-right',
+                'top-left',
+                'bottom-left',
+                'bottom-right',
+                'top-center',
+                'bottom-center',
+                'center'
+            ],
+            control: { type: 'select' },
+            description: 'Position of the message'
+        },
+        life: {
+            control: { type: 'number' },
+            description: 'Delay in milliseconds to close the message'
+        },
+        icon: {
+            control: { type: 'text' },
+            description:
+                'Icon of the message check https://primeng.org/icons for icons. You can hide it by setting it to "hidden"'
+        }
+    },
     parameters: {
         docs: {
             description: {
                 component:
-                    'Tooltip directive provides advisory information for a component.: https://primefaces.org/primeng/showcase/#/tooltip'
+                    'Tooltip directive provides advisory information for a component.: https://www.primefaces.org/primeng-v15-lts/toast'
             }
         }
     },
