33e726cf807193493ed1310d6e1d83d0cdcaea07
spbolton <steve.bolton@dotcms.com>
2024-01-23 01:52:41 -0500

    cleanup gradle

---
+++ dotcms-integration/src/main/java/com/dotcms/ittests/DummyInterface.java
@@ -0,0 +1,9 @@
+package com.dotcms.ittests;
+
+/*
+This interface is here as a placeholder of non test code that is part of the dotcms-integration module
+Without something in main to compile tests will not run.
+ */
+public interface DummyInterface {
+    public void aMethod();
+}
