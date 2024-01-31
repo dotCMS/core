75308f99aea9f41d06f8e3d533d844d1036ff27b
erickgonzalez <erick.gonzalez@dotcms.com>
2024-01-31 14:23:31 -0600

    test: upgrade task test ref:#25891

--- dotcms-integration/src/test/java/com/dotmarketing/startup/runonce/Task240131UpdateLanguageVariableContentTypeTest.java
+++ dotcms-integration/src/test/java/com/dotmarketing/startup/runonce/Task240131UpdateLanguageVariableContentTypeTest.java
@@ -1,2 +1,46 @@
-package com.dotmarketing.startup.runonce;public class Task240131UpdateLanguageVariableContentTypeTest {
+package com.dotmarketing.startup.runonce;
+
+import com.dotcms.languagevariable.business.LanguageVariableAPI;
+import com.dotcms.util.IntegrationTestInitService;
+import com.dotmarketing.business.APILocator;
+import com.dotmarketing.business.CacheLocator;
+import com.dotmarketing.common.db.DotConnect;
+import com.dotmarketing.common.db.DotDatabaseMetaData;
+import com.dotmarketing.db.DbConnectionFactory;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotSecurityException;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class Task240131UpdateLanguageVariableContentTypeTest {
+    @BeforeClass
+    public static void prepare() throws Exception {
+        // Setting web app environment
+        IntegrationTestInitService.getInstance().init();
+    }
+
+    @Test
+    public void testExecuteUpgrade() throws DotDataException, SQLException, DotSecurityException {
+        //Update system to true in case it's false
+        final DotConnect dc = new DotConnect();
+        dc.setSQL("update structure set system = true where velocity_var_name = ?");
+        dc.addParam(LanguageVariableAPI.LANGUAGEVARIABLE_VAR_NAME);
+        dc.loadResult();
+        //Flush cache to get latest changes
+        CacheLocator.getContentTypeCache2().clearCache();
+        //System must be true
+        assertTrue(APILocator.getContentTypeAPI(APILocator.systemUser()).find(LanguageVariableAPI.LANGUAGEVARIABLE_VAR_NAME).system());
+        //Run UT
+        Task240131UpdateLanguageVariableContentType upgradeTask = new Task240131UpdateLanguageVariableContentType();
+        upgradeTask.executeUpgrade();
+        //Flush cache to get latest changes
+        CacheLocator.getContentTypeCache2().clearCache();
+        //System must be false
+        assertFalse(APILocator.getContentTypeAPI(APILocator.systemUser()).find(LanguageVariableAPI.LANGUAGEVARIABLE_VAR_NAME).system());
+    }
 }
