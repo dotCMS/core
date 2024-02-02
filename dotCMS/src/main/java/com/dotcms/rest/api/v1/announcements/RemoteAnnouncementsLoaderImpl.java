28eb6768e4c9dfd0ec6a9fd3f8c5c3fd6907d762
Fabrizzio Araya <37148755+fabrizzio-dotCMS@users.noreply.github.com>
2024-02-02 16:33:57 -0600

    feat(Announcements): correcting query that retrieves announcements from prod and fixing postman Ref:s#25736  (#27480)
    * #25736 the postman is set to create the CT taking into account the announcementDate as publish date
    
    * #25736 megre latest from master
    
    * #25736 allowing time for publishing to take effect
    
    * #25736 debugging postman failure
    
    * #25736 correcting announcements remote query
    
    * #25736 code clean up
    
    * #25736 updating postman test
    
    * #25736 url query fix
    
    * #25736 adjusting test
    
    * #25736 imrproved IT
    
    * #25736 log should be debug

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/announcements/RemoteAnnouncementsLoaderImpl.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/announcements/RemoteAnnouncementsLoaderImpl.java
@@ -3,16 +3,14 @@ package com.dotcms.rest.api.v1.announcements;
 import com.dotcms.content.business.json.ContentletJsonHelper;
 import com.dotcms.rest.RestClientBuilder;
 import com.dotcms.system.announcements.Announcement;
-import com.dotmarketing.business.APILocator;
 import com.dotmarketing.exception.DotRuntimeException;
-import com.dotmarketing.portlets.languagesmanager.business.LanguageAPI;
-import com.dotmarketing.portlets.languagesmanager.model.Language;
 import com.dotmarketing.util.Config;
 import com.dotmarketing.util.Logger;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import io.vavr.Lazy;
 import io.vavr.control.Try;
@@ -21,6 +19,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
 import java.util.List;
+import java.util.function.Supplier;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
@@ -31,35 +30,47 @@ import javax.ws.rs.core.Response;
  */
 public class RemoteAnnouncementsLoaderImpl implements AnnouncementsLoader{
 
-    //The CT varN/ame used to retrieve the Announcements
-    static final Lazy<String>DOT_ANNOUNCEMENT_CT =
-            Lazy.of(()-> Config.getStringProperty("DOT_ANNOUNCEMENT_CT", "Announcement"));
-
     //This is the url to the dotCMS instance set to provide and feed all consumers with announcements
-    static final  Lazy<String> ANNOUNCEMENTS_BASE_URL =
+    static final  Lazy<String> BASE_URL_LAZY_SUPPLIER =
             Lazy.of(() -> Config.getStringProperty("ANNOUNCEMENTS_BASE_URL", "https://www.dotcms.com"));
 
-    //The query pattern to retrieve the Announcements
-    static final String ANNOUNCEMENTS_QUERY_PATTERN = "%s/api/content/render/false/query/+contentType:%s +languageId:%d +deleted:false +live:true/orderBy/%s.announcementDate desc";
+    static final String ANNOUNCEMENTS_QUERY = "/api/content/render/false/query/+contentType:Announcement%20+languageId:1%20+deleted:false%20+live:true%20/orderby/Announcement.announcementDate%20desc";
+
+    Supplier<String> baseURL = BASE_URL_LAZY_SUPPLIER;
+
+    /**
+     * Default constructor
+     */
+    public RemoteAnnouncementsLoaderImpl() {
+    }
+
+    /**
+     * Constructor
+     * @param baseURL Supplier<String>
+     */
+    @VisibleForTesting
+    public RemoteAnnouncementsLoaderImpl(final Supplier<String> baseURL) {
+        this.baseURL = baseURL;
+    }
 
     /**
      * Load the announcements from the remote dotCMS instance
-     * @param language Language
      * @return List<Announcement>
      */
     @Override
-    public List<Announcement> loadAnnouncements(final Language language) {
-        final JsonNode jsonNode = loadRemoteAnnouncements(language);
+    public List<Announcement> loadAnnouncements() {
+        final JsonNode jsonNode = loadRemoteAnnouncements();
         return toAnnouncements(jsonNode);
     }
 
     /**
      * Load the announcements from the remote dotCMS instance
-     * @param language Language
      * @return JsonNode
      */
-    JsonNode loadRemoteAnnouncements(final Language language) {
-        final String url = buildURL(language);
+    JsonNode loadRemoteAnnouncements() {
+        final String url = buildURL();
+        //Let's log the url to retrieve the announcements for debugging purposes on postman
+        Logger.debug(AnnouncementsHelperImpl.class, String.format("loading announcements from [%s]", url));
         try {
             final Client client = restClient();
             final WebTarget webTarget = client.target(url);
@@ -83,12 +94,10 @@ public class RemoteAnnouncementsLoaderImpl implements AnnouncementsLoader{
 
     /**
      * Build the url to retrieve the announcements
-     * @param language Language
      * @return String
      */
-     String buildURL(Language language) {
-        final String raw = String.format(ANNOUNCEMENTS_QUERY_PATTERN, ANNOUNCEMENTS_BASE_URL.get(),
-                DOT_ANNOUNCEMENT_CT.get(), language.getId(), DOT_ANNOUNCEMENT_CT.get());
+    String buildURL() {
+        final String raw = baseURL.get() + ANNOUNCEMENTS_QUERY;
         //clean up double slashes in the url
         return raw.replaceAll("(?<!(http:|https:))//", "/");
     }
@@ -99,23 +108,6 @@ public class RemoteAnnouncementsLoaderImpl implements AnnouncementsLoader{
      */
     static Client restClient() {
         return RestClientBuilder.newClient();
-    }
-
-    /**
-     * Get the language by id or code, if not found fallback to default language
-     * @param languageIdOrCode String
-     * @return Language
-     */
-    Language getLanguage(final String languageIdOrCode) {
-        final LanguageAPI languageAPI = APILocator.getLanguageAPI();
-        Language language;
-        try {
-            language = languageAPI.getLanguage(languageIdOrCode);
-        } catch (Exception e) {
-            Logger.debug(AnnouncementsHelperImpl.class, String.format(" failed to get lang [%s] with message: [%s] fallback to default language", languageIdOrCode, e.getMessage()));
-            language = languageAPI.getDefaultLanguage();
-        }
-        return language;
     }
 
     DateTimeFormatter formatter = new DateTimeFormatterBuilder()
@@ -129,7 +121,7 @@ public class RemoteAnnouncementsLoaderImpl implements AnnouncementsLoader{
      * @param root JsonNode
      * @return List<Announcement>
      */
-     List<Announcement> toAnnouncements(final JsonNode root) {
+    List<Announcement> toAnnouncements(final JsonNode root) {
 
         final ImmutableList.Builder<Announcement> announcements = ImmutableList.builder();
         final JsonNode nodes = root.get("contentlets");
@@ -147,9 +139,6 @@ public class RemoteAnnouncementsLoaderImpl implements AnnouncementsLoader{
             final LocalDateTime date = LocalDateTime.parse(dateString, formatter);
             final LocalDateTime modDate = LocalDateTime.parse(modDateString, formatter);
 
-            final Language language = getLanguage(langId);
-
-
             announcements.add(
                     Announcement.builder()
                             .identifier(identifier)
@@ -159,8 +148,7 @@ public class RemoteAnnouncementsLoaderImpl implements AnnouncementsLoader{
                             .announcementDateAsISO8601(date.toString())
                             .type(type)
                             .url(url)
-                            .languageId(language.getId())
-                            .languageCode(language.getIsoCode())
+                            .languageId(langId)
                             .modDate(modDate.toInstant(java.time.ZoneOffset.UTC))
                             .modDateAsISO8601(modDate.toString())
                             .description(description)
