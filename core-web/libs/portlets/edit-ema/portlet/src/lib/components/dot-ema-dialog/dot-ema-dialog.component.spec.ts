0e8c52a4635143a755f4b85dc76a7d073c3526d4
Jalinson Diaz <zjaaaldev@gmail.com>
2024-02-07 17:55:37 -0300

    Update dot-ema-dialog.component.spec.ts

--- core-web/libs/portlets/edit-ema/portlet/src/lib/components/dot-ema-dialog/dot-ema-dialog.component.spec.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/components/dot-ema-dialog/dot-ema-dialog.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { DotEmaDialogComponent } from './dot-ema-dialog.component';
 
 describe('DotEmaDialogComponent', () => {
