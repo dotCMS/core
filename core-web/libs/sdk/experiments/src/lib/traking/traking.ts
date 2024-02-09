72a3d00e6e1b3288d8faf15107de14dac36aac09
Arcadio Quintero <oidacra@gmail.com>
2024-02-09 11:09:42 -0500

    dotCMS/core#27536 Creation of a Centralized, Buildable Experiment Logic Library in Nx (#27558)
    * feat(sdk-experiments) initial setup of sdk-experiments library
    
    * feat(sdk-experiments) update yarn.lock
    
    * feat(sdk-experiments) add eslint rule
    
    * feat(sdk-experiments) fix comments
    
    * fix(chore) fix prettier format
    
    * feat(sdk-experiments) fix comments
    
    * feat(sdk-experiments) fix name of the project and workspace

---
+++ core-web/libs/sdk/experiments/src/lib/traking/traking.ts
@@ -0,0 +1,12 @@
+/**
+ * Function to implement tracking.
+ *
+ * @returns {string} A message indicating that the tracking functionality needs to be implemented.
+ *
+ * @example
+ * // Usage
+ * tracking(); // Returns "implement tracking"
+ */
+export const tracking = (): string => {
+  return 'implement tracking';
+};
---
+++ core-web/libs/sdk/experiments/src/lib/validator/validator.ts
@@ -0,0 +1,7 @@
+/**
+ * Represents a validator function.
+ * @returns {string} - A placeholder message indicating that the validator function needs to be implemented.
+ */
+export const validator = (): string => {
+  return 'implement validator';
+};
