0305bee8d028f3b559cf938883d6a850565b1ef6
Will Ezell <will@dotcms.com>
2024-02-09 14:53:12 -0500

    task(deps) removing stxx
    ref:#27579

--- dotCMS/src/test/java/com/dotmarketing/util/ConfigTest.java
+++ dotCMS/src/test/java/com/dotmarketing/util/ConfigTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -386,15 +387,24 @@ public class ConfigTest {
         setTestEnvVariables();
     }
 
-    private static void removeStaticFinalAndSetValue(Field field, Object value) throws Exception {
+
+
+    private static void removeStaticFinalAndSetValue(Field field, Object newValue) throws Exception {
         field.setAccessible(true);
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        Method getDeclaredFields0 = Class.class.getDeclaredMethod("getDeclaredFields0", boolean.class);
+        getDeclaredFields0.setAccessible(true);
+        Field[] fields = (Field[]) getDeclaredFields0.invoke(Field.class, false);
+        Field modifiersField = null;
+        for (Field each : fields) {
+            if ("modifiers".equals(each.getName())) {
+                modifiersField = each;
+                break;
+            }
+        }
         modifiersField.setAccessible(true);
         modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-        field.set(null, value);
+        field.set(null, newValue);
     }
-
-
 
 
 
