6590bfe2ba5c2e1cb369b4d69591ad18636cf596
Jalinson Diaz <zjaaaldev@gmail.com>
2024-02-09 16:49:08 -0300

    get rid of allowFileNameEdit and always show it by default

--- core-web/apps/dotcms-ui/src/app/portlets/shared/dot-content-types-edit/components/dot-binary-settings/dot-binary-settings.component.spec.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/shared/dot-content-types-edit/components/dot-binary-settings/dot-binary-settings.component.spec.ts
@@ -17,148 +17,140 @@ import { DotBinarySettingsComponent } from './dot-binary-settings.component';
 import { DotFieldVariablesService } from '../fields/dot-content-type-fields-variables/services/dot-field-variables.service';
 
 const messageServiceMock = new MockDotMessageService({
-    'contenttypes.dropzone.action.save': 'Save',
-    'contenttypes.dropzone.action.cancel': 'Cancel'
+  'contenttypes.dropzone.action.save': 'Save',
+  'contenttypes.dropzone.action.cancel': 'Cancel',
 });
 
 const SYSTEM_OPTIONS = JSON.stringify({
-    allowURLImport: false,
-    allowFileNameEdit: false,
-    allowCodeWrite: true
+  allowURLImport: false,
+  allowFileNameEdit: false,
+  allowCodeWrite: true,
 });
 
 describe('DotBinarySettingsComponent', () => {
-    let spectator: Spectator<DotBinarySettingsComponent>;
-    let component: DotBinarySettingsComponent;
-    let dotFieldVariableService: SpyObject<DotFieldVariablesService>;
-    let dotHttpErrorManagerService: SpyObject<DotHttpErrorManagerService>;
+  let spectator: Spectator<DotBinarySettingsComponent>;
+  let component: DotBinarySettingsComponent;
+  let dotFieldVariableService: SpyObject<DotFieldVariablesService>;
+  let dotHttpErrorManagerService: SpyObject<DotHttpErrorManagerService>;
 
-    const createComponent = createComponentFactory({
-        component: DotBinarySettingsComponent,
-        imports: [
-            NgFor,
-            FormsModule,
-            ReactiveFormsModule,
-            InputTextModule,
-            InputSwitchModule,
-            DividerModule,
-            DotMessagePipe
-        ],
-        providers: [
-            FormBuilder,
-            {
-                provide: DotFieldVariablesService,
-                useValue: {
-                    load: () =>
-                        of([
-                            {
-                                clazz: 'com.dotcms.contenttype.model.field.ImmutableStoryBlockField',
-                                fieldId: 'f965a51b-130a-435f-b646-41e07d685363',
-                                id: '9671d2c3-793b-41af-a485-e2c5fcba5fb',
-                                key: 'systemOptions',
-                                value: SYSTEM_OPTIONS
-                            },
-                            {
-                                clazz: 'com.dotcms.contenttype.model.field.ImmutableStoryBlockField',
-                                fieldId: 'f965a51b-130a-435f-b646-41e07d685363',
-                                id: '9671d2c3-793b-41af-a485-e2c5fcba5fb',
-                                key: 'accept',
-                                value: 'image/*'
-                            }
-                        ]),
-                    save: () => of([]),
-                    delete: () => of([])
-                }
-            },
-            {
-                provide: DotMessageService,
-                useValue: messageServiceMock
-            },
-            {
-                provide: DotHttpErrorManagerService,
-                useValue: {
-                    handle: () => of([])
-                }
-            }
-        ]
+  const createComponent = createComponentFactory({
+    component: DotBinarySettingsComponent,
+    imports: [
+      NgFor,
+      FormsModule,
+      ReactiveFormsModule,
+      InputTextModule,
+      InputSwitchModule,
+      DividerModule,
+      DotMessagePipe,
+    ],
+    providers: [
+      FormBuilder,
+      {
+        provide: DotFieldVariablesService,
+        useValue: {
+          load: () =>
+            of([
+              {
+                clazz: 'com.dotcms.contenttype.model.field.ImmutableStoryBlockField',
+                fieldId: 'f965a51b-130a-435f-b646-41e07d685363',
+                id: '9671d2c3-793b-41af-a485-e2c5fcba5fb',
+                key: 'systemOptions',
+                value: SYSTEM_OPTIONS,
+              },
+              {
+                clazz: 'com.dotcms.contenttype.model.field.ImmutableStoryBlockField',
+                fieldId: 'f965a51b-130a-435f-b646-41e07d685363',
+                id: '9671d2c3-793b-41af-a485-e2c5fcba5fb',
+                key: 'accept',
+                value: 'image/*',
+              },
+            ]),
+          save: () => of([]),
+          delete: () => of([]),
+        },
+      },
+      {
+        provide: DotMessageService,
+        useValue: messageServiceMock,
+      },
+      {
+        provide: DotHttpErrorManagerService,
+        useValue: {
+          handle: () => of([]),
+        },
+      },
+    ],
+  });
+
+  beforeEach(async () => {
+    spectator = createComponent({});
+    dotFieldVariableService = spectator.inject(DotFieldVariablesService);
+    dotHttpErrorManagerService = spectator.inject(DotHttpErrorManagerService);
+
+    component = spectator.component;
+  });
+
+  it('should setup form values', () => {
+    expect(component.form.get('accept').value).toBe('image/*');
+    expect(component.form.get('systemOptions').value).toEqual({
+      allowURLImport: false,
+      allowCodeWrite: true,
     });
+  });
 
-    beforeEach(async () => {
-        spectator = createComponent({});
-        dotFieldVariableService = spectator.inject(DotFieldVariablesService);
-        dotHttpErrorManagerService = spectator.inject(DotHttpErrorManagerService);
+  it('should emit changeControls when isVisible input is true', () => {
+    spyOn(component.changeControls, 'emit');
 
-        component = spectator.component;
-    });
+    spectator.setInput('isVisible', true);
 
-    it('should setup form values', () => {
-        expect(component.form.get('accept').value).toBe('image/*');
-        expect(component.form.get('systemOptions').value).toEqual({
-            allowURLImport: false,
-            allowCodeWrite: true,
-            allowFileNameEdit: false
-        });
-    });
+    expect(component.changeControls.emit).toHaveBeenCalled();
+  });
 
-    it('should emit changeControls when isVisible input is true', () => {
-        spyOn(component.changeControls, 'emit');
+  it('should emit valid output on form change', () => {
+    spyOn(component.valid, 'emit');
 
-        spectator.setInput('isVisible', true);
+    const acceptInput = spectator.query(byTestId('setting-accept'));
+    spectator.typeInElement('text/*', acceptInput);
 
-        expect(component.changeControls.emit).toHaveBeenCalled();
-    });
+    expect(component.valid.emit).toHaveBeenCalled();
+  });
 
-    it('should emit valid output on form change', () => {
-        spyOn(component.valid, 'emit');
+  it('should handler error if save properties failed', () => {
+    spyOn(dotFieldVariableService, 'save').and.returnValue(throwError({}));
+    spyOn(dotHttpErrorManagerService, 'handle').and.returnValue(of());
+    spyOn(component.save, 'emit');
 
-        const acceptInput = spectator.query(byTestId('setting-accept'));
-        spectator.typeInElement('text/*', acceptInput);
+    component.saveSettings();
 
-        expect(component.valid.emit).toHaveBeenCalled();
-    });
+    expect(dotHttpErrorManagerService.handle).toHaveBeenCalledTimes(1);
+    expect(component.save.emit).not.toHaveBeenCalled();
+  });
+  it('should not call save or delete when is empty and not previous variable exist', () => {
+    spyOn(dotFieldVariableService, 'load');
+    spyOn(dotFieldVariableService, 'delete').and.returnValue(of([]));
+    spyOn(dotFieldVariableService, 'save').and.returnValue(of([]));
 
-    it('should handler error if save properties failed', () => {
-        spyOn(dotFieldVariableService, 'save').and.returnValue(throwError({}));
-        spyOn(dotHttpErrorManagerService, 'handle').and.returnValue(of());
-        spyOn(component.save, 'emit');
+    component.form.get('accept').setValue('');
+    component.saveSettings();
 
-        component.saveSettings();
+    expect(dotFieldVariableService.delete).not.toHaveBeenCalled();
+    expect(dotFieldVariableService.save).not.toHaveBeenCalledTimes(2); // One for accept and one for systemOptions, accept should not call save or delete
+  });
 
-        expect(dotHttpErrorManagerService.handle).toHaveBeenCalledTimes(1);
-        expect(component.save.emit).not.toHaveBeenCalled();
-    });
-    it('should not call save or delete when is empty and not previous variable exist', () => {
-        spyOn(dotFieldVariableService, 'load');
-        spyOn(dotFieldVariableService, 'delete').and.returnValue(of([]));
-        spyOn(dotFieldVariableService, 'save').and.returnValue(of([]));
+  it('should have 2 switches with the corresponding control name', () => {
+    const switches = spectator.queryAll(byTestId('setting-switch'));
 
-        component.form.get('accept').setValue('');
-        component.saveSettings();
+    expect(switches.length).toBe(2);
+    expect(switches.find((s) => s.getAttribute('ng-reflect-name') === 'allowURLImport')).not.toBeNull();
+    expect(switches.find((s) => s.getAttribute('ng-reflect-name') === 'allowCodeWrite')).not.toBeNull();
+  });
 
-        expect(dotFieldVariableService.delete).not.toHaveBeenCalled();
-        expect(dotFieldVariableService.save).not.toHaveBeenCalledTimes(2); // One for accept and one for systemOptions, accept should not call save or delete
-    });
+  it('should have 1 input with the control name accept', () => {
+    const [acceptInput] = spectator.queryAll(byTestId('setting-accept'));
 
-    it('should have 3 switches with the corresponding control name', () => {
-        const switches = spectator.queryAll(byTestId('setting-switch'));
+    expect(acceptInput).not.toBeNull();
 
-        expect(switches.length).toBe(3);
-        expect(
-            switches.find((s) => s.getAttribute('ng-reflect-name') === 'allowURLImport')
-        ).not.toBeNull();
-        expect(
-            switches.find((s) => s.getAttribute('ng-reflect-name') === 'allowCodeWrite')
-        ).not.toBeNull();
-        expect(
-            switches.find((s) => s.getAttribute('ng-reflect-name') === 'allowFileNameEdit')
-        ).not.toBeNull();
-    });
-
-    it('should have 1 input with the control name accept', () => {
-        const [acceptInput] = spectator.queryAll(byTestId('setting-accept'));
-
-        expect(acceptInput).not.toBeNull();
-
-        expect(acceptInput.getAttribute('ng-reflect-name')).toBe('accept');
-    });
+    expect(acceptInput.getAttribute('ng-reflect-name')).toBe('accept');
+  });
 });
--- core-web/apps/dotcms-ui/src/app/portlets/shared/dot-content-types-edit/components/dot-binary-settings/dot-binary-settings.component.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/shared/dot-content-types-edit/components/dot-binary-settings/dot-binary-settings.component.ts
@@ -3,16 +3,16 @@ import { forkJoin, of } from 'rxjs';
 import { NgFor } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import {
-    ChangeDetectionStrategy,
-    Component,
-    DestroyRef,
-    EventEmitter,
-    Input,
-    OnChanges,
-    OnInit,
-    Output,
-    SimpleChanges,
-    inject
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  inject,
 } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -31,140 +31,125 @@ import { DotMessagePipe } from '@dotcms/ui';
 import { DotFieldVariablesService } from '../fields/dot-content-type-fields-variables/services/dot-field-variables.service';
 
 @Component({
-    selector: 'dot-binary-settings',
-    standalone: true,
-    imports: [
-        NgFor,
-        FormsModule,
-        ReactiveFormsModule,
-        InputTextModule,
-        InputSwitchModule,
-        DividerModule,
-        DotMessagePipe
-    ],
-    templateUrl: './dot-binary-settings.component.html',
-    styleUrl: './dot-binary-settings.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'dot-binary-settings',
+  standalone: true,
+  imports: [NgFor, FormsModule, ReactiveFormsModule, InputTextModule, InputSwitchModule, DividerModule, DotMessagePipe],
+  templateUrl: './dot-binary-settings.component.html',
+  styleUrl: './dot-binary-settings.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DotBinarySettingsComponent implements OnInit, OnChanges {
-    @Input() field: DotCMSContentTypeField;
-    @Input() isVisible: boolean = false;
+  @Input() field: DotCMSContentTypeField;
+  @Input() isVisible: boolean = false;
 
-    @Output() changeControls = new EventEmitter<DotDialogActions>();
-    @Output() valid = new EventEmitter<boolean>();
-    @Output() save = new EventEmitter<DotFieldVariable[]>();
+  @Output() changeControls = new EventEmitter<DotDialogActions>();
+  @Output() valid = new EventEmitter<boolean>();
+  @Output() save = new EventEmitter<DotFieldVariable[]>();
 
-    form: FormGroup;
+  form: FormGroup;
 
-    private fb: FormBuilder = inject(FormBuilder);
-    private fieldVariablesService = inject(DotFieldVariablesService);
-    private dotMessageService = inject(DotMessageService);
-    private dotHttpErrorManagerService = inject(DotHttpErrorManagerService);
-    private readonly destroyRef = inject(DestroyRef);
-    private FIELD_VARIABLES: Record<string, DotFieldVariable> = {};
+  private readonly fb: FormBuilder = inject(FormBuilder);
+  private readonly fieldVariablesService = inject(DotFieldVariablesService);
+  private readonly dotMessageService = inject(DotMessageService);
+  private readonly dotHttpErrorManagerService = inject(DotHttpErrorManagerService);
+  private readonly destroyRef = inject(DestroyRef);
 
-    protected readonly systemOptions = [
-        {
-            key: 'allowURLImport',
-            message: 'binary-field.settings.system.options.allow.url.import'
-        },
-        {
-            key: 'allowCodeWrite',
-            message: 'binary-field.settings.system.options.allow.code.write'
-        },
-        {
-            key: 'allowFileNameEdit',
-            message: 'binary-field.settings.system.options.allow.file.name.edit'
-        }
-    ];
+  private FIELD_VARIABLES: Record<string, DotFieldVariable> = {};
 
-    ngOnChanges(changes: SimpleChanges) {
-        const { isVisible } = changes;
-        if (isVisible?.currentValue) {
-            this.changeControls.emit(this.dialogActions());
-        }
+  protected readonly systemOptions = [
+    {
+      key: 'allowURLImport',
+      message: 'binary-field.settings.system.options.allow.url.import',
+    },
+    {
+      key: 'allowCodeWrite',
+      message: 'binary-field.settings.system.options.allow.code.write',
+    },
+  ];
+
+  ngOnChanges(changes: SimpleChanges) {
+    const { isVisible } = changes;
+    if (isVisible?.currentValue) {
+      this.changeControls.emit(this.dialogActions());
     }
+  }
 
-    ngOnInit(): void {
-        this.form = this.fb.group({
-            accept: '',
-            systemOptions: this.fb.group({
-                allowURLImport: false,
-                allowCodeWrite: false,
-                allowFileNameEdit: false
-            })
-        });
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      accept: '',
+      systemOptions: this.fb.group({
+        allowURLImport: false,
+        allowCodeWrite: false,
+      }),
+    });
 
-        this.form.valueChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => {
-            this.valid.emit(this.form.valid);
-        });
+    this.form.valueChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => {
+      this.valid.emit(this.form.valid);
+    });
 
-        this.fieldVariablesService.load(this.field).subscribe({
-            next: (fieldVariables: DotFieldVariable[]) => {
-                fieldVariables.forEach((variable) => {
-                    const { key, value } = variable;
-                    const control = this.form.get(key);
-                    if (control instanceof FormGroup) {
-                        const systemOptions = JSON.parse(value);
+    this.fieldVariablesService.load(this.field).subscribe({
+      next: (fieldVariables: DotFieldVariable[]) => {
+        fieldVariables.forEach((variable) => {
+          const { key, value } = variable;
+          const control = this.form.get(key);
+          if (control instanceof FormGroup) {
+            const systemOptions = JSON.parse(value);
 
-                        this.systemOptions.forEach(({ key }) => {
-                            control.get(key)?.setValue(systemOptions[key]);
-                        });
-                    } else {
-                        control.setValue(value);
-                    }
-
-                    this.FIELD_VARIABLES = { ...this.FIELD_VARIABLES, [key]: variable };
-                });
-            }
-        });
-    }
-
-    saveSettings(): void {
-        const updateActions = Object.keys(this.form.controls).map((key) => {
-            const control = this.form.get(key);
-            const value =
-                control instanceof FormGroup ? JSON.stringify(control.value) : control.value;
-            const fieldVariable: DotFieldVariable = {
-                ...this.FIELD_VARIABLES[key],
-                key,
-                value
-            };
-
-            if (!value) {
-                return of({});
-            }
-
-            return (
-                value
-                    ? this.fieldVariablesService.save(this.field, fieldVariable)
-                    : this.fieldVariablesService.delete(this.field, fieldVariable)
-            ).pipe(tap((variable) => (this.FIELD_VARIABLES[key] = variable))); // Update Variable Reference
-        });
-
-        forkJoin(updateActions)
-            .pipe(
-                take(1),
-                catchError((err: HttpErrorResponse) =>
-                    this.dotHttpErrorManagerService.handle(err).pipe(take(1))
-                )
-            )
-            .subscribe((value: DotFieldVariable[]) => {
-                this.form.markAsPristine();
-                this.save.emit(value);
+            this.systemOptions.forEach(({ key }) => {
+              control.get(key)?.setValue(systemOptions[key]);
             });
-    }
+          } else {
+            control.setValue(value);
+          }
 
-    private dialogActions() {
-        return {
-            cancel: {
-                label: this.dotMessageService.get('contenttypes.dropzone.action.cancel')
-            },
-            accept: {
-                action: () => this.saveSettings(),
-                disabled: this.form.invalid || this.form.pristine,
-                label: this.dotMessageService.get('contenttypes.dropzone.action.save')
-            }
-        };
-    }
+          this.FIELD_VARIABLES = { ...this.FIELD_VARIABLES, [key]: variable };
+        });
+      },
+    });
+  }
+
+  saveSettings(): void {
+    const updateActions = Object.keys(this.form.controls).map((key) => {
+      const control = this.form.get(key);
+      const value = control instanceof FormGroup ? JSON.stringify(control.value) : control.value;
+      const fieldVariable: DotFieldVariable = {
+        ...this.FIELD_VARIABLES[key],
+        key,
+        value,
+      };
+
+      if (!value) {
+        return of({});
+      }
+
+      return (
+        value
+          ? this.fieldVariablesService.save(this.field, fieldVariable)
+          : this.fieldVariablesService.delete(this.field, fieldVariable)
+      ).pipe(tap((variable) => (this.FIELD_VARIABLES[key] = variable))); // Update Variable Reference
+    });
+
+    forkJoin(updateActions)
+      .pipe(
+        take(1),
+        catchError((err: HttpErrorResponse) => this.dotHttpErrorManagerService.handle(err).pipe(take(1)))
+      )
+      .subscribe((value: DotFieldVariable[]) => {
+        this.form.markAsPristine();
+        this.save.emit(value);
+      });
+  }
+
+  private dialogActions() {
+    return {
+      cancel: {
+        label: this.dotMessageService.get('contenttypes.dropzone.action.cancel'),
+      },
+      accept: {
+        action: () => this.saveSettings(),
+        disabled: this.form.invalid || this.form.pristine,
+        label: this.dotMessageService.get('contenttypes.dropzone.action.save'),
+      },
+    };
+  }
 }
