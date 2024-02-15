7aa598f7a25b69a0ce9e8c6d4c6ca49b243a27dc
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-02-14 23:32:00 -0600

    Add activity

--- ema/vuejs/src/main.js
+++ ema/vuejs/src/main.js
@@ -5,11 +5,12 @@ import App from './App.vue'
 import router from './router'
 
 import Banner from './components/Banner.vue'
+import Activity from './components/Activity.vue'
 
 const app = createApp(App)
 
 app.use(router)
 
-app.component('Banner', Banner)
+app.component('Banner', Banner).component('Activity', Activity)
 
 app.mount('#app')
