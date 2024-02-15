84c7f2f4a3adf451bc143a1ee7a1e6e988e037b6
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-02-14 23:38:57 -0600

    Add product and webpagecontent

--- ema/vuejs/src/main.js
+++ ema/vuejs/src/main.js
@@ -7,11 +7,18 @@ import router from './router'
 import Banner from './components/Banner.vue'
 import Activity from './components/Activity.vue'
 import Image from './components/Image.vue'
+import Product from './components/Product.vue'
+import WebPageContent from './components/WebPageContent.vue'
 
 const app = createApp(App)
 
 app.use(router)
 
-app.component('Banner', Banner).component('Activity', Activity).component('Image', Image)
+app
+  .component('Banner', Banner)
+  .component('Activity', Activity)
+  .component('Image', Image)
+  .component('Product', Product)
+  .component('webPageContent', WebPageContent)
 
 app.mount('#app')
