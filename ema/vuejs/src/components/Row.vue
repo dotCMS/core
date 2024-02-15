11e8c6422db3995de47c86fe2617606744e5259f
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-02-14 21:44:33 -0600

    Pass content inject

---
+++ ema/vuejs/src/components/Row.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  inject: ['data'],
+  mounted() {
+    console.log(this.data) // This should now reflect updates
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p>Row</p>
+    <p v-if="data">{{ data.page.friendlyName }}</p>
+  </div>
+</template>
--- ema/vuejs/src/views/AllPages.vue
+++ ema/vuejs/src/views/AllPages.vue
@@ -1,37 +1,47 @@
 <script>
+import { ref, provide } from 'vue'
 import { client } from '../utils/client'
+import Row from '../components/Row.vue'
 
-// Define fetchData as a standalone function
 async function fetchData(routePath, callback) {
   const data = await client.page
     .get({
-      path: routePath || 'index', // Adjust based on your routing logic
+      path: routePath || 'index',
       language_id: 1
     })
-    .then(({ entity }) => {
-      return entity
-    })
+    .then(({ entity }) => entity)
 
   callback(data)
 }
 
 export default {
   name: 'AllPages',
-  data() {
-    return {
-      data: null
+  components: {
+    Row
+  },
+  setup() {
+    const data = ref(null)
+
+    provide('data', data)
+
+    // Function to update data
+    const updateData = (newData) => {
+      data.value = newData
     }
+
+    // Expose updateData so it can be used in lifecycle hooks
+    return { data, updateData }
   },
   beforeRouteEnter(to, from, next) {
-    // Directly use fetchData function
-    fetchData(to.href, (data) => {
-      next((vm) => (vm.data = data))
+    fetchData(to.path, (data) => {
+      next((vm) => {
+        vm.updateData(data) // Use the exposed method to update data
+      })
     })
   },
   beforeRouteUpdate(to, from, next) {
-    // Use fetchData within the component context
-    fetchData(to.href, (data) => {
-      this.data = data
+    fetchData(to.path, (data) => {
+      this.updateData(data) // Directly use the method within the component instance
       next()
     })
   }
@@ -41,5 +51,6 @@ export default {
 <template>
   <main>
     <p v-if="data">{{ data.page.friendlyName }}</p>
+    <Row />
   </main>
 </template>
