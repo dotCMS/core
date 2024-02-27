e256c1ed3798846750d9c596b29b463bcae5f561
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2024-02-27 13:19:32 -0600

    #27386 clean up code

--- tools/dotcms-cli/cli/src/main/java/com/dotcms/api/client/pull/PullHandler.java
+++ tools/dotcms-cli/cli/src/main/java/com/dotcms/api/client/pull/PullHandler.java
@@ -1,16 +1,10 @@
 package com.dotcms.api.client.pull;
 
-import com.dotcms.api.client.pull.error.ErrorPrinterStrategy;
-import com.dotcms.api.client.pull.error.strategy.DefaultErrorPrinterStrategy;
-import com.dotcms.api.client.pull.error.strategy.PullErrorPrinterStrategy;
-import com.dotcms.api.client.pull.error.strategy.TraversalTaskErrorPrinterStrategy;
 import com.dotcms.cli.common.OutputOptionMixin;
 import com.dotcms.model.pull.PullOptions;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.concurrent.ExecutionException;
-import javax.annotation.PostConstruct;
 import picocli.CommandLine.ExitCode;
 
 /**
@@ -19,15 +13,6 @@ import picocli.CommandLine.ExitCode;
  * @param <T> the type of elements to be pulled
  */
 public abstract class PullHandler<T> {
-
-    private final List<ErrorPrinterStrategy> strategies = new ArrayList<>();
-
-    @PostConstruct
-    public void prepareStrategies() {
-        this.strategies.add(new TraversalTaskErrorPrinterStrategy());
-        this.strategies.add(new PullErrorPrinterStrategy());
-        this.strategies.add(new DefaultErrorPrinterStrategy()); // Default strategy should be last
-    }
 
     /**
      * Returns the title for the T elements being pulled. Used for logging purposes and for console
@@ -58,36 +43,6 @@ public abstract class PullHandler<T> {
     public abstract int pull(List<T> contents,
             PullOptions pullOptions,
             OutputOptionMixin output) throws ExecutionException, InterruptedException;
-
-    /**
-     * Prints the errors encountered during the pull process.
-     *
-     * @param errors The list of errors to be printed.
-     * @param output The output option mixin for providing output messages.
-     */
-    protected void printErrors(final List<Exception> errors, final OutputOptionMixin output) {
-
-        if (!errors.isEmpty()) {
-
-            output.info(
-                    String.format(
-                            "%n%nFound [@|bold,red %s|@] errors during the pull process:",
-                            errors.size()
-                    )
-            );
-            for (final var error : errors) {
-
-                for (ErrorPrinterStrategy strategy : strategies) {
-                    if (strategy.isApplicable(error)) {
-                        output.error(strategy.getErrorMessage(error));
-                        break; // As soon as we found an applicable strategy, we break the loop
-                    }
-                }
-            }
-
-            output.info(String.format("%n%n"));
-        }
-    }
 
     /**
      * Handles the exceptions that occurred during the pull process.
