03e95c30fb1ee67184dce1894affa191cff0f56e
Neehakethi <neeha.kethi@dotcms.com>
2024-03-01 00:29:04 +0530

    issue-26004 Removing commented part of Unexportable annotation

--- dotCMS/src/main/java/com/dotcms/contenttype/model/field/ImageField.java
+++ dotCMS/src/main/java/com/dotcms/contenttype/model/field/ImageField.java
@@ -16,7 +16,6 @@ import org.immutables.value.Value;
 @JsonSerialize(as = ImmutableImageField.class)
 @JsonDeserialize(as = ImmutableImageField.class)
 @Value.Immutable
-//@com.dotcms.business.Unexportable
 public abstract class ImageField extends Field {
 
 	/**
