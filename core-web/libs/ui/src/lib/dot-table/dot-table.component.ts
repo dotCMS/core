e89b7939cb19d47c3c82cb859c579f848bdf3222
Manuel Rojas <manuel.rojas.21@gmail.com>
2024-03-12 09:21:44 -0600

    #27702 Adding initial datable

---
+++ core-web/libs/ui/src/lib/dot-table/dot-table.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import { TableModule } from 'primeng/table';
+
+@Component({
+  selector: 'dot-table',
+  standalone: true,
+  imports: [TableModule],
+  templateUrl: './dot-table.component.html',
+  styleUrl: './dot-table.component.scss'
+})
+export class DotTableComponent {
+
+  products = [
+   {
+      "code": 1,
+      "name": "Computer",
+      "price": 1200
+    },
+    {
+      "code": 2,
+      "name": "Printer",
+      "price": 200
+   }
+  ]
+
+}
