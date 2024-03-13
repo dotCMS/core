2a64fc8aa34982006005ffa5ce93d998988ec47e
Rafael Velazco <rjvelazco21@gmail.com>
2024-03-13 14:26:18 -0400

    chore: code cleanup

--- core-web/libs/edit-content/src/lib/fields/dot-wysiwyg-field/dot-wysiwyg-field.component.spec.ts
+++ core-web/libs/edit-content/src/lib/fields/dot-wysiwyg-field/dot-wysiwyg-field.component.spec.ts
@@ -3,12 +3,12 @@ import { Spectator, createComponentFactory } from '@ngneat/spectator';
 import { DotWYSIWYGFieldComponent } from './dot-wysiwyg-field.component';
 
 describe('DotWYSIWYGFieldComponent', () => {
-  let spectator: Spectator<DotWYSIWYGFieldComponent>;
-  const createComponent = createComponentFactory(DotWYSIWYGFieldComponent);
+    let spectator: Spectator<DotWYSIWYGFieldComponent>;
+    const createComponent = createComponentFactory(DotWYSIWYGFieldComponent);
 
-  it('should create', () => {
-    spectator = createComponent();
+    it('should create', () => {
+        spectator = createComponent();
 
-    expect(spectator.component).toBeTruthy();
-  });
+        expect(spectator.component).toBeTruthy();
+    });
 });
