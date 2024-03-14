9ec70fe64253f48d8879192f9dd212b9b24e9aec
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2024-03-13 22:18:29 -0600

    #27703 UT should able deal with non-exising ct

--- dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task240306MigrateLegacyLanguageVariables.java
+++ dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task240306MigrateLegacyLanguageVariables.java
@@ -2,91 +2,77 @@ package com.dotmarketing.startup.runonce;
 
 import static com.dotcms.languagevariable.business.LanguageVariableAPI.LANGUAGEVARIABLE_VAR_NAME;
 
-import com.dotcms.contenttype.model.type.ContentType;
-import com.dotcms.contenttype.model.type.KeyValueContentType;
-import com.dotmarketing.business.APILocator;
+import com.dotcms.languagevariable.business.ImmutableMigrationSummary;
+import com.dotcms.languagevariable.business.LegacyLangVarMigrationHelper;
+import com.dotmarketing.common.db.DotConnect;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotRuntimeException;
-import com.dotmarketing.portlets.contentlet.model.Contentlet;
-import com.dotmarketing.portlets.contentlet.model.IndexPolicy;
 import com.dotmarketing.startup.StartupTask;
-import com.dotmarketing.util.ConfigUtils;
 import com.dotmarketing.util.Logger;
-import com.google.errorprone.annotations.CanIgnoreReturnValue;
-import com.liferay.portal.language.LanguageUtil;
-import io.vavr.Lazy;
-import io.vavr.control.Try;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.stream.Stream;
+import java.util.Optional;
 
 public class Task240306MigrateLegacyLanguageVariables implements StartupTask {
 
-    Path messagesDir() {
-        return Paths.get(ConfigUtils.getDynamicContentPath(),"messages");
-     }
+    private ImmutableMigrationSummary migrationSummary = null;
+
     @Override
     public boolean forceRun() {
         return true;
     }
 
+    /**
+     * Checks if the language variable content type exists
+     * @return the inode of the language variable content type
+     */
+    public static Optional<String> langVarContentType() {
+        final String inode = new DotConnect().setSQL(
+                        "select inode from structure where velocity_var_name like ?")
+                .addParam(LANGUAGEVARIABLE_VAR_NAME).getString("inode");
+        return Optional.ofNullable(inode);
+    }
+
+    /**
+     * Checks if the language variable content type exists, if not, it creates it
+     * @return the inode of the language variable content type
+     * @throws DotDataException if there is an error creating the content type
+     */
+    Optional<String> checkContentType() throws DotDataException {
+        Optional<String> contentType = langVarContentType();
+        if (contentType.isPresent()) {
+            return contentType;
+        }
+        //The ContentType is not found, so we need to create it
+        Logger.info(this, "Content type not found. Creating default language variable content type");
+        new Task04210CreateDefaultLanguageVariable().executeUpgrade();
+        Logger.info(this, "Updating language variable content type ");
+        new Task240131UpdateLanguageVariableContentType().executeUpgrade();
+        return langVarContentType();
+    }
+
     @Override
     public void executeUpgrade() throws DotDataException, DotRuntimeException {
+        Logger.info(this, "Migrating legacy language variables");
+        final Optional<String> contentTypeInode = checkContentType();
+        if (contentTypeInode.isEmpty()) {
+            throw new DotRuntimeException("Failure Creating or Updating Language Variable Content Type. Skipping migration.");
+        }
+        Logger.info(this,"Language Variable ContentType inode is " + contentTypeInode.get());
         try {
-            final Path path = messagesDir();
-            migrateLegacyLanguageVariables(path);
+            final Path path = LegacyLangVarMigrationHelper.messagesDir();
+            Logger.info(this, "Migrating legacy language variables from " + path);
+            final LegacyLangVarMigrationHelper helper = new LegacyLangVarMigrationHelper(contentTypeInode.get());
+            this.migrationSummary = helper.migrateLegacyLanguageVariables(path);
+            Logger.info(this, "Legacy language variables migration completed");
+            Logger.info(this, "Migration Summary: " + migrationSummary.toString());
+            
         } catch (Exception e) {
             throw new DotRuntimeException("Error migrating legacy language variables", e);
         }
     }
 
-    Lazy<ContentType> langVarContentType = Lazy.of(() -> {
-        ContentType contentType = Try.of(()->APILocator.getContentTypeAPI(APILocator.systemUser()).find(LANGUAGEVARIABLE_VAR_NAME)).getOrNull();
-        if (contentType == null) {
-            throw new DotRuntimeException("Language variable content type not found");
-        }
-        return contentType;
-    });
-
-    @CanIgnoreReturnValue
-    boolean migrateLegacyLanguageVariables(final Path messagesDir) throws IOException {
-        if (!Files.exists(messagesDir)) {
-            Logger.info(this," No messages directory found. Skipping language variable migration.");
-            return false;
-        }
-        try (Stream<Path> files = Files.list(messagesDir).filter(path -> path.toString().endsWith(".properties")).sorted(Comparator.comparing(o -> o.getFileName().toString()))){
-            files.forEach(path -> {
-                // Do stuff
-                final String fileName = path.getFileName().toString();
-                final String cmsLanguage = fileName.replace("cms_language_", "").replace(".properties", "");
-                Logger.info(this," Extracted language code: " + cmsLanguage);
-                final Locale locale = LanguageUtil.validateLanguageTag(cmsLanguage);
-                final long languageId = APILocator.getLanguageAPI().getLanguage(locale.getLanguage(),locale.getCountry()).getId();
-                Logger.info(this," Extracted language id: " + languageId);
-            });
-        }
-        return true;
+    public Optional<ImmutableMigrationSummary> getMigrationSummary() {
+        return Optional.ofNullable(migrationSummary);
     }
-
-    Contentlet saveLanguageVariableContent(final String key, final String value, final long languageId) {
-        final ContentType contentType = langVarContentType.get();
-        final Contentlet langVar = new Contentlet();
-        langVar.setContentTypeId(contentType.id());
-        langVar.setLanguageId(languageId);
-        langVar.setIndexPolicy(IndexPolicy.FORCE);
-        langVar.setBoolProperty(Contentlet.DISABLE_WORKFLOW, true);
-        langVar.setStringProperty(KeyValueContentType.KEY_VALUE_KEY_FIELD_VAR, key);
-        langVar.setStringProperty(KeyValueContentType.KEY_VALUE_VALUE_FIELD_VAR, value);
-        return langVar;
-    }
-
-    Map<Locale, List<Locale>> languageVariantMap = new HashMap<>();
 
 }
