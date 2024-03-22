0cef9708ace883b9652a2ce0e140fa3e6cccb69e
Rafael Velazco <rjvelazco21@gmail.com>
2024-03-21 22:27:54 -0400

    chore: cover DotAssetSearchDialog Component tests

--- core-web/libs/ui/src/lib/components/dot-asset-search/components/dot-asset-search-dialog/dot-asset-search-dialog.component.spec.ts
+++ core-web/libs/ui/src/lib/components/dot-asset-search/components/dot-asset-search-dialog/dot-asset-search-dialog.component.spec.ts
@@ -3,26 +3,51 @@ import { MockComponent } from 'ng-mocks';
 
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 
+import { EMPTY_CONTENTLET } from '@dotcms/utils-testing';
+
 import { DotAssetSearchDialogComponent } from './dot-asset-search-dialog.component';
 
 import { DotAssetSearchComponent } from '../../dot-asset-search.component';
 
 describe('DotAssetSearchDialogComponent', () => {
     let spectator: Spectator<DotAssetSearchDialogComponent>;
-    let component: DotAssetSearchDialogComponent;
-
+    let dynamicDialogRef: DynamicDialogRef;
     const createComponent = createComponentFactory({
         component: DotAssetSearchDialogComponent,
         declarations: [MockComponent(DotAssetSearchComponent)],
-        providers: [DynamicDialogRef, DynamicDialogConfig]
+        providers: [
+            {
+                provide: DynamicDialogRef,
+                useValue: {
+                    close: (_) => {
+                        /* */
+                    }
+                }
+            },
+            {
+                provide: DynamicDialogConfig,
+                useValue: {
+                    data: {
+                        assetType: 'image'
+                    }
+                }
+            }
+        ]
     });
 
     beforeEach(() => {
         spectator = createComponent();
-        component = spectator.component;
+        dynamicDialogRef = spectator.inject(DynamicDialogRef, true);
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('should set editorAssetType from config data', () => {
+        const dotAssetSearchComponent = spectator.query(DotAssetSearchComponent);
+        expect(dotAssetSearchComponent.type).toBe('image');
+    });
+
+    it('should close dialog with selected asset on addAsset', () => {
+        const spy = spyOn(dynamicDialogRef, 'close');
+        spectator.triggerEventHandler(DotAssetSearchComponent, 'addAsset', EMPTY_CONTENTLET);
+        expect(spy).toHaveBeenCalledWith(EMPTY_CONTENTLET);
     });
 });
