dc50c20cb4713f2f8fc4ee4fad4917b69baf3ff4
AndreyDotcms <AndreyDotcms>
2024-03-26 14:18:11 -0600

    dotCMS/core#27531 progress, need to be improved

--- core-web/libs/dot-rules/src/lib/services/GoogleMapService.ts
+++ core-web/libs/dot-rules/src/lib/services/GoogleMapService.ts
@@ -1,9 +1,10 @@
 // tslint:disable:typedef
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { SiteService } from '@dotcms/dotcms-js';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Site, SiteService } from '@dotcms/dotcms-js';
 import { DotSiteService } from '@dotcms/data-access';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { map, skip, takeUntil, tap } from 'rxjs/operators';
 
 window['mapsApi$'] = new BehaviorSubject({ ready: false });
 
@@ -19,49 +20,59 @@ export class GoogleMapService {
     apiReady: Boolean = false;
     loadingApi: Boolean = false;
     mapsApi$: BehaviorSubject<{ ready: boolean; error?: any }>;
-
+    private destroy$ = new Subject<boolean>();
     constructor(private siteService: SiteService, private dotSiteService: DotSiteService) {
-        this.getApiKey();
+        this.getApiKey(this.siteService.currentSite.identifier);
         this.mapsApi$ = window['mapsApi$'];
         this.mapsApi$.subscribe((gMapApi) => {
             if (gMapApi != null) {
                 this.apiReady = true;
             }
         });
+
+        this.siteService.switchSite$
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(({ identifier }) => this.getApiKey(identifier));
     }
 
-    //this method gets the Google key from the current sie
-    private getApiKey(): void {
-        const siteId = this.siteService.currentSite.identifier;
-
+    //this method gets the Google key from the current site
+    getApiKey(siteId): void {
         this.siteService
             .getSiteById(siteId)
-            .pipe(takeUntilDestroyed())
-            .subscribe((site) => {
-                this.apiKey = site.googleMap || null;
+            .pipe(map((site) => site.googleMap || null))
+            .subscribe((apiKey) => {
+                this.apiKey = apiKey;
+                console.log('API KEY', this.apiKey);
             });
     }
 
     loadApi(): void {
-        if (!this.loadingApi) {
-            this.loadingApi = true;
-            let url: string;
-            if (this.apiKey) {
-                url = `https://maps.googleapis.com/maps/api/js?key=${this.apiKey}&callback=mapsApiReady`;
-            } else {
-                url = `https://maps.googleapis.com/maps/api/js?callback=mapsApiReady`;
-            }
-            this.addScript(url);
+        this.loadingApi = true;
+        let url: string;
+        if (this.apiKey) {
+            url = `https://maps.googleapis.com/maps/api/js?key=${this.apiKey}&callback=mapsApiReady`;
+        } else {
+            url = `https://maps.googleapis.com/maps/api/js?callback=mapsApiReady`;
         }
+        console.log('URL to render', url);
+        this.addScript(url);
     }
 
     addScript(url, callback?): void {
+        const id = 'google-maps-api';
+
+        if (document.getElementById(id)) {
+            document.getElementById(id).remove();
+        }
+
         const script = document.createElement('script');
         if (callback) {
             script.onload = callback;
         }
+        script.id = id;
         script.type = 'text/javascript';
         script.src = url;
+
         document.body.appendChild(script);
     }
 }
