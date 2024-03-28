e7d54fdf72ba39e486c12159ed3d28eceeae2e63
Jalinson Diaz <zjaaaldev@gmail.com>
2024-03-28 11:30:41 -0300

    add variant name to the info

--- core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-ema-info-display/dot-ema-info-display.component.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-ema-info-display/dot-ema-info-display.component.ts
@@ -11,6 +11,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { ButtonModule } from 'primeng/button';
 
+import { take } from 'rxjs/operators';
+
+import { DotExperimentsService } from '@dotcms/data-access';
 import { DotMessagePipe } from '@dotcms/ui';
 
 import { EditEmaStore } from '../../../dot-ema-shell/store/dot-ema.store';
@@ -19,7 +22,10 @@ import { EditorData } from '../../../shared/models';
 
 interface InfoOptions {
     icon: string;
-    info: string;
+    info: {
+        message: string;
+        args: string[];
+    };
     action?: () => void;
 }
 
@@ -39,6 +45,7 @@ export class DotEmaInfoDisplayComponent implements OnChanges {
     protected readonly store = inject(EditEmaStore);
     protected readonly router = inject(Router);
     protected readonly activatedRoute = inject(ActivatedRoute);
+    protected readonly experimentsService = inject(DotExperimentsService);
 
     protected readonly editorMode = EDITOR_MODE;
 
@@ -46,43 +53,63 @@ export class DotEmaInfoDisplayComponent implements OnChanges {
         if (!this.editorData.canEditPage) {
             this.options.set({
                 icon: 'pi pi-exclamation-circle warning',
-                info: 'editema.dont.have.edit.permission'
+                info: { message: 'editema.dont.have.edit.permission', args: [] }
             });
         }
 
         if (this.editorData.mode === this.editorMode.DEVICE) {
             this.options.set({
                 icon: this.editorData.device.icon,
-                info: `${this.editorData.device.name} ${this.editorData.device.cssWidth} x ${this.editorData.device.cssHeight}`,
+                info: {
+                    message: `${this.editorData.device.name} ${this.editorData.device.cssWidth} x ${this.editorData.device.cssHeight}`,
+                    args: []
+                },
                 action: () => this.goToEdit()
             });
         } else if (
             this.editorData.mode === this.editorMode.VARIANT &&
             this.editorData.canEditPage
         ) {
-            const modeLabel = this.editorData.variantInfo.canEditVariant ? 'Editing' : 'Viewing';
+            const info = this.editorData.variantInfo.canEditVariant
+                ? 'editpage.editing.variant'
+                : 'editpage.viewing.variant';
+            const experimentId = this.activatedRoute.snapshot.queryParams['experimentId'];
+            const variantId = this.activatedRoute.snapshot.queryParams['variantName'];
 
-            this.options.set({
-                icon: 'pi pi-file-edit',
-                info: `${modeLabel} <b>Some</b> Variant`,
-                action: () =>
-                    this.router.navigate(
-                        [
-                            '/edit-page/experiments/',
-                            this.editorData.variantInfo.pageId,
-                            this.activatedRoute.snapshot.queryParams['experimentId'],
-                            'configuration'
-                        ],
-                        {
-                            queryParams: {
-                                mode: null,
-                                variantName: null,
-                                experimentId: null
-                            },
-                            queryParamsHandling: 'merge'
-                        }
-                    )
-            });
+            this.experimentsService
+                .getById(experimentId)
+                .pipe(take(1))
+                .subscribe((experiment) => {
+                    const name =
+                        experiment.trafficProportion.variants.find(
+                            (variant) => variant.id === variantId
+                        )?.name ?? 'Unknown Variant';
+
+                    this.options.set({
+                        info: {
+                            message: info,
+                            args: [name]
+                        },
+                        icon: 'pi pi-file-edit',
+                        action: () =>
+                            this.router.navigate(
+                                [
+                                    '/edit-page/experiments/',
+                                    this.editorData.variantInfo.pageId,
+                                    this.activatedRoute.snapshot.queryParams['experimentId'],
+                                    'configuration'
+                                ],
+                                {
+                                    queryParams: {
+                                        mode: null,
+                                        variantName: null,
+                                        experimentId: null
+                                    },
+                                    queryParamsHandling: 'merge'
+                                }
+                            )
+                    });
+                });
         }
     }
 
