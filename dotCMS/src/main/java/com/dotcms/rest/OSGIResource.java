ab7523f91406038eef8ae2683292d38fbeb9536f
jdotcms <jonathan.sanchez@dotcms.com>
2024-03-28 13:39:59 -0600

    #27874 adding the new OSGI v1 endpoint

--- dotCMS/src/main/java/com/dotcms/rest/OSGIResource.java
+++ dotCMS/src/main/java/com/dotcms/rest/OSGIResource.java
@@ -13,7 +13,6 @@ import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -22,13 +21,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import com.dotcms.rest.exception.BadRequestException;
-import com.dotmarketing.business.APILocator;
-import com.dotmarketing.portlets.osgi.AJAX.OSGIAJAX;
-import com.liferay.util.FileUtil;
-import org.apache.commons.lang.BooleanUtils;
-import org.apache.felix.framework.OSGISystem;
 import org.apache.felix.framework.OSGIUtil;
 import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.glassfish.jersey.server.JSONP;
@@ -43,7 +35,6 @@ import com.dotmarketing.util.json.JSONArray;
 import com.dotmarketing.util.json.JSONException;
 import com.dotmarketing.util.json.JSONObject;
 import io.vavr.Tuple2;
-import org.osgi.framework.BundleException;
 
 /**
  * @author Jonathan Gamba
@@ -54,91 +45,6 @@ public class OSGIResource  {
 
     private final MultiPartUtils multiPartUtils = new MultiPartUtils();
     private final WebResource    webResource    = new WebResource();
-
-    /**
-     * This method returns a system list of all bundles installed in the OSGi environment at the time of the call to this method.
-     *
-     * @param request
-     * @param params
-     * @return
-     * @throws JSONException
-     */
-    @GET
-    @Path ("/getSystemInstalledBundles/{params:.*}")
-    @Produces (MediaType.APPLICATION_JSON)
-    public Response getSystemInstalledBundles (@Context HttpServletRequest request, @Context final HttpServletResponse response,
-                                               @PathParam ("params") String params ) throws JSONException {
-
-        final InitDataObject initData = new WebResource.InitBuilder(webResource)
-                .requiredBackendUser(true)
-                .requiredFrontendUser(false)
-                .requestAndResponse(request, response)
-                .params(params)
-                .requiredPortlet("dynamic-plugins")
-                .rejectWhenNoUser(true)
-                .init();
-
-        //Creating an utility response object
-        ResourceResponse responseResource = new ResourceResponse( initData.getParamsMap() );
-        StringBuilder responseMessage = new StringBuilder();
-
-
-        /*
-        This method returns a list of all bundles installed in the OSGi environment at the time of the call to this method. However,
-        since the Framework is a very dynamic environment, bundles can be installed or uninstalled at anytime.
-         */
-        final Bundle[] installedBundles = OSGISystem.getInstance().getBundles();
-
-        //Read the parameters
-        final boolean ignoreSystemBundles = "true".equalsIgnoreCase(initData.getParamsMap().get( "ignoresystembundles" ));
-
-        try {
-
-            JSONArray bundlesArray = new JSONArray();
-            for (Bundle bundle : installedBundles) {
-                String bundleLocation = bundle.getLocation();
-                boolean isSystem = ignoreSystemBundles && (bundleLocation.contains("felix/bundle") || bundleLocation.contains("System Bundle"));
-
-                // Getting the jar file name
-                String separator = File.separator;
-                if (bundle.getLocation().contains("/")) {
-                    separator = "/";
-                }
-                String jarFile = bundle.getLocation().contains(separator)
-                        ? bundle.getLocation().substring(bundle.getLocation().lastIndexOf(separator) + 1)
-                        : "System";
-
-                // Build the version string
-                String version = bundle.getVersion().getMajor() + "." + bundle.getVersion().getMinor() + "."
-                        + bundle.getVersion().getMicro();
-
-                // Reading and setting bundle information
-                JSONObject jsonResponse = new JSONObject();
-                jsonResponse.put("bundleId", bundle.getBundleId());
-                jsonResponse.put("symbolicName", bundle.getSymbolicName());
-                jsonResponse.put("location", bundle.getLocation());
-                jsonResponse.put("jarFile", jarFile);
-                jsonResponse.put("state", bundle.getState());
-                jsonResponse.put("version", version);
-                jsonResponse.put("separator", separator);
-                jsonResponse.put("isSystem", isSystem);
-                bundlesArray.add(jsonResponse);
-            }
-
-            responseMessage.append(bundlesArray.toString());
-        } catch (Exception e) {
-            Logger.error(this.getClass(), "Error getting installed OSGI bundles.", e);
-
-            if (e.getMessage() != null) {
-                responseMessage.append(e.getMessage());
-            } else {
-                responseMessage.append("Error getting installed OSGI bundles.");
-            }
-            return responseResource.responseError(responseMessage.toString());
-        }
-
-        return responseResource.response( responseMessage.toString() );
-    }
 
     /**
      * This method returns a list of all bundles installed in the OSGi environment at the time of the call to this method.
@@ -261,79 +167,7 @@ public class OSGIResource  {
         }
         
     }
-
-    /**
-     * Does the undeploy of a bundle/jar
-     *
-     * @param request
-     * @param response
-     * @return Response
-     * @throws JSONException
-     */
-    @DELETE
-    @Path("/bundle/{bundleId}/jar/{jar}")
-    @JSONP
-    @NoCache
-    @Produces({MediaType.APPLICATION_JSON, "application/javascript"})
-    public final Response undeploy(@Context final HttpServletRequest request,
-                                        @Context final HttpServletResponse response,
-                                        @PathParam ("bundleId") final String bundleId,
-                                        @PathParam ("jar") final String jarName) throws BundleException, IOException {
-
-        new WebResource.InitBuilder(webResource)
-                .requiredBackendUser(true)
-                .requiredFrontendUser(false)
-                .requestAndResponse(request, response)
-                .requiredPortlet("dynamic-plugins")
-                .rejectWhenNoUser(true)
-                .init();
-
-        final String sanitizeFileNameJarName = com.dotmarketing.util.FileUtil.sanitizeFileName(jarName);
-        Bundle bundle = null;
-        try {
-            bundle = OSGIUtil.getInstance().getBundle(Long.parseLong(bundleId));
-        } catch (NumberFormatException e) {
-            bundle = OSGIUtil.getInstance().getBundle(bundleId);
-        }
-        bundle.uninstall();
-
-        //Then move the bundle from the load folder to the undeployed folder
-        final String loadPath = OSGIUtil.getInstance().getFelixDeployPath();
-        final String undeployedPath = OSGIUtil.getInstance().getFelixUndeployPath();
-        final File from = new File(loadPath + File.separator + sanitizeFileNameJarName);
-        final File to   = new File(undeployedPath + File.separator + sanitizeFileNameJarName);
-
-        if(to.getCanonicalPath().startsWith(undeployedPath) && to.exists()) {
-            final boolean deleteOk = to.delete();
-            Logger.info(this, String.format(" File [%s] successfully un-deployed [%s].",
-                    to.getCanonicalPath(), BooleanUtils.toStringYesNo(deleteOk)));
-        }
-
-        final boolean success = FileUtil.move(from, to);
-        if (success) {
-            Logger.info(this, String.format("OSGI Bundle  %s Undeployed ", sanitizeFileNameJarName));
-            removeReferences(); // removes portlets and actionlets references
-            return Response.ok(true).build();
-        }
-
-        Logger.error(this, "Error undeploying OSGI Bundle " + sanitizeFileNameJarName);
-        throw new BadRequestException("Error undeploying OSGI Bundle:    " + sanitizeFileNameJarName);
-    }
-
-    private void removeReferences() {
-
-        //Remove Portlets in the list
-        OSGIUtil.getInstance().portletIDsStopped.forEach(p -> APILocator.getPortletAPI().deletePortlet(p));
-        Logger.info( this, "Portlets Removed: " + OSGIUtil.getInstance().portletIDsStopped.toString());
-
-        //Remove Actionlets in the list
-        OSGIUtil.getInstance().actionletsStopped.forEach(p -> OSGIUtil.getInstance().workflowOsgiService.removeActionlet(p));
-        Logger.info( this, "Actionlets Removed: " + OSGIUtil.getInstance().actionletsStopped.toString());
-
-        //Cleanup lists
-        OSGIUtil.getInstance().portletIDsStopped.clear();
-        OSGIUtil.getInstance().actionletsStopped.clear();
-    }
+    
     
     
     
