3706acd58f1a9b9ea0a0abfe9a453a7e29c15433
Rafael Velazco <rjvelazco21@gmail.com>
2024-04-01 13:54:29 -0400

    fix tests

--- core-web/libs/edit-content/src/lib/fields/dot-edit-content-wysiwyg-field/dot-wysiwyg-plugin/utils/editor.utils.ts
+++ core-web/libs/edit-content/src/lib/fields/dot-edit-content-wysiwyg-field/dot-wysiwyg-plugin/utils/editor.utils.ts
@@ -1,16 +1,28 @@
 import { DotCMSContentlet } from '@dotcms/dotcms-models';
 
+/**
+ * Take it from https://github.com/dotCMS/core/blob/792f5c1e62dacd404ddcf9172eaee7ba3524c4c8/dotCMS/src/main/webapp/html/portlet/ext/contentlet/field/edit_field_js.jsp#L754
+ */
 export const DEFAULT_IMAGE_URL_PATTERN = '/dA/{shortyId}/{name}?language_id={languageId}';
 
+/**
+ * Format the image node to be inserted in the WYSIWYG editor
+ *
+ * @param {string} pattern
+ * @param {DotCMSContentlet} asset
+ * @return {*}
+ */
 export const formatDotImageNode = (pattern: string, asset: DotCMSContentlet) => {
     const src = replaceURLPattern(pattern, asset);
 
-    return `<img src="${src}"
-    alt="${asset.title}"
-    data-field-name="${asset.titleImage}"
-    data-inode="${asset.inode}"
-    data-identifier="${asset.identifier}"
-    data-saveas="${asset.title}" />`;
+    return (
+        `<img src="${src}"\n` +
+        `alt="${asset.title}"\n` +
+        `data-field-name="${asset.titleImage}"\n` +
+        `data-inode="${asset.inode}"\n` +
+        `data-identifier="${asset.identifier}"\n` +
+        `data-saveas="${asset.title}" />`
+    );
 };
 
 /**
