6d40c5f9ec8afdd28fda5f335f11c74851206ea8
Neehakethi <neeha.kethi@dotcms.com>
2024-04-05 15:00:26 -0400

    issue-27725 : Removing unwanted import statement

--- dotCMS/src/main/java/com/dotmarketing/business/UserFactoryImpl.java
+++ dotCMS/src/main/java/com/dotmarketing/business/UserFactoryImpl.java
@@ -28,7 +28,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static com.dotmarketing.business.UserHelper.validateMaximumLength;
 
 /**
  * Implementation class for the {@link UserFactory} class.
