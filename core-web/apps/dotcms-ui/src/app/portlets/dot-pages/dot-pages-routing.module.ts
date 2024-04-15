600a2299cc729318520d891fd9cecaac298889f8
Rafael Velazco <rjvelazco21@gmail.com>
2024-04-15 17:53:30 -0400

    Refactor: Check the New Edit Page Feature Flag on a Routing level

--- core-web/apps/dotcms-ui/src/app/portlets/dot-pages/dot-pages-routing.module.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-pages/dot-pages-routing.module.ts
@@ -1,10 +1,76 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { NgModule, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, RouterModule, Routes } from '@angular/router';
+
+import { map, switchMap } from 'rxjs/operators';
 
 import { DotCreateContentletComponent } from '@components/dot-contentlet-editor/components/dot-create-contentlet/dot-create-contentlet.component';
 import { DotCreateContentletResolver } from '@components/dot-contentlet-editor/components/dot-create-contentlet/dot-create-contentlet.resolver.service';
+import { DotContentTypeService, DotContentletService, DotRouterService } from '@dotcms/data-access';
 
 import { DotPagesComponent } from './dot-pages.component';
+
+/**
+ * Check if the new edit content is enabled for a content an redirect the user to it.
+ * Else, let the user to go to the old edit content.
+ *
+ * @param {ActivatedRouteSnapshot} route
+ * @return {*}  {Observable<boolean>}
+ */
+export const newEditContentForContentletGuard: CanActivateFn = (
+    route: ActivatedRouteSnapshot
+): Observable<boolean> => {
+    const inode = route.paramMap.get('asset');
+    const dotContentletService = inject(DotContentletService);
+    const dotContentTypeService = inject(DotContentTypeService);
+    const dotRouterService = inject(DotRouterService); // Inject the Router service
+
+    return dotContentletService.getContentletByInode(inode).pipe(
+        switchMap((contentlet) => {
+            return dotContentTypeService.getContentType(contentlet.contentType).pipe(
+                map(({ metadata }) => {
+                    const newEditorEnabled = metadata?.CONTENT_EDITOR2_ENABLED;
+                    if (!newEditorEnabled) {
+                        return true;
+                    }
+
+                    dotRouterService.goToURL(`content/${inode}`);
+
+                    return false;
+                })
+            );
+        })
+    );
+};
+
+/**
+ * Check if new edit content portlet is enabled for a content type and redirect the user to it.
+ * Otherwise, allow the user to go to the old edit content portlet.
+ *
+ * @param {ActivatedRouteSnapshot} route
+ * @return {*}  {Observable<boolean>}
+ */
+export const newEditContentForContentTypeGuard: CanActivateFn = (
+    route: ActivatedRouteSnapshot
+): Observable<boolean> => {
+    const contentType = route.paramMap.get('contentType');
+    const dotContentTypeService = inject(DotContentTypeService);
+    const dotRouterService = inject(DotRouterService); // Inject the Router service
+
+    return dotContentTypeService.getContentType(contentType).pipe(
+        map(({ metadata }) => {
+            const newEditorEnabled = metadata?.CONTENT_EDITOR2_ENABLED;
+            if (!newEditorEnabled) {
+                return true;
+            }
+
+            dotRouterService.goToURL(`content/new/${contentType}`);
+
+            return false;
+        })
+    );
+};
 
 const routes: Routes = [
     {
@@ -17,12 +83,14 @@ const routes: Routes = [
                         (m) => m.DotPortletDetailModule
                     ),
                 path: ':asset',
+                canActivate: [newEditContentForContentletGuard],
                 data: {
                     reuseRoute: false
                 }
             },
             {
                 path: 'new/:contentType',
+                canActivate: [newEditContentForContentTypeGuard],
                 component: DotCreateContentletComponent,
                 resolve: {
                     url: DotCreateContentletResolver
