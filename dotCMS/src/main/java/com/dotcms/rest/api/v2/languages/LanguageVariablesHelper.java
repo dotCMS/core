aacbb90ac2d1d8c88ffbe73209d5d45cdaa7e86d
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2024-04-20 14:42:03 -0600

    #27979

--- dotCMS/src/main/java/com/dotcms/rest/api/v2/languages/LanguageVariablesHelper.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v2/languages/LanguageVariablesHelper.java
@@ -10,7 +10,6 @@ import com.dotmarketing.util.Logger;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
