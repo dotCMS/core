fe842f1d17abfd318afcba309df8145f58fdfe7b
KevinDavilaDotCMS <kfariid@gmail.com>
2024-04-23 11:27:59 -0500

    Added missing message on save content from inline editing

--- core-web/libs/portlets/edit-ema/portlet/src/lib/dot-ema-shell/store/dot-ema.store.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/dot-ema-shell/store/dot-ema.store.ts
@@ -456,9 +456,7 @@ export class EditEmaStore extends ComponentStore<EditEmaState> {
                             () => {
                                 this.messageService.add({
                                     severity: 'success',
-                                    summary: this.dotMessageService.get(
-                                        'editpage.content.update.success'
-                                    ),
+                                    summary: this.dotMessageService.get('message.content.saved'),
                                     life: 2000
                                 });
                                 this.setEditorMode(EDITOR_MODE.EDIT);
@@ -468,7 +466,7 @@ export class EditEmaStore extends ComponentStore<EditEmaState> {
                                 this.messageService.add({
                                     severity: 'error',
                                     summary: this.dotMessageService.get(
-                                        'editpage.content.update.error'
+                                        'editpage.content.update.contentlet.error'
                                     ),
                                     life: 2000
                                 });
