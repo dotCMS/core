7e3270e50cfd55d6b2dbdfb879260c1953171c4e
jdotcms <jonathan.sanchez@dotcms.com>
2024-04-26 13:22:14 -0600

    #28360 adding refactor for sonarq feedback

--- dotCMS/src/main/java/com/dotmarketing/portlets/workflows/actionlet/AsyncEmailActionlet.java
+++ dotCMS/src/main/java/com/dotmarketing/portlets/workflows/actionlet/AsyncEmailActionlet.java
@@ -4,33 +4,11 @@ import com.dotcms.api.system.event.message.MessageSeverity;
 import com.dotcms.api.system.event.message.SystemMessageEventUtil;
 import com.dotcms.api.system.event.message.builder.SystemMessageBuilder;
 import com.dotcms.concurrent.DotConcurrentFactory;
-import com.dotcms.mock.request.FakeHttpRequest;
-import com.dotcms.mock.response.BaseResponse;
-import com.dotmarketing.beans.Host;
-import com.dotmarketing.beans.Identifier;
-import com.dotmarketing.business.APILocator;
-import com.dotmarketing.business.web.WebAPILocator;
-import com.dotmarketing.exception.DotDataException;
-import com.dotmarketing.portlets.contentlet.model.Contentlet;
-import com.dotmarketing.portlets.contentlet.model.ContentletVersionInfo;
-import com.dotmarketing.portlets.fileassets.business.FileAsset;
-import com.dotmarketing.portlets.workflows.model.WorkflowActionClassParameter;
-import com.dotmarketing.portlets.workflows.model.WorkflowActionFailureException;
-import com.dotmarketing.portlets.workflows.model.WorkflowActionletParameter;
 import com.dotmarketing.portlets.workflows.model.WorkflowProcessor;
-import com.dotmarketing.util.DNSUtil;
-import com.dotmarketing.util.Logger;
 import com.dotmarketing.util.Mailer;
-import com.dotmarketing.util.UtilMethods;
-import com.dotmarketing.util.VelocityUtil;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 /**
  * Same of the {@link EmailActionlet} but runs asynchronously.
