1154b65cf8ac86bd6f61317f463940f194d7269b
Jonathan Gamba <jonathan.gamba@dotcms.com>
2024-05-02 09:34:38 -0600

    #27799 Implemented Resetable interface in API models
    Added a new Resetable interface and implemented it in the AuthenticationParam and RemoteURLParam classes. This implementation standardized the way objects reset to their original state across the API models. As a result, removed the reset method from these classes as it's now inherited from the Resetable interface.

--- tools/dotcms-cli/api-data-model/src/main/java/com/dotcms/api/client/model/RemoteURLParam.java
+++ tools/dotcms-cli/api-data-model/src/main/java/com/dotcms/api/client/model/RemoteURLParam.java
@@ -1,5 +1,6 @@
 package com.dotcms.api.client.model;
 
+import com.dotcms.api.Resetable;
 import java.net.URL;
 import java.util.Optional;
 
@@ -7,12 +8,10 @@ import java.util.Optional;
  * The RemoteURLParam interface is used in the {@link com.dotcms.cli.common.AuthenticationMixin} to
  * set and get the dotCMS URL parameter. It provides methods to set and retrieve the URL.
  */
-public interface RemoteURLParam {
+public interface RemoteURLParam extends Resetable {
 
     void setURL(URL remoteURL);
 
     Optional<URL> getURL();
-
-    void reset();
     
 }
