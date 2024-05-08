0b15161218bfd2d0078704981ba59f5064b70371
Humberto Morera <humberto.morera@dotcms.com>
2024-05-08 10:06:43 -0600

    implementation (languages): initial state, create a new route

---
+++ core-web/libs/portlets/dot-languages/portlet/src/lib/dot-languages/dot-languages.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'lib-dot-languages',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './dot-languages.component.html',
+    styleUrl: './dot-languages.component.css'
+})
+export class DotLanguagesComponent {}
