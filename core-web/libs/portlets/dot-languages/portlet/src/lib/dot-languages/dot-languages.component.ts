8e5b42dc53ad7c2c53d1f9141251d004f19f0246
Humberto Morera <humberto.morera@dotcms.com>
2024-05-09 09:32:53 -0600

    implementation (languages): PR feedback

--- core-web/libs/portlets/dot-languages/portlet/src/lib/dot-languages/dot-languages.component.ts
+++ core-web/libs/portlets/dot-languages/portlet/src/lib/dot-languages/dot-languages.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
     selector: 'lib-dot-languages',
     standalone: true,
     imports: [CommonModule],
     templateUrl: './dot-languages.component.html',
-    styleUrl: './dot-languages.component.css'
+    styleUrl: './dot-languages.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DotLanguagesComponent {}
