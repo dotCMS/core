7546f83ac9e818d4ea173dda9ba5521cb551b1ac
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2024-05-09 19:42:57 -0600

    #28251

--- dotCMS/src/main/java/com/liferay/portal/struts/MultiMessageResources.java
+++ dotCMS/src/main/java/com/liferay/portal/struts/MultiMessageResources.java
@@ -17,6 +17,10 @@
 
 package com.liferay.portal.struts;
 
+import static com.dotmarketing.portlets.languagesmanager.business.LanguageAPI.localizationEnhancements;
+
+import com.dotcms.languagevariable.business.LanguageVariable;
+import com.dotcms.languagevariable.business.LanguageVariableAPI;
 import com.dotcms.repackage.com.google.common.collect.ImmutableMap;
 import com.dotcms.repackage.org.apache.struts.util.MessageResourcesFactory;
 import com.dotcms.repackage.org.apache.struts.util.PropertyMessageResources;
@@ -27,7 +31,9 @@ import com.dotmarketing.portlets.languagesmanager.business.LanguageAPI;
 import com.dotmarketing.portlets.languagesmanager.model.LanguageKey;
 import com.dotmarketing.util.Logger;
 import com.dotmarketing.util.UtilMethods;
+import com.liferay.portal.language.LanguageUtil;
 import com.liferay.util.StringUtil;
+import io.vavr.control.Try;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -175,26 +181,42 @@ public class MultiMessageResources extends PropertyMessageResources {
     private void _loadProps(String name, String localeKey) {
 
         if (name.contains("cms_language")) {
-            LanguageAPI langAPI = APILocator.getLanguageAPI();
-            List<LanguageKey> keys;
-            if (localeKey.split("_").length > 1) {
-                keys = langAPI.getLanguageKeys(localeKey.split("_")[0], localeKey.split("_")[1]);
-            } else {
-                keys = langAPI.getLanguageKeys(localeKey.split("_")[0]);
 
-            }
+            if(Boolean.TRUE.equals(localizationEnhancements.get())){
 
-            if (keys.size() < 1) {
-                return;
-            }
+                final LanguageVariableAPI languageVariableAPI = APILocator.getLanguageVariableAPI();
+                final long languageId = LanguageUtil.getLanguageId(localeKey, false);
+                final List<LanguageVariable> vars = Try.of(()->languageVariableAPI.findVariables(languageId)).getOrElse(List.of());
+                if (vars.isEmpty()){
+                    return;
+                }
+                synchronized (messages) {
+                    for (LanguageVariable var : vars) {
+                        messages.put(messageKey(localeKey, var.key()), var.value());
+                    }
+                }
 
-            synchronized (messages) {
-                Iterator<LanguageKey> names = keys.iterator();
+            } else{
+                LanguageAPI langAPI = APILocator.getLanguageAPI();
+                List<LanguageKey> keys;
+                if (localeKey.split("_").length > 1) {
+                    keys = langAPI.getLanguageKeys(localeKey.split("_")[0], localeKey.split("_")[1]);
+                } else {
+                    keys = langAPI.getLanguageKeys(localeKey.split("_")[0]);
+                }
 
-                while (names.hasNext()) {
-                    LanguageKey langkey = (LanguageKey) names.next();
-                    String key = langkey.getKey();
-                    messages.put(messageKey(localeKey, key), langkey.getValue());
+                if (keys.size() < 1) {
+                    return;
+                }
+
+                synchronized (messages) {
+                    Iterator<LanguageKey> names = keys.iterator();
+
+                    while (names.hasNext()) {
+                        LanguageKey langkey = (LanguageKey) names.next();
+                        String key = langkey.getKey();
+                        messages.put(messageKey(localeKey, key), langkey.getValue());
+                    }
                 }
             }
 
