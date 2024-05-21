0c8648e2a768d03b2960a8d4e202bd1c5fbaa7e6
Rafael Velazco <rjvelazco21@gmail.com>
2024-05-21 16:45:21 -0400

    clean up v2

--- examples/angular/src/environments/environment.ts
+++ examples/angular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     dotcmsUrl: "http://localhost:8080",
-    authToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhcGlmZTU4Mjk3Yy0yZTlkLTRkZTctYWQwMC1hNTE2NjU4NTQ5OTkiLCJ4bW9kIjoxNzE2MzIwMzgyMDAwLCJuYmYiOjE3MTYzMjAzODIsImlzcyI6IjFhY2FmYzg1ZjgiLCJsYWJlbCI6IkVNQSIsImV4cCI6MTgxMDg3MjAwMCwiaWF0IjoxNzE2MzIwMzgyLCJqdGkiOiJiMTlhMDlmNC02YTg0LTQwODQtODBiZC1mZGFkMmExYzM2ZjAifQ.IjZ5Uejd_aMw0vkMyqo_W0bqdbL9gMsaQiVA0dF3wSw",
+    authToken: "YOUR_API_TOKEN",
     siteId: "true",
 };
