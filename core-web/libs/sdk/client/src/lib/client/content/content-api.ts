15ff5ce37c007c374a930b9c193dec703bb91f9a
Jalinson Diaz <zjaaaldev@gmail.com>
2024-05-23 13:30:37 -0300

    feedback

--- core-web/libs/sdk/client/src/lib/client/content/content-api.ts
+++ core-web/libs/sdk/client/src/lib/client/content/content-api.ts
@@ -1,15 +1,15 @@
 import { GetCollection } from './methods/get-collection';
 
 export class Content {
-    #requestOptions: Omit<RequestInit, 'body' | 'method'>;
-    #serverUrl;
+    private requestOptions: Omit<RequestInit, 'body' | 'method'>;
+    private serverUrl;
 
     constructor(requestOptions: Omit<RequestInit, 'body' | 'method'>, serverUrl: string) {
-        this.#requestOptions = requestOptions;
-        this.#serverUrl = serverUrl;
+        this.requestOptions = requestOptions;
+        this.serverUrl = serverUrl;
     }
 
     getCollection(contentType: string) {
-        return new GetCollection(this.#requestOptions, this.#serverUrl, contentType);
+        return new GetCollection(this.requestOptions, this.serverUrl, contentType);
     }
 }
