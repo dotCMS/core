e8ad9e6fe006b3d79a712807463bedd7a64af828
Humberto Morera <humberto.morera@dotcms.com>
2024-05-24 18:31:33 -0600

    implementation (language actions): sonarqube feedback

--- core-web/libs/portlets/dot-locales/portlet/src/lib/share/utils.ts
+++ core-web/libs/portlets/dot-locales/portlet/src/lib/share/utils.ts
@@ -1,4 +1,9 @@
 import { DotLanguage } from '@dotcms/dotcms-models';
 
-export const getLocaleISOCode = (locale: DotLanguage) =>
-    locale ? `${locale.languageCode}${locale.countryCode ? `-${locale.countryCode}` : ''}` : '';
+export const getLocaleISOCode = (locale: DotLanguage): string => {
+    if (locale) {
+        return `${locale.languageCode}${locale.countryCode ? `-${locale.countryCode}` : ''}`;
+    }
+
+    return '';
+};
