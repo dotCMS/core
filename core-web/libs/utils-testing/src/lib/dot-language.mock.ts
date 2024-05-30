886ffe4f98ffabe32c4b87b7b765726822003cba
Humberto Morera <31667212+hmoreras@users.noreply.github.com>
2024-05-30 15:16:11 -0600

    implementation (Languages): #28476 Support the different Menu actions in the table.  (#28669)
    ### Proposed Changes
    * add the menu options to the locale table and the confirmation
    messages.
    * Push Publish 
    * Set As Default 
    * Delete 
    
    
    ### Screenshots
    
    
    https://github.com/dotCMS/core/assets/31667212/646d62f0-b6d3-4f8d-b9e4-173e955160b8

--- core-web/libs/utils-testing/src/lib/dot-language.mock.ts
+++ core-web/libs/utils-testing/src/lib/dot-language.mock.ts
@@ -1,4 +1,4 @@
-import { DotLanguage } from '@dotcms/dotcms-models';
+import { DotLanguage, DotLanguagesISO } from '@dotcms/dotcms-models';
 
 export const mockDotLanguage: DotLanguage = {
     id: 1,
@@ -20,3 +20,37 @@ export const mockLanguageArray: DotLanguage[] = [
     mockDotLanguage,
     mockDotLanguageWithoutCountryCode
 ];
+
+export const mockLocales: DotLanguage[] = [
+    {
+        id: 1,
+        languageCode: 'en',
+        countryCode: 'US',
+        language: 'English',
+        country: 'United States',
+        isoCode: 'en-US',
+        defaultLanguage: true,
+        variables: { count: 1, total: 5 }
+    },
+    {
+        id: 2,
+        languageCode: 'es',
+        countryCode: 'ES',
+        language: 'Spanish',
+        country: 'Spain',
+        isoCode: 'es-ES',
+        defaultLanguage: false,
+        variables: { count: 1, total: 1 }
+    }
+];
+
+export const mockLanguagesISO: DotLanguagesISO = {
+    countries: [
+        { code: 'US', name: 'United States' },
+        { code: 'CA', name: 'Canada' }
+    ],
+    languages: [
+        { code: 'en', name: 'English' },
+        { code: 'es', name: 'Spanish' }
+    ]
+};
