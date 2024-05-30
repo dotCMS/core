6523b415a0b3271ab6d249e5b0e054482343cdad
Nicolas Molina <hi@nicobytes.com>
2024-05-30 16:46:30 -0400

    chore: add mock field

--- core-web/libs/edit-content/src/lib/fields/dot-edit-content-host-folder-field/dot-edit-content-host-folder-field.component.ts
+++ core-web/libs/edit-content/src/lib/fields/dot-edit-content-host-folder-field/dot-edit-content-host-folder-field.component.ts
@@ -1,11 +1,50 @@
-import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, inject, signal } from '@angular/core';
 import { AbstractControl, ControlContainer, ReactiveFormsModule } from '@angular/forms';
 
+import { TreeNode } from 'primeng/api';
 import { TreeSelectModule } from 'primeng/treeselect';
 
 import { DotCMSContentTypeField } from '@dotcms/dotcms-models';
 
 import { DotEditContentFieldSingleSelectableDataTypes } from '../../models/dot-edit-content-field.type';
+
+const files: TreeNode[] = [
+    {
+        label: 'Documents',
+        data: 'Documents Folder',
+        expandedIcon: 'pi pi-folder-open',
+        collapsedIcon: 'pi pi-folder',
+        children: [
+            {
+                label: 'Work',
+                data: 'Work Folder',
+                expandedIcon: 'pi pi-folder-open',
+                collapsedIcon: 'pi pi-folder',
+                children: [
+                    {
+                        label: 'Expenses.doc',
+                        icon: 'pi pi-file',
+                        data: 'Expenses Document'
+                    },
+                    { label: 'Resume.doc', icon: 'pi pi-file', data: 'Resume Document' }
+                ]
+            },
+            {
+                label: 'Home',
+                data: 'Home Folder',
+                expandedIcon: 'pi pi-folder-open',
+                collapsedIcon: 'pi pi-folder',
+                children: [
+                    {
+                        label: 'Invoices.txt',
+                        icon: 'pi pi-file',
+                        data: 'Invoices for this month'
+                    }
+                ]
+            }
+        ]
+    }
+];
 
 @Component({
     selector: 'dot-edit-content-host-folder-field',
@@ -25,13 +64,13 @@ export class DotEditContentHostFolderFieldComponent {
     @Input() field!: DotCMSContentTypeField;
     private readonly controlContainer = inject(ControlContainer);
 
-    options = [];
+    options = signal<TreeNode[]>(files);
 
     /**
      * Returns the form control for the select field.
      * @returns {AbstractControl} The form control for the select field.
      */
-    get formControl() {
+    get formControl(): AbstractControl {
         return this.controlContainer.control.get(
             this.field.variable
         ) as AbstractControl<DotEditContentFieldSingleSelectableDataTypes>;
