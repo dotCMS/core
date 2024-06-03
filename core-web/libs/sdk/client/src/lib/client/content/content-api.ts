6c581f075265d8e789d59fd4baf9d49be5435399
Jalinson Diaz <zjaaaldev@gmail.com>
2024-06-03 13:23:41 -0300

    add examples to JSDoc

--- core-web/libs/sdk/client/src/lib/client/content/content-api.ts
+++ core-web/libs/sdk/client/src/lib/client/content/content-api.ts
@@ -20,11 +20,41 @@ export class Content {
     /**
      * Takes a content type and returns a builder to filter and fetch the collection
      *
+     * @example
+     * ```javascript
+     * // Using await and async
+     * const blogs = await this.content
+     *     .getCollection('Blog')
+     *     .limit(10)
+     *     .page(2)
+     *     .sortBy([{ field: 'title', order: 'asc' }])
+     *     .query((queryBuilder) => queryBuilder.field('author').equals('John Doe'))
+     *     .depth(1);
+     * ```
+     * @example
+     * ```javascript
+     * // Using then and catch
+     * this.content
+     *      .getCollection('Blog')
+     *      .limit(10)
+     *      .page(2)
+     *      .sortBy([{ field: 'title', order: 'asc' }])
+     *      .query((queryBuilder) => queryBuilder.field('author').equals('John Doe'))
+     *      .depth(1)
+     *      .then((response) => {
+     *          console.log(response.contentlets);
+     *      })
+     *      .catch((error) => {
+     *          console.error(error);
+     *      });
+     * ```
+     *
+     *
      * @param {string} contentType The content type to get the collection
      * @return {CollectionBuilder} CollectionBuilder to filter and fetch the collection
      * @memberof Content
      */
-    getCollection(contentType: string): CollectionBuilder {
+    getCollection<T>(contentType: string): CollectionBuilder<T> {
         return new CollectionBuilder(this.#requestOptions, this.#serverUrl, contentType);
     }
 }
