9e48f920b95bd15146a6b1469cb3c1438c1eb96f
Victor Alfaro <victor.alfaro@dotcms.com>
2024-06-06 21:43:57 -0600

    #28719: Adding AI Generative postman tests

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/temp/TempFileAPI.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/temp/TempFileAPI.java
@@ -46,6 +46,7 @@ import com.liferay.portal.util.WebKeys;
 import com.liferay.util.Encryptor;
 import com.liferay.util.StringPool;
 
+import io.vavr.Lazy;
 import io.vavr.control.Try;
 
 public class TempFileAPI {
@@ -61,7 +62,7 @@ public class TempFileAPI {
 
   private static final String WHO_CAN_USE_TEMP_FILE = "whoCanUse.tmp";
   private static final String TEMP_RESOURCE_BY_URL_ADMIN_ONLY="TEMP_RESOURCE_BY_URL_ADMIN_ONLY";
-  
+  private static final Lazy<Boolean> allowAccessToPrivateSubnets = Lazy.of(()->Config.getBooleanProperty("ALLOW_ACCESS_TO_PRIVATE_SUBNETS", false));
   
 
   /**
@@ -216,7 +217,7 @@ public class TempFileAPI {
       /**
        * If url requested is on a private subnet, block by default
        */
-      if(IPUtils.isIpPrivateSubnet(url.getHost())) {
+      if(IPUtils.isIpPrivateSubnet(url.getHost()) && !Optional.ofNullable(allowAccessToPrivateSubnets.get()).map(Boolean::booleanValue).orElse(false)) {
           throw new DotRuntimeException("Unable to load file by url:" + url);
       }
       
