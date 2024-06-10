b514b1f0d64b87047319068175c8ef7a62ef26d6
Jalinson Diaz <zjaaaldev@gmail.com>
2024-06-10 11:18:32 -0300

    create contentlets component

---
+++ examples/nextjs/src/components/shared/contentlets.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Image from "next/image";
+const dateFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+};
+function Contentlets({ contentlets }) {
+    return (
+        <ul className="flex flex-col gap-7">
+            {contentlets.map((contentlet) => (
+                <li key={contentlet.identifier} className="flex gap-7 min-h-16">
+                    <div className="relative min-w-32">
+                        <Image
+                            src={`${process.env.NEXT_PUBLIC_DOTCMS_HOST}${
+                                contentlet.image
+                            }?language_id=${contentlet.languageId || 1}`}
+                            alt={contentlet.urlTitle}
+                            fill={true}
+                            className="object-cover"
+                        />
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <a
+                            className="text-sm text-yellow-200 font-bold"
+                            href={contentlet.urlMap}
+                        >
+                            {contentlet.title}
+                        </a>
+                        <time>
+                            {new Date(
+                                contentlet.publishDate
+                            ).toLocaleDateString("en-US", dateFormatOptions)}
+                        </time>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default Contentlets;
