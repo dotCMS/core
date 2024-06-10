52af8d28fb5b439a38749e7cdfd4f69ad833f550
Jalinson Diaz <zjaaaldev@gmail.com>
2024-06-10 14:05:49 -0300

    Update footer.js

--- examples/nextjs/src/components/layout/footer/footer.js
+++ examples/nextjs/src/components/layout/footer/footer.js
@@ -5,7 +5,7 @@ import Destinations from "./components/destinations";
 // Footer component
 function Footer() {
     return (
-        <footer className="p-4 text-white bg-purple-200 py-24">
+        <footer className="p-4 text-white bg-purple-100 py-24">
             <div className="grid md:grid-cols-3 sm:grid-cols-1 md:grid-rows-1 sm:grid-rows-3 gap-7 mx-24">
                 <AboutUs />
                 <Blogs />
