6a924bde9f24bf39cdb1be3860f1144a684e30be
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2024-06-12 17:40:31 -0600

    relocating test to the cli-jar module

--- tools/dotcms-cli/cli/src/test/java/com/dotcms/api/BuiltVersionServiceTest.java
+++ tools/dotcms-cli/cli/src/test/java/com/dotcms/api/BuiltVersionServiceTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
 @QuarkusTest
 @TestProfile(DotCMSITProfile.class)
 class BuiltVersionServiceTest {
