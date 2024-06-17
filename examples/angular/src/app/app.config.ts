0bfa57de49a5ba31e1a83f3d43d1f94dc20c2b22
KevinDavilaDotCMS <kfariid@gmail.com>
2024-06-17 14:56:59 -0500

    Added ImageConfig to render images from dotcms path

--- examples/angular/src/app/app.config.ts
+++ examples/angular/src/app/app.config.ts
@@ -6,16 +6,37 @@ import { ClientConfig } from '@dotcms/client';
 import { routes } from './app.routes';
 import { environment } from '../environments/environment';
 import { provideDotcmsClient } from './client-token/dotcms-client';
+import { IMAGE_LOADER, ImageLoaderConfig } from '@angular/common';
 
 const DOTCMS_CLIENT_CONFIG: ClientConfig = {
-    dotcmsUrl: environment.dotcmsUrl,
-    authToken: environment.authToken,
-    siteId: environment.siteId
+  dotcmsUrl: environment.dotcmsUrl,
+  authToken: environment.authToken,
+  siteId: environment.siteId,
 };
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideDotcmsClient(DOTCMS_CLIENT_CONFIG),
-        provideRouter(routes),
-    ],
+  providers: [
+    provideDotcmsClient(DOTCMS_CLIENT_CONFIG),
+    provideRouter(routes),
+    /**
+     * This is a custom image loader that will be used by the NgOptimizedImage component.
+     * It will prepend the dotCMS URL to the image src if the image is not an external URL.
+     * It will also append the language_id query parameter if the loaderParams object contains a languageId key.
+     * If you need to use an imagen from an external URL, you can set the isOutsideSRC key to true in the loaderParams object.
+     * <img [ngSrc]="https://my-url.com/some.jpg" [loaderParams]="{isOutsideSRC: true}" />
+     * If you need to customize the image loader, you can provide your own implementation.
+     */
+    {
+      provide: IMAGE_LOADER,
+      useValue: (config: ImageLoaderConfig) => {
+        let url = config.loaderParams?.['isOutsideSRC']
+          ? config.src
+          : `${environment.dotcmsUrl}${config.src}`;
+        if (config.loaderParams?.['languageId']) {
+          url = `${url}/${config.width}?language_id=${config.loaderParams['languageId']}`;
+        }
+        return url;
+      },
+    },
+  ],
 };
--- examples/angular/src/app/components/contentlets/contentlets.component.ts
+++ examples/angular/src/app/components/contentlets/contentlets.component.ts
@@ -1,19 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Contentlet } from '@dotcms/client/src/lib/client/content/shared/types';
 import { GenericContentlet } from '../../utils';
-import { environment } from '../../../environments/environment';
-import { NgOptimizedImage } from '@angular/common';
+import { DatePipe, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-contentlets',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, DatePipe],
   template: `<ul class="flex flex-col gap-7">
     @for(contentlet of contentlets; track contentlet.identifier) {
     <li class="flex gap-7 min-h-16">
       <a class="min-w-32 relative" [href]="contentlet.urlMap ?? contentlet.url">
         <img
-          [ngSrc]="getImageUrl(contentlet)"
+          [ngSrc]="contentlet.image"
           [fill]="true"
           [alt]="contentlet.urlTitle ?? contentlet.title"
           class="object-cover"
@@ -27,7 +26,7 @@ import { NgOptimizedImage } from '@angular/common';
           {{ contentlet.title }}
         </a>
         <time class="text-zinc-600">
-          {{ getDateString(contentlet.modDate) }}
+          {{ contentlet.modDate | date:'mediumDate' }}
         </time>
       </div>
     </li>
@@ -42,15 +41,4 @@ export class ContentletsComponent {
     month: 'long',
     day: 'numeric',
   };
-
-  getDateString(date: string): string {
-    return new Date(date).toLocaleDateString('en-US', this.dateFormatOptions);
-  }
-
-  getImageUrl(contentlet: Contentlet<GenericContentlet>): string {
-    const host = environment.dotcmsUrl;
-    return `${host}${contentlet.image}?language_id=${
-      contentlet.languageId || 1
-    }`;
-  }
 }
