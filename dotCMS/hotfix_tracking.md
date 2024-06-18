a614b0957ce2dab0e9c5c90646942ca16d18ccb9
erickgonzalez <erick.gonzalez@dotcms.com>
2024-06-18 10:03:26 -0600

    #28121 include in 24.04.24 LTS

--- dotCMS/hotfix_tracking.md
+++ dotCMS/hotfix_tracking.md
@@ -10,3 +10,4 @@ This maintenance release includes the following code fixes:
 4. https://github.com/dotCMS/core/issues/28173 : System objects should not be push published #28173
 5. https://github.com/dotCMS/core/issues/28360 : Move Async Email Actionlet to Core #28360
 6. https://github.com/dotCMS/core/issues/28089 : Pound char not decoded when using Rules or Vanity URLs #28089
+7. https://github.com/dotCMS/core/issues/28121 : Include AI portlet by default #28121
--- dotcms-integration/src/test/java/com/dotcms/MainSuite2b.java
+++ dotcms-integration/src/test/java/com/dotcms/MainSuite2b.java
@@ -105,58 +105,7 @@ import com.dotmarketing.quartz.job.PopulateContentletAsJSONJobTest;
 import com.dotmarketing.startup.StartupTasksExecutorDataTest;
 import com.dotmarketing.startup.StartupTasksExecutorTest;
 import com.dotmarketing.startup.runalways.Task00050LoadAppsSecretsTest;
-import com.dotmarketing.startup.runonce.Task05370AddAppsPortletToLayoutTest;
-import com.dotmarketing.startup.runonce.Task05380ChangeContainerPathToAbsoluteTest;
-import com.dotmarketing.startup.runonce.Task05390MakeRoomForLongerJobDetailTest;
-import com.dotmarketing.startup.runonce.Task05395RemoveEndpointIdForeignKeyInIntegrityResolverTablesIntegrationTest;
-import com.dotmarketing.startup.runonce.Task201013AddNewColumnsToIdentifierTableTest;
-import com.dotmarketing.startup.runonce.Task201014UpdateColumnsValuesInIdentifierTableTest;
-import com.dotmarketing.startup.runonce.Task201102UpdateColumnSitelicTableTest;
-import com.dotmarketing.startup.runonce.Task210218MigrateUserProxyTableTest;
-import com.dotmarketing.startup.runonce.Task210319CreateStorageTableTest;
-import com.dotmarketing.startup.runonce.Task210321RemoveOldMetadataFilesTest;
-import com.dotmarketing.startup.runonce.Task210506UpdateStorageTableTest;
-import com.dotmarketing.startup.runonce.Task210510UpdateStorageTableDropMetadataColumnTest;
-import com.dotmarketing.startup.runonce.Task210520UpdateAnonymousEmailTest;
-import com.dotmarketing.startup.runonce.Task210527DropReviewFieldsFromContentletTableTest;
-import com.dotmarketing.startup.runonce.Task210719CleanUpTitleFieldTest;
-import com.dotmarketing.startup.runonce.Task210802UpdateStructureTableTest;
-import com.dotmarketing.startup.runonce.Task210805DropUserProxyTableTest;
-import com.dotmarketing.startup.runonce.Task210816DeInodeRelationshipTest;
-import com.dotmarketing.startup.runonce.Task210901UpdateDateTimezonesTest;
-import com.dotmarketing.startup.runonce.Task211007RemoveNotNullConstraintFromCompanyMXColumnTest;
-import com.dotmarketing.startup.runonce.Task211012AddCompanyDefaultLanguageTest;
-import com.dotmarketing.startup.runonce.Task211101AddContentletAsJsonColumnTest;
-import com.dotmarketing.startup.runonce.Task211103RenameHostNameLabelTest;
-import com.dotmarketing.startup.runonce.Task220202RemoveFKStructureFolderConstraintTest;
-import com.dotmarketing.startup.runonce.Task220203RemoveFolderInodeConstraintTest;
-import com.dotmarketing.startup.runonce.Task220214AddOwnerAndIDateToFolderTableTest;
-import com.dotmarketing.startup.runonce.Task220215MigrateDataFromInodeToFolderTest;
-import com.dotmarketing.startup.runonce.Task220330ChangeVanityURLSiteFieldTypeTest;
-import com.dotmarketing.startup.runonce.Task220401CreateClusterLockTableTest;
-import com.dotmarketing.startup.runonce.Task220402UpdateDateTimezonesTest;
-import com.dotmarketing.startup.runonce.Task220413IncreasePublishedPushedAssetIdColTest;
-import com.dotmarketing.startup.runonce.Task220512UpdateNoHTMLRegexValueTest;
-import com.dotmarketing.startup.runonce.Task220606UpdatePushNowActionletNameTest;
-import com.dotmarketing.startup.runonce.Task220822CreateVariantTableTest;
-import com.dotmarketing.startup.runonce.Task220824CreateDefaultVariantTest;
-import com.dotmarketing.startup.runonce.Task220825CreateVariantFieldTest;
-import com.dotmarketing.startup.runonce.Task220829CreateExperimentsTableTest;
-import com.dotmarketing.startup.runonce.Task220912UpdateCorrectShowOnMenuPropertyTest;
-import com.dotmarketing.startup.runonce.Task220928AddLookbackWindowColumnToExperimentTest;
-import com.dotmarketing.startup.runonce.Task221007AddVariantIntoPrimaryKeyTest;
-import com.dotmarketing.startup.runonce.Task230110MakeSomeSystemFieldsRemovableByBaseTypeTest;
-import com.dotmarketing.startup.runonce.Task230328AddMarkedForDeletionColumnTest;
-import com.dotmarketing.startup.runonce.Task230426AlterVarcharLengthOfLockedByColTest;
-import com.dotmarketing.startup.runonce.Task230523CreateVariantFieldInContentletIntegrationTest;
-import com.dotmarketing.startup.runonce.Task230701AddHashIndicesToWorkflowTablesTest;
-import com.dotmarketing.startup.runonce.Task230707CreateSystemTableTest;
-import com.dotmarketing.startup.runonce.Task230713IncreaseDisabledWysiwygColumnSizeTest;
-import com.dotmarketing.startup.runonce.Task231109AddPublishDateToContentletVersionInfoTest;
-import com.dotmarketing.startup.runonce.Task240102AlterVarcharLengthOfRelationTypeTest;
-import com.dotmarketing.startup.runonce.Task240111AddInodeAndIdentifierLeftIndexesTest;
-import com.dotmarketing.startup.runonce.Task240112AddMetadataColumnToStructureTableTest;
-import com.dotmarketing.startup.runonce.Task230630CreateRunningIdsExperimentFieldIntegrationTest;
+import com.dotmarketing.startup.runonce.*;
 import com.dotmarketing.util.MaintenanceUtilTest;
 import com.dotmarketing.util.ResourceCollectorUtilTest;
 import com.dotmarketing.util.UtilMethodsITest;
@@ -335,7 +284,8 @@ import org.junit.runners.Suite.SuiteClasses;
         Task240111AddInodeAndIdentifierLeftIndexesTest.class,
         AnnouncementsHelperIntegrationTest.class,
         RemoteAnnouncementsLoaderIntegrationTest.class,
-        Task240112AddMetadataColumnToStructureTableTest.class
+        Task240112AddMetadataColumnToStructureTableTest.class,
+        Task240530AddDotAIPortletToLayoutTest.class
 })
 
 public class MainSuite2b {
