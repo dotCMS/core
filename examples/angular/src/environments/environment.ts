f397699da2c93b6fa934575d684fec59a1a97acd
KevinDavilaDotCMS <codeabien@gmail.com>
2024-06-19 14:26:27 -0500

    Updated enviroment

--- examples/angular/src/environments/environment.ts
+++ examples/angular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    dotcmsUrl: "http://localhost:8080",
-    authToken: "YOUR_API_TOKEN",
+    dotcmsUrl: "https://dotcms-qa-master.dotcms.site",
+    authToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhcGkxNGNjMDcwZi03YzIyLTQ2ODktYWFkNy03NmMxZmMwNjUxNmQiLCJ4bW9kIjoxNzE4ODE0NjE3MDAwLCJuYmYiOjE3MTg4MTQ2MTcsImlzcyI6IjNiOGU2ZjFkZmIiLCJsYWJlbCI6InJlYWQtb25seSIsImV4cCI6MTgxMzM4MTIwMCwiaWF0IjoxNzE4ODE0NjE3LCJqdGkiOiJmNTYwNjBlZC05MzFkLTRlYzYtODI2My1lZDlkMTQ4MjAyNmIifQ.MCYEQy9Kem8DK8txU1mFYeSbWUnCJxU6vJl4Wz2Ppuo",
     siteId: "true",
 };
