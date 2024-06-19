6c34f518ae9251bce0f7660c8251f0d6c976ee5d
Jalinson Diaz <zjaaaldev@gmail.com>
2024-06-19 16:59:21 -0300

    add test cases

--- core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/ema-contentlet-tools/ema-contentlet-tools.component.spec.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/ema-contentlet-tools/ema-contentlet-tools.component.spec.ts
@@ -365,6 +365,51 @@ describe('EmaContentletToolsComponent', () => {
         });
     });
 
+    describe('Contentlet outside container', () => {
+        beforeEach(
+            () =>
+                (spectator = createComponent({
+                    props: {
+                        contentletArea: {
+                            ...contentletAreaMock,
+                            width: 180,
+                            payload: {
+                                contentlet: {
+                                    identifier: 'contentlet-identifier-123',
+                                    inode: 'contentlet-inode-123',
+                                    title: 'Hello World',
+                                    contentType: 'test'
+                                },
+                                container: null,
+                                language_id: '1',
+                                pageContainers: [],
+                                pageId: '1',
+                                position: 'after'
+                            }
+                        }
+                    }
+                }))
+        );
+
+        it('should render only edit button', () => {
+            expect(spectator.query(byTestId('edit-button'))).toBeDefined();
+
+            const toBeNullTestIDs = [
+                'add-top-button',
+                'add-bottom-button',
+                'menu-add',
+                'delete-button',
+                'drag-button',
+                'edit-vtl-button',
+                'menu-vtl'
+            ];
+
+            toBeNullTestIDs.forEach((testId) => {
+                expect(spectator.query(byTestId(testId))).toBeNull();
+            });
+        });
+    });
+
     describe('VTL contentlet', () => {
         beforeEach(
             () =>
