cc14b9a639eaf0f73ac016791abc588e90d8e4f9
Humberto Morera <humberto.morera@dotcms.com>
2024-06-25 11:37:15 -0400

    implemenetation(Locales): remove comment

--- core-web/apps/dotcms-ui/src/app/app-routing.module.ts
+++ core-web/apps/dotcms-ui/src/app/app-routing.module.ts
@@ -61,7 +61,7 @@ const PORTLETS_ANGULAR: Route[] = [
             )
     },
     {
-        path: 'locales', // Will be changed to locales.
+        path: 'locales',
         canActivate: [MenuGuardService],
         canActivateChild: [MenuGuardService],
         data: {
