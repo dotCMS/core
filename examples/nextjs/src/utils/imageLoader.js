e0605c2ae05647df9d6b7443adc74a054060b98f
Jalinson Diaz <zjaaaldev@gmail.com>
2024-06-25 10:07:05 -0300

    add width to the image src

--- examples/nextjs/src/utils/imageLoader.js
+++ examples/nextjs/src/utils/imageLoader.js
@@ -1,15 +1,15 @@
 import { useDotcmsPageContext } from "@dotcms/react";
 
-const ImageLoader = ({ src }) => {
-    const dotcmsURL = new URL(process.env.NEXT_PUBLIC_DOTCMS_HOST);
+const ImageLoader = ({ src, width }) => {
+    const dotcmsURL = new URL(process.env.NEXT_PUBLIC_DOTCMS_HOST).origin;
 
     const context = useDotcmsPageContext();
 
     const languageId = context?.pageAsset.viewAs.language.id ?? 1;
 
-    return src.includes("/dA/") // Check if the image is a DotCMS asset or a file asset
-        ? `${dotcmsURL.origin}${src}?language_id=${languageId}`
-        : `${dotcmsURL.origin}/dA/${src}?language_id=${languageId}`;
+    const imageSRC = src.includes("/dA/") ? src : `/dA/${src}`; // Check if the image is a DotCMS asset or a file asset
+
+    return `${dotcmsURL}${imageSRC}/${width}?language_id=${languageId}`;
 };
 
 export default ImageLoader;
