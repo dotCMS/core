7639bc5f1ecfaf635f1f88bc56741bc353354eb3
Valentino Giardino <77643678+valentinogiardino@users.noreply.github.com>
2024-06-28 12:24:00 -0300

    Fix(#28143) cli case sensitivity issue with direction parameter (#29052)
    ### Proposed Changes
    * Transform the direction parameter to uppercase
    * Add test cases
    * Update CLI documentation to reflect this change
    
    ### Screenshots
    
    ![screenshot-case-insensitive-fix](https://github.com/dotCMS/core/assets/77643678/88bfad81-d990-46b4-a54b-b53433a14730)
    
    This PR fixes: https://github.com/dotCMS/core/issues/28143

--- tools/dotcms-cli/cli/src/main/java/com/dotcms/cli/command/contenttype/ContentTypeFind.java
+++ tools/dotcms-cli/cli/src/main/java/com/dotcms/cli/command/contenttype/ContentTypeFind.java
@@ -57,7 +57,7 @@ public class ContentTypeFind extends AbstractContentTypeCommand implements Calla
         String orderBy;
 
         @CommandLine.Option(names = {"-d", "--direction"},
-                description = "Set order direction. (ASC or DESC) ", defaultValue = "ASC")
+                description = "Set order direction. Accepts ASC or DESC (case insensitive)", defaultValue = "ASC")
         String direction;
 
         @CommandLine.Option(names = {"-p", "--page"},
@@ -128,7 +128,7 @@ public class ContentTypeFind extends AbstractContentTypeCommand implements Calla
         final ContentTypeAPI contentTypeAPI = clientFactory.getClient(ContentTypeAPI.class);
         final ResponseEntityView<List<ContentType>> responseEntityView = contentTypeAPI.getContentTypes(
                 filter.typeName, filter.page, filter.pageSize,
-                filter.orderBy, filter.direction, null, filter.site);
+                filter.orderBy, filter.direction.toUpperCase(), null, filter.site);
 
         final List<ContentType> types = responseEntityView.entity();
         if (types.isEmpty()) {
