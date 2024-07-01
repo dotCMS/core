235e2abb7d372174d49b1eceaa720e9691e28850
Daniel Colina <daniel.colina@dotcms.com>
2024-07-01 17:52:26 +0200

    #28678 testing including db dump into docker-compose manifest.

--- dotCMS/src/main/docker/original/ROOT/srv/50-load-dump-sql.sh
+++ dotCMS/src/main/docker/original/ROOT/srv/50-load-dump-sql.sh
@@ -4,9 +4,15 @@ set -e
 DB_NAME="dotcms"
 
 # Check if the DB_LOAD_DUMP environment variable is set and the file exists
-if [[ -n "${DB_LOAD_DUMP_SQL}" && -f "${DB_LOAD_DUMP_SQL}" ]]; then
+if [[ -n "${DB_LOAD_DUMP_SQL}" && -f "${DB_LOAD_DUMP_SQL}" && -z ${CUSTOM_STARTER_URL} ]]; then
     echo "Importing database dump from ${DB_LOAD_DUMP_SQL}..."
-    psql -h ${DB_BASE_URL} -U ${DB_USERNAME} -d ${DB_NAME} -f "${DB_LOAD_DUMP_SQL}"
+    whereis psql
+    whereis pg_dump
+    which psql
+    which pg_dump
+#    PGPASSWORD=${DB_PASSWORD} psql -h ${DB_BASE_URL} -U ${DB_USERNAME} -d ${DB_NAME} -f "${DB_LOAD_DUMP_SQL}"
+#    /usr/bin/psql -h ${DB_BASE_URL} -U ${DB_USERNAME} -d ${DB_NAME} -f "${DB_LOAD_DUMP_SQL}"
+#    /usr/bin/pg_dump -h ${DB_BASE_URL} -U ${DB_USERNAME} -W ${DB_PASSWORD} -d ${DB_NAME} -f "${DB_LOAD_DUMP_SQL}"
     echo "Dump successfully imported."
 else
     echo "Dump file not found at ${DB_LOAD_DUMP_SQL}"
