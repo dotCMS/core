39254a5820da3168b747331035cd38de0847bc28
Rafael Velazco <rjvelazco21@gmail.com>
2024-07-15 15:37:39 -0400

    chore: code cleanup v3

--- examples/angular/src/environments/environment.development.ts
+++ examples/angular/src/environments/environment.development.ts
@@ -5,6 +5,6 @@ export const environment = {
    * for security purposes, make sure is read-only.
    * More info: https://www.dotcms.com/docs/latest/permissions
    */
-  authToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhcGk2M2Y0YTUxMi03MDRkLTQ5YjAtYjZlNi1kZWM0MTZjMzNkMmMiLCJ4bW9kIjoxNzIwNTQxMzMxMDAwLCJuYmYiOjE3MjA1NDEzMzEsImlzcyI6ImQ0MDI0OTM2MDEiLCJsYWJlbCI6IlVWRSIsImV4cCI6MTgxNTEwNTYwMCwiaWF0IjoxNzIwNTQxMzMxLCJqdGkiOiIzNDA3OGJiMS0yNmM0LTQ2MmYtYTY2Mi04MjdhYTU5NDBlN2EifQ.BWoJ_O9GungNUB2QUiZoZKepLXuHjXmTw9hWNwq7XRY',
+  authToken: 'YOUR_AUTH_TOKEN',
   siteId: 'true',
 };
