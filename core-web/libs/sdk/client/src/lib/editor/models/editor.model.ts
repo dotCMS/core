e7bdc0c49cb221d5b18fa4ea4a303bfe5c22130a
Rafael Velazco <rjvelazco21@gmail.com>
2024-07-23 13:58:29 -0400

    feat(UVE): bring back  in NextJS

--- core-web/libs/sdk/client/src/lib/editor/models/editor.model.ts
+++ core-web/libs/sdk/client/src/lib/editor/models/editor.model.ts
@@ -9,4 +9,10 @@ export interface DotCMSPageEditorConfig {
      * The pathname of the page being edited. Optional.
      */
     pathname: string;
+    /**
+     * The reload function to call when the page is reloaded.
+     *
+     * @deprecated
+     */
+    onReload?: () => void;
 }
