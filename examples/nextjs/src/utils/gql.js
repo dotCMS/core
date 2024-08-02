c78ed2c01e1f8106097ac7162928970458b19c17
Rafael Velazco <rjvelazco21@gmail.com>
2024-08-02 09:45:15 -0400

    chore: add try/catch

--- examples/nextjs/src/utils/gql.js
+++ examples/nextjs/src/utils/gql.js
@@ -95,16 +95,25 @@ export const getGraphQLPageData = async (params) => {
     const query = getGraphQLPageQuery(params);
     const url = new URL(GRAPHQL_ENPOINT, process.env.NEXT_PUBLIC_DOTCMS_HOST);
 
-    const res = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${process.env.NEXT_PUBLIC_DOTCMS_AUTH_TOKEN}`,
-            "Content-Type": "application/json",
-            "dotcachettl": "0" // Bypasses GraphQL cache
-        },
-        body: JSON.stringify({ query }),
-        cache: "no-cache", // Invalidate cache for Next.js
-    });
-    const { data } = await res.json();
-    return data;
+    try {
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${process.env.NEXT_PUBLIC_DOTCMS_AUTH_TOKEN}`,
+                "Content-Type": "application/json",
+                "dotcachettl": "0" // Bypasses GraphQL cache
+            },
+            body: JSON.stringify({ query }),
+            cache: "no-cache", // Invalidate cache for Next.js
+        });
+        const { data } = await res.json();
+        return data;
+    } catch(err) {
+        console.group("Error fetching Page");
+        console.warn("Check your URL or DOTCMS_HOST: ", url.toString());
+        console.error(err);
+        console.groupEnd();
+
+        return { page: null };
+    }
 };
