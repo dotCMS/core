c621ac6e6d15b0f85dfc57563c875125005d3c9c
Rafael Velazco <rjvelazco21@gmail.com>
2024-08-05 13:01:10 -0400

    chore: improve docs

--- core-web/libs/sdk/client/src/lib/editor/models/editor.model.ts
+++ core-web/libs/sdk/client/src/lib/editor/models/editor.model.ts
@@ -1,5 +1,42 @@
 /**
- *
+ * @description Configuration for fetching data using GraphQL.
+ * @typedef {Object} GraphQLFetchConfig
+ * @property {"GHAPQL"} type - The type of fetch to perform. It can be either 'GHAPQL' or 'PAGEAPI'.
+ * @property {string} data - The query to be sent to the GHAPQL endpoint.
+ * @example
+ * {
+ *   type: 'GHAPQL',
+ *   data: 'query { ... }'
+ * }
+ */
+export type GraphQLFetchConfig = {
+    type: 'GHAPQL';
+    data: string;
+};
+
+/**
+ * @description Configuration for fetching data using Page API.
+ * @typedef {Object} PageAPIFetchConfig
+ * @property {"PAGEAPI"} type - The type of fetch to perform. It can be either 'GHAPQL' or 'PAGEAPI'.
+ * @property {{ [key: string]: string }} data - Parameters to be sent to the PAGEAPI endpoint.
+ * @example
+ * {
+ *   type: 'PAGEAPI',
+ *   data: { depth: '2' }
+ * }
+ */
+export type PageAPIFetchConfig = {
+    type: 'PAGEAPI';
+    data: { [key: string]: string };
+};
+
+/**
+ * @description Union type for fetch configurations.
+ * @typedef {GraphQLFetchConfig | PageAPIFetchConfig} DotCMSFetchConfig
+ */
+type DotCMSFetchConfig = GraphQLFetchConfig | PageAPIFetchConfig;
+
+/**
  * Represents the configuration options for the DotCMS page editor.
  * @export
  * @interface DotCMSPageEditorConfig
@@ -7,19 +44,36 @@
 export interface DotCMSPageEditorConfig {
     /**
      * The pathname of the page being edited. Optional.
+     * @type {string}
      */
     pathname: string;
     /**
      * The query string to use when fetching the page data.
-     *
      * @type {string}
      * @memberof DotCMSPageEditorConfig
      */
     query?: string;
     /**
+     * The fetch configuration for the page data.
+     * @type {DotCMSFetchConfig}
+     * @memberof DotCMSPageEditorConfig
+     * @description The fetch configuration can be either a GraphQLFetchConfig or a PageAPIFetchConfig.
+     * @example
+     * {
+     *  type: 'GHAPQL',
+     *  data: 'query { ... }'
+     * }
+     * @example
+     * {
+     * type: 'PAGEAPI',
+     * data: { depth: '2' }
+     * }
+     */
+    fetch?: DotCMSFetchConfig;
+    /**
      * The reload function to call when the page is reloaded.
-     *
      * @deprecated In future implementation we will be listening for the changes from the editor to update the page state so reload will not be needed.
+     * @type {Function}
      */
     onReload?: () => void;
 }
