178aec514267906c64d64b50d19cfdd08fd80cf8
Kevin Davila <kfariid@gmail.com>
2024-08-08 10:49:00 -0500

    Removed temp token

--- examples/angular/src/environments/environment.development.ts
+++ examples/angular/src/environments/environment.development.ts
@@ -5,6 +5,6 @@ export const environment = {
    * for security purposes, make sure is read-only.
    * More info: https://www.dotcms.com/docs/latest/permissions
    */
-  authToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhcGkxNjNkOTA5ZS0xNmI0LTRiNTYtYTI0OC1lOTIxZTM0NTFiMmQiLCJ4bW9kIjoxNzIyODgxMTkzMDAwLCJuYmYiOjE3MjI4ODExOTMsImlzcyI6ImNjMjFiZDYwMjQiLCJsYWJlbCI6ImZyb250ZW5kIiwiZXhwIjoxODE3NDQyMDAwLCJpYXQiOjE3MjI4ODExOTMsImp0aSI6IjE4MWY0ZGFkLWI3MTMtNGQ5Mi05ODZjLWRlODEyYWEzODVhMCJ9.Cc7q__2YKdaNWCxvl_1DXTmT0zFXTD5bm-FQtGjRyqc',
+  authToken: 'YOUR_AUTH_TOKEN',
   siteId: 'true',
 };
