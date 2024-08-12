5213a3546cd6d1958e5c72aa5067a3a9bc18b79c
valentinogiardino <valentinogiardino17@gmail.com>
2024-08-12 15:38:14 -0300

    #29270 Rollback to `CustomGraphQLException` as Base Class
    **Reason for Rollback**:
    - The change to use `GraphQLErrorException` as the base class for `PermissionDeniedGraphQLException` caused a violation of the SonarQube rule "Inheritance tree of classes should not be too deep."
    - The inheritance tree depth exceeded the allowed limit, leading to maintainability issues flagged by SonarQube.

--- dotCMS/src/main/java/com/dotcms/graphql/exception/PermissionDeniedGraphQLException.java
+++ dotCMS/src/main/java/com/dotcms/graphql/exception/PermissionDeniedGraphQLException.java
@@ -1,17 +1,27 @@
 package com.dotcms.graphql.exception;
 
-import graphql.GraphqlErrorException;
+/**
+ * Custom exception for permission denial in GraphQL operations.
+ * This exception is used to indicate that a user does not have the necessary permissions
+ * to access a specific resource.
+ */
+public class PermissionDeniedGraphQLException extends CustomGraphQLException {
 
-public class PermissionDeniedGraphQLException extends GraphqlErrorException {
+    private static final String DEFAULT_MESSAGE = "Permission denied: You do not have permission to access this resource.";
 
+    /**
+     * Constructs a new PermissionDeniedGraphQLException with a specific message.
+     *
+     * @param message The detail message to be used.
+     */
     public PermissionDeniedGraphQLException(String message) {
-        super(new Builder().message(message));
+        super(message);
     }
 
+    /**
+     * Constructs a new PermissionDeniedGraphQLException with a default message.
+     */
     public PermissionDeniedGraphQLException() {
-        super(new Builder().message("Permission denied: You do not have permission to access this resource."));
+        this(DEFAULT_MESSAGE);
     }
-
 }
-
-
