9d3de6eff03bf81639caf3fd075666164f26c829
Nicolas Molina <hi@nicobytes.com>
2024-08-13 02:42:59 -0400

    chore(edit-content): fix error

--- core-web/libs/edit-content/src/lib/fields/dot-edit-content-category-field/utils/category-field.utils.ts
+++ core-web/libs/edit-content/src/lib/fields/dot-edit-content-category-field/utils/category-field.utils.ts
@@ -69,7 +69,7 @@ const transformCategory = (
     const { key, inode, categoryName, childrenCount } = category;
     const hasChildren = childrenCount > 0;
 
-    const path = category.parentList ? getParentPath(category.parentList) : '';
+    const path = getParentPath(category.parentList ?? []);
 
     return {
         key,
@@ -181,7 +181,7 @@ export const updateChecked = (
         if (!currentChecked.some((entry) => entry.key === item.key)) {
             currentChecked = [
                 ...currentChecked,
-                { key: item.key, value: item.value, inode: item.inode }
+                { key: item.key, value: item.value, inode: item.inode, path: item?.path ?? '' }
             ];
         }
     } else {
@@ -199,14 +199,14 @@ export const updateChecked = (
  * @param parentList
  */
 export const getParentPath = (parentList: DotCategoryParent[]): string => {
-    if (parentList) {
-        return parentList
-            .slice(1)
-            .map((parent) => parent.name)
-            .join(' / ');
+    if (parentList.length === 0) {
+        return '';
     }
 
-    return '';
+    return parentList
+        .slice(1)
+        .map((parent) => parent.name)
+        .join(' / ');
 };
 
 /**
