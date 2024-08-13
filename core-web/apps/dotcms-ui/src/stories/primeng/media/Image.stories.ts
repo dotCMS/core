00cc70d3e1ec1dbc9c59217308880e059ee08bb7
Nicolas Molina <hi@nicobytes.com>
2024-08-13 03:16:53 -0400

    chore(storybook): upgrade in stories

--- core-web/apps/dotcms-ui/src/stories/primeng/media/Image.stories.ts
+++ core-web/apps/dotcms-ui/src/stories/primeng/media/Image.stories.ts
@@ -1,32 +1,37 @@
-import { Meta, StoryFn } from '@storybook/angular';
+import { faker } from '@faker-js/faker';
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { ImageModule } from 'primeng/image';
+import { ImageModule, Image } from 'primeng/image';
 
-export default {
+const meta: Meta<Image> = {
     title: 'PrimeNG/Media/Image',
     component: Image,
-    args: { src: 'https://picsum.photos/600/900', alt: 'Image Description', preview: true },
+    decorators: [
+        moduleMetadata({
+            imports: [ImageModule, BrowserAnimationsModule]
+        })
+    ],
+    args: {
+        src: faker.image.urlLoremFlickr({ width: 600, height: 900 }),
+        alt: 'Image Description',
+        preview: true
+    },
     parameters: {
         docs: {
             description: {
                 component: 'Displays an image with preview and tranformation options.'
             }
         }
-    }
-} as Meta;
-
-const Template = `<p-image [src]="src" [alt]="alt" [preview]="preview"></p-image>`;
-
-export const Main: StoryFn = (args) => {
-    return {
-        props: {
-            src: args.src,
-            alt: args.alt,
-            preview: args.preview
-        },
-        moduleMetadata: { imports: [ImageModule, BrowserAnimationsModule] },
-        template: Template
-    };
+    },
+    render: (args) => ({
+        props: args,
+        template: `<p-image [src]="src" [alt]="alt" [preview]="preview" />`
+    })
 };
+export default meta;
+
+type Story = StoryObj<Image>;
+
+export const Main: Story = {};
