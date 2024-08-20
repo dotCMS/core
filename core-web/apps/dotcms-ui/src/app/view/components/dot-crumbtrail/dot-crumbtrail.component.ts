006474a2d8cb77c30810b586b2b4a4318fb75d72
Nicolas Molina <hi@nicobytes.com>
2024-08-20 09:16:35 -0400

    chore(primeng): use signals

--- core-web/apps/dotcms-ui/src/app/view/components/dot-crumbtrail/dot-crumbtrail.component.ts
+++ core-web/apps/dotcms-ui/src/app/view/components/dot-crumbtrail/dot-crumbtrail.component.ts
@@ -1,17 +1,13 @@
-import { Observable } from 'rxjs';
-
 import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 
-import { DotCrumb, DotCrumbtrailService } from './service/dot-crumbtrail.service';
+import { DotCrumbtrailService } from './service/dot-crumbtrail.service';
 @Component({
     selector: 'dot-crumbtrail',
     template: '<p-breadcrumb [model]="$model()" />',
     styleUrls: ['./dot-crumbtrail.component.scss']
 })
 export class DotCrumbtrailComponent {
-    crumb: Observable<DotCrumb[]>;
-
     readonly #crumbTrailService = inject(DotCrumbtrailService);
     $model = toSignal(this.#crumbTrailService.crumbTrail$, {
         initialValue: []
