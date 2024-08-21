92eff873fe419df8efcfe7127eb1d78b08f020a6
Jalinson Diaz <zjaaaldev@gmail.com>
2024-08-21 14:22:52 -0300

    cleaning

---
+++ examples/astro/src/react/shared/entry.tsx
@@ -0,0 +1,48 @@
+import useImageSrc from "../hooks/useImageSrc";
+
+const dateFormatOptions: DateTimeFormatOptions = {
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+
+function Entry ({ contentlet })  {
+  const src = useImageSrc({
+    src: contentlet.image?.idPath ?? contentlet.image
+  })
+
+  return <>
+    <a
+      className="relative min-w-32"
+      href={contentlet.urlMap || contentlet.url}
+    >
+      {contentlet.image && (
+        <img
+          src={
+            src
+          }
+          alt={contentlet.urlTitle}
+          className="object-cover absolute w-full h-full top-0 left-0"
+        />
+      )}
+    </a>
+    <div className="flex flex-col gap-1">
+      <a
+        className="text-sm text-zinc-900 font-bold"
+        href={contentlet.urlMap || contentlet.url}
+      >
+        {contentlet.title}
+      </a>
+      <time className="text-zinc-600">
+        {new Date(
+          contentlet.modDate
+        ).toLocaleDateString(
+          "en-US",
+          dateFormatOptions
+        )}
+      </time>
+    </div>
+  </>
+}
+
+export default Entry
