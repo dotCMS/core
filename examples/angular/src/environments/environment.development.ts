fa3b374371c3066110d608143bfc944d5a24070e
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-08-21 18:03:40 -0600

    revert

--- examples/angular/src/environments/environment.development.ts
+++ examples/angular/src/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
-  dotcmsUrl: 'https://demo.dotcms.com',
+  dotcmsUrl: 'http://localhost:8080',
   /**
    * This auth token will be exposed in the client-side requests,
    * for security purposes, make sure is read-only.
    * More info: https://www.dotcms.com/docs/latest/permissions
    */
-  authToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhcGk1MzllYjlkZS1jMjE3LTRlM2EtOWU2Zi00OTVjYWYyNTU4YTIiLCJ4bW9kIjoxNzI0MjY1Mjk0MDAwLCJuYmYiOjE3MjQyNjUyOTQsImlzcyI6IjNkNmFjNThjYzgiLCJleHAiOjE3MjUxMjkyOTQsImlhdCI6MTcyNDI2NTI5NCwianRpIjoiZGMxZTMyZjctMjZmMS00NWZlLTlhNmEtNmMyNTM1N2FiYWVjIn0.WPM7-4vTbqQUrlDKcKPhDl2tbTMJJVPhLX-urrVWLNo',
+  authToken: 'YOUR_AUTH_TOKEN',
   siteId: 'true',
 };
