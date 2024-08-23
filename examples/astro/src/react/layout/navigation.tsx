48bc2643932a00ce29f55bb7bb7ed2601cf79519
Jalinson Diaz <zjaaaldev@gmail.com>
2024-08-23 17:11:56 -0300

    preserve query params

--- examples/astro/src/react/layout/navigation.tsx
+++ examples/astro/src/react/layout/navigation.tsx
@@ -7,22 +7,17 @@ export type NavigationProps = {
 };
 
 export const Navigation: FC<NavigationProps> = ({ items, className }) => {
+  const currentQueryParams = window.location.search;
+
   return (
     <nav className={className}>
       <ul className="flex space-x-4 text-white">
         <li>
-          <a href="/">
-            {" "}
-            {/* I NEED TO PRESERVE THE QUERY PARAMS*/}
-            Home
-          </a>
+          <a href={`/${currentQueryParams}`}> Home</a>
         </li>
         {items?.map((item) => (
           <li key={item.folder}>
-            <a
-              href={item.href} // I NEED TO PRESERVE THE QUERY PARAMS
-              target={item.target}
-            >
+            <a href={item.href + currentQueryParams} target={item.target}>
               {item.title}
             </a>
           </li>
