dotCMS/core

commit 46b0a3d3135cc802b449bd733e4b75f6fbb84d21

feat(logging): create global log4j config (#23631) (#29694)
### Proposed Changes
Issue #23631 

* Add global log4j2 configuration
* default log4j2.configurationFile set in tomcat setenv.sh
* default
Log4jContextSelector=org.apache.logging.log4j.core.async.BasicAsyncLoggerContextSelector
   * This provides a single context and is not based on the classloader
* created lib folder for logging jars and added to tomcat classpath
* moved redis session manager jars into their own folder
* Normalized maven dependencies to help with debugging and versions
centrally specified in bom
* Removed log4j reloading functionality 

As well as using a common configuration for tomcat and webapp logs. This
also resolves an inconsistency when The logging class is referenced in
code before the ContextLifecycleListener is called. Previously the wrong
configuration contained in the resources classpath of the ant-tooling
jar was picked up initially.

Tests

* Test tomcat log messages are output to the same location
* Test any issues with logging from plugins
* Test ability to override log4j config and change log levels for core
tomcat classes, webapp classes and plugin classes. currently can map to
/srv/OVERRIDE/WEB-INF/log4j in container or update CATALINA_OPTS to
include -Dlog4j2.configurationFile={path to file}
* Test session manager functionality is not broken
* Will need a review of the logging behavior from cloud engineering when
this image is applied into our cloud environment including any log-level
changes they may want to have by default


### Further changes
There is follow up work to this PR that improves the way that the config
can be specified and overridden. This change maintains the existing
config file behavior but just applies it across the entire application.



### Checklist
- [ ] Tests
- [ ] Translations
- [ ] Security Implications Contemplated (add notes if applicable)

### Additional Info
This PR resolves #23631 (create global log4j config).


### Screenshots

dotCMS/pom.xml
@@ -15,8 +15,6 @@
         <palantirJavaFormat.version>2.29.0</palantirJavaFormat.version>
         <src.dir>${project.basedir}/src/main/java</src.dir>
         <test.src.dir>${project.basedir}/src/test/java</test.src.dir>
-        <version.spotless.plugin>2.36.0</version.spotless.plugin>
-        <batik.version>1.16</batik.version>
         <glowroot.version>0.14.1</glowroot.version>
         <version.cargo.plugin>1.10.6</version.cargo.plugin>
         <assembly-directory>${basedir}/target/dist</assembly-directory>
@@ -27,6 +25,8 @@
         <buildDocker>true</buildDocker>
         <tomcat-dist-folder>dotserver/tomcat-${tomcat.version}</tomcat-dist-folder>
         <tomcat-lib-folder>${assembly-directory}/${tomcat-dist-folder}/lib</tomcat-lib-folder>
+        <tomcat-log4j-lib-folder>${assembly-directory}/${tomcat-dist-folder}/log4j2/lib</tomcat-log4j-lib-folder>
+        <session-manager-lib-folder>${assembly-directory}/${tomcat-dist-folder}/session-manager/lib</session-manager-lib-folder>
         <tomcat9-overrides>${project.basedir}/src/main/resources/container/tomcat9</tomcat9-overrides>
         <exploded-webapp-dir>${assembly-directory}/${tomcat-dist-folder}/webapps/ROOT</exploded-webapp-dir>
         <war.output.file>${project.build.directory}/${project.build.finalName}-war.war</war.output.file>
@@ -63,10 +63,6 @@
         <test.felix.undeployed.folder>felix/core/undeployed</test.felix.undeployed.folder>
         <test.felix.system.undeployed.folder>felix/system/undeployed</test.felix.system.undeployed.folder>
         <!-- <docker.platforms>linux/arm64,linux/amd64</docker.platforms>-->
-        <tomcat.redis-session-manager.version>1.3</tomcat.redis-session-manager.version>
-        <commons-pool2.version>2.11.1</commons-pool2.version>
-        <org.slfj4.version>1.7.36</org.slfj4.version>
-        <jedis.version>4.4.6</jedis.version>
         <tomcat.run.data.folder>${project.build.directory}/tomcat-run-data/${context.name}</tomcat.run.data.folder>
         <cleanup.data.folder>false</cleanup.data.folder>
         <!-- Add enterprise to sonar sources -->
@@ -86,6 +82,11 @@
             <artifactId>dotcms-core-bundles</artifactId>
             <version>${project.version}</version>
             <type>zip</type>
+            <scope>provided</scope>
+        </dependency>
+        <dependency>
+            <groupId>com.dotcms</groupId>
+            <artifactId>tomcat-redis-session-manager</artifactId>
             <scope>provided</scope>
         </dependency>
         <dependency>
@@ -1351,21 +1352,30 @@
             <!-- Support Java Commons Logging API in Log4j2 -->
             <groupId>org.apache.logging.log4j</groupId>
             <artifactId>log4j-jcl</artifactId>
+            <scope>runtime</scope>
         </dependency>
         <dependency>
-            <!-- Support SLF4j2 API in Log4j2 -->
+            <!-- Support Java Commons Logging API in Log4j2 -->
             <groupId>org.apache.logging.log4j</groupId>
-            <artifactId>log4j-slf4j2-impl</artifactId>
+            <artifactId>log4j-jcl</artifactId>
+            <scope>runtime</scope>
         </dependency>
         <dependency>
-            <!-- Servlet container initialization of Log4j2 -->
+            <groupId>org.slf4j</groupId>
+            <artifactId>slf4j-api</artifactId>
+            <scope>compile</scope>
+        </dependency>
+        <dependency>
+            <!-- Added to tomcat classpath -->
             <groupId>org.apache.logging.log4j</groupId>
-            <artifactId>log4j-web</artifactId>
+            <artifactId>log4j-appserver</artifactId>
+            <scope>provided</scope>
         </dependency>
         <dependency>
             <!-- Async support for Log4J 2.x -->
             <groupId>com.lmax</groupId>
             <artifactId>disruptor</artifactId>
+            <scope>runtime</scope>
         </dependency>
 
         <!-- Graalvm Js Engine -->
@@ -1402,6 +1412,12 @@
             </exclusions>
         </dependency>
 
+        <dependency>
+            <groupId>org.apache.tomcat</groupId>
+            <artifactId>tomcat-catalina</artifactId>
+            <version>${tomcat.version}</version>
+            <scope>provided</scope>
+        </dependency>
 
     </dependencies>
     <dependencyManagement>
@@ -1535,37 +1551,69 @@
                         <configuration>
                             <artifactItems>
                                 <artifactItem>
-                                    <groupId>com.dotcms</groupId>
-                                    <artifactId>tomcat-redis-session-manager</artifactId>
-                                    <version>${tomcat.redis-session-manager.version}</version>
+                                    <groupId>org.apache.logging.log4j</groupId>
+                                    <artifactId>log4j-api</artifactId>
                                     <type>jar</type>
-                                    <overWrite>true</overWrite>
-                                    <outputDirectory>${tomcat-lib-folder}</outputDirectory>
+                                    <overWrite>false</overWrite>
+                                    <outputDirectory>${tomcat-log4j-lib-folder}</outputDirectory>
                                 </artifactItem>
                                 <artifactItem>
-                                    <groupId>org.apache.commons</groupId>
-                                    <artifactId>commons-pool2</artifactId>
-                                    <version>${commons-pool2.version}</version>
+                                    <groupId>org.apache.logging.log4j</groupId>
+                                    <artifactId>log4j-core</artifactId>
                                     <type>jar</type>
-                                    <overWrite>true</overWrite>
-                                    <outputDirectory>${tomcat-lib-folder}</outputDirectory>
+                                    <overWrite>false</overWrite>
+                                    <outputDirectory>${tomcat-log4j-lib-folder}</outputDirectory>
+                                </artifactItem>
+                                <artifactItem>
+                                    <groupId>org.apache.logging.log4j</groupId>
+                                    <artifactId>log4j-appserver</artifactId>
+                                    <type>jar</type>
+                                    <overWrite>false</overWrite>
+                                    <outputDirectory>${tomcat-log4j-lib-folder}</outputDirectory>
+                                </artifactItem>
+                                <artifactItem>
+                                    <groupId>org.apache.logging.log4j</groupId>
+                                    <artifactId>log4j-slf4j2-impl</artifactId>
+                                    <type>jar</type>
+                                    <overWrite>false</overWrite>
+                                    <outputDirectory>${tomcat-log4j-lib-folder}</outputDirectory>
                                 </artifactItem>
                                 <artifactItem>
                                     <groupId>org.slf4j</groupId>
                                     <artifactId>slf4j-api</artifactId>
-                                    <version>${org.slfj4.version}</version>
+                                    <type>jar</type>
+                                    <overWrite>false</overWrite>
+                                    <outputDirectory>${tomcat-log4j-lib-folder}</outputDirectory>
+                                </artifactItem>
+                                <artifactItem>
+                                    <groupId>com.dotcms</groupId>
+                                    <artifactId>tomcat-redis-session-manager</artifactId>
                                     <type>jar</type>
                                     <overWrite>true</overWrite>
-                                    <outputDirectory>${tomcat-lib-folder}</outputDirectory>
+                                    <outputDirectory>${session-manager-lib-folder}</outputDirectory>
+                                </artifactItem>
+                                <artifactItem>
+                                    <groupId>org.apache.commons</groupId>
+                                    <artifactId>commons-pool2</artifactId>
+                                    <type>jar</type>
+                                    <overWrite>true</overWrite>
+                                    <outputDirectory>${session-manager-lib-folder}</outputDirectory>
                                 </artifactItem>
                                 <artifactItem>
                                     <groupId>redis.clients</groupId>
                                     <artifactId>jedis</artifactId>
-                                    <version>${jedis.version}</version>
                                     <type>jar</type>
                                     <overWrite>true</overWrite>
-                                    <outputDirectory>${tomcat-lib-folder}</outputDirectory>
+                                    <outputDirectory>${session-manager-lib-folder}</outputDirectory>
                                 </artifactItem>
+
+                                <dependency>
+                                    <!-- Async support for Log4J 2.x -->
+                                    <groupId>com.lmax</groupId>
+                                    <artifactId>disruptor</artifactId>
+                                    <overWrite>true</overWrite>
+                                    <outputDirectory>${tomcat-log4j-lib-folder}</outputDirectory>
+                                </dependency>
                             </artifactItems>
                         </configuration>
                     </execution>
@@ -1913,12 +1961,6 @@
                                             <file>${tomcat9-overrides}/bin/setenv.sh</file>
                                             <todir>bin</todir>
                                         </configfile>
-                                        <!--
-                                        <configfile>
-                                            <file>${tomcat9-overrides}/conf/catalina.properties</file>
-                                            <todir>conf</todir>
-                                        </configfile>
-                                        -->
                                         <configfile>
                                             <file>${tomcat9-overrides}/conf/server.xml</file>
                                             <todir>conf</todir>

dotCMS/src/main/resources/container/tomcat9/bin/setenv.bat
@@ -1,5 +1,6 @@
 @echo off
 
+rem Set CATALINA_OPTS with multiple options
 set "CATALINA_OPTS=%CATALINA_OPTS% -Dfile.encoding=UTF8"
 set "CATALINA_OPTS=%CATALINA_OPTS% --illegal-access=deny"
 set "CATALINA_OPTS=%CATALINA_OPTS% --add-opens java.base/java.lang=ALL-UNNAMED"
@@ -27,3 +28,32 @@ set "CATALINA_OPTS=%CATALINA_OPTS% -Djavax.xml.transform.TransformerFactory=com.
 set "CATALINA_OPTS=%CATALINA_OPTS% -Djavax.xml.parsers.DocumentBuilderFactory=com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl"
 set "CATALINA_OPTS=%CATALINA_OPTS% -Djavax.xml.parsers.SAXParserFactory=com.sun.org.apache.xerces.internal.jaxp.SAXParserFactoryImpl"
 set "CATALINA_OPTS=%CATALINA_OPTS% -Dorg.apache.tomcat.util.digester.PROPERTY_SOURCE=org.apache.tomcat.util.digester.EnvironmentPropertySource"
+
+
+rem Check if log4j2.configurationFile is already set
+echo %CATALINA_OPTS% | findstr /C:"-Dlog4j2.configurationFile" >nul
+if %errorlevel% neq 0 (
+  echo Setting log4j2.configurationFile=%TOMCAT_HOME%\webapps\ROOT\WEB-INF\log4j\log4j2.xml
+  set "CATALINA_OPTS=%CATALINA_OPTS% -Dlog4j2.configurationFile=%TOMCAT_HOME%\webapps\ROOT\WEB-INF\log4j\log4j2.xml"
+) else (
+  echo Log4j configuration already set
+)
+
+rem Check if Log4jContextSelector is already set
+echo %CATALINA_OPTS% | findstr /C:"-DLog4jContextSelector" >nul
+if %errorlevel% neq 0 (
+  echo Setting Log4jContextSelector to org.apache.logging.log4j.core.async.BasicAsyncLoggerContextSelector
+  set "CATALINA_OPTS=%CATALINA_OPTS% -DLog4jContextSelector=org.apache.logging.log4j.core.async.BasicAsyncLoggerContextSelector"
+) else (
+  echo Log4jContextSelector already set
+)
+
+rem Set the CLASSPATH
+set "ADDITIONAL_CLASSPATH=%CATALINA_HOME%\log4j2\lib\*;%CATALINA_HOME%\session-manager\lib\*"
+if "%CLASSPATH%" neq "" (
+  set "CLASSPATH=%CLASSPATH%;%ADDITIONAL_CLASSPATH%"
+) else (
+  set "CLASSPATH=%ADDITIONAL_CLASSPATH%"
+)
+
+set CLASSPATH

dotCMS/src/main/resources/container/tomcat9/bin/setenv.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 export CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8"
 export CATALINA_OPTS="$CATALINA_OPTS --illegal-access=deny"
 export CATALINA_OPTS="$CATALINA_OPTS --add-opens java.base/java.lang=ALL-UNNAMED"
@@ -27,3 +29,29 @@ export CATALINA_OPTS="$CATALINA_OPTS -Djavax.xml.transform.TransformerFactory=co
 export CATALINA_OPTS="$CATALINA_OPTS -Djavax.xml.parsers.DocumentBuilderFactory=com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl"
 export CATALINA_OPTS="$CATALINA_OPTS -Djavax.xml.parsers.SAXParserFactory=com.sun.org.apache.xerces.internal.jaxp.SAXParserFactoryImpl"
 export CATALINA_OPTS="$CATALINA_OPTS -Dorg.apache.tomcat.util.digester.PROPERTY_SOURCE=org.apache.tomcat.util.digester.EnvironmentPropertySource"
+
+
+if echo "$CATALINA_OPTS" | grep -q '\-Dlog4j2\.configurationFile'; then
+  echo "Log4j configuration already set"
+else
+  echo "Setting log4j2.configurationFile=$TOMCAT_HOME/webapps/ROOT/WEB-INF/log4j/log4j2.xml"
+  export CATALINA_OPTS="$CATALINA_OPTS -Dlog4j2.configurationFile=$TOMCAT_HOME/webapps/ROOT/WEB-INF/log4j/log4j2.xml"
+fi
+
+if echo "$CATALINA_OPTS" | grep -q '\-DLog4jContextSelector'; then
+  echo "Log4jContextSelector already set"
+else
+  echo "Setting Log4jContextSelector to org.apache.logging.log4j.core.async.BasicAsyncLoggerContextSelector"
+  export CATALINA_OPTS="$CATALINA_OPTS -DLog4jContextSelector=org.apache.logging.log4j.core.async.BasicAsyncLoggerContextSelector"
+fi
+
+ADDITIONAL_CLASSPATH="$CATALINA_HOME/log4j2/lib/*:$CATALINA_HOME/session-manager/lib/*"
+
+if [ -n "$CLASSPATH" ]; then
+  CLASSPATH="$CLASSPATH:$ADDITIONAL_CLASSPATH"
+else
+  CLASSPATH="$ADDITIONAL_CLASSPATH"
+fi
+
+export CLASSPATH
+

bom/application/pom.xml
@@ -1434,6 +1434,13 @@
             <!-- Apache Tomcat -->
 
             <dependency>
+                <groupId>com.dotcms</groupId>
+                <artifactId>tomcat-redis-session-manager</artifactId>
+                <version>1.3</version>
+                <scope>provided</scope>
+            </dependency>
+
+            <dependency>
                 <!-- Jasper is the Tomcat JSP Engine -->
                 <groupId>org.apache.tomcat</groupId>
                 <artifactId>tomcat-jasper</artifactId>

bom/logging/pom.xml
@@ -15,8 +15,8 @@
     <properties>
         <maven.deploy.skip>false</maven.deploy.skip>
         <flatten.mode>bom</flatten.mode>
-        <log4j.version>2.20.0</log4j.version>
-        <slf4j.version>2.0.7</slf4j.version>
+        <log4j.version>2.23.1</log4j.version>
+        <slf4j.version>2.0.9</slf4j.version>
     </properties>
 
     <dependencyManagement>
@@ -27,30 +27,42 @@
              Do not include incompatible artifacts e.g. -->
             <dependency>
                 <groupId>org.apache.logging.log4j</groupId>
-                <artifactId>log4j-core</artifactId>
+                <artifactId>log4j-api</artifactId>
                 <version>${log4j.version}</version>
                 <scope>compile</scope>
             </dependency>
             <dependency>
                 <!-- Support log4j1.2.x API in Log4j2 -->
                 <groupId>org.apache.logging.log4j</groupId>
-                <artifactId>log4j-1.2-api</artifactId>
+                <artifactId>log4j-iostreams</artifactId>
+                <version>${log4j.version}</version>
+            </dependency>
+            <dependency>
+                <groupId>org.apache.logging.log4j</groupId>
+                <artifactId>log4j-core</artifactId>
                 <version>${log4j.version}</version>
                 <scope>compile</scope>
             </dependency>
             <dependency>
                 <!-- Support Java Commons Logging API in Log4j2 -->
                 <groupId>org.apache.logging.log4j</groupId>
+                <artifactId>log4j-1.2-api</artifactId>
+                <version>${log4j.version}</version>
+                <scope>runtime</scope>
+            </dependency>
+            <dependency>
+                <!-- Support Java Commons Logging API in Log4j2 -->
+                <groupId>org.apache.logging.log4j</groupId>
                 <artifactId>log4j-jcl</artifactId>
                 <version>${log4j.version}</version>
-                <scope>compile</scope>
+                <scope>runtime</scope>
             </dependency>
             <dependency>
                 <!-- Support SLF4j2 API in Log4j2 -->
                 <groupId>org.apache.logging.log4j</groupId>
                 <artifactId>log4j-slf4j2-impl</artifactId>
                 <version>${log4j.version}</version>
-                <scope>compile</scope>
+                <scope>runtime</scope>
             </dependency>
             <dependency>
                 <groupId>org.slf4j</groupId>
@@ -61,16 +73,16 @@
             <dependency>
                 <!-- Servlet container initialization of Log4j2 -->
                 <groupId>org.apache.logging.log4j</groupId>
-                <artifactId>log4j-web</artifactId>
+                <artifactId>log4j-appserver</artifactId>
                 <version>${log4j.version}</version>
-                <scope>compile</scope>
+                <scope>runtime</scope>
             </dependency>
             <dependency>
                 <!-- Async support for Log4J 2.x -->
                 <groupId>com.lmax</groupId>
                 <artifactId>disruptor</artifactId>
                 <version>3.3.4</version>
-                <scope>compile</scope>
+                <scope>runtime</scope>
             </dependency>
 
         </dependencies>

dotcms-postman/pom.xml
@@ -113,7 +113,7 @@
                                     </bind>
                                 </volumes>
                                 <log>
-                                    <prefix>WireMock:</prefix>
+                                    <prefix>[WireMock]</prefix>
                                     <color>green</color>
                                 </log>
                                 <env>

parent/pom.xml
@@ -306,7 +306,7 @@
                                         <log>listening on IPv4 address "0.0.0.0", port 5432</log>
                                     </wait>
                                     <log>
-                                        <prefix>DB:</prefix>
+                                        <prefix>[DB]</prefix>
                                         <color>magenta</color>
                                     </log>
                                 </run>
@@ -345,7 +345,7 @@
                                         <shutdown>5000</shutdown>
                                     </wait>
                                     <log>
-                                        <prefix>ES:</prefix>
+                                        <prefix>[ES]</prefix>
                                         <color>cyan</color>
                                     </log>
                                 </run>
@@ -392,7 +392,7 @@
                                         <link>opensearch:es</link>
                                     </links>
                                     <log>
-                                        <prefix>DOTCMS:</prefix>
+                                        <prefix>[DOTCMS]</prefix>
                                         <color>cyan</color>
                                     </log>
                                 </run>
@@ -708,6 +708,13 @@
 
                         <trimStackTrace>false</trimStackTrace>
                     </configuration>
+                    <dependencies>
+                        <dependency>
+                            <groupId>org.apache.maven.surefire</groupId>
+                            <artifactId>surefire-junit47</artifactId>
+                            <version>${version.failsafe.plugin}</version>
+                        </dependency>
+                    </dependencies>
                 </plugin>
                 <plugin>
                     <groupId>org.apache.maven.plugins</groupId>
@@ -861,6 +868,13 @@
                         <systemPropertyVariables/>
                         <trimStackTrace>true</trimStackTrace>
                     </configuration>
+                   <dependencies>
+                        <dependency>
+                            <groupId>org.apache.maven.surefire</groupId>
+                            <artifactId>surefire-junit47</artifactId>
+                            <version>${version.surefire.plugin}</version>
+                        </dependency>
+                    </dependencies>
                 </plugin>
                 <plugin>
                     <groupId>org.apache.maven.plugins</groupId>

.mvn/maven-build-cache-config.xml
@@ -12,7 +12,7 @@ https://maven.apache.org/extensions/maven-build-cache-extension/maven-build-cach
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
     xsi:schemaLocation="http://maven.apache.org/BUILD-CACHE-CONFIG/1.0.0 https://maven.apache.org/xsd/build-cache-config-1.0.0.xsd">
     <configuration>
-        <enabled>true</enabled>
+        <enabled>false</enabled>
         <hashAlgorithm>SHA-256</hashAlgorithm>
         <validateXml>true</validateXml>
         <local>

commit 1c414a16e2702bdef4af83c11d0ff5e4528ddac1

fix: revert session manager jar location change from PR #29694 (#23631) (#29746)
### Proposed Changes
* Fix classloader issue with session manager introduced in PR #29694
* Use CATALINA_HOME in setenv scripts instead of TOMCAT_HOME. The first
is always available, the second is only set within the docker startup
scripts

Instructions for Log4j require the classes to be added to the system
classloader when tomcat loads by adding to the main java CLASSPATH.
These logging jars do not need to reference any of the tomcat jars
the session manager jars do need to reference the tomcat jars e.g.
catalina.jar in the main lib folder. This is what is called the
common.loader and is defined in catalina.properties file.
the session manager jars either need to be in a folder referenced in
this common.loader property or as before dropped into the existing lib
folder. There is no need to change the old behaviour at this time so we
can just change the install location back.

dotCMS/pom.xml
@@ -26,7 +26,7 @@
         <tomcat-dist-folder>dotserver/tomcat-${tomcat.version}</tomcat-dist-folder>
         <tomcat-lib-folder>${assembly-directory}/${tomcat-dist-folder}/lib</tomcat-lib-folder>
         <tomcat-log4j-lib-folder>${assembly-directory}/${tomcat-dist-folder}/log4j2/lib</tomcat-log4j-lib-folder>
-        <session-manager-lib-folder>${assembly-directory}/${tomcat-dist-folder}/session-manager/lib</session-manager-lib-folder>
+        <session-manager-lib-folder>${tomcat-lib-folder}</session-manager-lib-folder>
         <tomcat9-overrides>${project.basedir}/src/main/resources/container/tomcat9</tomcat9-overrides>
         <exploded-webapp-dir>${assembly-directory}/${tomcat-dist-folder}/webapps/ROOT</exploded-webapp-dir>
         <war.output.file>${project.build.directory}/${project.build.finalName}-war.war</war.output.file>

dotCMS/src/main/resources/container/tomcat9/bin/setenv.bat
@@ -33,8 +33,8 @@ set "CATALINA_OPTS=%CATALINA_OPTS% -Dorg.apache.tomcat.util.digester.PROPERTY_SO
 rem Check if log4j2.configurationFile is already set
 echo %CATALINA_OPTS% | findstr /C:"-Dlog4j2.configurationFile" >nul
 if %errorlevel% neq 0 (
-  echo Setting log4j2.configurationFile=%TOMCAT_HOME%\webapps\ROOT\WEB-INF\log4j\log4j2.xml
-  set "CATALINA_OPTS=%CATALINA_OPTS% -Dlog4j2.configurationFile=%TOMCAT_HOME%\webapps\ROOT\WEB-INF\log4j\log4j2.xml"
+  echo Setting log4j2.configurationFile=%CATALINA_HOME%\webapps\ROOT\WEB-INF\log4j\log4j2.xml
+  set "CATALINA_OPTS=%CATALINA_OPTS% -Dlog4j2.configurationFile=%CATALINA_HOME%\webapps\ROOT\WEB-INF\log4j\log4j2.xml"
 ) else (
   echo Log4j configuration already set
 )
@@ -49,7 +49,7 @@ if %errorlevel% neq 0 (
 )
 
 rem Set the CLASSPATH
-set "ADDITIONAL_CLASSPATH=%CATALINA_HOME%\log4j2\lib\*;%CATALINA_HOME%\session-manager\lib\*"
+set "ADDITIONAL_CLASSPATH=%CATALINA_HOME%\log4j2\lib\*"
 if "%CLASSPATH%" neq "" (
   set "CLASSPATH=%CLASSPATH%;%ADDITIONAL_CLASSPATH%"
 ) else (

dotCMS/src/main/resources/container/tomcat9/bin/setenv.sh
@@ -34,8 +34,8 @@ export CATALINA_OPTS="$CATALINA_OPTS -Dorg.apache.tomcat.util.digester.PROPERTY_
 if echo "$CATALINA_OPTS" | grep -q '\-Dlog4j2\.configurationFile'; then
   echo "Log4j configuration already set"
 else
-  echo "Setting log4j2.configurationFile=$TOMCAT_HOME/webapps/ROOT/WEB-INF/log4j/log4j2.xml"
-  export CATALINA_OPTS="$CATALINA_OPTS -Dlog4j2.configurationFile=$TOMCAT_HOME/webapps/ROOT/WEB-INF/log4j/log4j2.xml"
+  echo "Setting log4j2.configurationFile=$CATALINA_HOME/webapps/ROOT/WEB-INF/log4j/log4j2.xml"
+  export CATALINA_OPTS="$CATALINA_OPTS -Dlog4j2.configurationFile=$CATALINA_HOME/webapps/ROOT/WEB-INF/log4j/log4j2.xml"
 fi
 
 if echo "$CATALINA_OPTS" | grep -q '\-DLog4jContextSelector'; then
@@ -45,7 +45,7 @@ else
   export CATALINA_OPTS="$CATALINA_OPTS -DLog4jContextSelector=org.apache.logging.log4j.core.async.BasicAsyncLoggerContextSelector"
 fi
 
-ADDITIONAL_CLASSPATH="$CATALINA_HOME/log4j2/lib/*:$CATALINA_HOME/session-manager/lib/*"
+ADDITIONAL_CLASSPATH="$CATALINA_HOME/log4j2/lib/*"
 
 if [ -n "$CLASSPATH" ]; then
   CLASSPATH="$CLASSPATH:$ADDITIONAL_CLASSPATH"