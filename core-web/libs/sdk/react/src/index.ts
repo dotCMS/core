77da00c432fcaa3218b58b8e4c19cd85bb57d775
Kevin Davila <kfariid@gmail.com>
2024-08-25 17:32:22 -0500

    Fix for lint

--- core-web/libs/sdk/react/src/index.ts
+++ core-web/libs/sdk/react/src/index.ts
@@ -2,4 +2,4 @@ export * from './lib/components/DotcmsLayout/DotcmsLayout';
 export * from './lib/components/PageProvider/PageProvider';
 export * from './lib/components/Row/Row';
 export * from './lib/hooks/useDotcmsPageContext';
-export * from './lib/components/BlockEditorRenderer/BlockEditorRenderer'
+export * from './lib/components/BlockEditorRenderer/BlockEditorRenderer';
