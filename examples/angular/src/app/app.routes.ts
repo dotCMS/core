c7a000718f8773438c63692562453db847259691
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-08-28 16:10:03 -0600

    chore: Clean up the Angular example

--- examples/angular/src/app/app.routes.ts
+++ examples/angular/src/app/app.routes.ts
@@ -1,24 +1,22 @@
 import { Routes } from '@angular/router';
-
-import { canMatchPage } from './guards/can-match-page.guard';
-import { DotCMSPageResolver } from './resolver/dotcms-page.resolver';
-
 import { DotCMSPagesComponent } from './pages/pages.component';
 import { NotFoundComponent } from './pages/notFound/notFound.component';
 
 export const routes: Routes = [
   {
     path: '**',
-      resolve: {
-      // This should be called `context`.
-      context: DotCMSPageResolver,
-    },
-    canMatch: [canMatchPage],
     component: DotCMSPagesComponent,
-    runGuardsAndResolvers: 'always', // Run the resolver on every navigation. Even if the URL hasn't changed.
+    runGuardsAndResolvers: 'always'
   },
-  {
-    path: '**',
-    component: NotFoundComponent
-  },
+  { 
+    path: '',
+    pathMatch: 'full',
+    component: DotCMSPagesComponent,
+    runGuardsAndResolvers: 'always'
+  }, // Default route
+
+  // {
+  //   path: '**',
+  //   component: NotFoundComponent
+  // },
 ];
--- examples/angular/src/app/pages/pages.component.ts
+++ examples/angular/src/app/pages/pages.component.ts
@@ -8,7 +8,7 @@ import {
   signal,
 } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -16,9 +16,13 @@ import { NavigationComponent } from './components/navigation/navigation.componen
 
 import { DYNAMIC_COMPONENTS } from '../utils';
 
-import { DotcmsLayoutComponent } from '@dotcms/angular';
+import { DotcmsLayoutComponent, DotcmsNavigationItem } from '@dotcms/angular';
 import { JsonPipe } from '@angular/common';
 import { DOTCMS_CLIENT_TOKEN } from '../client-token/dotcms-client';
+import { map, withLatestFrom } from 'rxjs/operators';
+
+import { getPageRequestParams } from '@dotcms/client';
+
 
 @Component({
   selector: 'dotcms-pages',
@@ -42,22 +46,51 @@ export class DotCMSPagesComponent implements OnInit, OnDestroy {
   private readonly client = inject(DOTCMS_CLIENT_TOKEN);
 
   protected readonly editorCofig = { params: { depth: '2' } };
+  protected slug: string | null = null;
 
   ngOnInit() {
-    // Get the context data from the route
-    this.route.data
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((data) => {
-        this.context.set(data['context']);
-      });
+    this.route.url.pipe(
+      withLatestFrom(this.route.queryParamMap),
+      map(([segments, queryParams]) => ({
+        path: segments.length > 0 ? segments.map(segment => segment.path).join('/') : '/',
+        queryParams: queryParams
+      })),
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe(async ({ path, queryParams }) => {
+      const params = getPageRequestParams({ path, params: queryParams });
+      console.log(params);
 
+      const page = await this.client.page.get(params);
 
-    this.client.editor.on('changes', (pageAsset) => {
-      this.context.update((context) => ({ ...context, pageAsset }));
+      const navProps = {
+        path: '/',
+        depth: 2,
+        languageId: (queryParams as any)['language_id'],
+      };
+    
+      const navResponse = (await this.client.nav.get(navProps)) as {
+        entity: DotcmsNavigationItem;
+      };
+      const nav = navResponse?.entity;
+      console.log({page, nav});
+      // You can use path and queryParams here as needed
     });
+      
+
+    // this.route.data
+    //   .pipe(takeUntilDestroyed(this.destroyRef))
+    //   .subscribe((data) => {
+    //     console.log(data);
+    //     this.context.set(data['context']);
+    //   });
+
+
+    // this.client.editor.on('changes', (pageAsset) => {
+    //   this.context.update((context) => ({ ...context, pageAsset }));
+    // });
   }
 
   ngOnDestroy() {
-    this.client.editor.off('changes');
+    // this.client.editor.off('changes');
   }
 }
