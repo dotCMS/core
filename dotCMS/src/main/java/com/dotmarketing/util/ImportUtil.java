0e6f1cf930391e2c02ee550df373e794ae1a6ab1
Will Ezell <will@dotcms.com>
2024-08-29 11:53:49 -0400

    fix(imports) possible fix for imports running out of memory (#29169)
    This does a few things:
    - Removes reading the file in as a byte[] and instead passes around a
    `File` object
    - Changes how the import is run inline (helpful for glowroot traces) or
    to use the DotSubmitter thread pool. Config var is
    `IMPORT_CONTENTLETS_ASYNC`
    - Commit Granularity changed to 100, meaning every 100 contentlets will
    be committed and a new transaction will be started.
    - Copied CSV file to tmp dir.
    
    
    
    ref: #29162
    
    ---------
    
    Co-authored-by: erickgonzalez <erick.gonzalez@dotcms.com>

--- dotCMS/src/main/java/com/dotmarketing/util/ImportUtil.java
+++ dotCMS/src/main/java/com/dotmarketing/util/ImportUtil.java
@@ -100,7 +100,7 @@ public class ImportUtil {
     private final static String languageCodeHeader = "languageCode";
     private final static String countryCodeHeader = "countryCode";
 
-    private final static int commitGranularity = 500;
+    private final static int commitGranularity = 100;
     private final static int sleepTime = 200;
 
     public static final String[] IMP_DATE_FORMATS = new String[] { "d-MMM-yy", "MMM-yy", "MMMM-yy", "d-MMM", "dd-MMM-yyyy",
@@ -219,7 +219,6 @@ public class ImportUtil {
                 }
                 lineNumber++;
                 // Log preview/import status every 100 processed records
-                int loggingPoint = 100;
                 //Reading the whole file
                 if (headers.size() > 0) {
                     if (!preview) {
@@ -231,10 +230,6 @@ public class ImportUtil {
                             break;
                         }
                         lineNumber++;
-                        if (lineNumber % loggingPoint == 0) {
-                            final String action = preview ? "previewed." : "imported.";
-                            Logger.info(ImportUtil.class, String.format("-> %d entries have been %s", lineNumber, action));
-                        }
                         csvLine = csvreader.getValues();
                         try {
                             lines++;
@@ -281,9 +276,13 @@ public class ImportUtil {
                                 errors++;
                             }
 
-                            if ( !preview && (lineNumber % commitGranularity == 0) ) {
-                                HibernateUtil.closeAndCommitTransaction();
-                                HibernateUtil.startTransaction();
+                            if (lineNumber % commitGranularity == 0) {
+                                final String action = preview ? "previewed." : "imported.";
+                                Logger.info(ImportUtil.class, String.format("-> %d entries have been %s", lineNumber, action));
+                                if(!preview) {
+                                    HibernateUtil.closeAndCommitTransaction();
+                                    HibernateUtil.startTransaction();
+                                }
                             }
                         } catch (final DotRuntimeException ex) {
                             String errorMessage = getErrorMsgFromException(user, ex);
