b26825971c0f510b8bbca1378682bb95a036526c
Daniel Silva <daniel.silva@dotcms.com>
2024-08-30 12:22:56 -0600

    #29288 return total in content pull

--- dotcms-integration/src/test/java/com/dotcms/rendering/velocity/viewtools/content/ContentToolTest.java
+++ dotcms-integration/src/test/java/com/dotcms/rendering/velocity/viewtools/content/ContentToolTest.java
@@ -38,6 +38,7 @@ import com.dotmarketing.portlets.languagesmanager.business.LanguageAPI;
 import com.dotmarketing.portlets.languagesmanager.model.Language;
 import com.dotmarketing.portlets.structure.model.Relationship;
 import com.dotmarketing.util.PageMode;
+import com.dotmarketing.util.PaginatedArrayList;
 import com.dotmarketing.util.PaginatedContentList;
 import com.dotmarketing.util.WebKeys.Relationship.RELATIONSHIP_CARDINALITY;
 import com.liferay.portal.model.User;
@@ -53,6 +54,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.apache.velocity.context.Context;
@@ -879,5 +881,22 @@ public class ContentToolTest extends IntegrationTestBase {
                 children.containsAll(pullRelatedContent));
 
 
+    }
+
+    @Test
+    public void testPull_includeTotal() {
+        final ContentType blogLikeType = TestDataUtils.getBlogLikeContentType();
+
+        final ContentletDataGen contentletDataGen = new ContentletDataGen(blogLikeType.inode()).host(defaultHost);
+        IntStream.range(0, 10).forEach(i -> contentletDataGen.nextPersisted());
+
+        final ContentTool contentTool = getContentTool(defaultLanguage.getId());
+
+        final PaginatedArrayList<ContentMap> results = contentTool.pull(
+                "+contentType:" + blogLikeType.variable(), 0, 0,
+                "modDate desc"
+        );
+
+        Assert.assertEquals(10, results.getTotalResults());
     }
 }
