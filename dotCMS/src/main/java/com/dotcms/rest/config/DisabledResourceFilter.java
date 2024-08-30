c3c8f78676c151b4a081d2c877e457668572bfb9
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2024-08-30 17:24:59 -0600

    #29552

---
+++ dotCMS/src/main/java/com/dotcms/rest/config/DisabledResourceFilter.java
@@ -0,0 +1,33 @@
+package com.dotcms.rest.config;
+
+import com.dotmarketing.util.Logger;
+import com.jayway.jsonpath.internal.Utils;
+import java.io.IOException;
+import javax.annotation.Priority;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@Priority(1)
+public class DisabledResourceFilter  implements ContainerRequestFilter {
+
+    @Context
+    private ResourceInfo resourceInfo;
+
+    @Override
+    public void filter(final ContainerRequestContext requestContext) throws IOException {
+        Class<?> resourceClass = resourceInfo.getResourceClass();
+        if (resourceClass.isAnnotationPresent(Disabled.class)) {
+            final Disabled annotation = resourceClass.getAnnotation(Disabled.class);
+            final Status status = annotation.status();
+            final String message = Utils.isEmpty(annotation.message()) ? status.getReasonPhrase() : annotation.message();
+            requestContext.abortWith(Response.status(annotation.status()).entity(message).build());
+            Logger.debug(this.getClass(), "Resource " + resourceClass.getName() + " is marked disabled.");
+        }
+    }
+}
