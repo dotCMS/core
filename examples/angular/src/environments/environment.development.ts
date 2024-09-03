4e9b14992af7fee3a32aea5451ca241405f5fde6
Freddy Montes <751424+fmontes@users.noreply.github.com>
2024-09-03 15:38:51 -0600

    chore: clean up

--- examples/angular/src/environments/environment.development.ts
+++ examples/angular/src/environments/environment.development.ts
@@ -5,6 +5,6 @@ export const environment = {
    * for security purposes, make sure is read-only.
    * More info: https://www.dotcms.com/docs/latest/permissions
    */
-  authToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhcGliY2QwYzE2My00ZDYwLTQ1NmMtYmE4My1iM2MyYmIwNGI2MzEiLCJ4bW9kIjoxNzI1MzczNTYxMDAwLCJuYmYiOjE3MjUzNzM1NjEsImlzcyI6IjAwYjRmM2NjNGUiLCJleHAiOjE3MjYyMzc1NjEsImlhdCI6MTcyNTM3MzU2MSwianRpIjoiOWYwYjUyYTItMGZiMy00YjRhLTgyM2EtYTk2ZTgzNmI3ODg1In0.eJVw5e-ubL4L9kgyrz3x_czM5kprCJ9lCtbDfdf05N8',
+  authToken: 'YOUR_AUTH_TOKEN',
   siteId: 'true',
 };
