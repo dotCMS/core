b3427e962c40385faa3d45a87e191766c262c9aa
Fabrizzio Araya <37148755+fabrizzio-dotCMS@users.noreply.github.com>
2024-09-03 16:33:31 -0600

    feat(cdi) Upgrading Jersey to 2.28 and including CDI through Weld  Refs:29552  (#29822)
    Proposed Changes
    In this PR we are upgrading to jersey 2.28
    We are changing DotRestApplication to use package discovery to load
    Resources and Providers
    We're making some adjustments on some Mappers to preserve the present
    behavior
    We're introducing CDI through Weld container
    This PR fixes: https://github.com/dotCMS/core/issues/29552
    
    ---------
    
    Co-authored-by: spbolton <steve.bolton@dotcms.com>

--- dotCMS/src/main/java/com/dotcms/rest/exception/mapper/DefaultDotBadRequestExceptionMapper.java
+++ dotCMS/src/main/java/com/dotcms/rest/exception/mapper/DefaultDotBadRequestExceptionMapper.java
@@ -1,6 +1,8 @@
 package com.dotcms.rest.exception.mapper;
 
 import com.dotcms.rest.exception.BadRequestException;
+import javax.ws.rs.ext.Provider;
 
+@Provider
 public class DefaultDotBadRequestExceptionMapper extends DotBadRequestExceptionMapper<BadRequestException> {
 }
