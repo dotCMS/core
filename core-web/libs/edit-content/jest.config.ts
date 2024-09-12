82280d022251e23466f56b843839366869303161
Nicolas Molina <hi@nicobytes.com>
2024-09-12 19:02:15 -0400

    feat(e2e): fix unit tests to avoid run e2e tests

--- core-web/libs/edit-content/jest.config.ts
+++ core-web/libs/edit-content/jest.config.ts
@@ -18,5 +18,9 @@ export default {
         'jest-preset-angular/build/serializers/no-ng-attributes',
         'jest-preset-angular/build/serializers/ng-snapshot',
         'jest-preset-angular/build/serializers/html-comment'
-    ]
+    ],
+    testMatch: [
+        '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+        '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+    ],
 };
