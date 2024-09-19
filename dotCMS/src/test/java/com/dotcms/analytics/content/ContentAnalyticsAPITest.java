4c4ab7960177cce0dd0149ac80a86f4a1f478d02
Jose Castro <jose.castro@dotcms.com>
2024-09-19 16:44:20 -0600

    Unit tests for the API and Factory classes.

---
+++ dotCMS/src/test/java/com/dotcms/analytics/content/ContentAnalyticsAPITest.java
@@ -0,0 +1,93 @@
+package com.dotcms.analytics.content;
+
+import com.dotcms.analytics.model.ResultSetItem;
+import com.dotcms.analytics.query.AnalyticsQuery;
+import com.dotmarketing.business.APILocator;
+import com.liferay.portal.model.User;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Unit tests for the {@link ContentAnalyticsAPI} class.
+ *
+ * @author Jose Castro
+ * @since Sep 19th, 2024
+ */
+public class ContentAnalyticsAPITest {
+
+    /**
+     * <ul>
+     *     <li><b>Method to test: </b>{@link ContentAnalyticsAPI#runReport(AnalyticsQuery, User)}
+     *     </li>
+     *     <li><b>Given Scenario: </b>Run a report for a given query.</li>
+     *     <li><b>Expected Result: </b>The simulated results must adhere to the data specified in
+     *     the query.</li>
+     * </ul>
+     */
+    @Test
+    public void getContentAnalyticsReport() {
+        // ╔════════════════════════╗
+        // ║  Generating Test Data  ║
+        // ╚════════════════════════╝
+        final List<ResultSetItem> dataList = List.of(
+                new ResultSetItem(Map.of(
+                        "request.count", "4",
+                        "request.pageId", "9c5f42da-31b1-4935-9df6-153f5de1bdf2",
+                        "request.pageTitle", "Blogs",
+                        "request.url", "/blog/"
+                )),
+                new ResultSetItem(Map.of(
+                        "request.count", "3",
+                        "request.pageId", "44a076ad-affa-49d4-97b3-6caa3824e7e8",
+                        "request.pageTitle", "Destinations",
+                        "request.url", "/destinations/"
+                )),
+                new ResultSetItem(Map.of(
+                        "request.count", "2",
+                        "request.pageId", "a9f30020-54ef-494e-92ed-645e757171c2",
+                        "request.pageTitle", "home",
+                        "request.url", "/"
+                ))
+        );
+
+        // ╔══════════════════╗
+        // ║  Initialization  ║
+        // ╚══════════════════╝
+        final AnalyticsQuery analyticsQuery = new AnalyticsQuery.Builder()
+                .measures(Set.of("request.count"))
+                .orders("request.count DESC")
+                .dimensions(Set.of("request.url", "request.pageId", "request.pageTitle"))
+                .filters("request.whatAmI = ['PAGE']")
+                .limit(100)
+                .offset(1)
+                .build();
+        final User systemUser = APILocator.systemUser();
+        ContentAnalyticsFactory mockContentAnalyticsFactory = Mockito.mock(ContentAnalyticsFactory.class);
+        Mockito.when(mockContentAnalyticsFactory.getReport(analyticsQuery, systemUser)).thenReturn(new ReportResponse(dataList));
+        final ContentAnalyticsAPI contentAnalyticsAPI = new ContentAnalyticsAPIImpl(mockContentAnalyticsFactory);
+        final ReportResponse report = contentAnalyticsAPI.runReport(analyticsQuery, systemUser);
+
+        // ╔══════════════╗
+        // ║  Assertions  ║
+        // ╚══════════════╝
+        assertNotNull("The result list must never be empty", report.getResults());
+        assertEquals("There must be " + dataList.size() + " results in the list", dataList.size(), report.getResults().size());
+        int i = 0;
+        for (final ResultSetItem resultSetItem : report.getResults()) {
+            final ResultSetItem expectedResultSetItem = dataList.get(i);
+            assertEquals("", expectedResultSetItem.get("request.count").get(), resultSetItem.get("request.count").get());
+            assertEquals("", expectedResultSetItem.get("request.pageId").get(), resultSetItem.get("request.pageId").get());
+            assertEquals("", expectedResultSetItem.get("request.pageTitle").get(), resultSetItem.get("request.pageTitle").get());
+            assertEquals("", expectedResultSetItem.get("request.url").get(), resultSetItem.get("request.url").get());
+            i++;
+        }
+    }
+
+}
