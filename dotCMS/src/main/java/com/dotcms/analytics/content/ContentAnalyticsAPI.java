2fd7a69cfbf6de8b71b300a1ad2a47252f1c918c
Jose Castro <jose.castro@dotcms.com>
2024-09-23 14:34:49 -0600

    Missing Javadoc

--- dotCMS/src/main/java/com/dotcms/analytics/content/ContentAnalyticsAPI.java
+++ dotCMS/src/main/java/com/dotcms/analytics/content/ContentAnalyticsAPI.java
@@ -4,7 +4,10 @@ import com.dotcms.analytics.query.AnalyticsQuery;
 import com.liferay.portal.model.User;
 
 /**
- *
+ * This API allows users to access Content Analytics in dotCMS. Content Analytics will enable
+ * customers to track the health and engagement of their content at the level of individual content
+ * items. This could also allow users to eventually include viewing analytics data in the UI of the
+ * dotCMS back-end.
  *
  * @author Jose Castro
  * @since Sep 13th, 2024
