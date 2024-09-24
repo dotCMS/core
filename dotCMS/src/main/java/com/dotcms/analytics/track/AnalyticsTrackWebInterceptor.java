dd7c61ff28453f6e8d1a5031d6075757366c4e71
jdotcms <jonathan.sanchez@dotcms.com>
2024-09-23 23:12:27 -0600

    #30076 adding the flag to run or not the analytics interceptor

--- dotCMS/src/main/java/com/dotcms/analytics/track/AnalyticsTrackWebInterceptor.java
+++ dotCMS/src/main/java/com/dotcms/analytics/track/AnalyticsTrackWebInterceptor.java
@@ -1,18 +1,25 @@
 package com.dotcms.analytics.track;
 
+import com.dotcms.analytics.app.AnalyticsApp;
 import com.dotcms.analytics.track.collectors.WebEventsCollectorServiceFactory;
 import com.dotcms.analytics.track.matchers.FilesRequestMatcher;
 import com.dotcms.analytics.track.matchers.PagesAndUrlMapsRequestMatcher;
 import com.dotcms.analytics.track.matchers.RequestMatcher;
 import com.dotcms.analytics.track.matchers.VanitiesRequestMatcher;
+import com.dotcms.business.SystemTableUpdatedKeyEvent;
 import com.dotcms.filters.interceptor.Result;
 import com.dotcms.filters.interceptor.WebInterceptor;
+import com.dotcms.saml.DotSamlProxyFactory;
+import com.dotcms.system.event.local.model.EventSubscriber;
 import com.dotcms.util.CollectionsUtils;
 import com.dotcms.util.WhiteBlackList;
+import com.dotmarketing.business.APILocator;
 import com.dotmarketing.util.Config;
 import com.dotmarketing.util.Logger;
 import com.dotmarketing.util.UUIDUtil;
 import com.liferay.util.StringPool;
+import io.vavr.control.Try;
+import org.apache.commons.lang3.mutable.MutableBoolean;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,9 +33,11 @@ import java.util.function.Predicate;
  * Web Interceptor to track analytics
  * @author jsanca
  */
-public class AnalyticsTrackWebInterceptor  implements WebInterceptor {
+public class AnalyticsTrackWebInterceptor  implements WebInterceptor, EventSubscriber<SystemTableUpdatedKeyEvent> {
 
+    private final static String ANALYTICS_TURNED_ON_KEY = "ANALYTICS_TURNED_ON";
     private final static Map<String, RequestMatcher> requestMatchersMap = new ConcurrentHashMap<>();
+    private final MutableBoolean isTurnedOn = new MutableBoolean(Config.getBooleanProperty(ANALYTICS_TURNED_ON_KEY, true));
 
     /// private static final String[] DEFAULT_BLACKLISTED_PROPS = new String[]{"^/api/*"};
     private static final String[] DEFAULT_BLACKLISTED_PROPS = new String[]{StringPool.BLANK};
@@ -70,17 +79,34 @@ public class AnalyticsTrackWebInterceptor  implements WebInterceptor {
     @Override
     public Result intercept(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
 
-        if (whiteBlackList.isAllowed(request.getRequestURI())) {
-            final Optional<RequestMatcher> matcherOpt = this.anyMatcher(request, response, RequestMatcher::runBeforeRequest);
-            if (matcherOpt.isPresent()) {
+        try {
+            if (isAllowed(request)) {
+                final Optional<RequestMatcher> matcherOpt = this.anyMatcher(request, response, RequestMatcher::runBeforeRequest);
+                if (matcherOpt.isPresent()) {
 
-                addRequestId (request);
-                Logger.debug(this, () -> "intercept, Matched: " + matcherOpt.get().getId() + " request: " + request.getRequestURI());
-                fireNext(request, response, matcherOpt.get());
+                    addRequestId(request);
+                    Logger.debug(this, () -> "intercept, Matched: " + matcherOpt.get().getId() + " request: " + request.getRequestURI());
+                    fireNext(request, response, matcherOpt.get());
+                }
             }
+        } catch (Exception e) {
+            Logger.error(this, e.getMessage(), e);
         }
 
         return Result.NEXT;
+    }
+
+    private boolean isAllowed(final HttpServletRequest request) {
+
+        return  isTurnedOn.getValue() &&
+                anyConfig() &&
+                whiteBlackList.isAllowed(request.getRequestURI());
+    }
+
+    private boolean anyConfig() {
+        return
+                Try.of(()->APILocator.getAppsAPI().getAppDescriptor(AnalyticsApp.ANALYTICS_APP_KEY,
+                        APILocator.systemUser())).getOrElseGet(e->Optional.empty()).isPresent();
     }
 
     private void addRequestId(final HttpServletRequest request) {
@@ -92,7 +118,7 @@ public class AnalyticsTrackWebInterceptor  implements WebInterceptor {
     @Override
     public boolean afterIntercept(final HttpServletRequest request, final HttpServletResponse response) {
 
-        if (whiteBlackList.isAllowed(request.getRequestURI())) {
+        if (isAllowed(request)) {
             final Optional<RequestMatcher> matcherOpt = this.anyMatcher(request, response, RequestMatcher::runAfterRequest);
             if (matcherOpt.isPresent()) {
 
@@ -128,4 +154,10 @@ public class AnalyticsTrackWebInterceptor  implements WebInterceptor {
     }
 
 
+    @Override
+    public void notify(final SystemTableUpdatedKeyEvent event) {
+        if (event.getKey().contains(ANALYTICS_TURNED_ON_KEY)) {
+            isTurnedOn.setValue(Config.getBooleanProperty(ANALYTICS_TURNED_ON_KEY, true));
+        }
+    }
 }
