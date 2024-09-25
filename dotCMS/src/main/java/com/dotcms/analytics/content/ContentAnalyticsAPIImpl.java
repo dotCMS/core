e66f8b951a7cb5ceb9f9ce83379a8a3c6a45d466
Jose Castro <jose.castro@dotcms.com>
2024-09-24 18:33:48 -0600

    Create a REST Endpoint to allow running a query directly into CA data (#30044)
    ### Proposed Changes
    * Create a REST Endpoint to allow running a query directly into CA data.
    Initial commit.
    
    ---------
    
    Co-authored-by: jdotcms <jonathan.sanchez@dotcms.com>
    Co-authored-by: freddyDOTCMS <freddy0309@gmail.com>
    Co-authored-by: freddyDOTCMS <147462678+freddyDOTCMS@users.noreply.github.com>

---
+++ dotCMS/src/main/java/com/dotcms/analytics/content/ContentAnalyticsAPIImpl.java
@@ -0,0 +1,34 @@
+package com.dotcms.analytics.content;
+
+import com.dotcms.analytics.query.AnalyticsQuery;
+import com.dotmarketing.util.Logger;
+import com.liferay.portal.model.User;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+/**
+ * Implementation class for the {@link ContentAnalyticsAPIImpl} interface.
+ *
+ * @author Jose Castro
+ * @since Sep 13th, 2024
+ */
+@ApplicationScoped
+public class ContentAnalyticsAPIImpl implements ContentAnalyticsAPI {
+
+    private final ContentAnalyticsFactory contentAnalyticsFactory;
+
+    @Inject
+    public ContentAnalyticsAPIImpl(final ContentAnalyticsFactory contentAnalyticsFactory) {
+        this.contentAnalyticsFactory = contentAnalyticsFactory;
+    }
+
+    @Override
+    public ReportResponse runReport(final AnalyticsQuery query, final User user) {
+
+        Logger.debug(this, ()-> "Running the report for the query: " + query);
+        // note: should check any permissions for an user.
+        return this.contentAnalyticsFactory.getReport(query, user);
+    }
+
+}
