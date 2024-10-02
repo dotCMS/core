b6fda59af9bdafc9cd04969c81b66e2705388ed9
jdotcms <jonathan.sanchez@dotcms.com>
2024-10-01 21:26:57 -0600

    #30192 adding the date range parsing

--- dotCMS/src/test/java/com/dotcms/analytics/query/TimeDimensionParserTest.java
+++ dotCMS/src/test/java/com/dotcms/analytics/query/TimeDimensionParserTest.java
@@ -26,9 +26,9 @@ public class TimeDimensionParserTest {
     }
 
     /**
-     * Parse a simple query with only dimension and granularity
-     * Events.day day
-     * should return Events.day and day
+     * Parse a simple query with  dimension, granularity and dateRange
+     * Events.day day This Week
+     * should return Events.day and day and This Week
      */
     @Test
     public void test_parseTimeDimension_dimension_plus_granularity_and_dateRange_should_be_OK() throws Exception {
@@ -40,4 +40,37 @@ public class TimeDimensionParserTest {
         Assert.assertEquals("day", result.getGranularity());
         Assert.assertEquals("This Week", result.getDateRange());
     }
+
+    /**
+     * Parse a simple query with  dimension, granularity and dateRange
+     * Events.day day yesterday
+     * should return Events.day and day and yesterday
+     */
+    @Test
+    public void test_parseTimeDimension_dimension_plus_granularity_and_dateRange_yesterday_should_be_OK() throws Exception {
+        final TimeDimensionParser.TimeDimension result =
+                TimeDimensionParser.parseTimeDimension("Events.day day yesterday");
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals("Events.day", result.getDimension());
+        Assert.assertEquals("day", result.getGranularity());
+        Assert.assertEquals("yesterday", result.getDateRange());
+    }
+    /**
+     * Parse a simple query with  dimension, granularity and dateRange
+     * Events.day day Last 7 days
+     * should return Events.day and day and Last 7 days
+     */
+    @Test
+    public void test_parseTimeDimension_dimension_plus_granularity_and_dateRange_last_7_days_should_be_OK() throws Exception {
+        final TimeDimensionParser.TimeDimension result =
+                TimeDimensionParser.parseTimeDimension("Events.day day Last 7 days");
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals("Events.day", result.getDimension());
+        Assert.assertEquals("day", result.getGranularity());
+        Assert.assertEquals("Last 7 days", result.getDateRange());
+    }
+
+
 }
