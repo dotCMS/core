3c5afd7e3c174e875641df76fee055fce33d6323
Fabrizzio Araya <37148755+fabrizzio-dotCMS@users.noreply.github.com>
2024-10-10 13:55:22 -0600

    feat(JobQueue Resource) Refs: 29480   (#30303)
    ### Proposed Changes
    This PR includes the integration of the JobQueue implementation with
    rest services.
    Functionalities such as List, Cancel, Status, Create, and Monitor can be
    found here
    It also includes two sample JobProcessors to serve as examples and used
    be the respective postman tests

--- dotcms-integration/src/test/java/com/dotcms/Junit5Suite1.java
+++ dotcms-integration/src/test/java/com/dotcms/Junit5Suite1.java
@@ -3,6 +3,7 @@ package com.dotcms;
 import com.dotcms.jobs.business.api.JobQueueManagerAPICDITest;
 import com.dotcms.jobs.business.api.JobQueueManagerAPIIntegrationTest;
 import com.dotcms.jobs.business.queue.PostgresJobQueueIntegrationTest;
+import com.dotcms.rest.api.v1.job.JobQueueHelperIntegrationTest;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
@@ -10,7 +11,8 @@ import org.junit.platform.suite.api.Suite;
 @SelectClasses({
         JobQueueManagerAPICDITest.class,
         PostgresJobQueueIntegrationTest.class,
-        JobQueueManagerAPIIntegrationTest.class
+        JobQueueManagerAPIIntegrationTest.class,
+        JobQueueHelperIntegrationTest.class
 })
 public class Junit5Suite1 {
 
--- dotcms-integration/src/test/java/com/dotcms/jobs/business/api/JobQueueManagerAPICDITest.java
+++ dotcms-integration/src/test/java/com/dotcms/jobs/business/api/JobQueueManagerAPICDITest.java
@@ -5,39 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
-import com.dotcms.jobs.business.api.events.EventProducer;
-import com.dotcms.jobs.business.api.events.RealTimeJobMonitor;
-import com.dotcms.jobs.business.error.CircuitBreaker;
+import com.dotcms.TestBaseJunit5WeldInitiator;
 import com.dotcms.jobs.business.error.ExponentialBackoffRetryStrategy;
-import com.dotcms.jobs.business.error.RetryStrategy;
-import com.dotcms.jobs.business.error.RetryStrategyProducer;
 import com.dotcms.jobs.business.queue.JobQueue;
-import com.dotcms.jobs.business.queue.JobQueueProducer;
 import javax.inject.Inject;
-import org.jboss.weld.bootstrap.api.helpers.RegistrySingletonProvider;
-import org.jboss.weld.junit5.WeldInitiator;
-import org.jboss.weld.junit5.WeldJunit5Extension;
-import org.jboss.weld.junit5.WeldSetup;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 /**
  * Test class for verifying the CDI (Contexts and Dependency Injection) functionality of the
  * JobQueueManagerAPI implementation.
  */
-@ExtendWith(WeldJunit5Extension.class)
-public class JobQueueManagerAPICDITest {
 
-    @WeldSetup
-    public WeldInitiator weld = WeldInitiator.of(
-            WeldInitiator.createWeld()
-                    .containerId(RegistrySingletonProvider.STATIC_INSTANCE)
-                    .beanClasses(JobQueueManagerAPIImpl.class, JobQueueConfig.class,
-                            JobQueue.class, RetryStrategy.class, CircuitBreaker.class,
-                            JobQueueProducer.class, JobQueueConfigProducer.class,
-                            RetryStrategyProducer.class, RealTimeJobMonitor.class,
-                            EventProducer.class)
-    );
+public class JobQueueManagerAPICDITest extends TestBaseJunit5WeldInitiator {
 
     @Inject
     private JobQueueManagerAPI jobQueueManagerAPI;
