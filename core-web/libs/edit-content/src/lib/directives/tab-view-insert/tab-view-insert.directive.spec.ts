6a9cbc4475835574aaa117c3709d9d37d3edbfde
Arcadio Quintero <oidacra@gmail.com>
2024-10-10 13:21:15 -0400

    feat(edit-content) remove toolbar, added prepend and append areas to tabView

---
+++ core-web/libs/edit-content/src/lib/directives/tab-view-insert/tab-view-insert.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TabViewInsertDirective } from './tab-view-insert.directive';
+
+describe('TabViewInsertDirective', () => {
+    it('should create an instance', () => {
+        const directive = new TabViewInsertDirective();
+        expect(directive).toBeTruthy();
+    });
+});
