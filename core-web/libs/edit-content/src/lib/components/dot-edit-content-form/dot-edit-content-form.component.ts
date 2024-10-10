cb174161e6a43d63fd5f040dc01f83b9bc9c0034
Arcadio Quintero <oidacra@gmail.com>
2024-10-10 13:21:15 -0400

    feat(edit-content) remove toolbar, added prepend and append areas to tabView

--- core-web/libs/edit-content/src/lib/components/dot-edit-content-form/dot-edit-content-form.component.ts
+++ core-web/libs/edit-content/src/lib/components/dot-edit-content-form/dot-edit-content-form.component.ts
@@ -1,13 +1,16 @@
-import { CommonModule } from '@angular/common';
+import { Location } from '@angular/common';
 import {
     ChangeDetectionStrategy,
     Component,
-    EventEmitter,
+    computed,
+    DestroyRef,
     inject,
-    Input,
+    input,
     OnInit,
-    Output
+    output,
+    signal
 } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import {
     FormBuilder,
     FormControl,
@@ -21,10 +24,12 @@ import { TabViewModule } from 'primeng/tabview';
 
 import { DotMessageService } from '@dotcms/data-access';
 import { DotCMSContentTypeField, DotCMSContentTypeLayoutTab } from '@dotcms/dotcms-models';
-import { DotMessagePipe } from '@dotcms/ui';
+import { DotMessagePipe, DotWorkflowActionsComponent } from '@dotcms/ui';
 
 import { resolutionValue } from './utils';
 
+import { TabViewInsertDirective } from '../../directives/tab-view-insert/tab-view-insert.directive';
+import { DotEditContentStore } from '../../feature/edit-content/store/edit-content.store';
 import {
     CALENDAR_FIELD_TYPES,
     FLATTENED_FIELD_TYPES
@@ -32,6 +37,7 @@ import {
 import { FIELD_TYPES } from '../../models/dot-edit-content-field.enum';
 import { FILTERED_TYPES } from '../../models/dot-edit-content-form.enum';
 import { EditContentForm } from '../../models/dot-edit-content-form.interface';
+import { DotWorkflowActionParams } from '../../models/dot-edit-content.model';
 import { getFinalCastedValue, transformLayoutToTabs } from '../../utils/functions.util';
 import { DotEditContentAsideComponent } from '../dot-edit-content-aside/dot-edit-content-aside.component';
 import { DotEditContentFieldComponent } from '../dot-edit-content-field/dot-edit-content-field.component';
@@ -41,39 +47,84 @@ import { DotEditContentFieldComponent } from '../dot-edit-content-field/dot-edit
     standalone: true,
     templateUrl: './dot-edit-content-form.component.html',
     styleUrls: ['./dot-edit-content-form.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [
-        CommonModule,
         ReactiveFormsModule,
         DotEditContentFieldComponent,
         ButtonModule,
         DotMessagePipe,
         DotEditContentAsideComponent,
-        TabViewModule
-    ]
+        TabViewModule,
+        DotWorkflowActionsComponent,
+        TabViewInsertDirective
+    ],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DotEditContentFormComponent implements OnInit {
-    @Input()
-    formData!: EditContentForm;
+    readonly #location = inject(Location);
+    readonly $store = inject(DotEditContentStore);
+    readonly #destroyRef = inject(DestroyRef);
 
-    @Output() changeValue = new EventEmitter();
+    $tabs = signal<DotCMSContentTypeLayoutTab[]>([]);
+
+    /**
+     * This property is required and expects an object of type EditContentForm.
+     * It is used to populate the form with initial data and structure.
+     *
+     * @type {EditContentForm}
+     * @memberof DotEditContentFormComponent
+     */
+    $formData = input.required<EditContentForm>({ alias: 'formData' });
+
+    /**
+     * Output event emitter that informs when the form has changed.
+     * Emits an object of type Record<string, string> containing the updated form values.
+     *
+     * @memberof DotEditContentFormComponent
+     */
+    changeValue = output<Record<string, string>>();
 
     form!: FormGroup;
+
     readonly fieldTypes = FIELD_TYPES;
     readonly filteredTypes = FILTERED_TYPES;
-    protected tabs: DotCMSContentTypeLayoutTab[] = [];
-    private readonly fb = inject(FormBuilder);
-    private readonly dotMessageService = inject(DotMessageService);
 
-    get areMultipleTabs(): boolean {
-        return this.tabs.length > 1;
-    }
+    readonly #fb = inject(FormBuilder);
+    readonly #dotMessageService = inject(DotMessageService);
+
+    /**
+     * Computed property that determines if the content type has multiple tabs.
+     *
+     * @returns {boolean} True if there are multiple tabs, false otherwise.
+     * @memberof DotEditContentFormComponent
+     */
+    $areMultipleTabs = computed(() => this.$tabs().length > 1);
 
     ngOnInit() {
-        if (this.formData) {
+        if (this.$formData()) {
             this.initilizeForm();
             this.setLayoutTabs();
         }
+    }
+
+    private createFormControl(field: DotCMSContentTypeField) {
+        const validators = this.getFieldValidators(field);
+        const initialValue = this.getInitialValue(field);
+
+        return this.#fb.control({ value: initialValue, disabled: field.readOnly }, validators);
+    }
+
+    private getFieldValidators(field: DotCMSContentTypeField) {
+        const validators = [];
+        if (field.required) validators.push(Validators.required);
+        if (field.regexCheck) validators.push(Validators.pattern(field.regexCheck));
+
+        return validators;
+    }
+
+    private getInitialValue(field: DotCMSContentTypeField) {
+        const contentlet = this.$store.contentlet();
+
+        return contentlet[field.variable] || null;
     }
 
     /**
@@ -82,13 +133,13 @@ export class DotEditContentFormComponent implements OnInit {
      * @memberof DotEditContentFormComponent
      */
     initilizeForm() {
-        this.form = this.fb.group({});
+        this.form = this.#fb.group({});
 
-        this.form.valueChanges.subscribe((value) => {
+        this.form.valueChanges.pipe(takeUntilDestroyed(this.#destroyRef)).subscribe((value) => {
             this.onFormChange(value);
         });
 
-        this.formData.contentType.fields.forEach((field) => {
+        this.$formData().contentType.fields.forEach((field) => {
             if (this.isFilteredType(field)) {
                 return;
             }
@@ -117,9 +168,7 @@ export class DotEditContentFormComponent implements OnInit {
      * @memberof DotEditContentFormComponent
      */
     onFormChange(value) {
-        this.formData.contentType.fields.forEach(({ variable, fieldType }) => {
-            // Shorthand for conditional assignment
-
+        this.$formData().contentType.fields.forEach(({ variable, fieldType }) => {
             if (FLATTENED_FIELD_TYPES.includes(fieldType as FIELD_TYPES)) {
                 value[variable] = value[variable]?.join(',');
             }
@@ -130,6 +179,7 @@ export class DotEditContentFormComponent implements OnInit {
                     .replace(/T|\.\d{3}Z/g, (match: string) => (match === 'T' ? ' ' : '')); // To remove the T and .000Z from the date)
             }
         });
+
         this.changeValue.emit(value);
     }
 
@@ -145,7 +195,7 @@ export class DotEditContentFormComponent implements OnInit {
         const validators = [];
 
         const resolutionFn = resolutionValue[field.fieldType];
-        const value = resolutionFn ? resolutionFn(this.formData.contentlet, field) : '';
+        const value = resolutionFn ? resolutionFn(this.$formData().contentlet, field) : '';
 
         if (field.required) validators.push(Validators.required);
         if (field.regexCheck) {
@@ -157,7 +207,7 @@ export class DotEditContentFormComponent implements OnInit {
             }
         }
 
-        return this.fb.control(
+        return this.#fb.control(
             {
                 value: getFinalCastedValue(value, field) ?? null,
                 disabled: field.readOnly
@@ -173,9 +223,42 @@ export class DotEditContentFormComponent implements OnInit {
      * @memberof DotEditContentFormComponent
      */
     private setLayoutTabs() {
-        this.tabs = transformLayoutToTabs(
-            this.dotMessageService.get('Content'),
-            this.formData.contentType.layout
+        this.$tabs.set(
+            transformLayoutToTabs(
+                this.#dotMessageService.get('Content'),
+                this.$formData().contentType.layout
+            )
         );
+    }
+
+    /**
+     * Fire the workflow action.
+     *
+     * @param {DotCMSWorkflowAction} action
+     * @memberof EditContentLayoutComponent
+     */
+    fireWorkflowAction({ actionId, inode, contentType }: DotWorkflowActionParams): void {
+        this.$store.fireWorkflowAction({
+            actionId,
+            inode,
+            data: {
+                contentlet: {
+                    ...this.form.value,
+                    contentType
+                }
+            }
+        });
+    }
+
+    /**
+     * Navigates back to the previous page.
+     *
+     * This method uses the Angular Location service to navigate
+     * to the previous page in the browser's history.
+     *
+     * @memberof DotEditContentFormComponent
+     */
+    goBack(): void {
+        this.#location.back();
     }
 }
