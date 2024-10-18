ba757fbc0053b1788aec267f3b88de4f0c3c84d6
Mehdi <10160868+mbiuki@users.noreply.github.com>
2024-10-18 11:50:51 -0400

    Create semgrep.test.js
    This is a test to experiment comment mode for Semgrep onboarding.

---
+++ semgrep.test.js
@@ -0,0 +1,27 @@
+const el = element.innerHTML;
+
+function bad1(userInput) {
+// ruleid: insecure-document-method
+  el.innerHTML = '<div>' + userInput + '</div>';
+}
+
+function bad2(userInput) {
+// ruleid: insecure-document-method
+  document.body.outerHTML = userInput;
+}
+
+function bad3(userInput) {
+  const name = '<div>' + userInput + '</div>';
+// ruleid: insecure-document-method
+  document.write(name);
+}
+
+function ok1() {
+  const name = "<div>it's ok</div>";
+// ok: insecure-document-method
+  el.innerHTML = name;
+}
+
+function ok2() {
+// ok: insecure-document-method
+  documen
