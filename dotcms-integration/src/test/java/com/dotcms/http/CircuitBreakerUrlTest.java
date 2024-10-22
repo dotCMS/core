f2c9bf8f5483d55053a5b7c9e107c3075241bdaa
erickgonzalez <erick.gonzalez@dotcms.com>
2024-10-22 12:48:25 -0600

    fix(test) ref: #30420

--- dotcms-integration/src/test/java/com/dotcms/http/CircuitBreakerUrlTest.java
+++ dotcms-integration/src/test/java/com/dotcms/http/CircuitBreakerUrlTest.java
@@ -376,7 +376,7 @@ public class CircuitBreakerUrlTest {
         for (int i = 0; i < 10; i++) {
             try {
                 String x = new CircuitBreakerUrl(goodUrl, 2000).doString();
-                assert (x.contains("<meta name=\"author\" content=\"dotCMS\">"));
+                assert (x.contains("content=\"dotCMS\""));
 
             } catch (Exception e) {
                 assert (e instanceof CircuitBreakerOpenException);
