71b5d402618796aabb3c34f281edaf89eef5adf6
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2024-10-25 15:36:33 -0600

    #30187

--- dotCMS/src/main/java/com/dotcms/jobs/business/queue/PostgresJobQueue.java
+++ dotCMS/src/main/java/com/dotcms/jobs/business/queue/PostgresJobQueue.java
@@ -28,7 +28,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Vetoed;
 
 /**
