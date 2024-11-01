3b0884e7890fe0ccd3e09698d20de4f450c493f8
Rafael Velazco <rjvelazco21@gmail.com>
2024-11-01 10:48:07 -0400

    SDK: Unable to Edit Unpublished Page in Angular Example (#30542)
    This pull request includes several updates to the `DotCMSPagesComponent`
    in the Angular example application. The changes primarily focus on
    integrating editor-related functionality and improving code formatting.
    
    ### Editor Integration:
    
    * Added `DOTCMS_CLIENT_TOKEN` import and injected it into the
    `DotCMSPagesComponent` to enable editor-related functionalities.
    [[1]](diffhunk://#diff-db19dde961c6af3dbddd86db7544e4ea4ff42baae9095cee4e3017c1d4dcb395L20-R26)
    [[2]](diffhunk://#diff-db19dde961c6af3dbddd86db7544e4ea4ff42baae9095cee4e3017c1d4dcb395R66-R85)
    * Implemented a new method `#listenToEditorChanges` to handle editor
    changes and update the component state accordingly.
    
    ### Code Formatting:
    
    * Reformatted the import statements for better readability and
    maintainability.
    * Adjusted the method signature formatting for `#setPageContent` to be
    consistent with the rest of the codebase.
    
    ### Video
    
    
    https://github.com/user-attachments/assets/7ee487ce-412f-4504-8ff1-f92dac86beee
    
    ---------
    
    Co-authored-by: Jalinson Diaz <zjaaaldev@gmail.com>

--- examples/angular/src/app/pages/pages.component.ts
+++ examples/angular/src/app/pages/pages.component.ts
@@ -17,8 +17,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PageService } from './services/page.service';
-import { CUSTOMER_ACTIONS, postMessageToEditor } from '@dotcms/client';
+import {
+  CLIENT_ACTIONS,
+  isInsideEditor,
+  postMessageToEditor,
+} from '@dotcms/client';
 import { DYNAMIC_COMPONENTS } from './components';
+import { DOTCMS_CLIENT_TOKEN } from '../app.config';
 
 export type PageError = {
   message: string;
@@ -51,6 +56,8 @@ export class DotCMSPagesComponent implements OnInit {
   readonly #destroyRef = inject(DestroyRef);
   readonly #router = inject(Router);
   readonly #pageService = inject(PageService);
+  readonly #client = inject(DOTCMS_CLIENT_TOKEN);
+
   protected readonly $context = signal<PageRender>({
     page: null,
     nav: null,
@@ -63,6 +70,10 @@ export class DotCMSPagesComponent implements OnInit {
   protected readonly editorCofig: any = { params: { depth: 2 } };
 
   ngOnInit() {
+    if (isInsideEditor()) {
+      this.#listenToEditorChanges();
+    }
+
     this.#router.events
       .pipe(
         filter(
@@ -90,10 +101,7 @@ export class DotCMSPagesComponent implements OnInit {
       });
   }
 
-  #setPageContent(
-    page: DotCMSPageAsset,
-    nav: DotcmsNavigationItem | null
-  ) {
+  #setPageContent(page: DotCMSPageAsset, nav: DotcmsNavigationItem | null) {
     this.$context.set({
       status: 'success',
       page,
@@ -122,6 +130,20 @@ export class DotCMSPagesComponent implements OnInit {
      * This is a temporary workaround to avoid the editor to be stuck in the loading state.
      * This will be removed once the editor is able to detect when the client is ready without use DotcmsLayoutComponent.
      */
-    postMessageToEditor({ action: CUSTOMER_ACTIONS.CLIENT_READY, payload: {} });
+    postMessageToEditor({ action: CLIENT_ACTIONS.CLIENT_READY, payload: {} });
+  }
+
+  #listenToEditorChanges() {
+    this.#client.editor.on('changes', (page) => {
+      if (!page) {
+        return;
+      }
+      this.$context.update((state) => ({
+        ...state,
+        page: page as DotCMSPageAsset,
+        status: 'success',
+        error: null,
+      }));
+    });
   }
 }
