92696491e0ddc979aec919a0e71b05fa169b044c
Nicolas Molina Monroy <hi@nicobytes.com>
2024-11-08 10:31:57 -0400

    chore(edit-content): implement endpoint to fetch sites  (#30562)
    ### Parent Issue
    
    #30215
    
    ### Proposed Changes
    
    This pull request includes multiple changes to the `dot-site` service
    and the `dot-select-existing-file` component, primarily focusing on
    adding pagination support and improving the file selection UI and its
    state management.
    
    ### DotSiteService Enhancements:
    * Added a `DEFAULT_PAGE` constant and included a `page` parameter in the
    `getSites` method to support pagination.
    (`core-web/libs/data-access/src/lib/dot-site/dot-site.service.ts`)
    [[1]](diffhunk://#diff-c4872eacb42281fa7aa6fd3d5c084e08fd41e79eb2778581421e7344f9aa4e12R20-R21)
    [[2]](diffhunk://#diff-c4872eacb42281fa7aa6fd3d5c084e08fd41e79eb2778581421e7344f9aa4e12L44-R56)
    * Updated test cases to reflect the new `page` parameter in the URLs.
    (`core-web/libs/data-access/src/lib/dot-site/dot-site.service.spec.ts`)
    [[1]](diffhunk://#diff-ad4a37aa4e92da7343f4da8185ea2031a67319500d820946e0d24fa515e33241L30-R30)
    [[2]](diffhunk://#diff-ad4a37aa4e92da7343f4da8185ea2031a67319500d820946e0d24fa515e33241L44-R44)
    
    ### DotSelectExistingFile Component Enhancements:
    * Improved the `dot-sidebar` component to handle loading states and
    display skeleton loaders while data is being fetched.
    (`core-web/libs/edit-content/src/lib/fields/dot-edit-content-file-field/components/dot-select-existing-file/components/dot-sidebar/dot-sidebar.component.html`,
    `core-web/libs/edit-content/src/lib/fields/dot-edit-content-file-field/components/dot-select-existing-file/components/dot-sidebar/dot-sidebar.component.ts`)
    [[1]](diffhunk://#diff-c3ff3590364bc12ded7173d7374838dfab13f67598f80daa198dfd96c935d459L1-R22)
    [[2]](diffhunk://#diff-b6e7f5a1ad7c221178b3ddf3f3dfc06021ba87b4e18ef9d7f6c6c9a6eab016d3L1-R82)
    * Added event handling for node expansion in the `dot-sidebar` component
    to dynamically load child nodes.
    (`core-web/libs/edit-content/src/lib/fields/dot-edit-content-file-field/components/dot-select-existing-file/dot-select-existing-file.component.html`)
    * Introduced a new test suite for the `SelectExisingFileStore` to cover
    folder loading and child node expansion logic.
    (`core-web/libs/edit-content/src/lib/fields/dot-edit-content-file-field/components/dot-select-existing-file/store/select-existing-file.store.test.ts`)
    * Updated the `SelectExisingFileStore` to manage the state of folder
    nodes, including loading states and expanded nodes.
    (`core-web/libs/edit-content/src/lib/fields/dot-edit-content-file-field/components/dot-select-existing-file/store/select-existing-file.store.ts`)
    [[1]](diffhunk://#diff-4e8a1b332dae5fb8ead291b76bf6099437aaf4aa6fd0a759ffe71c3074ffe7f0R2-R22)
    [[2]](diffhunk://#diff-4e8a1b332dae5fb8ead291b76bf6099437aaf4aa6fd0a759ffe71c3074ffe7f0L20-R35)
    [[3]](diffhunk://#diff-4e8a1b332dae5fb8ead291b76bf6099437aaf4aa6fd0a759ffe71c3074ffe7f0L36-R51)
    [[4]](diffhunk://#diff-4e8a1b332dae5fb8ead291b76bf6099437aaf4aa6fd0a759ffe71c3074ffe7f0R70-R71)
    [[5]](diffhunk://#diff-4e8a1b332dae5fb8ead291b76bf6099437aaf4aa6fd0a759ffe71c3074ffe7f0L75-R153)
    
    ### Miscellaneous:
    * Minor CSS adjustments to the `dot-select-existing-file` component to
    improve layout and styling.
    (`core-web/libs/edit-content/src/lib/fields/dot-edit-content-file-field/components/dot-select-existing-file/dot-select-existing-file.component.scss`)
    * Refactored the `file-field.store.spec.ts` to use `TestBed` for
    dependency injection and added missing imports.
    (`core-web/libs/edit-content/src/lib/fields/dot-edit-content-file-field/store/file-field.store.spec.ts`)
    [[1]](diffhunk://#diff-57a0231d8f4135b48e8dd7e31f8e216157f1fd49a145d9cc0c7d7bf912a00de4L2-R4)
    [[2]](diffhunk://#diff-57a0231d8f4135b48e8dd7e31f8e216157f1fd49a145d9cc0c7d7bf912a00de4L19-R22)
    
    ### Checklist
    - [x] Tests
    - [x] Translations
    - [x] Security Implications Contemplated (add notes if applicable)
    
    ### Screenshots
    
    
    
    https://github.com/user-attachments/assets/56370a81-b204-456e-85c2-7d7dc9f26a57

--- core-web/libs/edit-content/src/lib/fields/dot-edit-content-file-field/components/dot-select-existing-file/components/dot-sidebar/dot-sidebar.component.ts
+++ core-web/libs/edit-content/src/lib/fields/dot-edit-content-file-field/components/dot-select-existing-file/components/dot-sidebar/dot-sidebar.component.ts
@@ -1,29 +1,83 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { faker } from '@faker-js/faker';
+
+import { SlicePipe } from '@angular/common';
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    inject,
+    input,
+    output,
+    signal
+} from '@angular/core';
 
 import { TreeNode } from 'primeng/api';
-import { TreeModule } from 'primeng/tree';
+import { SkeletonModule } from 'primeng/skeleton';
+import { TreeModule, TreeNodeExpandEvent } from 'primeng/tree';
+
+import { TruncatePathPipe } from '@dotcms/edit-content/pipes/truncate-path.pipe';
 
 @Component({
     selector: 'dot-sidebar',
     standalone: true,
-    imports: [TreeModule],
+    imports: [TreeModule, SlicePipe, TruncatePathPipe, SkeletonModule],
     templateUrl: './dot-sidebar.component.html',
     styleUrls: ['./dot-sidebar.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DotSideBarComponent {
     /**
-     * An observable that emits an array of TreeNode objects representing folders.
+     * A readonly private field that holds an instance of ChangeDetectorRef.
+     * This is used to detect and respond to changes in the component's data-bound properties.
+     */
+    readonly #cd = inject(ChangeDetectorRef);
+    /**
+     * An observable that emits an array of TreeNode objects representing the folders.
      *
      * @type {Observable<TreeNode[]>}
      * @alias folders
      */
     $folders = input.required<TreeNode[]>({ alias: 'folders' });
     /**
-     * Represents a loading state for the component.
+     * A boolean observable that indicates the loading state.
      *
      * @type {boolean}
-     * @alias loading
      */
     $loading = input.required<boolean>({ alias: 'loading' });
+
+    /**
+     * Signal that generates an array of strings representing percentages.
+     * Each percentage is a random value between 75% and 100%.
+     * The array contains 50 elements.
+     *
+     * @returns {string[]} An array of 50 percentage strings.
+     */
+    $fakeColumns = signal<string[]>(Array.from({ length: 50 }).map((_) => this.getPercentage()));
+
+    /**
+     * Event emitter for when a tree node is expanded.
+     *
+     * This event is triggered when a user expands a node in the tree structure.
+     * It emits an event of type `TreeNodeExpandEvent`.
+     */
+    onNodeExpand = output<TreeNodeExpandEvent>();
+
+    /**
+     * Triggers change detection manually.
+     * This method is used to ensure that the view is updated when the model changes.
+     * It calls the `detectChanges` method on the ChangeDetectorRef instance.
+     */
+    detectChanges() {
+        this.#cd.detectChanges();
+    }
+    /**
+     * Generates a random percentage string between 75% and 100%.
+     *
+     * @returns {string} A string representing a percentage between 75% and 100%.
+     */
+    getPercentage(): string {
+        const number = faker.number.int({ max: 100, min: 75 });
+
+        return `${number}%`;
+    }
 }
