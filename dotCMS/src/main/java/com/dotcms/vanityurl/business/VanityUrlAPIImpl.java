4c5d2c5d97d5c83d36a02922f590ffa1216f17ed
daniel.solis <2894221+dsolistorres@users.noreply.github.com>
2024-11-12 10:05:49 -0600

    fix(hosts) : retrieve live version if a host has live and working versions (#30358)
    #Closes #28362
    
    ### Proposed Changes
    * For the sites portlet, if a site has live and working versions, the
    live version is shown.
    * When resolving a site by name, id or alias, the live version of the
    site is retrieved for front-end users.
    
    ### Checklist
    - [x] Tests
    
    ---------
    
    Co-authored-by: Kevin Davila <144152756+KevinDavilaDotCMS@users.noreply.github.com>
    Co-authored-by: Jalinson Diaz <zjaaaldev@gmail.com>
    Co-authored-by: Jonathan <jonathan.sanchez@dotcms.com>
    Co-authored-by: Will Ezell <will@dotcms.com>
    Co-authored-by: erickgonzalez <erick.gonzalez@dotcms.com>
    Co-authored-by: Humberto Morera <31667212+hmoreras@users.noreply.github.com>
    Co-authored-by: Jonathan Gamba <jonathan.gamba@dotcms.com>
    Co-authored-by: Nicolas Molina Monroy <hi@nicobytes.com>
    Co-authored-by: Rafael Velazco <rjvelazco21@gmail.com>
    Co-authored-by: freddyDOTCMS <147462678+freddyDOTCMS@users.noreply.github.com>
    Co-authored-by: Fabrizzio Araya <37148755+fabrizzio-dotCMS@users.noreply.github.com>
    Co-authored-by: Daniel Enrique Colina Rodríguez <daniel.colina@dotcms.com>
    Co-authored-by: Jose Castro <jose.castro@dotcms.com>

--- dotCMS/src/main/java/com/dotcms/vanityurl/business/VanityUrlAPIImpl.java
+++ dotCMS/src/main/java/com/dotcms/vanityurl/business/VanityUrlAPIImpl.java
@@ -24,6 +24,7 @@ import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotRuntimeException;
 import com.dotmarketing.portlets.contentlet.business.ContentletAPI;
 import com.dotmarketing.portlets.contentlet.business.DotContentletValidationException;
+import com.dotmarketing.portlets.contentlet.business.HostAPI;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.portlets.languagesmanager.business.LanguageAPI;
 import com.dotmarketing.portlets.languagesmanager.model.Language;
@@ -115,7 +116,8 @@ public class VanityUrlAPIImpl implements VanityUrlAPI {
 
   @Override
   public void populateAllVanityURLsCache() throws DotDataException {
-    for (final Host site : Try.of(() -> APILocator.getHostAPI().findAllFromDB(APILocator.systemUser(), false)).getOrElse(List.of())) {
+    for (final Host site : Try.of(() -> APILocator.getHostAPI().findAllFromDB(APILocator.systemUser(),
+            HostAPI.SearchType.INCLUDE_SYSTEM_HOST)).getOrElse(List.of())) {
       populateVanityURLsCacheBySite(site);
     }
     populateVanityURLsCacheBySite(APILocator.getHostAPI().findSystemHost());
