17eb8d08349ecc27d63718bcfe06be0443554b99
Kevin Davila <kfariid@gmail.com>
2024-11-21 11:33:20 -0500

    WIP

--- core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-toolbar/dot-uve-toolbar.component.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-toolbar/dot-uve-toolbar.component.ts
@@ -1,9 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+
+import { DotMessagePipe } from '@dotcms/ui';
+
+
 @Component({
     selector: 'dot-uve-toolbar',
     standalone: true,
-    imports: [],
+    imports: [ButtonModule, DotMessagePipe],
     templateUrl: './dot-uve-toolbar.component.html',
     styleUrl: './dot-uve-toolbar.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
