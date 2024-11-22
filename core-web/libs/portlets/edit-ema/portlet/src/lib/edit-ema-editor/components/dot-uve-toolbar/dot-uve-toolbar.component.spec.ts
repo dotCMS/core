d3e2a46487685c092a1aa89511395a3ed1167250
Kevin Davila <144152756+KevinDavilaDotCMS@users.noreply.github.com>
2024-11-22 07:55:59 -0500

    chore(uve): FTM -  Implement look feel new UVE toolbar (#30745)
    https://github.com/user-attachments/assets/cdf40d63-bf66-470b-9638-04ded6930173
    
    
    
    
    
    This pull request includes enhancements to the `dot-uve-toolbar`
    component in the `edit-ema` portlet. The changes focus on improving the
    toolbar's functionality and updating the tests to ensure comprehensive
    coverage.
    
    Enhancements to the toolbar component:
    
    *
    [`dot-uve-toolbar.component.html`](diffhunk://#diff-9937556e73b051b878ba22ad1ce971a70019a617d7979b3e0bcc814801ad350bL1-R24):
    Replaced the placeholder text with a toolbar containing buttons for
    preview, bookmark, copy URL, API link, and spans for experiments,
    language selector, persona selector, and workflows.
    
    Styling updates:
    
    *
    [`dot-uve-toolbar.component.scss`](diffhunk://#diff-dd2bc3ab605f3a154f1a4b3b5de1f8e364e7bbc5a3fb4b078bba74aaedcad312R1-R7):
    Added styles to align items and set gaps for the toolbar groups using
    variables.
    
    Testing improvements:
    
    *
    [`dot-uve-toolbar.component.spec.ts`](diffhunk://#diff-3eaa147616a5d1ff374a5fa27b0f38f0159a9039ef7e8d672dec43631f48a9e1L1-R45):
    Refactored tests to use `@ngneat/spectator` for better readability and
    added tests for each toolbar button and span.
    
    Module imports:
    
    *
    [`dot-uve-toolbar.component.ts`](diffhunk://#diff-217a9e619d6590c4f652e85353b9637ba5e464ddeb0424be35aef39bb8dceb30R3-R11):
    Imported `ButtonModule`, `ToolbarModule`, and `DotMessagePipe` to
    support the new toolbar functionality.
    
    ---------
    
    Co-authored-by: Kevin Davila <kfariid@gmail.com>

--- core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-toolbar/dot-uve-toolbar.component.spec.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-toolbar/dot-uve-toolbar.component.spec.ts
@@ -1,22 +1,47 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { byTestId, Spectator } from '@ngneat/spectator';
+import { createComponentFactory } from '@ngneat/spectator/jest';
 
 import { DotUveToolbarComponent } from './dot-uve-toolbar.component';
 
 describe('DotUveToolbarComponent', () => {
-    let component: DotUveToolbarComponent;
-    let fixture: ComponentFixture<DotUveToolbarComponent>;
-
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [DotUveToolbarComponent]
-        }).compileComponents();
-
-        fixture = TestBed.createComponent(DotUveToolbarComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+    let spectator: Spectator<DotUveToolbarComponent>;
+    const createComponent = createComponentFactory({
+        component: DotUveToolbarComponent
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    beforeEach(() => {
+        spectator = createComponent();
+    });
+
+    it('should have preview button', () => {
+        expect(spectator.query(byTestId('uve-toolbar-preview'))).toBeTruthy();
+    });
+
+    it('should have bookmark button', () => {
+        expect(spectator.query(byTestId('uve-toolbar-bookmark'))).toBeTruthy();
+    });
+
+    it('should have copy url button', () => {
+        expect(spectator.query(byTestId('uve-toolbar-copy-url'))).toBeTruthy();
+    });
+
+    it('should have api link button', () => {
+        expect(spectator.query(byTestId('uve-toolbar-api-link'))).toBeTruthy();
+    });
+
+    it('should have experiments button', () => {
+        expect(spectator.query(byTestId('uve-toolbar-running-experiment'))).toBeTruthy();
+    });
+
+    it('should have language selector', () => {
+        expect(spectator.query(byTestId('uve-toolbar-language-selector'))).toBeTruthy();
+    });
+
+    it('should have persona selector', () => {
+        expect(spectator.query(byTestId('uve-toolbar-persona-selector'))).toBeTruthy();
+    });
+
+    it('should have workflows button', () => {
+        expect(spectator.query(byTestId('uve-toolbar-workflow-actions'))).toBeTruthy();
     });
 });
