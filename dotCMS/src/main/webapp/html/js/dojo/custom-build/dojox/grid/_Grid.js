d62f6ef8918716e427b103a40d71ad2d2f1cca30
Geronimo Ortiz <geronimo.ortiz@dotcms.com>
2024-11-26 13:23:42 -0300

    Issue 27749 console error category field (#30746)
    Added a check to validate if _messagesNode_ is undefined before the
    function tries to do innerHTML

--- dotCMS/src/main/webapp/html/js/dojo/custom-build/dojox/grid/_Grid.js
+++ dotCMS/src/main/webapp/html/js/dojo/custom-build/dojox/grid/_Grid.js
@@ -651,14 +651,13 @@ define("dojox/grid/_Grid", [
 		},
 		
 		showMessage: function(message){
-			if (typeof messagesNode !== undefined) {
-				if (message) {
-					this.messagesNode.innerHTML = message;
-					this.messagesNode.style.display = "";
-				} else {
-					this.messagesNode.innerHTML = "";
-					this.messagesNode.style.display = "none";
-				}
+			if (!this.messagesNode) return;
+			if (message) {
+				this.messagesNode.innerHTML = message;
+				this.messagesNode.style.display = "";
+			} else {
+				this.messagesNode.innerHTML = "";
+				this.messagesNode.style.display = "none";
 			}
 		},
 
