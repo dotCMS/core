2718697de7b97e5ae0cd4130e1e953615449ac11
valentinogiardino <valentinogiardino17@gmail.com>
2024-11-26 17:58:57 -0300

    #30771 add swagger schema

---
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/content/_import/ContentImportParamsSchema.java
@@ -0,0 +1,44 @@
+package com.dotcms.rest.api.v1.content._import;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Schema for content import parameters.")
+public class ContentImportParamsSchema {
+
+    @Schema(
+            description = "The CSV file to import.",
+            type = "string",
+            format = "binary",
+            required = true
+    )
+    private String file;
+
+    @Schema(
+            description = "JSON string representing import settings.",
+            type = "string",
+            required = true,
+            example = "{\n" +
+                    "  \"contentType\": \"activity\",\n" +
+                    "  \"language\": \"en-US\",\n" +
+                    "  \"workflowActionId\": \"1234\",\n" +
+                    "  \"fields\": [\"title\"]\n" +
+                    "}"
+    )
+    private String form;
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    public String getForm() {
+        return form;
+    }
+
+    public void setForm(String form) {
+        this.form = form;
+    }
+}
