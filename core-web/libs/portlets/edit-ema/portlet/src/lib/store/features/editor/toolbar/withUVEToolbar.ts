537815adc9b564b58a642ad89a494a9af832ed66
Kevin Davila <kfariid@gmail.com>
2024-11-27 12:39:42 -0500

    Removed comments

--- core-web/libs/portlets/edit-ema/portlet/src/lib/store/features/editor/toolbar/withUVEToolbar.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/store/features/editor/toolbar/withUVEToolbar.ts
@@ -44,7 +44,7 @@ export interface UVEToolbarProps {
 }
 
 export function withUVEToolbar() {
-    const oldToolbar = signalStoreFeature(
+    return signalStoreFeature(
         {
             state: type<UVEState>()
         },
@@ -126,89 +126,4 @@ export function withUVEToolbar() {
             }
         }))
     )
-
-    // const newToolbar = signalStoreFeature(
-    //     {
-    //         state: type<UVEState>()
-    //     },
-    //     withState<EditorToolbarState>(initialState),
-    //     withComputed((store) => ({
-    //         $toolbar: computed<UVEToolbarProps>(() => {
-    //             const params = store.pageParams();
-    //             const url = sanitizeURL(params?.url);
-
-    //             const pageAPIQueryParams = createPageApiUrlWithQueryParams(url, params);
-    //             const pageAPIResponse = store.pageAPIResponse();
-    //             const experiment = store.experiment?.();
-
-    //             const pageAPI = `/api/v1/page/${
-    //                 store.isTraditionalPage() ? 'render' : 'json'
-    //             }/${pageAPIQueryParams}`;
-
-    //             const isExperimentRunning = experiment?.status === DotExperimentStatus.RUNNING;
-    //             const isPageLocked = computePageIsLocked(
-    //                 pageAPIResponse?.page,
-    //                 store.currentUser()
-    //             );
-    //             const shouldShowUnlock = isPageLocked && pageAPIResponse?.page.canLock;
-
-    //             const unlockButton = {
-    //                 inode: pageAPIResponse?.page.inode,
-    //                 loading: store.status() === UVE_STATUS.LOADING
-    //             };
-
-    //             const shouldShowInfoDisplay =
-    //                 !getIsDefaultVariant(pageAPIResponse?.viewAs.variantId) ||
-    //                 !store.canEditPage() ||
-    //                 isPageLocked ||
-    //                 !!store.device() ||
-    //                 !!store.socialMedia();
-
-    //             const bookmarksUrl = createFavoritePagesURL({
-    //                 languageId: Number(params?.language_id),
-    //                 pageURI: url,
-    //                 siteId: pageAPIResponse?.site?.identifier
-    //             });
-    //             const clientHost = `${params?.clientHost ?? window.location.origin}`;
-    //             const siteId = pageAPIResponse?.site?.identifier;
-
-    //             return {
-    //                 editorToolbar: store.isPreviewMode() ? null : {                        
-    //                     bookmarksUrl,
-    //                     copyUrl: createFullURL(params, siteId),
-    //                     apiUrl: pageAPI,
-    //                 },
-    //                 previewToolbar: store.isPreviewMode() ? {
-    //                     deviceSelector: {
-    //                         apiLink: `${clientHost}${pageAPI}`,
-    //                         hideSocialMedia: !store.isTraditionalPage()
-    //                     },
-    //                 } : null,
-    //                 currentLanguage: pageAPIResponse?.viewAs.language,
-    //                 urlContentMap: store.isEditState()
-    //                     ? (pageAPIResponse?.urlContentMap ?? null)
-    //                     : null,
-    //                 runningExperiment: isExperimentRunning ? experiment : null,
-    //                 workflowActionsInode: store.canEditPage() ? pageAPIResponse?.page.inode : null,
-    //                 personaSelector: {
-    //                     pageId: pageAPIResponse?.page.identifier,
-    //                     value: pageAPIResponse?.viewAs.persona ?? DEFAULT_PERSONA
-    //                 },
-    //                 unlockButton: shouldShowUnlock ? unlockButton : null,
-    //                 showInfoDisplay: shouldShowInfoDisplay,
-    //             };
-    //         }),
-    //     })),
-    //     withMethods((store) => ({
-    //         // Fake method to toggle preview mode
-    //         // This method should be implemented in the real application
-    //         togglePreviewMode: (preview) => {
-    //             patchState(store, {
-    //                 isPreviewMode: preview
-    //             })
-    //         }
-    //     }))
-    // )
-
-    return oldToolbar;
 }
