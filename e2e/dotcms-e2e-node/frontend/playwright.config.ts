df7b58a231c3a551a8f84fc6ebd71201e43f9cb5
Victor Alfaro <victor.alfaro@dotcms.com>
2024-12-03 13:11:37 -0600

    test

--- e2e/dotcms-e2e-node/frontend/playwright.config.ts
+++ e2e/dotcms-e2e-node/frontend/playwright.config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: parseInt(process.env.RETRIES),
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: parseInt(process.env.WORKERS),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   timeout: 30000,
 
