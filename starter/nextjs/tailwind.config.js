cb442e83648da734adafd26d268d869b44a31430
fmontes <751424+fmontes@users.noreply.github.com>
2024-12-17 15:38:17 -0600

    fix: add container to safelist

--- starter/nextjs/tailwind.config.js
+++ starter/nextjs/tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
-
     plugins: [],
+    safelist: [
+        'container'
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+        },
+    },
 };
