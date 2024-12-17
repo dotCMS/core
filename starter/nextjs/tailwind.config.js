97781dc839a2c71da81481ea1d3c7c31989b56e1
fmontes <751424+fmontes@users.noreply.github.com>
2024-12-17 12:48:58 -0600

    chore: add cleaned nextjs starter

---
+++ starter/nextjs/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+
+    plugins: [],
+};
