afce0496e0d44684e3c547e1e8bdb066cb954e01
gortiz-dotcms <geronimo.ortiz@dotcms.com>
2024-12-17 15:14:33 -0300

    fix: fix test (#28761)

--- core-web/libs/portlets/edit-ema/portlet/src/lib/components/dot-ema-dialog/store/dot-ema-dialog.store.spec.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/components/dot-ema-dialog/store/dot-ema-dialog.store.spec.ts
@@ -123,6 +123,7 @@ describe('DotEmaDialogStoreService', () => {
             _content_struts_action: '/ext/contentlet/edit_contentlet',
             _content_cmd: 'edit',
             inode: '123',
+            angularCurrentPortlet: 'edit-page',
             variantName: TEST_VARIANT
         });
 
