d972c6c698407f8b0cdde80e9c055195a8b3ef6f
gortiz-dotcms <geronimo.ortiz@dotcms.com>
2025-01-06 13:04:10 -0300

    fix(import): add empty check (#30126)

--- dotCMS/src/main/java/com/dotcms/util/RelationshipUtil.java
+++ dotCMS/src/main/java/com/dotcms/util/RelationshipUtil.java
@@ -86,22 +86,24 @@ public class RelationshipUtil {
 
         //Filter can be an identifier or a lucene query (comma separated)
         for (final String elem : filter.split(StringPool.COMMA)) {
-            if (!UUIDUtil.isUUID(elem.trim()) && !isLuceneQuery){
-                throw new DotValidationException("The field has a value (" + filter  + ") that is not an identifier or a lucene query");
-            }
-            if (UUIDUtil.isUUID(elem.trim()) && !relatedContentlets.containsKey(elem.trim())) {
-                final Identifier identifier = identifierAPI.find(elem.trim());
-                final Contentlet relatedContentlet = contentletAPI
-                        .findContentletForLanguage(language, identifier);
-                relatedContentlets.put(relatedContentlet.getIdentifier(), isCheckout ? contentletAPI
-                        .checkout(relatedContentlet.getInode(), user, respectFrontendRoles)
-                        : relatedContentlet);
-            } else {
-                relatedContentlets
-                        .putAll((isCheckout ? contentletAPI.checkoutWithQuery(elem, user, false)
-                                : contentletAPI.search(elem, 0, 0, sortBy, user, false)).stream()
-                                .collect(Collectors
-                                        .toMap(Contentlet::getIdentifier, Function.identity(),(oldValue, newValue) -> oldValue)));
+            if (!filter.isEmpty()) {
+                if (!UUIDUtil.isUUID(elem.trim()) && !isLuceneQuery) {
+                    throw new DotValidationException("The field has a value (" + filter + ") that is not an identifier or a lucene query");
+                }
+                if (UUIDUtil.isUUID(elem.trim()) && !relatedContentlets.containsKey(elem.trim())) {
+                    final Identifier identifier = identifierAPI.find(elem.trim());
+                    final Contentlet relatedContentlet = contentletAPI
+                            .findContentletForLanguage(language, identifier);
+                    relatedContentlets.put(relatedContentlet.getIdentifier(), isCheckout ? contentletAPI
+                            .checkout(relatedContentlet.getInode(), user, respectFrontendRoles)
+                            : relatedContentlet);
+                } else {
+                    relatedContentlets
+                            .putAll((isCheckout ? contentletAPI.checkoutWithQuery(elem, user, false)
+                                    : contentletAPI.search(elem, 0, 0, sortBy, user, false)).stream()
+                                    .collect(Collectors
+                                            .toMap(Contentlet::getIdentifier, Function.identity(), (oldValue, newValue) -> oldValue)));
+                }
             }
         }
 
