9080340261a036399ac438891ad8f2c3c4b7ea80
Nicolas Molina <hi@nicobytes.com>
2025-01-14 11:52:42 -0400

    fix(edit-content): apply format

--- core-web/libs/utils-testing/src/lib/field-util.ts
+++ core-web/libs/utils-testing/src/lib/field-util.ts
@@ -1,9 +1,10 @@
+import { faker } from '@faker-js/faker';
+
 import {
     DotCMSContentTypeField,
     DotCMSContentTypeLayoutRow,
     DotCMSContentTypeLayoutColumn
 } from '@dotcms/dotcms-models';
-import { fa, faker } from '@faker-js/faker';
 
 export const EMPTY_FIELD: DotCMSContentTypeField = {
     contentTypeId: null,
