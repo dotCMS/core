0108942031b5344205a74c5c0e3f6a09dd22ea66
Nicolas Molina <hi@nicobytes.com>
2025-01-16 12:36:08 -0400

    chore(e2e): sync with master

--- e2e/dotcms-e2e-node/frontend/utils/dotCMSUtils.ts
+++ e2e/dotcms-e2e-node/frontend/utils/dotCMSUtils.ts
@@ -1,47 +1,38 @@
-import { Page, expect, Locator } from "@playwright/test";
-import { loginLocators } from "../locators/globalLocators";
+import { Page, expect, Locator } from '@playwright/test';
+import { loginLocators } from '../locators/globalLocators';
 
 export class dotCMSUtils {
-  /**
-   *  Login to dotCMS
-   * @param page
-   * @param username
-   * @param password
-   */
-  async login(page: Page, username: string, password: string) {
-    await page.goto("/dotAdmin");
-    await page.waitForLoadState();
-    const userNameInputLocator = page.locator(loginLocators.userNameInput);
-    await waitForVisibleAndCallback(userNameInputLocator, () =>
-      userNameInputLocator.fill(username),
-    );
-    const passwordInputLocator = page.locator(loginLocators.passwordInput);
-    await waitForVisibleAndCallback(passwordInputLocator, () =>
-      passwordInputLocator.fill(password),
-    );
-    const loginBtnLocator = page.getByTestId(loginLocators.loginBtn);
-    await waitForVisibleAndCallback(loginBtnLocator, () =>
-      loginBtnLocator.click(),
-    );
-    const gettingStartedLocator = page.getByRole("link", {
-      name: "Getting Started",
-    });
-    await waitForVisibleAndCallback(gettingStartedLocator, () =>
-      expect(gettingStartedLocator).toBeVisible(),
-    );
-  }
 
-  /**
-   * Navigate to the content portlet providing the menu, group and tool locators
-   * @param menu
-   * @param group
-   * @param tool
-   */
-  async navigate(menu: Locator, group: Locator, tool: Locator) {
-    await menu.click();
-    await group.click();
-    await tool.click();
-  }
+    /**
+     *  Login to dotCMS
+     * @param page
+     * @param username
+     * @param password
+     */
+    async login(page: Page, username: string, password: string) {
+        await page.goto('/dotAdmin');
+        await page.waitForLoadState()
+        const userNameInputLocator = page.locator(loginLocators.userNameInput);
+        await waitForVisibleAndCallback(userNameInputLocator, () => userNameInputLocator.fill(username));
+        const passwordInputLocator = page.locator(loginLocators.passwordInput);
+        await waitForVisibleAndCallback(passwordInputLocator, () => passwordInputLocator.fill(password));
+        const loginBtnLocator = page.getByTestId(loginLocators.loginBtn);
+        await waitForVisibleAndCallback(loginBtnLocator, () => loginBtnLocator.click());
+        const gettingStartedLocator = page.getByRole('link', { name: 'Getting Started' });
+        await waitForVisibleAndCallback(gettingStartedLocator, () => expect(gettingStartedLocator).toBeVisible());
+    }
+
+    /**
+     * Navigate to the content portlet providing the menu, group and tool locators
+     * @param menu
+     * @param group
+     * @param tool
+     */
+    async navigate(menu : Locator, group : Locator, tool : Locator) {
+        await menu.click();
+        await group.click();
+        await tool.click();
+    }
 }
 
 /**
@@ -49,12 +40,9 @@ export class dotCMSUtils {
  * @param locator
  * @param state
  */
-export const waitFor = async (
-  locator: Locator,
-  state: "attached" | "detached" | "visible" | "hidden",
-): Promise<void> => {
-  await locator.waitFor({ state: state });
-};
+export const waitFor = async (locator: Locator, state: "attached" | "detached" | "visible" | "hidden"): Promise<void> => {
+    await locator.waitFor({state: state});
+}
 
 /**
  * Wait for the locator to be visible
@@ -62,13 +50,9 @@ export const waitFor = async (
  * @param state
  * @param callback
  */
-export const waitForAndCallback = async (
-  locator: Locator,
-  state: "attached" | "detached" | "visible" | "hidden",
-  callback: () => Promise<void>,
-): Promise<void> => {
-  await waitFor(locator, state);
-  await callback();
+export const waitForAndCallback = async (locator: Locator, state: "attached" | "detached" | "visible" | "hidden", callback: () => Promise<void>): Promise<void> => {
+    await waitFor(locator, state);
+    await callback();
 };
 
 /**
@@ -76,9 +60,6 @@ export const waitForAndCallback = async (
  * @param locator
  * @param callback
  */
-export const waitForVisibleAndCallback = async (
-  locator: Locator,
-  callback: () => Promise<void>,
-): Promise<void> => {
-  await waitForAndCallback(locator, "visible", callback);
+export const waitForVisibleAndCallback = async (locator: Locator, callback: () => Promise<void>): Promise<void> => {
+    await waitForAndCallback(locator, 'visible', callback);
 };
