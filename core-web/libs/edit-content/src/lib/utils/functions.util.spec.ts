90ab813f2d8e3fd51dbc6ba4ae8aa9d2ea1f1742
Arcadio Quintero <oidacra@gmail.com>
2025-01-16 09:41:29 -0500

    feat(edit-content) fix lint

--- core-web/libs/edit-content/src/lib/utils/functions.util.spec.ts
+++ core-web/libs/edit-content/src/lib/utils/functions.util.spec.ts
@@ -5,6 +5,7 @@ import {
     DotCMSContentTypeFieldVariable,
     DotLanguage
 } from '@dotcms/dotcms-models';
+import { createFakeContentlet } from '@dotcms/utils-testing';
 
 import { MOCK_CONTENTTYPE_2_TABS, MOCK_FORM_CONTROL_FIELDS } from './edit-content.mock';
 import * as functionsUtil from './functions.util';
@@ -21,7 +22,6 @@ import {
 } from './functions.util';
 import { CALENDAR_FIELD_TYPES, JSON_FIELD_MOCK, MULTIPLE_TABS_MOCK } from './mocks';
 
-import { createFakeContentlet } from '../../../../utils-testing/src/lib/dot-contentlet.mock';
 import { FLATTENED_FIELD_TYPES } from '../models/dot-edit-content-field.constant';
 import { DotEditContentFieldSingleSelectableDataType } from '../models/dot-edit-content-field.enum';
 import { NON_FORM_CONTROL_FIELD_TYPES } from '../models/dot-edit-content-form.enum';
