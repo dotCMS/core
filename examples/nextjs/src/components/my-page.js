318c4e8b7da3c4044b5211d0e9b14f98d80adabd
Jalinson Diaz <zjaaaldev@gmail.com>
2025-01-23 14:26:22 -0300

    revert testing changes

--- examples/nextjs/src/components/my-page.js
+++ examples/nextjs/src/components/my-page.js
@@ -78,7 +78,12 @@ export function MyPage({ pageAsset, nav }) {
                         components: componentsMap
                     }}
                     config={{
-                        pathname
+                        pathname,
+                        editor: {
+                            params: {
+                                depth: 3
+                            }
+                        }
                     }}
                 />
             </main>
