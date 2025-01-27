83f906164bcab19ca371e53cb10a2aa3d8121883
Nicolas Molina <hi@nicobytes.com>
2025-01-27 17:30:50 -0400

    chore(e2e): sync with master

--- e2e/dotcms-e2e-node/frontend/tests/newEditContent/fields/textField.spec.ts
+++ e2e/dotcms-e2e-node/frontend/tests/newEditContent/fields/textField.spec.ts
@@ -36,7 +36,7 @@ test.afterEach(async ({ page, request }) => {
 });
 
 test.describe("text field", () => {
-  test("should save a text field", async ({ page }) => {
+  test.skip("should save a text field", async ({ page }) => {
     const locatorField = page.getByTestId("textField");
 
     await expect(locatorField).toBeVisible();
