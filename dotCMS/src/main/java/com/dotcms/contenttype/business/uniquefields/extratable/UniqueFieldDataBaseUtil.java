05309860e48ee120a947d1b1f5803917c447532d
freddyDOTCMS <freddy0309@gmail.com>
2025-02-03 14:12:06 -0600

    Tetsing chatGPT feedback

--- dotCMS/src/main/java/com/dotcms/contenttype/business/uniquefields/extratable/UniqueFieldDataBaseUtil.java
+++ dotCMS/src/main/java/com/dotcms/contenttype/business/uniquefields/extratable/UniqueFieldDataBaseUtil.java
@@ -34,15 +34,19 @@ import static com.dotcms.util.CollectionsUtils.list;
 @ApplicationScoped
 public class UniqueFieldDataBaseUtil {
 
-    private static final String INSERT_SQL = "INSERT INTO unique_fields (unique_key_val, supporting_values) VALUES (encode(sha256(?::bytea), 'hex'), ?)";
+    private static final String INSERT_SQL = "INSERT INTO unique_fields (unique_key_val, supporting_values) VALUES (encode(sha256(convert_to(?::text, 'UTF8')), 'hex'), ?)";
 
     private static final String RECALCULATE_UNIQUE_KEY_VAL = "UPDATE unique_fields\n" +
-            "SET unique_key_val = encode(sha256(\n" +
-            "    jsonb_extract_path_text(supporting_values, '" + CONTENT_TYPE_ID_ATTR + "')::bytea || \n" +
-            "    jsonb_extract_path_text(supporting_values, '" + FIELD_VARIABLE_NAME_ATTR + "')::bytea || \n" +
-            "    jsonb_extract_path_text(supporting_values, '" + LANGUAGE_ID_ATTR + "')::bytea || \n" +
-            "    jsonb_extract_path_text(supporting_values, '" + FIELD_VALUE_ATTR + "')::bytea\n" +
-            "    %s \n" +
+            "SET unique_key_val = encode(sha256(" +
+                "convert_to(\n" +
+                    "CONCAT(" +
+                        "    jsonb_extract_path_text(supporting_values, '" + CONTENT_TYPE_ID_ATTR + "')::text || \n" +
+                        "    jsonb_extract_path_text(supporting_values, '" + FIELD_VARIABLE_NAME_ATTR + "')::text || \n" +
+                        "    jsonb_extract_path_text(supporting_values, '" + LANGUAGE_ID_ATTR + "')::text || \n" +
+                        "    jsonb_extract_path_text(supporting_values, '" + FIELD_VALUE_ATTR + "')::text\n" +
+                        "    %s \n" +
+                    "),'UTF8'\n" +
+                ")\n" +
             "), 'hex'), \n" +
             "supporting_values = jsonb_set(supporting_values, '{" + UNIQUE_PER_SITE_ATTR + "}', '%s') \n" +
             "WHERE supporting_values->>'" + CONTENT_TYPE_ID_ATTR + "' = ?\n" +
@@ -52,7 +56,7 @@ public class UniqueFieldDataBaseUtil {
 
     private static final String UPDATE_CONTENT_LIST ="UPDATE unique_fields " +
             "SET supporting_values = jsonb_set(supporting_values, '{" + CONTENTLET_IDS_ATTR + "}', ?::jsonb) " +
-            "WHERE unique_key_val = encode(sha256(?::bytea), 'hex')";
+            "WHERE unique_key_val = encode(sha256(convert_to(?::text, 'UTF8')), 'hex')";
 
     private static final String UPDATE_CONTENT_LIST_WITH_HASH ="UPDATE unique_fields " +
             "SET supporting_values = jsonb_set(supporting_values, '{" + CONTENTLET_IDS_ATTR + "}', ?::jsonb) " +
@@ -100,11 +104,11 @@ public class UniqueFieldDataBaseUtil {
             "            sha256(" +
             "                    convert_to(" +
             "                            CONCAT(" +
-            "                                    content_type_id," +
-            "                                    field_var_name," +
-            "                                    language_id," +
-            "                                    COALESCE(host_id::text, '')," +
-            "                                    COALESCE(field_value::text, '')," +
+            "                                    content_type_id::text," +
+            "                                    field_var_name::text," +
+            "                                    language_id::text," +
+            "                                    host_id::text," +
+            "                                    field_value::text," +
             "                                    CASE WHEN uniquePerSite = 'true' THEN COALESCE(host_id::text, '') ELSE '' END" +
             "                            )," +
             "                            'UTF8'" +
