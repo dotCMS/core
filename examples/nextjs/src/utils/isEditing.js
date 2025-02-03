ac30f4befec967bac51093acf311352151b5c7a1
Jalinson Diaz <zjaaaldev@gmail.com>
2025-02-03 12:53:10 -0300

    Update isEditing.js

--- examples/nextjs/src/utils/isEditing.js
+++ examples/nextjs/src/utils/isEditing.js
@@ -1,5 +1,9 @@
 import { getUVEState } from '@dotcms/uve';
 
+/**
+ * Check if the user is in edit mode
+ * @returns {boolean}
+ */
 export function isEditing() {
     return getUVEState()?.mode === 'edit';
 }
