d8445095eb194be42b776902bee2512e033ca10e
Jose Castro <jose.castro@dotcms.com>
2025-02-06 15:26:24 -0600

    Fixes #31312 : Adding some of the new AI Telemetry Metrics

---
+++ dotCMS/src/main/java/com/dotcms/telemetry/collectors/ai/TotalSitesUsingDotaiMetricType.java
@@ -0,0 +1,67 @@
+package com.dotcms.telemetry.collectors.ai;
+
+import com.dotcms.security.apps.AppDescriptor;
+import com.dotcms.security.apps.AppsAPI;
+import com.dotcms.telemetry.MetricCategory;
+import com.dotcms.telemetry.MetricFeature;
+import com.dotcms.telemetry.MetricType;
+import com.dotcms.telemetry.MetricValue;
+import com.dotmarketing.business.APILocator;
+import com.dotmarketing.exception.DoesNotExistException;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotSecurityException;
+
+import javax.ws.rs.NotSupportedException;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class TotalSitesUsingDotaiMetricType implements MetricType {
+
+    @Override
+    public String getName() {
+        return "TOTAL_SITES_USING_DOTAI";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Total number of Sites using dotAI";
+    }
+
+    @Override
+    public MetricCategory getCategory() {
+        return MetricCategory.DIFFERENTIATING_FEATURES;
+    }
+
+    @Override
+    public MetricFeature getFeature() {
+        return MetricFeature.AI;
+    }
+
+    @Override
+    public Optional<MetricValue> getStat() throws DotDataException {
+        final AppsAPI appsAPI = APILocator.getAppsAPI();
+        final String key = "dotAI";
+        try {
+            final Optional<AppDescriptor> appDescriptorOptional = appsAPI
+                    .getAppDescriptor(key, APILocator.systemUser());
+            if (appDescriptorOptional.isEmpty()) {
+                throw new DoesNotExistException(String.format("No App was found for key `%s`. ", key)
+                );
+            }
+            final AppDescriptor appDescriptor = appDescriptorOptional.get();
+            final Map<String,Set<String>> appKeysByHost = appsAPI.appKeysByHost();
+            final Set<String> sitesWithConfigurations = appsAPI
+                    .filterSitesForAppKey(appDescriptor.getKey(), appKeysByHost.keySet(), APILocator.systemUser());
+            return Optional.of(new MetricValue(this.getMetric(), sitesWithConfigurations.size()));
+        } catch (final DotSecurityException e) {
+            throw new DotDataException(e);
+        }
+    }
+
+    @Override
+    public Optional<Object> getValue() {
+        throw new NotSupportedException();
+    }
+
+}
