e6d09c7a7b859189e026a86007f0a9f992487a90
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2025-02-06 08:57:38 -0600

    #31185

--- dotCMS/src/main/java/com/dotcms/rest/config/DotServiceLocatorImpl.java
+++ dotCMS/src/main/java/com/dotcms/rest/config/DotServiceLocatorImpl.java
@@ -1,10 +1,12 @@
 package com.dotcms.rest.config;
 
-import org.jvnet.hk2.annotations.Service;
 import org.jvnet.hk2.internal.ServiceLocatorImpl;
 import org.jvnet.hk2.internal.Utilities;
 
-@Service
+/**
+ * Here's why we're doing this: We need to override the inject method in the ServiceLocatorImpl
+ * as it performs a checkStatus that we don't want to do when we're shutting down
+ */
 public class DotServiceLocatorImpl extends ServiceLocatorImpl {
 
     String name;
@@ -20,10 +22,16 @@ public class DotServiceLocatorImpl extends ServiceLocatorImpl {
         this.name = name;
     }
 
+
+    /**
+     * Injects the given object using the given strategy
+     * @param injectMe The object to be analyzed and injected into
+     * @param strategy The name of the {@link ClassAnalyzer} that should be used. If
+     * null the default analyzer will be used
+     */
     @Override
     public void inject(Object injectMe, String strategy) {
         //Here we check if we're shutting down
-        System.out.println(":: DotServiceLocatorImpl.inject!!!!");
         Utilities.justInject(injectMe, this, strategy);
     }
 
