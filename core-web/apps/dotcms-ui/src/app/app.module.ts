53c36e7d4b1e343a7e8e6c64fa09a86da80ab0b7
fmontes <751424+fmontes@users.noreply.github.com>
2025-02-06 14:28:38 -0600

    refactor(ui): Update UI components and styling with PrimeNG and Lato font

--- core-web/apps/dotcms-ui/src/app/app.module.ts
+++ core-web/apps/dotcms-ui/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { MonacoEditorModule } from '@materia-ui/ngx-monaco-editor';
+import { definePreset } from '@primeng/themes';
+import Aura from '@primeng/themes/aura';
 import { MarkdownModule } from 'ngx-markdown';
 
 import { CommonModule } from '@angular/common';
@@ -9,6 +11,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // App is our top level component
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { providePrimeNG } from 'primeng/config';
+
 import { DotMessagePipe, DotSafeHtmlPipe } from '@dotcms/ui';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -18,6 +24,41 @@ import { CUSTOM_MODULES, NGFACES_MODULES } from './modules';
 import { ENV_PROVIDERS } from './providers';
 import { DotDirectivesModule } from './shared/dot-directives.module';
 import { SharedModule } from './shared/shared.module';
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{blue.50}',
+            100: '{blue.100}',
+            200: '{blue.200}',
+            300: '{blue.300}',
+            400: '{blue.400}',
+            500: '{blue.500}',
+            600: '{blue.600}',
+            700: '{blue.700}',
+            800: '{blue.800}',
+            900: '{blue.900}',
+            950: '{blue.950}'
+        },
+        colorScheme: {
+            light: {
+                primary: {
+                    color: '{blue.900}',
+                    inverseColor: '#ffffff',
+                    hoverColor: '{blue.500}',
+                    activeColor: '{blue.600}'
+                },
+                highlight: {
+                    background: '{blue.50}',
+                    focusBackground: '{blue.50}',
+                    color: '{blue.800}',
+                    focusColor: '{blue.800}'
+                }
+            },
+        }
+    }
+});
+
 
 @NgModule({
     bootstrap: [AppComponent],
@@ -39,7 +80,17 @@ import { SharedModule } from './shared/shared.module';
         MarkdownModule.forRoot(),
         DotMessagePipe
     ],
-    providers: [ENV_PROVIDERS],
+    providers: [ENV_PROVIDERS,
+        provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: MyPreset,
+                options: {
+                    darkModeSelector: false
+                }
+            }
+        })
+    ],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {}
