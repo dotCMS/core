d4b201ecfe514437dd10edef7d492b78374a42a9
Nicolas Molina <hi@nicobytes.com>
2025-02-11 11:52:02 -0400

    chore(e2e): allow create content types dynamically

--- e2e/dotcms-e2e-node/frontend/models/newContentType.model.ts
+++ e2e/dotcms-e2e-node/frontend/models/newContentType.model.ts
@@ -1,22 +1,42 @@
+/** 
+ * Enumeration of available field types in the content type system.
+ * @enum {string}
+ */
 export enum TYPES {
   Text = "text",
   SiteOrFolder = "siteOrFolder",
 }
 
+/** 
+ * Union type of all possible field types.
+ * @typedef {TYPES} Fields
+ */
 export type Fields = `${TYPES}`;
 
+/** 
+ * Interface representing a text field configuration.
+ * @interface
+ */
 export interface TextField {
   title: string;
   fieldType: `${TYPES.Text}`;
-  requeried?: boolean;
+  required?: boolean;
   hintText?: string;
 }
 
+/** 
+ * Interface representing a site or host field configuration.
+ * @interface
+ */
 export interface SiteorHostField {
   title: string;
   fieldType: `${TYPES.SiteOrFolder}`;
-  requeried?: boolean;
+  required?: boolean;
   hintText?: string;
 }
 
+/** 
+ * Union type of all possible field type configurations.
+ * @typedef {TextField | SiteorHostField} FieldsTypes
+ */
 export type FieldsTypes = TextField | SiteorHostField;
