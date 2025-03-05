10b251f3bf8cdd29434b04c9b33dbc910211e02c
Humberto Morera <31667212+hmoreras@users.noreply.github.com>
2025-03-05 09:14:11 -0600

    implementation ( Edit Content ): #31288 Content Lock/Unlock Management System  (#31531)
    ### Proposed Changes
    
    [ ] On new content or when the user can’t lock the switch will be
    disabled.
    [ ] Once the content is created the user can lock / unlock the content. 
    [ ] When is locked the form will be disabled and a message will be show
    of who is blocking the content.
    [ ] If the user switch to other language, the content won’t be blocked. 
    
    
    
    ### Screenshots
    
    
    https://github.com/user-attachments/assets/6de76276-0a4b-4394-a7fe-6577a766ca30

--- core-web/apps/dotcms-ui/src/app/portlets/dot-starter/dot-starter-resolver.service.spec.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-starter/dot-starter-resolver.service.spec.ts
@@ -15,7 +15,7 @@ export const CurrentUserDataMock = {
     email: 'admin@dotcms.com',
     givenName: 'TEST',
     roleId: 'e7d23sde-5127-45fc-8123-d424fd510e3',
-    surnaname: 'User',
+    surname: 'User',
     userId: 'testId'
 };
 
