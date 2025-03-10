28b45d0b9d06ce206857b785513f9fdef16607b8
Rafael Velazco <rjvelazco21@gmail.com>
2025-03-10 13:27:32 -0400

    feat(sdk react): Export DotCMSShowControl component

--- core-web/libs/sdk/react/src/next.ts
+++ core-web/libs/sdk/react/src/next.ts
@@ -1,1 +1,3 @@
 export { DotCMSLayoutBody } from './lib/next/components/DotCMSLayoutBody/DotCMSLayoutBody';
+
+export { DotCMSShowControl } from './lib/next/components/DotCMSShowControl/DotCMSShowControl';
