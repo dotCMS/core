6564ab7af64a91901a2a8ffe90bda9134dd19d68
Arcadio Quintero <oidacra@gmail.com>
2025-03-24 19:32:28 -0400

    feat(dotcms-ui): enhance DotRelativeDatePipe tests with new scenarios
    This commit adds new test cases for the DotRelativeDatePipe to cover scenarios for returning relative time strings for minutes and hours. Additionally, it updates the DotMessageService mock to include relevant messages for relative date formatting. The changes improve test coverage and ensure the pipe behaves as expected for various time intervals.

--- core-web/libs/ui/src/lib/pipes/dot-relative-date/dot-relative-date.pipe.spec.ts
+++ core-web/libs/ui/src/lib/pipes/dot-relative-date/dot-relative-date.pipe.spec.ts
@@ -26,6 +26,7 @@ const getDateAndTimeFormat = (date: Date): string => {
 describe('DotRelativeDatePipe', () => {
     let formatDateService: DotFormatDateService;
     let pipe: DotRelativeDatePipe;
+    let messageService: DotMessageService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -40,7 +41,12 @@ describe('DotRelativeDatePipe', () => {
                 },
                 {
                     provide: DotMessageService,
-                    useValue: new MockDotMessageService({ new: 'New' })
+                    useValue: new MockDotMessageService({
+                        'relative.date.now': 'Now',
+                        'relative.date.minute.ago': '1 minute ago',
+                        'relative.date.minutes.ago': '{0} minutes ago',
+                        'relative.date.hours.ago': '{0} hours ago'
+                    })
                 },
                 DotFormatDateService,
                 DotRelativeDatePipe
@@ -48,6 +54,7 @@ describe('DotRelativeDatePipe', () => {
         });
 
         formatDateService = TestBed.inject(DotFormatDateService);
+        messageService = TestBed.inject(DotMessageService);
         pipe = TestBed.inject(DotRelativeDatePipe);
     });
 
@@ -74,6 +81,18 @@ describe('DotRelativeDatePipe', () => {
             date.setDate(date.getDate() - 1);
             const dateAndTime = getDateAndTimeFormat(date);
             expect(pipe.transform(dateAndTime)).toEqual('1 day ago');
+        });
+
+        it('should return minutes ago for time less than 2 hours', () => {
+            const date = new Date();
+            date.setMinutes(date.getMinutes() - 30);
+            expect(pipe.transform(date.getTime())).toEqual('30 minutes ago');
+        });
+
+        it('should return hours ago for time less than 24 hours', () => {
+            const date = new Date();
+            date.setHours(date.getHours() - 5);
+            expect(pipe.transform(date.getTime())).toEqual('5 hours ago');
         });
     });
 
