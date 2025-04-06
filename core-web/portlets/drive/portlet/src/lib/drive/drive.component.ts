a70c7bb600ce77acd5e4d87ca24e44524b12545b
Freddy Montes <751424+fmontes@users.noreply.github.com>
2025-04-05 19:28:12 -0600

    Render content

--- core-web/portlets/drive/portlet/src/lib/drive/drive.component.ts
+++ core-web/portlets/drive/portlet/src/lib/drive/drive.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+import { TableModule } from 'primeng/table';
+import { take } from 'rxjs/operators';
+
+import { DotESContentService, queryEsParams } from '@dotcms/data-access';
+import { ESContent, DotCMSContentlet } from '@dotcms/dotcms-models';
 
 @Component({
     selector: 'lib-drive',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, TableModule],
+    providers: [DotESContentService],
     templateUrl: './drive.component.html',
     styleUrl: './drive.component.scss'
 })
-export class DriveComponent {}
+export class DriveComponent implements OnInit {
+    items: DotCMSContentlet[] = [];
+    loading = false;
+
+    constructor(private dotESContentService: DotESContentService) {}
+
+    ngOnInit() {
+        this.loadRootFiles();
+    }
+
+    private loadRootFiles(): void {
+        this.loading = true;
+
+        const params: queryEsParams = {
+            query: '+path:/*',
+            itemsPerPage: 40
+        };
+
+        this.dotESContentService.get(params)
+            .pipe(take(1))
+            .subscribe((response: ESContent) => {
+                this.items = response.jsonObjectView.contentlets;
+                this.loading = false;
+            });
+    }
+}
