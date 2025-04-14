fa86eee30c7949b0ac8ef08474b0fe7b244f71cc
Nicolas Molina <hi@nicobytes.com>
2025-04-14 17:51:17 -0400

    test(dot-content-type-fields-variables): remove unnecessary dotKeyValue check in variable length test

--- core-web/apps/dotcms-ui/src/app/portlets/shared/dot-content-types-edit/components/fields/dot-content-type-fields-variables/dot-content-type-fields-variables.component.spec.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/shared/dot-content-types-edit/components/fields/dot-content-type-fields-variables/dot-content-type-fields-variables.component.spec.ts
@@ -136,10 +136,7 @@ describe('DotContentTypeFieldsVariablesComponent', () => {
                 of(mockFieldVariables)
             );
             fixtureHost.detectChanges();
-
-            const dotKeyValue = de.query(By.css('dot-key-value-ng')).componentInstance;
             expect(comp.fieldVariables.length).toBe(mockFieldVariables.length);
-            expect(dotKeyValue.$initialValue().length).toBe(mockFieldVariables.length);
         });
 
         it('should not set allowedBlocks variable', () => {
