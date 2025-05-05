822b2509d291f0ec038f113f7442b9d043cf5b13
Rafael Velazco <rjvelazco21@gmail.com>
2025-05-05 12:39:40 -0400

    fix(SDK Angular): Video component path

---
+++ core-web/libs/sdk/angular/next/components/dotcms-block-editor-renderer/blocks/video.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectionStrategy, Component, computed, Input } from '@angular/core';
+
+import { BlockEditorNode } from '@dotcms/types';
+
+@Component({
+    selector: 'dotcms-block-editor-renderer-video',
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    template: `
+        <video
+            [controls]="true"
+            preload="metadata"
+            [poster]="this.$posterURL()"
+            [width]="attrs?.['width']"
+            [height]="attrs?.['height']">
+            <track default kind="captions" srclang="en" />
+            <source [src]="this.$srcURL()" [type]="attrs?.['mimeType']" />
+            Your browser does not support the
+            <code>video</code>
+            element.
+        </video>
+    `
+})
+export class DotVideoBlock {
+    @Input() attrs!: BlockEditorNode['attrs'];
+
+    protected readonly $srcURL = computed(() => this.attrs?.['src']);
+
+    protected readonly $posterURL = computed(() => this.attrs?.['data']?.['thumbnail']);
+}
