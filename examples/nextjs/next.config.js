43d0979677fcac5eac15080dde01cb44a245d6db
Rafael Velazco <rjvelazco21@gmail.com>
2025-05-12 18:34:28 -0400

    feat(Example NextJS): Add redirects to remove trailing slashes from URLs

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -3,7 +3,6 @@
 const url = new URL(process.env.NEXT_PUBLIC_DOTCMS_HOST);
 
 const nextConfig = {
-    trailingSlash: true,
     images: {
         remotePatterns: [
             {
@@ -20,6 +19,15 @@ const nextConfig = {
             {
                 source: '/dA/:path*',
                 destination: `${process.env.NEXT_PUBLIC_DOTCMS_HOST}/dA/:path*`,
+            },
+        ];
+    },
+    async redirects() {
+        return [
+            {
+                source: '/:path*/index',
+                destination: '/:path*/',
+                permanent: true,
             },
         ];
     },
