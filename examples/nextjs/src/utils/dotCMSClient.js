9891a40ef5b6124b2b23ad69d6a3f99b6082e37b
Rafael Velazco <rjvelazco21@gmail.com>
2025-05-13 16:33:35 -0400

    chore(Example NextJS): Update environment variables and refactor dotCMSClient to use site ID from .env

--- examples/nextjs/src/utils/dotCMSClient.js
+++ examples/nextjs/src/utils/dotCMSClient.js
@@ -3,7 +3,7 @@ import { createDotCMSClient } from "@dotcms/client/next";
 export const dotCMSClient = createDotCMSClient({
     dotcmsUrl: process.env.NEXT_PUBLIC_DOTCMS_HOST,
     authToken: process.env.NEXT_PUBLIC_DOTCMS_AUTH_TOKEN,
-    siteId: "59bb8831-6706-4589-9ca0-ff74016e02b2",
+    siteId: process.env.NEXT_PUBLIC_DOTCMS_SITE_ID,
     requestOptions: {
         cache: "no-cache",
     }
--- examples/nextjs/src/utils/imageLoader.js
+++ examples/nextjs/src/utils/imageLoader.js
@@ -1,6 +1,6 @@
 const ImageLoader = ({ src, width = 250 }) => {
     const dotcmsURL = new URL(process.env.NEXT_PUBLIC_DOTCMS_HOST).origin;
-    const imageSRC = src.includes('/dA/') ? src : `/dA/${src}`; // Check if the image is a DotCMS asset or a file asset
+    const imageSRC = src.includes('/dA/') ? src : `/dA/${src}`;
 
     return `${dotcmsURL}${imageSRC}/${width}w`;
 };
