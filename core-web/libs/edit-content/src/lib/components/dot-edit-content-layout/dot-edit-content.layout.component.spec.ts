2e016b1e8d09ab9a4e42cd8c2e4d2bd9aafaf877
Humberto Morera <humberto.morera@dotcms.com>
2025-05-16 08:55:56 -0600

    remove comments

--- core-web/libs/edit-content/src/lib/components/dot-edit-content-layout/dot-edit-content.layout.component.spec.ts
+++ core-web/libs/edit-content/src/lib/components/dot-edit-content-layout/dot-edit-content.layout.component.spec.ts
@@ -208,30 +208,6 @@ describe('EditContentLayoutComponent', () => {
                 expect(spectator.query(byTestId('edit-content-layout__beta-message'))).toBeFalsy();
             }));
 
-            // it('should call $store.disableNewContentEditor and prevent default when beta message link is clicked', fakeAsync(() => {
-            //     spectator.detectChanges();
-            //     tick();
-
-            //     const link = spectator.query(byTestId('edit-content-layout__beta-message-link')) as HTMLAnchorElement;
-            //     expect(link).toBeTruthy();
-
-            //     // Spy on the store method
-            //     const disableNewContentEditorSpy = jest.spyOn(store, 'disableNewContentEditor');
-
-            //     // Create a fake event with preventDefault
-            //     const event = new MouseEvent('click');
-            //     Object.defineProperty(event, 'preventDefault', { value: jest.fn() });
-
-            //     // Dispatch the event
-            //     link.dispatchEvent(event);
-
-            //     expect(event.preventDefault).toHaveBeenCalled();
-            //     expect(disableNewContentEditorSpy).toHaveBeenCalled();
-
-            //     // Flush any remaining timers
-            //     tick(1000); // or just tick() or flush() if available
-            // }));
-
             it('should have correct link to old editor', async () => {
                 // Initialize the content type
                 dotContentTypeService.getContentType.mockReturnValue(of(CONTENT_TYPE_MOCK));
