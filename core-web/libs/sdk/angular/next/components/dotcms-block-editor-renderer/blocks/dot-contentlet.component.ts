9e939d30c0a512b7c03ff504de28165e2679f38d
Rafael Velazco <rjvelazco21@gmail.com>
2025-05-27 14:16:40 -0400

    test(DotContentletBlock): Add unit tests for DotContentletBlock component to enhance coverage and error handling

--- core-web/libs/sdk/angular/next/components/dotcms-block-editor-renderer/blocks/dot-contentlet.component.ts
+++ core-web/libs/sdk/angular/next/components/dotcms-block-editor-renderer/blocks/dot-contentlet.component.ts
@@ -1,29 +1,30 @@
 import { AsyncPipe, NgComponentOutlet } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, Input } from '@angular/core';
 
-import { Contentlet, BlockEditorNode, UVE_MODE } from '@dotcms/types';
+import { BlockEditorNode, UVE_MODE } from '@dotcms/types';
 import { getUVEState } from '@dotcms/uve';
 
 import { DynamicComponentEntity } from '../../../models';
 import { CustomRenderer } from '../dotcms-block-editor-renderer.component';
 
 @Component({
-    selector: 'app-unknown-content-type',
+    selector: 'dotcms-unknown-content-type',
+    standalone: true,
     template: `
         <div
             data-testId="unknown-content-type"
             style="
-        background-color: #fffaf0;
-        color: #333;
-        padding: 1rem;
-        border-radius: 0.5rem;
-        margin-bottom: 1rem;
-        margin-top: 1rem;
-        border: 1px solid #ed8936;
-      ">
+                background-color: #fffaf0;
+                color: #333;
+                padding: 1rem;
+                border-radius: 0.5rem;
+                margin-bottom: 1rem;
+                margin-top: 1rem;
+                border: 1px solid #ed8936;
+            ">
             <strong style="color: #c05621">Dev Warning</strong>
             : The content type
-            <strong style="color: #c05621">{{ contentType }}</strong>
+            <strong style="color: #c05621">{{ contentType || 'Unknown' }}</strong>
             is not recognized. Please ensure a custom renderer is provided for this content type.
             <br />
             Learn more about how to create a custom renderer in the
@@ -48,42 +49,46 @@ export class UnknownContentTypeComponent {
 @Component({
     selector: 'dotcms-block-editor-renderer-contentlet',
     standalone: true,
-    imports: [NgComponentOutlet, AsyncPipe],
+    imports: [NgComponentOutlet, AsyncPipe, UnknownContentTypeComponent],
     changeDetection: ChangeDetectionStrategy.OnPush,
-    template:
-        '<ng-container *ngComponentOutlet="contentComponent | async; inputs: { contentlet: $data() }"></ng-container>'
+    template: `
+        @if (contentComponent) {
+            <ng-container
+                *ngComponentOutlet="
+                    contentComponent | async;
+                    inputs: { contentlet: $data() }
+                "></ng-container>
+        } @else if (isDevMode) {
+            <dotcms-unknown-content-type [contentType]="$data()?.contentType" />
+        }
+    `
 })
 export class DotContentletBlock {
     @Input() customRenderers: CustomRenderer | undefined;
     @Input() attrs: BlockEditorNode['attrs'];
 
     contentComponent: DynamicComponentEntity | undefined;
-    protected readonly $data = computed(() => this.attrs?.['data'] as Contentlet<unknown>);
+    protected readonly $data = computed(() => this.attrs?.['data']);
     private readonly DOT_CONTENT_NO_DATA_MESSAGE =
         '[DotCMSBlockEditorRenderer]: No data provided for Contentlet Block. Try to add a contentlet to the block editor. If the error persists, please contact the DotCMS support team.';
     private readonly DOT_CONTENT_NO_MATCHING_COMPONENT_MESSAGE = (contentType: string) =>
         `[DotCMSBlockEditorRenderer]: No matching component found for content type: ${contentType}. Provide a custom renderer for this content type to fix this error.`;
-    private get isDevMode() {
+    protected get isDevMode() {
         return getUVEState()?.mode === UVE_MODE.EDIT;
     }
 
     ngOnInit() {
         if (!this.$data()) {
             console.error(this.DOT_CONTENT_NO_DATA_MESSAGE);
+
+            return;
         }
 
-        this.contentComponent = this.customRenderers?.[this.$data()?.contentType];
-
-        if (!this.contentComponent && this.isDevMode) {
-            this.contentComponent = import('./dot-contentlet.component').then(
-                (m) => m.UnknownContentTypeComponent
-            );
-        }
+        const contentType = this.$data()?.contentType || '';
+        this.contentComponent = this.customRenderers?.[contentType];
 
         if (!this.contentComponent) {
-            console.error(
-                this.DOT_CONTENT_NO_MATCHING_COMPONENT_MESSAGE(this.$data()?.contentType)
-            );
+            console.warn(this.DOT_CONTENT_NO_MATCHING_COMPONENT_MESSAGE(contentType));
         }
     }
 }
