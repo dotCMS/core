5532cdca79b4882d52623b55776a10ae740d0e5f
Rafael Velazco <rjvelazco21@gmail.com>
2025-06-19 13:23:29 -0400

    clean up

--- examples/astro/astro.config.mjs
+++ examples/astro/astro.config.mjs
@@ -1,12 +1,8 @@
-import { loadEnv } from "vite";
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import tailwindcss from "@tailwindcss/vite";
-import { getDotCMSHost } from "./src/utils/env";
-
-const env = loadEnv(process.env.NODE_ENV || "development", process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,7 +15,10 @@ export default defineConfig({
       proxy: {
         /* Proxy to dotCMS API for Assets (images, videos, etc.)
           Learn more: https://dev.dotcms.com/docs/image-resizing-and-processing#simpleShortyResize */
-        "/dA": getDotCMSHost(env),
+        "/dA": {
+          target: import.meta.env.PUBLIC_DOTCMS_HOST,
+          changeOrigin: true,
+        },
       },
     },
   },
