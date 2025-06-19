38fc91673e317324e3f1e2836276b471356a0e21
freddyDOTCMS <freddy0309@gmail.com>
2025-06-19 13:44:09 -0600

    Adding JSON serializer

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/analytics/content/util/AnalyticsEventsResultSerializer.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/analytics/content/util/AnalyticsEventsResultSerializer.java
@@ -10,7 +10,22 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Custom JSON serializer for {@link AnalyticsEventsResult} objects.
+ * This serializer converts AnalyticsEventsResult instances into a JSON representation
+ * with fields for status, success count, failed count, and error details.
+ * It handles special cases like omitting fields with -1 values and properly formatting error objects.
+ */
 public class AnalyticsEventsResultSerializer extends JsonSerializer<AnalyticsEventsResult>  {
+    /**
+     * Serializes an AnalyticsEventsResult object into JSON format.
+     * 
+     * @param analyticsEventsResult The AnalyticsEventsResult object to serialize
+     * @param jsonGenerator The JSON generator used for writing JSON content
+     * @param serializerProvider The serializer provider that can be used to get serializers for
+     *                          serializing Objects value contains, if any
+     * @throws IOException If an I/O error occurs during JSON generation
+     */
     @Override
     public void serialize(final AnalyticsEventsResult analyticsEventsResult,
                           final JsonGenerator jsonGenerator,
@@ -35,6 +50,15 @@ public class AnalyticsEventsResultSerializer extends JsonSerializer<AnalyticsEve
         jsonGenerator.writeObject(json);
     }
 
+    /**
+     * Converts an AnalyticsValidatorUtil.Error object to a Map representation.
+     * This method handles the conversion of error objects to a format suitable for JSON serialization.
+     * If the eventIndex is -1, it removes this field from the resulting map.
+     *
+     * @param error The error object to convert
+     * @return A Map containing the error information
+     * @throws RuntimeException If an error occurs during the conversion process
+     */
     private static Map<String, Object> getMap(AnalyticsValidatorUtil.Error error)  {
         try {
             final String errorJson = JsonUtil.getJsonStringFromObject(error);
