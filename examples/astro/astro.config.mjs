3c11653d67edb52854fab9823aa929ecce855035
Rafael Velazco <rjvelazco21@gmail.com>
2025-06-19 14:16:27 -0400

    feat(astro): Update Astro configuration to enhance TypeScript support and add Vercel rewrites

--- examples/astro/astro.config.mjs
+++ examples/astro/astro.config.mjs
@@ -13,12 +13,8 @@ export default defineConfig({
     server: {
       proxy: {
         /* Proxy to dotCMS API for Assets (images, videos, etc.)
-              Learn more: https://dev.dotcms.com/docs/image-resizing-and-processing#simpleShortyResize */
-        "/dA": {
-          target: process.env.PUBLIC_DOTCMS_HOST,
-          changeOrigin: true,
-          secure: true,
-        },
+        Learn more: https://dev.dotcms.com/docs/image-resizing-and-processing#simpleShortyResize */
+        "/dA": process.env.PUBLIC_DOTCMS_HOST || "http://localhost:8080"
       },
     },
   },
