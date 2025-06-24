f6bcdcac59c438cc654d9e7f567c1d2ded4f8e8a
Kevin Davila <144152756+KevinDavilaDotCMS@users.noreply.github.com>
2025-06-24 11:57:26 -0500

    chore(uve): Fixed save contentlets from palette content-types (#32514)
    https://github.com/user-attachments/assets/28e4e6d8-48ab-4e4b-85ac-7d48c68640c6
    
    Issue: Previously, we used a conditional check that, due to how it was
    written, didn’t properly handle the expected case:
    
    `if (!actionPayload || graphql)
    `
    
    In this condition, the evaluation wasn’t based on a strict boolean
    check, but rather on the truthiness of the graphql object. As a result,
    in headless mode—where graphql always had a value—the condition would
    always evaluate as true.
    
    This caused the current content to always be "reloaded," and the request
    to save the new contentlet was never actually made.
    
    Now, a preliminary evaluation is performed without needing to combine
    actionPayload and graphql in a single condition. All tests are still
    passing, and after manual testing, no issues were found as a result of
    this change.
    
    ---------
    
    Co-authored-by: Kevin <kfariid@gmail.com>

--- core-web/libs/portlets/edit-ema/portlet/src/lib/shared/models.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/shared/models.ts
@@ -212,6 +212,12 @@ export interface EditEmaDialogState {
     status: DialogStatus;
     url: string;
     type: DialogType;
+    /**
+     * Represent the action payload of the dialog, with the edited contentlet or the new contentlet
+     * that is being created, both inside PageAsset.
+     *
+     * Can be null when the dialog is opened outside of PageAsset, like from the Content API.
+     */
     actionPayload?: ActionPayload;
     form: DialogForm;
     clientAction: CLIENT_ACTIONS;
