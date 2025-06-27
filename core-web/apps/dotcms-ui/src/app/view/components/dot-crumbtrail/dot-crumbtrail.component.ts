e9e5199c3cf0d6e5dfb77de81efa340bdfe7ef09
Nicolas Molina Monroy <hi@nicobytes.com>
2025-06-27 11:45:08 -0400

    chore(dotcms-ui): Update breadcrumb component with new HTML structure and SCSS styles

--- core-web/apps/dotcms-ui/src/app/view/components/dot-crumbtrail/dot-crumbtrail.component.ts
+++ core-web/apps/dotcms-ui/src/app/view/components/dot-crumbtrail/dot-crumbtrail.component.ts
@@ -1,15 +1,35 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 
 import { DotCrumbtrailService } from './service/dot-crumbtrail.service';
 @Component({
     selector: 'dot-crumbtrail',
-    template: '<p-breadcrumb [model]="$model()" />',
+    templateUrl: './dot-crumbtrail.component.html',
     styleUrls: ['./dot-crumbtrail.component.scss']
 })
 export class DotCrumbtrailComponent {
     readonly #crumbTrailService = inject(DotCrumbtrailService);
-    $model = toSignal(this.#crumbTrailService.crumbTrail$, {
+    $breadcrumbsMenu = toSignal(this.#crumbTrailService.crumbTrail$, {
         initialValue: []
+    });
+
+    // Collapsed items: all except last
+    $collapsedBreadcrumbs = computed(() => {
+        const crumbs = this.$breadcrumbsMenu();
+
+        return crumbs.length > 1 ? crumbs.slice(0, -1) : [];
+    });
+
+    // Last item: the last breadcrumb
+    $lastBreadcrumb = computed(() => {
+        const crumbs = this.$breadcrumbsMenu();
+
+        const last = crumbs.length ? crumbs.at(-1) : null;
+
+        if (last) {
+            return last.label;
+        }
+
+        return null;
     });
 }
