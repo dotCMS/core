3a60d6fbe58f2f5fea1dfea4b9078ad265a46e71
Jalinson Diaz <zjaaaldev@gmail.com>
2025-07-02 19:50:06 -0300

    fix test

--- core-web/libs/portlets/dot-content-drive/portlet/src/lib/utils/functions.ts
+++ core-web/libs/portlets/dot-content-drive/portlet/src/lib/utils/functions.ts
@@ -7,7 +7,7 @@ export function decodeFilters(filters: string): Record<string, string> {
 
     return filtersArray.reduce(
         (acc, filter) => {
-            const [key, value] = filter.split(':');
+            const [key, value] = filter.split(':').map((part) => part.trim());
 
             // We have to handle the multiselector (,) but this is enough to pave the path for now
             acc[key] = value;
