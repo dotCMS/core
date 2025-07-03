ef4005fc4f53c557b9fbcdccc5e484467dbe07cd
Jalinson Diaz <zjaaaldev@gmail.com>
2025-07-03 10:50:33 -0300

    Update dot-content-drive.model.ts

--- core-web/libs/dotcms-models/src/lib/dot-content-drive.model.ts
+++ core-web/libs/dotcms-models/src/lib/dot-content-drive.model.ts
@@ -1,6 +1,5 @@
-// This will extend the DotCMSContentlet with the following properties, but for now we will just use the DotCMSContentlet
-
 import { DotCMSContentlet } from './dot-contentlet.model';
 
-// until we have folders on the request response
+// This will extend the DotCMSContentlet with more properties,
+// but for now we will just use the DotCMSContentlet until we have folders on the request response
 export type DotContentDriveItem = DotCMSContentlet;
