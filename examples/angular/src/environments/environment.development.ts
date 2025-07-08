94c70a8268325b836d22f1406822a2fac9fe5061
Kevin <kfariid@gmail.com>
2025-07-08 13:18:42 -0500

    Bring back hardcoded unnecessary code

--- examples/angular/src/environments/environment.development.ts
+++ examples/angular/src/environments/environment.development.ts
@@ -5,6 +5,6 @@ export const environment = {
      * for security purposes, make sure is read-only.
      * More info: https://www.dotcms.com/docs/latest/permissions
      */
-    authToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhcGkzYTM2OGNiZS1hOTc3LTRiNDItYmZkYS01NWE2YTYwOWE1ZTEiLCJ4bW9kIjoxNzUxOTk1NzMxMDAwLCJuYmYiOjE3NTE5OTU3MzEsImlzcyI6ImQzMjZhZDY4NGMiLCJsYWJlbCI6ImZyb250ZW5kIiwiZXhwIjoxODQ2NjQ1MjAwLCJpYXQiOjE3NTE5OTU3MzEsImp0aSI6IjVlYTgxM2FkLTBkNjEtNDU2ZS04YzY5LWM5Yzg3MjMzY2Q1NyJ9.BKbxa3ACQbrvx9rFO_q0LU-BWJLlHaYnHL7LlkHwfWM',
+    authToken: 'YOUR_AUTH_TOKEN',
     siteId: 'YOUR_SITE_ID'
 };
