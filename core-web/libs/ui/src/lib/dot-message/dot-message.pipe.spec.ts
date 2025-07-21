353537f665a086d5bdf852729de2ce44fb34eea5
Nicolas Molina Monroy <hi@nicobytes.com>
2025-07-21 11:34:02 -0400

    test(ui): enhance pipe tests for DotMessagePipe and DotSafeHtmlPipe with improved readability and maintainability

--- core-web/libs/ui/src/lib/dot-message/dot-message.pipe.spec.ts
+++ core-web/libs/ui/src/lib/dot-message/dot-message.pipe.spec.ts
@@ -6,47 +6,45 @@ import { MockDotMessageService } from '@dotcms/utils-testing';
 import { DotMessagePipe } from './dot-message.pipe';
 
 describe('DotMessagePipe', () => {
-  let spectator: SpectatorPipe<DotMessagePipe>;
+    let spectator: SpectatorPipe<DotMessagePipe>;
 
-  const mockMessages = {
-    'apps.search.placeholder': 'Search',
-    'apps.best': 'Test {0} {1}'
-  };
+    const mockMessages = {
+        'apps.search.placeholder': 'Search',
+        'apps.best': 'Test {0} {1}'
+    };
 
-  const messageServiceMock = new MockDotMessageService(mockMessages);
+    const messageServiceMock = new MockDotMessageService(mockMessages);
 
-  const createPipe = createPipeFactory({
-    pipe: DotMessagePipe,
-    providers: [
-        { provide: DotMessageService, useValue: messageServiceMock },
-    ]
-  });
-
-  it('should return empty string if param is undefined', () => {
-    spectator = createPipe(`{{ value | dm }}`, {
-      hostProps: { value: undefined }
+    const createPipe = createPipeFactory({
+        pipe: DotMessagePipe,
+        providers: [{ provide: DotMessageService, useValue: messageServiceMock }]
     });
-    expect(spectator.element.textContent).toBe('');
-  });
 
-  it('should return message requested', () => {
-    spectator = createPipe(`{{ value | dm }}`, {
-      hostProps: { value: 'apps.search.placeholder' }
+    it('should return empty string if param is undefined', () => {
+        spectator = createPipe(`{{ value | dm }}`, {
+            hostProps: { value: undefined }
+        });
+        expect(spectator.element.textContent).toBe('');
     });
-    expect(spectator.element.textContent).toBe('Search');
-  });
 
-  it('should return message requested with replaced values', () => {
-    spectator = createPipe(`{{ value | dm:args }}`, {
-      hostProps: { value: 'apps.best', args: ['Costa Rica', 'Panama'] }
+    it('should return message requested', () => {
+        spectator = createPipe(`{{ value | dm }}`, {
+            hostProps: { value: 'apps.search.placeholder' }
+        });
+        expect(spectator.element.textContent).toBe('Search');
     });
-    expect(spectator.element.textContent).toBe('Test Costa Rica Panama');
-  });
 
-  it('should return key if message not found', () => {
-    spectator = createPipe(`{{ value | dm }}`, {
-      hostProps: { value: 'no.label.found' }
+    it('should return message requested with replaced values', () => {
+        spectator = createPipe(`{{ value | dm:args }}`, {
+            hostProps: { value: 'apps.best', args: ['Costa Rica', 'Panama'] }
+        });
+        expect(spectator.element.textContent).toBe('Test Costa Rica Panama');
     });
-    expect(spectator.element.textContent).toBe('no.label.found');
-  });
+
+    it('should return key if message not found', () => {
+        spectator = createPipe(`{{ value | dm }}`, {
+            hostProps: { value: 'no.label.found' }
+        });
+        expect(spectator.element.textContent).toBe('no.label.found');
+    });
 });
--- core-web/libs/ui/src/lib/pipes/dot-safe-html/dot-safe-html.pipe.spec.ts
+++ core-web/libs/ui/src/lib/pipes/dot-safe-html/dot-safe-html.pipe.spec.ts
@@ -5,28 +5,28 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { DotSafeHtmlPipe } from './dot-safe-html.pipe';
 
 describe('DotSafeHtmlPipe', () => {
-  let spectator: SpectatorPipe<DotSafeHtmlPipe>;
-  let sanitizer: SpyObject<DomSanitizer>;
-  const safeHtml = 'safeHtml: <p>Hello World</p>';
+    let spectator: SpectatorPipe<DotSafeHtmlPipe>;
+    let sanitizer: SpyObject<DomSanitizer>;
+    const safeHtml = 'safeHtml: <p>Hello World</p>';
 
-  const createPipe = createPipeFactory({
-    pipe: DotSafeHtmlPipe,
-    providers: [
-      mockProvider(DomSanitizer, {
-        bypassSecurityTrustHtml:  jest.fn().mockReturnValue(safeHtml)
-      })
-    ]
-  });
-
-  it('should call DomSanitizer.bypassSecurityTrustHtml with value', () => {
-    const value = '<p>Hello World</p>';
-
-    spectator = createPipe(`{{ value | safeHtml }}`, {
-      hostProps: { value }
+    const createPipe = createPipeFactory({
+        pipe: DotSafeHtmlPipe,
+        providers: [
+            mockProvider(DomSanitizer, {
+                bypassSecurityTrustHtml: jest.fn().mockReturnValue(safeHtml)
+            })
+        ]
     });
-    sanitizer = spectator.inject(DomSanitizer);
-    spectator.detectChanges();
-    expect(sanitizer.bypassSecurityTrustHtml).toHaveBeenCalledWith(value);
-    expect(spectator.element.textContent).toBe(safeHtml);
-  });
+
+    it('should call DomSanitizer.bypassSecurityTrustHtml with value', () => {
+        const value = '<p>Hello World</p>';
+
+        spectator = createPipe(`{{ value | safeHtml }}`, {
+            hostProps: { value }
+        });
+        sanitizer = spectator.inject(DomSanitizer);
+        spectator.detectChanges();
+        expect(sanitizer.bypassSecurityTrustHtml).toHaveBeenCalledWith(value);
+        expect(spectator.element.textContent).toBe(safeHtml);
+    });
 });
