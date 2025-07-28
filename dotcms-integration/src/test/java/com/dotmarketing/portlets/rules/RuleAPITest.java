516cd9aaad8dad2b37e70dd46bf9b443fb7af594
Will Ezell <will@dotcms.com>
2025-07-28 17:00:56 -0400

    fix(validator):  rule conditionlet validation fixed, was broken in #32677
    ref: #32835

---
+++ dotcms-integration/src/test/java/com/dotmarketing/portlets/rules/RuleAPITest.java
@@ -0,0 +1,51 @@
+package com.dotmarketing.portlets.rules;
+
+import com.dotcms.util.IntegrationTestInitService;
+import com.dotmarketing.business.APILocator;
+import com.dotmarketing.portlets.rules.actionlet.RuleActionlet;
+import com.dotmarketing.portlets.rules.conditionlet.Conditionlet;
+import java.util.List;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RuleAPITest {
+
+
+  @BeforeClass
+  public static void prepare() throws Exception {
+    // Setting web app environment
+    IntegrationTestInitService.getInstance().init();
+
+    //Setting web app environment
+    IntegrationTestInitService.getInstance().init();
+
+  }
+
+
+  @Test
+  public void ensureConditionletsLoad() throws Exception {
+
+    List<Conditionlet<?>> conditionlets = APILocator.getRulesAPI().findConditionlets();
+
+    assert conditionlets.size() > 5;
+    assert conditionlets.stream().anyMatch(
+        c -> c.getClass().getName().equals("com.dotmarketing.portlets.rules.conditionlet.ReferringURLConditionlet"));
+
+
+  }
+
+  @Test
+  public void ensureActionletsLoad() throws Exception {
+
+    List<RuleActionlet> actionlets = APILocator.getRulesAPI().findActionlets();
+
+    assert actionlets.size() > 0;
+
+    assert actionlets.stream().anyMatch(
+        c -> c.getClass().getName().equals("com.dotmarketing.portlets.rules.actionlet.SendRedirectActionlet"));
+
+
+  }
+
+
+}
