7239a0b1d08aa1068ac1edb22abc48a9130738e2
Stephen Bolton <steve.bolton@dotcms.com>
2025-07-28 11:16:28 +0100

    update config

--- dotCMS/src/main/java/com/dotmarketing/db/DockerSecretDataSourceStrategy.java
+++ dotCMS/src/main/java/com/dotmarketing/db/DockerSecretDataSourceStrategy.java
@@ -1,6 +1,8 @@
 package com.dotmarketing.db;
 
 
+import static com.dotmarketing.db.DataSourceStrategyProvider.CONNECTION_DB_IDLE_TIMEOUT;
+
 import com.dotmarketing.util.Constants;
 import com.google.common.annotations.VisibleForTesting;
 import com.liferay.util.SystemEnvironmentProperties;
@@ -82,9 +84,10 @@ public class DockerSecretDataSourceStrategy implements DotDataSourceStrategy {
         config.setConnectionTimeout(Integer.parseInt(
                 dockerSecretsMap.getOrDefault(DataSourceStrategyProvider.CONNECTION_DB_CONNECTION_TIMEOUT, "5000")));
         config.setIdleTimeout(
-                Integer.parseInt(dockerSecretsMap.getOrDefault(
-                        DataSourceStrategyProvider.CONNECTION_DB_MIN_IDLE, "10"))
-                        * 1000);
+                Long.parseLong(
+                        systemEnvironmentProperties.getVariable(CONNECTION_DB_IDLE_TIMEOUT) != null
+                                ? systemEnvironmentProperties.getVariable(CONNECTION_DB_IDLE_TIMEOUT)
+                                : "600000"));
         config.setMaxLifetime(Integer.parseInt(
                 dockerSecretsMap.getOrDefault(DataSourceStrategyProvider.CONNECTION_DB_MAX_WAIT, "60000")));
         config.setConnectionTestQuery(dockerSecretsMap.get(
