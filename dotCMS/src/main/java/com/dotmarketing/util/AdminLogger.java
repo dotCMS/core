0cc891be4bef505821572a2a1ddd0d8979e074f4
Will Ezell <will@dotcms.com>
2025-08-06 10:27:14 -0400

    fix(logging): redirects AdminLogger to SecurityLogger
    ref: #32919

--- dotCMS/src/main/java/com/dotmarketing/util/AdminLogger.java
+++ dotCMS/src/main/java/com/dotmarketing/util/AdminLogger.java
@@ -1,34 +1,21 @@
 package com.dotmarketing.util;
 
-import com.dotmarketing.logConsole.model.LogMapper;
 import com.liferay.portal.model.User;
 
 
-/**
- * The AdminLogger class is used to log the large scale admin actions to admin-audit.log
- */
+
 public class AdminLogger {
 
     private static String filename = "dotcms-adminaudit.log";
 
     public static void log ( Class cl, String methodName, String msg ) {
 
-        if ( LogMapper.getInstance().isLogEnabled( filename ) ) {
-            Logger.info( cl, methodName + " : " + msg );
-            Logger.info( AdminLogger.class, cl.toString() + " : " + methodName + " : " + msg );
-        }
+        SecurityLogger.logInfo(cl, msg);
     }
 
     public static void log ( Class cl, String methodName, String msg, User user ) {
 
-        if ( user == null || user.getUserId() == null ) {
-            log( cl, methodName, msg );
-        } else {
-            if ( LogMapper.getInstance().isLogEnabled( filename ) ) {
-                Logger.info( cl, "UserId : " + user.getUserId() + " : " + methodName + " : " + msg );
-                Logger.info( AdminLogger.class, "UserId : " + user.getUserId() + " : " + cl.toString() + " : " + methodName + " : " + msg );
-            }
-        }
+        SecurityLogger.logInfo(cl, msg);
     }
 
 }
