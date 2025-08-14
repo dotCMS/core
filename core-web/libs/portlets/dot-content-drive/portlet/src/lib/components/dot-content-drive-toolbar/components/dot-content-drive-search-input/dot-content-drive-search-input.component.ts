4682b31bb47510359af549e4672ac32f8b01df70
Rafael Velazco <rjvelazco21@gmail.com>
2025-08-14 15:30:01 -0400

    feat(content-drive): add search input component to toolbar

--- core-web/libs/portlets/dot-content-drive/portlet/src/lib/components/dot-content-drive-toolbar/components/dot-content-drive-search-input/dot-content-drive-search-input.component.ts
+++ core-web/libs/portlets/dot-content-drive/portlet/src/lib/components/dot-content-drive-toolbar/components/dot-content-drive-search-input/dot-content-drive-search-input.component.ts
@@ -1,12 +1,17 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+
 import { DotContentDriveStore } from '../../../../store/dot-content-drive.store';
 
 @Component({
-    selector: 'dot-content-drive-tree-toggler',
+    selector: 'dot-content-drive-search-input',
     templateUrl: './dot-content-drive-search-input.component.html',
     styleUrl: './dot-content-drive-search-input.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [IconFieldModule, InputIconModule, InputTextModule]
 })
 export class DotContentDriveSearchInputComponent {
     #store = inject(DotContentDriveStore);
