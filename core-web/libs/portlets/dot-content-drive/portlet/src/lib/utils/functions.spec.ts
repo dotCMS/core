542da8aade6cd988d9e4d38920b43c130d19d76f
Jalinson Diaz <zjaaaldev@gmail.com>
2025-08-18 09:35:38 -0300

    fix import

--- core-web/libs/portlets/dot-content-drive/portlet/src/lib/utils/functions.spec.ts
+++ core-web/libs/portlets/dot-content-drive/portlet/src/lib/utils/functions.spec.ts
@@ -1,3 +1,5 @@
+import { describe, it, expect } from '@jest/globals';
+
 import { decodeFilters, encodeFilters, decodeByFilterKey } from './functions';
 
 import { DotContentDriveFilters } from '../shared/models';
