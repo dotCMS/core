73d78d4fe00c796951e3f8228266c4be5ca1c16a
Neeha2383 <neeha.kethi@dotcms.com>
2025-08-28 14:36:24 -0600

    Remove legacy content-types portlet from layouts
    Fixes #33063

---
+++ dotCMS/src/main/java/com/dotmarketing/startup/runonce/Task33063RemoveContentTypesLegacyPortletFromLayouts.java
@@ -0,0 +1,33 @@
+package com.dotmarketing.startup.runonce;
+
+import com.dotmarketing.business.CacheLocator;
+import com.dotmarketing.common.db.DotConnect;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotRuntimeException;
+import com.dotmarketing.startup.StartupTask;
+import io.vavr.control.Try;
+
+/**
+ * Removes the legacy content-types portlet from all layouts to prevent
+ * Portlet Exception Error when users try to search using the legacy tool.
+ * The new content-types-angular portlet should be used instead.
+ * @author Neeha kethi
+ */
+public class Task33063RemoveContentTypesLegacyPortletFromLayouts implements StartupTask {
+
+	@Override
+	public boolean forceRun() {
+		return Try.of(() -> new DotConnect()
+				.setSQL("select count(portlet_id) as count from cms_layouts_portlets where portlet_id = 'content-types'")
+				.getInt("count")).getOrElse(0) > 0;
+	}
+
+	@Override
+	public void executeUpgrade() throws DotDataException, DotRuntimeException {
+		new DotConnect()
+				.setSQL("delete from cms_layouts_portlets where portlet_id = 'content-types'")
+				.loadResult();
+
+		CacheLocator.getLayoutCache().clearCache();
+	}
+}
