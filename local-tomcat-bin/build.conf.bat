ec7a874ad5473aafe492b5cfb2649ffcb4e1fbde
daniel.solis <daniel.solis@dotcms.com>
2025-08-28 18:45:06 -0600

    chore(tomcat) : upgraded tomcat versiom to 8.5.50 for 5.1.6.k

---
+++ local-tomcat-bin/build.conf.bat
@@ -0,0 +1,5 @@
+set "HOME_FOLDER=webapps/ROOT"
+
+rem ## Set this property to deploy plugins to a different directory.  This is mostly useful when the dotCMS is hosted in an application server.
+rem ##In such a case, the location below must point to the root of the exploded dotCMS war.
+set "target.root=webapps/ROOT"
