6bde226d564bbd472d91b8ff88cd241f0c8a981f
hassandotcms <muhammad.hassan@dotcms.com>
2025-09-03 20:36:27 +0500

    modernize(tags): list tags api with pagination   - refactor: fix direction query param ref: #32960

--- dotCMS/src/main/java/com/dotcms/rest/api/v2/tags/TagResource.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v2/tags/TagResource.java
@@ -204,7 +204,7 @@ public class TagResource {
             UtilMethods.isSet(filter) ? filter : "",
             resolvedSiteId,
             global,
-            orderBy,
+            "DESC".equalsIgnoreCase(direction) ? "-" + orderBy : orderBy,
             offset,
             effectivePerPage
         );
