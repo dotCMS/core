f78ec18c2f634ffbb2f8d7a9ea1cf522486ab733
Rafael Velazco <rjvelazco21@gmail.com>
2025-09-07 22:22:41 -0400

    chore(CD): test buildTreeFolderNodes function

--- core-web/libs/portlets/dot-content-drive/portlet/src/lib/utils/tree-folder.utils.spec.ts
+++ core-web/libs/portlets/dot-content-drive/portlet/src/lib/utils/tree-folder.utils.spec.ts
@@ -2,6 +2,7 @@ import { DotFolder } from '@dotcms/data-access';
 
 import {
     ALL_FOLDER,
+    buildTreeFolderNodes,
     createTreeNode,
     generateAllParentPaths,
     TreeNodeItem
@@ -12,7 +13,7 @@ describe('Sidebar Utils', () => {
         it('should have correct structure', () => {
             expect(ALL_FOLDER).toEqual({
                 key: 'ALL_FOLDER',
-                label: 'ROOT',
+                label: 'Root',
                 loading: false,
                 data: {
                     type: 'folder',
@@ -203,6 +204,348 @@ describe('Sidebar Utils', () => {
 
             expect(result.parent).toBe(parentNode);
             expect(result.parent?.key).toBe('parent-456');
+        });
+    });
+
+    describe('buildTreeFolderNodes', () => {
+        // Mock data based on real example data
+        const mockFolderHierarchy: DotFolder[][] = [
+            [
+                {
+                    addChildrenAllowed: true,
+                    hostName: 'demo.dotcms.com',
+                    id: 'SYSTEM_FOLDER',
+                    path: '/'
+                },
+                {
+                    addChildrenAllowed: true,
+                    hostName: 'demo.dotcms.com',
+                    id: '513aec5b-3aaa-4df2-b306-83e77ba334d9',
+                    path: '/activities/'
+                },
+                {
+                    addChildrenAllowed: true,
+                    hostName: 'demo.dotcms.com',
+                    id: '83bb5752-4264-43c4-84c8-28176603431a',
+                    path: '/application/'
+                },
+                {
+                    addChildrenAllowed: true,
+                    hostName: 'demo.dotcms.com',
+                    id: 'fa455fb5-b961-4d0c-9e63-e79a8ba8622a',
+                    path: '/blog/'
+                },
+                {
+                    addChildrenAllowed: true,
+                    hostName: 'demo.dotcms.com',
+                    id: '2ad0dd36-5b07-41ac-b9f5-c7c54085ac58',
+                    path: '/images/'
+                }
+            ],
+            [
+                {
+                    addChildrenAllowed: true,
+                    hostName: 'demo.dotcms.com',
+                    id: '83bb5752-4264-43c4-84c8-28176603431a',
+                    path: '/application/'
+                },
+                {
+                    addChildrenAllowed: true,
+                    hostName: 'demo.dotcms.com',
+                    id: 'd4ab08ba-6ae6-4937-9fb4-b67d801ace72',
+                    path: '/application/apivtl/'
+                },
+                {
+                    addChildrenAllowed: true,
+                    hostName: 'demo.dotcms.com',
+                    id: 'c7eb5d4e72030ba98d6b78d2d2279cf8',
+                    path: '/application/block-editor/'
+                },
+                {
+                    addChildrenAllowed: true,
+                    hostName: 'demo.dotcms.com',
+                    id: 'b8a303ae-4cb4-40bf-9f27-b5b29b3350dc',
+                    path: '/application/containers/'
+                },
+                {
+                    addChildrenAllowed: true,
+                    hostName: 'demo.dotcms.com',
+                    id: '953db9f6-fc35-4d28-be2e-6124997ea3d9',
+                    path: '/application/templates/'
+                }
+            ]
+        ];
+
+        it('should handle empty folder hierarchy', () => {
+            const result = buildTreeFolderNodes([], '/test/');
+
+            expect(result.rootNodes).toEqual([]);
+            expect(result.selectedNode).toEqual(ALL_FOLDER);
+        });
+
+        it('should build tree structure for single level hierarchy', () => {
+            const singleLevel: DotFolder[][] = [
+                [
+                    {
+                        addChildrenAllowed: true,
+                        hostName: 'demo.dotcms.com',
+                        id: 'SYSTEM_FOLDER',
+                        path: '/'
+                    },
+                    {
+                        addChildrenAllowed: true,
+                        hostName: 'demo.dotcms.com',
+                        id: 'folder-1',
+                        path: '/test/'
+                    },
+                    {
+                        addChildrenAllowed: true,
+                        hostName: 'demo.dotcms.com',
+                        id: 'folder-2',
+                        path: '/other/'
+                    }
+                ]
+            ];
+
+            const result = buildTreeFolderNodes(singleLevel, '/test/');
+
+            expect(result.rootNodes).toHaveLength(2);
+            expect(result.rootNodes[0]).toEqual({
+                key: 'folder-1',
+                label: '/test/',
+                data: {
+                    id: 'folder-1',
+                    hostname: 'demo.dotcms.com',
+                    path: '/test/',
+                    type: 'folder'
+                },
+                leaf: false,
+                children: [],
+                expanded: true
+            });
+            expect(result.rootNodes[1]).toEqual({
+                key: 'folder-2',
+                label: '/other/',
+                data: {
+                    id: 'folder-2',
+                    hostname: 'demo.dotcms.com',
+                    path: '/other/',
+                    type: 'folder'
+                },
+                leaf: false
+            });
+            expect(result.selectedNode?.key).toBe('ALL_FOLDER');
+        });
+
+        it('should build complex tree structure with nested hierarchy', () => {
+            const result = buildTreeFolderNodes(mockFolderHierarchy, '/application/');
+
+            // Should have 4 root nodes (excluding the SYSTEM_FOLDER placeholder)
+            expect(result.rootNodes).toHaveLength(4);
+
+            // Check root nodes structure
+            expect(result.rootNodes.map((node) => node.key)).toEqual([
+                '513aec5b-3aaa-4df2-b306-83e77ba334d9', // activities
+                '83bb5752-4264-43c4-84c8-28176603431a', // application
+                'fa455fb5-b961-4d0c-9e63-e79a8ba8622a', // blog
+                '2ad0dd36-5b07-41ac-b9f5-c7c54085ac58' // images
+            ]);
+
+            // The application folder should be expanded and have children
+            const applicationNode = result.rootNodes.find(
+                (node) => node.key === '83bb5752-4264-43c4-84c8-28176603431a'
+            );
+            expect(applicationNode?.expanded).toBe(true);
+            expect(applicationNode?.children).toHaveLength(4);
+            expect(applicationNode?.children?.map((child) => child.key)).toEqual([
+                'd4ab08ba-6ae6-4937-9fb4-b67d801ace72', // apivtl
+                'c7eb5d4e72030ba98d6b78d2d2279cf8', // block-editor
+                'b8a303ae-4cb4-40bf-9f27-b5b29b3350dc', // containers
+                '953db9f6-fc35-4d28-be2e-6124997ea3d9' // templates
+            ]);
+
+            // Selected node should be the application folder
+            expect(result.selectedNode?.key).toBe('83bb5752-4264-43c4-84c8-28176603431a');
+            expect(result.selectedNode?.data.path).toBe('/application/');
+        });
+
+        it('should handle deeper nested path selection', () => {
+            const deepHierarchy: DotFolder[][] = [
+                [
+                    {
+                        addChildrenAllowed: true,
+                        hostName: 'demo.dotcms.com',
+                        id: 'SYSTEM_FOLDER',
+                        path: '/'
+                    },
+                    {
+                        addChildrenAllowed: true,
+                        hostName: 'demo.dotcms.com',
+                        id: 'level1-folder',
+                        path: '/level1/'
+                    }
+                ],
+                [
+                    {
+                        addChildrenAllowed: true,
+                        hostName: 'demo.dotcms.com',
+                        id: 'level1-folder',
+                        path: '/level1/'
+                    },
+                    {
+                        addChildrenAllowed: true,
+                        hostName: 'demo.dotcms.com',
+                        id: 'level2-folder',
+                        path: '/level1/level2/'
+                    }
+                ],
+                [
+                    {
+                        addChildrenAllowed: true,
+                        hostName: 'demo.dotcms.com',
+                        id: 'level2-folder',
+                        path: '/level1/level2/'
+                    },
+                    {
+                        addChildrenAllowed: true,
+                        hostName: 'demo.dotcms.com',
+                        id: 'level3-folder',
+                        path: '/level1/level2/level3/'
+                    }
+                ]
+            ];
+
+            const result = buildTreeFolderNodes(deepHierarchy, '/level1/level2/level3/');
+
+            // Should have 1 root node
+            expect(result.rootNodes).toHaveLength(1);
+
+            // Root node should be expanded with children
+            const rootNode = result.rootNodes[0];
+            expect(rootNode.key).toBe('level1-folder');
+            expect(rootNode.expanded).toBe(true);
+            expect(rootNode.children).toHaveLength(1);
+
+            // Level 2 should also be expanded with children
+            const level2Node = rootNode.children?.[0];
+            expect(level2Node?.key).toBe('level2-folder');
+            expect(level2Node?.expanded).toBe(true);
+            expect(level2Node?.children).toHaveLength(1);
+
+            // Level 3 should be the selected node
+            const level3Node = level2Node?.children?.[0];
+            expect(level3Node?.key).toBe('level3-folder');
+            // The selected node should be the last node that was found on the target path
+            expect(result.selectedNode?.key).toBe('level2-folder');
+        });
+
+        it('should return ALL_FOLDER as selected when target path does not match any folder', () => {
+            const result = buildTreeFolderNodes(mockFolderHierarchy, '/nonexistent/');
+
+            expect(result.rootNodes).toHaveLength(4);
+            expect(result.selectedNode).toEqual(ALL_FOLDER);
+        });
+
+        it('should handle root path selection', () => {
+            const rootHierarchy: DotFolder[][] = [
+                [
+                    {
+                        addChildrenAllowed: true,
+                        hostName: 'demo.dotcms.com',
+                        id: 'SYSTEM_FOLDER',
+                        path: '/'
+                    },
+                    {
+                        addChildrenAllowed: true,
+                        hostName: 'demo.dotcms.com',
+                        id: 'folder-1',
+                        path: '/test/'
+                    }
+                ]
+            ];
+
+            const result = buildTreeFolderNodes(rootHierarchy, '/');
+
+            expect(result.rootNodes).toHaveLength(1);
+            expect(result.selectedNode).toEqual(ALL_FOLDER);
+        });
+
+        it('should properly handle folder nodes that are not on target path', () => {
+            const result = buildTreeFolderNodes(mockFolderHierarchy, '/application/');
+
+            // Other root nodes should not be expanded
+            const activitiesNode = result.rootNodes.find(
+                (node) => node.key === '513aec5b-3aaa-4df2-b306-83e77ba334d9'
+            );
+            const blogNode = result.rootNodes.find(
+                (node) => node.key === 'fa455fb5-b961-4d0c-9e63-e79a8ba8622a'
+            );
+            const imagesNode = result.rootNodes.find(
+                (node) => node.key === '2ad0dd36-5b07-41ac-b9f5-c7c54085ac58'
+            );
+
+            expect(activitiesNode?.expanded).toBeUndefined();
+            expect(activitiesNode?.children).toBeUndefined();
+            expect(blogNode?.expanded).toBeUndefined();
+            expect(blogNode?.children).toBeUndefined();
+            expect(imagesNode?.expanded).toBeUndefined();
+            expect(imagesNode?.children).toBeUndefined();
+        });
+
+        it('should handle empty target path', () => {
+            const result = buildTreeFolderNodes(mockFolderHierarchy, '');
+
+            expect(result.rootNodes).toHaveLength(4);
+            expect(result.selectedNode).toEqual(ALL_FOLDER);
+        });
+
+        it('should correctly identify nodes on target path using generateAllParentPaths', () => {
+            const result = buildTreeFolderNodes(mockFolderHierarchy, '/application/');
+
+            // Verify that the correct node is identified as being on the target path
+            const applicationNode = result.rootNodes.find(
+                (node) => node.key === '83bb5752-4264-43c4-84c8-28176603431a'
+            );
+
+            expect(applicationNode?.expanded).toBe(true);
+            expect(applicationNode?.children).toBeDefined();
+
+            // Other nodes should not be on the path
+            const otherNodes = result.rootNodes.filter(
+                (node) => node.key !== '83bb5752-4264-43c4-84c8-28176603431a'
+            );
+
+            otherNodes.forEach((node) => {
+                expect(node.expanded).toBeUndefined();
+                expect(node.children).toBeUndefined();
+            });
+        });
+
+        it('should handle folder hierarchy with missing levels gracefully', () => {
+            const incompleteHierarchy: DotFolder[][] = [
+                [
+                    {
+                        addChildrenAllowed: true,
+                        hostName: 'demo.dotcms.com',
+                        id: 'SYSTEM_FOLDER',
+                        path: '/'
+                    },
+                    {
+                        addChildrenAllowed: true,
+                        hostName: 'demo.dotcms.com',
+                        id: 'folder-1',
+                        path: '/test/'
+                    }
+                ]
+                // Missing second level that would match /test/deep/
+            ];
+
+            const result = buildTreeFolderNodes(incompleteHierarchy, '/test/deep/');
+
+            expect(result.rootNodes).toHaveLength(1);
+            expect(result.rootNodes[0].key).toBe('folder-1');
+            expect(result.rootNodes[0].expanded).toBe(true);
+            expect(result.selectedNode?.key).toBe('ALL_FOLDER');
         });
     });
 });
