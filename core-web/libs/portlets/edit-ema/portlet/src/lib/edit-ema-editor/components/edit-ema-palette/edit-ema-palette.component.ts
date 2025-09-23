359bb0a3b56fadc3fb019075fc9af5c31bc1c4b4
Rafael Velazco <rjvelazco21@gmail.com>
2025-09-23 12:24:27 -0400

    feat(Content Palette): Update contentlet list when language changes

--- core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/edit-ema-palette/edit-ema-palette.component.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/edit-ema-palette/edit-ema-palette.component.ts
@@ -4,10 +4,12 @@ import { AsyncPipe } from '@angular/common';
 import {
     ChangeDetectionStrategy,
     Component,
-    Input,
     OnDestroy,
     OnInit,
-    inject
+    effect,
+    inject,
+    input,
+    untracked
 } from '@angular/core';
 
 import { DotESContentService } from '@dotcms/data-access';
@@ -26,19 +28,30 @@ import { DotPaletteStore, PALETTE_TYPES } from './store/edit-ema-palette.store';
     providers: [DotESContentService, DotPaletteStore]
 })
 export class EditEmaPaletteComponent implements OnInit, OnDestroy {
-    @Input() languageId: number;
-    @Input() variantId: string;
-    @Input() containers: DotCMSPageAssetContainers;
+    languageId = input.required<number>();
+    variantId = input.required<string>();
+    containers = input.required<DotCMSPageAssetContainers>();
 
-    private readonly store = inject(DotPaletteStore);
+    readonly #store = inject(DotPaletteStore);
     private destroy$ = new Subject<void>();
 
-    readonly vm$ = this.store.vm$;
+    protected readonly vm$ = this.#store.vm$;
+    protected readonly PALETTE_TYPES_ENUM = PALETTE_TYPES;
 
-    PALETTE_TYPES_ENUM = PALETTE_TYPES;
+    readonly UPDATE_LANGUAGE_EFFECT = effect(() => {
+        const { currentPaletteType } = untracked(() => this.#store.state()) || {};
+        const variantId = untracked(() => this.variantId());
+        const languageId = this.languageId();
+
+        if (currentPaletteType === PALETTE_TYPES.CONTENTTYPE) {
+            return;
+        }
+
+        this.#store.reloadContentlets({ languageId, variantId });
+    });
 
     ngOnInit() {
-        this.store.loadAllowedContentTypes({ containers: this.containers });
+        this.#store.loadAllowedContentTypes({ containers: this.containers() });
     }
 
     /**
@@ -47,11 +60,11 @@ export class EditEmaPaletteComponent implements OnInit, OnDestroy {
      * @param contentTypeName - The name of the content type.
      */
     showContentletsFromContentType(contentTypeName: string) {
-        this.store.loadContentlets({
+        this.#store.loadContentlets({
             filter: '',
-            languageId: this.languageId.toString(),
+            languageId: this.languageId().toString(),
             contenttypeName: contentTypeName,
-            variantId: this.variantId
+            variantId: this.variantId()
         });
     }
 
@@ -59,7 +72,7 @@ export class EditEmaPaletteComponent implements OnInit, OnDestroy {
      * Shows the content types in the palette.
      */
     showContentTypes() {
-        this.store.resetContentlets();
+        this.#store.resetContentlets();
     }
 
     /**
@@ -69,28 +82,28 @@ export class EditEmaPaletteComponent implements OnInit, OnDestroy {
      * @memberof EditEmaPaletteComponent
      */
     onPaginate({ contentTypeVarName, page }) {
-        this.store.loadContentlets({
+        this.#store.loadContentlets({
             filter: '',
-            languageId: this.languageId.toString(),
+            languageId: this.languageId().toString(),
             contenttypeName: contentTypeVarName,
             page: page,
-            variantId: this.variantId
+            variantId: this.variantId()
         });
     }
 
     loadContentTypes(filter: string, allowedContent: string[]) {
-        this.store.loadContentTypes({
+        this.#store.loadContentTypes({
             filter,
             allowedContent
         });
     }
 
     loadContentlets(filter: string, currentContentType: string) {
-        this.store.loadContentlets({
+        this.#store.loadContentlets({
             filter,
             contenttypeName: currentContentType,
-            languageId: this.languageId.toString(),
-            variantId: this.variantId
+            languageId: this.languageId().toString(),
+            variantId: this.variantId()
         });
     }
 
