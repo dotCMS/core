ef76958c62ab0fb802d27d5b4474d51876a385a5
Freddy Montes <751424+fmontes@users.noreply.github.com>
2025-09-23 17:08:14 -0600

    feat(ng-ssr): integrate Tailwind CSS for styling
    - Added .postcssrc.json for Tailwind CSS configuration.
    - Updated package.json to include Tailwind CSS and PostCSS dependencies.
    - Modified styles.css to import Tailwind CSS for global styles.
    - Enhanced app.config.ts with new HTTP transfer cache options for improved performance.
    
    These changes enhance the styling capabilities and performance of the application.

--- examples/angular-ssr/src/app/app.config.ts
+++ examples/angular-ssr/src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+  withHttpTransferCacheOptions,
+} from '@angular/platform-browser';
 import { provideDotCMSClient, provideDotCMSImageLoader } from '@dotcms/angular';
 import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { AngularHttpClient } from './angular-httpclient';
@@ -12,13 +20,20 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(withFetch()),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(routes),
+    provideClientHydration(
+      withEventReplay(),
+      withHttpTransferCacheOptions({
+        includePostRequests: true,
+        includeRequestsWithAuthHeaders: true,
+      })
+    ),
     provideDotCMSClient({
       dotcmsUrl: 'http://localhost:8080',
       authToken:
-          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhcGk0NmRiNjExMi01YTQxLTRlYTQtODEzNC04ZDEzMDA4NTA0Y2QiLCJ4bW9kIjoxNzU4NjM5ODk5MDAwLCJuYmYiOjE3NTg2Mzk4OTksImlzcyI6ImQ2MWM5Y2I2OTAiLCJsYWJlbCI6InRva2VuIiwiZXhwIjoxODUzMzAxNjAwLCJpYXQiOjE3NTg2Mzk4OTksImp0aSI6IjJhYzE3MGQ5LWE5OTQtNGNjNC04YmY0LTkzNjRmN2UzYzRiZCJ9.IfYSpCEv0sAYWWjWSK96norKYSUvRvqwJWHyRosei9k',
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhcGk0NmRiNjExMi01YTQxLTRlYTQtODEzNC04ZDEzMDA4NTA0Y2QiLCJ4bW9kIjoxNzU4NjM5ODk5MDAwLCJuYmYiOjE3NTg2Mzk4OTksImlzcyI6ImQ2MWM5Y2I2OTAiLCJsYWJlbCI6InRva2VuIiwiZXhwIjoxODUzMzAxNjAwLCJpYXQiOjE3NTg2Mzk4OTksImp0aSI6IjJhYzE3MGQ5LWE5OTQtNGNjNC04YmY0LTkzNjRmN2UzYzRiZCJ9.IfYSpCEv0sAYWWjWSK96norKYSUvRvqwJWHyRosei9k',
       siteId: 'YOUR_SITE_ID',
-       httpClient: (http: HttpClient) => new AngularHttpClient(http)
-  })
-  ]
+      httpClient: (http: HttpClient) => new AngularHttpClient(http),
+    }),
+  ],
 };
