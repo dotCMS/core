0bcdedf8d6f8ca1cad36408a6ff2b6d3363f0973
Freddy Montes <751424+fmontes@users.noreply.github.com>
2025-09-25 05:35:06 -0600

    feat(ng-ssr): add footer component and enhance app layout
    - Introduced FooterComponent with dynamic content for blogs and destinations.
    - Updated App component to fetch and manage content data, including navigation and logo.
    - Enhanced app.html to include the new footer and improved navigation handling.
    
    These changes improve the overall layout and user experience of the application.

--- examples/angular-ssr/src/app/app.ts
+++ examples/angular-ssr/src/app/app.ts
@@ -4,32 +4,90 @@ import { RouterOutlet } from '@angular/router';
 import { DotCMSComposedPageResponse, DotCMSNavigationItem } from '@dotcms/types';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Blog, Destination, FileAsset } from './dotcms/types/contentlet.model';
 
-type PageResponse = { content: { navigation: DotCMSNavigationItem } };
+type PageResponse = {
+  content: {
+    navigation: DotCMSNavigationItem;
+    logoImage: FileAsset[];
+    blogs: Blog[];
+    destinations: Destination[];
+  };
+};
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, NavigationComponent],
+  imports: [RouterOutlet, HeaderComponent, NavigationComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   private readonly http = inject(HttpClient);
 
-  navigation = signal<DotCMSNavigationItem[]>([]);
+  content = signal<PageResponse['content'] | undefined>(undefined);
 
   ngOnInit() {
     const pageParams = {
       graphql: {
         content: {
-          navigation: `
-              DotNavigation(uri: "/", depth: 2) {
-                  children {
-                      folder
-                      href
-                      title
+          blogs: `
+            search(query: "+contenttype:Blog +live:true", limit: 10) {
+              title
+              identifier
+              ... on Blog {
+                inode
+                image {
+                  fileName
+                  fileAsset {
+                    versionPath
                   }
+                }
+                urlMap
+                modDate
+                urlTitle
+                teaser
+                author {
+                  firstName
+                  lastName
+                  inode
+                }
               }
+            }
+          `,
+          destinations: `
+            search(query: "+contenttype:Destination +live:true", limit: 10) {
+              title
+              identifier
+              ... on Destination {
+                inode
+                image {
+                  fileName
+                  fileAsset {
+                    versionPath
+                  }
+                }
+                urlMap
+                modDate
+                url
+              }
+            }
+          `,
+          logoImage: `
+            FileAssetCollection(query: "+title:logo.png") {
+              fileAsset {
+                  versionPath
+              }
+            }
+          `,
+          navigation: `
+            DotNavigation(uri: "/", depth: 2) {
+              children {
+                folder
+                href
+                title
+              }
+            }
           `,
         },
       },
@@ -38,7 +96,7 @@ export class App {
     this.http
       .post<DotCMSComposedPageResponse<PageResponse>>('/api/page', { url: '/', params: pageParams })
       .subscribe((response) => {
-        this.navigation.set(response?.content?.navigation.children || []);
+        this.content.set(response?.content);
       });
   }
 }
