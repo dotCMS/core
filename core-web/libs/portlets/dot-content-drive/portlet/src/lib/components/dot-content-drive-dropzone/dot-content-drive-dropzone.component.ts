cd6d5f8023bbab56e4d10c593215bfc8bc3f3d2b
Jalinson Diaz <zjaaaldev@gmail.com>
2025-10-01 19:17:54 -0300

    feedback

--- core-web/libs/portlets/dot-content-drive/portlet/src/lib/components/dot-content-drive-dropzone/dot-content-drive-dropzone.component.ts
+++ core-web/libs/portlets/dot-content-drive/portlet/src/lib/components/dot-content-drive-dropzone/dot-content-drive-dropzone.component.ts
@@ -4,17 +4,20 @@ import {
     ChangeDetectionStrategy,
     Component,
     effect,
+    ElementRef,
     HostBinding,
-    HostListener
+    HostListener,
+    inject
 } from '@angular/core';
 
 import { DotMessagePipe } from '@dotcms/ui';
 import { WINDOW } from '@dotcms/utils';
 
+import { DotContentDriveStore } from '../../store/dot-content-drive.store';
+
 type DotContentDriveDropzoneState = {
     active: boolean;
     files?: FileList;
-    invalidFiles?: FileList;
 };
 
 @Component({
@@ -31,16 +34,19 @@ type DotContentDriveDropzoneState = {
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DotContentDriveDropzoneComponent {
+    readonly #elementRef = inject(ElementRef);
     readonly #state = signalState<DotContentDriveDropzoneState>({
-        invalidFiles: undefined,
         active: false,
         files: undefined
     });
+
+    readonly #store = inject(DotContentDriveStore);
 
     @HostBinding('class.active') get active() {
         return this.#state.active();
     }
 
+    // Effect to upload the files when the dropzone is active
     uploadFilesEffect = effect(() => {
         const files = this.#state.files();
         if (!files || !files?.length) {
@@ -52,6 +58,10 @@ export class DotContentDriveDropzoneComponent {
         // this.uploadFiles(files);
     });
 
+    /**
+     * @description Set the dropzone as active when the drag enters the dropzone
+     * @param event - DragEvent
+     */
     @HostListener('dragenter', ['$event'])
     onDragEnter(event: DragEvent & { fromElement: HTMLElement }) {
         event.stopPropagation();
@@ -65,53 +75,58 @@ export class DotContentDriveDropzoneComponent {
             active: true
         });
 
-        // if (
-        //     validFilesMasks.some((mask) => files.some((file) => file.name.includes(mask)))
-        // ) {
-        //     this.#state.invalidFiles.set(invalidFiles);
-        //     return;
-        // } else {
-        //     this.#state.invalidFiles.set([]);
-        // }// if (
-        //     validFilesMasks.some((mask) => files.some((file) => file.name.includes(mask)))
-        // ) {
-        //     this.#state.invalidFiles.set(invalidFiles);
-        //     return;
-        // } else {
-        //     this.#state.invalidFiles.set([]);
-        // }
+        // Reset the context menu
+        this.#store.resetContextMenu();
     }
 
+    /**
+     * @description Prevent the default behavior to allow drop and not opening the file in the browser
+     * @param event - DragEvent
+     */
     @HostListener('dragover', ['$event'])
     onDragOver(event: DragEvent) {
         event.stopPropagation();
         event.preventDefault();
     }
 
+    /**
+     * @description Set the dropzone as inactive when the drag leaves the dropzone
+     * @param event - DragEvent
+     */
     @HostListener('dragleave', ['$event'])
     onDragLeave(event: DragEvent) {
         event.preventDefault();
 
-        if (event.relatedTarget) {
-            return;
+        // Check if the relatedTarget (where the drag is going) is still within the dropzone
+        const relatedTarget = event.relatedTarget as Node;
+        if (relatedTarget && this.#elementRef.nativeElement.contains(relatedTarget)) {
+            return; // Still within the dropzone, don't deactivate
         }
 
+        // Drag has left the dropzone
         patchState(this.#state, {
             files: undefined,
-            active: false,
-            invalidFiles: undefined
+            active: false
         });
     }
 
+    /**
+     * @description Set the dropzone as inactive when the drag ends
+     * @param event - DragEvent
+     */
     @HostListener('dragend', ['$event'])
     onDragEnd(event: DragEvent) {
         event.preventDefault();
         patchState(this.#state, {
             files: undefined,
-            active: false,
-            invalidFiles: undefined
+            active: false
         });
     }
+
+    /**
+     * @description Set the dropzone as inactive when the drag ends on the dropzone
+     * @param event - DragEvent
+     */
 
     @HostListener('drop', ['$event'])
     onDrop(event: DragEvent) {
