b939dab56f52c760c06e5cbcae3cbea644d07ccf
Rafael Velazco <rjvelazco21@gmail.com>
2025-10-22 16:20:11 -0400

    style: Refactor UVE Palette List with search and sort controls, enhance layout

--- core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-palette/components/dot-uve-palette-list/dot-uve-palette-list.component.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-palette/components/dot-uve-palette-list/dot-uve-palette-list.component.ts
@@ -1,5 +1,10 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+
 import { DotCMSContentType } from '@dotcms/dotcms-models';
 
 import { MOCK_CONTENT_TYPES } from '../../utils';
@@ -7,7 +12,13 @@ import { DotUvePaletteItemComponent } from '../dot-uve-palette-item/dot-uve-pale
 
 @Component({
     selector: 'dot-uve-palette-list',
-    imports: [DotUvePaletteItemComponent],
+    imports: [
+        DotUvePaletteItemComponent,
+        ButtonModule,
+        IconFieldModule,
+        InputIconModule,
+        InputTextModule
+    ],
     templateUrl: './dot-uve-palette-list.component.html',
     styleUrl: './dot-uve-palette-list.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
