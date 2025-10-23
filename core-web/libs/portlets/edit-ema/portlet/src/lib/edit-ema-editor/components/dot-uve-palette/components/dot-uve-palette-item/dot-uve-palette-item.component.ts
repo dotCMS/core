d2ddfb4878750767858a29559587e1b9c2c1daa3
Rafael Velazco <rjvelazco21@gmail.com>
2025-10-23 13:09:04 -0400

    refactor(dot-uve-palette-item): Simplify HTML structure and enhance styling for grid and list views

--- core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-palette/components/dot-uve-palette-item/dot-uve-palette-item.component.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-palette/components/dot-uve-palette-item/dot-uve-palette-item.component.ts
@@ -1,15 +1,37 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, input } from '@angular/core';
 
 import { DotCMSContentType } from '@dotcms/dotcms-models';
 
 @Component({
     selector: 'dot-uve-palette-item',
-    imports: [CommonModule],
+    imports: [],
     templateUrl: './dot-uve-palette-item.component.html',
     styleUrl: './dot-uve-palette-item.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    host: {
+        '[attr.data-type]': '"content-type"',
+        '[attr.draggable]': 'true'
+    }
 })
 export class DotUvePaletteItemComponent {
+    $view = input<'grid' | 'list'>('grid', { alias: 'view' });
     $contentType = input.required<DotCMSContentType>({ alias: 'contentType' });
+
+    @HostBinding('class.list-view')
+    get isListView() {
+        return this.$view() === 'list';
+    }
+
+    @HostBinding('attr.data-item')
+    get dataItem() {
+        const contentType = this.$contentType();
+        return JSON.stringify({
+            contentType: {
+                variable: contentType.variable,
+                name: contentType.name,
+                baseType: contentType.baseType
+            },
+            move: false
+        });
+    }
 }
