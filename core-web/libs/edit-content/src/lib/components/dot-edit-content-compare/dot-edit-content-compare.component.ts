e967a2af2a667c6a044cf57ab14a3622cc9beb67
Humberto Morera <humberto.morera@dotcms.com>
2025-10-23 09:22:41 -0600

    remove migrated module in DotEditContentCompareComponent

--- core-web/libs/edit-content/src/lib/components/dot-edit-content-compare/dot-edit-content-compare.component.ts
+++ core-web/libs/edit-content/src/lib/components/dot-edit-content-compare/dot-edit-content-compare.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
 import { ButtonModule } from 'primeng/button';
 
-import { DotContentCompareModule } from '@dotcms/portlets/dot-ema/ui';
+import { DotContentCompareComponent } from '@dotcms/portlets/dot-ema/ui';
 import { DotMessagePipe } from '@dotcms/ui';
 
 import { DotEditContentStore } from '../../store/edit-content.store';
@@ -10,7 +10,7 @@ import { DotEditContentStore } from '../../store/edit-content.store';
 @Component({
     selector: 'dot-edit-content-compare',
     standalone: true,
-    imports: [ButtonModule, DotContentCompareModule, DotMessagePipe],
+    imports: [ButtonModule, DotContentCompareComponent, DotMessagePipe],
     templateUrl: './dot-edit-content-compare.component.html',
     styleUrls: ['./dot-edit-content-compare.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
