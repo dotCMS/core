42bb8a96bc5b48da57956e8820e08335d3f68361
Jalinson Diaz <zjaaaldev@gmail.com>
2025-10-23 11:22:24 -0300

    Update dot-uve-palette.component.ts

--- core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-palette/dot-uve-palette.component.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-palette/dot-uve-palette.component.ts
@@ -2,13 +2,11 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 import { TabViewModule } from 'primeng/tabview';
 
-import { DotMessagePipe } from '@dotcms/ui';
-
 import { DotUvePaletteListComponent } from './components/dot-uve-palette-list/dot-uve-palette-list.component';
 
 @Component({
     selector: 'dot-uve-palette',
-    imports: [TabViewModule, DotMessagePipe, DotUvePaletteListComponent],
+    imports: [TabViewModule, DotUvePaletteListComponent],
     templateUrl: './dot-uve-palette.component.html',
     styleUrl: './dot-uve-palette.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
