0704ea750fa1443e1b26088afdf92dfd01aabbb9
Rafael Velazco <rjvelazco21@gmail.com>
2025-10-23 10:42:20 -0400

    feat: Update DotUvePaletteList to utilize input properties for content types and pagination, enhancing data handling and pagination logic

--- core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-palette/components/dot-uve-palette-list/dot-uve-palette-list.component.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-palette/components/dot-uve-palette-list/dot-uve-palette-list.component.ts
@@ -1,6 +1,4 @@
-import { patchState, signalState } from '@ngrx/signals';
-
-import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 
 import { ButtonModule } from 'primeng/button';
 import { IconFieldModule } from 'primeng/iconfield';
@@ -8,10 +6,9 @@ import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
 import { PaginatorModule, PaginatorState } from 'primeng/paginator';
 
-import { DotCMSContentType } from '@dotcms/dotcms-models';
+import { DotCMSContentType, DotPagination } from '@dotcms/dotcms-models';
 import { DotMessagePipe } from '@dotcms/ui';
 
-import { MOCK_CONTENT_TYPES } from '../../utils';
 import { DotUvePaletteItemComponent } from '../dot-uve-palette-item/dot-uve-palette-item.component';
 
 @Component({
@@ -30,35 +27,21 @@ import { DotUvePaletteItemComponent } from '../dot-uve-palette-item/dot-uve-pale
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DotUvePaletteListComponent {
-    $contentTypes = signal<DotCMSContentType[]>([
-        ...MOCK_CONTENT_TYPES,
-        ...MOCK_CONTENT_TYPES,
-        ...MOCK_CONTENT_TYPES,
-        ...MOCK_CONTENT_TYPES
-    ]);
-
-    readonly state = signalState({
-        $first: 0,
-        $rows: 20,
-        $totalRecords: this.$contentTypes().length
-    });
-
-    readonly $first = this.state.$first;
-    readonly $rows = this.state.$rows;
-    readonly $totalRecords = this.state.$totalRecords;
+    $contentTypes = input.required<DotCMSContentType[]>({ alias: 'contentTypes' });
+    $pagination = input.required<DotPagination>({ alias: 'pagination' });
 
     // Computed signal for paginated content types
-    readonly $paginatedContentTypes = computed(() => {
-        const start = this.state.$first();
-        const end = start + this.state.$rows();
-        return this.$contentTypes().slice(start, end);
+    readonly $start = computed(() => {
+        return (this.$pagination().currentPage - 1) * this.$pagination().perPage;
+    });
+    readonly $rowsPerPage = computed(() => {
+        return this.$pagination().perPage;
+    });
+    readonly $totalRecords = computed(() => {
+        return this.$pagination().totalEntries;
     });
 
-    onPageChange(event: PaginatorState) {
-        patchState(this.state, {
-            $first: event.first,
-            $rows: event.rows,
-            $totalRecords: this.$contentTypes().length
-        });
+    onPageChange(_event: PaginatorState) {
+        // Do nothing for now
     }
 }
