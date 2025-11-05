2a0f854c7e3caf5a425641a4d6f5921310721be2
erickgonzalez <erick.gonzalez@dotcms.com>
2025-11-05 10:41:36 -0600

    refactor(telemetry): remove `ExperimentFeatureFlagMetricType` and add `ImportContentletsJobTriggeredMetricType`
    ### Changes:
    - Removed `ExperimentFeatureFlagMetricType` and its related test.
    - Replaced it with `ImportContentletsJobTriggeredMetricType` in telemetry metrics.
    - Added debug logging for client and snapshot information in telemetry.
    - Improved telemetry data capture with enhanced logging and new metric.

---
+++ dotCMS/src/main/java/com/dotcms/telemetry/collectors/content/ImportContentletsJobTriggeredMetricType.java
@@ -0,0 +1,32 @@
+package com.dotcms.telemetry.collectors.content;
+
+import com.dotcms.telemetry.MetricCategory;
+import com.dotcms.telemetry.MetricFeature;
+import com.dotcms.telemetry.collectors.DBMetricType;
+
+public class ImportContentletsJobTriggeredMetricType implements DBMetricType {
+    @Override
+    public String getName() {
+        return "IMPORT_CONTENTLETS_JOB_TRIGGERED";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Customer have used the Import Contentlets Job";
+    }
+
+    @Override
+    public MetricCategory getCategory() {
+        return MetricCategory.DIFFERENTIATING_FEATURES;
+    }
+
+    @Override
+    public MetricFeature getFeature() {
+        return MetricFeature.CONTENTLETS;
+    }
+
+    @Override
+    public String getSqlQuery() {
+        return "select * from job where queue_name='importContentlets'";
+    }
+}
