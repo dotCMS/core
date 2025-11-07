80e10964f0975a578429a39caa21b3a6934eb2ab
Will Ezell <will@dotcms.com>
2025-11-07 09:37:27 -0500

    task(db): Update db.properties values to lower the number of default idle connections. (#33675)
    ## Summary
    This Pull Request updates database connection pool configurations in the
    db.properties file to adjust connection timeout, pool size, and
    connection lifetime.
    
    ### Main Changes
    
    - Updated connectionTimeout from 3000ms to 5000ms for new connections.
    - Decreased minimumIdle connections from 10 to 3.
    - Kept maximumPoolSize the same at 60.
    - Increased maxLifetime for connections from 120000ms (2 minutes) to
    900000ms (15 minutes).
    - Retained leakDetectionThreshold at 60000ms.
    
    
    ref: #33674

--- dotCMS/src/main/resources/container/tomcat9/bin/setenv.sh
+++ dotCMS/src/main/resources/container/tomcat9/bin/setenv.sh
@@ -28,10 +28,23 @@ export DB_USERNAME=${DB_USERNAME:-"dotcmsdbuser"}
 export DB_PASSWORD=${DB_PASSWORD:-"password"}
 export DB_HOST=${DB_HOST:-"db.dotcms.site"}
 export DB_NAME=${DB_NAME:-"dotcms"}
-export DB_MAX_WAIT=${DB_MAX_WAIT:-"180000"}
+
+# Max Connection Lifetime 15m
+export DB_MAX_WAIT=${DB_MAX_WAIT:-"900000"}
+
+# Min Idle Connections
+export DB_MIN_IDLE=${DB_MIN_IDLE:-"3"}
+
+# Max Connections
 export DB_MAX_TOTAL=${DB_MAX_TOTAL:-"200"}
+
+# Try new Connection Timeout - 5s
 export DB_CONNECTION_TIMEOUT=${DB_CONNECTION_TIMEOUT:-"5000"}
-export DB_MIN_IDLE=${DB_MIN_IDLE:-"10"}
+
+# remove idle connections after 5m
+export DB_IDLE_TIMEOUT=${DB_IDLE_TIMEOUT:-"300000"}
+
+
 export DB_VALIDATION_QUERY=${DB_VALIDATION_QUERY:-""}
 export DB_LEAK_DETECTION_THRESHOLD=${DB_LEAK_DETECTION_THRESHOLD:-"300000"}
 export DB_DEFAULT_TRANSACTION_ISOLATION=${DB_DEFAULT_TRANSACTION_ISOLATION:-""}
