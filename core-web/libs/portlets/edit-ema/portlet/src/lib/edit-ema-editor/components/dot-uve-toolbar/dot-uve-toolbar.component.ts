950e8611a8064dde381339baf38599c1cebe90b6
Kevin <kfariid@gmail.com>
2025-11-20 13:26:29 -0500

    Fixed gap between dates on FTM datepicker

--- core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-toolbar/dot-uve-toolbar.component.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-toolbar/dot-uve-toolbar.component.ts
@@ -41,7 +41,7 @@ import { EditEmaPersonaSelectorComponent } from './components/edit-ema-persona-s
 
 import { DEFAULT_DEVICES, DEFAULT_PERSONA, PERSONA_KEY } from '../../../shared/consts';
 import { UVEStore } from '../../../store/dot-uve.store';
-import { convertLocalTimeToUTC, createFullURL } from '../../../utils';
+import { convertLocalTimeToUTC, convertUTCToLocalTime, createFullURL } from '../../../utils';
 
 @Component({
     selector: 'dot-uve-toolbar',
@@ -110,8 +110,7 @@ export class DotUveToolbarComponent {
     protected readonly $pageParams = this.#store.pageParams;
     protected readonly $previewDate = computed<Date>(() => {
         const publishDate = this.$pageParams().publishDate;
-
-        const previewDate = publishDate ? new Date(publishDate) : new Date();
+        const previewDate = publishDate ? convertUTCToLocalTime(new Date(publishDate)) : new Date();
 
         return previewDate;
     });
@@ -151,9 +150,9 @@ export class DotUveToolbarComponent {
      */
     protected fetchPageOnDate(publishDate: Date = new Date()) {
         const publishDateUTC = convertLocalTimeToUTC(publishDate);
-
+        
         this.#store.trackUVECalendarChange({ selectedDate: publishDateUTC });
-
+        
         this.#store.loadPageAsset({
             mode: UVE_MODE.LIVE,
             publishDate: publishDateUTC
