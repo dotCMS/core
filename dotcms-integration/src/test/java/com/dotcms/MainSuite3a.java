c0238503756ff5ac5120c006139242310d01e40f
Will Ezell <will@dotcms.com>
2025-11-25 09:56:34 -0500

    Update MainSuite3a.java
    fixing merge error

--- dotcms-integration/src/test/java/com/dotcms/MainSuite3a.java
+++ dotcms-integration/src/test/java/com/dotcms/MainSuite3a.java
@@ -48,7 +48,7 @@ import org.junit.runners.Suite;
         Task250826AddIndexesToUniqueFieldsTableTest.class,
         SecondaryCategoryPermissionTest.class,
         RequestCostReportTest.class,
-        OpenAIVisionAPIImplTest.class
+        OpenAIVisionAPIImplTest.class,
         ContentDriveHelperContentletAPIComparisonTest.class
 })
 
