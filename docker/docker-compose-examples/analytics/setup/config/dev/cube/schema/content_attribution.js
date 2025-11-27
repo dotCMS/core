58cf62e6d4c82d4e28dca407bf12f8fc9abe77f4
freddyDOTCMS <freddy0309@gmail.com>
2025-11-27 09:34:48 -0600

    conersion cube

---
+++ docker/docker-compose-examples/analytics/setup/config/dev/cube/schema/content_attribution.js
@@ -0,0 +1,98 @@
+/**
+ * This model calculates how often a piece of content appears in the path leading to a conversion.
+ * Let’s walk through an example to see what this means.
+ *
+ * Imagine we have the following events ordered by utc_time:
+ *
+ * page_view, url = /page-1, utc_time = 2025-11-26 08:09:49
+ * content_impression, content_id = 1, utc_time = 2025-11-26 08:09:59
+ * conversion, name = download, utc_time = 2025-11-26 08:10:09
+ * page_view, url = /page-1, utc_time = 2025-11-27 08:11:00
+ * content_click, content_id = 2, utc_time = 2025-11-27 08:11:31
+ * conversion, name = download, utc_time = 2025-11-28 08:10:39
+ *
+ * With this model, if we aggregate the data with a daily granularity, we would get results like:
+ *
+ * page_view — identifier: /page-1, day: 2025-11-26, count: 1
+ * content_impression — identifier: 1, day: 2025-11-26, count: 1
+ * content_click — identifier: 2, day: 2025-11-28, count: 1
+ * page_view — identifier: /page-1, day: 2025-11-28, count: 1
+ */
+
+cube('ContentAttribution', {
+   sql: `SELECT * FROM content_presents_in_conversion FINAL
+         WHERE ${FILTER_PARAMS.request.customerId.filter('customer_id')}
+           AND (
+             ${FILTER_PARAMS.request.clusterId ? FILTER_PARAMS.request.clusterId.filter('cluster_id') : '1=1'}
+             OR (${FILTER_PARAMS.request.clusterId ? 'FALSE' : '(cluster_id IS NULL OR cluster_id = \'\')'})
+        )`,
+   dimensions: {
+       id: {
+           sql: `concat(customer_id, '-', cluster_id,  '-', 'context_user_id',  '-', 'event_type','-', identifier, '-', title, '-', day)`,
+           type: `string`,
+           primaryKey: true
+       },
+       day: {
+           sql: `day`,
+           type: `time`,
+           title: 'Conversion Day',
+           description: 'Day when the conversion happend'
+       },
+       customerId: {
+           sql: `customer_id`,
+           type: `string`,
+           title: 'Customer ID',
+           description: 'Multi-tenant customer identifier for security filtering'
+       },
+       clusterId: {
+           sql: `cluster_id`,
+           type: `string`,
+           title: 'Cluster ID',
+           description: 'Multi-tenant cluster identifier for security filtering'
+       },
+       siteId: {
+           sql: `context_site_id`,
+           type: `string`,
+           title: 'Site ID',
+           description: 'dotCMS site identifier'
+       },
+       eventType: {
+           sql: `event_type`,
+           type: `string`,
+           title: 'Event Type',
+           description: 'Type of tracked event (pageview, content_impression, content_click, etc.)'
+       },
+       userId: {
+           sql: `context_user_id`,
+           type: `string`,
+           title: 'User ID',
+           description: 'Unique identifier for the user'
+       },
+       identifier: {
+           sql: `identifier`,
+           type: `string`,
+           title: 'Identifier',
+           description: 'If the content is a Page it is the URL, otherwise it is the content identifier'
+       },
+       title: {
+           sql: `title`,
+           type: `string`,
+           title: 'Identifier',
+           description: 'If the content is a Page it is the Page title, otherwise it is the content title field value'
+       },
+       conversionName: {
+           sql: `conversion_name`,
+           type: `string`,
+           title: 'Conversion Name',
+           description: 'Conversions this content helped achieve"',
+       }
+   },
+   measures: {
+       count: {
+           sql: `conversion_count`,
+           type: `sum`,
+           title: 'Conversions',
+           description: 'Total of conversion that this content helped achieve"'
+       }
+   }
+});
