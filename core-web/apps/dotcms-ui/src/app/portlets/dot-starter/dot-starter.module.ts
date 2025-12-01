774c054e950bb93140fe31adab5e27ada443e270
Freddy Montes <751424+fmontes@users.noreply.github.com>
2025-12-01 12:26:16 -0600

    revert dot-starter changes

--- core-web/apps/dotcms-ui/src/app/portlets/dot-starter/dot-starter.module.ts
+++ core-web/apps/dotcms-ui/src/app/portlets/dot-starter/dot-starter.module.ts
@@ -3,10 +3,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { CheckboxModule } from 'primeng/checkbox';
-import { DatePickerModule } from 'primeng/datepicker';
-import { InputTextModule } from 'primeng/inputtext';
-import { SelectModule } from 'primeng/select';
-import { TextareaModule } from 'primeng/textarea';
 
 import { DotMessagePipe } from '@dotcms/ui';
 
@@ -16,14 +12,9 @@ import { dotStarterRoutes } from './dot-starter.routes';
 
 import { DotToolbarAnnouncementsComponent } from '../../view/components/dot-toolbar/components/dot-toolbar-announcements/dot-toolbar-announcements.component';
 
-
 @NgModule({
     declarations: [DotStarterComponent],
     imports: [
-        SelectModule,
-        DatePickerModule,
-        InputTextModule,
-        TextareaModule,
         CommonModule,
         RouterModule.forChild(dotStarterRoutes),
         DotMessagePipe,
