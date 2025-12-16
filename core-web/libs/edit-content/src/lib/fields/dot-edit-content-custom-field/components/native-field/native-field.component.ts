bacfdf3ac559ec01ce33671e1665c5f01f06b5c5
Nicolas Molina Monroy <hi@nicobytes.com>
2025-12-16 17:03:52 -0500

    feat(dot-edit-content): Enhance native field component with styling and template updates

--- core-web/libs/edit-content/src/lib/fields/dot-edit-content-custom-field/components/native-field/native-field.component.ts
+++ core-web/libs/edit-content/src/lib/fields/dot-edit-content-custom-field/components/native-field/native-field.component.ts
@@ -70,6 +70,7 @@ export class NativeFieldComponent implements OnInit, OnDestroy {
      */
     $templateCode = computed(() => {
         const rendered = this.$field().rendered;
+        console.log('rendered 2as', rendered);
         return rendered;
     });
     /**
