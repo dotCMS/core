63dad54465b18742080547c4f881479b9029d1bc
Freddy Montes <751424+fmontes@users.noreply.github.com>
2025-12-18 19:49:48 -0600

    add blue

--- core-web/apps/dotcms-ui/src/app/theme.config.ts
+++ core-web/apps/dotcms-ui/src/app/theme.config.ts
@@ -2,6 +2,21 @@ import { definePreset } from '@primeuix/themes';
 import Lara from '@primeuix/themes/lara';
 
 export const CustomLaraPreset = definePreset(Lara, {
+    semantic: {
+        primary: {
+            '50': '#f0f3fe',
+            '100': '#dee3fb',
+            '200': '#c4cff9',
+            '300': '#9caff4',
+            '400': '#6c86ee',
+            '500': '#4b60e7',
+            '600': '#3540db',
+            '700': '#2c2fc9',
+            '800': '#2c2aa3',
+            '900': '#272881',
+            '950': '#1c1c4f'
+        }
+    },
     components: {
         treeselect: {
             tree: {
