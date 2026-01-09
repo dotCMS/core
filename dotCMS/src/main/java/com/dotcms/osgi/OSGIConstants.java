8edcb18c730ac6ed91b54147c72707d0149035c1
Will Ezell <will@dotcms.com>
2026-01-09 11:02:36 -0500

    testing pipeline

--- dotCMS/src/main/java/com/dotcms/osgi/OSGIConstants.java
+++ dotCMS/src/main/java/com/dotcms/osgi/OSGIConstants.java
@@ -9,4 +9,5 @@ public class OSGIConstants {
     public static final String BUNDLE_NAME_DOTCMS_SAML = "com.dotcms.samlbundle";
     public static final String BUNDLE_NAME_DOTCMS_TIKA = "com.dotcms.tika";
 
+    
 }
