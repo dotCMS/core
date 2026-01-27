cd1ef46679c9fae6b5f2ee9775b5eae5da9b448e
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2026-01-27 17:06:05 -0600

    #4148

--- dotcms-integration/src/test/java/com/dotcms/MainSuite3a.java
+++ dotcms-integration/src/test/java/com/dotcms/MainSuite3a.java
@@ -1,12 +1,9 @@
 package com.dotcms;
 
-import com.dotcms.content.index.VersionedIndicesAPITest;
-import com.dotcms.content.index.opensearch.OpenSearchClientConfigTest;
-import com.dotcms.content.index.opensearch.OpenSearchClientProviderIntegrationTest;
-import com.dotcms.cost.RequestCostReportTest;
 import com.dotcms.ai.api.OpenAIVisionAPIImplTest;
 import com.dotcms.contenttype.business.StoryBlockValidationTest;
 import com.dotcms.contenttype.test.StoryBlockUtilTest;
+import com.dotcms.cost.RequestCostReportTest;
 import com.dotcms.jitsu.validators.AnalyticsValidatorUtilTest;
 import com.dotcms.junit.MainBaseSuite;
 import com.dotcms.rest.api.v1.drive.ContentDriveHelperContentletAPIComparisonTest;
@@ -62,10 +59,7 @@ import org.junit.runners.Suite;
         Task251103AddStylePropertiesColumnInMultiTreeTest.class,
         StoryBlockValidationTest.class,
         StoryBlockUtilTest.class,
-        VersionedIndicesAPITest.class,
         Task251212AddVersionColumnIndicesTableTest.class,
-        OpenSearchClientProviderIntegrationTest.class,
-        OpenSearchClientConfigTest.class
 })
 
 public class MainSuite3a {
