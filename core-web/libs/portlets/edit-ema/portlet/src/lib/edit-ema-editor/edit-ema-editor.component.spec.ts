26aeadb5b3b9cf5a4d6da77e7eaf2ad57d108fb4
Kevin Davila <144152756+KevinDavilaDotCMS@users.noreply.github.com>
2026-01-29 14:45:31 -0500

    feat(style-editor): Hide Style editor palette on delete/move contentlets (#34440)
    https://github.com/user-attachments/assets/5bafe28b-0e29-42dc-933f-06a3d983f0b7
    
    
    This PR fixes: #34425

--- core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/edit-ema-editor.component.spec.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/edit-ema-editor.component.spec.ts
@@ -113,8 +113,8 @@ import {
 } from '../shared/mocks';
 import { ActionPayload, ContentTypeDragPayload } from '../shared/models';
 import { UVEStore } from '../store/dot-uve.store';
-import { SDK_EDITOR_SCRIPT_SOURCE, TEMPORAL_DRAG_ITEM } from '../utils';
 import * as uveUtils from '../utils';
+import { SDK_EDITOR_SCRIPT_SOURCE, TEMPORAL_DRAG_ITEM } from '../utils';
 
 global.URL.createObjectURL = jest.fn(
     () => 'blob:http://localhost:3000/12345678-1234-1234-1234-123456789012'
@@ -614,6 +614,350 @@ describe('EditEmaEditorComponent', () => {
                     expect(saveMock).toHaveBeenCalledWith([
                         { contentletsId: [], identifier: '123', personaTag: undefined, uuid: '123' }
                     ]);
+                });
+            });
+
+            describe('checkAndResetActiveContentlet', () => {
+                let resetActiveContentletSpy: jest.SpyInstance;
+
+                beforeEach(() => {
+                    resetActiveContentletSpy = jest.spyOn(store, 'resetActiveContentlet');
+                });
+
+                afterEach(() => {
+                    resetActiveContentletSpy.mockClear();
+                });
+
+                describe('on delete', () => {
+                    it('should reset activeContentlet when deleting the active contentlet', () => {
+                        const activeContentlet: ActionPayload = {
+                            pageId: '123',
+                            language_id: '1',
+                            container: {
+                                identifier: 'container-1',
+                                uuid: 'uuid-1',
+                                acceptTypes: 'test',
+                                maxContentlets: 1,
+                                contentletsId: ['contentlet-1']
+                            },
+                            pageContainers: [
+                                {
+                                    identifier: 'container-1',
+                                    uuid: 'uuid-1',
+                                    contentletsId: ['contentlet-1']
+                                }
+                            ],
+                            contentlet: {
+                                identifier: 'contentlet-1',
+                                inode: 'inode-1',
+                                title: 'Test',
+                                contentType: 'test'
+                            }
+                        };
+
+                        store.setActiveContentlet(activeContentlet);
+
+                        const payload: ActionPayload = {
+                            pageId: '123',
+                            language_id: '1',
+                            container: {
+                                identifier: 'container-1',
+                                uuid: 'uuid-1',
+                                acceptTypes: 'test',
+                                maxContentlets: 1,
+                                contentletsId: ['contentlet-1']
+                            },
+                            pageContainers: [
+                                {
+                                    identifier: 'container-1',
+                                    uuid: 'uuid-1',
+                                    contentletsId: ['contentlet-1']
+                                }
+                            ],
+                            contentlet: {
+                                identifier: 'contentlet-1',
+                                inode: 'inode-1',
+                                title: 'Test',
+                                contentType: 'test'
+                            }
+                        };
+
+                        store.setContentletArea({
+                            x: 100,
+                            y: 100,
+                            width: 500,
+                            height: 500,
+                            payload
+                        });
+
+                        spectator.detectChanges();
+
+                        const confirmDialog = spectator.query(byTestId('confirm-dialog'));
+
+                        spectator.triggerEventHandler(
+                            DotUveContentletToolsComponent,
+                            'deleteContent',
+                            payload
+                        );
+
+                        spectator.detectComponentChanges();
+
+                        confirmDialog
+                            .querySelector('.p-confirm-dialog-accept')
+                            .dispatchEvent(new Event('click'));
+
+                        expect(resetActiveContentletSpy).toHaveBeenCalledTimes(1);
+                    });
+
+                    it('should not reset activeContentlet when deleting a different contentlet', () => {
+                        const activeContentlet: ActionPayload = {
+                            pageId: '123',
+                            language_id: '1',
+                            container: {
+                                identifier: 'container-1',
+                                uuid: 'uuid-1',
+                                acceptTypes: 'test',
+                                maxContentlets: 1,
+                                contentletsId: ['contentlet-1', 'contentlet-2']
+                            },
+                            pageContainers: [
+                                {
+                                    identifier: 'container-1',
+                                    uuid: 'uuid-1',
+                                    contentletsId: ['contentlet-1', 'contentlet-2']
+                                }
+                            ],
+                            contentlet: {
+                                identifier: 'contentlet-1',
+                                inode: 'inode-1',
+                                title: 'Test',
+                                contentType: 'test'
+                            }
+                        };
+
+                        store.setActiveContentlet(activeContentlet);
+
+                        const payload: ActionPayload = {
+                            pageId: '123',
+                            language_id: '1',
+                            container: {
+                                identifier: 'container-1',
+                                uuid: 'uuid-1',
+                                acceptTypes: 'test',
+                                maxContentlets: 1,
+                                contentletsId: ['contentlet-1', 'contentlet-2']
+                            },
+                            pageContainers: [
+                                {
+                                    identifier: 'container-1',
+                                    uuid: 'uuid-1',
+                                    contentletsId: ['contentlet-1', 'contentlet-2']
+                                }
+                            ],
+                            contentlet: {
+                                identifier: 'contentlet-2',
+                                inode: 'inode-2',
+                                title: 'Other',
+                                contentType: 'test'
+                            }
+                        };
+
+                        store.setContentletArea({
+                            x: 100,
+                            y: 100,
+                            width: 500,
+                            height: 500,
+                            payload
+                        });
+
+                        spectator.detectChanges();
+
+                        const confirmDialog = spectator.query(byTestId('confirm-dialog'));
+
+                        spectator.triggerEventHandler(
+                            DotUveContentletToolsComponent,
+                            'deleteContent',
+                            payload
+                        );
+
+                        spectator.detectComponentChanges();
+
+                        confirmDialog
+                            .querySelector('.p-confirm-dialog-accept')
+                            .dispatchEvent(new Event('click'));
+
+                        expect(resetActiveContentletSpy).not.toHaveBeenCalled();
+                    });
+                });
+
+                describe('on move', () => {
+                    it('should reset activeContentlet when moving it to a different container', () => {
+                        const contentlet = CONTENTLETS_MOCK_FOR_EDITOR[0];
+                        const activeContentlet: ActionPayload = {
+                            pageId: '123',
+                            language_id: '1',
+                            container: {
+                                identifier: 'container-1',
+                                uuid: 'uuid-1',
+                                acceptTypes: 'Banner,Activity',
+                                maxContentlets: 25,
+                                contentletsId: [contentlet.identifier]
+                            },
+                            pageContainers: [
+                                {
+                                    identifier: 'container-1',
+                                    uuid: 'uuid-1',
+                                    contentletsId: [contentlet.identifier]
+                                }
+                            ],
+                            contentlet: {
+                                identifier: contentlet.identifier,
+                                inode: contentlet.inode,
+                                title: contentlet.title,
+                                contentType: contentlet.contentType
+                            }
+                        };
+
+                        store.setActiveContentlet(activeContentlet);
+
+                        const savePageSpy = jest.spyOn(store, 'savePage');
+
+                        store.setEditorDragItem({
+                            baseType: contentlet.baseType,
+                            contentType: contentlet.contentType,
+                            draggedPayload: {
+                                item: {
+                                    contentlet: {
+                                        ...contentlet,
+                                        identifier: contentlet.identifier
+                                    },
+                                    container: {
+                                        acceptTypes: 'Banner,Activity',
+                                        identifier: 'container-1',
+                                        maxContentlets: 25,
+                                        uuid: 'uuid-1'
+                                    }
+                                },
+                                type: 'contentlet',
+                                move: true
+                            }
+                        });
+
+                        const drop = new Event('drop');
+
+                        Object.defineProperty(drop, 'target', {
+                            writable: false,
+                            value: {
+                                dataset: {
+                                    dropzone: 'true',
+                                    position: 'before',
+                                    payload: JSON.stringify({
+                                        container: {
+                                            acceptTypes: 'Banner,Activity',
+                                            identifier: 'container-2',
+                                            maxContentlets: 25,
+                                            uuid: 'uuid-2'
+                                        },
+                                        contentlet: {
+                                            identifier: '456',
+                                            title: 'Pivot',
+                                            inode: 'pivot-inode',
+                                            contentType: 'Banner'
+                                        }
+                                    })
+                                }
+                            }
+                        });
+
+                        window.dispatchEvent(drop);
+
+                        expect(savePageSpy).toHaveBeenCalled();
+                        expect(resetActiveContentletSpy).toHaveBeenCalledTimes(1);
+                    });
+
+                    it('should not reset activeContentlet when moving it within the same container', () => {
+                        const contentlet = CONTENTLETS_MOCK_FOR_EDITOR[0];
+                        const activeContentlet: ActionPayload = {
+                            pageId: '123',
+                            language_id: '1',
+                            container: {
+                                identifier: 'container-1',
+                                uuid: 'uuid-1',
+                                acceptTypes: 'Banner,Activity',
+                                maxContentlets: 25,
+                                contentletsId: [contentlet.identifier, '456']
+                            },
+                            pageContainers: [
+                                {
+                                    identifier: 'container-1',
+                                    uuid: 'uuid-1',
+                                    contentletsId: [contentlet.identifier, '456']
+                                }
+                            ],
+                            contentlet: {
+                                identifier: contentlet.identifier,
+                                inode: contentlet.inode,
+                                title: contentlet.title,
+                                contentType: contentlet.contentType
+                            }
+                        };
+
+                        store.setActiveContentlet(activeContentlet);
+
+                        const savePageSpy = jest.spyOn(store, 'savePage');
+
+                        store.setEditorDragItem({
+                            baseType: contentlet.baseType,
+                            contentType: contentlet.contentType,
+                            draggedPayload: {
+                                item: {
+                                    contentlet: {
+                                        ...contentlet,
+                                        identifier: contentlet.identifier
+                                    },
+                                    container: {
+                                        acceptTypes: 'Banner,Activity',
+                                        identifier: 'container-1',
+                                        maxContentlets: 25,
+                                        uuid: 'uuid-1'
+                                    }
+                                },
+                                type: 'contentlet',
+                                move: true
+                            }
+                        });
+
+                        const drop = new Event('drop');
+
+                        Object.defineProperty(drop, 'target', {
+                            writable: false,
+                            value: {
+                                dataset: {
+                                    dropzone: 'true',
+                                    position: 'before',
+                                    payload: JSON.stringify({
+                                        container: {
+                                            acceptTypes: 'Banner,Activity',
+                                            identifier: 'container-1',
+                                            maxContentlets: 25,
+                                            uuid: 'uuid-1'
+                                        },
+                                        contentlet: {
+                                            identifier: '456',
+                                            title: 'Pivot',
+                                            inode: 'pivot-inode',
+                                            contentType: 'Banner'
+                                        }
+                                    })
+                                }
+                            }
+                        });
+
+                        window.dispatchEvent(drop);
+
+                        expect(savePageSpy).toHaveBeenCalled();
+                        expect(resetActiveContentletSpy).not.toHaveBeenCalled();
+                    });
                 });
             });
 
