98406e8cff437e676428e155da7071021941aefa
Adrian Molina <adrian.molina@dotcms.com>
2026-01-29 17:03:46 -0400

    Fix tests

--- core-web/libs/portlets/dot-content-drive/portlet/src/test-setup.ts
+++ core-web/libs/portlets/dot-content-drive/portlet/src/test-setup.ts
@@ -26,3 +26,13 @@ console.error = function (...data) {
 // We need to setup the ResizeObserver mock globally for testing
 // PrimeNG Tabs use this and fails if not setup
 setupResizeObserverMock();
+
+// Mock MutationObserver for PrimeNG components
+global.MutationObserver = class MutationObserver {
+    constructor(callback: MutationCallback) {}
+    disconnect() {}
+    observe(target: Node, options?: MutationObserverInit) {}
+    takeRecords(): MutationRecord[] {
+        return [];
+    }
+};
