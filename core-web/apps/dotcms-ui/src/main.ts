c1981dcf594da0899d133964ebb615b60c23b70d
Freddy Montes <751424+fmontes@users.noreply.github.com>
2026-02-04 12:46:10 -0600

    clean up

--- core-web/apps/dotcms-ui/src/main.ts
+++ core-web/apps/dotcms-ui/src/main.ts
@@ -3,7 +3,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { defineCustomElements } from '@dotcms/dotcms-webcomponents/loader';
-//
+
 import { AppComponent } from './app/app.component';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
