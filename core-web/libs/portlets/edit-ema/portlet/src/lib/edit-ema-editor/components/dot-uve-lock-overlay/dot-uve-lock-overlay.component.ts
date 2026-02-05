511bfac5b2553088957bb83cef3bf8308a807a4a
Rafael Velazco <rjvelazco21@gmail.com>
2026-02-05 13:27:35 -0400

    feedback(UVE): make controllers same height v4

--- core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-lock-overlay/dot-uve-lock-overlay.component.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/edit-ema-editor/components/dot-uve-lock-overlay/dot-uve-lock-overlay.component.ts
@@ -8,7 +8,9 @@ import { UVEStore } from '../../../store/dot-uve.store';
     selector: 'dot-uve-lock-overlay',
     imports: [DotMessagePipe],
     templateUrl: './dot-uve-lock-overlay.component.html',
-    styleUrls: ['./dot-uve-lock-overlay.component.scss']
+    host: {
+        class: 'block absolute top-0 left-0 w-full h-full z-10 pointer-events-none'
+    }
 })
 export class DotUveLockOverlayComponent {
     readonly #store = inject(UVEStore);
@@ -20,7 +22,7 @@ export class DotUveLockOverlayComponent {
 
         if (!isLocked) {
             return {
-                icon: 'pi pi-lock-open',
+                icon: 'pi pi-lock-open text-[1.75rem]!',
                 title: 'uve.editor.overlay.lock.unlocked.page.title',
                 message: 'uve.editor.overlay.lock.unlocked.page.description'
             };
@@ -29,7 +31,7 @@ export class DotUveLockOverlayComponent {
         if (!isLockedByCurrentUser) {
             return {
                 title: 'uve.editor.overlay.lock.locked.page.title',
-                icon: 'pi pi-lock',
+                icon: 'pi pi-lock text-[1.75rem]',
                 message: 'uve.editor.overlay.lock.locked.page.description'
             };
         }
