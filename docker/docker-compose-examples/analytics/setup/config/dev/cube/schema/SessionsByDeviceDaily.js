08ab173f4df4c94739160be5c1e9c1b129e55318
Jose Castro <jose.castro@dotcms.com>
2026-02-10 15:57:19 -0600

    Adding missing changes to other Cubes

--- docker/docker-compose-examples/analytics/setup/config/dev/cube/schema/SessionsByDeviceDaily.js
+++ docker/docker-compose-examples/analytics/setup/config/dev/cube/schema/SessionsByDeviceDaily.js
@@ -1,6 +1,6 @@
 /**
  * =====================================================================================
- * 2) SessionsByDeviceDaily Cube
+ * SessionsByDeviceDaily Cube
  * =====================================================================================
  *
  * Source table:
@@ -19,6 +19,7 @@
 cube(`SessionsByDeviceDaily`, {
     sql: `
         SELECT
+            cluster_id,
             customer_id,
             context_site_id,
             day,
@@ -28,6 +29,10 @@ cube(`SessionsByDeviceDaily`, {
             total_duration_engaged_seconds,
             updated_at
         FROM clickhouse_test_db.sessions_by_device_daily
+        WHERE ${FILTER_PARAMS.EngagementDaily.customerId.filter('customer_id')} AND (
+            ${FILTER_PARAMS.EngagementDaily.clusterId ? FILTER_PARAMS.EngagementDaily.clusterId.filter('cluster_id') : '1=1'}
+                OR (${FILTER_PARAMS.EngagementDaily.clusterId ? 'FALSE' : '(cluster_id IS NULL OR cluster_id = \'\')'})) AND
+            ${FILTER_PARAMS.EngagementDaily.day.filter('day')}
     `,
 
     measures: {
