b81d17373472a0841112b939185581d8bfeb2dbf
Kevin <kevin.davila@dotcms.com>
2026-03-05 18:48:27 -0500

    Added validation to show style editor button based on Feature flags

--- core-web/libs/portlets/edit-ema/portlet/src/lib/store/features/flags/models.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/store/features/flags/models.ts
@@ -2,7 +2,8 @@ import { FeaturedFlags } from '@dotcms/dotcms-models';
 
 type UVEFlagKeys =
     | FeaturedFlags.FEATURE_FLAG_UVE_TOGGLE_LOCK
-    | FeaturedFlags.FEATURE_FLAG_UVE_STYLE_EDITOR;
+    | FeaturedFlags.FEATURE_FLAG_UVE_STYLE_EDITOR
+    | FeaturedFlags.FEATURE_FLAG_UVE_STYLE_EDITOR_FOR_TRADITIONAL_PAGES;
 
 export type UVEFlags = { [K in UVEFlagKeys]?: boolean };
 
