37bf57f985fab29b5db71fafa2f3458b2aa2efd1
erickgonzalez <erick.gonzalez@dotcms.com>
2026-03-16 11:38:13 -0600

    test: testing auto-labeler safe
    Ref: #34993

--- dotCMS/src/main/java/com/dotcms/rest/api/v1/apps/AppsResource.java
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/apps/AppsResource.java
@@ -20,16 +20,7 @@ import java.util.List;
 import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.BeanParam;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -421,7 +412,7 @@ public class AppsResource {
             @Context final HttpServletRequest request,
             @Context final HttpServletResponse response,
             @PathParam("key") final String serviceKey,
-            @QueryParam("removeDescriptor") final boolean removeDescriptor
+            @QueryParam("removeDescriptor") @DefaultValue("true") final boolean removeDescriptor
     ) {
         try {
             final InitDataObject initData =
