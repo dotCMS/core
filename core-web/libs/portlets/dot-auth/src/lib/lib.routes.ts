bba71e43b869456ef71336b77e7899137f668d4c
Scott Wicken <1562170+swicken@users.noreply.github.com>
2026-04-20 11:20:06 -0400

    feat(dot-auth): list component, edit dialog, shell and routes

--- core-web/libs/portlets/dot-auth/src/lib/lib.routes.ts
+++ core-web/libs/portlets/dot-auth/src/lib/lib.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const dotAuthRoutes: Routes = [];
+import { DotAuthShellComponent } from './dot-auth-shell/dot-auth-shell.component';
+
+export const dotAuthRoutes: Routes = [
+    {
+        path: '',
+        component: DotAuthShellComponent
+    }
+];
