b223c83d0cefe9458288028a256336d994b63eed
ddariod <dario.daza@dotcms.com>
2026-04-21 15:35:06 -0500

    unify style editor FF for traditional and headless

--- core-web/libs/portlets/edit-ema/portlet/src/lib/store/features/editor/withEditor.ts
+++ core-web/libs/portlets/edit-ema/portlet/src/lib/store/features/editor/withEditor.ts
@@ -137,10 +137,6 @@ export function withEditor() {
             });
 
             const editorCanEditStyles = computed(() => {
-                if (store.pageType() === PageType.TRADITIONAL) {
-                    return store.flags()?.FEATURE_FLAG_UVE_STYLE_EDITOR_FOR_TRADITIONAL_PAGES;
-                }
-
                 return store.flags()?.FEATURE_FLAG_UVE_STYLE_EDITOR;
             });
 
