0b3c42f1eeb269645822511cc65d32521c111c39
Scott Wicken <1562170+swicken@users.noreply.github.com>
2026-05-04 14:35:03 -0400

    chore(dotauth): apply auto-formatting and add structuredClone polyfill
    Formatting changes from nx format:write plus a structuredClone polyfill
    for the test environment.

--- core-web/libs/portlets/dot-auth/src/test-setup.ts
+++ core-web/libs/portlets/dot-auth/src/test-setup.ts
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 
 setupZoneTestEnv({
     errorOnUnknownElements: true,
     errorOnUnknownProperties: true
 });
+
+if (!global.structuredClone) {
+    global.structuredClone = (obj: any) => JSON.parse(JSON.stringify(obj));
+}
