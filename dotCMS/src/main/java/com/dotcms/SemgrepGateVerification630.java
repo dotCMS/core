79a59fc36f73bc1964fda08480ea224b8f40f634
Mehdi <10160868+mbiuki@users.noreply.github.com>
2026-06-12 14:05:49 -0400

    Update SemgrepGateVerification630.java
    this is for test and discussion we are having right now to check the change made in the CUSTOM_INJECTION-1 from semgrep from comment mode to block mode to see if this is effective i.e. it is going to blocke this PR from merging

--- dotCMS/src/main/java/com/dotcms/SemgrepGateVerification630.java
+++ dotCMS/src/main/java/com/dotcms/SemgrepGateVerification630.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.*;
 
 /**
  * TEMPORARY verification fixture for dotCMS/private-issues#630.
