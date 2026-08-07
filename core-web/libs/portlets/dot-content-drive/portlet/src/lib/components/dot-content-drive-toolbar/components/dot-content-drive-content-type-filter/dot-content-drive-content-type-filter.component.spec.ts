d4ec7396ca7a56f3268bff4b3fdd9f84c5494492
Nicolas Molina Monroy <hi@nicobytes.com>
2026-08-07 13:53:46 -0400

    Reformat import statement to fit project line-length style

--- core-web/libs/portlets/dot-content-drive/portlet/src/lib/components/dot-content-drive-toolbar/components/dot-content-drive-content-type-filter/dot-content-drive-content-type-filter.component.spec.ts
+++ core-web/libs/portlets/dot-content-drive/portlet/src/lib/components/dot-content-drive-toolbar/components/dot-content-drive-content-type-filter/dot-content-drive-content-type-filter.component.spec.ts
@@ -1,9 +1,4 @@
-import {
-    createComponentFactory,
-    mockProvider,
-    Spectator,
-    SpyObject
-} from '@openng/spectator/jest';
+import { createComponentFactory, mockProvider, Spectator, SpyObject } from '@openng/spectator/jest';
 import { of } from 'rxjs';
 
 import { provideHttpClient } from '@angular/common/http';
